d6c24b80f4dbfd535550cc4789240715ad15ac69
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-05-23 12:35:12 +0200

    Layout/SpaceAfterComma

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -4,16 +4,16 @@ module WelcomeHelper
   end
 
   def sign_up_pro_button
-    link_to 'Je m\'inscris', new_pro_registration_path ,class: 'btn btn-primary'
+    link_to 'Je m\'inscris', new_pro_registration_path , class: 'btn btn-primary'
   end
 
   def sign_in_pro_button
-    link_to 'Se connecter', new_pro_session_path ,class: 'btn btn-outline-primary'
+    link_to 'Se connecter', new_pro_session_path , class: 'btn btn-outline-primary'
   end
 
   def link_logo
     link_to root_path do
-      holder_tag '200x50', 'Lapin Logo',class: 'img-fluid'
+      holder_tag '200x50', 'Lapin Logo', class: 'img-fluid'
     end
   end
 end
