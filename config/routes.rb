d8adde4eef059a390dc5fa03ba83c155e1de546f
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-05-28 16:08:50 +0200

    Add organisation dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   namespace :admin do
     resources :pros
     resources :super_admins
+    resources :organisations
     root to: "pros#index"
 
     authenticate :super_admin do
