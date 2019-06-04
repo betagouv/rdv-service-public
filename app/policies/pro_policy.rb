9c9a731ebd77bf2f6bc821fe35b009d1cf6d87eb
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-06-04 13:58:23 +0200

    Rename method

--- app/policies/pro_policy.rb
+++ app/policies/pro_policy.rb
@@ -10,11 +10,11 @@ class ProPolicy < ApplicationPolicy
   end
 
   def show?
-    same_pro
+    same_pro_or_admin?
   end
 
   def edit?
-    same_pro
+    same_pro_or_admin?
   end
 
   def invite?
@@ -27,7 +27,7 @@ class ProPolicy < ApplicationPolicy
 
   private
 
-  def same_pro
+  def same_pro_or_admin?
     @pro == @record || @pro.admin?
   end
 end
