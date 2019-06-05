7fd56282a06745fb5428b6205c5162edc9621703
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-06-05 15:16:48 +0200

    When destroying specialite, should destroy motifs

--- app/models/specialite.rb
+++ app/models/specialite.rb
@@ -1,7 +1,7 @@
 class Specialite < ApplicationRecord
   belongs_to :organisation
   has_many :pros
-  has_many :motifs
+  has_many :motifs, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :organisation }
   validates :organisation, presence: true
