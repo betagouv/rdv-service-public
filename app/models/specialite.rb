6f1d2cdc2a8741a68059a6c09c987d04ba9cf7b3
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-06-05 15:33:54 +0200

    When destroying specialite, nullify pros

--- app/models/specialite.rb
+++ app/models/specialite.rb
@@ -1,6 +1,6 @@
 class Specialite < ApplicationRecord
   belongs_to :organisation
-  has_many :pros
+  has_many :pros, dependent: :nullify
   has_many :motifs, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { scope: :organisation }
