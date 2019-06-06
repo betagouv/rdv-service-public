038b26c8c915ca64a21eead04e92559d01408a33
Beg <ben.guillou@gmail.com>
2019-06-06 15:03:41 +0200

    Admin can change specialite

--- db/migrate/20190606113052_populate_specialite.rb
+++ db/migrate/20190606113052_populate_specialite.rb
@@ -1,7 +1,7 @@
 class PopulateSpecialite < ActiveRecord::Migration[5.2]
   def change
-    %w( Médecin Infirmière Puéricultrice Sage-femme Psychologue Secrétaire ).each do |spe|
-      Specialite.create(name:  spe)
+    %w[Médecin Infirmière Puéricultrice Sage-femme Psychologue Secrétaire].each do |spe|
+      Specialite.create(name: spe)
     end
   end
 end
