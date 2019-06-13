3796c3b418fcaca94842fb234f77160d46038cb3
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-06-13 17:10:03 +0200

    Add uniqueness validation on :name

--- app/models/evenement_type.rb
+++ app/models/evenement_type.rb
@@ -2,5 +2,6 @@ class EvenementType < ApplicationRecord
   belongs_to :motif
   has_one :organisation, through: :motif
 
-  validates :name, :motif, :color, :default_duration_in_min, presence: true
+  validates :name, presence: true, uniqueness: { scope: :organisation }
+  validates :motif, :color, :default_duration_in_min, presence: true
 end
