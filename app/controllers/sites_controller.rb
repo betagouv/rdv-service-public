4967333970e911567bbd4b28e7d42b8962f1eed5
Beg <ben.guillou@gmail.com>
2019-06-17 18:41:02 +0200

    Modify site

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,26 +1,27 @@
 class SitesController < DashboardAuthController
+  respond_to :html, :json
+
+
   before_action :set_organisation
 
   def new
     @site = Site.new(organisation: @organisation)
     authorize(@site)
+    respond_right_bar_with @site
   end
 
   def create
     @site = Site.new(organisation: @organisation)
     @site.assign_attributes(site_params)
     authorize(@site)
-    if @site.save
-      flash.notice = "Site créé"
-      redirect_to @site.organisation
-    else
-      render :edit
-    end
+    flash.notice = "Site créé" if @site.save
+    respond_right_bar_with @site, location: @site.organisation
   end
 
   def edit
-    @site = Site.find(params[:id])
+    @site = policy_scope(Site).find(params[:id])
     authorize(@site)
+    respond_right_bar_with @site
   end
 
   def update
@@ -46,6 +47,6 @@ class SitesController < DashboardAuthController
   private
 
   def site_params
-    params.require(:site).permit(:name, :address)
+    params.require(:site).permit(:name, :address, :horaires, :telephone)
   end
 end
