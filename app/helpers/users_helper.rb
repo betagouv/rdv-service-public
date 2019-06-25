e04d72b9165ea7d309afe07d93ac448a3679bb27
Beg <ben.guillou@gmail.com>
2019-06-25 14:18:30 +0200

    Rubocop fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -9,7 +9,7 @@ module UsersHelper
     link_to 'Ajouter un usager', new_organisation_user_path, class: "btn #{btn_style}", data: { rightbar: true } if policy(User).create?
   end
 
-  def formatted_phone_number user
+  def formatted_phone_number(user)
     user.phone_number.chars.each_slice(2).map(&:join).join(' ') unless user.phone_number.blank?
   end
 end
