a74e3337e75283e561e966790f5e333e471280e6
Beg <ben.guillou@gmail.com>
2019-06-25 14:17:24 +0200

    Fix phone number formatting (basic group by 2)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -8,4 +8,8 @@ module UsersHelper
   def add_user_button(btn_style = 'btn-primary')
     link_to 'Ajouter un usager', new_organisation_user_path, class: "btn #{btn_style}", data: { rightbar: true } if policy(User).create?
   end
+
+  def formatted_phone_number user
+    user.phone_number.chars.each_slice(2).map(&:join).join(' ') unless user.phone_number.blank?
+  end
 end
