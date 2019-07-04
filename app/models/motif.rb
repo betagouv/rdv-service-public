95a360abe8b9a2f840b292d67755579764d5ce2d
Beg <ben.guillou@gmail.com>
2019-07-04 14:20:58 +0200

    Fix mignon's sheet

--- app/models/motif.rb
+++ app/models/motif.rb
@@ -2,7 +2,8 @@ class Motif < ApplicationRecord
   belongs_to :organisation
   belongs_to :specialite
   has_many :rdvs, dependent: :restrict_with_exception
-
+  has_and_belongs_to_many :plage_ouvertures
+  
   validates :name, presence: true, uniqueness: { scope: :organisation }
   validates :color, :default_duration_in_min, presence: true
 end
