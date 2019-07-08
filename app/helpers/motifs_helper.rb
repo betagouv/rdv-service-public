876189bfa02403dc099a5df86b1428b279ad1361
Beg <ben.guillou@gmail.com>
2019-07-08 11:57:05 +0200

    Fix rubocop

--- app/helpers/motifs_helper.rb
+++ app/helpers/motifs_helper.rb
@@ -3,7 +3,7 @@ module MotifsHelper
     link_to "Créer un motif", new_organisation_motif_path(@organisation.id), class: "btn #{btn_style}", data: { rightbar: true } if policy(Motif).create?
   end
 
-  def online_badge motif
-    content_tag(:span, 'En ligne', class:'badge badge-danger') if motif.online
+  def online_badge(motif)
+    content_tag(:span, 'En ligne', class: 'badge badge-danger') if motif.online
   end
 end
