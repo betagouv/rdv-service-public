33012b7741f3b1f2144a847f30f4c7e979bf62f3
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-07-09 16:14:15 +0200

    Add pros to rdvs

--- app/models/rdv.rb
+++ app/models/rdv.rb
@@ -2,6 +2,7 @@ class Rdv < ApplicationRecord
   belongs_to :organisation
   belongs_to :motif
   belongs_to :user
+  has_and_belongs_to_many :pros
 
   validates :user, :organisation, :motif, :start_at, :duration_in_min, presence: true
   validates :max_users_limit, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
