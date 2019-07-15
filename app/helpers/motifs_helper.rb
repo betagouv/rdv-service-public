437f06de97eed7051927e7d8dee1c8a51878b409
Beg <ben.guillou@gmail.com>
2019-07-15 16:56:33 +0200

    Rubocop

--- app/helpers/motifs_helper.rb
+++ app/helpers/motifs_helper.rb
@@ -8,7 +8,7 @@ module MotifsHelper
   end
 
   def min_max_delay_options
-    [["1/2 heure", 30.minutes],["1 heure", 1.hour], ["2 heures", 2.hour],
+    [["1/2 heure", 30.minutes], ["1 heure", 1.hour], ["2 heures", 2.hour],
      ["3 heures", 3.hour], ["6 heures", 6.hour], ["12 heures", 12.hour],
      ["1 jour", 1.day], ["2 jours", 2.day], ["3 jours", 3.day], ["1 semaine", 1.week], ["2 semaines", 1.week],
      ["1 mois", 1.month], ["2 mois", 2.month], ["3 mois", 3.month], ["6 mois", 6.month], ["1 an", 1.year]]
