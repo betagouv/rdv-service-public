ed6007ff93244ba4b0f0b47e99514a00dad86c3e
Beg <ben.guillou@gmail.com>
2019-08-21 14:29:14 +0200

    Typo

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,4 +12,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w[print.css mail.css landing.js landing.css]
-Rails.application.config.assets.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
+Rails.application.config.assets.precompile << /\.(?:svg|eot|woff|ttf)\z/
