98dfb060de281929c905ca4ab065faa052bbfe46
Beg <ben.guillou@gmail.com>
2019-09-24 10:49:52 +0200

    Fix update account for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -37,6 +37,7 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :service_id])
     elsif resource_class == User
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :birth_date])
     end
   end
 end
