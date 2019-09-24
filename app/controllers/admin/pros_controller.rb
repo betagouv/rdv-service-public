ff86c3503956898d495e798930ef84b78bdac10e
Beg <ben.guillou@gmail.com>
2019-09-24 14:59:33 +0200

    Signout user when sign_in_as pro

--- app/controllers/admin/pros_controller.rb
+++ app/controllers/admin/pros_controller.rb
@@ -1,6 +1,7 @@
 module Admin
   class ProsController < Admin::ApplicationController
     def sign_in_as
+      sign_out(:user)
       pro = Pro.find(params[:id])
       sign_in(:pro, pro, bypass: true)
       redirect_to root_url
