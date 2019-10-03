8a3a4398b894ca792962ecdeb4f7dbd43a2e86cd
Beg <ben.guillou@gmail.com>
2019-10-03 11:17:46 +0200

    Using scope for Motif.active

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,13 +4,9 @@ class Service < ApplicationRecord
   belongs_to :organisation
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
-  def active_motifs
-    motifs.active
-  end
-
   def self.with_online_motif_in_departement(departement)
     organisations_ids_from_departement = Organisation.where(departement: departement).pluck(:id)
     services_ids_with_at_least_one_motif = Motif.active.online.where(organisation_id: organisations_ids_from_departement).joins(:plage_ouvertures).pluck(:service_id).uniq
-    Service.where(id: services_ids_with_at_least_one_motif).includes(:motifs).where(motifs: { deleted_at: nil })
+    Service.where(id: services_ids_with_at_least_one_motif).includes(:motifs).merge(Motif.active).references(:motifs)
   end
 end
