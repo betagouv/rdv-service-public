147d68cf46e0e926b369919f64bf5114bc492d2d
Beg <ben.guillou@gmail.com>
2019-10-28 12:00:49 +0100

    Remove Secretariat motifs from new

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,4 +2,5 @@ class Service < ApplicationRecord
   has_many :agents, dependent: :nullify
   has_many :motifs, dependent: :destroy
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+  scope :with_motifs, -> { where.not(name: 'Secrétariat') }
 end
