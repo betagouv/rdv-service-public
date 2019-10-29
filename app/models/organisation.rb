bc195b0e02062bc45c5dac2be254ffca50117809
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-10-29 18:20:32 +0100

    Rdvs nested in organisations

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -3,6 +3,7 @@ class Organisation < ApplicationRecord
   has_many :lieux, dependent: :destroy
   has_many :motifs, dependent: :destroy
   has_many :absences, dependent: :destroy
+  has_many :rdvs, dependent: :destroy
 
   validates :name, presence: true
   validates :departement, presence: true, length: { is: 2 }
