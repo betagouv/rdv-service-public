6bc1e5d0904f39312aa8aed3cd4c83ee8e5e8ad9
jjf21 <carl.quivron@gmail.com>
2019-11-12 15:12:14 +0100

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Organisation.create!(name: "MDS du 75")
+Organisation.create!(name: "MDS du 75", departement: "59")
 
 Lieu.create!(name: "Maison Gare du Nord", telephone: "0123456789", organisation_id: 1, address: "1 rue de Maubeuge, 75009 Paris", horaires: "Du lundi au vendredi de 9h à 18h")
 
 100.times do |_u|
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, organisation_id: 1, birth_date: Faker::Date.birthday)
+  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, birth_date: Faker::Date.birthday, password: Faker::Internet.password, organisation_ids: [1])
 end
 
 Service.create!(name: "Protection Maternelle Infantile")
