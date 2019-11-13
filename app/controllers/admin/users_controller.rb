dc41b9c56ca10bf6aff471b00e8b81c6b902f152
jjf21 <carl.quivron@gmail.com>
2019-11-13 17:39:59 +0100

    add a button to logged in as user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,21 +1,12 @@
 module Admin
   class UsersController < Admin::ApplicationController
-    # To customize the behavior of this controller,
-    # you can overwrite any of the RESTful actions. For example:
-    #
-    # def index
-    #   super
-    #   @resources = User.
-    #     page(params[:page]).
-    #     per(10)
-    # end
 
-    # Define a custom finder by overriding the `find_resource` method:
-    # def find_resource(param)
-    #   User.find_by!(slug: param)
-    # end
-
-    # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
-    # for more information
+    def sign_in_as
+      sign_out(:user)
+      user = User.find(params[:id])
+      sign_in(:user, user, bypass: true)
+      redirect_to root_url
+    end
+    
   end
 end
