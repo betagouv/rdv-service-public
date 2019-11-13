bc2e405d7168c5797ef1a7776f3801f0837bd9ec
jjf21 <carl.quivron@gmail.com>
2019-11-13 17:42:51 +0100

    clean rubocop

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,10 @@
 module Admin
   class UsersController < Admin::ApplicationController
-
     def sign_in_as
       sign_out(:user)
       user = User.find(params[:id])
       sign_in(:user, user, bypass: true)
       redirect_to root_url
     end
-    
   end
 end
