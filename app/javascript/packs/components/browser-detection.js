8c423d130670bf1ac7f6c38772d8f7b2fda32adf
jjf21 <carl.quivron@gmail.com>
2019-11-14 11:12:14 +0100

    change browser version

--- app/javascript/packs/components/browser-detection.js
+++ app/javascript/packs/components/browser-detection.js
@@ -3,17 +3,13 @@ const browser = Bowser.getParser(window.navigator.userAgent);
 
 document.addEventListener('turbolinks:load', function() {
   const oldBrowser = browser.satisfies({
-    chrome: "<29",
-    firefox: "<28",
-    opera: "<15",
-    edge: "<12",
-    ie: '<11'
+    chrome: "<78",
+    firefox: "<70",
+    edge: "<17",
   });
 
-  if (isInvalidValidBrowser){
-    let html = "<div id='browser-upgrade' class='text-center bg-warning py-1'><b>Votre navigateur n'est pas optimal pour utiliser Lapins.</b> <a href='http://browsehappy.com/' target='_blank'>Télécharger un navigateur récent</a>  (Chrome, Firefox, Safari, InternetExplorer 11) </div>"
   if (oldBrowser){
-    let html = "<div id='browser-upgrade' class='text-center bg-warning py-1'><b>Votre navigateur n'est pas optimal pour utiliser Lapins.</b> <a href='http://browsehappy.com/' target='_blank'>Télécharger un navigateur récent</a>  (Chrome, Firefox, Safari, Internet Explorer 11) </div>"
+    let html = "<div id='browser-upgrade' class='text-center bg-warning py-1'><b>Votre navigateur n'est pas optimal pour utiliser Lapins.</b> <a href='http://browsehappy.com/' target='_blank'>Télécharger un navigateur récent</a>  (Chrome, Firefox, Edge) </div>"
     if ($('#browser-upgrade').length == 0){
       $('body').prepend(html)
     }
