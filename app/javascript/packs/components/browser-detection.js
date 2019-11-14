b45013bdf4a6f18e5f2b2d21d33e932f398d4d08
jjf21 <carl.quivron@gmail.com>
2019-11-14 09:23:47 +0100

    change variable name + correct text

--- app/javascript/packs/components/browser-detection.js
+++ app/javascript/packs/components/browser-detection.js
@@ -2,7 +2,7 @@ import Bowser from "bowser";
 const browser = Bowser.getParser(window.navigator.userAgent);
 
 document.addEventListener('turbolinks:load', function() {
-  const isInvalidValidBrowser = browser.satisfies({
+  const oldBrowser = browser.satisfies({
     chrome: "<29",
     firefox: "<28",
     opera: "<15",
@@ -12,6 +12,8 @@ document.addEventListener('turbolinks:load', function() {
 
   if (isInvalidValidBrowser){
     let html = "<div id='browser-upgrade' class='text-center bg-warning py-1'><b>Votre navigateur n'est pas optimal pour utiliser Lapins.</b> <a href='http://browsehappy.com/' target='_blank'>Télécharger un navigateur récent</a>  (Chrome, Firefox, Safari, InternetExplorer 11) </div>"
+  if (oldBrowser){
+    let html = "<div id='browser-upgrade' class='text-center bg-warning py-1'><b>Votre navigateur n'est pas optimal pour utiliser Lapins.</b> <a href='http://browsehappy.com/' target='_blank'>Télécharger un navigateur récent</a>  (Chrome, Firefox, Safari, Internet Explorer 11) </div>"
     if ($('#browser-upgrade').length == 0){
       $('body').prepend(html)
     }
