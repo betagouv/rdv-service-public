fabeba34abba6d1679f12e3b7c52cdf2784b46b1
jjf21 <carl.quivron@gmail.com>
2019-11-18 18:09:35 +0100

    rubocop

--- app/services/twilio_text_messenger.rb
+++ app/services/twilio_text_messenger.rb
@@ -12,11 +12,10 @@ class TwilioTextMessenger
     message += "RDV #{@rdv.motif.name} #{I18n.l(@rdv.starts_at, format: :human)} (durée : #{@rdv.duration_in_min} minutes) a été confirmé.\n"
     if @rdv.motif.by_phone
       message += "RDV Tél au #{@user.phone_number}.\n"
-      #{Rails.application.routes.url_helpers.users_rdvs_url(host: ActionMailer::Base.default_url_options)}
     else
       message += "Adresse: #{@rdv.location}.\n"
     end
-    message += "Infos et annulation: .\n"
+    message += "Infos et annulation: link.\n"
 
     twilio_client = Twilio::REST::Client.new
     twilio_client.messages.create({
