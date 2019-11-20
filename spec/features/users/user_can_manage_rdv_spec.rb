e9edeb96e50ca8688ddaf396c3905f415a42acb4
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-11-20 12:45:00 +0100

    If rdv is no longer cancellable tell user to call the MDS

--- spec/features/users/user_can_manage_rdv_spec.rb
+++ spec/features/users/user_can_manage_rdv_spec.rb
@@ -1,36 +1,32 @@
 describe "User can manage their rdvs" do
-  let(:organisation) { create(:organisation) }
-  let(:user) { create(:user, organisations: [organisation]) }
+  let(:rdv) { create(:rdv, starts_at: starts_at) }
+  let(:user) { rdv.users.first }
 
   before do
-    visit new_user_session_path
-    sign_in(user)
+    login_as(user, scope: :user)
+    visit users_rdvs_path
   end
 
   context "when cancellable" do
-    let!(:rdv) { create(:rdv, users: [user], organisation: organisation, starts_at: 5.hours.from_now) }
+    let(:starts_at) { 5.hours.from_now }
 
     scenario "default", js: true do
-      visit users_rdvs_path
       expect(page).to have_content(rdv.motif.name)
       expect(page).to have_selector('li', text: "Annuler le RDV")
       click_link("Annuler le RDV")
       alert = page.driver.browser.switch_to.alert
       alert.accept
-      expect(page).not_to have_content(rdv.motif.name)
+      expect(page).to have_selector('.badge', text: "Annulé")
     end
   end
 
   context "when not cancellable" do
-    let!(:rdv) { create(:rdv, users: [user], organisation: organisation, starts_at: 4.hours.from_now) }
-    let!(:rdv2) { create(:rdv, users: [user], organisation: organisation, starts_at: 4.hours.ago) }
+    let(:starts_at) { 4.hours.from_now }
 
     scenario "default", js: true do
-      visit users_rdvs_path
       expect(page).to have_content(rdv.motif.name)
-      expect(page).to have_content(rdv2.motif.name)
       expect(page).not_to have_selector('li', text: "Annuler le RDV")
-      expect(page).to have_selector('span', text: "Vous ne pouvez plus annuler ce RDV en ligne.")
+      expect(page).to have_selector('li', text: "Ce rendez-vous commence dans moins de 4 heures, il n'est plus annulable en ligne.")
     end
   end
 end
