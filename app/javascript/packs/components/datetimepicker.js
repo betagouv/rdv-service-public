11ec835b823e76aaaf38a787c1628117ff685b60
jjf21 <carl.quivron@gmail.com>
2019-11-28 17:07:43 +0100

    disable scrolling options

--- app/javascript/packs/components/datetimepicker.js
+++ app/javascript/packs/components/datetimepicker.js
@@ -8,17 +8,22 @@ class Datetimepicker {
       format:'d/m/Y',
       timepicker:false,
       mask: true,
+      scrollMonth: false,
+      scrollInput: false,
     });
     $("[data-behaviour='datetimepicker']").datetimepicker({
       format:'d/m/Y H:i',
       mask: false,
       step: 5,
+      scrollMonth: false,
+      scrollInput: false,
     });
     $("[data-behaviour='timepicker']").datetimepicker({
       format:'H:i',
       step: 5,
       datepicker: false,
       mask: true,
+      scrollInput: false,
     });
   }
 }
