18ee1d82db1a94e7c06f0efa7ab01589397344f6
Mathieu Magnin <mathieu.magnin@gmail.com>
2019-12-10 19:31:30 +0100

    Restrict sign in as to ENVs with no sensitive data

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,15 @@
 module Admin
   class UsersController < Admin::ApplicationController
     def sign_in_as
-      sign_out(:agent)
       user = User.find(params[:id])
-      sign_in(:user, user, bypass: true)
-      redirect_to root_url
+      if sign_in_as_allowed?
+        sign_out(:agent)
+        sign_in(:user, user, bypass: true)
+        redirect_to root_url
+      else
+        flash[:error] = "Fonctionnalité désactivée sur cet environnement."
+        redirect_to admin_user_path(user)
+      end
     end
   end
 end
