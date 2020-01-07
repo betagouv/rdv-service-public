bfcb11da49ae932111e5e9b31fd74eb50f04c798
jjf21 <carl.quivron@gmail.com>
2020-01-07 11:39:08 +0100

    add a simple validation on phone number organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -8,4 +8,5 @@ class Organisation < ApplicationRecord
 
   validates :name, presence: true
   validates :departement, presence: true, length: { is: 2 }
+  validates :phone_number, numericality: { allow_blank: true, only_integer: true} , length: { maximum: 10 }
 end
