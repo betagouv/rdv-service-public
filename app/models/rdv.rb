0e850ce006e17b65d4cbafb7b010f5bcb0b54a52
jjf21 <carl.quivron@gmail.com>
2020-01-22 18:04:53 +0100

    add feature spec

--- app/models/rdv.rb
+++ app/models/rdv.rb
@@ -93,7 +93,7 @@ class Rdv < ApplicationRecord
   end
 
   def available_to_file_attente?
-    !cancelled? && starts_at > 14.days.from_now
+    !cancelled? && (starts_at - Time.now) > motif.min_booking_delay
   end
 
   private
--- spec/features/users/user_can_manage_rdv_spec.rb
+++ spec/features/users/user_can_manage_rdv_spec.rb
@@ -28,4 +28,22 @@ describe "User can manage their rdvs" do
       expect(page).to have_selector('p.font-italic', text: "Ce rendez-vous commence dans moins de 4 heures, il n'est plus annulable en ligne.")
     end
   end
+
+  context "when available for file attente" do
+    let(:starts_at) { 15.days.from_now }
+    scenario "default", js: true do
+      expect(page).to have_content("Je souhaite être prévenu si un créneau se libère.")
+      check 'active_0'
+      expect(page).to have_content("Vous êtes à présent sur la liste d'attente")
+      uncheck 'active_0'
+      expect(page).to have_content("Vous n'êtes plus sur la liste d'attente")
+    end
+  end
+
+  context "when not available for file attente" do
+    let(:starts_at) { 30.minutes.from_now }
+    scenario "default", js: true do
+      expect(page).not_to have_content("Je souhaite être prévenu si un créneau se libère.")
+    end
+  end
 end
