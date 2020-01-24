265a8ddfed877f05f170ef7416e2974a2baf1127
Mathieu Magnin <mathieu.magnin@gmail.com>
2020-01-24 12:19:22 +0100

    Display jours feries on agenda

--- config/routes.rb
+++ config/routes.rb
@@ -100,6 +100,7 @@ Rails.application.routes.draw do
           resources step, only: [:new, :create], module: "rdvs"
         end
       end
+      resources :jours_feries, only: [:index]
     end
   end
 
