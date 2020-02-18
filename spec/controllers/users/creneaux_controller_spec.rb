cca9682d9ce457b774c60df46754555794493895
jjf21 <carl.quivron@gmail.com>
2020-02-18 11:09:52 +0100

    tests x3

--- spec/controllers/users/creneaux_controller_spec.rb
+++ spec/controllers/users/creneaux_controller_spec.rb
@@ -28,7 +28,8 @@ RSpec.describe Users::CreneauxController, type: :controller do
 
       before { subject }
 
-      it { expect(response.body).to include("Voici les créneaux disponibles pour avancer votre rendez-vous.") }
+      it { expect(response.body).to include("Voici les créneaux disponibles pour avancer votre rendez-vous du") }
+      it { expect(response.body).to include("#{I18n.l(rdv.starts_at, format: :human)}") }
       it { expect(assigns(:date_range)).to eq(3.days.from_now.to_date..3.days.from_now.to_date) }
       it { expect(assigns(:creneaux)).not_to be_empty }
     end
