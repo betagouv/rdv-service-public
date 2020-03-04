a3c3787a6d611ee91aa81547035dea3afc490822
jjf21 <carl.quivron@gmail.com>
2020-03-04 13:08:35 +0100

    agenda

--- app/policies/agent/agent_policy.rb
+++ app/policies/agent/agent_policy.rb
@@ -4,7 +4,7 @@ class Agent::AgentPolicy < Agent::AdminPolicy
   end
 
   def show?
-    @context.agent.can_access_others_planning? || @context.agent == @record
+   same_agent_or_has_access?
   end
 
   def reinvite?
