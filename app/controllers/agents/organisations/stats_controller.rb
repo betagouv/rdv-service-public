095cc8e7c6ceeb0b4691db53bd26b0b02a78f0f7
Beg <ben.guillou@gmail.com>
2020-03-11 17:31:59 +0100

    Commit controller

---
+++ app/controllers/agents/organisations/stats_controller.rb
@@ -0,0 +1,19 @@
+class Agents::Organisations::StatsController < AgentAuthController
+  respond_to :html, :json
+
+  before_action :set_organisation
+
+  def index
+    @stats = Stat.new(rdvs: policy_scope(Rdv), users: policy_scope(User))
+  end
+
+  def rdvs
+    authorize(@organisation)
+    render json: Stat.new(rdvs: policy_scope(Rdv)).rdv_group_by_week_fr.chart_json
+  end
+
+  def users
+    authorize(@organisation)
+    render json: Stat.new(users: policy_scope(User)).users_group_by_week
+  end
+end
