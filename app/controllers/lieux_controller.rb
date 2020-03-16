1df34558363f7cd0cbc4a03792fbaff7ee9133b3
Beg <ben.guillou@gmail.com>
2020-03-16 17:37:31 +0100

    [Coronavirus] Annonce home et bloquage de la prise de RDV en ligne

--- app/controllers/lieux_controller.rb
+++ app/controllers/lieux_controller.rb
@@ -24,9 +24,14 @@ class LieuxController < ApplicationController
     start_date = params[:date]&.to_date || Date.today
     @date_range = start_date..(start_date + 6.days)
     @lieu = Lieu.find(params[:id])
-    @creneaux = Creneau.for_motif_and_lieu_from_date_range(@motif, @lieu, @date_range)
 
-    @next_availability = @creneaux.empty? ? Creneau.next_availability_for_motif_and_lieu(@motif, @lieu, @date_range.end) : nil
+    if Flipflop.corona?
+      @creneaux = []
+      @next_availability = nil
+    else
+      @creneaux = Creneau.for_motif_and_lieu_from_date_range(@motif, @lieu, @date_range)
+      @next_availability = @creneaux.empty? ? Creneau.next_availability_for_motif_and_lieu(@motif, @lieu, @date_range.end) : nil
+    end
     @max_booking_delay = Motif.active.online.joins(:organisation).where(organisations: { departement: @departement }, name: @motif).maximum('max_booking_delay')
     respond_to do |format|
       format.html
