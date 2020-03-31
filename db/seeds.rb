2307a53261127fe89de3f23ce55a923b6d9d1690
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-03-31 08:56:32 +0200

    improve seeds and add ruby-gemset to gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,53 @@
-Organisation.create!(name: "MDS du 75", phone_number: "0123456789", departement: "75")
+org1 = Organisation.create!(name: "MDS du 75", phone_number: "0123456789", departement: "75")
 
-Service.create!(name: "Protection Maternelle Infantile", short_name: "PMI")
-Service.create!(name: "Service Social", short_name: "Service Social")
+service1 = Service.create!(name: "Protection Maternelle Infantile", short_name: "PMI")
+_service2 = Service.create!(name: "Service Social", short_name: "Service Social")
 
-Lieu.create!(name: "Maison Paris Sud", organisation_id: 1, address: "18 Rue des Terres au Curé, 75013 Paris", latitude: 48.85295, longitude: 2.34998)
+lieu1 = Lieu.create!(
+  name: "Maison Paris Sud",
+  organisation: org1,
+  address: "18 Rue des Terres au Curé, 75013 Paris",
+  latitude: 48.85295,
+  longitude: 2.34998
+)
 
-Motif.create!(name: 'Consultation médicale', color: '#FF7C00', organisation_id: 1, service_id: 1)
+motif1 = Motif.create!(
+  name: 'Consultation médicale',
+  color: '#FF7C00',
+  organisation: org1,
+  service: service1,
+  online: true
+)
 
 3.times do |_u|
-  User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, birth_date: Faker::Date.birthday, password: Faker::Internet.password, organisation_ids: [1])
+  User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    birth_date: Faker::Date.birthday,
+    password: '123456',
+    organisations: [org1]
+  )
 end
 
-Agent.create!(email: 'contact@rdv-solidarites.fr', role: 1, first_name: 'Johnny', last_name: 'Validay', password: '123456', service_id: 1, organisation_ids: [1])
+agent1 = Agent.create!(
+  email: 'contact@rdv-solidarites.fr',
+  role: 1, # == admin
+  first_name: 'Johnny',
+  last_name: 'Validay',
+  password: '123456',
+  service: service1,
+  organisations: [org1]
+)
+
+_plage_ouverture1 = PlageOuverture.create!(
+  title: 'Permanence classique',
+  organisation: org1,
+  agent: agent1,
+  lieu: lieu1,
+  motifs: [motif1],
+  first_day: Date.tomorrow,
+  start_time: Tod::TimeOfDay.new(8),
+  end_time: Tod::TimeOfDay.new(12),
+  recurrence: Montrose.every(:day)
+)
