c23094531f9f50d5ceb69a7ebdb574df28fc6529
Yannick Francois <yaf@elsif.fr>
2020-04-06 15:35:41 +0200

    initialise matomo

--- app/javascript/packs/components/analytic.js
+++ app/javascript/packs/components/analytic.js
@@ -2,14 +2,15 @@ class Analytic {
 
   constructor() {
     var _paq = window._paq || [];
+    _paq.push(['trackPageView']);
     _paq.push(['enableLinkTracking']);
     (function() {
       var u="//stats.data.gouv.fr/";
       _paq.push(['setTrackerUrl', u+'piwik.php']);
       _paq.push(['setSiteId', ENV.MATOMO_APP_ID]);
       var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-    })();
+      g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js';
+      s.parentNode.insertBefore(g,s); })();
   }
 
   trackPageView(partialUrl) {
@@ -20,18 +21,27 @@ class Analytic {
       'invitation_token', 'confirmation_token', 'unlock_token',
       'reset_password_token'];
 
+    paramsToFilter.forEach(function(paramToFilter) {
+      let expression = new RegExp(`${paramToFilter}=([^&]+)`);
+      href = href.replace(expression, '');
+    });
 
-    paramsToFilter.forEach(function(paramToFilter) { let expression = new
-        RegExp(`${paramToFilter}=([^&]+)`); href = href.replace(expression,
-          ''); });
+    if (window._paq) {
+      _paq.push(['setCustomUrl', href.split('#')[0]]);
+      if (partialUrl) {
+        _paq.push(['setCustomUrl', partialUrl]);
+      }
+      _paq.push(['trackPageView']);
+    }
 
-    if (window._paq) { _paq.push(['setCustomUrl', href.split('#')[0]]); if
-      (partialUrl) { _paq.push(['setCustomUrl', partialUrl]); }
-      _paq.push(['trackPageView']); } }
+  }
 
-  trackModalView(evt) { this.trackPageView($(evt.currentTarget).data("url")) }
+  trackModalView(evt) {
+    this.trackPageView($(evt.currentTarget).data("url"))
+  }
 
-  trackRightbarView(evt) { this.trackPageView($(evt.currentTarget).data("url"))
+  trackRightbarView(evt) {
+    this.trackPageView($(evt.currentTarget).data("url"))
   }
 
 }
