11f98a44404633417ba1e704e13ec83b3a505edc
jjf21 <carl.quivron@gmail.com>
2020-04-07 14:15:09 +0200

    improve test

--- spec/features/agents/agent_can_initiate_rdv_from_calendar.rb
+++ spec/features/agents/agent_can_initiate_rdv_from_calendar.rb
@@ -1,4 +1,6 @@
 describe "Agent can initiate a Rdv from calendar" do
+  include UsersHelper
+
   let!(:agent) { create(:agent, :admin, first_name: "Alain") }
   let!(:agent2) { create(:agent, first_name: "Robert") }
   let!(:plage_ouverture) { create(:plage_ouverture, :weekly, agent: agent) }
@@ -11,30 +13,43 @@ describe "Agent can initiate a Rdv from calendar" do
   end
 
   scenario "for him", js: true do
-    # Step 1
     expect_page_title("Votre agenda")
 
-    find('.fc-bgevent', match: :first).click
-
-    select(motif.name, from: "rdv_motif_id")
-    click_button('Continuer')
-
-    expect(find(".select2")).to have_content(agent.full_name_and_service)
-    expect(find(".select2")).not_to have_content(agent2.full_name_and_service)
+    initiate_rdv(agent)
   end
 
   scenario "for an other agent", js: true do
     select(agent2.full_name, from: 'id')
-
-    # Step 1
     expect_page_title("Agenda de #{agent2.full_name_and_service}")
 
+    initiate_rdv(agent2)
+  end
+
+  def initiate_rdv(agent)
+    # Step 1
     find('.fc-bgevent', match: :first).click
 
+    # Step 2
     select(motif.name, from: "rdv_motif_id")
     click_button('Continuer')
 
-    expect(find(".select2")).not_to have_content(agent.full_name_and_service)
-    expect(find(".select2")).to have_content(agent2.full_name_and_service)
+    # Step 3
+    expect(find(".select2")).to have_content(agent.full_name_and_service)
+    click_button('Continuer')
+
+    # Step 4
+    select_user(user)
+    expect(page).to have_content(full_name_and_birthdate(user))
+    click_button('Continuer')
+
+    # Step 5
+    expect(page).to have_content("Le rendez-vous a été créé.")
+  end
+
+  def select_user(user)
+    find(:css, ".select2-search__field").set(user.full_name)
+    sleep(0.5)
+    expect(page).to have_content(full_name_and_birthdate(user))
+    find('.select2-search__field').native.send_keys(:return)
   end
 end
