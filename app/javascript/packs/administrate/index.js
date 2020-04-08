913b59f51504e251eadff1a0bd2b2a3182bf2062
jjf21 <carl.quivron@gmail.com>
2020-04-08 17:22:29 +0200

    move administrate to webpack

--- app/javascript/packs/administrate/index.js
+++ app/javascript/packs/administrate/index.js
@@ -1,7 +1,9 @@
 import 'packs/components/table'
 import 'packs/components/date_time_picker'
 import 'selectize'
+import { PlacesInput } from 'packs/components/places-input.js';
 
 $(document).on('turbolinks:load', function() {
   $(".field-unit--has-many select").selectize({});
+  new PlacesInput(document.querySelector('.places-js-container'));
 });
