a7e14c044ad226d81040598f2c475601669773be
Yannick Francois <yaf@elsif.fr>
2020-04-14 16:40:16 +0200

    utilise la created_at date pour les tests

--- spec/models/stat_spec.rb
+++ spec/models/stat_spec.rb
@@ -7,14 +7,14 @@ describe Stat, type: :model do
 
     it "return 2=>1 with one home rdv" do
       home_motif = create(:motif, location_type: :home)
-      create(:rdv, motif: home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0))
+      create(:rdv, motif: home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0))
       stats = Stat.new(rdvs: Rdv.all)
       expect(stats.rdvs_group_by_type[["à domicile", "05/04/2020"]]).to eq(1)
     end
 
     it "return 2=>2 with two home rdv" do
       home_motif = create(:motif, location_type: :home)
-      2.times { create(:rdv, motif: home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0)) }
+      2.times { create(:rdv, motif: home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0)) }
       stats = Stat.new(rdvs: Rdv.all)
       expect(stats.rdvs_group_by_type[["à domicile", "05/04/2020"]]).to eq(2)
     end
@@ -22,8 +22,8 @@ describe Stat, type: :model do
     it "return 2=>2 with two different motif of home rdv" do
       home_motif = create(:motif, location_type: :home)
       other_home_motif = create(:motif, location_type: :home)
-      create(:rdv, motif: home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0))
-      create(:rdv, motif: other_home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0))
+      create(:rdv, motif: home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0))
+      create(:rdv, motif: other_home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0))
       stats = Stat.new(rdvs: Rdv.all)
       expect(stats.rdvs_group_by_type[["à domicile", "05/04/2020"]]).to eq(2)
     end
@@ -31,8 +31,8 @@ describe Stat, type: :model do
     it "return {2=>1, 1=>1} with one home rdv and one phone" do
       home_motif = create(:motif, location_type: :home)
       phone_motif = create(:motif, location_type: :phone)
-      create(:rdv, motif: home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0))
-      create(:rdv, motif: phone_motif, starts_at: DateTime.new(2020, 4, 7, 11, 0))
+      create(:rdv, motif: home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0))
+      create(:rdv, motif: phone_motif, created_at: DateTime.new(2020, 4, 7, 11, 0))
       stats = Stat.new(rdvs: Rdv.all)
       expect(stats.rdvs_group_by_type[["à domicile", "05/04/2020"]]).to eq(1)
       expect(stats.rdvs_group_by_type[["par téléphone", "05/04/2020"]]).to eq(1)
@@ -42,9 +42,9 @@ describe Stat, type: :model do
       home_motif = create(:motif, location_type: :home)
       phone_motif = create(:motif, location_type: :phone)
       public_office_motif = create(:motif, location_type: :public_office)
-      create(:rdv, motif: home_motif, starts_at: DateTime.new(2020, 4, 7, 10, 0))
-      create(:rdv, motif: phone_motif, starts_at: DateTime.new(2020, 4, 7, 11, 0))
-      create(:rdv, motif: public_office_motif, starts_at: DateTime.new(2020, 4, 7, 9, 40))
+      create(:rdv, motif: home_motif, created_at: DateTime.new(2020, 4, 7, 10, 0))
+      create(:rdv, motif: phone_motif, created_at: DateTime.new(2020, 4, 7, 11, 0))
+      create(:rdv, motif: public_office_motif, created_at: DateTime.new(2020, 4, 7, 9, 40))
       stats = Stat.new(rdvs: Rdv.all)
       expect(stats.rdvs_group_by_type[["à domicile", "05/04/2020"]]).to eq(1)
       expect(stats.rdvs_group_by_type[["par téléphone", "05/04/2020"]]).to eq(1)
