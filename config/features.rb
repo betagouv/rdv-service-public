387afe16e52f56b94a6e3a99530eb5489f923c0c
jjf21 <carl.quivron@gmail.com>
2020-04-16 17:07:23 +0200

    rubocop

--- config/features.rb
+++ config/features.rb
@@ -2,7 +2,7 @@ Flipflop.configure do
   # Strategies will be used in the order listed here.
   strategy :active_record # or :sequel, :redis
   strategy :default
-  
+
   feature :visite_a_domicile,
           title: 'Visite à domicile',
           description: "Mise en place des visites à domicile"
