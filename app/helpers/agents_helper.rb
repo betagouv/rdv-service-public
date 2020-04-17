cb42a86bf5cd32f6ffce75bad2e5395a0eff7d41
Yannick François <yannick.francois@beta.gouv.fr>
2020-04-17 17:58:19 +0200

    [Agent][Usager] Trouver un creneau à partir de la fiche usager (#560)
    * pose un créneau depuis une fiche usager
    
    * pose un rendez-vous à partir d'une fiche usager
    
    * test les controllers
    
    * précise l'usage d'un ID plutôt que l'usager
    
    * respect les règles rubocop
    
    * ajoute l'utilisateur dans les paramètres s'il est présent
    
    * utilise le full_name des concerns
    
    * utilise un id plutôt que l'objet entier
    
    * corrige une typo
    
    * ne pas utiliser de variable d'instance dans une vue partiel
    
    * test la présence du user
    
    * test la présence de la variable user_id
    
    * revois l'usage de user_id dans les vue partielle

--- app/helpers/agents_helper.rb
+++ app/helpers/agents_helper.rb
@@ -14,4 +14,17 @@ module AgentsHelper
   def delete_dropdown_link(agent)
     link_to 'Supprimer', organisation_agent_path(current_organisation, agent), data: { confirm: "Êtes-vous sûr de vouloir supprimer cet agent ?" }, method: :delete, class: 'dropdown-item' if policy([:agent, agent]).destroy?
   end
+
+  def build_link_to_rdv_wizard_params(lieu, motif, creneau, user_id)
+    params = {}
+    params[:step] = 2
+    params[:starts_at] = creneau.starts_at
+    params[:motif_id] = motif.id
+    params[:plage_ouverture_location] = lieu.address
+    params[:organisation_id] = motif.organisation_id
+    params[:duration_in_min] = motif.default_duration_in_min
+    params["agent_ids[]"] = creneau.agent_id
+    params["user_ids[]"] = user_id if user_id
+    params
+  end
 end
