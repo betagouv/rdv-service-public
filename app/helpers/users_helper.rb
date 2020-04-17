5457d83bcd56978e659b4ed2cb499990d65942e7
Yannick Francois <yaf@elsif.fr>
2020-04-17 15:33:24 +0200

    utilise le full_name des concerns

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -11,10 +11,6 @@ module UsersHelper
     label
   end
 
-  def full_name(user)
-    "#{user.first_name} #{user.last_name}"
-  end
-
   def user_show_path(user)
     user.relative? ? organisation_relative_path(current_organisation, user) : organisation_user_path(current_organisation, user)
   end
