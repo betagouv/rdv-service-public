19fca93a5ea6181f4eb0a095681d9fc5794a92af
Yannick Francois <yaf@elsif.fr>
2020-04-17 09:12:13 +0200

    pose un rendez-vous à partir d'une fiche usager

--- app/controllers/agents/creneaux/agent_searches_controller.rb
+++ app/controllers/agents/creneaux/agent_searches_controller.rb
@@ -3,6 +3,7 @@ class Agents::Creneaux::AgentSearchesController < AgentAuthController
 
   def index
     skip_policy_scope
+    @user = params[:user]
     respond_to do |format|
       format.html do
         @organisation = current_organisation
