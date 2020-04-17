26961b1075b47b418df980f9d8c7f3371675750b
Yannick Francois <yaf@elsif.fr>
2020-04-17 15:28:10 +0200

    ajoute l'utilisateur dans les paramètres s'il est présent

--- app/controllers/agents/creneaux/agent_searches_controller.rb
+++ app/controllers/agents/creneaux/agent_searches_controller.rb
@@ -3,7 +3,7 @@ class Agents::Creneaux::AgentSearchesController < AgentAuthController
 
   def index
     skip_policy_scope
-    @user = User.find(params[:user_id])
+    @user = User.find(params[:user_id]) if params[:user_id]
     respond_to do |format|
       format.html do
         @organisation = current_organisation
