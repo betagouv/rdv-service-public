fdf9eeec8230eac14bbae2d0bb336799862f7844
jjf21 <carl.quivron@gmail.com>
2020-04-29 10:58:59 +0200

    handle specific cases

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,6 +5,10 @@ module UsersHelper
     "#{I18n.l(user.birth_date)} - #{user.age}"
   end
 
+  def relative_tag(user)
+    user.relative? ? content_tag(:span, 'Proche', class: 'badge badge-info') : nil
+  end
+
   def full_name_and_birthdate(user)
     label = user.full_name
     label += " - #{birth_date_and_age(user)}" if user.birth_date
--- app/webpacker/components/select_users_from_wizard.js
+++ app/webpacker/components/select_users_from_wizard.js
@@ -4,7 +4,6 @@ class SelectUsersFromWizard {
     this.urlSearchParams = new URLSearchParams(window.location.search.substr(1))
     this.organisationId = $("input[name=current_organisation]").val()
     $('.js-new-rdv-users-select').on('select2:select', (e) => {
-      this.displayLoader()
       this.urlSearchParams.append("user_ids[]", e.params.data.id)
       window.location.search = this.urlSearchParams.toString();
     });
@@ -14,11 +13,15 @@ class SelectUsersFromWizard {
       $(".js-add-user-interface").removeClass("d-none")
       $(e.currentTarget).remove()
     })
+
+    $('js-toggle-add-user-interface').on('click', e => {
+      $('js-toggle-add-user-interface').addClass('d-none')
+      $('js-add-user-interface').removeClass('d-none')
+    })
   }
 
   attachRemoveUserListeners = () => {
     $('.js-remove-user').click(event => {
-      this.displayLoader();
       const userId = event.currentTarget.getAttribute('data-user-id')
       let userIds = this.urlSearchParams.getAll("user_ids[]")
       userIds.splice(userIds.indexOf(userId), 1)
@@ -27,11 +30,6 @@ class SelectUsersFromWizard {
       window.location.search = this.urlSearchParams.toString();
     });
   }
-
-  displayLoader = () => {
-    $(".js-rdv-step-card").addClass("card-loading")
-  }
-
 }
 
 export { SelectUsersFromWizard };
