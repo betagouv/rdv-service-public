7148d0ec77bc99e190d33fda2c46cb1610e30ec0
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-04-30 13:06:07 +0200

    fix lint

--- scripts/deploy.rb
+++ scripts/deploy.rb
@@ -8,8 +8,8 @@ end
 
 Typhoeus.post(
   ENV['SLACK_DEPLOY_WEBHOOK_URL'],
-  headers: {'Content-Type' => 'application/json; charset=utf-8'},
-  body: JSON.dump({text: "Deployment to production is being triggered..."})
+  headers: { 'Content-Type' => 'application/json; charset=utf-8' },
+  body: JSON.dump({ text: "Deployment to production is being triggered..." })
 )
 
 exec("scalingo integration-link-manual-deploy -a production-rdv-solidarites master")
