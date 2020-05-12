8577410fe1f83a2f22efbd7e829ef9af837f096a
jjf21 <carl.quivron@gmail.com>
2020-05-12 19:46:34 +0200

    add a step 1 to user stepper wizard

--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -7,7 +7,10 @@ class Users::UsersController < UserAuthController
   def update
     @user = current_user
     authorize(@user)
-    if @user.update(user_params)
+    user_saved = @user.update(user_params)
+    if params[:from_wizard].presence
+      redirect_to new_users_rdv_wizard_step_path(step: 2, **wizard_params)
+    elsif user_saved
       flash[:notice] = "Vos informations ont été mises à jour."
       redirect_to users_informations_path
     else
@@ -16,6 +19,10 @@ class Users::UsersController < UserAuthController
   end
 
   private
+
+  def wizard_params
+    params.require(:from_wizard).permit(:departement, :latitude, :lieu_id, :longitude, :latitude, :motif_id, :starts_at, :where, user_ids: [])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :birth_name, :phone_number, :birth_date, :address, :caisse_affiliation, :affiliation_number, :family_situation, :number_of_children, :logement)
