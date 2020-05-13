45f2f682c80d5049b64c20c659fda50e9d8dc127
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2020-05-13 11:17:11 +0200

    [bug] fix agents rdv wizard users reload after error (#636)
    * fix reloading page after users update in agent rdv wizard
    
    * in agent rdv wizard fix remove user id when multiple
    
    * refactor rdv wizard step2 js : better readability + more pure

--- app/webpacker/components/rdv_wizard_step2.js
+++ app/webpacker/components/rdv_wizard_step2.js
@@ -3,40 +3,57 @@ class RdvWizardStep2 {
   constructor() {
     if (!$('.js-new-rdv-users-select').length) { return; }
 
-    this.urlSearchParams = new URLSearchParams(window.location.search.substr(1))
-    this.organisationId = $("input.js-current-organisation-id").val()
+    this.canonicalUrlStr = document.querySelector("input[name=js-canonical-path]").value;
+    this.organisationId = document.querySelector("input[name=js-current-organisation-id]").value;
+
+    this.attachAddUserListener()
+    this.attachRemoveUserListeners()
+    this.attachToggleAddUserInterface()
+  }
+
+  getCanonicalUrl = () => new URL(this.canonicalUrlStr, window.location)
+
+  getCanonicalUrlSearchParams = () =>
+    new URLSearchParams(this.getCanonicalUrl().search.substr(1))
+
+  attachAddUserListener = () => {
     $('.js-new-rdv-users-select').on('select2:select', (e) => {
       this.showSpinner()
-      this.urlSearchParams.append("user_ids[]", e.params.data.id)
-      window.location.search = this.urlSearchParams.toString();
+      const urlSearchParams = this.getCanonicalUrlSearchParams()
+      urlSearchParams.append("user_ids[]", e.params.data.id)
+      this.redirectToUrlWithSearchParams(urlSearchParams)
     });
-    this.attachRemoveUserListeners()
-    $(".js-toggle-add-user-interface").on('click', (e) => {
-      e.preventDefault()
-      $(".js-add-user-interface").removeClass("d-none")
-      $(e.currentTarget).remove()
-    })
-
-    $('js-toggle-add-user-interface').on('click', e => {
-      $('js-toggle-add-user-interface').addClass('d-none')
-      $('js-add-user-interface').removeClass('d-none')
-    })
   }
 
   attachRemoveUserListeners = () => {
     $('.js-remove-user').click(event => {
       this.showSpinner()
       const userId = event.currentTarget.getAttribute('data-user-id')
-      let userIds = this.urlSearchParams.getAll("user_ids[]")
+      let urlSearchParams = this.getCanonicalUrlSearchParams()
+      let userIds = urlSearchParams.getAll("user_ids[]")
       userIds.splice(userIds.indexOf(userId), 1)
-      this.urlSearchParams.delete("user_ids[]")
-      userIds.forEach(id => this.urlSearchParams.append("user_ids[]", id))
-      window.location.search = this.urlSearchParams.toString();
+      urlSearchParams.delete("user_ids[]")
+      userIds.forEach(id => urlSearchParams.append("user_ids[]", id))
+      this.redirectToUrlWithSearchParams(urlSearchParams)
     });
+  }
+
+  attachToggleAddUserInterface = () => {
+    $(".js-toggle-add-user-interface").on('click', (e) => {
+      e.preventDefault()
+      $(".js-add-user-interface").removeClass("d-none")
+      $(e.currentTarget).remove()
+    })
   }
 
   showSpinner = () => {
     $(".js-users-spinner").removeClass("d-none")
+  }
+
+  redirectToUrlWithSearchParams = (urlSearchParams) => {
+    const canonicalUrl = this.getCanonicalUrl()
+    canonicalUrl.search = urlSearchParams.toString();
+    window.location = canonicalUrl;
   }
 }
 
