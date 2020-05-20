c5e65942f40365df163e68d6b53aedab6326ca58
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-05-20 18:55:15 +0200

    add script to scale down all review apps easily

---
+++ scripts/scale_down_all_review_apps.rb
@@ -0,0 +1,8 @@
+# ruby ./scripts/scale_down_all_review_apps.rb
+
+res = `scalingo apps` # backticks run shell commands
+res.scan(/demo-rdv-solidarites-pr(\d+)/).each do |matches|
+  pr_number = matches.first
+  puts "./scripts/scale_review_app.sh #{pr_number} down"
+  system("./scripts/scale_review_app.sh #{pr_number} down")
+end
