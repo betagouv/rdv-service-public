f33e30d28ce8a4df5a9b7cc46067d96faf44646e
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-05-29 17:12:32 +0200

    fixing plage ouverture ICS

--- app/mailers/agents/plage_ouverture_mailer.rb
+++ app/mailers/agents/plage_ouverture_mailer.rb
@@ -2,11 +2,22 @@ class Agents::PlageOuvertureMailer < ApplicationMailer
   def plage_ouverture_created(plage_ouverture)
     @plage_ouverture = plage_ouverture
     ics = PlageOuverture::Ics.new(plage_ouverture: @plage_ouverture)
-    attachments[ics.name] = {
-      mime_type: 'text/calendar',
+    attachments["invite.ics"] = {
+      mime_type: 'application/ics',
       content: ics.to_ical,
       encoding: "8bit", # fixes encoding issues in ICS
     }
-    mail(to: plage_ouverture.agent.email, subject: "Votre planning #{BRAND}")
+    m = mail(
+      to: plage_ouverture.agent.email,
+      subject: "#{BRAND} #{plage_ouverture.title} - Plage d'ouverture"
+    )
+    m.add_part(
+      Mail::Part.new do
+        content_type "text/calendar; method=REQUEST"
+        body ics.to_ical
+        content_transfer_encoding "8bit"
+      end
+    )
+    m
   end
 end
