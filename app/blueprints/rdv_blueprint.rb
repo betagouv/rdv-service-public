1ca3bbdd7062b88c7d1fb6be01d257f7d9ed2b24
jjf21 <carl.quivron@gmail.com>
2020-06-09 14:22:38 +0200

    fix webhook

--- app/blueprints/rdv_blueprint.rb
+++ app/blueprints/rdv_blueprint.rb
@@ -1,12 +1,10 @@
 class RdvBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :uuid, :status, :duration_in_min, :starts_at
+  fields :uuid, :status, :duration_in_min, :starts_at, :address
 
   association :organisation, blueprint: OrganisationBlueprint
   association :motif, blueprint: MotifBlueprint
   association :users, blueprint: UserBlueprint
   association :agents, blueprint: AgentBlueprint
-
-  field :address, &:address
 end
