305d42b8ecd4994c94e4aee2f8ce38615e27937a
Thomas Guillet <guillet.thomas@gmail.com>
2020-06-11 10:23:04 +0200

    Corrige et enrichi les stats

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -2,7 +2,7 @@ class StatsController < ApplicationController
   before_action :scope_rdv_to_departement
 
   def index
-    @stats = Stat.new(rdvs: @rdvs, users: @users)
+    @stats = Stat.new(agents: @agents, organisations: @organisations, rdvs: @rdvs, users: @users)
   end
 
   def rdvs
@@ -19,6 +19,14 @@ class StatsController < ApplicationController
     render json: stats.chart_json
   end
 
+  def agents
+    render json: Stat.new(agents: @agents).agents_group_by_week
+  end
+
+  def organisations
+    render json: Stat.new(organisations: @organisations).organisations_group_by_week
+  end
+
   def users
     render json: Stat.new(users: @users).users_group_by_week
   end
@@ -28,9 +36,13 @@ class StatsController < ApplicationController
     if @departement.present?
       @rdvs = Rdv.joins(:organisation).where(organisations: { departement: @departement })
       @users = User.joins(:organisations).where(organisations: { departement: @departement })
+      @agents = Agent.joins(:organisations).where(organisations: { departement: @departement })
+      @organisations = Organisation.where(organisations: { departement: @departement })
     else
       @rdvs = Rdv.all
       @users = User.all
+      @agents = Agent.all
+      @organisations = Organisation.all
     end
 
     @departements = Organisation.all.map(&:departement).uniq.sort
--- app/models/stat.rb
+++ app/models/stat.rb
@@ -1,8 +1,9 @@
 class Stat
   include ActiveModel::Model
-  attr_accessor :users, :rdvs
+  attr_accessor :agents, :organisations, :users, :rdvs
 
   DEFAULT_RANGE = (Date.strptime('01/02/2020', '%d/%m/%Y').beginning_of_day..Time.zone.now.end_of_day).freeze
+  DEFAULT_FORMAT = '%d/%m/%Y'.freeze
 
   def rdvs_for_default_range
     rdvs.where(created_at: DEFAULT_RANGE)
@@ -12,24 +13,40 @@ class Stat
     users.active.where(created_at: DEFAULT_RANGE)
   end
 
+  def users_group_by_week
+    users.active.group_by_week('users.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
+  end
+
+  def organisations_for_default_range
+    organisations.where(created_at: DEFAULT_RANGE)
+  end
+
+  def organisations_group_by_week
+    organisations.group_by_week('organisations.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
+  end
+
+  def agents_for_default_range
+    agents.active.where(created_at: DEFAULT_RANGE)
+  end
+
+  def agents_group_by_week
+    agents.active.group_by_week('agents.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
+  end
+
   def rdvs_group_by_week
-    rdvs.group(:created_by).group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: '%d/%m/%Y').count
+    rdvs.group(:created_by).group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
   end
 
   def rdvs_group_by_type
-    rdvs.joins(:motif).group('motifs.location_type').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: '%d/%m/%Y').count.transform_keys { |key| [I18n.t(Motif.location_types.invert[key[0]]), key[1]] }
+    rdvs.joins(:motif).group('motifs.location_type').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count.transform_keys { |key| [I18n.t(Motif.location_types.invert[key[0]]), key[1]] }
   end
 
   def rdvs_group_by_departement
-    rdvs.joins(:organisation).group('organisations.departement').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: '%d/%m/%Y').count
+    rdvs.joins(:organisation).group('organisations.departement').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
   end
 
   def rdvs_group_by_service
-    rdvs.joins(motif: :service).group('services.name').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: '%d/%m/%Y').count
-  end
-
-  def users_group_by_week
-    users.active.group_by_week('users.created_at', range: DEFAULT_RANGE, format: '%d/%m/%Y').count
+    rdvs.joins(motif: :service).group('services.name').group_by_week('rdvs.created_at', range: DEFAULT_RANGE, format: DEFAULT_FORMAT).count
   end
 
   def rdvs_group_by_week_fr
