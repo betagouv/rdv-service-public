5329f8b6ef5699e82a29d3f5de290d72a7735aa0
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-06-17 11:53:31 +0200

    REFACTOR: simplify welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+  PERMITTED_PARAMS = [
+    :departement, :where, :service, :motif_name, :latitude, :longitude
+  ].freeze
+
   before_action :set_lieu_variables, only: [:welcome_departement, :welcome_service]
 
   def index; end
@@ -6,20 +10,26 @@ class WelcomeController < ApplicationController
   def welcome_agent; end
 
   def search
-    search_params = params.require(:search).permit(:departement, :where, :service, :motif_name, :latitude, :longitude)
+    return redirect_to lieux_path(search: search_params) \
+      if search_params[:service].present? && search_params[:motif_name].present?
 
     if search_params[:service].present?
-      if search_params[:motif_name].present?
-        redirect_to lieux_path(search: { departement: search_params[:departement], service: search_params[:service], motif_name: search_params[:motif_name], where: search_params[:where], latitude: search_params[:latitude], longitude: search_params[:longitude] })
-      else
-        redirect_to welcome_service_path(search_params[:departement], search_params[:service], where: search_params[:where], latitude: search_params[:latitude], longitude: search_params[:longitude])
-      end
-    elsif search_params[:departement].present?
-      redirect_to welcome_departement_path(search_params[:departement], where: search_params[:where], latitude: search_params[:latitude], longitude: search_params[:longitude])
-    else
-      flash[:error] = "L'adresse entrée n'est pas valide. Vous devez choisir une des valeurs proposées sur le champ adresse."
-      redirect_to root_path
+      return redirect_to welcome_service_path(
+        search_params[:departement],
+        search_params[:service],
+        **search_params.except(:departement, :service)
+      )
     end
+
+    if search_params[:departement].present?
+      return redirect_to welcome_departement_path(
+        search_params[:departement],
+        **search_params.except(:departement)
+      )
+    end
+
+    flash[:error] = "L'adresse entrée n'est pas valide. Vous devez choisir une des valeurs proposées sur le champ adresse."
+    redirect_to root_path
   end
 
   def welcome_departement
@@ -31,11 +41,20 @@ class WelcomeController < ApplicationController
   end
 
   def set_lieu_variables
-    lieu_params = params.permit(:departement, :where, :service, :latitude, :longitude)
     @departement = lieu_params[:departement]
     @latitude = lieu_params[:latitude]
     @longitude = lieu_params[:longitude]
     @where = lieu_params[:where]
     @service = Service.find(lieu_params[:service]) if lieu_params[:service]
+  end
+
+  private
+
+  def search_params
+    params.require(:search).permit(*PERMITTED_PARAMS)
+  end
+
+  def lieu_params
+    params.permit(*PERMITTED_PARAMS)
   end
 end
