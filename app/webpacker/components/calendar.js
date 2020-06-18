7f1212d2afc0c13b4e5c1e8fc096fe21b6edb49f
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-06-18 10:13:33 +0200

    REFACTOR: extract functions from JS calendar class

--- app/webpacker/components/calendar.js
+++ app/webpacker/components/calendar.js
@@ -10,53 +10,46 @@ const browser = Bowser.getParser(window.navigator.userAgent);
 class CalendarRdvSolidarites {
 
   constructor() {
-    const calendarEl = document.getElementById('calendar');
-
-    if (calendarEl == null || calendarEl.innerHTML !== "")
+    this.calendarEl = document.getElementById('calendar');
+    if (this.calendarEl == null || this.calendarEl.innerHTML !== "")
       return
 
-    let defaultView = "timeGridOneDay";
-    if (!browser.is("mobile")) {
-      let viewFromLocalStorage = localStorage.getItem("calendarDefaultView");
+    this.data = this.calendarEl.dataset
+    this.fullCalendarInstance = this.initFullCalendar(this.calendarEl)
+    this.fullCalendarInstance.render();
+    const refreshCalendarInterval = setInterval(() => this.fullCalendarInstance.refetchEvents(), 3000);
 
-      defaultView = ['dayGridMonth', 'timeGridWeek', 'timeGridOneDay'].includes(viewFromLocalStorage) ? viewFromLocalStorage : "timeGridWeek";
-    }
+    $(document).on('turbolinks:before-cache turbolinks:before-render', function() {
+      clearTimeout(refreshCalendarInterval)
+    });
 
-    const { eventSourcesJson, defaultDateJson, selectedEventId, organisationId, agentId } = calendarEl.dataset
-    const defaultDate = JSON.parse(defaultDateJson || sessionStorage.getItem('calendarStartDate'))
-    var calendar = new Calendar(calendarEl, {
+    document.addEventListener("turbolinks:before-cache", () => {
+      // force calendar reload on turbolinks re-visit, otherwise event listeners
+      // are not attached
+      this.calendarEl.innerHTML = ""
+      // fixes hanging tooltip on back
+      $(".tooltip").removeClass("show")
+    })
+
+  }
+
+  initFullCalendar = () => {
+    return new Calendar(this.calendarEl, {
       plugins: [dayGridPlugin, timeGridPlugin, listPlugin, interactionPlugin],
       locale: frLocale,
-      eventSources: JSON.parse(eventSourcesJson),
+      eventSources: JSON.parse(this.data.eventSourcesJson),
       eventSourceFailure: function (errorObj) {
         alert("Une erreur s'est produite lors de la récupération des données du calendrier.");
       },
-      defaultDate: defaultDate,
-      defaultView: defaultView,
+      defaultDate: this.getDefaultDate(),
+      defaultView: this.getDefaultView(),
       viewSkeletonRender: function (info) {
         localStorage.setItem("calendarDefaultView", info.view.type);
       },
       weekends: false,
       height: "auto",
       selectable: true,
-      select: function(info) {
-        if ($('body').hasClass('right-bar-enabled')) return false;
-
-        let startDate = moment(info.start);
-        const urlSearchParams = new URLSearchParams({
-          starts_at: info.startStr,
-          "agent_ids[]": agentId,
-        });
-        let plage_ouvertures = calendar.getEvents()
-          .filter(e => e.rendering == "background")
-          .filter(e => startDate.isBetween(e.start, e.end, null, "[]"));
-
-        if (plage_ouvertures[0] !== undefined) {
-          urlSearchParams.append('plage_ouverture_location', plage_ouvertures[0].extendedProps.location);
-        }
-        window.location = `/organisations/${organisationId}/rdv_wizard_step/new?${urlSearchParams.toString()}`;;
-
-      },
+      select: this.selectEvent,
       header: {
         center: 'dayGridMonth,timeGridWeek,timeGridOneDay'
       },
@@ -76,73 +69,90 @@ class CalendarRdvSolidarites {
       },
       minTime: '07:00:00',
       maxTime: '20:00:00',
-      datesRender: function(info) {
-        sessionStorage.setItem("calendarStartDate", JSON.stringify(info.view.currentStart))
-        const printLinkElt = document.querySelector(".js-link-print-rdvs")
-        printLinkElt.classList.toggle("d-none", info.view.type != "timeGridOneDay")
-        if (info.view.type != "timeGridOneDay") return
+      datesRender: this.datesRender,
+      eventRender: this.eventRender
+    });
+  }
 
-        const url = new URL(printLinkElt.href)
-        const date = moment(info.view.currentStart)
-        printLinkElt.querySelector(".js-date").innerHTML = date.format("DD/MM/YYYY")
-        url.searchParams.set("date", date.format("YYYY-MM-DD"))
-        printLinkElt.href = url.toString()
-      },
-      eventRender: function (info) {
-        let $el = $(info.el);
-        if(info.event.extendedProps.past == true) {
-          $el.addClass("fc-event-past");
-        };
-        if(info.event.extendedProps.duration <= 30) {
-          $el.addClass("fc-event-small");
-        };
-        if (selectedEventId && info.event.id == selectedEventId) $el.addClass("selected");
-        $el.addClass("fc-event-"+ info.event.extendedProps.status);
-        if (info.event.extendedProps.unclickable != true){
-          let title = `${moment(info.event.start).format('H:mm')} - ${moment(info.event.end).format('H:mm')}`;
-          if (info.event.rendering == 'background') {
-            $el.append("<div class=\"fc-title\" style=\"color: white; padding: 2px 4px; font-size: 12px; font-weight: bold;\">" + info.event.title + "</div>");
+  getDefaultView = () => {
+    let defaultView = "timeGridOneDay";
+    if (!browser.is("mobile")) {
+      let viewFromLocalStorage = localStorage.getItem("calendarDefaultView");
 
-            title += `<br><strong>${info.event.title}</strong><br> <small>Lieu : ${info.event.extendedProps.lieu}</small>`;
-          } else {
-            if (info.event.extendedProps.duration) {
-              title += ` <small>(${info.event.extendedProps.duration} min)</small>`;
-              title += ` <br>${info.event.extendedProps.motif}`;
-            }
-            title += `<br><strong>${info.event.title}</strong>`;
-            title += `<br><strong>Statut:</strong> ${info.event.extendedProps.readableStatus}`;
-          }
+      defaultView = ['dayGridMonth', 'timeGridWeek', 'timeGridOneDay'].includes(viewFromLocalStorage) ? viewFromLocalStorage : "timeGridWeek";
+    }
+    return defaultView;
+  }
 
-          $el.attr("title", title);
-          $el.attr("data-toggle", "tooltip");
-          $el.attr("data-html", "true");
-          $el.tooltip()
+  getDefaultDate = () => {
+    return JSON.parse(this.data.defaultDateJson || sessionStorage.getItem('calendarStartDate'))
+  }
+
+  selectEvent = (info) => {
+    if ($('body').hasClass('right-bar-enabled')) return false;
+
+    let startDate = moment(info.start);
+    const urlSearchParams = new URLSearchParams({
+      starts_at: info.startStr,
+      "agent_ids[]": this.data.agentId,
+    });
+    let plage_ouvertures = this.fullCalendarInstance.getEvents()
+      .filter(e => e.rendering == "background")
+      .filter(e => startDate.isBetween(e.start, e.end, null, "[]"));
+
+    if (plage_ouvertures[0] !== undefined) {
+      urlSearchParams.append('plage_ouverture_location', plage_ouvertures[0].extendedProps.location);
+    }
+    window.location = `/organisations/${this.data.organisationId}/rdv_wizard_step/new?${urlSearchParams.toString()}`;;
+  }
+
+  datesRender = (info) => {
+    sessionStorage.setItem("calendarStartDate", JSON.stringify(info.view.currentStart))
+    const printLinkElt = document.querySelector(".js-link-print-rdvs")
+    printLinkElt.classList.toggle("d-none", info.view.type != "timeGridOneDay")
+    if (info.view.type != "timeGridOneDay") return
+
+    const url = new URL(printLinkElt.href)
+    const date = moment(info.view.currentStart)
+    printLinkElt.querySelector(".js-date").innerHTML = date.format("DD/MM/YYYY")
+    url.searchParams.set("date", date.format("YYYY-MM-DD"))
+    printLinkElt.href = url.toString()
+  }
+
+  eventRender = (info) => {
+    let $el = $(info.el);
+    if(info.event.extendedProps.past == true) {
+      $el.addClass("fc-event-past");
+    };
+    if(info.event.extendedProps.duration <= 30) {
+      $el.addClass("fc-event-small");
+    };
+    if (this.data.selectedEventId && info.event.id == this.data.selectedEventId)
+      $el.addClass("selected");
+    $el.addClass("fc-event-"+ info.event.extendedProps.status);
+    if (info.event.extendedProps.unclickable != true){
+      let title = `${moment(info.event.start).format('H:mm')} - ${moment(info.event.end).format('H:mm')}`;
+      if (info.event.rendering == 'background') {
+        $el.append("<div class=\"fc-title\" style=\"color: white; padding: 2px 4px; font-size: 12px; font-weight: bold;\">" + info.event.title + "</div>");
+
+        title += `<br><strong>${info.event.title}</strong><br> <small>Lieu : ${info.event.extendedProps.lieu}</small>`;
+      } else {
+        if (info.event.extendedProps.duration) {
+          title += ` <small>(${info.event.extendedProps.duration} min)</small>`;
+          title += ` <br>${info.event.extendedProps.motif}`;
         }
+        title += `<br><strong>${info.event.title}</strong>`;
+        title += `<br><strong>Statut:</strong> ${info.event.extendedProps.readableStatus}`;
       }
-    });
 
-    window.calendar = calendar
-    calendar.render();
-
-    const refreshCalendarInterval = setInterval(function(){ calendar.refetchEvents() }, 30000);
-
-    $(document).on('turbolinks:before-cache turbolinks:before-render', function() {
-      clearTimeout(refreshCalendarInterval)
-    });
+      $el.attr("title", title);
+      $el.attr("data-toggle", "tooltip");
+      $el.attr("data-html", "true");
+      $el.tooltip()
+    }
   }
 }
 
 document.addEventListener('turbolinks:load', function() {
   new CalendarRdvSolidarites()
 });
-
-
-document.addEventListener("turbolinks:before-cache", function() {
-  const calendarElt = document.getElementById('calendar')
-  if (!calendarElt) { return false }
-  // force calendar reload on turbolinks re-visit, otherwise event listeners
-  // are not attached
-  calendarElt.innerHTML = ""
-  // fixes hanging tooltip on back
-  $(".tooltip").removeClass("show")
-})
