ac4a77536490dd327530f46212fd8b8974e13f5e
jjf21 <carl.quivron@gmail.com>
2020-06-24 14:40:01 +0200

    remove AR object as argument of job

--- app/jobs/sms_sender_job.rb
+++ app/jobs/sms_sender_job.rb
@@ -1,5 +1,7 @@
 class SmsSenderJob < ApplicationJob
-  def perform(status, rdv, user, options = {})
+  def perform(status, rdv_id, user_id, options = {})
+    rdv = Rdv.find(rdv_id)
+    user = Rdv.find(user_id)
     SendTransactionalSmsService.perform_with(status, rdv, user, options) if Rails.env.production?
   end
 end
--- app/services/notifications/rdv/rdv_cancelled_by_agent_service.rb
+++ app/services/notifications/rdv/rdv_cancelled_by_agent_service.rb
@@ -5,6 +5,6 @@ class Notifications::Rdv::RdvCancelledByAgentService < ::BaseService
 
   def notify_user(user)
     Users::RdvMailer.rdv_cancelled_by_agent(@rdv, user).deliver_later if user.email.present?
-    SmsSenderJob.perform_later(:rdv_cancelled, @rdv, user) if user.formatted_phone
+    SmsSenderJob.perform_later(:rdv_cancelled, @rdv.id, user.id) if user.formatted_phone
   end
 end
