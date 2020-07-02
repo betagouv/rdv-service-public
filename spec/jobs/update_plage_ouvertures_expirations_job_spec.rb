29db8a4818249dc9780b3311380db25ccc071638
jjf21 <carl.quivron@gmail.com>
2020-07-02 11:06:39 +0200

    follow adispaquale review

--- spec/jobs/update_plage_ouvertures_expirations_job_spec.rb
+++ spec/jobs/update_plage_ouvertures_expirations_job_spec.rb
@@ -1,12 +1,12 @@
-RSpec.describe UpdatePlageOuverturesExpirationsJob, type: :job do
+describe UpdatePlageOuverturesExpirationsJob, type: :job do
+  let!(:plage_ouverture_reguliere) { create(:plage_ouverture, recurrence: Montrose.every(:week, until: Time.now)) }
+  let!(:plage_ouverture_exceptionnelle) { create(:plage_ouverture, :no_recurrence, first_day: Date.today) }
+
   subject do
     UpdatePlageOuverturesExpirationsJob.perform_now
   end
 
   context "when plage ouverture are expired" do
-    let!(:plage_ouverture_reguliere) { create(:plage_ouverture, recurrence: Montrose.every(:week, until: Time.now).to_json) }
-    let!(:plage_ouverture_exceptionnelle) { create(:plage_ouverture, :no_recurrence, first_day: Date.today) }
-
     it "should call notification service" do
       travel_to(2.days.from_now)
       expect { subject }.to change(PlageOuverture.where(expired_cached: true), :count).from(0).to(2)
@@ -14,9 +14,6 @@ RSpec.describe UpdatePlageOuverturesExpirationsJob, type: :job do
   end
 
   context "when plage ouverture are not expired" do
-    let!(:plage_ouverture_reguliere) { create(:plage_ouverture, recurrence: Montrose.every(:week, until: Time.now).to_json) }
-    let!(:plage_ouverture_exceptionnelle) { create(:plage_ouverture, :no_recurrence, first_day: Date.today) }
-
     it "should call notification service" do
       expect { subject }.not_to change(PlageOuverture.where(expired_cached: true), :count)
     end
