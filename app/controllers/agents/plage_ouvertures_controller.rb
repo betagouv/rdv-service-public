08dd26d20bd68cbd87db11a0ca870720bcfbb7c8
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-07-23 18:14:09 +0200

    simplify ordering to updated_at desc

--- app/controllers/agents/plage_ouvertures_controller.rb
+++ app/controllers/agents/plage_ouvertures_controller.rb
@@ -8,7 +8,7 @@ class Agents::PlageOuverturesController < AgentAuthController
     plage_ouvertures = policy_scope(PlageOuverture)
       .includes(:lieu, :organisation)
       .where(agent_id: filter_params[:agent_id])
-      .order(recurrence: :asc, updated_at: :desc)
+      .order(updated_at: :desc)
     respond_to do |f|
       f.json { @plage_ouverture_occurences = plage_ouvertures.flat_map { |po| po.occurences_for(date_range_params).map { |occurence| [po, occurence] } }.sort_by(&:second) }
       f.html do
