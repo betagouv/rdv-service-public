28974cf8e4860df9c3e8963e2f4de518716da986
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-07-28 10:18:44 +0200

    add users.phone_number_formatted column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_132842) do
+ActiveRecord::Schema.define(version: 2020_07_28_074728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -316,6 +316,7 @@ ActiveRecord::Schema.define(version: 2020_07_02_132842) do
     t.string "birth_name"
     t.text "old_notes"
     t.string "email_original"
+    t.string "phone_number_formatted"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true, where: "(email IS NOT NULL)"
     t.index ["invitation_token"], name: "index_users_on_invitation_token", unique: true
