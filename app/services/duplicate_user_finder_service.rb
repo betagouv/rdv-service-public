5a346117a0d60d0ec07cce1bfb75515173369735
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2020-07-29 17:42:41 +0200

    [agent] improve duplicate user warning UI on creation + allow duplicate phone numbers
    * [agent] improve duplicate user warning UI on creation + allow duplicate phone numbers
    
    * fix other organisation case

--- app/services/duplicate_user_finder_service.rb
+++ app/services/duplicate_user_finder_service.rb
@@ -1,31 +1,45 @@
-class DuplicateUserFinderService
+class DuplicateUserFinderService < BaseService
   attr_reader :user
 
-  def initialize(user)
+  def initialize(user, skip_warnings: false)
     @user = user
+    @skip_warnings = skip_warnings
   end
 
   def perform
-    user_with_same_email = users_in_scope.where.not(email: nil).find_by(email: @user.email)
-    return user_with_same_email if user_with_same_email.present?
-
-    similar_users = User.none
-    if @user.phone_number.present?
-      similar_users = similar_users.or(users_in_scope.where(phone_number: @user.phone_number))
-    end
-    if @user.birth_date.present? && @user.first_name.present? && @user.last_name.present?
-      similar_users = similar_users.or(
-        users_in_scope.where(
-          first_name: @user.first_name.capitalize,
-          last_name: @user.last_name.upcase,
-          birth_date: @user.birth_date
-        )
-      )
-    end
-    similar_users.left_joins(:rdvs).group(:id).order('COUNT(rdvs.id) DESC').first
+    check_email || check_identity || (check_phone_number unless @skip_warnings) || nil
   end
 
   private
+
+  def check_email
+    similar_user = users_in_scope.where.not(email: nil).find_by(email: @user.email)
+    return nil unless similar_user.present?
+
+    OpenStruct.new(severity: :error, attributes: [:email], user: similar_user)
+  end
+
+  def check_identity
+    return nil unless @user.birth_date.present? && @user.first_name.present? && @user.last_name.present?
+
+    similar_user = users_in_scope.where(
+      first_name: @user.first_name.capitalize,
+      last_name: @user.last_name.upcase,
+      birth_date: @user.birth_date
+    ).left_joins(:rdvs).group(:id).order('COUNT(rdvs.id) DESC').first
+    return nil unless similar_user.present?
+
+    OpenStruct.new(severity: :error, attributes: [:first_name, :last_name, :birth_date], user: similar_user)
+  end
+
+  def check_phone_number
+    return nil unless @user.phone_number.present?
+
+    similar_user = users_in_scope.where(phone_number: @user.phone_number).left_joins(:rdvs).group(:id).order('COUNT(rdvs.id) DESC').first
+    return nil unless similar_user.present?
+
+    OpenStruct.new(severity: :warning, attributes: [:phone_number], user: similar_user)
+  end
 
   def users_in_scope
     User.active
