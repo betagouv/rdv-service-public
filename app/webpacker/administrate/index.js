27c7c88b02ff1ab0a1f3ded6744cc974c1ae23b6
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-08-03 18:45:28 +0200

    remove selectize JS package dependency used only in admin and deprecated

--- app/webpacker/administrate/index.js
+++ app/webpacker/administrate/index.js
@@ -1,9 +1,6 @@
-import 'selectize'
-
 import '../components/table'
 import { PlacesInputs } from '../components/places-inputs.js';
 
 $(document).on('turbolinks:load', function() {
-  $(".field-unit--has-many select").selectize({});
   new PlacesInputs();
 });
