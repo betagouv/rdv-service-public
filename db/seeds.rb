e58f1a45fa6c4a5ced5305a50517b8fdb6fabce2
Thomas Guillet <guillet.thomas@gmail.com>
2020-08-04 10:44:46 +0200

    Ajoute des numéros de téléphone aux usagers de test

--- db/seeds.rb
+++ db/seeds.rb
@@ -205,6 +205,7 @@ user_org_paris_nord_patricia = User.new(
   email: "patricia_duroy@demo.rdv-solidarites.fr",
   birth_date: Date.parse("20/06/1975"),
   password: "123456",
+  phone_number: "0123456789",
   organisation_ids: [org_paris_nord.id]
 )
 
@@ -218,6 +219,7 @@ user_org_paris_nord_lea = User.new(
   email: "lea_dupont@demo.rdv-solidarites.fr",
   birth_date: Date.parse("01/12/1982"),
   password: "123456",
+  phone_number: "0123456788",
   organisation_ids: [org_paris_nord.id]
 )
 
@@ -231,6 +233,7 @@ user_org_paris_nord_jean = User.new(
   email: "jean_moustache@demo.rdv-solidarites.fr",
   birth_date: Date.parse("10/01/1973"),
   password: "123456",
+  phone_number: "0123456787",
   organisation_ids: [org_paris_nord.id]
 )
 
