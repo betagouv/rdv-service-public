5851e4a2170b80c7b85624d28eaa39e7ab6d3765
Thomas Guillet <guillet.thomas@gmail.com>
2020-08-04 12:19:24 +0200

    Ajoute des champs aux notifications (#819)

--- app/blueprints/user_blueprint.rb
+++ app/blueprints/user_blueprint.rb
@@ -1,6 +1,6 @@
 class UserBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :first_name, :last_name, :email, :address, :birth_date
+  fields :first_name, :last_name, :email, :address, :birth_date, :phone_number
   association :responsible, blueprint: UserBlueprint
 end
