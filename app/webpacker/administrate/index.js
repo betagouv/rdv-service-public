64ba0431cea4503ab953a20c7e45aae1511cdffc
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-08-04 11:51:48 +0200

    use select2 in super admin instead of selectize

--- app/webpacker/administrate/index.js
+++ app/webpacker/administrate/index.js
@@ -1,6 +1,8 @@
 import '../components/table'
+import 'select2/dist/js/select2.min.js';
 import { PlacesInputs } from '../components/places-inputs.js';
 
 $(document).on('turbolinks:load', function() {
   new PlacesInputs();
+  $(".field-unit--has-many select").select2({theme: "bootstrap4"})
 });
