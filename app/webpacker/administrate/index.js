631a68d6edc080a89531ed01aba952968207df4e
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2020-08-04 13:46:04 +0200

    remove selectize JS package dependency used only in admin and deprecated (#818)
    * remove selectize JS package dependency used only in admin and deprecated
    
    * use select2 in super admin instead of selectize

--- app/webpacker/administrate/index.js
+++ app/webpacker/administrate/index.js
@@ -1,9 +1,8 @@
-import 'selectize'
-
 import '../components/table'
+import 'select2/dist/js/select2.min.js';
 import { PlacesInputs } from '../components/places-inputs.js';
 
 $(document).on('turbolinks:load', function() {
-  $(".field-unit--has-many select").selectize({});
   new PlacesInputs();
+  $(".field-unit--has-many select").select2({theme: "bootstrap4"})
 });
