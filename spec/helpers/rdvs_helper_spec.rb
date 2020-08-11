6a50ae21df37074b2dc27e96e2fa68e2e789454a
Yannick François <yannick.francois@beta.gouv.fr>
2020-08-11 09:50:13 +0200

    Refactor interface fiche usager (#817)
    * revoie la page usager et rendez-vous
    
    * revise le titre du bloc rdv
    
    * prépare les rendez-vous nécessaires
    
    * affiche le lien vers le calendrier

--- spec/helpers/rdvs_helper_spec.rb
+++ spec/helpers/rdvs_helper_spec.rb
@@ -33,4 +33,11 @@ describe RdvsHelper do
       it { should eq "Marie DENIS 🏠" }
     end
   end
+
+  describe "#rdv_time_and_duration" do
+    it "return starts_at hour, minutes and duration" do
+      rdv = build(:rdv, starts_at: DateTime.new(2020, 3, 23, 12, 46), duration_in_min: 4)
+      expect(rdv_time_and_duration(rdv)).to eq("13h46 (4 minutes)")
+    end
+  end
 end
