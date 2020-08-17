6fd76d4c8d269beb4f3cc22025f4bcdeb1035523
Yannick Francois <yaf@elsif.fr>
2020-08-17 18:14:06 +0200

    retrouve les prénoms et noms prochent

--- app/services/find_duplicate_users_service.rb
+++ app/services/find_duplicate_users_service.rb
@@ -1,10 +1,34 @@
 class FindDuplicateUsersService < BaseService
+  attr_reader :user
+
+  SCORE_THRESHOLD = 0.8
 
   def initialize(user)
+    @user = user
   end
 
   def perform
-    []
+    result = []
+    name1 = format_user_name(user)
+
+    user.organisations.each do |organisation|
+      organisation.users.where.not(id: user.id).each do |other_user|
+        name2 = format_user_name(other_user)
+        score = 1 - (DamerauLevenshtein.distance(name1, name2).to_f / [name1.length, name2.length].max)
+        result << other_user if score >= SCORE_THRESHOLD
+      end
+    end
+    result
+  end
+
+  private
+
+  def format_user_name(user)
+    "#{user.first_name} #{user.last_name}"
+      .parameterize
+      .gsub(/(monsieur\-|madame\-|mr\-|mme\-|m\-|mlle\-)/, "")
+      .gsub("-", " ")
+      .strip
   end
 
 end
--- spec/services/find_duplicate_users_service_spec.rb
+++ spec/services/find_duplicate_users_service_spec.rb
@@ -1,9 +1,40 @@
 describe FindDuplicateUsersService, type: :service do
   describe ".perform" do
     it "return empty array when no duplicate" do
-      user = build(:user)
-      duplicateUsers = FindDuplicateUsersService.perform_with(user)
-      expect(duplicateUsers).to eq([])
+      assert_duplication_found(build(:user), [])
     end
+
+    it "return user that match perfectly with given user" do
+      organisation = create(:organisation)
+      user = create(:user, first_name: "André", last_name: "André", organisations: [organisation])
+      duplicate = create(:user, first_name: "André", last_name: "Andrée", organisations: [organisation])
+      assert_duplication_found(user, [duplicate])
+    end
+
+    it "not return a user that to different" do
+      organisation = create(:organisation)
+      user = create(:user, first_name: "André", last_name: "André", organisations: [organisation])
+      create(:user, first_name: "Stéphane", last_name: "Henri", organisations: [organisation])
+      assert_duplication_found(user, [])
+    end
+
+    it "match with monsieur in the name" do
+      organisation = create(:organisation)
+      user = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUES")
+      duplicate = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUS")
+      assert_duplication_found(user, [duplicate])
+    end
+
+    it "not match, even with monsieur / madame" do
+      organisation = create(:organisation)
+      user = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUES")
+      duplicate = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JANY")
+      assert_duplication_found(user, [])
+    end
+  end
+
+  def assert_duplication_found(user, users)
+    duplicateUsers = FindDuplicateUsersService.perform_with(user)
+    expect(duplicateUsers).to eq(users)
   end
 end
