e5a1a44163b3f3886a8e8404fbd8a27500b0e3c8
Yannick Francois <yaf@elsif.fr>
2020-08-18 10:19:44 +0200

    simplifie le service pour trouver le premier doublon potentiel

---
+++ app/services/mark_first_duplicate_users_service.rb
@@ -0,0 +1,49 @@
+class MarkFirstDuplicateUsersService < BaseService
+  attr_reader :user
+
+  SCORE_THRESHOLD = 0.8
+
+  def initialize(user)
+    @user = user
+  end
+
+  def perform
+    mark first_duplicate
+  end
+
+  def first_duplicate
+    user.organisations.each do |organisation|
+      organisation.users.where.not(id: user.id).each do |other_user|
+        return other_user if names_are_close?(user, other_user)
+      end
+    end
+    nil
+  end
+
+  def names_are_close?(user, other_user)
+    score(user, other_user) >= SCORE_THRESHOLD
+  end
+
+  def score(user, other_user)
+    name1 = format_user_name(user)
+    name2 = format_user_name(other_user)
+    1 - (DamerauLevenshtein.distance(name1, name2).to_f / [name1.length, name2.length].max)
+  end
+
+  def mark(duplicate)
+    user.update(potential_duplicate: duplicate) unless duplicate.blank?
+    duplicate
+  end
+
+  private
+
+  def format_user_name(user)
+    "#{user.first_name} #{user.last_name}"
+      .parameterize
+      .gsub(/(monsieur\-|madame\-|mr\-|mme\-|m\-|mlle\-)/, "")
+      .gsub("-", " ")
+      .strip
+  end
+
+end
+
--- spec/services/mark_first_duplicate_users_service_spec.rb
+++ spec/services/mark_first_duplicate_users_service_spec.rb
@@ -1,40 +1,41 @@
-describe FindDuplicateUsersService, type: :service do
+describe MarkFirstDuplicateUsersService, type: :service do
   describe ".perform" do
     it "return empty array when no duplicate" do
-      assert_duplication_found(build(:user), [])
+      assert_duplication_found(create(:user), nil)
     end
 
-    it "return user that match perfectly with given user" do
+    it "return user that match with given user" do
       organisation = create(:organisation)
       user = create(:user, first_name: "André", last_name: "André", organisations: [organisation])
       duplicate = create(:user, first_name: "André", last_name: "Andrée", organisations: [organisation])
-      assert_duplication_found(user, [duplicate])
+      assert_duplication_found(user, duplicate)
     end
 
     it "not return a user that to different" do
       organisation = create(:organisation)
       user = create(:user, first_name: "André", last_name: "André", organisations: [organisation])
       create(:user, first_name: "Stéphane", last_name: "Henri", organisations: [organisation])
-      assert_duplication_found(user, [])
+      assert_duplication_found(user, nil)
     end
 
     it "match with monsieur in the name" do
       organisation = create(:organisation)
       user = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUES")
       duplicate = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUS")
-      assert_duplication_found(user, [duplicate])
+      assert_duplication_found(user, duplicate)
     end
 
     it "not match, even with monsieur / madame" do
       organisation = create(:organisation)
       user = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JACQUES")
       duplicate = create(:user, organisations: [organisation], first_name: "Monsieur / Madame Jean", last_name: "JANY")
-      assert_duplication_found(user, [])
+      assert_duplication_found(user, nil)
     end
   end
 
-  def assert_duplication_found(user, users)
-    duplicateUsers = FindDuplicateUsersService.perform_with(user)
-    expect(duplicateUsers).to eq(users)
+  def assert_duplication_found(user, expected_duplicate)
+    duplicate = MarkFirstDuplicateUsersService.perform_with(user)
+    expect(duplicate).to eq(expected_duplicate)
+    expect(user.reload.potential_duplicate).to eq(expected_duplicate)
   end
 end
