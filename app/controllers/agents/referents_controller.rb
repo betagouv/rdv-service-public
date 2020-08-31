51fe5923d4e0d594265cd0972163da35fe0cb906
Yannick Francois <yaf@elsif.fr>
2020-08-31 11:56:34 +0200

    affiche les agents référents du responsable

--- app/controllers/agents/referents_controller.rb
+++ app/controllers/agents/referents_controller.rb
@@ -1,10 +1,9 @@
 class Agents::ReferentsController < AgentAuthController
   before_action :set_organisation, only: [:create]
+  before_action :set_user
 
   def create
-    user = User.find(params[:user_id])
-    authorize(user)
-    flash[:error] = "Erreur lors de la modification des référents" unless user.update(agents: Agent.where(id: params[:user][:agent_ids]))
-    redirect_to organisation_user_path(current_organisation, user)
+    flash[:error] = "Erreur lors de la modification des référents" unless @user.update(agents: Agent.where(id: params[:user][:agent_ids]))
+    redirect_to organisation_user_path(current_organisation, @user)
   end
 end
