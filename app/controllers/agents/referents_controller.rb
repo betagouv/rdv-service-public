3308b78352b5b6cd97bbda96a832a2a00d441534
Yannick Francois <yaf@elsif.fr>
2020-08-31 14:44:39 +0200

    supprime la policy agent qui est trop restrictive dans ce cas

--- app/controllers/agents/referents_controller.rb
+++ app/controllers/agents/referents_controller.rb
@@ -2,7 +2,7 @@ class Agents::ReferentsController < AgentAuthController
   def update
     user = policy_scope(User).find(params[:user_id])
     authorize(user)
-    agents = policy_scope(Agent).where(id: params[:user][:agent_ids])
+    agents = current_organisation.agents.where(id: params[:user][:agent_ids])
     flash[:error] = "Erreur lors de la modification des référents" unless user.update(agents: agents)
     redirect_to organisation_user_path(current_organisation, user)
   end
