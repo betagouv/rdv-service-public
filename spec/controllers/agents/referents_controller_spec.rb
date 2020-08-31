8a5097b710f72abb01927fcf32e351c7c1b343ef
Yannick Francois <yaf@elsif.fr>
2020-08-31 14:31:21 +0200

    corrige les tests

--- spec/controllers/agents/referents_controller_spec.rb
+++ spec/controllers/agents/referents_controller_spec.rb
@@ -1,5 +1,5 @@
 describe Agents::ReferentsController, type: :controller do
-  describe "#create" do
+  describe "#update" do
     it "update user's agent_ids reference and redirect to user's page" do
       organisation = create(:organisation)
       user = create(:user, agents: [], organisations: [organisation])
@@ -9,7 +9,7 @@ describe Agents::ReferentsController, type: :controller do
       expect(agent.organisations).to eq([organisation])
       sign_in agent
 
-      post :create, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: [lea.id, stef.id] } }
+      post :update, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: [lea.id, stef.id] } }
 
       expect(user.reload.agents.sort).to eq([lea, stef].sort)
       expect(response).to redirect_to(organisation_user_path(organisation, user))
@@ -22,7 +22,7 @@ describe Agents::ReferentsController, type: :controller do
       user = create(:user, agents: [lea], organisations: [organisation])
       sign_in agent
 
-      post :create, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: ["bad agent id"] } }
+      post :update, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: ["bad agent id"] } }
 
       expect(user.reload.agents).to eq([])
       expect(response).to redirect_to(organisation_user_path(organisation, user))
@@ -37,7 +37,7 @@ describe Agents::ReferentsController, type: :controller do
 
       allow_any_instance_of(User).to receive(:update).and_return(false)
 
-      post :create, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: ["bad agent id"] } }
+      post :update, params: { organisation_id: organisation.id, user_id: user.id, user: { agent_ids: ["bad agent id"] } }
 
       expect(flash[:error]).to eq("Erreur lors de la modification des référents")
       expect(user.reload.agents).to eq([lea])
