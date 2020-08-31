fe3da114743579c33c1c044f38e65c8254f57f58
Yannick Francois <yaf@elsif.fr>
2020-08-31 12:03:19 +0200

    utilise la policy pour retrouver l'usager

--- app/controllers/agents/referents_controller.rb
+++ app/controllers/agents/referents_controller.rb
@@ -1,9 +1,9 @@
 class Agents::ReferentsController < AgentAuthController
   before_action :set_organisation, only: [:create]
-  before_action :set_user
 
   def create
-    flash[:error] = "Erreur lors de la modification des référents" unless @user.update(agents: Agent.where(id: params[:user][:agent_ids]))
-    redirect_to organisation_user_path(current_organisation, @user)
+    user = policy_scope(User).find(params[:id])
+    flash[:error] = "Erreur lors de la modification des référents" unless user.update(agents: Agent.where(id: params[:user][:agent_ids]))
+    redirect_to organisation_user_path(current_organisation, user)
   end
 end
