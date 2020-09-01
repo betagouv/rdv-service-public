662f605fad22322736688aaee30061044719c57b
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2020-09-01 16:02:21 +0200

    [agent] create new organisation autonomously for admins (#849)
    * [agent] Create new organisation autonomously for admins
    
    * added Admin::OrganisationsController spec for organisation creation
    
    Co-authored-by: Yannick François <yannick.francois@beta.gouv.fr>

--- spec/mailers/previews/admins/organisation_mailer_preview.rb
+++ spec/mailers/previews/admins/organisation_mailer_preview.rb
@@ -1,6 +1,6 @@
 class Admins::OrganisationMailerPreview < ActionMailer::Preview
   def organisation_created
     agent = Organisation.first.agents.first
-    Admins::OrganisationMailer.organisation_created(agent)
+    Admins::OrganisationMailer.organisation_created(agent, agent.organisations.first)
   end
 end
