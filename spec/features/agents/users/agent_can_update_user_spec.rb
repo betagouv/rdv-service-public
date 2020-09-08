933b1c7d2ee6fc5b39a8ec31fcb567698617ec70
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-09-08 13:47:02 +0200

    in users show moved notes from separate card to main one

--- spec/features/agents/users/agent_can_update_user_spec.rb
+++ spec/features/agents/users/agent_can_update_user_spec.rb
@@ -5,7 +5,7 @@ describe "Agent can update user" do
     create(:user, first_name: "Jean", last_name: "LEGENDE", email: "jean@legende.com", organisations: [organisation])
   end
 
-  def navigate_to_user_show_page
+  before do
     login_as(agent, scope: :agent)
     visit authenticated_agent_root_path
     click_link "Vos usagers"
@@ -13,11 +13,10 @@ describe "Agent can update user" do
     expect_page_title("Vos usagers")
     click_link "Jean LEGENDE"
     expect_page_title("Jean LEGENDE")
+    click_link "Modifier"
   end
 
   scenario "update existing user's email" do
-    navigate_to_user_show_page
-    click_link "Modifier"
     fill_in :user_first_name, with: "jeanne"
     fill_in :user_last_name, with: "reynolds"
     fill_in "Email", with: "jeanne@reynolds.com"
@@ -29,27 +28,23 @@ describe "Agent can update user" do
     expect(page).to have_content("En attente de confirmation pour jeanne@reynolds.com")
   end
 
+  scenario "update user notes" do
+    fill_in "Remarques", with: "Pas très sympa"
+    click_button "Modifier"
+    expect(page).to have_content("Pas très sympa")
+  end
+
   context "unregistered user" do
     let!(:user) do
       create(:user, :unregistered, first_name: "Jean", last_name: "LEGENDE", email: nil, organisations: [organisation])
     end
 
     scenario "add email to existing user", js: true do
-      navigate_to_user_show_page
-      click_link "Modifier"
       fill_in "Email", with: "jean@legende.com"
       click_button "Modifier"
       click_link "Inviter"
       open_email("jean@legende.com")
       expect(current_email.subject).to eq I18n.t("devise.mailer.invitation_instructions.subject")
     end
-  end
-
-  it "allows updating user notes from show page" do
-    navigate_to_user_show_page
-    fill_in :user_user_profiles_attributes_0_notes, with: "Tres cool"
-    find(".card#notes input[type=submit]").click
-    expect(page).to have_content("L'usager a été modifié")
-    expect(page).to have_content("Tres cool")
   end
 end
