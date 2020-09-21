55ba7920315a1cd4a1a9e7133b4f3c2e473ff585
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-09-21 11:07:15 +0200

    various fixes

--- app/controllers/admin/creneaux/agent_searches_controller.rb
+++ app/controllers/admin/creneaux/agent_searches_controller.rb
@@ -2,7 +2,6 @@ class Admin::Creneaux::AgentSearchesController < AgentAuthController
   respond_to :html, :js
 
   def index
-    skip_policy_scope # TODO: remove
     @user = User.find(params[:user_id]) if params[:user_id]
     @form = build_agent_creneaux_search_form
     @search_results = SearchCreneauxForAgentsService.perform_with(@form) \
@@ -13,7 +12,9 @@ class Admin::Creneaux::AgentSearchesController < AgentAuthController
         @agents = policy_scope(Agent).complete.active.order_by_last_name
         @lieux = policy_scope(Lieu).ordered_by_name
       end
-      format.js
+      format.js do
+        skip_policy_scope # TODO: improve pundit checks for creneaux
+      end
     end
   end
 
@@ -24,8 +25,8 @@ class Admin::Creneaux::AgentSearchesController < AgentAuthController
       organisation_id: current_organisation.id,
       motif_id: params[:motif_id],
       from_date: params[:from_date],
+      user_id: params[:user_id].presence,
       agent_ids: params[:agent_ids]&.reject(&:blank?) || [],
-      user_ids: params[:user_ids]&.reject(&:blank?) || [],
       lieu_ids: params[:lieu_ids]&.reject(&:blank?) || []
     )
   end
