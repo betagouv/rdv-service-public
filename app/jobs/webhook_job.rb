fd51670c7a2e096d59b228b9b00e1cd3bbf7da37
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2020-10-07 10:32:49 +0200

    Ensure HTTP errors in web hook logic are reported - fixed (#915)
    * Ensure HTTP errors in web hook logic are reported (#913)
    
    * Force encoding to UTF-8 in webhook response bodies
    
    There is a bug in Typhoeus. See
    https://github.com/typhoeus/typhoeus/issues/580
    This will force the response_body to always be UTF-8.
    
    Some response_body are coming back with encoding=ASCII-8BIT but are not writable to
    sentry, with errors like this:
    
    Co-authored-by: Thomas Guillet <guillet.thomas@gmail.com>

--- app/jobs/webhook_job.rb
+++ app/jobs/webhook_job.rb
@@ -1,11 +1,14 @@
+class OutgoingWebhookError < StandardError; end
+
 class WebhookJob < ApplicationJob
   TIMEOUT = 10
 
   def perform(payload, webhook_endpoint_id)
     webhook_endpoint = WebhookEndpoint.find(webhook_endpoint_id)
 
-    Typhoeus.post(
+    request = Typhoeus::Request.new(
       webhook_endpoint.target_url,
+      method: :post,
       headers: {
         "Content-Type" => "application/json; charset=utf-8",
         "X-Lapin-Signature" => OpenSSL::HMAC.hexdigest("SHA256", webhook_endpoint.secret, payload),
@@ -13,5 +16,11 @@ class WebhookJob < ApplicationJob
       body: payload,
       timeout: TIMEOUT
     )
+
+    request.on_complete do |response|
+      raise OutgoingWebhookError, "Webhook failed with status code #{response.code} and body #{response.body.force_encoding('UTF-8')[0...1000]}" unless response.success?
+    end
+
+    request.run
   end
 end
