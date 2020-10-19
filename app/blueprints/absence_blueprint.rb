cb66d161020f5e4a948bfbd3452bec24dff71a8f
Thomas Guillet <guillet.thomas@gmail.com>
2020-10-19 16:54:56 +0200

    Add end_day to Absence JSON payload

--- app/blueprints/absence_blueprint.rb
+++ app/blueprints/absence_blueprint.rb
@@ -1,7 +1,7 @@
 class AbsenceBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :ical_uid, :title, :first_day, :start_time, :end_time
+  fields :ical_uid, :title, :first_day, :end_day, :start_time, :end_time
   association :agent, blueprint: AgentBlueprint
   association :organisation, blueprint: OrganisationBlueprint
 end
