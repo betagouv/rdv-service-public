aa7f0a9caed3a50f05bf957c0d4252a7e362812e
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-10-20 18:26:40 +0200

    fix parsing RDV wizard context params in Devise concern

--- app/controllers/concerns/from_rdv_params.rb
+++ app/controllers/concerns/from_rdv_params.rb
@@ -1,11 +1,17 @@
-module FromRdvParams
+module CanHaveRdvWizardContext
   extend ActiveSupport::Concern
 
-  def set_resources_from_rdv_params
-    stored_location = session[:user_return_to]
-    return if stored_location.blank? || Rails.application.routes.recognize_path(stored_location) != { controller: "users/rdv_wizard_steps", action: "new" }
+  included do
+    before_action :set_rdv_wizard_context_variables
+  end
 
-    parsed = Rack::Utils.parse_nested_query(URI.parse(stored_location).query)
+  def set_rdv_wizard_context_variables
+    return if session[:user_return_to].blank?
+
+    parsed_uri = URI.parse(session[:user_return_to])
+    return if parsed_uri.path != "/users/rdv_wizard_step/new"
+
+    parsed = Rack::Utils.parse_nested_query(parsed_uri.query)
     @motif = Motif.find(parsed["motif_id"]) if parsed["motif_id"].present?
     @starts_at = Time.parse(parsed["starts_at"]) if parsed["starts_at"].present?
     @lieu = Lieu.find(parsed["lieu_id"]) if parsed["lieu_id"].present?
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,9 @@
 class Users::SessionsController < Devise::SessionsController
   layout "user_registration"
 
-  include FromRdvParams
+  include CanHaveRdvWizardContext
 
   before_action :exclude_signed_in_agents
-  before_action :set_resources_from_rdv_params
 
   def create
     if auth_options[:scope] == :user && (self.resource = Agent.find_by(email: params[:user]["email"])) && resource.valid_password?(params[:user]["password"])
