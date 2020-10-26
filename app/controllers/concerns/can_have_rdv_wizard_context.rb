682801548e529add2480cb715125cdcfb634e792
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-10-26 20:12:15 +0100

    DRY up users rdv wizard summary template + display it on step1

--- app/controllers/concerns/can_have_rdv_wizard_context.rb
+++ app/controllers/concerns/can_have_rdv_wizard_context.rb
@@ -2,18 +2,18 @@ module CanHaveRdvWizardContext
   extend ActiveSupport::Concern
 
   included do
-    before_action :set_rdv_wizard_context_variables
+    before_action :set_rdv_wizard_from_devise_return_path
   end
 
-  def set_rdv_wizard_context_variables
+  def set_rdv_wizard_from_devise_return_path
     return if session[:user_return_to].blank?
 
     parsed_uri = URI.parse(session[:user_return_to])
     return if parsed_uri.path != "/users/rdv_wizard_step/new"
 
-    parsed = Rack::Utils.parse_nested_query(parsed_uri.query)
-    @motif = Motif.find(parsed["motif_id"]) if parsed["motif_id"].present?
-    @starts_at = Time.parse(parsed["starts_at"]) if parsed["starts_at"].present?
-    @lieu = Lieu.find(parsed["lieu_id"]) if parsed["lieu_id"].present?
+    parsed_params = Rack::Utils.parse_nested_query(parsed_uri.query).to_h.symbolize_keys
+    return if [:motif_id, :starts_at, :lieu_id].any? { parsed_params[_1].blank? }
+
+    @rdv_wizard = UserRdvWizard::Step1.new(nil, parsed_params)
   end
 end
