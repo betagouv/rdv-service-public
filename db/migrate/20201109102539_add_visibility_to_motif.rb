3a0b108aa14dbb924323536d6bca938042ad3d64
Yannick Francois <yaf@elsif.fr>
2020-11-10 11:07:53 +0100

    migration sans retour possible
    https://github.com/betagouv/rdv-solidarites.fr/pull/947#discussion_r520361545
    
    Co-authored-by: Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>

--- db/migrate/20201109102539_add_visibility_to_motif.rb
+++ db/migrate/20201109102539_add_visibility_to_motif.rb
@@ -1,5 +1,5 @@
 class AddVisibilityToMotif < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :motifs, :visibility_type, :integer, null: false, default: Motif.visibility_types[:visible_and_notified]
 
     Motif.where(disable_notifications_for_users: true).update_all(visibility_type: Motif.visibility_types[:visible_and_not_notified])
