fe69f0e33dd1fba0000288e9468326efc889ca9f
Yannick Francois <yaf@elsif.fr>
2020-11-16 17:51:00 +0100

    Sépare les assertions

--- spec/services/rdv_updater_spec.rb
+++ spec/services/rdv_updater_spec.rb
@@ -8,6 +8,16 @@ describe RdvUpdater, type: :service do
       expect(rdv_updater.update(rdv_params)).to eq(true)
       expect(rdv.reload.context).to eq("un autre context")
     end
+
+    it "call rdv.update with params" do
+      rdv = create(:rdv, context: "un contexte")
+      rdv_updater = RdvUpdater.new(rdv)
+      rdv_params = {context: "un autre context"}
+
+      expect(rdv).to receive(:update).with(rdv_params).and_return(true)
+      rdv_updater.update(rdv_params)
+    end
+
   end
 
 end
