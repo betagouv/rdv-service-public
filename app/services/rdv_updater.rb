d30eb2fcd385a8ff0b83a07963f4bc95946dc5e1
Yannick Francois <yaf@elsif.fr>
2020-11-23 09:48:12 +0100

    applique les recommandations du linter

--- app/services/rdv_updater.rb
+++ app/services/rdv_updater.rb
@@ -1,27 +1,29 @@
 module RdvUpdater
-  def self.update(rdv, rdv_params)
-    rdv.updated_at = Time.zone.now
-    # TODO: replace this manual touch. It forces creating a version when an
-    # agent or a user is removed from the RDV. the touch: true option on the
-    # association does not do it for some reason I could not figure out
+  class << self
+    def update(rdv, rdv_params)
+      rdv.updated_at = Time.zone.now
+      # TODO: replace this manual touch. It forces creating a version when an
+      # agent or a user is removed from the RDV. the touch: true option on the
+      # association does not do it for some reason I could not figure out
 
-    if rdv_params[:status].present?
-      rdv_params[:cancelled_at] = cancel_status?(rdv_params[:status]) ? Time.zone.now : nil
+      if rdv_params[:status].present?
+        rdv_params[:cancelled_at] = cancel_status?(rdv_params[:status]) ? Time.zone.now : nil
+      end
+
+      return false unless rdv.update(rdv_params)
+
+      if rdv_params[:status] == "excused"
+        Notifications::Rdv::RdvCancelledByAgentService.perform_with(rdv)
+        "Le rendez-vous a été annulé."
+      else
+        "Le rendez-vous a été modifié."
+      end
     end
 
-    return false unless rdv.update(rdv_params)
+    private
 
-    if rdv_params[:status] == "excused"
-      Notifications::Rdv::RdvCancelledByAgentService.perform_with(rdv)
-      "Le rendez-vous a été annulé."
-    else
-      "Le rendez-vous a été modifié."
+    def cancel_status?(status)
+      %w[excused notexcused].include?(status)
     end
-  end
-
-  private
-
-  def self.cancel_status?(status)
-    %w[excused notexcused].include?(status)
   end
 end
