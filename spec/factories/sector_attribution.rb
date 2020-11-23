d8b48dd46686f34fd1bbb0b764199a88bec19f53
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-11-23 18:21:46 +0100

    refactor Users::GeoSearch specs

---
+++ spec/factories/sector_attribution.rb
@@ -0,0 +1,16 @@
+FactoryBot.define do
+  factory :sector_attribution do
+    sector
+    organisation
+    level { SectorAttribution::LEVEL_ORGANISATION }
+
+    trait :level_organisation do
+      level { SectorAttribution::LEVEL_ORGANISATION }
+    end
+
+    trait :level_agent do
+      level { SectorAttribution::LEVEL_AGENT }
+      agent { build(:agent, organisations: [organisation]) }
+    end
+  end
+end
