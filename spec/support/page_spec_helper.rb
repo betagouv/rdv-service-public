7c95fe92ff05457cdf4e2359a300353402bf0597
Yannick François <yannick.francois@beta.gouv.fr>
2020-12-15 17:09:05 +0100

    ajoute un bloc rendez-vous future sur fiche usager (#989)
    * ajoute un bloc rendez-vous future sur fiche usager
    
    * contextualise le bloc historique de RDV
    
    * supprime le bloc historique dans la fiche usager
    
    déplace le bouton « voir tous les rdvs » dans le bloc d'affichage de
    status de tout les RDV
    
    * affiche les rdv futur sur la fiche usager
    
    affiche les rdv passés sur la fiche RDV
    
    * Rectifie les tests d'intégrations
    
    * passage du linter
    
    * Utilise today? au lieu d'une égalité
    
    * Remaniement pour limiter la duplication
    
    * Une seul vue, avec la liste des RDV en paramètre
    
    * Extrait un titre adapté au contexte
    
    * Affiche une liste courte de rdv
    
    * adapte le test

--- spec/support/page_spec_helper.rb
+++ spec/support/page_spec_helper.rb
@@ -6,10 +6,4 @@ module PageSpecHelper
   def expect_page_with_no_record_text(text)
     expect(page).to have_selector(".card .card-body p.lead", text: text)
   end
-
-  def rdv_title_spec(rdv)
-    # TODO: remove this method and hardcode expected results for more explicit
-    # specs. this method is a bad duplicate of the one in RdvsHelper
-    "Le #{I18n.l(rdv.starts_at, format: :human)} (durée : #{rdv.duration_in_min} minutes)"
-  end
 end
