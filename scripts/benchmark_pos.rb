4c25a3a3561518e0b1decf5caf9dfdb15fda0a55
Adrien Di Pasquale <adrien@dipasquale.fr>
2020-12-24 10:51:37 +0100

    improve scripts

--- scripts/benchmark_pos.rb
+++ scripts/benchmark_pos.rb
@@ -1,10 +1,23 @@
 require "benchmark"
-ActiveRecord::Base.logger = nil
+# ActiveRecord::Base.logger = nil
 
 motif_name = "Je souhaite avoir un entretien avec mon référent"
 location_type = "public_office"
 date_range = (Date.today..(Date.today + 6.days))
-lieux = Organisation.find(101).lieux.for_motif(Motif.find(800))
+lieux = Organisation.find(101).lieux.for_motif(Motif.find(800)) # [153, 151, 344]
+
+# res = Benchmark.measure do
+#   SearchCreneauxForAgentsService.perform_with(
+#     OpenStruct.new(
+#       motif: Motif.find(800),
+#       agent_ids: nil,
+#       date_range: date_range,
+#       organisation: Organisation.find(101),
+#       lieu_ids: []
+#     )
+#   )
+# end
+# puts res
 
 res = Benchmark.measure do
   lieux.each do |lieu|
