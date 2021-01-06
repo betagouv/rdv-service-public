2b4bfcca611261c3f80b12f81aee895909f684b1
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-01-06 12:06:14 +0100

    migration to fix agents with missing uid

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_091156) do
+ActiveRecord::Schema.define(version: 2021_01_06_104714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
