3cd4a14db8c850ae359be70ba9addedd17e8a475
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-01-08 18:27:34 +0100

    updated deployed_changes script for changelog to use github project

--- scripts/get_deployed_changes.rb
+++ scripts/get_deployed_changes.rb
@@ -1,28 +1,66 @@
-# gem install ruby-trello
-# then in terminal:
-# irb -rubygems
-# irb> require 'trello'
-# irb> Trello.open_public_key_url                         # copy your public key
-# irb> Trello.open_authorization_url key: 'yourpublickey' # copy your member token
+# ruby scripts/get_deployed_changes.rb --archive
 
-# ruby scripts/get_deployed_changes.rb
+# initial setup:
+# head to https://github.com/settings/tokens and create a token with public_repo permission
+# store it in your .env like `GITHUB_CHANGELOG_USERPWD=adipasquale:XXXX`
+
+COLUMN_ID = 12_100_682
 
 require "dotenv/load"
-require "trello"
+require "typhoeus"
+require "json"
 
-Trello.configure do |config|
-  config.developer_public_key = ENV["TRELLO_DEVELOPER_PUBLIC_KEY"]
-  config.member_token = ENV["TRELLO_MEMBER_TOKEN"]
-end
-
-board = Trello::Board.find("5cdac252d7d01b6f728c0308")
-list_deployed = board.lists.last
-list_deployed.cards.each do |card|
-  puts "- [#{card.name}](#{card.short_url})"
+res = Typhoeus.get(
+  "https://api.github.com/projects/columns/#{COLUMN_ID}/cards",
+  userpwd: ENV["GITHUB_CHANGELOG_USERPWD"],
+  headers: { "Accept" => "application/vnd.github.inertia-preview+json" }
+)
+done_cards = JSON.parse(res.body)
+open_issues = []
+done_cards.each do |card|
+  issue = JSON.parse(Typhoeus.get(card["content_url"], userpwd: ENV["GITHUB_CHANGELOG_USERPWD"]).body)
+  open_issues << issue if issue["state"] == "open"
+  puts "- [#{issue['title']}](#{issue['html_url']})"
 end
 
 if ARGV[0] == "--archive"
-  puts "archiving all deployed cards..."
-  list_deployed.archive_all_cards
-  puts "done"
+  if open_issues.any?
+    puts "closing #{open_issues.count} open issues..."
+    open_issues.each do |issue|
+      puts issue["url"]
+      Typhoeus.patch(
+        issue["url"],
+        body: JSON.dump({ state: "closed" }),
+        userpwd: ENV["GITHUB_CHANGELOG_USERPWD"]
+      )
+      puts "done!"
+    end
+  end
+
+  puts "archive all #{done_cards.count} done cards..."
+  done_cards.each do |card|
+    Typhoeus.patch(
+      card["url"],
+      body: JSON.dump({ archived: true }),
+      userpwd: ENV["GITHUB_CHANGELOG_USERPWD"],
+      headers: { "Accept" => "application/vnd.github.inertia-preview+json" }
+    )
+  end
 end
+
+# commands to find column ID:
+
+# curl \
+#   -i -u adipasquale:XXXX \
+#   -H "Accept: application/vnd.github.inertia-preview+json"
+#   https://api.github.com/repos/betagouv/rdv-solidarites.fr/projects
+
+# curl \
+#   -i -u adipasquale:XXX \
+#   -H "Accept: application/vnd.github.inertia-preview+json" \
+#   https://api.github.com/projects/6111007/columns
+
+# curl \
+#   -i -u adipasquale:XXXX \
+#   -H "Accept: application/vnd.github.inertia-preview+json" \
+#   https://api.github.com/projects/columns/12100682/cards
