cc1d237c2af94c63601eb876dbace36ffac1e03c
Yannick Francois <yaf@elsif.fr>
2021-01-26 15:31:11 +0100

    supprime une variable inutilisée

--- app/service_models/rdvs_overlapping.rb
+++ app/service_models/rdvs_overlapping.rb
@@ -2,8 +2,6 @@ class RdvsOverlapping
   delegate :starts_at, :ends_at, :duration_in_min, :agents, to: :rdv
   attr_reader :rdv
 
-  THRESHOLD = 1.minute
-
   def initialize(rdv)
     @rdv = rdv
   end
