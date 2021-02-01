7baf928f2333b564272a8312bcc2f3a33053e0c7
Yannick Francois <yaf@elsif.fr>
2021-02-01 11:09:27 +0100

    Supprime les champs hidden

--- app/webpacker/components/motif-filters.js
+++ app/webpacker/components/motif-filters.js
@@ -1,14 +1,13 @@
 class MotifFilters {
   constructor() {
-    document.querySelectorAll('.motif-filters').forEach(formElt =>
-      formElt.addEventListener('change', this.changeFilters)
+    console.log("dans le motif filters constructor")
+    document.querySelectorAll('.motif-filters').forEach(formElt => {
+      $(formElt).on('change', this.changeFilters)
+    }
     )
   }
 
   changeFilters = () => {
-    ["service", "online", "location_type"].forEach(function (field) {
-      document.querySelector(`#${field}_filter`).value = document.querySelector(`#${field}_filter_select`).value
-    })
     document.querySelector(".search-and-filter-form").submit()
   }
 }
