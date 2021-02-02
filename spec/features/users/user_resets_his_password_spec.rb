169c233fef3ba14706c26838bcb9a6ed6e98c0ef
Kevin Vinhas <kevin@vinhas.fr>
2021-02-02 23:18:38 +0100

    Correct the expectation of password resetting page
    The "Didn't have confirmation" bit has been removed.

--- spec/features/users/user_resets_his_password_spec.rb
+++ spec/features/users/user_resets_his_password_spec.rb
@@ -5,7 +5,6 @@ feature "User resets his password spec" do
     scenario "sees proper translation of links" do
       expect(page).to have_content("Mot de passe oublié ?")
       expect(page).to have_link("Se connecter")
-      expect(page).to have_link("Je n'ai pas reçu le mail de confirmation")
     end
   end
 end
