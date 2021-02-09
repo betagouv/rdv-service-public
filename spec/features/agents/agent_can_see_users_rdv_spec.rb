76a89d0643e345a10b99fed20e85f345bf310e7a
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-02-09 10:53:43 +0100

    fix specs

--- spec/features/agents/agent_can_see_users_rdv_spec.rb
+++ spec/features/agents/agent_can_see_users_rdv_spec.rb
@@ -13,7 +13,7 @@ describe "can see users' RDV" do
   context "with no RDV" do
     before { click_link user.full_name }
     it do
-      expect(page).to have_content("À venir\n0 RDVs")
+      expect(page).to have_content("À venir\n0 RDV")
       expect(page).to have_content("aucun prochains rendez-vous")
     end
   end
@@ -23,7 +23,7 @@ describe "can see users' RDV" do
     let!(:rdv) { create :rdv, :future, users: [user], organisation: organisation, motif: motif, agents: [agent] }
     before { click_link user.full_name }
     it do
-      expect(page).to have_content("À venir\n1 RDVs")
+      expect(page).to have_content("À venir\n1 RDV")
       click_link "Voir tous les rendez-vous de #{user.full_name}"
       expect_page_title("Liste des RDV")
       expect(page).to have_content("le #{I18n.l(rdv.starts_at, format: :human)} (durée : #{rdv.duration_in_min} minutes)")
