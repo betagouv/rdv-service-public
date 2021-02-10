4beabb2df7971122bc7e55cd3c8c77afd598e96a
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2021-02-10 12:58:40 +0100

    better support floats in XLS zones import + migration to fix corrupt values (#1179)

--- app/lib/csv_or_xls_reader.rb
+++ app/lib/csv_or_xls_reader.rb
@@ -28,7 +28,17 @@ module CsvOrXlsReader
       book = Spreadsheet.open(@form_file.tempfile)
       worksheet = book.worksheets.first
       header_row = worksheet.row(0)
-      worksheet.each(1).map { header_row.zip(_1.map(&:to_s)).to_h }
+      worksheet.each(1).map do |row|
+        header_row.zip(row.map { cast_value(_1) }).to_h
+      end
+    end
+
+    def cast_value(cell_value)
+      # workaround https://github.com/zdavatz/spreadsheet/issues/41
+      # XLS does not store Floats properly
+      return cell_value.to_i.to_s if cell_value.is_a?(Float) && cell_value.to_s.ends_with?(".0")
+
+      cell_value.to_s
     end
   end
 end
