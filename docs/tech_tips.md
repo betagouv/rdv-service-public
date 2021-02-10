c6f3d4a8c3f774cc29da8cc8cf2298ad70931ec8
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-02-10 12:06:38 +0100

    rename command_utiles to tech_tips + introduce scalingo dump script

--- docs/tech_tips.md
+++ docs/tech_tips.md
@@ -1,9 +1,10 @@
-
 ## Restore production
 
 Pour tester les migrations avec les données de prod, il faut parfois récupérer un backup de la prod localement. Ça permet aussi de tester que nous arrivons bien à récupérer un backup valable de la production.
 
-Le plus simple est de passer par l'interface de Scalingo, dans l'extension PostGreSQL, télécharger le dernier backup puis lancer la commande suivante.
+L'option la plus simple est d'appeler le script `rails runner scripts/scalingo_dump.rb`. Il vous faudra au préalable rajouter une clé d'API Scalingo dans votre fichier `.env` local - c'est expliqué dans la source du script.
+
+Une autre option est de passer par l'interface de Scalingo, dans l'extension PostGreSQL, télécharger le dernier backup puis lancer la commande suivante.
 
 ```bash
 pg_restore -d rdv_solidarites_production_dump ~/Downloads/20210128000000_production__6670.pgsql
---
+++ scripts/scalingo_dump.rb
@@ -0,0 +1,72 @@
+# rails runner scripts/scalingo_dump.rb
+
+# initial setup:
+# head to https://my.osc-secnum-fr1.scalingo.com/profile and create a new token
+# store it in your .env like `SCALINGO_TOKEN=ab-enbxajkaxxxx`
+
+HEADERS = {
+  "Accept" => "application/json",
+  "Content-Type" => "application/json",
+}.freeze
+
+bearer_token = JSON.parse(
+  Typhoeus.post(
+    "https://auth.scalingo.com/v1/tokens/exchange",
+    headers: HEADERS,
+    userpwd: ":#{ENV['SCALINGO_API_TOKEN']}"
+  ).body
+)["token"]
+
+addons = JSON.parse(
+  Typhoeus.get(
+    "https://api.osc-secnum-fr1.scalingo.com/v1/apps/production-rdv-solidarites/addons",
+    headers: HEADERS.merge({ "Authorization" => "Bearer #{bearer_token}" })
+  ).body
+)["addons"]
+addon_db = addons.first { _1["name"] == "PostgreSQL" }
+puts addon_db
+
+bearer_token_db = JSON.parse(
+  Typhoeus.post(
+    "https://api.osc-secnum-fr1.scalingo.com/v1/apps/production-rdv-solidarites/addons/#{addon_db['id']}/token",
+    headers: HEADERS.merge({ "Authorization" => "Bearer #{bearer_token}" })
+  ).body
+)["addon"]["token"]
+puts bearer_token_db
+
+backups = JSON.parse(
+  Typhoeus.get(
+    "https://db-api.osc-secnum-fr1.scalingo.com/api/databases/#{addon_db['id']}/backups",
+    headers: HEADERS.merge({ "Authorization" => "Bearer #{bearer_token_db}" })
+  ).body
+)["database_backups"]
+backup_most_recent = backups.sort_by { _1["created_at"] }.last
+
+backup_dl_url = JSON.parse(
+  Typhoeus.get(
+    "https://db-api.osc-secnum-fr1.scalingo.com/api/databases/#{addon_db['id']}/backups/#{backup_most_recent['id']}/archive",
+    headers: HEADERS.merge({ "Authorization" => "Bearer #{bearer_token_db}" })
+  ).body
+)["download_url"]
+puts backup_dl_url
+
+puts "downloading #{(backup_most_recent['size'] / 1024 / 1024).round}MB backup from #{backup_most_recent['created_at']}..."
+backup_tar_path = "./prod-dump.tar.gz"
+File.open(backup_tar_path, "wb") do |file|
+  file.write(URI.open(backup_dl_url).read)
+end
+puts "done downloading to {backup_tar_path}, now untar ..."
+
+`tar -xvzf #{backup_tar_path}`
+puts "untar done!"
+`rm #{backup_tar_path}`
+
+backup_pgsql_filename = Dir.entries(".").select { _1.ends_with?(".pgsql") }.first
+backup_pgsql_path = "./#{backup_pgsql_filename}"
+
+`dropdb rdv_solidarites_production_dump`
+`createdb rdv_solidarites_production_dump`
+`pg_restore -d rdv_solidarites_production_dump #{backup_pgsql_path}`
+`rm #{backup_pgsql_path}`
+
+`sed -i.backup 's/lapin_development/rdv_solidarites_production_dump/' config/database.yml`
