d45519dd27812fa6d957bc1a97a8a8fc7db8604f
Adrien Di Pasquale <adrien@dipasquale.fr>
2021-03-09 15:06:28 +0100

    parallelize GH action specs run

---
+++ scripts/rspec_splitted.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "date"
+
+# Add some randomization. Different test order for every run.
+tests = Dir["spec/**/*_spec.rb"]
+  .shuffle(random: Random.new(ENV["GITHUB_RUN_ID"].to_i))
+  .select
+  .with_index do |_el, i|
+    i % ENV["NUMBER_OF_NODES"].to_i == ENV["CI_NODE_INDEX"].to_i
+  end
+
+exec "bundle exec rspec #{tests.join(' ')}"
