98f332f98bb9a43abef73ccb35f8151b8cd91d36
Quentin Blanc <qblanc@hotmail.com>
2021-03-19 17:51:22 +0100

    variable d'environnement pour cors gem

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins ENV["CORS_ALLOWED_ORIGINS"]
     resource "*", headers: :any, methods: [:get, :post, :patch, :put], expose: ["client", "uid", "access-token"]
   end
 end
