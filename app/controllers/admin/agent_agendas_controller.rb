8145cd4186b6d9d60adf98cae400a67ef7558774
Yannick Francois <yaf@elsif.fr>
2021-03-20 21:56:32 +0100

    supprime l'usage d'une variable inutile

--- app/controllers/admin/agent_agendas_controller.rb
+++ app/controllers/admin/agent_agendas_controller.rb
@@ -2,11 +2,10 @@ class Admin::AgentAgendasController < ApplicationController
   include Admin::AuthenticatedControllerConcern
 
   def show
-    current_organisation = Organisation.find(params[:organisation_id])
+    @organisation = Organisation.find(params[:organisation_id])
     @agent = policy_scope_admin(Agent).find(params[:id])
-    authorize_admin(AgentAgenda.new(agent: @agent, organisation: current_organisation))
+    authorize_admin(AgentAgenda.new(agent: @agent, organisation: @organisation))
     @status = params[:status]
-    @organisation = current_organisation
     @selected_event_id = params[:selected_event_id]
     @date = params[:date].present? ? Date.parse(params[:date]) : nil
   end
