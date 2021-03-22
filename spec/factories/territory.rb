79bfac70add5d3b6f94ac978a4b78b473be4ea88
Yannick Francois <yaf@elsif.fr>
2021-03-22 09:29:51 +0100

    Autorise jusqu'au département 999

--- spec/factories/territory.rb
+++ spec/factories/territory.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   sequence(:territory_name) { |n| "Territoire n°#{n}" }
-  sequence(:departement_number) { |n| ((10 + n) % 100).to_s }
+  sequence(:departement_number) { |n| ((10 + n) % 1_000).to_s }
 
   factory :territory do
     name { generate(:territory_name) }
