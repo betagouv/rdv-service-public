07d724e9cfa069d70c6799ddc27565f04d1684d5
Yannick Francois <yaf@elsif.fr>
2021-03-22 21:50:15 +0100

    supprime les gardes

--- app/service_functions/admin/occurrence.rb
+++ app/service_functions/admin/occurrence.rb
@@ -1,8 +1,5 @@
 module Admin::Occurrence
   def self.extract_from(elements, period)
-    return [] unless elements.is_a?(Array)
-    return [] unless elements.map { |e| e.respond_to?(:occurences_for) }.uniq == [true]
-
     elements.flat_map do |element|
       element.occurences_for(period).map { |occurrence| [element, occurrence] }
     end.sort_by(&:second)
