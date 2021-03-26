193c8d868388e231c30f103f673cb635de9a443b
Yannick Francois <yaf@elsif.fr>
2021-03-26 13:52:19 +0100

    remet à zéro les séquences à la fin de chaque test

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -67,4 +67,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+    FactoryBot.rewind_sequences
+  end
 end
