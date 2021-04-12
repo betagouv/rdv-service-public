1008e2a8f6d4b0ee85e7ab51cf6553eef950d781
Nicolas Bouilleaud <nico@bou.io>
2021-04-12 15:29:07 +0200

    Make rubocop happy

--- spec/form_models/admin/user_form_spec.rb
+++ spec/form_models/admin/user_form_spec.rb
@@ -1,20 +1,21 @@
 describe Admin::UserForm, type: :form do
+  subject { described_class.new(user, view_locals: { current_organisation: organisation, }) }
+
   let!(:organisation) { create(:organisation) }
 
-  subject { Admin::UserForm.new(user, view_locals: { current_organisation: organisation, }) }
   before do
-    expect(DuplicateUsersFinderService).to receive(:perform_with).with(user).and_return(duplicate_users_mock)
+    allow(DuplicateUsersFinderService).to receive(:perform_with).with(user).and_return(duplicate_users_mock)
   end
 
   context "no errors whatsoever" do
     let(:user) { build(:user, first_name: "Jean", last_name: "Jacques") }
     let(:duplicate_users_mock) { [] }
 
-    it "should be valid" do
+    it "is valid" do
       expect(subject.valid?).to eq true
     end
 
-    it "should save the user" do
+    it "saves the user" do
       expect(user).to receive(:save)
       subject.save
     end
@@ -24,13 +25,13 @@ describe Admin::UserForm, type: :form do
     let(:user) { build(:user, first_name: "Jean", last_name: nil) }
     let(:duplicate_users_mock) { [] }
 
-    it "should not be valid" do
+    it "is not valid" do
       expect(subject.valid?).to eq false
       expect(subject.errors).to be_present
       expect(subject.errors[:last_name]).to be_present
     end
 
-    it "should not save the user" do
+    it "does not save the user" do
       expect(user).not_to receive(:save)
       subject.save
     end
@@ -41,7 +42,7 @@ describe Admin::UserForm, type: :form do
     let!(:existing_user) { create(:user, first_name: "Jeannot", email: "jean@jacques.fr") }
     let(:duplicate_users_mock) { [OpenStruct.new(severity: :error, attributes: [:email], user: existing_user)] }
 
-    it "should not be valid" do
+    it "is not valid" do
       expect(subject.valid?).to eq false
       expect(subject.errors).to be_present
       expect(subject.errors[:base]).to be_present
@@ -49,7 +50,7 @@ describe Admin::UserForm, type: :form do
       expect(subject.errors[:base][0]).to include("Un usager avec le même email existe déjà")
     end
 
-    it "should not save the user" do
+    it "does not save the user" do
       expect(user).not_to receive(:save)
       subject.save
     end
@@ -60,7 +61,7 @@ describe Admin::UserForm, type: :form do
     let!(:existing_user) { create(:user, first_name: "Jeannot", phone_number: "0101010101") }
     let(:duplicate_users_mock) { [OpenStruct.new(severity: :warning, attributes: [:phone_number], user: existing_user)] }
 
-    it "should not be valid" do
+    it "is not valid" do
       expect(subject.valid?).to eq false
       expect(subject.warnings).to be_present
       expect(subject.warnings[:base]).to be_present
@@ -68,23 +69,24 @@ describe Admin::UserForm, type: :form do
       expect(subject.warnings[:base][0]).to include("Un usager avec le même numéro de téléphone existe déjà")
     end
 
-    it "should not save the user" do
+    it "does not save the user" do
       expect(user).not_to receive(:save)
       subject.save
     end
   end
 
   context "duplication warning bypassed" do
+    subject { described_class.new(user, active_warnings_confirm_decision: true, view_locals: { current_organisation: organisation, }) }
+
     let(:user) { build(:user, first_name: "Jean", last_name: "Jacques", phone_number: "0101010101") }
     let!(:existing_user) { create(:user, first_name: "Jeannot", phone_number: "0101010101") }
     let(:duplicate_users_mock) { [OpenStruct.new(severity: :warning, attributes: [:phone_number], user: existing_user)] }
-    subject { Admin::UserForm.new(user, active_warnings_confirm_decision: true, view_locals: { current_organisation: organisation, }) }
 
-    it "should be valid" do
+    it "is valid" do
       expect(subject.valid?).to eq true
     end
 
-    it "should save the user" do
+    it "saves the user" do
       expect(user).to receive(:save)
       subject.save
     end
@@ -99,7 +101,7 @@ describe Admin::UserForm, type: :form do
     let!(:existing_user) { create(:user, first_name: "Jeannot", phone_number: "0101010101") }
     let(:duplicate_users_mock) { [OpenStruct.new(severity: :warning, attributes: [:phone_number], user: existing_user)] }
 
-    it "should not be valid" do
+    it "is not valid" do
       expect(subject.valid?).to eq false
       expect(subject.warnings).to be_present
       expect(subject.warnings[:base]).to be_present
@@ -107,7 +109,7 @@ describe Admin::UserForm, type: :form do
       expect(subject.warnings[:base][0]).to include("Un usager avec le même numéro de téléphone existe déjà")
     end
 
-    it "should not save the user" do
+    it "does not save the user" do
       expect(user).not_to receive(:save)
       subject.save
     end
@@ -122,12 +124,12 @@ describe Admin::UserForm, type: :form do
     let!(:existing_user) { create(:user, first_name: "Jeannot", phone_number: "0101010101") }
     let(:duplicate_users_mock) { [OpenStruct.new(severity: :warning, attributes: [:phone_number], user: existing_user)] }
 
-    it "should be valid" do
+    it "is valid" do
       expect(subject.valid?).to eq true
       expect(subject.warnings).to be_empty
     end
 
-    it "should save the user" do
+    it "saves the user" do
       expect(user).to receive(:save)
       subject.save
     end
