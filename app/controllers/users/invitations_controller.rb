c17bf4e1736fc02c396541a09ac1bd9385d6d855
Nicolas Bouilleaud <nico@bou.io>
2021-04-19 11:12:21 +0200

    Prefill the phone_number in invitations from signup
    fixes #1328

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,3 +1,13 @@
 class Users::InvitationsController < Devise::InvitationsController
   layout "user_registration"
+
+  def edit
+    # Reuse prefilled params from the invitation email
+    resource.assign_attributes(prefilled_params)
+    super
+  end
+
+  def prefilled_params
+    params.permit(:first_name, :last_name, :phone_number)
+  end
 end
--- app/mailers/custom_devise_mailer.rb
+++ app/mailers/custom_devise_mailer.rb
@@ -7,6 +7,7 @@ class CustomDeviseMailer < Devise::Mailer
 
   def invitation_instructions(record, token, opts = {})
     @token = token
+    @prefill_params = opts[:prefill_params] || {}
     opts[:reply_to] = record.invited_by.email if record.is_a? Agent
     devise_mail(record, :invitation_instructions, opts)
   end
