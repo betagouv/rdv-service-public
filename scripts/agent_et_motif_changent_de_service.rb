18e79811f97e042a2c7ccf536f4a6d9c19b838cb
Yannick Francois <yaf@elsif.fr>
2021-04-21 22:20:47 +0200

    utilise des noms de fichiers en snake_case

--- scripts/agent_et_motif_changent_de_service.rb
+++ scripts/agent_et_motif_changent_de_service.rb
@@ -1,7 +1,7 @@
 # rails runner scripts/agent-et-motif-changent-de-service.rb
 
 ID_ORGANISATION = 1
-IDS_SERVICE_SOURCE = [1, 2]
+IDS_SERVICE_SOURCE = [1, 2].freeze
 ID_SERVICE_DESTINATION = 4
 
 services_source = Service.where(id: IDS_SERVICE_SOURCE)
@@ -9,22 +9,21 @@ service_destination = Service.find(ID_SERVICE_DESTINATION)
 organisation = Organisation.find(ID_ORGANISATION)
 
 puts "Hello !"
-puts "Déplace les agents et les motifs des services #{services_source.map(&:name).join(", ")} vers le service #{service_destination.name}"
+puts "Déplace les agents et les motifs des services #{services_source.map(&:name).join(', ')} vers le service #{service_destination.name}"
 
 Agent.transaction do
   Motif.where(organisation_id: ID_ORGANISATION, service: services_source).each do |motif|
     new_motif = motif.dup
     new_motif.organisation_id = ID_ORGANISATION
     new_motif.service_id = ID_SERVICE_DESTINATION
-    unless new_motif.save
-      puts "error ! on suppose que le nom existe déjà"
-      new_motif.name += "*"
-      new_motif.save!
-    end
+    next if new_motif.save
+
+    puts "error ! on suppose que le nom existe déjà"
+    new_motif.name += "*"
+    new_motif.save!
   end
 
   organisation.agents.each do |agent|
     agent.update_column(:service_id, ID_SERVICE_DESTINATION)
   end
-
 end
--- scripts/copie_vers_un_nouveau_service.rb
+++ scripts/copie_vers_un_nouveau_service.rb
@@ -1,13 +1,13 @@
 # rails runner scripts/copie-vers-un-nouveau-service.rb
 
-IDS_SERVICE_SOURCE = [1, 2]
+IDS_SERVICE_SOURCE = [1, 2].freeze
 ID_SERVICE_DESTINATION = 4
 
 services_source = Service.where(id: IDS_SERVICE_SOURCE)
 service_destination = Service.find(ID_SERVICE_DESTINATION)
 
 puts "Hello !"
-puts "Copie des libellées de motifs des services #{services_source.map(&:name).join(", ")} vers le service #{service_destination.name}"
+puts "Copie des libellées de motifs des services #{services_source.map(&:name).join(', ')} vers le service #{service_destination.name}"
 
 libelle_motifs_source = MotifLibelle.where(service_id: IDS_SERVICE_SOURCE)
 libelle_motifs_source.each do |source|
@@ -19,5 +19,3 @@ puts "#{libelle_motifs_source.count} libellés motif source"
 puts "#{MotifLibelle.where(service: service_destination).count} libellés motif créés"
 
 # NOTE : attention, il peut y avoir des doublons. Dans mon exemple de test, il y a RDV Collectif dans la PMI ET dans le Social. À l'arrivé on se retrouve avec un seul motif RDV Collectif
-
-
