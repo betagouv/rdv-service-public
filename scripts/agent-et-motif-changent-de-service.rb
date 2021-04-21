75dcaf6a628632a404806877fd6482b1721b8d64
Yannick Francois <yaf@elsif.fr>
2021-04-21 22:14:34 +0200

    utilise la bonne variable au bon endroit

--- scripts/agent-et-motif-changent-de-service.rb
+++ scripts/agent-et-motif-changent-de-service.rb
@@ -6,15 +6,15 @@ ID_SERVICE_DESTINATION = 4
 
 services_source = Service.where(id: IDS_SERVICE_SOURCE)
 service_destination = Service.find(ID_SERVICE_DESTINATION)
-organisation = Organsiation.find(ID_ORGANISATION)
+organisation = Organisation.find(ID_ORGANISATION)
 
 puts "Hello !"
 puts "Déplace les agents et les motifs des services #{services_source.map(&:name).join(", ")} vers le service #{service_destination.name}"
 
 Agent.transaction do
-  Motif.where(organisation_id: ORGANISATION_ID, service: services_source).each do |motif|
+  Motif.where(organisation_id: ID_ORGANISATION, service: services_source).each do |motif|
     new_motif = motif.dup
-    new_motif.organisation_id = ORGANISATION_ID
+    new_motif.organisation_id = ID_ORGANISATION
     new_motif.service_id = ID_SERVICE_DESTINATION
     unless new_motif.save
       puts "error ! on suppose que le nom existe déjà"
@@ -24,7 +24,7 @@ Agent.transaction do
   end
 
   organisation.agents.each do |agent|
-    agent.update_column(service: service_destination)
+    agent.update_column(:service_id, ID_SERVICE_DESTINATION)
   end
 
 end
