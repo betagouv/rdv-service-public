b22eea1947f165e3a9971214cb86eaf197d7a57d
Nicolas Bouilleaud <nico@bou.io>
2021-04-22 09:46:45 +0200

    Ajout de logs supplémentaire

--- scripts/agent_et_motif_changent_de_service.rb
+++ scripts/agent_et_motif_changent_de_service.rb
@@ -12,18 +12,23 @@ puts "Hello !"
 puts "Déplace les agents et les motifs des services #{services_source.map(&:name).join(', ')} vers le service #{service_destination.name}"
 
 Agent.transaction do
+  puts "Déplacement des motifs:"
   Motif.where(organisation_id: ID_ORGANISATION, service: services_source).each do |motif|
+    puts motif.name.to_s
     new_motif = motif.dup
     new_motif.organisation_id = ID_ORGANISATION
     new_motif.service_id = ID_SERVICE_DESTINATION
     next if new_motif.save
 
-    puts "error ! on suppose que le nom existe déjà"
+    puts "Erreur: #{new_motif.errors.full_messages.to_sentence}. Essayons en changeant le nom."
     new_motif.name += "*"
     new_motif.save!
   end
 
+  puts "Déplacement des agents:"
   organisation.agents.each do |agent|
+    puts agent.full_name.to_s
     agent.update_column(:service_id, ID_SERVICE_DESTINATION)
   end
+  puts "Terminé"
 end
