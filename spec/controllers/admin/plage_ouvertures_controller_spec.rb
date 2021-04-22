a682dce15fbe0a9d8a9e8c9ba09e1a69178a9058
Yannick Francois <yaf@elsif.fr>
2021-04-22 19:08:26 +0200

    mise à jour

--- spec/controllers/admin/plage_ouvertures_controller_spec.rb
+++ spec/controllers/admin/plage_ouvertures_controller_spec.rb
@@ -42,106 +42,6 @@ describe Admin::PlageOuverturesController, type: :controller do
       end
     end
 
-    describe "GET #index.json" do
-      subject(:response) do
-        get :index, params: { format: "json", organisation_id: organisation.id, agent_id: agent.id, start: start_date, end: end_date }
-      end
-
-      let(:parsed_response) { JSON.parse(response.body) }
-      let(:agent) { create(:agent, basic_role_in_organisations: [organisation]) }
-      let!(:plage_ouverture) do
-        create(:plage_ouverture, :every_two_weeks, title: "Une semaine sur deux les mercredis à partir du 17/07",
-                                                   first_day: Date.new(2019, 7, 17), lieu: lieu1, agent: agent, organisation: organisation, active_warnings_confirm_decision: true)
-      end
-      let!(:plage_ouverture2) do
-        create(:plage_ouverture, :weekly, title: "Tous les lundis à partir du 22/07",
-                                          first_day: Date.new(2019, 7, 22), lieu: lieu1, agent: agent, organisation: organisation, active_warnings_confirm_decision: true)
-      end
-      let!(:plage_ouverture3) do
-        create(:plage_ouverture, title: "Une seule fois le 24/07",
-                                 first_day: Date.new(2019, 7, 24), lieu: lieu1, agent: agent, organisation: organisation, active_warnings_confirm_decision: true)
-      end
-      let!(:plage_ouverture4) do
-        create(:plage_ouverture, title: "Une seule fois le 24/07",
-                                 first_day: Date.new(2019, 7, 24), lieu: lieu1, agent: agent, organisation: organisation, active_warnings_confirm_decision: true)
-      end
-
-      before do
-        sign_in agent
-      end
-
-      context "from 08/07/2019 to 14/07/2019" do
-        let(:start_date) { Date.new(2019, 7, 8) }
-        let(:end_date) { Date.new(2019, 7, 14) }
-
-        it { expect(response).to have_http_status(:ok) }
-        it { expect(response.body).to eq("[]") }
-      end
-
-      context "from 22/07/2019 to 28/07/2019" do
-        let(:start_date) { Date.new(2019, 7, 22) }
-        let(:end_date) { Date.new(2019, 7, 28) }
-
-        it "returns 3 occurrences from plage_ouverture2 3 and 4" do
-          expect(parsed_response.size).to eq(3)
-
-          first = parsed_response[0]
-          expect(first.size).to eq(6)
-          expect(first["title"]).to eq(plage_ouverture2.title)
-          expect(first["start"]).to eq(plage_ouverture2.starts_at.as_json)
-          expect(first["end"]).to eq(plage_ouverture2.first_occurrence_ends_at.as_json)
-          expect(first["backgroundColor"]).to eq("#6fceff80")
-          expect(first["rendering"]).to eq("background")
-          expect(first["extendedProps"]).to eq({ lieu: "MDS Sud", location: "10 rue Belsunce" }.as_json)
-
-          second = parsed_response[1]
-          expect(second.size).to eq(6)
-          expect(second["title"]).to eq(plage_ouverture3.title)
-          expect(second["start"]).to eq("2019-07-24T08:00:00.000+02:00")
-          expect(second["end"]).to eq("2019-07-24T12:00:00.000+02:00")
-          expect(second["backgroundColor"]).to eq("#6fceff80")
-          expect(second["rendering"]).to eq("background")
-          expect(second["extendedProps"]).to eq({ lieu: "MDS Sud", location: "10 rue Belsunce" }.as_json)
-
-          third = parsed_response[2]
-          expect(third.size).to eq(6)
-          expect(third["title"]).to eq(plage_ouverture4.title)
-          expect(third["start"]).to eq("2019-07-24T08:00:00.000+02:00")
-          expect(third["end"]).to eq("2019-07-24T12:00:00.000+02:00")
-          expect(third["backgroundColor"]).to eq("#6fceff80")
-          expect(third["rendering"]).to eq("background")
-          expect(third["extendedProps"]).to eq({ lieu: "MDS Sud", location: "10 rue Belsunce" }.as_json)
-        end
-      end
-
-      context "from 29/07/2019 to 04/08/2019" do
-        let(:start_date) { Date.new(2019, 7, 29) }
-        let(:end_date) { Date.new(2019, 8, 4) }
-
-        it "returns two occurrences one from plage_ouverture and one from plage_ouverture2" do
-          expect(parsed_response.size).to eq(2)
-
-          first = parsed_response[0]
-          expect(first.size).to eq(6)
-          expect(first["title"]).to eq(plage_ouverture2.title)
-          expect(first["start"]).to eq("2019-07-29T08:00:00.000+02:00")
-          expect(first["end"]).to eq("2019-07-29T12:00:00.000+02:00")
-          expect(first["backgroundColor"]).to eq("#6fceff80")
-          expect(first["rendering"]).to eq("background")
-          expect(first["extendedProps"]).to eq({ lieu: plage_ouverture2.lieu.name, location: plage_ouverture2.lieu.address }.as_json)
-
-          second = parsed_response[1]
-          expect(second.size).to eq(6)
-          expect(second["title"]).to eq(plage_ouverture.title)
-          expect(second["start"]).to eq("2019-07-31T08:00:00.000+02:00")
-          expect(second["end"]).to eq("2019-07-31T12:00:00.000+02:00")
-          expect(second["backgroundColor"]).to eq("#6fceff80")
-          expect(second["rendering"]).to eq("background")
-          expect(second["extendedProps"]).to eq({ lieu: plage_ouverture.lieu.name, location: plage_ouverture.lieu.address }.as_json)
-        end
-      end
-    end
-
     describe "GET #new" do
       it "returns a success response" do
         get :new, params: { organisation_id: organisation.id, agent_id: agent.id }
@@ -226,7 +126,7 @@ describe Admin::PlageOuverturesController, type: :controller do
                 lieu_id: lieu1.id,
                 organisation_id: organisation.id,
                 agent_id: agent.id
-                # missing fields
+              # missing fields
               }
             }
           )
