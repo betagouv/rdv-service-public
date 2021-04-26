921cc423a4194b9e5bb5cabd5e3b0cbfbd8d0fce
Yannick François <yannick.francois@beta.gouv.fr>
2021-04-26 19:52:39 +0200

    Prépare un script pour copier des agents et motifs d'un service à l´autre (#1364)
    * Create brakeman-analysis.yml
    
    * copie des libellés d'un service à l'autre
    
    * déplace les agents et les motifs de service
    
    * reporte les commentaires dans la description de PR
    
    * supprime un fichier intru
    
    * un seul niveau de transaction
    
    * Simplifie la lecture d'une requête
    
    * update_column met à jour sans validation
    
    * utilise la méthode `dup`
    
    * utilise la bonne variable au bon endroit
    
    * utilise des noms de fichiers en snake_case
    
    * Ajout d’un quatrième service à seeds pour pouvoir tester en local
    
    * Ajout de logs supplémentaire
    
    * place les bons ID pour la migration de Colombes
    
    * précise les services pour la copie des libellés
    
    Co-authored-by: Nicolas Bouilleaud <nico@bou.io>

---
+++ scripts/agent_et_motif_changent_de_service.rb
@@ -0,0 +1,34 @@
+# rails runner scripts/agent-et-motif-changent-de-service.rb
+
+ID_ORGANISATION = 162
+IDS_SERVICE_SOURCE = [5, 7].freeze
+ID_SERVICE_DESTINATION = 22
+
+services_source = Service.where(id: IDS_SERVICE_SOURCE)
+service_destination = Service.find(ID_SERVICE_DESTINATION)
+organisation = Organisation.find(ID_ORGANISATION)
+
+puts "Hello !"
+puts "Déplace les agents et les motifs des services #{services_source.map(&:name).join(', ')} vers le service #{service_destination.name}"
+
+Agent.transaction do
+  puts "Déplacement des motifs:"
+  Motif.where(organisation_id: ID_ORGANISATION, service: services_source).each do |motif|
+    puts motif.name.to_s
+    new_motif = motif.dup
+    new_motif.organisation_id = ID_ORGANISATION
+    new_motif.service_id = ID_SERVICE_DESTINATION
+    next if new_motif.save
+
+    puts "Erreur: #{new_motif.errors.full_messages.to_sentence}. Essayons en changeant le nom."
+    new_motif.name += "*"
+    new_motif.save!
+  end
+
+  puts "Déplacement des agents:"
+  organisation.agents.each do |agent|
+    puts agent.full_name.to_s
+    agent.update_column(:service_id, ID_SERVICE_DESTINATION)
+  end
+  puts "Terminé"
+end
---
+++ scripts/copie_vers_un_nouveau_service.rb
@@ -0,0 +1,21 @@
+# rails runner scripts/copie-vers-un-nouveau-service.rb
+
+IDS_SERVICE_SOURCE = [5, 7].freeze
+ID_SERVICE_DESTINATION = 22
+
+services_source = Service.where(id: IDS_SERVICE_SOURCE)
+service_destination = Service.find(ID_SERVICE_DESTINATION)
+
+puts "Hello !"
+puts "Copie des libellées de motifs des services #{services_source.map(&:name).join(', ')} vers le service #{service_destination.name}"
+
+libelle_motifs_source = MotifLibelle.where(service_id: IDS_SERVICE_SOURCE)
+libelle_motifs_source.each do |source|
+  puts "copie du libelle #{source.name}"
+  MotifLibelle.find_or_create_by!(name: source.name, service: service_destination)
+end
+
+puts "#{libelle_motifs_source.count} libellés motif source"
+puts "#{MotifLibelle.where(service: service_destination).count} libellés motif créés"
+
+# NOTE : attention, il peut y avoir des doublons. Dans mon exemple de test, il y a RDV Collectif dans la PMI ET dans le Social. À l'arrivé on se retrouve avec un seul motif RDV Collectif
