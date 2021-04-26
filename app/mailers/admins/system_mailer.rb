2e893b424a11727e7660f780e5ae161820b3ba0e
Nicolas Bouilleaud <nico@bou.io>
2021-04-26 16:18:45 +0200

    Improvements to the RdvEvent system mailer

--- app/mailers/admins/system_mailer.rb
+++ app/mailers/admins/system_mailer.rb
@@ -1,7 +1,8 @@
 class Admins::SystemMailer < ApplicationMailer
   def rdv_events_stats
-    @today_stats = RdvEvent.date_stats(Date.today)
-    @yesterday_stats = RdvEvent.date_stats(Date.yesterday)
-    mail(to: "contact@rdv-solidarites.fr", subject: "[monitoring] Notifications Stats")
+    @today_stats = RdvEvent.date_stats(Time.zone.today)
+    @yesterday_stats = RdvEvent.date_stats(Time.zone.today - 1.day)
+    title = I18n.t("admins.system_mailer.rdv_events_stats.title", date: I18n.l(Time.zone.today))
+    mail(to: "contact@rdv-solidarites.fr", subject: "[monitoring] #{title}")
   end
 end
