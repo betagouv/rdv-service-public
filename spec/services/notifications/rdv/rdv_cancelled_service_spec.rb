8aef6854fac09921cd81996800614165d5013b69
Yannick François <yannick.francois@beta.gouv.fr>
2021-04-30 10:05:36 +0200

    fige le temps utilisé dans les tests (#1393)
    * déploie en automatique depuis scalingo
    
    * fige le temps utilisé dans les tests
    
    * linter

--- spec/services/notifications/rdv/rdv_cancelled_service_spec.rb
+++ spec/services/notifications/rdv/rdv_cancelled_service_spec.rb
@@ -1,34 +1,39 @@
 describe Notifications::Rdv::RdvCancelledService, type: :service do
-  subject { described_class.perform_with(rdv) }
-
-  let(:user1) { build(:user) }
-  let(:agent1) { build(:agent, first_name: "Sean", last_name: "PAUL") }
-  let(:agent2) { build(:agent) }
-  let(:starts_at_initial) { 2.hours.from_now }
-  let!(:rdv) { create_rdv_skip_notify(starts_at: starts_at_initial, users: [user1], agents: [agent1, agent2]) }
-
-  before do
-    PaperTrail.request.whodunnit = "[Agent] Sean PAUL"
-    update_rdv_skip_notify!(rdv, status: :excused)
-  end
-
   context "starts in more than 2 days" do
-    let(:starts_at_initial) { 3.days.from_now }
-
     it "does not triggers sending mail to agents" do
+      starts_at_initial = Time.zone.parse("2021-04-26 8h15")
+      travel_to(starts_at_initial)
+
+      agent1 = build(:agent, first_name: "Sean", last_name: "PAUL")
+      agent2 = build(:agent)
+      rdv =  create_rdv_skip_notify(starts_at: starts_at_initial + 3.days, agents: [agent1, agent2])
+
+      PaperTrail.request.whodunnit = "[Agent] Sean PAUL"
+      update_rdv_skip_notify!(rdv, status: :excused)
+
       expect(Agents::RdvMailer).not_to receive(:rdv_starting_soon_cancelled)
-      subject
+      described_class.perform_with(rdv)
     end
   end
 
   context "starts today or tomorrow" do
     it "triggers sending mails to the agents (except the one who initiated the change)" do
+      starts_at_initial = Time.zone.parse("2021-04-29 8h15")
+      travel_to(starts_at_initial)
+
+      agent1 = build(:agent, first_name: "Sean", last_name: "PAUL")
+      agent2 = build(:agent)
+      rdv =  create_rdv_skip_notify(starts_at: starts_at_initial + 1.day, agents: [agent1, agent2])
+
+      PaperTrail.request.whodunnit = "[Agent] Sean PAUL"
+      update_rdv_skip_notify!(rdv, status: :excused)
+
       expect(Agents::RdvMailer).not_to receive(:rdv_starting_soon_cancelled)
         .with(rdv, agent1, "[Agent] Sean PAUL", starts_at_initial)
       expect(Agents::RdvMailer).to receive(:rdv_starting_soon_cancelled)
         .with(rdv, agent2, "[Agent] Sean PAUL")
         .and_return(double(deliver_later: nil))
-      subject
+      described_class.perform_with(rdv)
     end
   end
 end
