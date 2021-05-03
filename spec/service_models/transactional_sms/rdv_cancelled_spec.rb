4b531586b71990a53547d0a06be2a57702d377ce
Yannick Francois <yaf@elsif.fr>
2021-05-03 19:17:16 +0200

    utilise le numéro de lieu si disponible

--- spec/service_models/transactional_sms/rdv_cancelled_spec.rb
+++ spec/service_models/transactional_sms/rdv_cancelled_spec.rb
@@ -1,13 +1,42 @@
 describe TransactionalSms::RdvCancelled, type: :service do
+  let(:user) { build(:user) }
   let(:pmi) { build(:service, short_name: "PMI") }
   let(:motif) { build(:motif, service: pmi) }
-  let(:rdv) { build(:rdv, motif: motif, starts_at: Time.zone.local(2021, 12, 10, 13, 10)) }
-  let(:user) { build(:user) }
 
   describe "#content" do
-    subject { described_class.new(rdv, user).content }
+    context "with lieu phone number" do
+      it "contains cancelled RDV's infos and lieu's phone number" do
+        lieu = build(:lieu, phone_number: "0123456789")
+        organisation = build(:organisation, phone_number: "9876543210")
+        rdv = build(:rdv, motif: motif, organisation: organisation, lieu: lieu, starts_at: Time.zone.local(2021, 12, 10, 13, 10))
+        expected_content = "RDV PMI vendredi 10/12 à 13h10 a été annulé\n"
+        expected_content += "Appelez le 0123456789 "
+        expected_content += "ou allez sur https://rdv-solidarites.fr pour reprendre RDV."
+        expect(described_class.new(rdv, user).content).to eq(expected_content)
+      end
+    end
 
-    it { is_expected.to include("RDV PMI vendredi 10/12 à 13h10 a été annulé") }
-    it { is_expected.to include("Allez sur https://rdv-solidarites.fr pour reprendre RDV") }
+    context "with only organisation number" do
+      it "contains cancelled RDV's infos" do
+        lieu = build(:lieu, phone_number: nil)
+        organisation = build(:organisation, phone_number: "9876543210")
+        rdv = build(:rdv, motif: motif, organisation: organisation, lieu: lieu, starts_at: Time.zone.local(2021, 12, 10, 13, 10))
+        expected_content = "RDV PMI vendredi 10/12 à 13h10 a été annulé\n"
+        expected_content += "Appelez le 9876543210 "
+        expected_content += "ou allez sur https://rdv-solidarites.fr pour reprendre RDV."
+        expect(described_class.new(rdv, user).content).to eq(expected_content)
+      end
+    end
+
+    context "with no phone number" do
+      it "contains cancelled RDV's infos" do
+        lieu = build(:lieu, phone_number: nil)
+        organisation = build(:organisation, phone_number: nil)
+        rdv = build(:rdv, motif: motif, organisation: organisation, lieu: lieu, starts_at: Time.zone.local(2021, 12, 10, 13, 10))
+        expected_content = "RDV PMI vendredi 10/12 à 13h10 a été annulé\n"
+        expected_content += "Allez sur https://rdv-solidarites.fr pour reprendre RDV."
+        expect(described_class.new(rdv, user).content).to eq(expected_content)
+      end
+    end
   end
 end
