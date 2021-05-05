2ca30e728170d41c7f269a3e50f43c70bbc8e0d7
Nicolas Bouilleaud <nico@bou.io>
2021-05-05 14:48:02 +0200

    Fix devise_error_messages! html joins

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -2,7 +2,7 @@ module DeviseHelper
   def devise_error_messages!
     return "" if resource.errors.empty?
 
-    messages = resource.errors.full_messages.map { |msg| tag.li(msg) }.join
+    messages = safe_join resource.errors.full_messages.map { |msg| tag.li(msg) }
     tag.div(class: "alert alert-danger alert-block devise-bs") do
       tag.button(type: "button", class: "close", data: { dismiss: "alert" }) do
         "&times;"
