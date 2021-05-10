b758d1a881ad591327e8eeafe7083f1aea436a0b
Yannick Francois <yaf@elsif.fr>
2021-05-10 19:22:52 +0200

    ajoute des validations de configuration sms

--- app/models/territory.rb
+++ app/models/territory.rb
@@ -8,6 +8,8 @@ class Territory < ApplicationRecord
   validates :departement_number, length: { maximum: 3 }, if: -> { departement_number.present? }
   validates :name, presence: true, if: -> { persisted? }
   validates :departement_number, uniqueness: true, allow_blank: true
+  validates :sms_provider, presence: true
+  validate :sms_configuration_match_provider
 
   scope :with_agent, lambda { |agent|
     joins(:roles).where(agent_territorial_roles: { agent_id: agent.id })
@@ -17,6 +19,11 @@ class Territory < ApplicationRecord
   }
 
   enum sms_provider: { netsize: "netsize", send_in_blue: "send_in_blue" }, _prefix: true
+
+  FIELDS_FOR_SMS_CONFIGURATION = {
+    send_in_blue: ["api_key"],
+    netsize: %w[api_url user_pwd]
+  }.freeze
 
   before_create :fill_name_for_departements
 
@@ -30,5 +37,16 @@ class Territory < ApplicationRecord
     return if name.present? || departement_number.blank?
 
     self.name = Departements::NAMES[departement_number]
+  end
+
+  def sms_configuration_match_provider
+    configuration_keys = sms_configuration.keys || []
+    missing_keys = FIELDS_FOR_SMS_CONFIGURATION[sms_provider.to_sym] - configuration_keys
+
+    if missing_keys.any?
+      errors.add(:sms_configuration, "doit contenir les valeurs pour #{missing_keys}")
+    else
+      true
+    end
   end
 end
