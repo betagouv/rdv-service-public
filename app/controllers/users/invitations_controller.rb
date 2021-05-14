c3f31f7f8beed64db631422c7f9f81c450de7b69
Quentin Blanc <qblanc@hotmail.com>
2021-05-14 15:56:41 +0200

    ajout d'un simple_invitation_token et obligation pour l'utilisateur sans email d'en renseigner un pour pouvoir créer son compte

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Users::InvitationsController < Devise::InvitationsController
+  skip_before_action :authenticate_inviter!, only: [:new, :redirect]
   layout "user_registration"
 
   def edit
@@ -9,15 +10,15 @@ class Users::InvitationsController < Devise::InvitationsController
     super
   end
 
-  def new
-  end
+  def new ; end
 
   def redirect
-    user = User.where(caisse_affiliation: 1).find_by(affiliation_number: params[:affiliation_number])
-    if user.raw_invitation_token == params[:invitation_token]
+    user = User.where(caisse_affiliation: "caf").find_by(affiliation_number: params[:invite][:affiliation_number])
+    if user.simple_invitation_token == params[:invite][:simple_invitation_token]
+      user.invite! { |u| u.skip_invitation = true }
       redirect_to accept_user_invitation_url(invitation_token: user.raw_invitation_token)
     else
-      redirect_to :invitations_landing, flash: { error: "Les informations renseignées ne correspondent pas à un compte existant."}
+      redirect_to :invitations_landing, flash: { error: "Les informations renseignées ne correspondent pas à un compte existant." }
     end
   end
 end
