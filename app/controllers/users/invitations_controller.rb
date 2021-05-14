471eaae67df5e1e1f8457f5dab2fae2deed2bc56
Quentin Blanc <qblanc@hotmail.com>
2021-05-14 19:26:26 +0200

    another linter fix

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::InvitationsController < Devise::InvitationsController
-  skip_before_action :authenticate_inviter!, only: %i[new, redirect]
+  skip_before_action :authenticate_inviter!, only: %i[new redirect]
   layout "user_registration"
 
   def edit
