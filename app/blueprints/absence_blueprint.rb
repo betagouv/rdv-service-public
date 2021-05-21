19d39b0fd98fe8605e62b359e58deca3b2a59632
Yannick Francois <yaf@elsif.fr>
2021-05-22 01:48:35 +0200

    partage la recurrence

--- app/blueprints/absence_blueprint.rb
+++ app/blueprints/absence_blueprint.rb
@@ -6,4 +6,12 @@ class AbsenceBlueprint < Blueprinter::Base
   fields :ical_uid, :title, :first_day, :end_day, :start_time, :end_time
   association :agent, blueprint: AgentBlueprint
   association :organisation, blueprint: OrganisationBlueprint
+
+  field :rrule do |absence|
+    Admin::Ics::Absence.rrule(absence)
+  end
+
+  field :ical do |absence|
+    Admin::Ics::Absence.to_ical(Admin::Ics::Absence.payload(absence))
+  end
 end
