48e7bc3289511c58817adab4422811ccebc728e8
Yannick Francois <yaf@elsif.fr>
2021-05-28 10:45:11 +0200

    simplifie l'usage de la configuration sms

--- app/controllers/admin/territories/sms_configurations_controller.rb
+++ app/controllers/admin/territories/sms_configurations_controller.rb
@@ -16,8 +16,8 @@ class Admin::Territories::SmsConfigurationsController < Admin::Territories::Base
 
   def check_allowed_departement
     if current_departement_not_allowed_to_configure_sms?
-      redirect_to action: :show,
-                  alert: "Vous ne pouvez pas modifier la configuration d'envoie de SMS tant que votre département n'a pas de marché distinct"
+      flash[:alert] = "Vous ne pouvez pas modifier la configuration d'envoie de SMS tant que votre département n'a pas de marché distinct"
+      redirect_to action: :show
     end
   end
 
@@ -27,11 +27,9 @@ class Admin::Territories::SmsConfigurationsController < Admin::Territories::Base
 
   def sms_configuration_params
     sms_configuration = {}
-    Territory::BASE_SMS_CONFIGURATION.each do |provider, config_fields|
-      sms_configuration[provider] = {}
-      config_fields.each do |config_field|
-        sms_configuration[provider][config_field] = params[provider][config_field] if params[provider].present? && params[provider][config_field].present?
-      end
+    provider = params[:territory][:sms_provider]
+    Territory::FIELDS_FOR_SMS_CONFIGURATION[provider.to_sym].each do |config_field|
+      sms_configuration[config_field] = params[provider][config_field] if params[provider].present? && params[provider][config_field].present?
     end
 
     params.require(:territory).permit(:sms_provider).merge(sms_configuration: sms_configuration.compact)
