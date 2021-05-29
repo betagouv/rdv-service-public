cce7d540f0d8bd0d961298c5c80eba3900ee1bb9
Yannick Francois <yaf@elsif.fr>
2021-05-29 21:41:48 +0200

    corrige les spec

--- spec/features/agents/admin_can_see_the_organisation_stats_spec.rb
+++ spec/features/agents/admin_can_see_the_organisation_stats_spec.rb
@@ -6,7 +6,7 @@ describe "Admin can configure the organisation" do
     agent_admin = create(:agent, admin_role_in_organisations: [organisation])
     login_as(agent_admin, scope: :agent)
     visit authenticated_agent_root_path
-    click_link "Statistiques de l'organisation"
+    click_link "Statistiques"
     expect(page).to have_content("Statistiques")
     expect(page).to have_content("RDV créés")
     expect(page).to have_content("Usagers créés")
