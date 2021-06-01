59145c39bd327d3ba10ce1156273ead19fdaeba8
Yannick Francois <yaf@elsif.fr>
2021-06-01 19:52:33 +0200

    supprime un commentaire inutile

--- app/webpacker/components/show-sms-provider-configuration-form.js
+++ app/webpacker/components/show-sms-provider-configuration-form.js
@@ -1,7 +1,5 @@
 class ShowSmsProviderConfigurationForm {
   constructor() {
-    // have to use jQuery here because of select2
-    //$(".js-merge-users-user-select").on("change", this.userSelected)
     $("#territory_sms_provider").on("change", this.providerSelect)
     $(`.js-${$("#territory_sms_provider").val()}`).removeClass("d-none")
   }
