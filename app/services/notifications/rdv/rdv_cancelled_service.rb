c0d53d481c9f35e865ead868740ca30c14427158
Nicolas Bouilleaud <nico@bou.io>
2021-06-03 09:57:51 +0200

    Merge all the Notification::RdvCancelled services

--- app/services/notifications/rdv/rdv_cancelled_service.rb
+++ app/services/notifications/rdv/rdv_cancelled_service.rb
@@ -8,4 +8,23 @@ class Notifications::Rdv::RdvCancelledService < ::BaseService
   def notify_agent(agent)
     Agents::RdvMailer.rdv_cancelled(@rdv, agent, @author).deliver_later
   end
+
+  def notify_user_by_mail(user)
+    # Only send sms for excused cancellations (not for no-show)
+    return unless @rdv.status == "excused"
+
+    Users::RdvMailer.rdv_cancelled(@rdv, user, @author).deliver_later
+
+    event_name = @author.is_a?(Agent) ? :cancelled_by_agent : :cancelled_by_user
+    @rdv.events.create!(event_type: RdvEvent::TYPE_NOTIFICATION_MAIL, event_name: event_name)
+  end
+
+  def notify_user_by_sms(user)
+    # Only send sms for excused cancellations by an Agent (not for no-show, not for self-cancellation)
+    return unless @author.is_a? Agent
+    return unless @rdv.status == "excused"
+
+    SendTransactionalSmsJob.perform_later(:rdv_cancelled, @rdv.id, user.id)
+    @rdv.events.create!(event_type: RdvEvent::TYPE_NOTIFICATION_SMS, event_name: :cancelled_by_agent)
+  end
 end
