141ee8bcefcf8906e2eb808781df7e248016a477
Quentin Blanc <qblanc@hotmail.com>
2021-06-08 18:02:14 +0200

    rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_103647) do
+ActiveRecord::Schema.define(version: 2021_06_07_154248) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -217,6 +217,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_103647) do
     t.boolean "follow_up", default: false
     t.string "visibility_type", default: "visible_and_notified", null: false
     t.string "sectorisation_level", default: "departement"
+    t.text "cancel_warning_message"
     t.index ["deleted_at"], name: "index_motifs_on_deleted_at"
     t.index ["name", "organisation_id", "location_type", "service_id"], name: "index_motifs_on_name_scoped", unique: true, where: "(deleted_at IS NULL)"
     t.index ["organisation_id"], name: "index_motifs_on_organisation_id"
