e9dd5df52c320af04f482073e6fc6c58b4af35a2
Nicolas Bouilleaud <nico@bou.io>
2021-06-17 10:26:04 +0200

    Ajoute une bannière “nom de l’instance” à part pour la production
    fixes #1058
    
    Ajoute une bannière à:
    * l’app agent
    * l’app user
    * l’app superadmin
    * les emails transactionnels
    * les emails Devise
    * les sms transactionnels
    
    Utilise la variable d’environnement RDV_SOLIDARITES_INSTANCE_NAME; en environnement de développement, indique “development”.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -6,4 +6,5 @@ class ApplicationMailer < ActionMailer::Base
   default from: "contact@rdv-solidarites.fr"
   append_view_path Rails.root.join("app/views/mailers")
   layout "mailer"
+  add_template_helper RdvSolidaritesInstanceNameHelper
 end
