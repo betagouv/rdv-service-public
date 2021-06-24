de86dd35c1d04336cddff017b5d6cf4f54b5a1d8
Yannick Francois <yaf@elsif.fr>
2021-06-24 14:12:36 +0200

    corrige les features

--- spec/features/agents/agent_can_see_users_rdv_spec.rb
+++ spec/features/agents/agent_can_see_users_rdv_spec.rb
@@ -4,7 +4,7 @@ describe "can see users' RDV" do
   let!(:organisation) { create(:organisation) }
   let!(:service) { create(:service) }
   let!(:agent) { create(:agent, basic_role_in_organisations: [organisation], service: service) }
-  let!(:user) { create(:user, organisations: [organisation]) }
+  let!(:user) { create(:user, first_name: "Tanguy", last_name: "Laverdure", organisations: [organisation]) }
 
   before do
     login_as(agent, scope: :agent)
@@ -13,7 +13,7 @@ describe "can see users' RDV" do
   end
 
   context "with no RDV" do
-    before { click_link user.full_name }
+    before { click_link "LAVERDURE Tanguy" }
 
     it do
       expect(page).to have_content("À venir\n0 RDV")
@@ -25,11 +25,11 @@ describe "can see users' RDV" do
     let!(:motif) { create(:motif, organisation: organisation, service: service) }
     let!(:rdv) { create :rdv, :future, users: [user], organisation: organisation, motif: motif, agents: [agent] }
 
-    before { click_link user.full_name }
+    before { click_link "LAVERDURE Tanguy" }
 
     it do
       expect(page).to have_content("À venir\n1 RDV")
-      click_link "Voir tous les rendez-vous de #{user.full_name}"
+      click_link "Voir tous les rendez-vous de Tanguy LAVERDURE"
       expect_page_title("Liste des RDV")
       expect(page).to have_content("le #{I18n.l(rdv.starts_at, format: :human)} (durée : #{rdv.duration_in_min} minutes)")
     end
