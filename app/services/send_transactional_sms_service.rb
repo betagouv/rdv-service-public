fcc901679ef64b891a8f2a30665bd352c1119ea8
Nicolas Bouilleaud <nico@bou.io>
2021-07-08 15:25:08 +0200

    Improve SMS error management and cleanup

--- app/services/send_transactional_sms_service.rb
+++ app/services/send_transactional_sms_service.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-class NetsizeTimeout < StandardError; end
-
-class NetsizeHttpError < StandardError; end
-
-class NetsizeApiError < StandardError; end
-
 class SendTransactionalSmsService < BaseService
+  class Timeout < StandardError; end
+
+  class HttpError < StandardError; end
+
+  class ApiError < StandardError; end
+
   SENDER_NAME = "RdvSoli"
 
   def initialize(phone_number, content, tags, provider = nil, configuration = nil)
@@ -40,51 +40,50 @@ class SendTransactionalSmsService < BaseService
     Rails.logger.info("SMS DebugLogger: this would have been sent: #{self}")
   end
 
+  # SendInBlue
+  #
   def send_with_send_in_blue
     config = SibApiV3Sdk::Configuration.new
     config.api_key["api-key"] = @configuration["api_key"]
     api_client = SibApiV3Sdk::ApiClient.new(config)
-    SibApiV3Sdk::TransactionalSMSApi.new(api_client).send_transac_sms(
-      SibApiV3Sdk::SendTransacSms.new(
-        sender: SENDER_NAME,
-        recipient: @phone_number_formatted,
-        content: @content,
-        tag: @tags.join(" ")
+    begin
+      SibApiV3Sdk::TransactionalSMSApi.new(api_client).send_transac_sms(
+        SibApiV3Sdk::SendTransacSms.new(
+          sender: SENDER_NAME,
+          recipient: @phone_number,
+          content: @content,
+          tag: @tags.join(" ")
+        )
       )
-    )
+    rescue SibApiV3Sdk::ApiError => e
+      raise ApiError, { message: self, response: "#{e.code} #{e.response_body}" }
+    end
   end
 
+  # NetSize
+  #
   def send_with_netsize
-    request = Typhoeus::Request.new(
-      @configuration["api_url"],
+    base_url = @configuration["api_url"].presence || "https://europe.ipx.com/restapi/v1/sms/send"
+    response = Typhoeus::Request.new(
+      base_url,
       method: :post,
       userpwd: @configuration["api_key"],
       headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" },
       timeout: 5,
       body: {
-        destinationAddress: @phone_number_formatted,
+        destinationAddress: @phone_number,
         messageText: @content,
         originatingAddress: SENDER_NAME,
         originatorTON: 1,
         campaignName: @tags.join(" ").truncate(49),
         maxConcatenatedMessages: 10
       }
-    )
-    request.on_complete { netsize_on_complete(_1) }
-    request.run
-  end
+    ).run
 
-  def netsize_on_complete(response)
-    if response.success?
-      parsed_res = JSON.parse(response.body)
-      return if parsed_res["responseCode"].zero?
+    raise Timeout if response.timed_out?
+    raise HttpError, { message: self, response: "code: #{response.code}" } if response.failure?
 
-      ::Sentry.set_extras(parsed_res)
-      raise NetsizeApiError, "HTTP 200, responseCode: #{parsed_res['responseCode']}, #{parsed_res['responseMessage']}"
-    end
-
-    raise NetsizeTimeout if response.timed_out?
-
-    raise NetsizeHttpError, "code: #{response.code}, message: #{response.return_message}"
+    parsed_res = JSON.parse(response.body)
+    raise ApiError, { message: self, response: parsed_res } unless parsed_res["responseCode"].zero?
   end
 end
