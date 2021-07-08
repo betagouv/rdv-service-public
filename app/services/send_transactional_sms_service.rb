3fe313e42aa3001e293bce505a8e3ade9dc5437a
Nicolas Bouilleaud <nico@bou.io>
2021-07-08 15:25:08 +0200

    Make SMS Service independant from RDVs or Territories or anything
    Just pass it the params and let it do its job.
    
    followup #1570

--- app/services/send_transactional_sms_service.rb
+++ app/services/send_transactional_sms_service.rb
@@ -7,24 +7,19 @@ class NetsizeHttpError < StandardError; end
 class NetsizeApiError < StandardError; end
 
 class SendTransactionalSmsService < BaseService
-  attr_reader :transactional_sms
-
-  DEFAULT_SMS_PROVIDER = "netsize"
-  DEFAULT_SMS_CONFIGURATION = {
-    "api_url" => ENV["NETSIZE_API_USERPWD"],
-    "user_pwd" => "https://europe.ipx.com/restapi/v1/sms/send"
-  }.freeze
-
   SENDER_NAME = "RdvSoli"
 
-  def initialize(transactional_sms)
-    @transactional_sms = transactional_sms
-    territory = Territory.find(@transactional_sms.rdv.organisation_territory_id)
-    @configuration = territory.sms_configuration || DEFAULT_SMS_CONFIGURATION
+  def initialize(phone_number, content, tags, provider = nil, configuration = nil)
+    @phone_number = phone_number
+    @content = content
+    @tags = tags
 
-    @provider = :debug_logger
-    @provider = territory.sms_provider || DEFAULT_SMS_PROVIDER if Rails.env.production?
-    @provider = ENV["FORCE_SMS_PROVIDER"].to_sym if ENV["FORCE_SMS_PROVIDER"].present?
+    @provider = ENV["DEVELOPMENT_FORCE_SMS_PROVIDER"].presence || provider || ENV["DEFAULT_SMS_PROVIDER"].presence || :debug_logger
+    default_configuration = {
+      "api_url" => ENV["DEFAULT_SMS_PROVIDER_API_URL"],
+      "api_key" => ENV["DEFAULT_SMS_PROVIDER_KEY"]
+    }
+    @configuration = configuration || default_configuration
   end
 
   def perform
@@ -40,9 +35,9 @@ class SendTransactionalSmsService < BaseService
     SibApiV3Sdk::TransactionalSMSApi.new(api_client).send_transac_sms(
       SibApiV3Sdk::SendTransacSms.new(
         sender: SENDER_NAME,
-        recipient: transactional_sms.phone_number_formatted,
-        content: transactional_sms.content,
-        tag: transactional_sms.tags.join(" ")
+        recipient: @phone_number_formatted,
+        content: @content,
+        tag: @tags.join(" ")
       )
     )
   end
@@ -51,15 +46,15 @@ class SendTransactionalSmsService < BaseService
     request = Typhoeus::Request.new(
       @configuration["api_url"],
       method: :post,
-      userpwd: @configuration["user_pwd"],
+      userpwd: @configuration["api_key"],
       headers: { "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8" },
       timeout: 5,
       body: {
-        destinationAddress: transactional_sms.phone_number_formatted,
-        messageText: transactional_sms.content,
+        destinationAddress: @phone_number_formatted,
+        messageText: @content,
         originatingAddress: SENDER_NAME,
         originatorTON: 1,
-        campaignName: transactional_sms.tags.join(" ").truncate(49),
+        campaignName: @tags.join(" ").truncate(49),
         maxConcatenatedMessages: 10
       }
     )
