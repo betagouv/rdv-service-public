b050d55dae60ed67096486100d83040e9627fc04
Amine Dhobb <amine.dhobb@gmail.com>
2021-07-19 14:17:12 +0200

    feat(api): Add options when retrieving motifs (#1585)
    Co-authored-by: Yannick François <yannick.francois@beta.gouv.fr>

--- app/controllers/api/v1/motifs_controller.rb
+++ app/controllers/api/v1/motifs_controller.rb
@@ -3,6 +3,8 @@
 class Api::V1::MotifsController < Api::V1::BaseController
   def index
     motifs = policy_scope(Motif)
+    motifs = motifs.active if params[:active] == "true"
+    motifs = motifs.reservable_online if params[:reservable_online] == "true"
     render_collection(motifs.order(:id))
   end
 end
