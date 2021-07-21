8d2cba822cd3dd3f0756e1dae2c5705bd227d01a
Yannick Francois <yaf@elsif.fr>
2021-07-22 00:34:23 +0200

    ajoute l'enum sfrMail2SMS

--- app/mailers/admins/sfr_mail_2_sms.rb
+++ app/mailers/admins/sfr_mail_2_sms.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SfrMail2SmsMailer < ApplicationMailer
+class Admins::SfrMail2Sms < ApplicationMailer
   default from: "ne-pas-repondre-grc@hauts-de-seine.fr"
   def send_sms(recipient, phone_number, message)
     mail(to: recipient, subject: phone_number) do |format|
