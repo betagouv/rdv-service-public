3eb72d53d7e1da494e52751eb86c91953f01549f
Yannick François <yannick.francois@beta.gouv.fr>
2021-07-30 11:38:15 +0200

    précise le content-type du message (#1607)

--- app/mailers/admins/grc_92_mailer.rb
+++ app/mailers/admins/grc_92_mailer.rb
@@ -3,6 +3,7 @@
 class Admins::Grc92Mailer < ApplicationMailer
   default from: "ne-pas-repondre-grc@hauts-de-seine.fr"
   def send_sms(recipient, phone_number, message)
+    headers["Content-Type"] = "text/plain"
     mail(to: recipient, subject: phone_number) do |format|
       format.text { render plain: "#{message}\n-- RDV-Solidarités" }
     end
