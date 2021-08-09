ad7cebf3153adfeab9a89b708eaad248ab6e31a0
Yannick Francois <yaf@elsif.fr>
2021-08-09 20:13:09 +0200

    utilise un smtp alternatif pour cet envoie

--- app/mailers/admins/grc_92_mailer.rb
+++ app/mailers/admins/grc_92_mailer.rb
@@ -2,9 +2,19 @@
 
 class Admins::Grc92Mailer < ApplicationMailer
   default from: "ne-pas-repondre-grc@hauts-de-seine.fr"
+
   def send_sms(recipient, phone_number, message)
     headers["Content-Type"] = "text/plain"
-    mail(to: recipient, subject: phone_number) do |format|
+
+    delivery_options = {
+      user_name: ENV["ALTERNATE_SMTP_USERNAME"],
+      password: ENV["ALTERNATE_SMTP_PASSWORD"],
+      address: ENV["ALTERNATE_SMTP_ADDRESS"],
+      port: ENV["ALTERNATE_SMTP_PORT"],
+      authentication: ENV["ALTERNATE_SMTP_AUTHENTIFICATION"]
+    }
+
+    mail(to: recipient, subject: phone_number, delivery_method_options: delivery_options) do |format|
       format.text { render plain: "#{message}\n-- RDV-Solidarités" }
     end
   end
