a8c7beee0e2770d53b538256150d6330d41a6e3b
Nicolas Bouilleaud <nico@bou.io>
2021-08-19 19:33:42 +0200

    Remove MailerPreview from administrate
    There's http://localhost:5000/rails/mailers in development if you need it, and we shouldn’t be using previews in production anyway. If we need to log the emails that were _actually_ sent, see #1431

--- app/dashboards/mailer_preview_dashboard.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# cf https://github.com/thoughtbot/administrate/blob/master/docs/adding_controllers_without_related_model.md
-
-require "administrate/custom_dashboard"
-
-class MailerPreviewDashboard < Administrate::CustomDashboard
-  resource "MailerPreview" # for administrate views, not an actual resource
-end
