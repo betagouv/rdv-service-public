1838062d4c1b92c9b919c4a1bad7975a6fd5167a
Yannick Francois <yaf@elsif.fr>
2021-08-23 14:35:55 +0200

    ajoute un webhook

--- app/controllers/admin/territories/webhook_endpoints_controller.rb
+++ app/controllers/admin/territories/webhook_endpoints_controller.rb
@@ -1,9 +1,26 @@
 # frozen_string_literal: true
 
 class Admin::Territories::WebhookEndpointsController < Admin::Territories::BaseController
-
   def index
     @webhooks = WebhookEndpoint.where(organisation: current_territory.organisations)
   end
 
+  def new
+    @webhook = WebhookEndpoint.new
+  end
+
+  def create
+    @webhook = WebhookEndpoint.new(**webhook_endpoint_params)
+    if @webhook.save
+      redirect_to admin_territory_webhook_endpoints_path(current_territory)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def webhook_endpoint_params
+    params.require(:webhook_endpoint).permit(:target_url, :secret, :organisation_id)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -112,7 +112,7 @@ Rails.application.routes.draw do
       resources :territories, only: [:update] do
         scope module: "territories" do
           resources :agent_territorial_roles, only: %i[index new create destroy]
-          resources :webhook_endpoints, only: %i[index]
+          resources :webhook_endpoints, except: %i[show]
           resource :sms_configuration, only: %i[show edit update]
           resources :zone_imports, only: %i[new create]
           resources :zones, only: [:index] # exports only
