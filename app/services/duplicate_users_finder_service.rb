93fc82081db995520c4a328d9f0a6e2fd2a0aa2e
Yannick Francois <yaf@elsif.fr>
2021-08-24 21:39:49 +0200

    Revert "composition avec une base commune"
    This reverts commit a20a0c9f720e27f35121cb7228bcaedb06b6610e.

--- app/services/duplicate_users_finder_service.rb
+++ app/services/duplicate_users_finder_service.rb
@@ -2,27 +2,26 @@
 
 class DuplicateUsersFinderService
   def self.perform(user, organisation = nil)
-    similar_users = users_in_scope(user, organisation)
     [
-      find_duplicate_based_on_email(user, similar_users),
-      find_duplicate_based_on_identity(user, similar_users),
-      find_duplicate_based_on_phone_number(user, similar_users)
+      find_duplicate_based_on_email(user, organisation),
+      find_duplicate_based_on_identity(user, organisation),
+      find_duplicate_based_on_phone_number(user, organisation)
     ].compact
   end
 
-  def self.find_duplicate_based_on_email(user, similar_users)
+  def self.find_duplicate_based_on_email(user, organisation)
     return if user.email.blank?
 
-    similar_user = similar_users.where(email: user.email).first
+    similar_user = users_in_scope(user, organisation).where(email: user.email).first
     return nil if similar_user.blank?
 
     OpenStruct.new(severity: :error, attributes: [:email], user: similar_user)
   end
 
-  def self.find_duplicate_based_on_identity(user, similar_users)
+  def self.find_duplicate_based_on_identity(user, organisation)
     return nil unless user.birth_date.present? && user.first_name.present? && user.last_name.present?
 
-    similar_user = similar_users.where(
+    similar_user = users_in_scope(user, organisation).where(
       first_name: user.first_name.capitalize,
       last_name: user.last_name.upcase,
       birth_date: user.birth_date
@@ -32,10 +31,10 @@ class DuplicateUsersFinderService
     OpenStruct.new(severity: :warning, attributes: %i[first_name last_name birth_date], user: similar_user)
   end
 
-  def self.find_duplicate_based_on_phone_number(user, similar_users)
+  def self.find_duplicate_based_on_phone_number(user, organisation)
     return nil if user.phone_number_formatted.blank?
 
-    similar_user = similar_users
+    similar_user = users_in_scope(user, organisation)
       .where(phone_number_formatted: user.phone_number_formatted)
       .first
     return if similar_user.nil?
