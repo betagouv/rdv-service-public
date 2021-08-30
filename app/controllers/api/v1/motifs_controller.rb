5a2b6a7de457c834519b1b22586d8d77fc0cec1a
aminedhobb <amine.dhobb@gmail.com>
2021-08-30 16:52:44 +0200

    fix(api): Enhance motifs endpoint

--- app/controllers/api/v1/motifs_controller.rb
+++ app/controllers/api/v1/motifs_controller.rb
@@ -3,8 +3,22 @@
 class Api::V1::MotifsController < Api::V1::BaseController
   def index
     motifs = policy_scope(Motif)
-    motifs = motifs.active if params[:active] == "true"
-    motifs = motifs.reservable_online if params[:reservable_online] == "true"
+
+    unless params[:active].nil?
+      motifs = active_motifs? ? motifs.where(deleted_at: nil) : motifs.where.not(deleted_at: nil)
+    end
+    motifs = motifs.where(reservable_online: reservable_online_motifs?) unless params[:reservable_online].nil?
+
     render_collection(motifs.order(:id))
+  end
+
+  private
+
+  def active_motifs?
+    ActiveRecord::Type::Boolean.new.cast(params[:active])
+  end
+
+  def reservable_online_motifs?
+    ActiveRecord::Type::Boolean.new.cast(params[:reservable_online])
   end
 end
