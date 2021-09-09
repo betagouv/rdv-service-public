1467bf603cf6a858dca221d505325c1a0d20868b
Nicolas Bouilleaud <nico@bou.io>
2021-09-09 14:20:27 +0200

    Fix and add tests for full_name_concern

---
+++ spec/models/concerns/full_name_concern_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+describe FullNameConcern do
+  let(:marie) { build :user, first_name: "Marie", last_name: "Curie", birth_name: "Skłodowska" }
+  let(:pierre) { build :user, first_name: "Pierre", last_name: "Curie", birth_name: "" }
+
+  describe "#full_name" do
+    let(:subject) { person.full_name }
+
+    context "with birth_name" do
+      let(:person) { marie }
+
+      it { is_expected.to eq "Marie Curie (Skłodowska)" }
+    end
+
+    context "without birth_name" do
+      let(:person) { pierre }
+
+      it { is_expected.to eq "Pierre Curie" }
+    end
+  end
+
+  describe "reverse_full_name" do
+    let(:subject) { person.reverse_full_name }
+
+    context "with birth_name" do
+      let(:person) { marie }
+
+      it { is_expected.to eq "Curie (Skłodowska) Marie" }
+    end
+
+    context "without birth_name" do
+      let(:person) { pierre }
+
+      it { is_expected.to eq "Curie Pierre" }
+    end
+  end
+
+  describe "short_name" do
+    let(:subject) { person.short_name }
+
+    context "with birth_name" do
+      let(:person) { marie }
+
+      it { is_expected.to eq "M. Curie" }
+    end
+
+    context "without birth_name" do
+      let(:person) { pierre }
+
+      it { is_expected.to eq "P. Curie" }
+    end
+  end
+end
