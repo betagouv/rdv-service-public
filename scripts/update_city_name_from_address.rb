acccb91f5237b1f6baeb2888e7fbfd3bb929f64d
Yannick Francois <yaf@elsif.fr>
2021-09-21 00:54:40 +0200

    utilise les données à mesure quelles sont lue

--- scripts/update_city_name_from_address.rb
+++ scripts/update_city_name_from_address.rb
@@ -15,23 +15,25 @@ def geocode(file)
     method: :post,
     body: { data: File.new(file) }
   )
-  result_lines = CSV.parse(response.body)[1..]
-
   geocoded_addresses = {}
-  result_lines.each do |line|
+
+  CSV.parse(response.body) do |line|
+    next if line[0] == "id"
+
     geocoded_addresses[line[0]] = {
       city_name: line[12],
       post_code: line[11],
       city_code: line[14]
     }
   end
+  puts geocoded_addresses.length
   geocoded_addresses
 end
 
 def addresses_in_csv
   file = Tempfile.create("bla.csv")
   CSV.open(file, "wb") do |csv|
-    csv << %w[nom adresse]
+    csv << %w[id adresse]
     User.where.not(address: [nil, ""]).where(city_name: [nil, ""]).pluck(:id, :address).each do |id, address|
       csv << [id, address]
     end
