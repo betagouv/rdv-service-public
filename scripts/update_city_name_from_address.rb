f0d5e64a162eaafc37d748afbe1b15b1b150c707
Yannick Francois <yaf@elsif.fr>
2021-09-21 00:46:28 +0200

    utilise typhoeus

--- scripts/update_city_name_from_address.rb
+++ scripts/update_city_name_from_address.rb
@@ -1,31 +1,27 @@
 # frozen_string_literal: true
 
 require "csv"
-require "rest-client"
 
 API_ENDPOINT = "https://api-adresse.data.gouv.fr/search/csv/"
 
 def update_user_city_name_from(geocoded_addresses)
   puts "#{geocoded_addresses.length} ville(s) d'usager à mettre à jour"
-  geocoded_addresses.each do |id, data|
-    User.find(id).update(
-      city_name: data[:address],
-      post_code: data[:postal_code],
-      city_code: data[:city_code]
-    )
-  end
+  User.update(geocoded_addresses.keys, geocoded_addresses.values)
 end
 
 def geocode(file)
-  response = RestClient.post(API_ENDPOINT, { data: File.new(file) })
-
-  result_lines = CSV.parse(response.body, encoding: response.encoding)[1..]
+  response = Typhoeus.post(
+    API_ENDPOINT,
+    method: :post,
+    body: { data: File.new(file) }
+  )
+  result_lines = CSV.parse(response.body)[1..]
 
   geocoded_addresses = {}
   result_lines.each do |line|
     geocoded_addresses[line[0]] = {
-      address: line[12],
-      postal_code: line[11],
+      city_name: line[12],
+      post_code: line[11],
       city_code: line[14]
     }
   end
@@ -36,10 +32,8 @@ def addresses_in_csv
   file = Tempfile.create("bla.csv")
   CSV.open(file, "wb") do |csv|
     csv << %w[nom adresse]
-    User.all.each do |user|
-      next if user.address.blank?
-
-      csv << [user.id, user.address]
+    User.where.not(address: [nil, ""]).where(city_name: [nil, ""]).pluck(:id, :address).each do |id, address|
+      csv << [id, address]
     end
   end
   file
