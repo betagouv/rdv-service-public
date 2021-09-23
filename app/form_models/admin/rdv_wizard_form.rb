9656155afd9ddbb4375b256943296c7efa401d4b
Yannick Francois <yaf@elsif.fr>
2021-09-23 23:12:18 +0200

    renomme card_title en step_title

--- app/form_models/admin/rdv_wizard_form.rb
+++ app/form_models/admin/rdv_wizard_form.rb
@@ -3,5 +3,4 @@
 module Admin::RdvWizardForm
   # cf https://medium.com/@nicolasblanco/developing-a-wizard-or-multi-steps-forms-in-rails-d2f3b7c692ce
   STEPS = %w[step1 step2 step3 step4].freeze
-
 end
