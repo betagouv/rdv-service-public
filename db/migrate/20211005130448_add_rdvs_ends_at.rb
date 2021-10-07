eedc4bd94fcc3c9f5b1d95bbd5821f692dc91036
Nicolas Bouilleaud <nico@bou.io>
2021-10-07 16:23:11 +0200

    Add Rdv#ends_at

---
+++ db/migrate/20211005130448_add_rdvs_ends_at.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class AddRdvsEndsAt < ActiveRecord::Migration[6.0]
+  def change
+    add_column :rdvs, :ends_at, :datetime
+
+    up_only do
+      Rdv.update_all("ends_at = starts_at + duration_in_min * INTERVAL '1 minute'") # rubocop:disable Rails/SkipsModelValidations
+    end
+
+    add_index :rdvs, :ends_at
+
+    change_column_null :rdvs, :ends_at, false
+    rename_column :rdvs, :duration_in_min, :old_duration_in_min
+    change_column_null :rdvs, :old_duration_in_min, true
+  end
+end
