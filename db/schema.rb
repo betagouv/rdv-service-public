f869090127bf10f2ebb40b03289d0e9f15e16ff9
Nicolas Bouilleaud <nico@bou.io>
2021-10-13 17:48:07 +0200

    Index Motif#location_type
    It’s used in a query in motifs_for_plage_ouverture

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_07_105322) do
+ActiveRecord::Schema.define(version: 2021_10_13_154729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -251,6 +251,7 @@ ActiveRecord::Schema.define(version: 2021_10_07_105322) do
     t.string "sectorisation_level", default: "departement"
     t.text "custom_cancel_warning_message"
     t.index ["deleted_at"], name: "index_motifs_on_deleted_at"
+    t.index ["location_type"], name: "index_motifs_on_location_type"
     t.index ["name", "organisation_id", "location_type", "service_id"], name: "index_motifs_on_name_scoped", unique: true, where: "(deleted_at IS NULL)"
     t.index ["name"], name: "index_motifs_on_name"
     t.index ["organisation_id"], name: "index_motifs_on_organisation_id"
