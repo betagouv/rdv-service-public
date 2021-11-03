380cdd418822ea05dfb0afa74012058c2f7bd1f2
Rebecca <rebeccadumazert@hotmail.fr>
2021-11-03 14:19:54 +0100

    utilise un hook (callback) plutôt qu'un cronjob

--- db/migrate/20211028135237_add_to_be_informed_rdvs_count_to_agents.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddToBeInformedRdvsCountToAgents < ActiveRecord::Migration[6.0]
-  def change
-    add_column :agents, :to_be_informed_rdv_count, :int, default: 0
-  end
-end
