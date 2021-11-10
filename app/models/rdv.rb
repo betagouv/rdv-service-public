0198a2f2b234accb97f1d72b844a98afe0b3dca9
Rebecca <rebeccadumazert@hotmail.fr>
2021-11-10 16:15:03 +0100

    nouveau style navbar / corrige calcule rdv à renseigner

--- app/models/rdv.rb
+++ app/models/rdv.rb
@@ -59,8 +59,8 @@ class Rdv < ApplicationRecord
   scope :with_lieu, ->(lieu) { joins(:lieu).where(lieux: { id: lieu.id }) }
   scope :visible, -> { joins(:motif).where(motifs: { visibility_type: [Motif::VISIBLE_AND_NOTIFIED, Motif::VISIBLE_AND_NOT_NOTIFIED] }) }
 
-  before_save :update_unknow_past_rdv_count, if: -> { (status_changed? || new_record?) && past? }
   after_save :associate_users_with_organisation
+  after_commit :update_unknow_past_rdv_count, if: -> { (status_previously_changed? || new_record? || destroyed?) && past? }
   after_commit :reload_uuid, on: :create
 
   def self.ongoing(time_margin: 0.minutes)
@@ -208,9 +208,8 @@ class Rdv < ApplicationRecord
   end
 
   def update_unknow_past_rdv_count
-    updater = unknown? ? 1 : -1
     agents.each do |agent|
-      agent.update(unknow_past_rdv_count: agent.unknow_past_rdv_count + updater)
+      agent.update(unknow_past_rdv_count: agent.rdvs.status(:unknown_past).count)
     end
   end
 
