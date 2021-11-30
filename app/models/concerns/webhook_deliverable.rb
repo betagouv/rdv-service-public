d4078b9ea863dca7c2aea5772094054bc2005176
aminedhobb <amine.dhobb@gmail.com>
2021-11-30 11:27:01 +0100

    refactor webhook deliverable

--- app/models/concerns/webhook_deliverable.rb
+++ app/models/concerns/webhook_deliverable.rb
@@ -2,36 +2,32 @@
 
 # Hooks into :after_commit (:create and :update) and :around_destroy
 # to create jobs for webhooks.
-# The receiver must
-# * have a corresponding `<class>Blueprint` class.
-# * have an :organisation
+# The receiver must have a corresponding `<class>Blueprint` class.
 module WebhookDeliverable
   extend ActiveSupport::Concern
 
-  def generate_webhook_payload(action)
+  def generate_webhook_payload(action, api_options)
     meta = {
       model: self.class.name,
       event: action,
       timestamp: Time.zone.now
     }
     blueprint_class = "#{self.class.name}Blueprint".constantize
-    api_options = defined?(organisation) ? organisation.territory.api_options : {} # See issue #1657
     blueprint_class.render(self, root: :data, meta: meta, api_options: api_options)
   end
 
   def generate_payload_and_send_webhook(action)
-    payload = generate_webhook_payload(action)
-    send_webhook(payload)
+    subscribed_webhook_endpoints.each do |endpoint|
+      WebhookJob.perform_later(generate_webhook_payload(action, endpoint.organisation.territory.api_options), endpoint.id)
+    end
   end
 
   def generate_payload_and_send_webhook_for_destroy
-    payload = generate_webhook_payload(:destroyed)
+    payloads = subscribed_webhook_endpoints.index_with do |endpoint|
+      generate_webhook_payload(:destroyed, endpoint.organisation.territory.api_options)
+    end
     yield
-    send_webhook(payload)
-  end
-
-  def send_webhook(payload)
-    subscribed_webhook_endpoints.each do |endpoint|
+    payloads.each do |endpoint, payload|
       WebhookJob.perform_later(payload, endpoint.id)
     end
   end
