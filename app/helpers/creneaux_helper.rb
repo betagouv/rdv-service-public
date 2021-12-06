5b2e6ab57b7c9af2ea434e2f4c48e0633c36bdf1
rebeccadumazert <rebeccadumazert@hotmail.fr>
2021-12-06 11:35:03 +0100

    ajoute icon de couleur par agent

--- app/helpers/creneaux_helper.rb
+++ app/helpers/creneaux_helper.rb
@@ -3,25 +3,16 @@
 module CreneauxHelper
   COLORS = %w[
     color-scheme-bleu
-    color-scheme-red
-    color-scheme-green
-    color-scheme-yellow
-    color-scheme-pink
+    color-scheme-turquoise
+    color-scheme-indigo
     color-scheme-purple
-    color-scheme-grey
-    color-scheme-beige
-    color-scheme-apple-green
-    color-scheme-burgundy
-    color-scheme-duck-green
-    color-scheme-light-blue
-    color-scheme-light-grey
-    color-scheme-light-purple
-    color-scheme-strong-green
-    color-scheme-strong-blue
-    color-scheme-strong-grey
-    color-scheme-strong-red
-    color-scheme-strong-pink
-    color-scheme-strong-purple
+    color-scheme-pink
+    color-scheme-yellow
+    color-scheme-red
+    color-scheme-orange
+    color-scheme-green
+    color-scheme-teal
+    color-scheme-lightturquoise
   ].freeze
   def agent_color(color_index)
     COLORS[color_index % COLORS.length]
