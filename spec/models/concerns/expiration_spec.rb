8477e52c7c54511b95f53be25c2c40890d8aebd4
Yannick Francois <yaf@elsif.fr>
2021-12-06 22:56:13 +0100

    Prend en compte le cas de la date de fin d'absence or récurrence

--- spec/models/concerns/expiration_spec.rb
+++ spec/models/concerns/expiration_spec.rb
@@ -5,21 +5,21 @@ describe Expiration, type: :concern do
     it "#{element} is expired when end_day before today" do
       today = Time.zone.parse("20210323 13:45")
       travel_to(today)
-      objet = create(:absence, first_day: today - 5.days, end_day: today - 5.days)
+      objet = build(element, first_day: today - 5.days, end_day: today - 5.days)
       expect(objet.expired?).to be true
     end
 
     it "#{element} is not past when end_day after today" do
       today = Time.zone.parse("20210323 13:45")
       travel_to(today)
-      objet = create(:absence, first_day: today + 3.days, end_day: today + 3.days)
+      objet = build(element, first_day: today + 3.days, end_day: today + 3.days)
       expect(objet.expired?).to be false
     end
 
     it "#{element} is not past when end_day before today with a recurrence after today" do
       today = Time.zone.parse("20210323 13:45")
       travel_to(today)
-      objet = create(:absence, first_day: today - 1.day, end_day: today - 1.day, recurrence: Montrose.every(:week, until: today + 1.month, starts: today - 1.day))
+      objet = build(element, first_day: today - 1.day, end_day: today - 1.day, recurrence: Montrose.every(:week, until: today + 1.month, starts: today - 1.day))
       expect(objet.expired?).to be false
     end
   end
