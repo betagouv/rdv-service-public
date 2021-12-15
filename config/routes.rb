e092aeb832a15e69c61fde2f757bff93b2bdd5fc
Nicolas Bouilleaud <nico@bou.io>
2021-12-15 19:22:21 +0100

    bin/rails app:update: Unwanted changes to revert

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   ## OAUTH ##
   devise_scope :user do
     get "omniauth/franceconnect/callback" => "omniauth_callbacks#franceconnect"
