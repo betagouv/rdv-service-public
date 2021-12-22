793b66d7fcc8bd9e16b4f1860df81f06a9314c25
Yannick Francois <yaf@elsif.fr>
2021-12-22 10:58:01 +0100

    précise l'index

--- db/migrate/20211220210821_create_teams.rb
+++ db/migrate/20211220210821_create_teams.rb
@@ -4,9 +4,11 @@ class CreateTeams < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
       t.belongs_to :territory
-      t.string :name, index: { unique: true }
+      t.string :name
       t.timestamps
     end
+
+    add_index :teams, "to_tsvector('simple'::regconfig, COALESCE(motifs.name, ''::text))", using: :gin, name: "index_teams_name_vector"
 
     create_table :agent_teams do |t|
       t.belongs_to :team
