930d922c915064411eb8752960feb2e34644be20
Yannick Francois <yaf@elsif.fr>
2021-12-22 10:29:02 +0100

    recherche doublon usager sur nom, prénom ET naissance

--- app/services/duplicate_users_finder_service.rb
+++ app/services/duplicate_users_finder_service.rb
@@ -5,7 +5,7 @@ class DuplicateUsersFinderService < BaseService
     @user = user
     @organisation = organisation
   end
-
+  
   def perform
     [
       find_duplicate_based_on_email,
@@ -13,41 +13,40 @@ class DuplicateUsersFinderService < BaseService
       find_duplicate_based_on_phone_number
     ].compact
   end
-
+  
   private
-
+  
   attr_reader :user, :organisation
-
+  
   def find_duplicate_based_on_email
     return if user.email.blank?
-
+    
     similar_user = users_in_scope.where(email: user.email).first
     return nil if similar_user.blank?
-
+    
     OpenStruct.new(severity: :error, attributes: [:email], user: similar_user)
   end
-
+  
   def find_duplicate_based_on_identity
     return nil unless user.birth_date.present? && user.first_name.present? && user.last_name.present?
-
-    similar_user = users_in_scope.where(
-      birth_date: user.birth_date,
-      User.arel_table[:first_name].matches(user.first_name),
-      User.arel_table[:last_name].matches(user.last_name),
-    ).first
+    
+    similar_user = users_in_scope.
+      where(birth_date: user.birth_date).
+      where(User.arel_table[:first_name].matches(user.first_name)).
+      where(User.arel_table[:last_name].matches(user.last_name)).first
     return nil if similar_user.blank?
-
+  
     OpenStruct.new(severity: :warning, attributes: %i[first_name last_name birth_date], user: similar_user)
   end
 
   def find_duplicate_based_on_phone_number
     return nil if user.phone_number_formatted.blank?
-
+    
     similar_user = users_in_scope
-      .where(phone_number_formatted: user.phone_number_formatted)
-      .first
+    .where(phone_number_formatted: user.phone_number_formatted)
+    .first
     return if similar_user.nil?
-
+    
     OpenStruct.new(severity: :warning, attributes: [:phone_number], user: similar_user)
   end
 
