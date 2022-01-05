d112640efcdf3432441b49bdd0901d31fc696eb2
Yannick Francois <yaf@elsif.fr>
2022-01-05 17:53:38 +0100

    Recherche les agents plutôt que de les lister tous

--- app/controllers/admin/territories/agents_controller.rb
+++ app/controllers/admin/territories/agents_controller.rb
@@ -6,6 +6,15 @@ class Admin::Territories::AgentsController < Admin::Territories::BaseController
     @agents = params[:search].present? ? @agents.search_by_text(params[:search]) : @agents.order_by_last_name
   end
 
+  def search
+    agents = policy_scope_admin(Agent)
+      .joins(:organisations).where(organisations: { id: current_territory.organisations.map(&:id) })
+      .active.complete.limit(10)
+    @agents = search_params[:term].present? ? agents.search_by_text(search_params[:term]) : agents.order_by_last_name
+
+    skip_authorization
+  end
+
   def edit
     @agent = Agent.find(params[:id])
   end
@@ -21,5 +30,19 @@ class Admin::Territories::AgentsController < Admin::Territories::BaseController
 
   def agent_params
     params.require(:agent).permit(team_ids: [])
+  end
+
+  def search_params
+    @search_params ||= begin
+      search_params = params.permit(:term)
+      search_params[:term] = clean_search_term(search_params[:term])
+      search_params
+    end
+  end
+
+  def clean_search_term(term)
+    return nil if term.blank?
+
+    I18n.transliterate(term)
   end
 end
