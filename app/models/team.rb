5beaf20f91bf5c55364cdc8de2ec7e7aebd3e052
Yannick Francois <yaf@elsif.fr>
2022-01-07 12:48:36 +0100

    valide que les agents sont du même territoire

--- app/models/team.rb
+++ app/models/team.rb
@@ -17,8 +17,17 @@ class Team < ApplicationRecord
   has_many :agents, through: :agent_teams
 
   validates :name, presence: true, uniqueness: true
+  validate :agent_from_same_territory, if: -> { agents.any? }
 
   def to_s
     name
+  end
+
+  private
+
+  def agent_from_same_territory
+    return if agents.flat_map(&:territories).uniq.count == 1
+
+    errors.add(:agents, :not_from_same_territory)
   end
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -10,6 +10,15 @@ describe Team, type: :model do
       create(:team, name: "ubberTeam")
       expect(build(:team, name: "ubberTeam")).to be_invalid
     end
+
+    it "invalid when add agents from different territory" do
+      territory = create(:territory)
+      organisation = create(:organisation, territory: territory)
+      agent = create(:agent, basic_role_in_organisations: [organisation])
+      agent_from_other_territory = create(:agent, basic_role_in_organisations: [create(:organisation, territory: create(:territory))])
+
+      expect(build(:team, territory: territory, agents: [agent, agent_from_other_territory])).to be_invalid
+    end
   end
 
   describe "#to_s" do
