cf5181709f0b6dd51d78bad57b757cc85ad5aa39
Yannick Francois <yaf@elsif.fr>
2022-01-10 23:19:01 +0100

    fix spec

--- spec/helpers/plage_ouvertures_helper_spec.rb
+++ spec/helpers/plage_ouvertures_helper_spec.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 describe PlageOuverturesHelper do
+  let(:now) { Time.zone.parse("2021-12-23 09:00") }
+
+  before do
+    travel_to(now)
+  end
+
   describe "#time_collections_for_plage_ouverture" do
     it "return 156 entries" do
       expect(time_collections_for_plage_ouverture.count).to eq(156)
@@ -26,29 +32,29 @@ describe PlageOuverturesHelper do
   describe "#display_recurrence" do
     it "with a weekly recurrence" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week))
-      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, le mardi", "de 08:00 à 12:00", "à partir du mardi 11 janvier 2022"])
+      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, le mardi", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
 
     it "with a weekly recurrence on wednesday" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week, on: ["wednesday"]))
-      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, les mercredis", "de 08:00 à 12:00", "à partir du mardi 11 janvier 2022"])
+      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, les mercredis", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
 
     it "with a monthly recurrence" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:month, day: { 3 => [2] }))
-      expect(display_recurrence(plage_ouverture)).to eq(["Tous les  mois, le 2ème mercredi", "de 08:00 à 12:00", "à partir du mardi 11 janvier 2022"])
+      expect(display_recurrence(plage_ouverture)).to eq(["Tous les  mois, le 2ème mercredi", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
   end
 
   describe "#plage_ouverture_occurrence_text" do
     it "returns occurrence text" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week))
-      expect(plage_ouverture_occurrence_text(plage_ouverture)).to eq("Toutes les  semaines, le mardi de 08:00 à 12:00 à partir du mardi 11 janvier 2022")
+      expect(plage_ouverture_occurrence_text(plage_ouverture)).to eq("Toutes les  semaines, le mardi de 08:00 à 12:00 à partir du mardi 28 décembre 2021")
     end
 
     it "returns" do
       plage_ouverture = build(:plage_ouverture)
-      expect(plage_ouverture_occurrence_text(plage_ouverture)).to eq("mardi 11 janvier 2022de 08:00 à 12:00")
+      expect(plage_ouverture_occurrence_text(plage_ouverture)).to eq("mardi 28 décembre 2021de 08:00 à 12:00")
     end
   end
 
