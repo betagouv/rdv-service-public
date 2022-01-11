164f83b474117ba393c6d860439828c7b27f5742
Yannick Francois <yaf@elsif.fr>
2022-01-11 22:24:18 +0100

    déplace les test sur text_search

--- spec/models/concerns/text_search_spec.rb
+++ spec/models/concerns/text_search_spec.rb
@@ -34,5 +34,43 @@ describe TextSearch, type: :concern do
     include_examples "#combined_search_terms"
   end
 
-  # Test sur qui inclue ce concerns
+  describe(User) do
+    let(:other_object) { create(:team, name: "dont") }
+    let(:object) { create(:team, name: "findme") }
+    let(:object_to_save) { build(:team, name: "findme") }
+
+    include_examples "#refresh_search_terms"
+    include_examples "#combined_search_terms"
+
+    it "returns users that match with first name" do
+      create(:user, first_name: "jean")
+      patricia = create(:user, first_name: "patricia")
+      expect(described_class.search_by_text("patricia")).to eq([patricia])
+    end
+
+    it "returns users that match with partial email" do
+      create(:user, email: "jean@moustache.fr")
+      patricia = create(:user, email: "patoche@duroy.fr")
+      expect(described_class.search_by_text("patoche@dur")).to eq([patricia])
+    end
+
+    it "returns users that match with email" do
+      create(:user, email: "jean@moustache.fr")
+      patricia = create(:user, email: "patoche@duroy.fr")
+      expect(described_class.search_by_text("patoche@duroy.fr")).to eq([patricia])
+    end
+
+    it "returns users that match with phone_number_formatted" do
+      jean = create(:user, phone_number: "01 30 30 04 04")
+      create(:user, phone_number: "01 31 34 34 34")
+      expect(described_class.search_by_text("+3313030")).to eq([jean])
+    end
+
+    it "orders results by relevance" do
+      durand = create(:user, first_name: "Louis", last_name: "Durand")
+      dupont = create(:user, first_name: "Louis", last_name: "Dupont")
+      expect(described_class.search_by_text("Louis Durand")).to eq([durand, dupont])
+    end
+  end
+
 end
