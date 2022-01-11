0f19dfcc2da90849c558510d12315d4f555d7a0b
Yannick Francois <yaf@elsif.fr>
2022-01-11 18:05:50 +0100

    linter

--- spec/mailers/previews/agents/export_mailer_preview.rb
+++ spec/mailers/previews/agents/export_mailer_preview.rb
@@ -4,10 +4,9 @@ class Agents::ExportMailerPreview < ActionMailer::Preview
   def rdv_export
     agent = Agent.first
     options = {
-      start: Date.today.in_time_zone - 4.year - 7.days,
-      end: Date.today.in_time_zone - 4.year,
+      start: Date.today.in_time_zone - 4.years - 7.days,
+      end: Date.today.in_time_zone - 4.years
     }
     Agents::ExportMailer.rdv_export(agent.id, agent.organisations.first.id, options)
   end
 end
-
