51c1f076099e74854e97645db5b7abd440d1921d
Yannick Francois <yaf@elsif.fr>
2022-01-12 17:08:10 +0100

    selectionne l'environnement avant tout le reste

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
+ENV["RAILS_ENV"] ||= "test"
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require "spec_helper"
-ENV["RAILS_ENV"] ||= "test"
 require File.expand_path("../config/environment", __dir__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
