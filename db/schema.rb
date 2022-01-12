3dc83895f3121326d9eca6a85fe53daf6c357137
Yannick Francois <yaf@elsif.fr>
2022-01-12 18:11:18 +0100

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_10_222218) do
+ActiveRecord::Schema.define(version: 2022_01_10_132442) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
