bfdbdef91fe6f5ed56b022ebfaf94cd40b38ef05
rebeccadumazert <rebeccadumazert@hotmail.fr>
2022-01-14 14:10:33 +0100

    rend accessible footer

--- spec/features/accessibility/user_pages_spec.rb
+++ spec/features/accessibility/user_pages_spec.rb
@@ -10,4 +10,9 @@ describe "welcome", js: true do
     visit accueil_mds_path
     expect(page).to be_axe_clean
   end
+
+  it "accessibility_path page is accessible" do
+    visit accessibility_path
+    expect(page).to be_axe_clean
+  end
 end
