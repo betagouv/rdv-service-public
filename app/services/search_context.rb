23be494f0b11a59236b584c96e16035be5f35f3b
aminedhobb <amine.dhobb@gmail.com>
2022-01-18 23:28:03 +0100

    refactor(invitation): Change search context

--- app/services/search_context.rb
+++ app/services/search_context.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class SearchContext
-  attr_reader :errors, :query, :departement, :address, :city_code, :street_ban_id, :latitude, :longitude
+  attr_reader :errors, :query, :departement, :address, :city_code, :street_ban_id, :latitude, :longitude,
+              :motif_name_with_location_type
 
   def initialize(current_user, query = {})
     @current_user = current_user
@@ -13,9 +14,10 @@ class SearchContext
     @city_code = query[:city_code]
     @departement = query[:departement]
     @street_ban_id = query[:street_ban_id]
-    @organisation_id = query[:organisation_id]
+    @organisation_ids = query[:organisation_ids]
+    @motif_search_terms = query[:motif_search_terms]
+    @motif_name_with_location_type = query[:motif_name_with_location_type]
     @service_id = query[:service_id]
-    @motif_id = query[:motif_id]
     @lieu_id = query[:lieu_id]
     @start_date = query[:date]
     @errors = []
@@ -24,9 +26,10 @@ class SearchContext
   # *** Method that outputs the next step for the user to complete its rdv journey ***
   # *** It is used in #to_partial_parth to render the matching partial view ***
   def current_step
+    # byebug
     if address.blank?
       :address_selection
-    elsif motif.nil?
+    elsif !motif_selected?
       :motif_selection
     elsif lieu.nil?
       :lieu_selection
@@ -52,19 +55,19 @@ class SearchContext
   end
 
   def unique_motifs_by_name_and_location_type
-    @unique_motifs_by_name_and_location_type ||= available_motifs.uniq { [_1.name, _1.location_type] }
+    @unique_motifs_by_name_and_location_type ||= matching_motifs.uniq { [_1.name, _1.location_type] }
   end
 
-  def organisation
-    @organisation ||= @organisation_id.blank? ? nil : Organisation.find(@organisation_id)
+  def selected_motif_name
+    motif_selected? ? unique_motifs_by_name_and_location_type.first.name : nil
+  end
+
+  def motif_selected?
+    unique_motifs_by_name_and_location_type.length == 1
   end
 
   def service
     @service ||= @service_id.blank? ? nil : Service.find(@service_id)
-  end
-
-  def motif
-    @motif ||= @motif_id.blank? ? nil : Motif.find(@motif_id)
   end
 
   def lieu
@@ -73,12 +76,10 @@ class SearchContext
 
   def lieux
     @lieux ||= \
-      if motif.nil?
-        Lieu.none
-      else
-        Lieu.for_motif(motif).includes(:organisation)
-          .sort_by { |lieu| lieu.distance(@latitude.to_f, @longitude.to_f) }
-      end
+      Lieu
+        .with_open_slots_for_motifs(@matching_motifs)
+        .includes(:organisation)
+        .sort_by { |lieu| lieu.distance(@latitude.to_f, @longitude.to_f) }
   end
 
   def next_availability_by_lieux
@@ -98,6 +99,10 @@ class SearchContext
     start_date..(start_date + 6.days)
   end
 
+  def max_booking_delay
+    matching_motifs.maximum("max_booking_delay")
+  end
+
   def creneaux
     @creneaux ||= creneaux_search.creneaux
   end
@@ -114,12 +119,24 @@ class SearchContext
 
   def creneaux_search_for(lieu, date_range)
     Users::CreneauxSearch.new(
-      user: @current_user,
-      motif: motif,
+      user: @current_user || invited_user,
+      motif: matching_motifs.where(organisation: lieu.organisation).first,
       lieu: lieu,
       date_range: date_range,
       geo_search: geo_search
     )
+  end
+
+  def matching_motifs
+    @matching_motifs ||= begin
+      motifs = if @motif_name_with_location_type.present?
+                 available_motifs.search_by_name_with_location_type(@motif_name_with_location_type)
+               else
+                 available_motifs
+               end
+      motifs = motifs.where(organisation: lieu.organisation) if lieu.present?
+      motifs
+    end
   end
 
   def available_motifs
@@ -127,8 +144,21 @@ class SearchContext
   end
 
   def available_motifs_for_invitation
-    geo_search.available_motifs.where(organisation: organisation, service: service).presence ||
-      Motif.available_with_plages_ouvertures.where(organisation: organisation, service: service)
+    # we retrieve the geolocalised available motifs, if there are none we fallback
+    # on the availabe motifs for the organisations passed in the query
+    invitation_geo_search_motifs.presence || invitation_organisations_motifs
+  end
+
+  def invitation_geo_search_motifs
+    motifs = geo_search.available_motifs
+    motifs = motifs.search_by_text(@motif_search_terms) if @motif_search_terms.present?
+    motifs
+  end
+
+  def invitation_organisations_motifs
+    motifs = Motif.available_with_plages_ouvertures.where(organisation_id: @organisation_ids)
+    motifs = motifs.search_by_text(@motif_search_terms) if @motif_search_terms.present?
+    motifs
   end
 
   def invited_user
@@ -139,7 +169,7 @@ class SearchContext
   end
 
   def invitation_valid?
-    token_valid? && current_user_is_invited_user? && user_belongs_to_current_organsation?
+    token_valid? && current_user_is_invited_user?
   end
 
   def token_valid?
@@ -154,13 +184,6 @@ class SearchContext
     return true if @current_user == invited_user
 
     @errors << I18n.t("devise.invitations.current_user_mismatch")
-    false
-  end
-
-  def user_belongs_to_current_organsation?
-    return true if invited_user.organisation_ids.include?(organisation.id)
-
-    @errors << I18n.t("devise.invitations.organisation_mismatch")
     false
   end
 end
