betagouv/rdv-service-public

commit 511d05d26c6959af23c9434c93703520f3d1e3a8

Properly handle blank values in human_attribute_value

app/models/concerns/human_attribute_value.rb
@@ -29,11 +29,13 @@ module HumanAttributeValue
     # > Rdv.human_attribute_value(:status, :excused, count: 2)
     # => "Annulées (excusées)" # I18n.t('activerecord.attributes.rdv/statuses.excused.other')
     def human_attribute_value(enum_name, value, options = {})
-      return if value.nil?
+      return if value.blank?
 
+      options = { default: value }.merge(options) # make sure to return the value unchanged if not found
       unless options.delete(:disable_cast)
         value = attribute_types[enum_name.to_s].cast(value)
       end
+
       context = options.delete(:context)
       enum_i18n_scope = [enum_name.to_s.pluralize, context].compact.join("/")
       human_attribute_name("#{enum_i18n_scope}.#{value}", options)

commit 99344b723a643bc25ad878cff0d0c928a2af1d29

Fix display of disabled Rdv notifications
fixes #2135

broken in 511d05d26c6959af23c9434c93703520f3d1e3a8 (#2114)

app/models/concerns/human_attribute_value.rb
@@ -29,7 +29,7 @@ module HumanAttributeValue
     # > Rdv.human_attribute_value(:status, :excused, count: 2)
     # => "Annulées (excusées)" # I18n.t('activerecord.attributes.rdv/statuses.excused.other')
     def human_attribute_value(enum_name, value, options = {})
-      return if value.blank?
+      return if value.nil? || value == ""
 
       options = { default: value }.merge(options) # make sure to return the value unchanged if not found
       unless options.delete(:disable_cast)