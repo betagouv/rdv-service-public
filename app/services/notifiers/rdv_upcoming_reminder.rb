550ea4acc149ab2d7623453486603ed472987203
Nicolas Bouilleaud <nico@bou.io>
2022-02-14 18:13:19 +0100

    Specify lower priority for reminder notifications

--- app/services/notifiers/rdv_upcoming_reminder.rb
+++ app/services/notifiers/rdv_upcoming_reminder.rb
@@ -8,12 +8,12 @@ class Notifiers::RdvUpcomingReminder < Notifiers::RdvBase
   end
 
   def notify_user_by_mail(user)
-    Users::RdvMailer.rdv_upcoming_reminder(@rdv.payload(nil, user), user).deliver_later
+    Users::RdvMailer.rdv_upcoming_reminder(@rdv.payload(nil, user), user).deliver_later(queue: :mailers_low)
     @rdv.events.create!(event_type: RdvEvent::TYPE_NOTIFICATION_MAIL, event_name: :upcoming_reminder)
   end
 
   def notify_user_by_sms(user)
-    Users::RdvSms.rdv_upcoming_reminder(@rdv, user).deliver_later
+    Users::RdvSms.rdv_upcoming_reminder(@rdv, user).deliver_later(queue: :sms_low)
     @rdv.events.create!(event_type: RdvEvent::TYPE_NOTIFICATION_SMS, event_name: :upcoming_reminder)
   end
 end
