5905c369bf49b35590dff4a366624012630fc02a
rebeccadumazert <rebeccadumazert@hotmail.fr>
2022-03-09 16:39:02 +0100

    ajoute test sur path_agenda_agent

--- spec/features/accessibility/accueil_mds_spec.rb
+++ spec/features/accessibility/accueil_mds_spec.rb
@@ -4,4 +4,7 @@ describe "accueil_mds", js: true do
   it "root path page is accessible" do
     expect_page_to_be_axe_clean(accueil_mds_path)
   end
+  it "agent agenda path page is accessible" do
+    expect_page_to_be_axe_clean(admin_organisation_agent_agenda_path)
+  end
 end
