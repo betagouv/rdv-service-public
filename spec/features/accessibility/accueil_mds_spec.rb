679aefb993429d2e1edf4a8a836582efeec84bc9
rebeccadumazert <rebeccadumazert@hotmail.fr>
2022-03-11 17:51:43 +0100

    domain model mis à jour

--- spec/features/accessibility/accueil_mds_spec.rb
+++ spec/features/accessibility/accueil_mds_spec.rb
@@ -6,13 +6,12 @@ describe "accueil_mds", js: true do
   end
 
   it "agent agenda path page is accessible" do
-    
     territory = create(:territory, departement_number: "75")
     organisation = create(:organisation, territory: territory)
 
     agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
     login_as agent
-    
+
     path = admin_organisation_agent_agenda_path(organisation, agent.id)
     expect_page_to_be_axe_clean(path)
   end
