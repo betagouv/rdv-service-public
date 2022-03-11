8ce4c2ceab9155c2177670849878de5824dd902d
rebeccadumazert <rebeccadumazert@hotmail.fr>
2022-03-11 13:46:15 +0100

    test accessibilité sur la page agenda

--- spec/features/accessibility/accueil_mds_spec.rb
+++ spec/features/accessibility/accueil_mds_spec.rb
@@ -4,7 +4,16 @@ describe "accueil_mds", js: true do
   it "root path page is accessible" do
     expect_page_to_be_axe_clean(accueil_mds_path)
   end
+
   it "agent agenda path page is accessible" do
-    expect_page_to_be_axe_clean(admin_organisation_agent_agenda_path)
+    
+    territory = create(:territory, departement_number: "75")
+    organisation = create(:organisation, territory: territory)
+
+    agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
+    login_as agent
+    
+    path = admin_organisation_agent_agenda_path(organisation, agent.id)
+    expect_page_to_be_axe_clean(path)
   end
 end
