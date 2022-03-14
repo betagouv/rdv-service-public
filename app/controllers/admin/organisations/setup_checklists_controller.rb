fe9462f885b1011e2656b32f2009b916b1951931
Victor Mours <victor.mours@gmail.com>
2022-03-14 12:04:43 +0100

    new dedicated onboarding for cnfs

--- app/controllers/admin/organisations/setup_checklists_controller.rb
+++ app/controllers/admin/organisations/setup_checklists_controller.rb
@@ -6,11 +6,17 @@ class Admin::Organisations::SetupChecklistsController < AgentAuthController
   def show
     authorize(@organisation)
     @lieux = policy_scope(Lieu)
-    @other_agents = policy_scope(Agent)
-      .joins(:organisations).where(organisations: { id: current_organisation.id })
-      .where.not(id: current_agent)
-      .order_by_last_name
-
     @motifs = policy_scope(Motif)
+
+    if current_agent.conseiller_numerique?
+      render "show_conseiller_numerique"
+    else
+      @other_agents = policy_scope(Agent)
+        .joins(:organisations).where(organisations: { id: current_organisation.id })
+        .where.not(id: current_agent)
+        .order_by_last_name
+
+      render "show"
+    end
   end
 end
