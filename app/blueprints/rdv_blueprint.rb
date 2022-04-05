b1407be0ffcd99a108a2686028b75f17523a818f
Victor Mours <victor.mours@gmail.com>
2022-04-05 10:06:29 +0200

    Add rdv collectif fields in rdv blueprint

--- app/blueprints/rdv_blueprint.rb
+++ app/blueprints/rdv_blueprint.rb
@@ -3,7 +3,8 @@
 class RdvBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :uuid, :status, :starts_at, :ends_at, :duration_in_min, :address, :context, :cancelled_at, :created_by
+  fields :uuid, :status, :starts_at, :ends_at, :duration_in_min, :address, :context, :cancelled_at,
+         :max_participants_count, :users_count, :name, :collectif, :created_by
 
   association :organisation, blueprint: OrganisationBlueprint
   association :motif, blueprint: MotifBlueprint
