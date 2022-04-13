755cd61553b7531d8c1d35cbc45ee568a0b85071
Thomas Guillet <thomas@palya.fr>
2022-04-13 18:12:14 +0200

    Permet la création de Territoire dans la vue super admin

---
+++ app/dashboards/territory_dashboard.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require "administrate/base_dashboard"
+
+class TerritoryDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    departement_number: Field::String,
+    name: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    departement_number
+    name
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    name
+    departement_number
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    name
+    departement_number
+  ].freeze
+
+  def display_resource(territory)
+    "Territory ##{territory.id} - #{territory.name} (#{territory.departement_number})"
+  end
+end
