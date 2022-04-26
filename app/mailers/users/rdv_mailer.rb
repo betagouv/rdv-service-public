26e4f252cee96365e425b112cdec3776c3dc4dab
Nicolas Bouilleaud <nico@bou.io>
2022-04-26 13:59:57 +0200

    Save email subject in rdv receipt

--- app/mailers/users/rdv_mailer.rb
+++ app/mailers/users/rdv_mailer.rb
@@ -18,7 +18,7 @@ class Users::RdvMailer < ApplicationMailer
     self.ics_payload = @rdv.payload(:create, @user)
     subject = t("users.rdv_mailer.rdv_created.title", date: l(@rdv.starts_at, format: :human))
     mail(subject: subject)
-    save_receipt
+    save_receipt(subject)
   end
 
   def rdv_date_updated(old_starts_at)
@@ -27,26 +27,26 @@ class Users::RdvMailer < ApplicationMailer
     self.ics_payload = @rdv.payload(:update, @user)
     subject = t("users.rdv_mailer.rdv_date_updated.title", date: relative_date(@old_starts_at))
     mail(subject: subject)
-    save_receipt
+    save_receipt(subject)
   end
 
   def rdv_upcoming_reminder
     self.ics_payload = @rdv.payload(nil, @user)
     subject = t("users.rdv_mailer.rdv_upcoming_reminder.title", date: l(@rdv.starts_at, format: :human))
     mail(subject: subject)
-    save_receipt
+    save_receipt(subject)
   end
 
   def rdv_cancelled
     self.ics_payload = @rdv.payload(:destroy, @user)
     subject = t("users.rdv_mailer.rdv_cancelled.title", date: l(@rdv.starts_at, format: :human), organisation: @rdv.organisation.name)
     mail(subject: subject)
-    save_receipt
+    save_receipt(subject)
   end
 
   private
 
-  def save_receipt
-    Receipt.create!(rdv: @rdv, user: @user, event: action_name, channel: :mail, result: :processed, email_address: @user.email)
+  def save_receipt(subject)
+    Receipt.create!(rdv: @rdv, user: @user, event: action_name, channel: :mail, result: :processed, email_address: @user.email, content: subject)
   end
 end
