ee17f8ce0011c3f2635287e9fb812ed3ca30576d
Nicolas Bouilleaud <nico@bou.io>
2022-04-26 17:39:40 +0200

    Fix Absence and PlageOuverture specs

--- spec/mailers/agents/plage_ouverture_mailer_spec.rb
+++ spec/mailers/agents/plage_ouverture_mailer_spec.rb
@@ -3,52 +3,31 @@
 describe Agents::PlageOuvertureMailer, type: :mailer do
   { created: "créée", updated: "modifiée", destroyed: "supprimée" }.each do |action, verb|
     context "when #{action}" do
-      let(:ics_payload) do
-        {
-          name: "plage-ouverture--.ics",
-          object: "plage_ouverture",
-          action: action,
-          agent_email: "bob@demo.rdv-solidarites.fr",
-          starts_at: Time.zone.parse("20190423 13h00"),
-          ical_uid: "plage_ouverture_@RDV Solidarités",
-          title: "Plage d'ouverture #{verb}",
-          first_occurrence_ends_at: Time.zone.parse("20190423 18h00"),
-          address: "une adresse"
-        }
-      end
+      let(:plage_ouverture) { create :plage_ouverture, agent: create(:agent, email: "bob@demo.rdv-solidarites.fr") }
 
       it "mail to plage ouverture's agent" do
-        mail = described_class.send("plage_ouverture_#{action}", ics_payload)
+        mail = described_class.with(plage_ouverture: plage_ouverture).send("plage_ouverture_#{action}")
         expect(mail.to).to eq(["bob@demo.rdv-solidarites.fr"])
       end
 
       it "have a good subject" do
-        mail = described_class.send("plage_ouverture_#{action}", ics_payload)
-        expect(mail.subject).to eq("RDV Solidarités - Plage d'ouverture #{verb}")
+        mail = described_class.with(plage_ouverture: plage_ouverture).send("plage_ouverture_#{action}")
+        expect(mail.subject).to eq("RDV Solidarités - Plage d’ouverture #{verb} - #{plage_ouverture.title}")
       end
 
       it "has a ICS file join with UID" do
-        mail = described_class.send("plage_ouverture_#{action}", ics_payload)
+        mail = described_class.with(plage_ouverture: plage_ouverture).send("plage_ouverture_#{action}")
         cal = mail.find_first_mime_type("text/calendar")
-        expect(cal.decoded).to match("UID:plage_ouverture_@RDV Solidarités")
+        expect(cal.decoded).to match("UID:plage_ouverture_#{plage_ouverture.id}@RDV Solidarités")
       end
     end
   end
 
   describe "#plage_ouverture_destroyed" do
+    let(:plage_ouverture) { create :plage_ouverture }
+
     it "have a STATUS:CANCELLED in ICS file joined" do
-      ics_payload = {
-        name: "plage-ouverture--.ics",
-        object: "plage_ouverture",
-        action: :destroy,
-        agent_email: "bob@demo.rdv-solidarites.fr",
-        starts_at: Time.zone.parse("20190423 13h00"),
-        ical_uid: "plage_ouverture_@RDV Solidarités",
-        title: "Plage d'ouverture supprimée",
-        first_occurrence_ends_at: Time.zone.parse("20190423 18h00"),
-        address: "une adresse"
-      }
-      mail = described_class.send("plage_ouverture_destroyed", ics_payload)
+      mail = described_class.with(plage_ouverture: plage_ouverture).send("plage_ouverture_destroyed")
       cal = mail.find_first_mime_type("text/calendar")
       expect(cal.decoded).to match("STATUS:CANCELLED")
     end
--- spec/models/concerns/payloads/plage_ouverture_spec.rb
+++ spec/models/concerns/payloads/plage_ouverture_spec.rb
@@ -2,7 +2,7 @@
 
 describe Payloads::PlageOuverture do
   describe "#payload" do
-    %i[name agent_email starts_at recurrence ical_uid title ends_at address].each do |key|
+    %i[name starts_at recurrence ical_uid ends_at].each do |key|
       it "return an hash with key #{key}" do
         plage_ouverture = build(:plage_ouverture)
         expect(plage_ouverture.payload).to have_key(key)
@@ -20,12 +20,6 @@ describe Payloads::PlageOuverture do
       let(:plage_ouverture) { build(:plage_ouverture, title: "something", start_time: Time.zone.parse("12h30"), first_day: Date.new(2020, 11, 13)) }
 
       it { expect(plage_ouverture.payload[:name]).to eq("plage-ouverture-something-2020-11-13-12-30-00-0100.ics") }
-    end
-
-    describe ":agent_email" do
-      let(:plage_ouverture) { build(:plage_ouverture, agent: build(:agent, email: "polo@demo.rdv-solidarites.fr")) }
-
-      it { expect(plage_ouverture.payload[:agent_email]).to eq("polo@demo.rdv-solidarites.fr") }
     end
 
     describe ":starts_at" do
@@ -47,24 +41,11 @@ describe Payloads::PlageOuverture do
       it { expect(plage_ouverture.payload[:ical_uid]).to eq("plage_ouverture_#{plage_ouverture.id}@#{BRAND}") }
     end
 
-    describe ":title" do
-      let(:plage_ouverture) { build(:plage_ouverture, title: "Permanence") }
-
-      it { expect(plage_ouverture.payload[:title]).to eq("Permanence") }
-    end
-
     describe ":ends_at" do
       let(:starts_at) { Time.zone.parse("20201009 11h45") }
       let(:plage_ouverture) { build(:plage_ouverture, end_time: starts_at + 5.hours, first_day: starts_at.to_date) }
 
       it { expect(plage_ouverture.payload[:ends_at]).to eq(starts_at + 5.hours) }
-    end
-
-    describe ":address" do
-      let(:lieu) { build(:lieu, address: "10 rue de là-bas") }
-      let(:plage_ouverture) { build(:plage_ouverture, lieu: lieu) }
-
-      it { expect(plage_ouverture.payload[:address]).to eq("10 rue de là-bas") }
     end
   end
 end
