c93bf69dad3d0d99ee1e156e1e5b5d2a0d68ac9d
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-04-27 14:19:50 +0200

    Complete specs for password reset for user and agent

---
+++ spec/features/agents/agent_can_reset_his_password_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+describe "Agent resets his password spec" do
+  let!(:agent) { create(:agent) }
+
+  it "works by sending a reset email" do
+    visit new_agent_password_path
+    expect(page).to have_content("Mot de passe oublié ?")
+    expect(page).to have_link("Se connecter")
+
+    fill_in "agent_email", with: agent.email
+    expect { click_on "Envoyer" }.to change { emails_sent_to(agent.email).size }.by(1)
+
+    open_email(agent.email)
+    current_email.click_link("Changer")
+    expect(page).to have_content("Définir mon mot de passe")
+    fill_in "password", with: "correct horse battery staple"
+    expect { click_on "Enregistrer" }.to change { agent.reload.encrypted_password }
+    expect(page).to have_content("Votre mot de passe a été édité avec succès")
+    expect(page).to have_link("Mes organisations")
+  end
+end
---
+++ spec/features/users/user_can_reset_his_password_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+describe "User resets his password spec" do
+  let!(:user) { create(:user) }
+
+  it "works by sending a reset email" do
+    visit new_user_password_path
+    expect(page).to have_content("Mot de passe oublié ?")
+    expect(page).to have_link("Se connecter")
+
+    fill_in "user_email", with: user.email
+    expect { click_on "Envoyer" }.to change { emails_sent_to(user.email).size }.by(1)
+
+    open_email(user.email)
+    current_email.click_link("Changer")
+    expect(page).to have_content("Définir mon mot de passe")
+    fill_in "password", with: "correct horse battery staple"
+    expect { click_on "Enregistrer" }.to change { user.reload.encrypted_password }
+    expect(page).to have_content("Votre mot de passe a été édité avec succès")
+    expect(page.current_path).to eq("/users/rdvs")
+  end
+end
