fb72dfd61e8ada33bb2ad2694461528d96b57f12
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-05-03 17:47:37 +0200

    Add method to disable webhooks at the record level, then use it in the cron job

--- spec/jobs/cron_job/destroy_old_rdvs_job_spec.rb
+++ spec/jobs/cron_job/destroy_old_rdvs_job_spec.rb
@@ -11,4 +11,10 @@ describe CronJob::DestroyOldRdvsJob do
     expect(Rdv.all).to include(rdv_occurring_23_months_ago, rdv_occurring_11_months_ago)
     expect(Rdv.all).not_to include(rdv_occurring_25_months_ago)
   end
+
+  it "does not call any webhook" do
+    expect(WebhookJob).not_to receive(:perform_later)
+    travel_to(25.months.ago) { create(:rdv, starts_at: Time.zone.today.change(hour: 16)) }
+    described_class.new.perform
+  end
 end
