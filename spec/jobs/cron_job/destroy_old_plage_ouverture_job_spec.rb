909534398dab91a857307aa3dd976cb83dea87af
nathalie <nathalie@fitlab.fr>
2022-05-05 16:12:13 +0200

    suppression des plages d'ouvertures de plus d'un an

--- spec/jobs/cron_job/destroy_old_plage_ouverture_job_spec.rb
+++ spec/jobs/cron_job/destroy_old_plage_ouverture_job_spec.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 describe CronJob::DestroyOldPlageOuvertureJob do
-  it "Destroy exceptional po closed since 2 months" do
+  it "Destroy exceptional po closed since 2 years" do
     now = Time.zone.parse("20220405 10:00")
     travel_to(now)
-    po_exceptionelle_closed_since_2_months = create(:plage_ouverture, first_day: now - 2.months, recurrence: nil)
+    po_exceptionelle_closed_since_2_years = create(:plage_ouverture, first_day: now - 2.years, recurrence: nil)
 
     described_class.new.perform
 
-    expect(PlageOuverture.all).not_to include(po_exceptionelle_closed_since_2_months)
+    expect(PlageOuverture.all).not_to include(po_exceptionelle_closed_since_2_years)
   end
 
   it "keep exceptional po closed since 10 days" do
@@ -21,20 +21,20 @@ describe CronJob::DestroyOldPlageOuvertureJob do
     expect(PlageOuverture.all).to include(po_exceptionelle_closed_since_10_days)
   end
 
-  it "destroy recurrence po closed since 2 months" do
+  it "destroy recurrence po closed since 2 years" do
     now = Time.zone.parse("20220405 10:00")
     travel_to(now)
-    po_recurrence_closed_since_2_months = create(:plage_ouverture, first_day: now - 5.months, recurrence: Montrose.every(:week, starts: now - 5.months, until: now - 2.months))
+    po_recurrence_closed_since_2_years = create(:plage_ouverture, first_day: now - 3.years, recurrence: Montrose.every(:week, starts: now - 3.years, until: now - 2.years))
 
     described_class.new.perform
 
-    expect(PlageOuverture.all).not_to include(po_recurrence_closed_since_2_months)
+    expect(PlageOuverture.all).not_to include(po_recurrence_closed_since_2_years)
   end
 
   it "keep recurrence po closed since 10 days" do
     now = Time.zone.parse("20220405 10:00")
     travel_to(now)
-    po_recurrence_closed_since_10_days = create(:plage_ouverture, first_day: now - 5.months, recurrence: Montrose.every(:week, starts: now - 5.months, until: now - 10.days))
+    po_recurrence_closed_since_10_days = create(:plage_ouverture, first_day: now - 3.years, recurrence: Montrose.every(:week, starts: now - 3.years, until: now - 10.days))
 
     described_class.new.perform
 
@@ -44,7 +44,7 @@ describe CronJob::DestroyOldPlageOuvertureJob do
   it "keep recurrence po still open" do
     now = Time.zone.parse("20220405 10:00")
     travel_to(now)
-    po_recurrence_not_closed = create(:plage_ouverture, first_day: now - 5.months, recurrence: Montrose.every(:week, starts: now - 5.months, until: nil))
+    po_recurrence_not_closed = create(:plage_ouverture, first_day: now - 3.years, recurrence: Montrose.every(:week, starts: now - 3.years, until: nil))
 
     described_class.new.perform
 
