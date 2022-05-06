ce3bbb6d9c050459e59c9009f5cbe68781ec6053
Victor Mours <victor.mours@beta.gouv.fr>
2022-05-06 15:57:39 +0200

    Add regression test and fix for when next slot is after max booking delay

--- app/services/next_availability_service.rb
+++ app/services/next_availability_service.rb
@@ -5,12 +5,15 @@ class NextAvailabilityService
     available_creneau = nil
     from = from.to_datetime
 
-    from.step(from + 6.months, 7).find do |date|
+    max_search_date = [motif.end_booking_delay.to_datetime, from + 6.months].min
+
+    from.step(max_search_date, 7).find do |date|
       # NOTE: LOOP 2 loop here for ~ 27 weeks
       # We break out of the loop once we find a creneau.
-      #
 
-      creneaux = SlotBuilder.available_slots(motif, lieu, date..(date + 7.days), OffDays.all_in_date_range(date..(date + 7.days)), agents)
+      max_creneau_date = [motif.end_booking_delay.to_datetime, date + 7.days].min
+
+      creneaux = SlotBuilder.available_slots(motif, lieu, date..max_creneau_date, OffDays.all_in_date_range(date..max_creneau_date), agents)
       # NOTE: We build the whole list of creneaux of the week just to return the first one.
       available_creneau = creneaux.min_by(&:starts_at) if creneaux.any?
     end
