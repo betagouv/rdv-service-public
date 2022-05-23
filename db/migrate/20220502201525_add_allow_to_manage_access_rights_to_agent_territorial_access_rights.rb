2fe6795d8ebb4b1ff60066059377b60c9953830e
Yannick François <yannick.francois@beta.gouv.fr>
2022-05-23 18:27:38 +0200

    empêche la valeur null

--- db/migrate/20220502201525_add_allow_to_manage_access_rights_to_agent_territorial_access_rights.rb
+++ db/migrate/20220502201525_add_allow_to_manage_access_rights_to_agent_territorial_access_rights.rb
@@ -2,8 +2,8 @@
 
 class AddAllowToManageAccessRightsToAgentTerritorialAccessRights < ActiveRecord::Migration[6.1]
   def change
-    add_column :agent_territorial_access_rights, :allow_to_manage_access_rights, :boolean, default: false
-    add_column :agent_territorial_access_rights, :allow_to_invite_agents, :boolean, default: false
+    add_column :agent_territorial_access_rights, :allow_to_manage_access_rights, :boolean, default: false, null: false
+    add_column :agent_territorial_access_rights, :allow_to_invite_agents, :boolean, default: false, null: false
 
     # Tous les agents qui ont été créé depuis la dernière migration n'ont pas
     # été associés à un territoire via la table des droits d'accès
