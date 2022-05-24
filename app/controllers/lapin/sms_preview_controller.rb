12e20f008d91be1abaaf2c67a4ce0ec5341d270c
Nicolas Bouilleaud <nico@bou.io>
2022-05-24 17:24:06 +0200

    Better random test data

--- app/controllers/lapin/sms_preview_controller.rb
+++ app/controllers/lapin/sms_preview_controller.rb
@@ -18,12 +18,51 @@ module Lapin
       action_name = KNOWN_SMS_ACTIONS[klass].find { |action| action.to_s == params[:action_name] }
       return head :forbidden if action_name.nil?
 
-      user = User.joins(:rdvs).where.not(phone_number: nil).sample
-      rdv = user.rdvs.sample
-      token = "ABCTOKEN"
-
       @title = "#{klass}/#{action_name}"
-      @sms = klass.send(action_name, rdv, user, token)
+      data = test_data
+      @sms = klass.send(action_name, data[:rdv], data[:user], data[:token])
+    end
+
+    private
+
+    def test_data
+      user = OpenStruct.new(phone_number: "+33 6 39 98 12 34")
+      rdv = OpenStruct.new(starts_at: 10.days.from_now.at_noon)
+      rdv.motif = OpenStruct.new(service: OpenStruct.new(short_name: "Aide au logement"))
+      rdv.to_param = 999
+
+      show_relatives = [true, false].sample
+      if show_relatives
+        user1 = OpenStruct.new(full_name: "Irène Curie")
+        user2 = OpenStruct.new(full_name: "Ève Curie")
+
+        user.relatives = [user1, user2]
+        rdv.users = user.relatives
+      end
+
+      show_agent = [true, false].sample
+      if show_agent
+        rdv[:follow_up?] = true
+        rdv.agents = [OpenStruct.new(short_name: "James Bond")]
+      else
+        rdv.agents = []
+      end
+
+      location_type = %i[public_office phone home].sample
+      rdv.location_type = location_type
+      case location_type
+      when :home
+        rdv[:home?] = true
+      when :phone
+        rdv[:phone?] = true
+      when :public_office
+        rdv[:public_office?] = true
+        rdv.address_complete = "57 rue de Varenne, 75007 Paris"
+      end
+
+      rdv.phone_number = ["+33 1 99 00 12 34", nil].sample
+
+      { user: user, rdv: rdv, token: "ABCTOKEN" }
     end
   end
 end
