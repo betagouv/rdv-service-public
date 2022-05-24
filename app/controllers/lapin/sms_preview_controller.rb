0fbae2f451e1e3f6eca98e2ce29cb16f27bf624c
Nicolas Bouilleaud <nico@bou.io>
2022-05-24 17:24:06 +0200

    Parameterise SMS preview

--- app/controllers/lapin/sms_preview_controller.rb
+++ app/controllers/lapin/sms_preview_controller.rb
@@ -19,48 +19,55 @@ module Lapin
       return head :forbidden if action_name.nil?
 
       @title = "#{klass}/#{action_name}"
-      data = test_data
+      data = mock_data
       @sms = klass.send(action_name, data[:rdv], data[:user], data[:token])
     end
 
     private
 
-    def test_data
+    def mock_params
+      @mock_params ||= begin
+        params.permit(:show_relatives, :show_agent, :location_type, :phone_number)
+
+        params[:show_relatives] = params[:show_relatives].to_b
+        params[:show_agent] = params[:show_agent].to_b
+        params[:location_type] = "public_office" unless params[:location_type].in? Motif.location_types.keys
+        params[:phone_number] ||= "+33 1 99 00 12 34"
+
+        params
+      end
+    end
+
+    def mock_data
       user = OpenStruct.new(phone_number: "+33 6 39 98 12 34")
       rdv = OpenStruct.new(starts_at: 10.days.from_now.at_noon)
       rdv.motif = OpenStruct.new(service: OpenStruct.new(short_name: "Aide au logement"))
       rdv.to_param = 999
 
-      show_relatives = [true, false].sample
-      if show_relatives
-        user1 = OpenStruct.new(full_name: "Irène Curie")
-        user2 = OpenStruct.new(full_name: "Ève Curie")
-
-        user.relatives = [user1, user2]
+      if mock_params[:show_relatives]
+        user.relatives = [OpenStruct.new(full_name: "Anaïs Chaumont")]
         rdv.users = user.relatives
       end
 
-      show_agent = [true, false].sample
-      if show_agent
+      if mock_params[:show_agent]
         rdv[:follow_up?] = true
-        rdv.agents = [OpenStruct.new(short_name: "James Bond")]
+        rdv.agents = [OpenStruct.new(short_name: "Jean-François Lenormand")]
       else
         rdv.agents = []
       end
 
-      location_type = %i[public_office phone home].sample
-      rdv.location_type = location_type
-      case location_type
+      rdv.location_type = mock_params[:location_type].to_sym
+      case rdv.location_type
       when :home
         rdv[:home?] = true
       when :phone
         rdv[:phone?] = true
       when :public_office
         rdv[:public_office?] = true
-        rdv.address_complete = "57 rue de Varenne, 75007 Paris"
+        rdv.address_complete = "13 bis rue Marcel Pagnol, 53700 Villaines-la-Juhel"
       end
 
-      rdv.phone_number = ["+33 1 99 00 12 34", nil].sample
+      rdv.phone_number = mock_params[:phone_number]
 
       { user: user, rdv: rdv, token: "ABCTOKEN" }
     end
