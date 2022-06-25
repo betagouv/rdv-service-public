bdc9e9a8e5c30b7d47c114b8f9180a4700edd79b
Yannick François <yannick.francois@beta.gouv.fr>
2022-06-26 00:04:10 +0200

    Répare les liens CGU, mention légales et politique de confidentialité.

---
+++ spec/features/anybody/anybody_can_see_legal_pages_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+describe "Anybody can see legal pages" do
+  it "displays legal mention" do
+    visit root_path
+    expect(page).to have_content("Mentions Légales")
+    click_link "Mentions Légales"
+    expect(page).to have_selector("h1", text: "Mentions légales")
+  end
+
+  it "displays CGU" do
+    visit root_path
+    expect(page).to have_content("C.G.U.")
+    click_link "C.G.U."
+    expect(page).to have_selector("h1", text: "Conditions d’utilisation de la plateforme RDV-Solidarités")
+  end
+
+  it "displays privacy policy" do
+    visit root_path
+    expect(page).to have_content("Politique de confidentialité")
+    click_link "Politique de confidentialité"
+    expect(page).to have_selector("h1", text: "Politique de confidentialité")
+  end
+end
