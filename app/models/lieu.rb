7e3d5efd78a2efe0b3c145f4c501d04091723636
nathalie <nathalie@fitlab.fr>
2022-07-01 11:32:24 +0200

    commit en réponse au commentaire

--- app/models/lieu.rb
+++ app/models/lieu.rb
@@ -8,7 +8,8 @@ class Lieu < ApplicationRecord
   # Attributes
   auto_strip_attributes :name
   enum availability: { enabled: "enabled", disabled: "disabled", single_use: "single_use" }
-  # TODO: supprimer cet attribut :enabled
+
+  # TODO: supprimer cet attribut `:enabled` si bien lié au champ `old_enabled` (cf `schema.rb`)
   attribute :enabled, :boolean
 
   # Relations
