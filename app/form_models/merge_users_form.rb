fe217d99db0f6c49ee6029f536ce5c243db493a5
nathalie <nathalie@fitlab.fr>
2022-08-01 18:33:20 +0200

    WIP

--- app/form_models/merge_users_form.rb
+++ app/form_models/merge_users_form.rb
@@ -13,6 +13,7 @@ class MergeUsersForm
 
   validates_presence_of :user1, :user2
   validate :different_users?
+  validate :dont_destroy_franceconnected_values
 
   def initialize(organisation, **kwargs)
     @organisation = organisation
@@ -109,6 +110,12 @@ class MergeUsersForm
   end
 
   def different_users?
+    return true if user1.nil? || user2.nil?
+
+    errors.add(:base, "Mauvaise sélection d'usagers") if user1.id == user2.id
+  end
+
+  def dont_destroy_franceconnected_values
     return true if user1.nil? || user2.nil?
 
     errors.add(:base, "Mauvaise sélection d'usagers") if user1.id == user2.id
---
+++ spec/merge_users_form_spec.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+describe MergeUsersForm, type: :form do
+
+end
