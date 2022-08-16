0208a8c0ddbec902cb3b9a2cf42ec6bc4cebe1be
Victor Mours <victor.mours@beta.gouv.fr>
2022-08-16 17:27:19 +0200

    make notification rules clearer

--- app/services/notifiers/rdv_cancelled.rb
+++ app/services/notifiers/rdv_cancelled.rb
@@ -2,9 +2,7 @@
 
 class Notifiers::RdvCancelled < Notifiers::RdvBase
   def notify_user_by_mail(user)
-    # Only send sms for excused cancellations (not for no-show)
-    # if the rdv is collectif, there can still be cancellation notification if a participant is removed (regardless of the rdv status)
-    return unless @rdv.status.in?(%w[excused revoked]) || @rdv.collectif?
+    return unless notify_cancellation?
 
     user_mailer(user).rdv_cancelled.deliver_later
   end
@@ -12,12 +10,20 @@ class Notifiers::RdvCancelled < Notifiers::RdvBase
   def notify_user_by_sms(user)
     # Only send sms for excused cancellations by an Agent (not for no-show, not for self-cancellation)
     return unless @author.is_a? Agent
-    return unless @rdv.status.in?(%w[excused revoked]) || @rdv.collectif?
+    return unless notify_cancellation?
 
     Users::RdvSms.rdv_cancelled(@rdv, user, @rdv_users_tokens_by_user_id[user.id]).deliver_later
   end
 
   protected
+
+  def notify_cancellation?
+    # if the rdv is collectif, there can still be cancellation notification if a participant is removed (regardless of the rdv status)
+    return true if @rdv.collectif?
+
+    # Only send sms for excused cancellations (not for no-show)
+    @rdv.status.in?(%w[excused revoked])
+  end
 
   def rdvs_users_to_notify
     @rdv.rdvs_users.where(send_lifecycle_notifications: true)
