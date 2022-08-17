610f0a216faf28fe964124ccb7dc94b8122b2bac
Victor Mours <victor.mours@beta.gouv.fr>
2022-08-17 17:00:43 +0200

    Update the public facing site for the new domain
    (cherry picked from commit 4757e8fed6e5832a543e62146e38756859c282d2)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class StaticPagesController < ApplicationController
-  def mds; end
+  def mds
+    redirect_to root_path unless current_domain == Domain::RDV_SOLIDARITES
+  end
 
   def accessibility; end
 
   def contact; end
+
+  def domaines; end
+
+  def rdv_solidarites_presentation_for_agents
+    redirect_to agent_session_path unless current_domain == Domain::RDV_SOLIDARITES
+  end
 end
