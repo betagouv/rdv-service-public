ece22d5831d6a135239a495ba4b853951ea853d8
Yannick François <yannick.francois@beta.gouv.fr>
2022-09-03 18:05:26 +0200

    Automatise le test d'accessibilité du login agent

--- spec/features/accessibility/agents_pages_spec.rb
+++ spec/features/accessibility/agents_pages_spec.rb
@@ -1,6 +1,11 @@
 # frozen_string_literal: true
 
 describe "agents page", js: true do
+  it "login is accessible" do
+    path = new_agent_session_path
+    expect_page_to_be_axe_clean(path)
+  end
+
   it "agent agenda path page is accessible" do
     territory = create(:territory, departement_number: "75")
     organisation = create(:organisation, territory: territory)
