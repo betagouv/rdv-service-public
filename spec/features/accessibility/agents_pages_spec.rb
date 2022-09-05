3f299f8d6217920752052b796cf41fecbf733d77
Yannick François <yannick.francois@beta.gouv.fr>
2022-09-05 09:40:47 +0200

    automatise des tests d'accessibilité sur le coté agents

--- spec/features/accessibility/agents_pages_spec.rb
+++ spec/features/accessibility/agents_pages_spec.rb
@@ -6,13 +6,46 @@ describe "agents page", js: true do
     expect_page_to_be_axe_clean(path)
   end
 
-  it "agent agenda path page is accessible" do
+  it "agenda without event page is accessible" do
     territory = create(:territory, departement_number: "75")
     organisation = create(:organisation, territory: territory)
     agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
     login_as agent
 
     path = admin_organisation_agent_agenda_path(organisation, agent)
+    expect_page_to_be_axe_clean(path)
+  end
+
+  it "agenda with 3 rdvs is accessible" do
+    territory = create(:territory, departement_number: "75")
+    organisation = create(:organisation, territory: territory)
+    agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
+    create_list(:rdv, 3, agents: [agent])
+    login_as agent
+
+    path = admin_organisation_agent_agenda_path(organisation, agent)
+    expect_page_to_be_axe_clean(path)
+  end
+
+  it "admin organisation plage_ouvertures path is accessible" do
+    territory = create(:territory, departement_number: "75")
+    organisation = create(:organisation, territory: territory)
+    agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
+    create_list(:plage_ouverture, 3, agent: agent)
+    login_as agent
+
+    path = admin_organisation_agent_plage_ouvertures_path(organisation, agent)
+    expect_page_to_be_axe_clean(path)
+  end
+
+  xit "admin organisation users path is accessible" do
+    territory = create(:territory, departement_number: "75")
+    organisation = create(:organisation, territory: territory)
+    create_list(:user, 3, organisations: [organisation])
+    agent = create(:agent, email: "totoagent@example.com", basic_role_in_organisations: [organisation])
+    login_as agent
+
+    path = admin_organisation_users_path(organisation)
     expect_page_to_be_axe_clean(path)
   end
 
