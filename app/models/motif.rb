bf1e637e06ee30be6e7a401c464fa8ca7d3e51c9
Quentin Blanc <qblanc@hotmail.com>
2022-09-14 18:52:20 +0200

    feat(db): add enum values to motif_category for rsa

--- app/models/motif.rb
+++ app/models/motif.rb
@@ -29,7 +29,9 @@ class Motif < ApplicationRecord
   enum location_type: { public_office: "public_office", phone: "phone", home: "home" }
   enum category: { rsa_orientation: "rsa_orientation",
                    rsa_accompagnement: "rsa_accompagnement",
-                   rsa_orientation_on_phone_platform: "rsa_orientation_on_phone_platform", }
+                   rsa_orientation_on_phone_platform: "rsa_orientation_on_phone_platform",
+                   rsa_cer_signature: "rsa_cer_signature",
+                   rsa_insertion_offer: "rsa_insertion_offer", }
 
   # Relations
   belongs_to :organisation
