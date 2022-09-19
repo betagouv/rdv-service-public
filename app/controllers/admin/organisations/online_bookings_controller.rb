90ddf2bf2bfd38c47e13dfe151715da5c4d5e3c3
sebastiencarceles <sebastien.carceles@gmail.com>
2022-09-19 15:13:12 +0200

    manage with or without online bookable motifs

--- app/controllers/admin/organisations/online_bookings_controller.rb
+++ app/controllers/admin/organisations/online_bookings_controller.rb
@@ -6,6 +6,9 @@ class Admin::Organisations::OnlineBookingsController < AgentAuthController
 
   def show
     authorize(@organisation)
+    all_motifs = policy_scope(Motif).available_motifs_for_organisation_and_agent(current_organisation, current_agent)
+    @total_motifs_count = all_motifs.count
+    @motifs = all_motifs.reservable_online.includes(:organisation).includes(:service)
   end
 
   private
