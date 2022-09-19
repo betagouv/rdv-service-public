bf5f48ea180295d32d1440dbe0cd363c05af951a
sebastiencarceles <sebastien.carceles@gmail.com>
2022-09-19 15:13:12 +0200

    manage with or without online bookable motifs

--- spec/requests/admin/organisations/online_bookings_spec.rb
+++ spec/requests/admin/organisations/online_bookings_spec.rb
@@ -2,19 +2,36 @@
 
 RSpec.describe "Admin::Organisations::OnlineBookings", type: :request do
   let(:organisation) { create(:organisation) }
-  let(:service_cnfs) { create(:service, :conseiller_numerique) }
-  # TODO: SEB factory to create a conseiller numerique:
-  let(:agent) { create(:agent, admin_role_in_organisations: [organisation], service: service_cnfs) }
+  let(:agent) { create(:agent, :cnfs, admin_role_in_organisations: [organisation]) }
 
   before { sign_in agent }
 
   describe "GET /admin/organisations/:organisation_id/online_booking" do
     subject(:show_request) { get admin_organisation_online_booking_path(organisation) }
 
-    before { show_request }
+    it "is successful" do
+      show_request
+      expect(response).to be_successful
+    end
 
-    it { expect(response).to be_successful }
-    it { expect(response).to render_template(:show) }
+    it { expect(show_request).to render_template(:show) }
+
+    context "when there is at least one motif that can be booked online" do
+      let!(:motif) { create(:motif, organisation: organisation, service: agent.service, reservable_online: true) }
+
+      it "shows the motif" do
+        show_request
+        expect(response.body).to include("conserver au moins un motif ouvert à la réservation en ligne")
+        expect(response.body).to include(motif.name)
+      end
+    end
+
+    context "when there is no motifs that can be booked online" do
+      it "shows a message to create motifs" do
+        show_request
+        expect(response.body).to include("aucun n'est ouvert à la réservation en ligne")
+      end
+    end
 
     describe "edge cases" do
       context "when the current agent is not a conseiller numerique" do
