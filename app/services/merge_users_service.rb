b69cc36a6e5bcc6cb792d8ef4afc61064f894ae0
nathalie <nathalie@fitlab.fr>
2022-09-26 15:55:44 +0200

    conserve les attributs franceconnect lors de la fusion d'usagers

--- app/services/merge_users_service.rb
+++ app/services/merge_users_service.rb
@@ -26,6 +26,10 @@ class MergeUsersService < BaseService
     user_attributes_to_merge.each do |attribute|
       @user_target.send("#{attribute}=", @user_to_merge.send(attribute))
     end
+    if @user_to_merge.logged_once_with_franceconnect?
+      @user_target.logged_once_with_franceconnect = true
+      @user_target.franceconnect_openid_sub = @user_to_merge.franceconnect_openid_sub
+    end
     @user_target.save!
   end
 
