4963c46a8e3d40c796e9471c1947de390acc8822
Quentin Blanc <qblanc@hotmail.com>
2022-09-27 18:11:31 +0200

    add rsa follow up motif category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_20_123327) do
+ActiveRecord::Schema.define(version: 2022_09_27_160954) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2022_09_20_123327) do
     "rsa_orientation_on_phone_platform",
     "rsa_cer_signature",
     "rsa_insertion_offer",
+    "rsa_follow_up",
   ], force: :cascade
 
   create_enum :rdv_status, [
