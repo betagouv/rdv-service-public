72353b8f62ecff499497ef02c4ea2acc9ae9008e
sebastiencarceles <sebastien.carceles@gmail.com>
2022-09-29 14:25:47 +0200

    don't show the booking link before setup is done

--- app/controllers/admin/organisations/online_bookings_controller.rb
+++ app/controllers/admin/organisations/online_bookings_controller.rb
@@ -31,6 +31,8 @@ class Admin::Organisations::OnlineBookingsController < AgentAuthController
   end
 
   def booking_link
+    return [root_url, "..."].join unless shareable_booking_link?
+
     if current_organisation.external_id.present?
       public_link_to_external_org_url(current_organisation.territory.departement_number, current_organisation.external_id)
     else
