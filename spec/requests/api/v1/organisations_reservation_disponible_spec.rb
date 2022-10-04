947795405e82cc6f82b6484dd253e3643f93a94c
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-10-04 17:55:09 +0200

    Update spec to new response

--- spec/requests/api/v1/organisations_reservation_disponible_spec.rb
+++ spec/requests/api/v1/organisations_reservation_disponible_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-describe "api/v1/organisations/reservation_disponible requests", type: :request do
+describe "public_api/public_links requests", type: :request do
   let!(:territory) { create(:territory, departement_number: "CN") }
   let!(:organisation_a) { create(:organisation, new_domain_beta: true, external_id: "ext_id_A", territory: territory) }
   let!(:organisation_b) { create(:organisation, new_domain_beta: true, external_id: "ext_id_B", territory: territory) }
@@ -11,7 +11,7 @@ describe "api/v1/organisations/reservation_disponible requests", type: :request 
   context "when plages are defined" do
     let(:params) do
       {
-        external_ids: %w[ext_id_A ext_id_B ext_id_C ext_id_D ext_id_E],
+        external_ids: %w[ext_id_A ext_id_B ext_id_C ext_id_D ext_id_E, ext_id_F],
         departement: "CN",
       }
     end
@@ -27,30 +27,12 @@ describe "api/v1/organisations/reservation_disponible requests", type: :request 
       # Organisation B has a plage in 5 days
       # Organisation C has a plage that expired
       # Organisation D has no plage
-      # Organisation E is not in provided territory, it should be missing from response
-      # Organisation F does not exist, it should be missing from response
-      expected_response = [
-        {
-          "organisation_external_id" => "ext_id_A",
-          "reservation_disponible" => true,
-          "public_url" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_a.id}",
-        },
-        {
-          "organisation_external_id" => "ext_id_B",
-          "reservation_disponible" => true,
-          "public_url" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_b.id}",
-        },
-        {
-          "organisation_external_id" => "ext_id_C",
-          "reservation_disponible" => false,
-          "public_url" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_c.id}",
-        },
-        {
-          "organisation_external_id" => "ext_id_D",
-          "reservation_disponible" => false,
-          "public_url" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_d.id}",
-        },
-      ]
+      # Organisation E is not in provided territory
+      # Organisation F does not exist
+      expected_response = {
+        "ext_id_A" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_a.id}",
+        "ext_id_B" => "http://www.rdv-aide-numerique-test.localhost/org/#{organisation_b.id}",
+      }
       expect(JSON.parse(response.body)).to match_array(expected_response)
     end
   end
