38739d08014a8c6c290f54dc457fdca1fbe09337
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-10-06 15:29:41 +0200

    Remove temporary fix (see #2779)
    (cherry picked from commit 9066a33a584769c7e8440444e39e3200a09aa578)

--- spec/support/sentry_helper.rb
+++ spec/support/sentry_helper.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 # Sentry test helper setup, see:
-# https://github.com/getsentry/sentry-ruby/blob/29f5875/sentry-ruby/lib/sentry/test_helper.rb
+# https://github.com/getsentry/sentry-ruby/blob/5fc11d9/sentry-ruby/lib/sentry/test_helper.rb
 def stub_sentry_events
   around do |example|
     setup_sentry_test
     example.run
     teardown_sentry_test
-    Sentry.get_current_scope.clear_breadcrumbs
   end
 end
