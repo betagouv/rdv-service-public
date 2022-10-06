fa767307ebe295a4697df5e2535365ee5a62bf19
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-10-06 16:05:10 +0200

    Cache response

--- app/controllers/public_api/public_links_controller.rb
+++ app/controllers/public_api/public_links_controller.rb
@@ -6,6 +6,17 @@ class PublicApi::PublicLinksController < ActionController::Base # rubocop:disabl
 
     territory = Territory.find_by!(departement_number: departement)
 
+    # Using cache to prevent overloading db in case of accidentally intensive API calls
+    response_body = Rails.cache.fetch("public_api/public_links/#{territory.id}", expires_in: 1.minute) do
+      { public_links: public_links_for(territory) }.to_json
+    end
+
+    render json: response_body
+  end
+
+  private
+
+  def public_links_for(territory)
     plage_ouvertures = PlageOuverture.where(organisations: { territory_id: territory.id })
       .not_expired
       .in_range((Time.zone.now..))
@@ -13,13 +24,11 @@ class PublicApi::PublicLinksController < ActionController::Base # rubocop:disabl
       .joins(:organisation)
       .distinct(:organisation_id)
 
-    results = plage_ouvertures.map do |plage_ouverture|
+    plage_ouvertures.map do |plage_ouverture|
       {
         external_id: plage_ouverture.organisation.external_id,
         public_link: public_link_to_org_url(organisation_id: plage_ouverture.organisation.id, host: plage_ouverture.organisation.domain.dns_domain_name),
       }
     end
-
-    render json: { public_links: results }.to_json
   end
 end
