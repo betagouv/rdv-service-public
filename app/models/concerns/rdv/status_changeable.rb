7f0b159f14bdafc4f542e58a208136085f9725a8
Romain Neuville <neuvilleromain@gmail.com>
2022-10-10 16:53:33 +0200

    status_changeable for rdv and rdv_users

--- app/models/concerns/rdv/status_changeable.rb
+++ app/models/concerns/rdv/status_changeable.rb
@@ -6,28 +6,52 @@ module Rdv::StatusChangeable
   def change_status(author, status)
     Rdv.transaction do
       # Consequences on participations with RDV.status changes :
-      if update(status:)
+      if update(status: status)
         case status
         when "unknown"
           # Setting to unknown means resetting the rdv status by agents and reset ALL participants statuses
-          # All participants get a creation notification (like a new rdv)
-          rdvs_users.map { _1.change_status(author, status) }
-        # Others participations changes are not applied on already revoked or excused participants (not_cancelled)
+          notify!(author)
+          rdvs_users.update(status: status)
         when "excused"
-          # Collective rdv cannot be globally excused
-          # On non collectives rdv all paricipants are excused
-          rdvs_users.not_cancelled.map { _1.change_status(author, status) unless collectif? }
+          # Collective rdv cannot be globally excused (todordvc test)
+          return if collectif?
+
+          # On non collectives rdv all participants are excused
+          notify!(author)
+          rdvs_users.not_cancelled.update(status: status)
         when "revoked"
-          # When rdv status is revoked, all participants are revoked (and get a cancellation notification)
-          rdvs_users.not_cancelled.map { _1.change_status(author, status) }
+          # When rdv status is revoked, all participants are revoked
+          notify!(author)
+          rdvs_users.not_cancelled.update(status: status)
         when "seen", "noswhow"
           # When rdv status is seen or noshow, all unknown statuses are changed
-          rdvs_users.not_cancelled.where(status: "unknown").map { _1.change_status(author, status) }
+          rdvs_users.not_cancelled.where(status: "unknown").update(status: status)
         end
         true
       else
         false
       end
     end
+  end
+
+  def notify!(author)
+    if rdv_cancelled?
+      @rdv_users_tokens_by_user_id = Notifiers::RdvCancelled.perform_with(self, author)
+    end
+    if rdv_status_reloaded_from_cancelled?
+      @rdv_users_tokens_by_user_id = Notifiers::RdvCreated.perform_with(self, author)
+    end
+  end
+
+  def rdv_cancelled?
+    previous_changes["status"]&.last.in? %w[excused revoked]
+  end
+
+  def rdv_status_reloaded_from_cancelled?
+    status_previously_was.in?(%w[revoked excused]) && status == "unknown"
+  end
+
+  def rdv_user_token(user_id)
+    @rdv_users_tokens_by_user_id&.fetch(user_id, nil)
   end
 end
--- app/models/concerns/rdvs_user/status_changeable.rb
+++ app/models/concerns/rdvs_user/status_changeable.rb
@@ -7,7 +7,7 @@ module RdvsUser::StatusChangeable
     return if self.status == status
 
     RdvsUser.transaction do
-      if update(status:)
+      if update(status: status)
         notify!(author)
         true
       else
@@ -17,9 +17,23 @@ module RdvsUser::StatusChangeable
   end
 
   def notify!(author)
-    if status == "excused"
+    if rdv_cancelled? && user_valid_for_lifecycle_notifications?
       Notifiers::RdvCancelled.perform_with(rdv, author, [user])
     end
-    # TODO cases...
+    if rdv_status_reloaded_from_cancelled? && user_valid_for_lifecycle_notifications?
+      Notifiers::RdvCreated.perform_with(rdv, author, [user])
+    end
+  end
+
+  def rdv_cancelled?
+    previous_changes["status"]&.last.in? %w[excused revoked]
+  end
+
+  def rdv_status_reloaded_from_cancelled?
+    status_previously_was.in?(%w[revoked excused]) && status == "unknown"
+  end
+
+  def user_valid_for_lifecycle_notifications?
+    send_lifecycle_notifications == true
   end
 end
