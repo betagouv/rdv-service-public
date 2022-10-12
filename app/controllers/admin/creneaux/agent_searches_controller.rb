fed7b0e6173cb78de4c9224765affe3661a78d65
sebastiencarceles <sebastien.carceles@gmail.com>
2022-10-12 09:30:30 +0200

    review: use before actions

--- app/controllers/admin/creneaux/agent_searches_controller.rb
+++ app/controllers/admin/creneaux/agent_searches_controller.rb
@@ -2,12 +2,11 @@
 
 class Admin::Creneaux::AgentSearchesController < AgentAuthController
   respond_to :html, :js
+  before_action :set_form
+  before_action :set_search_results
+  before_action :set_search_results_without_lieu
 
   def index
-    @form = helpers.build_agent_creneaux_search_form(current_organisation, params)
-    @search_results = search_results
-    @search_results_without_lieu = search_results_without_lieu
-
     if only_one_lieu? && !creneaux_without_lieu?
       skip_policy_scope # TODO: improve pundit checks for creneaux
 
@@ -30,22 +29,26 @@ class Admin::Creneaux::AgentSearchesController < AgentAuthController
 
   private
 
-  def search_results
-    return nil unless (params[:commit].present? || request.format.js?) && @form.valid?
-
-    if @form.motif.individuel?
-      SearchCreneauxForAgentsService.perform_with(@form)
-    else
-      SearchRdvCollectifForAgentsService.new(@form).lieu_search
-    end
+  def set_form
+    @form = helpers.build_agent_creneaux_search_form(current_organisation, params)
   end
 
-  def search_results_without_lieu
-    return nil unless (params[:commit].present? || request.format.js?) && @form.valid?
-    return nil unless @form.motif&.phone?
+  def set_search_results
+    return unless (params[:commit].present? || request.format.js?) && @form.valid?
+
+    @search_results = if @form.motif.individuel?
+                        SearchCreneauxForAgentsService.perform_with(@form)
+                      else
+                        SearchRdvCollectifForAgentsService.new(@form).lieu_search
+                      end
+  end
+
+  def set_search_results_without_lieu
+    return unless (params[:commit].present? || request.format.js?) && @form.valid?
+    return unless @form.motif&.phone?
 
     # Les motifs par téléphone sont nécessairement individuels
-    SearchCreneauxWithoutLieuForAgentsService.perform_with(@form)
+    @search_results_without_lieu = SearchCreneauxWithoutLieuForAgentsService.perform_with(@form)
   end
 
   def only_one_lieu?
