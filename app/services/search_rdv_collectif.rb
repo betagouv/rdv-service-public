e8898307ce469678e6f196ddfde52738c7a1e4ed
Yannick François <yannick.francois@beta.gouv.fr>
2022-10-17 22:32:51 +0200

    affiche les RDV collectifs coté publique

---
+++ app/services/search_rdv_collectif.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module SearchRdvCollectif
+  class << self
+    def next_availability_for_lieu(motif, lieu, user = nil)
+      next_availability = rdvs_collectif_at(motif, lieu, user).first
+      OpenStruct.new(lieu: lieu, next_availability: next_availability)
+    end
+
+    def starts_at_for_first(rdvs)
+      return nil if rdvs.empty?
+
+      rdvs.first.starts_at
+    end
+
+    def rdvs_collectif_at(motif, lieu, user = nil)
+      collective_rdvs = Rdv.where(lieu: lieu).where(motif: motif).collectif.future.joins(:motif).where("motifs.reservable_online = true").with_remaining_seats.order("starts_at asc")
+      collective_rdvs = collective_rdvs.reject { |c| c.rdvs_users.map(&:id).include?(user.id) } if user.present?
+      collective_rdvs
+    end
+
+    def available_slots(motif, lieu)
+      Rdv.collectif.future.with_remaining_seats.where(motif_id: motif.id).where(lieu_id: lieu.id)
+    end
+    alias creneaux available_slots
+  end
+end
