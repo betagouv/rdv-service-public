acb3ea9bbb0426e71fe488391a006110ea4b4e15
Yannick François <yannick.francois@beta.gouv.fr>
2022-10-20 00:17:58 +0200

    Utilise la participation, sans le `register`

--- app/controllers/users/participants_controller.rb
+++ app/controllers/users/participants_controller.rb
@@ -7,16 +7,26 @@ class Users::ParticipantsController < UserAuthController
 
   def create
     rdv = Rdv.find(params[:rdv_id])
-    authorize(rdv)
-    user = User.find(params[:user_id])
+    user = User.find(params[:user_id]) if params[:user_id].present?
+    user ||= current_user
+    # C'est plus une mise à jour de la participation que vraiment une création
+    # d'un point de vue des autorisations
+    authorize(user, :update?)
+
+    if rdv.users.include?(user.id)
+      flash[:notice] = "Usager déjà inscrit pour cet atelier."
+      redirect_to users_rdv_path(rdv) and return
+    end
+
     if rdv.motif.collectif?
-      rdv.users = rdv.users - user.responsible.self_and_relatives_and_responsible
+      rdv.users = rdv.users - current_user.self_and_relatives
       rdv.users << user
     else
       rdv.users = [user]
     end
     rdv.save
-    flash[:notice] = "L'usager #{user.full_name} a été selectionné pour le RDV"
+ 
+    flash[:notice] = "Inscription confirmée"
     redirect_to users_rdv_path(rdv)
   end
 end
