a54899589750b9a35119d1ef6a9549526b2f2f1b
sebastiencarceles <sebastien.carceles@gmail.com>
2022-10-25 14:07:18 +0200

    small refactoring to normalize not_found

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -1,15 +1,21 @@
 # frozen_string_literal: true
 
 class Api::V1::InvitationsController < Api::V1::AgentAuthBaseController
+  before_action :set_user
+
   def show
+    authorize(@user)
+    render_record(@user)
+  end
+
+  private
+
+  def set_user
     # rubocop:disable Rails/DynamicFindBy
     # find_by_invitation_token is a method added by the devise_invitable gem
-    user = User.find_by_invitation_token(params[:token], true)
+    @user = User.find_by_invitation_token(params[:token], true)
     # rubocop:enable Rails/DynamicFindBy
 
-    raise ActiveRecord::RecordNotFound unless user
-
-    authorize(user)
-    render_record(user)
+    render_error :not_found, not_found: :user unless @user
   end
 end
