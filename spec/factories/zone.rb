399d3b9f8707e68e7b55d9f968af50e201618163
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-10-27 13:42:20 +0200

    Implement factory spec

--- spec/factories/zone.rb
+++ spec/factories/zone.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  sequence(:city_code) { |n| (62_001 + n).to_s }
-
   factory :zone do
     sector
 
     level { Zone::LEVEL_CITY }
-    city_code { generate(:city_code) }
+    city_code do
+      "#{sector.territory.departement_number}000"
+    end
     city_name { "ARQUES" }
 
     trait :level_street do
