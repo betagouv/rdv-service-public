777c3ec5245e169d2c735667b0deb285a811e549
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-03 10:50:42 +0100

    wip: ajout de la deuxieme page

--- app/controllers/prescripteurs_controller.rb
+++ app/controllers/prescripteurs_controller.rb
@@ -1,7 +1,26 @@
 # frozen_string_literal: true
 
 class PrescripteursController < ApplicationController
-  include CanHaveRdvWizardContext
+  def sign_in
+    parsed_uri = URI.parse(session[:user_return_to])
+    parsed_params = Rack::Utils.parse_nested_query(parsed_uri.query).to_h.symbolize_keys
+    session[:rdv_wizard_params] = parsed_params
+    @rdv_wizard = UserRdvWizard::Step1.new(nil, session[:rdv_wizard_params])
+  end
 
-  def sign_in; end
+  def new_user
+    @user_form = user_form_object
+    @rdv_wizard = UserRdvWizard::Step1.new(nil, session[:rdv_wizard_params])
+    @prescripteur_info = { full_name: "Alex Prescripteur" }
+  end
+
+  private
+
+  def user_form_object
+    Admin::UserForm.new(
+      User.new,
+      ignore_benign_errors: params.dig(:user, :ignore_benign_errors),
+      view_locals: { current_organisation: nil }
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -215,6 +215,7 @@ Rails.application.routes.draw do
 
   namespace :prescripteurs do
     get "sign_in"
+    get "new_user"
   end
 
   %w[contact mds accessibility mentions_legales cgu politique_de_confidentialite domaines health_check].each do |page_name|
