3119da8fde6d11411847c78bd42908f4ee53a509
aminedhobb <amine.dhobb@gmail.com>
2022-11-08 19:43:40 +0100

    feat(migration): Add rsa_main_tendue motif category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_20_152222) do
+ActiveRecord::Schema.define(version: 2022_11_08_181921) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 2022_10_20_152222) do
     "rsa_follow_up",
     "rsa_accompagnement_social",
     "rsa_accompagnement_sociopro",
+    "rsa_main_tendue",
   ], force: :cascade
 
   create_enum :rdv_status, [
