7eed2ff0be957f7ffa74108f6b0ef4ca70f6e1d1
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-08 18:19:58 +0100

    Fix for search context

--- app/helpers/search_context_helper.rb
+++ app/helpers/search_context_helper.rb
@@ -1,33 +1,31 @@
 # frozen_string_literal: true
 
 module SearchContextHelper
-  def path_to_motif_selection(params)
-    root_path(
-      departement: params[:departement],
-      city_code: params[:city_code],
-      longitude: params[:longitude],
-      latitude: params[:latitude],
-      street_ban_id: params[:street_ban_id],
-      address: params[:address],
-      service_id: params[:service_id]
-    )
+  def path_to_motif_selection(_params)
+    root_path(common_params(params))
   end
 
   def path_to_lieu_selection(params)
     root_path(
-      departement: params[:departement],
-      city_code: params[:city_code],
-      longitude: params[:longitude],
-      latitude: params[:latitude],
-      street_ban_id: params[:street_ban_id],
-      address: params[:address],
-      service_id: params[:service_id],
-      motif_name_with_location_type: params[:motif_name_with_location_type]
+      common_params(params).merge(
+        motif_name_with_location_type: params[:motif_name_with_location_type]
+      )
     )
   end
 
   def path_to_creneau_selection(params)
     root_path(
+      common_params(params).merge(
+        motif_name_with_location_type: params[:motif_name_with_location_type],
+        lieu_id: params[:lieu_id]
+      )
+    )
+  end
+
+  private
+
+  def common_params(params)
+    {
       departement: params[:departement],
       city_code: params[:city_code],
       longitude: params[:longitude],
@@ -35,8 +33,7 @@ module SearchContextHelper
       street_ban_id: params[:street_ban_id],
       address: params[:address],
       service_id: params[:service_id],
-      motif_name_with_location_type: params[:motif_name_with_location_type],
-      lieu_id: params[:lieu_id]
-    )
+      organisation_id: params[:organisation_id],
+    }
   end
 end
