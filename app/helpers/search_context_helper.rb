3dc3997b76886653fdfec75a71055d976378ea24
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-09 16:07:51 +0100

    refactor SearchContext helper

--- app/helpers/search_context_helper.rb
+++ app/helpers/search_context_helper.rb
@@ -2,38 +2,31 @@
 
 module SearchContextHelper
   def path_to_motif_selection(params)
-    root_path(common_params(params))
+    root_path(params.slice(motif_selection_params))
   end
 
   def path_to_lieu_selection(params)
-    root_path(
-      common_params(params).merge(
-        motif_name_with_location_type: params[:motif_name_with_location_type]
-      )
-    )
+    attributes = motif_selection_params + [:motif_name_with_location_type]
+    root_path(params.slice(*attributes))
   end
 
   def path_to_creneau_selection(params)
-    root_path(
-      common_params(params).merge(
-        motif_name_with_location_type: params[:motif_name_with_location_type],
-        lieu_id: params[:lieu_id]
-      )
-    )
+    attributes = motif_selection_params + %i[motif_name_with_location_type lieu_id]
+    root_path(params.slice(*attributes))
   end
 
   private
 
-  def common_params(params)
-    {
-      departement: params[:departement],
-      city_code: params[:city_code],
-      longitude: params[:longitude],
-      latitude: params[:latitude],
-      street_ban_id: params[:street_ban_id],
-      address: params[:address],
-      service_id: params[:service_id],
-      organisation_id: params[:organisation_id],
-    }
+  def motif_selection_params
+    %i[
+      departement
+      city_code
+      longitude
+      latitude
+      street_ban_id
+      address
+      service_id
+      organisation_id
+    ]
   end
 end
