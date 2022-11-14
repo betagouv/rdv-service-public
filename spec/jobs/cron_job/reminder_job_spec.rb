0d2913fbe24d687521d7f2fbf2ee0ba9d281b9c9
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-11-14 17:31:01 +0100

    Don't let exception interrupt loop

--- spec/jobs/cron_job/reminder_job_spec.rb
+++ spec/jobs/cron_job/reminder_job_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe CronJob::ReminderJob, type: :job do
-  subject { described_class.perform_now }
+  subject(:perform_now) { described_class.perform_now }
 
   let(:now) { Time.zone.parse("01-01-2019 09:00") }
 
@@ -15,7 +15,7 @@ RSpec.describe CronJob::ReminderJob, type: :job do
 
     it "calls notification service" do
       expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv1, nil)
-      subject
+      perform_now
     end
   end
 
@@ -28,7 +28,7 @@ RSpec.describe CronJob::ReminderJob, type: :job do
       expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv1, nil)
       expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv2, nil)
       expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv3, nil)
-      subject
+      perform_now
     end
   end
 
@@ -37,7 +37,29 @@ RSpec.describe CronJob::ReminderJob, type: :job do
 
     it "calls notification service" do
       expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with)
-      subject
+      perform_now
+    end
+  end
+
+  describe "error handling" do
+    let!(:rdv1) { create(:rdv, starts_at: 2.days.from_now) }
+    let!(:rdv2) { create(:rdv, starts_at: 2.days.from_now) }
+    let!(:rdv3) { create(:rdv, starts_at: 2.days.from_now) }
+
+    stub_sentry_events
+
+    it "does not stop on first error" do
+      # Something unexpected happens when processing rdv2
+      allow(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv2, nil).and_raise("woopsie")
+
+      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv1, nil)
+      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv2, nil)
+      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv3, nil)
+
+      perform_now
+
+      # Exception raised by rdv_2 is sent to Sentry
+      expect(sentry_events.last.exception.values.last.value).to eq("woopsie")
     end
   end
 end
