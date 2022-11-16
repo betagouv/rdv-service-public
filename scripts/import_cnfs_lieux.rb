8e3f575a9cfb847e206c0a2a1a6b39885acb1478
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-16 12:27:58 +0100

    wip

--- scripts/import_cnfs_lieux.rb
+++ scripts/import_cnfs_lieux.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+ActiveRecord::Base.logger = nil
 # permanences = JSON.load_file("/tmp/uploads/permanences.json")
 permanences = JSON.load_file("tmp/permanences.json")
 
@@ -7,7 +8,17 @@ permanences.each do |permanence|
   organisation = Organisation.find_by(external_id: permanence["structureId"])
   next unless organisation
 
-  puts "Pour #{organisation.name}"
+  # Ne pas importer des nouveaux lieux si ils ont déjà été configurés manuellement
+  next if organisation.lieux.count >= 2
+
+  matching_lieu = organisation.lieux.find do |lieu|
+    lieu.address.downcase.gsub(/[^0-9a-z]/, "").start_with?(permanence["adresse"].downcase.gsub(/[^0-9a-z]/, ""))
+  end
+
+  # Ne pas importer le lieu de permanence s'il existe déjà
+  next if matching_lieu
+
+  puts "Nouveau lieu potentiel pour #{organisation.name} :"
   puts "adresse de permanence #{permanence['adresse']}"
   puts "adresses de lieux #{organisation.lieux.map(&:address)}"
 end
