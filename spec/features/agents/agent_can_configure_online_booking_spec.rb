840cfd6889267c836b22a507f8aa5c6d068d52b7
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-23 18:29:15 +0100

    refactor specs

--- spec/features/agents/agent_can_configure_online_booking_spec.rb
+++ spec/features/agents/agent_can_configure_online_booking_spec.rb
@@ -4,17 +4,28 @@ describe "CNFS agent can configure online booking" do
   let!(:organisation) { create(:organisation) }
   let!(:agent) { create(:agent, :cnfs, admin_role_in_organisations: [organisation]) }
 
-  before { login_as(agent, scope: :agent) }
+  before do
+    login_as(agent, scope: :agent)
 
-  context "when there is a bookable motif and plages d'ouverture" do
-    let!(:motif) { create(:motif, organisation: organisation, service: agent.service, reservable_online: true) }
+    create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif individuel fermé sans plages d'ouverture")
+    create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif individuel ouvert sans plages d'ouverture")
 
-    before { motif.plage_ouvertures << create(:plage_ouverture, organisation: organisation, agent: agent) }
+    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif individuel fermé avec plages d'ouverture")
+    create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
 
-    it "can copy the booking link" do
-      visit admin_organisation_online_booking_path(organisation)
-      expect(page).not_to have_css('[data-clipboard-target="copy-button"][disabled]')
-      expect(page).to have_css('[data-clipboard-target="copy-button"]')
-    end
+    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif individuel ouvert avec plages d'ouverture")
+    create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
+
+    create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif collectif fermé sans créneaux")
+
+    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif collectif ouvert avec créneaux")
+    create(:rdv, motif: motif, max_participants_count: 5)
+  end
+
+  it "displays the information of the various motifs" do
+    visit admin_organisation_online_booking_path(organisation)
+    expect(page).to have_css('[data-clipboard-target="copy-button"]')
+    expect(page).to have_content("adsfasdf") # TODO
+    # vérifier que les liens de petits cta vont sur les bonnes pages préremplies correctement
   end
 end
