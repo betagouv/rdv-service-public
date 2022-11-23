039c275dadf58ab15743d81fda17b1aa537d5b20
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-23 18:05:04 +0100

    link is always used

--- spec/features/agents/agent_can_configure_online_booking_spec.rb
+++ spec/features/agents/agent_can_configure_online_booking_spec.rb
@@ -6,35 +6,15 @@ describe "CNFS agent can configure online booking" do
 
   before { login_as(agent, scope: :agent) }
 
-  context "when there is no bookable motifs" do
-    it "goes to the right apge but can't copy the booking link", js: true do
-      visit authenticated_agent_root_path
-      click_link "Paramètres"
-      click_link "Réservation en ligne"
-
-      expect(page).to have_current_path(admin_organisation_online_booking_path(organisation))
-      expect(page).to have_css('[data-clipboard-target="copy-button"][disabled]')
-    end
-  end
-
-  context "when there is a bookable motif" do
+  context "when there is a bookable motif and plages d'ouverture" do
     let!(:motif) { create(:motif, organisation: organisation, service: agent.service, reservable_online: true) }
 
-    context "when there is no related plage d'ouverture" do
-      it "can't copy the booking link" do
-        visit admin_organisation_online_booking_path(organisation)
-        expect(page).to have_css('[data-clipboard-target="copy-button"][disabled]')
-      end
-    end
+    before { motif.plage_ouvertures << create(:plage_ouverture, organisation: organisation, agent: agent) }
 
-    context "when there is a related plage d'ouverture" do
-      before { motif.plage_ouvertures << create(:plage_ouverture, organisation: organisation, agent: agent) }
-
-      it "can copy the booking link" do
-        visit admin_organisation_online_booking_path(organisation)
-        expect(page).not_to have_css('[data-clipboard-target="copy-button"][disabled]')
-        expect(page).to have_css('[data-clipboard-target="copy-button"]')
-      end
+    it "can copy the booking link" do
+      visit admin_organisation_online_booking_path(organisation)
+      expect(page).not_to have_css('[data-clipboard-target="copy-button"][disabled]')
+      expect(page).to have_css('[data-clipboard-target="copy-button"]')
     end
   end
 end
