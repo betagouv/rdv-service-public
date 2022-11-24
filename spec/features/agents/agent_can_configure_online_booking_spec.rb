97f7804fe902f350dfea3071d7b3b0b5c52266f0
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-24 17:15:39 +0100

    minor fixes during review

--- spec/features/agents/agent_can_configure_online_booking_spec.rb
+++ spec/features/agents/agent_can_configure_online_booking_spec.rb
@@ -13,26 +13,33 @@ describe "Agents can configure online booking" do
       login_as(agent, scope: :agent)
       visit admin_organisation_online_booking_path(organisation)
 
+      # On a 3 croix qui indiquent respectivement :
+      # - le motif n'est pas réservable en ligne
+      # - c'est parce que l'option n'a pas été activée via le formulaire du motif
+      # - il n'y a pas de plages d'ouverture
       expect(page).to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red", count: 3)
 
       expect(page).to have_content("Ce motif n'est pas réservable en ligne")
       expect(page).to have_link("modifier")
       expect(page).to have_content("Pas de plages d'ouverture")
-      expect(page).to have_link("ajouter")
 
       motif.update!(reservable_online: true)
 
       visit admin_organisation_online_booking_path(organisation)
+      expect(page).to have_content("Réservable en ligne")
+
       expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 1)
       expect(page).to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red", count: 2)
-      expect(page).to have_content("Réservable en ligne")
+
+      click_link("ajouter")
+      expect(page).to have_checked_field(motif.name)
 
       create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
 
       visit admin_organisation_online_booking_path(organisation)
-      expect(page).to have_content("1 plage d'ouverture")
       expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 3)
       expect(page).not_to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red")
+      expect(page).to have_content("1 plage d'ouverture")
     end
   end
 
