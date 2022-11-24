b30814641317ac92d1cb2881f638df515cb4d555
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-24 16:35:35 +0100

    add spec

--- spec/features/agents/agent_can_configure_online_booking_spec.rb
+++ spec/features/agents/agent_can_configure_online_booking_spec.rb
@@ -1,31 +1,61 @@
 # frozen_string_literal: true
 
-describe "CNFS agent can configure online booking" do
+describe "Agents can configure online booking" do
   let!(:organisation) { create(:organisation) }
   let!(:agent) { create(:agent, :cnfs, admin_role_in_organisations: [organisation]) }
 
-  before do
-    login_as(agent, scope: :agent)
+  context "motif individuel" do
+    let!(:motif) do
+      create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif individuel")
+    end
 
-    create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif individuel fermé sans plages d'ouverture")
-    create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif individuel ouvert sans plages d'ouverture")
+    it "displays the motif's status" do
+      login_as(agent, scope: :agent)
+      visit admin_organisation_online_booking_path(organisation)
 
-    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif individuel fermé avec plages d'ouverture")
-    create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
+      expect(page).to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red", count: 3)
 
-    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif individuel ouvert avec plages d'ouverture")
-    create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
+      expect(page).to have_content("Ce motif n'est pas réservable en ligne")
+      expect(page).to have_link("modifier")
+      expect(page).to have_content("Pas de plages d'ouverture")
+      expect(page).to have_link("ajouter")
 
-    create(:motif, organisation: organisation, service: agent.service, reservable_online: false, collectif: false, name: "Motif collectif fermé sans créneaux")
+      motif.update!(reservable_online: true)
 
-    motif = create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: false, name: "Motif collectif ouvert avec créneaux")
-    create(:rdv, motif: motif, max_participants_count: 5)
+      visit admin_organisation_online_booking_path(organisation)
+      expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 1)
+      expect(page).to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red", count: 2)
+      expect(page).to have_content("Réservable en ligne")
+
+      create(:plage_ouverture, motifs: [motif], agent: agent, organisation: organisation)
+
+      visit admin_organisation_online_booking_path(organisation)
+      expect(page).to have_content("1 plage d'ouverture")
+      expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 3)
+      expect(page).not_to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red")
+    end
   end
 
-  it "displays the information of the various motifs" do
-    visit admin_organisation_online_booking_path(organisation)
-    expect(page).to have_css('[data-clipboard-target="copy-button"]')
-    expect(page).to have_content("adsfasdf") # TODO
-    # vérifier que les liens de petits cta vont sur les bonnes pages préremplies correctement
+  context "motif collectif" do
+    let!(:motif) do
+      create(:motif, organisation: organisation, service: agent.service, reservable_online: true, collectif: true, name: "Motif collectif")
+    end
+
+    it "displays the motif's status" do
+      login_as(agent, scope: :agent)
+
+      visit admin_organisation_online_booking_path(organisation)
+      expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 1)
+      expect(page).to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red", count: 2)
+      expect(page).to have_content("Réservable en ligne")
+      expect(page).to have_content("Aucun rendez-vous avec des places disponibles")
+
+      create(:rdv, motif: motif, max_participants_count: 5)
+
+      visit admin_organisation_online_booking_path(organisation)
+      expect(page).to have_css("i.fa-solid.fa-circle-check.color-scheme-green", count: 3)
+      expect(page).not_to have_css("i.fa-regular.fa-circle-xmark.color-scheme-red")
+      expect(page).to have_content("1 rendez-vous avec des places disponibles")
+    end
   end
 end
