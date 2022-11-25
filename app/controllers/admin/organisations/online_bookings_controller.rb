7be6a136ce1f10ec9ec80f3b1d4cc190b27ee42a
Victor Mours <victor.mours@beta.gouv.fr>
2022-11-25 14:35:16 +0100

    enable page based on domain rather than service

--- app/controllers/admin/organisations/online_bookings_controller.rb
+++ app/controllers/admin/organisations/online_bookings_controller.rb
@@ -2,7 +2,6 @@
 
 class Admin::Organisations::OnlineBookingsController < AgentAuthController
   before_action :set_organisation
-  before_action :check_conseiller_numerique
 
   helper_method :booking_link
 
@@ -17,10 +16,6 @@ class Admin::Organisations::OnlineBookingsController < AgentAuthController
   end
 
   private
-
-  def check_conseiller_numerique
-    redirect_to authenticated_agent_root_path unless current_agent.conseiller_numerique?
-  end
 
   def booking_link
     public_link_to_org_url(organisation_id: current_organisation.id)
