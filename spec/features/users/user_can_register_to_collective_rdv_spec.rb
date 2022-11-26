fd39b2b98bfb753c39bd00c67a88149979fb77d9
Romain Neuville <neuvilleromain@gmail.com>
2022-11-26 17:13:25 +0100

    feature test in progress

--- spec/features/users/user_can_register_to_collective_rdv_spec.rb
+++ spec/features/users/user_can_register_to_collective_rdv_spec.rb
@@ -1,75 +1,152 @@
 # frozen_string_literal: true
 
-# RSpec.describe "Adding a user to a collective RDV" do
-#   context "with a signed in user" do
-#     it "works" do
-#       territory = create(:territory, departement_number: "75")
-#       organisation = create(:organisation, territory: territory)
-#       agent = create(:agent, organisations: [organisation])
-#       motif = create(:motif, :collectif, reservable_online: true, organisation: organisation, name: "Atelier")
-#       rdv = create(:rdv, :without_users, motif: motif, agents: [agent], organisation: organisation)
-#       user = create(:user, phone_number: "+33601010101", email: "frederique@example.com")
-#       login_as(user, scope: :user)
+RSpec.describe "Adding a user to a collective RDV" do
+  include Rails.application.routes.url_helpers
 
-#       params = {
-#         address: "Paris 75001",
-#         city_code: "75056",
-#         date: "2022-09-23+17%3A00%3A00+%2B0200",
-#         departement: "75",
-#         latitude: "48.859",
-#         lieu_id: rdv.lieu.id,
-#         longitude: "2.347",
-#         motif_name_with_location_type: motif.name_with_location_type,
-#         service_id: motif.service.id,
-#       }
-#       visit root_path(params)
+  before do
+    stub_netsize_ok
+  end
 
-#       expect(page).to have_content("Atelier")
-#       expect(page).to have_content("S'inscrire")
+  let!(:territory) { create(:territory, departement_number: "75") }
+  let!(:webhook_endpoint) { create(:webhook_endpoint, organisation: organisation, subscriptions: ["rdv"], target_url: "https://example.com") }
+  let!(:organisation) { create(:organisation, territory: territory) }
+  let!(:agent) { create(:agent, organisations: [organisation], rdv_notifications_level: "all") }
+  let!(:service) { create(:service) }
+  let!(:motif) { create(:motif, :collectif, reservable_online: true, organisation: organisation, service: service) }
+  let!(:motif2) { create(:motif, :collectif, reservable_online: true, organisation: organisation, service: service) }
+  let!(:lieu1) { create(:lieu, organisation: organisation) }
+  let!(:lieu2) { create(:lieu, organisation: organisation) }
+  let!(:rdv) { create(:rdv, :without_users, motif: motif, agents: [agent], organisation: organisation, lieu: lieu1) }
+  let!(:user) { create(:user, phone_number: "+33601010101", email: "frederique@example.com") }
+  let!(:invited_user) { create(:user) }
+  let!(:rdv2) { create(:rdv, :without_users, motif: motif2, agents: [agent], organisation: organisation, lieu: lieu2) }
+  let!(:invitation_token) do
+    invited_user.invite! { |u| u.skip_invitation = true }
+    invited_user.raw_invitation_token
+  end
 
-#       expect do
-#         click_link("S'inscrire")
-#       end.to change { rdv.reload.users.count }.from(0).to(1)
-# # TODO : Continuer le parcours A REVOIR entiérement
-#       expect(page).to have_content("Inscription confirmée")
-#     end
-#   end
+  let(:params) do
+    {
+      address: "Paris 75001",
+      city_code: "75056",
+      departement: "75",
+      latitude: "48.859",
+      longitude: "2.347",
+    }
+  end
 
-#   context "with a not signed in user" do
-#     it "redirect to login page before subscription" do
-#       territory = create(:territory, departement_number: "75")
-#       organisation = create(:organisation, territory: territory)
-#       agent = create(:agent, organisations: [organisation])
-#       motif = create(:motif, :collectif, reservable_online: true, organisation: organisation, name: "Atelier")
-#       rdv = create(:rdv, :without_users, motif: motif, agents: [agent], organisation: organisation)
-#       user = create(:user, phone_number: "+33601010101", email: "frederique@example.com")
+  def basic_participation_process
+    # Motif selector
+    expect(page).to have_content("Sélectionnez le motif de votre RDV :")
+    click_link(motif.name)
+    # Restriction modal
+    expect(page).to have_content(motif.restriction_for_rdv)
+    click_link("Accepter", match: :first)
+    # Lieu selector
+    expect(page).to have_content("Sélectionnez un lieu de RDV :")
+    click_link("Prochaine disponibilité")
 
-#       params = {
-#         address: "Paris 75001",
-#         city_code: "75056",
-#         date: "2022-09-23+17%3A00%3A00+%2B0200",
-#         departement: "75",
-#         latitude: "48.859",
-#         lieu_id: rdv.lieu.id,
-#         longitude: "2.347",
-#         motif_name_with_location_type: motif.name_with_location_type,
-#         service_id: motif.service.id,
-#       }
-#       visit root_path(params)
+    # Participation and back buttons
+    expect do
+      click_link("S'inscrire")
+      click_link("Revenir en arrière")
+      click_link("S'inscrire")
+      # TODO User change on form
 
-#       expect(page).to have_content("Atelier")
-#       expect(page).to have_content("S'inscrire")
+      click_button("Continuer")
+      click_link("Revenir en arrière")
+      # TODO test modifier
+      click_button("Continuer")
+      click_button("Continuer")
+      stub_request(:post, "https://example.com/")
+      click_on("Confirmer ma participation")
+    end.to change { rdv.reload.users.count }.from(0).to(1)
+    expect(page).to have_content("Inscription confirmée")
 
-#       click_link("S'inscrire")
+    # Expect Notifications
+    perform_enqueued_jobs
+    expect(ActionMailer::Base.deliveries.map(&:to).flatten).to match_array([agent.email, user.email])
 
-#       expect(page).to have_content("Vous devez vous connecter ou vous inscrire pour continuer.")
+    # Expect Webhooks
+    rdv.reload
+    expect(WebMock).to(have_requested(:post, "https://example.com/").with do |req|
+      JSON.parse(req.body)["data"]["users"].map { |user| user["id"] } == [user.id]
+    end)
+  end
 
-#       fill_in("user_email", with: user.email)
-#       fill_in("password", with: user.password)
-#       click_button("Se connecter")
+  context "with a signed in user" do
+    it "works" do
+      login_as(user, scope: :user)
+      visit root_path(params)
+      basic_participation_process
+    end
+  end
 
-#       expect(page).to have_content("Inscription confirmée")
-#       expect(rdv.reload.users.count).to eq(1)
-#     end
-#   end
-# end
+  context "with a not signed in user" do
+    it "redirect to login page before subscription" do
+      visit root_path(params)
+      # Motif selector
+      expect(page).to have_content("Sélectionnez le motif de votre RDV :")
+      click_link(motif.name)
+      # Restriction modal
+      expect(page).to have_content(motif.restriction_for_rdv)
+      click_link("Accepter", match: :first)
+      # Lieu selector
+      expect(page).to have_content("Sélectionnez un lieu de RDV :")
+      click_link("Prochaine disponibilité")
+      click_link("S'inscrire")
+
+      expect(page).to have_content("Vous devez vous connecter ou vous inscrire pour continuer.")
+
+      fill_in("user_email", with: user.email)
+      fill_in("password", with: user.password)
+
+      click_button("Se connecter")
+      click_button("Continuer")
+      click_button("Continuer")
+      click_on("Confirmer ma participation")
+
+      expect(page).to have_content("Inscription confirmée")
+      expect(rdv.reload.users.count).to eq(1)
+    end
+  end
+
+  context "with an invited user (Token)" do
+    it "works" do
+      params.merge!(invitation_token: invitation_token)
+      visit prendre_rdv_path(params)
+
+      expect(page).to have_content("Sélectionnez le motif de votre RDV :")
+      click_link(motif.name)
+      expect(page).to have_content(motif.restriction_for_rdv)
+      click_link("Accepter", match: :first)
+      expect(page).to have_content("Sélectionnez un lieu de RDV :")
+      click_link("Prochaine disponibilité")
+
+      expect do
+        click_link("S'inscrire")
+        click_button("Continuer")
+        stub_request(:post, "https://example.com/")
+        click_on("Confirmer ma participation")
+      end.to change { rdv.reload.users.count }.from(0).to(1)
+      expect(page).to have_content("Inscription confirmée")
+      expect(::Addressable::URI.parse(current_url).query_values).to match("invitation_token" => /^[A-Z0-9]{8}$/)
+      perform_enqueued_jobs
+      expect(ActionMailer::Base.deliveries.map(&:to).flatten).to match_array([agent.email, invited_user.email])
+
+      rdv.reload
+      expect(WebMock).to(have_requested(:post, "https://example.com/").with do |req|
+        JSON.parse(req.body)["data"]["users"].map { |user| user["id"] } == [invited_user.id]
+      end)
+    end
+  end
+
+#  - Ne pas afficher un rdv revoked ou full ou passé ou pas ouvert à la réservation
+#  - Ne pas pouvoir prendre rdv sur un rdv non collectif (au niveau du controlleur) override des params avec un id de rdv non collectif
+#  - Message pour une participation existante
+#  - Message pour un changement de status
+#  - RDV annulé vérifier le tag
+#  - tester le user helper : can_change_participants?(rdv) pour les non invité et invité (no 'modifier' dans le resumé)
+#  - Tester tout les boutons "modifier"
+
+end
