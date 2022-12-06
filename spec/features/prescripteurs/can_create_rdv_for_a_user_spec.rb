71f9b0e8475ed5b47b1d0a7f8418e20f9b3464a6
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-12-06 15:23:36 +0100

    Improve test

--- spec/features/prescripteurs/can_create_rdv_for_a_user_spec.rb
+++ spec/features/prescripteurs/can_create_rdv_for_a_user_spec.rb
@@ -60,11 +60,10 @@ RSpec.describe "prescripteur can create RDV for a user" do
     fill_in "Téléphone", with: "0611223344"
 
     stub_netsize_ok
-    expect { click_on "Confirmer le rendez-vous" }.to change(Rdv, :count).by(1)
+    expect { click_on "Confirmer le rendez-vous" }.to change(Rdv, :count).by(1).and(change(User, :count).by(1))
 
     created_rdv = Rdv.last
-    expect(created_rdv.users.map(&:full_name)).to eq(["Patricia DUROY"])
-    expect(created_rdv.users.map(&:created_through)).to eq(["prescripteur"])
+    expect(created_rdv.users.first).to have_attributes(full_name: "Patricia DUROY", created_through: "prescripteur", phone_number: "0611223344")
     expect(created_rdv.agents).to eq([agent])
     expect(created_rdv.rdvs_users.first.prescripteur).to have_attributes(
       first_name: "Alex",
