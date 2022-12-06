97844c55978d932322f7b36b1d8f56858f2a679c
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-12-06 10:20:03 +0100

    Utiliser des sous-requêtes SQL plutôt que de passer des listes d'IDs (#3148)
    * Use subqueries instead of retrieving IDS and passing them
    
    * Add rule to 2-contributions.md
    
    * Improve doc

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,4 +9,26 @@ class ApplicationRecord < ActiveRecord::Base
   def new_and_blank?
     new_record? && attributes == self.class.new.attributes
   end
+
+  # Allows combining structurally different queries using subqueries.
+  # The resulting query looks like this:
+  #     SELECT * FROM table
+  #     WHERE `table.deleted_at` IS NULL -- conditions previously defined on self
+  #     AND
+  #       (
+  #         `table.id` IN (SELECT... first subquery)
+  #         OR
+  #         `table.id` IN (SELECT... second subquery)
+  #         OR
+  #         ...
+  #       ) -- subqueries combined with OR
+  #
+  # It can be useful when merging scopes with different join tables:
+  #     agents_with_open_plage = Agent.joins(:plage_ouvertures).merge(PlageOuverture.reservable_online)
+  #     agents_with_open_rdv_collectif = Agent.joins(:rdvs).merge(Rdv.collectif)
+  #     Agent.where_id_in_subqueries([agents_with_open_plage, agents_with_open_rdv_collectif])
+  #
+  def self.where_id_in_subqueries(subqueries)
+    subqueries.map { |scope| where(id: scope) }.reduce(:or)
+  end
 end
