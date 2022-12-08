d5d073a5b8f889dc495c5c45f9f139b445d134e1
Victor Mours <victor.mours@beta.gouv.fr>
2022-12-08 16:23:11 +0100

    wip

--- app/controllers/concerns/can_have_rdv_wizard_context.rb
+++ app/controllers/concerns/can_have_rdv_wizard_context.rb
@@ -11,19 +11,14 @@ module CanHaveRdvWizardContext
     return if session[:user_return_to].blank?
 
     parsed_uri = URI.parse(session[:user_return_to])
-    parsed_params = Rack::Utils.parse_nested_query(parsed_uri.query).to_h.symbolize_keys
-    case parsed_uri.path
-    when %r{/users/rdv_wizard_step/new}
-      return if %i[motif_id starts_at lieu_id].any? { parsed_params[_1].blank? }
-      @rdv_wizard = UserRdvWizard::Step1.new(nil, parsed_params)
-      return if @rdv_wizard.creneau.present?
-    when %r{/users/rdvs/\d*/participations/new}
-      @rdv_wizard = PrescripteurRdvWizard.new(parsed_params, current_domain)
-    else
-      @rdv_wizard = nil
-      return
-    end
+    return if parsed_uri.path != "/users/rdv_wizard_step/new" && !%r{/users/rdvs/\d*/participations/new}.match?(parsed_uri.path)
 
+    parsed_params = Rack::Utils.parse_nested_query(parsed_uri.query).to_h.symbolize_keys
+
+    @rdv_wizard = UserRdvWizard::Step1.new(nil, parsed_params)
+    return if @rdv_wizard.creneau.present?
+
+    @rdv_wizard = nil
     session.delete(:user_return_to)
   end
 end
