8a4f1f385b94b338668fab26d6a8c439614fdc6c
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-12-08 15:08:37 +0100

    Parcours de prise de RDV par prescripteur (#3090)
    * First days of work in one big stashed commit
    
    various controller refactors
    
    Improve display of benign errors
    
    improve error message
    
    Display benign error
    
    Start working on user validations
    
    Work on notifications
    
    create prescripteur
    
    Make basic spec pass! 🎉
    
    Rename session key
    
    display and update prescripteur
    
    set steps in fil d'ariane
    
    save rdv wizard info from one step to the next
    
    wip: adding prescripteur page
    
    Add link to devise page
    
    Write feature spec
    
    Add foreign key
    
    Add prescripteurs to db
    
    * Update migration to Rails 7 version
    
    * Store prescripteur in RdvsUser, not Rdv
    
    * Fix validity of prescripteur.rb factory
    
    * Fix lint errors
    
    * Wip collectif spec
    
    * Improve markup for sign up button
    
    * Show confirmation page
    
    * Add contact links on confirmation page
    
    * Change unicity constraint on users.email index
    
    * don't ask for beneficiaire email for prescripteurs
    
    * Add error for non mobile phone number
    
    * wip
    
    * Check creneau availability on prescripteur flow
    
    * Add a lieux for CNFS in seeds.rb
    
    * Center cards
    
    * Flush flow session upon RDV creation
    
    * No need for comment here
    
    * add email template
    
    * Touch migration date
    
    * Travel to time before calling factories
    
    * Preload associations
    
    * Please rubocop
    
    * Please my slim linter
    
    * xit specs for RDV collectifs
    
    * Set User#created_through to "prescripteur" upon creation
    
    * Improve test
    
    * Only show prescripteur login on RDV Aide Numérique
    
    * Remove spec for feature that will be done in branch `prise-de-rdv-collectif-par-prescripteur`
    
    * Use proper domain in spec
    
    * Use proper scoping in routes
    
    * Link user to the RDV's organisation
    
    * Add little comments to feature spec
    
    * Check notifications individually
    
    * Don't assume we have a user
    
    * Add spec for BeneficiaireForm
    
    * Set the created by for prescripteurs
    
    * Add comment to explain feature flag
    
    * Revert a little something I tried
    
    Co-authored-by: Victor Mours <victor.mours@beta.gouv.fr>

---
+++ app/mailers/prescripteur_mailer.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class PrescripteurMailer < ApplicationMailer
+  include DateHelper
+
+  attr_reader :domain
+
+  def rdv_created(rdvs_user, domain_name)
+    @domain = Domain.find_by_name(domain_name)
+    @user = rdvs_user.user
+    @rdv = rdvs_user.rdv
+    mail(
+      subject: I18n.t("prescripteurs_mailer.rdv_created.title", domain_name: @domain.name, date: relative_date(@rdv.starts_at)),
+      to: rdvs_user.prescripteur.email
+    )
+  end
+end
