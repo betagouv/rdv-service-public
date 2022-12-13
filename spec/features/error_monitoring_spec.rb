a96dac4c52e8b3d60e3107620372b8766b6c2936
Victor Mours <victor.mours@beta.gouv.fr>
2022-12-13 18:33:13 +0100

    add failing spec

--- spec/features/error_monitoring_spec.rb
+++ spec/features/error_monitoring_spec.rb
@@ -24,21 +24,24 @@ RSpec.describe "Error monitoring" do
         # Let's break the link
         page.execute_script(<<-JS
           let link = document.querySelector("[href='#{admin_organisation_rdv_path(rdv.organisation, rdv)}']")
-          link.setAttribute("href", "#{admin_organisation_rdv_path(rdv.organisation, rdv.id + 3)})")
+          link.setAttribute("href", "#{admin_organisation_rdv_path(rdv.organisation, rdv.id + 1)})")
         JS
                            )
       end
 
       it "warns us through Sentry" do
         click_link("durée")
-        expect(page).to have_content("RDV")
+        expect(page).to have_content("RDV") # Wait for the click to be processed
         expect(sentry_events.count).to eq 1
         expect(sentry_events.last.exception.values.first.type).to eq("ActiveRecord::RecordNotFound")
       end
     end
 
     context "when there is a broken link somewhere else" do
-      it "ignores it" do
+      it "doesnt spam Sentry, because there's nothing we can do to fix outside links" do
+        visit admin_organisation_rdvs_path(rdv.organisation, rdv.id + 1)
+        expect(page).to have_content("RDV") # Wait for the click to be processed
+        expect(sentry_events.count).to eq 0
       end
     end
   end
