6123af1e7cd98db55aec2dab3e17de118223aedf
Victor Mours <victor.mours@beta.gouv.fr>
2022-12-14 17:43:54 +0100

    Add failing spec

---
+++ spec/features/agents/rdv_wizard/step4_spec.rb
@@ -0,0 +1,34 @@
+require "rails_helper"
+
+RSpec.describe "Step 4 of the rdv wizard" do
+  let(:motif) { create(:motif, :by_phone) }
+  let(:organisation) { motif.organisation }
+  let(:agent) { create(:agent, service: motif.service, basic_role_in_organisations: [organisation]) }
+
+  let(:params) do
+    {
+      organisation_id: organisation.id,
+      user_ids: [user.id],
+      duration_in_min: 30,
+      motif_id: motif.id,
+      starts_at: 2.days.from_now,
+      step: 4,
+    }
+  end
+
+  context "when booking a rdv for a relative" do
+    let(:user) { create(:user, :relative, responsible: responsible) }
+    let(:responsible) { create(:user, email: nil) }
+
+    before { stub_netsize_ok }
+
+    it "sends a sms with a valid link" do
+      login_as(agent, scope: :agent)
+      visit new_admin_organisation_rdv_wizard_step_path(params)
+      click_button "Créer RDV"
+      expect(page).to have_content("Le rendez-vous a été créé.")
+      perform_enqueued_jobs
+      expect(Receipt.last.content).to include("token")
+    end
+  end
+end
