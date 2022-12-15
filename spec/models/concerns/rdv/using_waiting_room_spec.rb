eefdbb04130b0e2ca9434d86ce87d2d60bcd2c77
Yannick François <yannick.francois@beta.gouv.fr>
2022-12-15 09:47:59 +0100

    Modifie un commentaire dans les specs
    https://github.com/betagouv/rdv-solidarites.fr/pull/2911#discussion_r1048357007

--- spec/models/concerns/rdv/using_waiting_room_spec.rb
+++ spec/models/concerns/rdv/using_waiting_room_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe Rdv::UsingWaitingRoom do
   it "Reset all `user_in_waiting_room` keys in redis" do
     list_rdv = create_list(:rdv, 3)
 
-    # On place les RDV dans le REDIS pour signaler
-    # que les usagers sont en salle d'attente
+    # On garde une trace des RDV
+    # pour signaler que les usagers 
+    # sont en salle d'attente
     list_rdv.map(&:set_user_in_waiting_room!)
 
     Rdv.reset_user_in_waiting_room!
