d970690fd93229972168f88aaa4698f35a7b2d60
Yannick François <yannick.francois@beta.gouv.fr>
2022-12-16 13:43:30 +0100

    Corrige les specs

--- spec/features/accessibility/configuration_pages_spec.rb
+++ spec/features/accessibility/configuration_pages_spec.rb
@@ -9,22 +9,4 @@ describe "configuration pages", js: true do
     path = admin_territory_path(territory)
     expect_page_to_be_axe_clean(path)
   end
-
-  it "show SMS configuration" do
-    territory = create(:territory)
-    agent = create(:agent, role_in_territories: [territory])
-    login_as agent
-
-    path = admin_territory_sms_configuration_path(territory)
-    expect_page_to_be_axe_clean(path)
-  end
-
-  it "edit SMS configuration" do
-    territory = create(:territory, has_own_sms_provider: true)
-    agent = create(:agent, role_in_territories: [territory])
-    login_as agent
-
-    path = edit_admin_territory_sms_configuration_path(territory)
-    expect_page_to_be_axe_clean(path)
-  end
 end
--- spec/mailers/admins/sfr_mail_2_sms_mailer_spec.rb
+++ spec/mailers/admins/sfr_mail_2_sms_mailer_spec.rb
@@ -1,10 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.describe Admins::Grc92Mailer do
-  let(:mail) { described_class.send_sms("test@example.com", "0611223344", "test sms") }
-
+RSpec.describe Admins::SfrMail2SmsMailer, type: :mailer do
   describe "#send_sms" do
     it "sends the email correctly" do
+      mail = described_class.send_sms("test@example.com/ne-pas-repondre-grc@hauts-de-seine.fr", "0611223344", "test sms")
       expect(mail).to have_attributes(
         from: ["ne-pas-repondre-grc@hauts-de-seine.fr"],
         reply_to: nil
