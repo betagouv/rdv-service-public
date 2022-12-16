0c4b9905d969c3794b0a410d00de7f3fe2e4ce79
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-12-16 09:31:58 +0100

    Improve spec

--- spec/jobs/custom_mailer_delivery_job_spec.rb
+++ spec/jobs/custom_mailer_delivery_job_spec.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 RSpec.describe CustomMailerDeliveryJob do
-  it "discards job when record can't be found" do
+  mailer = Class.new(ApplicationMailer) do
+    def a_sample_email(absence)
+      mail(body: "Voici l'info: #{absence}")
+    end
+  end
+
+  it "discards job when deserialization fails because if ActiveRecord::RecordNotFound" do
     absence = create(:absence)
-    Agents::AbsenceMailer.with(absence: absence).absence_created.deliver_later
+    mailer.a_sample_email(absence).deliver_later
     absence.destroy!
     expect { perform_enqueued_jobs }.not_to raise_error(ActiveJob::DeserializationError)
   end
 
   # Sometimes we have DB failures, these should not cause the job to be discarded
   it "raises error when hitting a ActiveJob::DeserializationError error that is not a RecordNotFound" do
-    Agents::AbsenceMailer.with(absence: create(:absence)).absence_created.deliver_later
+    mailer.a_sample_email(create(:absence)).deliver_later
     ActiveRecord::Base.remove_connection # Simulate a DB failure
     expect { perform_enqueued_jobs }.to raise_error(ActiveJob::DeserializationError)
     ActiveRecord::Base.establish_connection
