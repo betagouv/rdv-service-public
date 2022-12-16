577313012e06105c7fcda4d890ea36f81e2dba8a
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2022-12-16 09:34:18 +0100

    Improve spec again

--- spec/jobs/custom_mailer_delivery_job_spec.rb
+++ spec/jobs/custom_mailer_delivery_job_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe CustomMailerDeliveryJob do
     absence = create(:absence)
     mailer.a_sample_email(absence).deliver_later
     absence.destroy!
-    expect { perform_enqueued_jobs }.not_to raise_error(ActiveJob::DeserializationError)
+    expect { perform_enqueued_jobs }.not_to raise_error
   end
 
   # Sometimes we have DB failures, these should not cause the job to be discarded
   it "raises error when hitting a ActiveJob::DeserializationError error that is not a RecordNotFound" do
     mailer.a_sample_email(create(:absence)).deliver_later
     ActiveRecord::Base.remove_connection # Simulate a DB failure
-    expect { perform_enqueued_jobs }.to raise_error(ActiveJob::DeserializationError)
+    expect { perform_enqueued_jobs }.to raise_error(ActiveJob::DeserializationError, /Error while trying to deserialize arguments: No connection pool/)
     ActiveRecord::Base.establish_connection
   end
 end
