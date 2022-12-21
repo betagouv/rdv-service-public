08a4400139d3308ad7d0bf13b23bc6457d4ac7bf
Romain Neuville <neuvilleromain@gmail.com>
2022-12-21 16:55:45 +0100

    adapt status_changeable test

--- spec/models/concerns/rdvs_user/status_changeable_spec.rb
+++ spec/models/concerns/rdvs_user/status_changeable_spec.rb
@@ -16,58 +16,51 @@ RSpec.describe RdvsUser::StatusChangeable, type: :concern do
     describe "when rdv_user is revoked or excused" do
       RdvsUser::CANCELLED_STATUSES.each do |status|
         it "send notifications and change rdv_user object status to #{status}" do
-          expect(Notifiers::RdvCancelled).to receive(:new).with(rdv, agent, [rdv_user1.user]).and_call_original
           rdv_user1.change_status_and_notify(agent, status)
-          perform_enqueued_jobs
-          expect(ActionMailer::Base.deliveries.map(&:to).flatten).to match_array([rdv_user1.user.email])
           expect(rdv_user1.reload.status).to eq(status)
+          expect_performed_notifications_for(rdv, rdv_user1.user, "rdv_cancelled")
         end
       end
 
       it "do not send notification when already excused or lifecycle off" do
         rdv_user_with_excused_status.update!(status: "excused")
-        expect(Notifiers::RdvCancelled).not_to receive(:new).with(rdv, agent, [rdv_user_with_excused_status.user])
-        expect(Notifiers::RdvCancelled).not_to receive(:new).with(rdv, agent, [rdv_user_with_lifecycle_disabled.user])
         rdv_user_with_excused_status.change_status_and_notify(agent, "revoked")
         rdv_user_with_lifecycle_disabled.change_status_and_notify(agent, "revoked")
         expect(rdv_user_with_excused_status.reload.status).to eq("revoked")
         expect(rdv_user_with_lifecycle_disabled.reload.status).to eq("revoked")
+        dont_expect_any_performed_notifications
       end
     end
 
     describe "when rdv_user is seen (no notifications)" do
       it "doesnt send notifications and change rdv_user object status" do
-        expect(Notifiers::RdvCancelled).not_to receive(:new).with(rdv, agent, [rdv_user1.user])
-        expect(Notifiers::RdvCreated).not_to receive(:new).with(rdv, agent, [rdv_user1.user])
         rdv_user1.change_status_and_notify(agent, "seen")
         expect(rdv_user1.reload.status).to eq("seen")
+        dont_expect_any_performed_notifications(rdv_user1.user)
       end
     end
 
     describe "when rdv_user is noshow (no notifications)" do
       it "doesnt send notifications and change rdv_user object status" do
-        expect(Notifiers::RdvCancelled).not_to receive(:new).with(rdv, agent, [rdv_user1.user])
-        expect(Notifiers::RdvCreated).not_to receive(:new).with(rdv, agent, [rdv_user1.user])
         rdv_user1.change_status_and_notify(agent, "noshow")
         expect(rdv_user1.reload.status).to eq("noshow")
+        dont_expect_any_performed_notifications(rdv_user1.user)
       end
     end
 
     describe "when rdv_user is reloaded from cancelled (excused or revoked)" do
       it "send notifications creation and change rdv_user object status" do
-        expect(Notifiers::RdvCreated).to receive(:new).with(rdv, agent, [rdv_user1.user])
-        rdv_user1.change_status_and_notify(agent, "excused")
+        rdv_user1.update(status: "excused")
         rdv_user1.change_status_and_notify(agent, "unknown")
-        perform_enqueued_jobs
-        expect(ActionMailer::Base.deliveries.map(&:to).flatten).to match_array([rdv_user1.user.email])
         expect(rdv_user1.reload.status).to eq("unknown")
+        expect_performed_notifications_for(rdv, rdv_user1.user, "rdv_created")
       end
 
       it "do not send notification creation when lifecycle off and change rdv_user object status" do
-        expect(Notifiers::RdvCreated).not_to receive(:new).with(rdv, agent, [rdv_user_with_lifecycle_disabled.user])
         rdv_user_with_lifecycle_disabled.change_status_and_notify(agent, "excused")
         rdv_user_with_lifecycle_disabled.change_status_and_notify(agent, "unknown")
         expect(rdv_user_with_lifecycle_disabled.reload.status).to eq("unknown")
+        dont_expect_any_performed_notifications(rdv_user1.user)
       end
     end
 
