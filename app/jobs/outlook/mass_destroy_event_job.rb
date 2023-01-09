f5c44ec0e9cd4d7423621ff197d8f0848d51336d
Victor Mours <victor.mours@beta.gouv.fr>
2023-01-09 17:52:39 +0100

    Avoid infinite loop of Outlook::DestroyEventJob in case of error

--- app/jobs/outlook/mass_destroy_event_job.rb
+++ app/jobs/outlook/mass_destroy_event_job.rb
@@ -3,10 +3,8 @@
 module Outlook
   class MassDestroyEventJob < ApplicationJob
     def perform(agent)
-      while agent.agents_rdvs.exists_in_outlook.any?
-        agent.agents_rdvs.exists_in_outlook.each do |agents_rdv|
-          Outlook::DestroyEventJob.perform_now(agents_rdv.outlook_id, agents_rdv.agent)
-        end
+      agent.agents_rdvs.exists_in_outlook.each do |agents_rdv|
+        Outlook::DestroyEventJob.perform_now(agents_rdv.outlook_id, agents_rdv.agent)
       end
       agent.update!(microsoft_graph_token: nil, refresh_microsoft_graph_token: nil)
     end
