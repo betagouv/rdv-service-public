e9559542bd465e274f034cb6627990bc6d13a315
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-01-11 18:32:40 +0100

    Finish the fix on `relative_date`

--- app/helpers/date_helper.rb
+++ app/helpers/date_helper.rb
@@ -6,9 +6,9 @@ module DateHelper
 
     date = date.to_date
     if date == Date.current
-      t "date.helpers.today"
+      I18n.t "date.helpers.today"
     elsif date == Date.current + 1
-      t "date.helpers.tomorrow"
+      I18n.t "date.helpers.tomorrow"
     else
       I18n.l(date, format: fallback_format)
     end
