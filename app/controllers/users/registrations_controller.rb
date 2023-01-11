0059caccc7070c8f23ac84821a8c9024bf51a337
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-01-11 16:07:32 +0100

    Improve readability

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -7,7 +7,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   after_action :allow_iframe
 
   def create
-    return invite_and_redirect if User.find_by(email: sign_up_params[:email], confirmed_at: nil)
+    return invite_and_redirect(existing_unconfirmed_user) if existing_unconfirmed_user
 
     super
   end
@@ -41,13 +41,16 @@ class Users::RegistrationsController < Devise::RegistrationsController
     users_pending_registration_path(email_tld: resource.email_tld)
   end
 
-  def invite_and_redirect
-    user = User.find_by(email: sign_up_params[:email], confirmed_at: nil)
-    user.invite!(nil, user_params: sign_up_params) do |user|
+  def invite_and_redirect(user)
+    user.invite!(nil, user_params: sign_up_params) do |u|
       # This is our only way of passing the domain to the Devise mailer
-      user.sign_up_domain = current_domain
+      u.sign_up_domain = current_domain
     end
     set_flash_message! :notice, :signed_up_but_unconfirmed
     respond_with user, location: after_inactive_sign_up_path_for(user)
+  end
+
+  def existing_unconfirmed_user
+    @existing_unconfirmed_user ||= User.find_by(email: sign_up_params[:email], confirmed_at: nil)
   end
 end
