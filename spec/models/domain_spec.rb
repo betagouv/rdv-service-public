af8ac5fec995f429bb4541ef0ccb899a85bb781d
Victor Mours <victor.mours@beta.gouv.fr>
2023-01-17 11:58:05 +0100

    Add spec to make sure domains have all required keys

---
+++ spec/models/domain_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+describe Domain do
+  it "has domains initialized with all the required keys" do
+    Domain::ALL.each do |domain|
+      expect(domain.to_h.compact.keys).to match_array(described_class.members)
+    end
+  end
+end
