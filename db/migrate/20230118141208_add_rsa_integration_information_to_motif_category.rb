f9a14acefa68437fca57297dbb7ee6d7eaa21bdd
Quentin Blanc <qblanc@hotmail.com>
2023-01-18 15:17:21 +0100

    linter fix

--- db/migrate/20230118141208_add_rsa_integration_information_to_motif_category.rb
+++ db/migrate/20230118141208_add_rsa_integration_information_to_motif_category.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRsaIntegrationInformationToMotifCategory < ActiveRecord::Migration[7.0]
   def change
     add_enum_value :motif_category, "rsa_integration_information"
