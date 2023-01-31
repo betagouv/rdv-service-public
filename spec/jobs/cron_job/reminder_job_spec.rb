28d83cc6b443058f7525d190f06beaa3f298ca39
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-01-31 21:27:49 +0100

    Notifs de rappel : utiliser une stratégie de scheduling plus solide (#3300)
    * Revert "Send reminders every hour, not every day at 09:00 (#3286)"
    
    This reverts commit f7b19276657b44c69894cb97d5d5c3f83a55f71b.
    
    * New strategy: set reminder job times based on RDV time

--- spec/jobs/cron_job/reminder_job_spec.rb
+++ spec/jobs/cron_job/reminder_job_spec.rb
@@ -3,80 +3,43 @@
 RSpec.describe CronJob::ReminderJob, type: :job do
   subject(:perform_now) { described_class.perform_now }
 
-  let(:now) { Time.zone.parse("01-01-2019 14:57") }
-
   before do
-    travel_to(now)
-    freeze_time
+    travel_to(Time.zone.parse("2019-01-01 03:00"))
   end
 
-  context "single rdv in 48 to 49 hours" do
-    let!(:rdv_in_48_hours_and_30_minutes) { create(:rdv, starts_at: 48.hours.from_now + 30.minutes) }
+  context "single rdv the day after tomorrow at 10:30" do
+    let!(:rdv) { create(:rdv, starts_at: Time.zone.parse("2019-01-03 10:30")) }
 
-    it "calls notification service" do
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv_in_48_hours_and_30_minutes, nil)
-      perform_now
+    it "enqueues reminder job today at 10:30" do
+      expect { perform_now }.to have_enqueued_job(RdvUpcomingReminderJob).with(rdv).at(Time.zone.parse("2019-01-01 10:30"))
     end
   end
 
-  context "RDVs before 48 hours, within 48-49 hours, after 49 hours" do
-    # RDVs in the pas
-    let!(:rdv_2_days_ago) { create(:rdv, starts_at: 2.days.ago) }
-    let!(:rdv_in_10_minutes) { create(:rdv, starts_at: 10.minutes.from_now) }
-    let!(:rdv_in_47_hours_and_30_minutes) { create(:rdv, starts_at: 47.hours.from_now + 30.minutes) }
+  context "2 rdvs in 2 days, 1 tomorrow" do
+    # RDV tomorrow
+    let!(:rdv_tomorrow1030) { create(:rdv, starts_at: Time.zone.parse("2019-01-02 10:30")) }
 
-    # RDVs in about 48 hours
-    let!(:rdv_in_48_hours_and_10_minutes) { create(:rdv, starts_at: 48.hours.from_now + 10.minutes) }
-    let!(:rdv_in_48_hours_and_30_minutes) { create(:rdv, starts_at: 48.hours.from_now + 30.minutes) }
+    # RDVs day after tomorrow
+    let!(:rdv_day_after_tomorrow1030) { create(:rdv, starts_at: Time.zone.parse("2019-01-03 10:30")) }
+    let!(:rdv_day_after_tomorrow1400) { create(:rdv, starts_at: Time.zone.parse("2019-01-03 14:00")) }
 
-    # RDVs after 49 hours
-    let!(:rdv_in_49_hours_and_30_minutes) { create(:rdv, starts_at: 49.hours.from_now + 30.minutes) }
-    let!(:rdv_in_2_weeks) { create(:rdv, starts_at: 2.weeks.from_now) }
+    # RDV in 3 days
+    let!(:rdv_in_three_days1400) { create(:rdv, starts_at: Time.zone.parse("2019-01-04 14:00")) }
 
-    it "calls notification service" do
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv_2_days_ago, nil)
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv_in_10_minutes, nil)
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv_in_47_hours_and_30_minutes, nil)
+    it "enqueues jobs only for RDVs that happen the day after tomorrow" do
+      expect { perform_now }
+        .to have_enqueued_job(RdvUpcomingReminderJob).with(rdv_day_after_tomorrow1030).at(Time.zone.parse("2019-01-01 10:30"))
+        .and have_enqueued_job(RdvUpcomingReminderJob).with(rdv_day_after_tomorrow1400).at(Time.zone.parse("2019-01-01 14:00"))
 
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv_in_48_hours_and_10_minutes, nil)
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv_in_48_hours_and_30_minutes, nil)
-
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv_in_49_hours_and_30_minutes, nil)
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with).with(rdv_in_2_weeks, nil)
-
-      perform_now
+      expect(enqueued_jobs.size).to eq(2) # other RDVs not enqueued
     end
   end
 
-  context "rdv in the right time scope bu cancelled" do
-    let!(:rdv_in_48_hours_and_30_minutes) { create(:rdv, :excused, starts_at: 48.hours.from_now + 30.minutes) }
+  context "rdv in 2 days but cancelled" do
+    let!(:cancelled_rdv_day_after_tomorrow1030) { create(:rdv, :excused, starts_at: Time.zone.parse("2019-01-03 10:30")) }
 
-    it "does not call notification service" do
-      expect(Notifiers::RdvUpcomingReminder).not_to receive(:perform_with)
-      perform_now
-    end
-  end
-
-  describe "error handling" do
-    let!(:rdv1) { create(:rdv, starts_at: 2.days.from_now) }
-    let!(:rdv2) { create(:rdv, starts_at: 2.days.from_now) }
-    let!(:rdv3) { create(:rdv, starts_at: 2.days.from_now) }
-
-    stub_sentry_events
-
-    it "does not stop on first error" do
-      # Something unexpected happens when processing rdv2
-      allow(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv2, nil).and_raise("woopsie")
-
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv1, nil)
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv2, nil)
-      expect(Notifiers::RdvUpcomingReminder).to receive(:perform_with).with(rdv3, nil)
-
-      perform_now
-
-      # Exception raised by rdv_2 is sent to Sentry
-      expect(sentry_events.last.exception.values.last.value).to eq("woopsie (RuntimeError)")
-      expect(sentry_events.last.breadcrumbs.compact.first.data[:rdv_id]).to eq(rdv2.id)
+    it "does not enqueue any job" do
+      expect { perform_now }.not_to have_enqueued_job
     end
   end
 end
