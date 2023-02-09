fcdb872ca24acbd9158fd6037f4414467922930c
Quentin Blanc <qblanc@hotmail.com>
2023-02-09 11:08:25 +0100

    add id in agent role blueprint

--- app/blueprints/agent_role_blueprint.rb
+++ app/blueprints/agent_role_blueprint.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class AgentRoleBlueprint < Blueprinter::Base
-  fields :level
+  identifier :id
 
+  fields :level
   association :organisation, blueprint: OrganisationBlueprint
   association :agent, blueprint: AgentBlueprint
 end
