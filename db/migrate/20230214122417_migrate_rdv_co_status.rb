4f9b05eef7986f11ef0fadd8ab09e065eacae8e8
Romain Neuville <neuvilleromain@gmail.com>
2023-02-14 13:57:28 +0100

    migration is ok

--- db/migrate/20230214122417_migrate_rdv_co_status.rb
+++ db/migrate/20230214122417_migrate_rdv_co_status.rb
@@ -3,8 +3,8 @@ class MigrateRdvCoStatus < ActiveRecord::Migration[7.0]
     # Change invalid collectives rdv statuses noshow and excused (approx 500 rdvs in production)
     # This will set collectives rdv statuses to seen (if one or more participation is seen)
     # This will set collectives rdv statuses to revoked (if no participation is seen or unknown)
-    Rdv.collectif.where(status: "noshow").update_rdv_status_from_participation
-    Rdv.collectif.where(status: "excused").update_rdv_status_from_participation
+    Rdv.collectif.where(status: "noshow").map(&:update_rdv_status_from_participation)
+    Rdv.collectif.where(status: "excused").map(&:update_rdv_status_from_participation)
 
     # We set restants collective rdvs statuses to 'unknown'
     Rdv.collectif.where(status: "noshow").each { |rdv| rdv.update!(status: "unknown") }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_142918) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_14_122417) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "unaccent"
