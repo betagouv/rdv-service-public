3064887b35dbf680d0b0c55b4b58670c8dca0c1e
Romain Neuville <neuvilleromain@gmail.com>
2023-03-07 17:05:39 +0100

    fix migration for prod

--- db/migrate/20230214122417_migrate_rdv_co_status.rb
+++ db/migrate/20230214122417_migrate_rdv_co_status.rb
@@ -5,11 +5,24 @@ class MigrateRdvCoStatus < ActiveRecord::Migration[7.0]
     # Change invalid collectives rdv statuses noshow and excused (approx 500 rdvs in production)
     # This will set collectives rdv statuses to seen (if one or more participation is seen)
     # This will set collectives rdv statuses to revoked (if no participation is seen or unknown)
-    Rdv.collectif.where(status: "noshow").map(&:update_rdv_status_from_participation)
-    Rdv.collectif.where(status: "excused").map(&:update_rdv_status_from_participation)
+    Rdv.collectif.where(status: "noshow").each do |rdv|
+      if rdv.rdvs_users.any?(&:seen?)
+        rdv.update_column(:status, "seen")
+        next
+      end
+      if rdv.rdvs_users.none?(&:seen?) && rdv.rdvs_users.none?(&:unknown?)
+        rdv.update_columns(status: "revoked", cancelled_at: Time.zone.now)
+      end
+    end
 
-    # We set restants collective rdvs statuses to 'revoked'
-    Rdv.collectif.where(status: "noshow").each { |rdv| rdv.update!(status: "revoked") }
-    Rdv.collectif.where(status: "excused").each { |rdv| rdv.update!(status: "revoked") }
+    Rdv.collectif.where(status: "excused").each do |rdv|
+      if rdv.rdvs_users.any?(&:seen?)
+        rdv.update_column(:status, "seen")
+        next
+      end
+      if rdv.rdvs_users.none?(&:seen?) && rdv.rdvs_users.none?(&:unknown?)
+        rdv.update_columns(status: "revoked", cancelled_at: Time.zone.now)
+      end
+    end
   end
 end
