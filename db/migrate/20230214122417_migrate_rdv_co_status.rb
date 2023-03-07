c04d2138b091576906cda7a8a63ae73a904f976a
Romain Neuville <neuvilleromain@gmail.com>
2023-03-07 17:22:09 +0100

    fix linter

--- db/migrate/20230214122417_migrate_rdv_co_status.rb
+++ db/migrate/20230214122417_migrate_rdv_co_status.rb
@@ -6,25 +6,27 @@ class MigrateRdvCoStatus < ActiveRecord::Migration[7.0]
     # This will set collectives rdv statuses to seen (if one or more participation is seen)
     # This will set collectives rdv statuses to revoked (if no participation is seen or unknown)
     Rdv.collectif.where(status: "noshow").each do |rdv|
-      if rdv.rdvs_users.any?(&:seen?)
-        rdv.update_column(:status, "seen")
-        next
-      end
-      if rdv.rdvs_users.none?(&:seen?) && rdv.rdvs_users.none?(&:unknown?)
-        cancelled_at_date = rdv.cancelled_at || rdv.updated_at
-        rdv.update_columns(status: "revoked", cancelled_at: cancelled_at_date)
-      end
+      update_to_seen(rdv)
+      update_to_revoked(rdv)
     end
 
     Rdv.collectif.where(status: "excused").each do |rdv|
-      if rdv.rdvs_users.any?(&:seen?)
-        rdv.update_column(:status, "seen")
-        next
-      end
-      if rdv.rdvs_users.none?(&:seen?) && rdv.rdvs_users.none?(&:unknown?)
-        cancelled_at_date = rdv.cancelled_at || rdv.updated_at
-        rdv.update_columns(status: "revoked", cancelled_at: cancelled_at_date)
-      end
+      update_to_seen(rdv)
+      update_to_revoked(rdv)
+    end
+  end
+
+  def update_to_seen(rdv)
+    if rdv.rdvs_users.any?(&:seen?)
+      rdv.update_column(:status, "seen")
+      next
+    end
+  end
+
+  def update_to_revoked(rdv)
+    if rdv.rdvs_users.none?(&:seen?) && rdv.rdvs_users.none?(&:unknown?)
+      cancelled_at_date = rdv.cancelled_at || rdv.updated_at
+      rdv.update_columns(status: "revoked", cancelled_at: cancelled_at_date)
     end
   end
 end
