8ede001d83458a739bee18bdd1294dfb8f562d6f
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-07 17:32:10 +0100

    add todo for other bug

--- app/jobs/outlook/mass_create_event_job.rb
+++ app/jobs/outlook/mass_create_event_job.rb
@@ -5,6 +5,7 @@ module Outlook
     queue_as :outlook_sync
 
     def perform(agent)
+      # TODO: fix bug: if rdv is cancelled, we don't want to create it in outlook
       agent.agents_rdvs.future.each(&:sync_create_in_outlook_asynchronously)
     end
   end
