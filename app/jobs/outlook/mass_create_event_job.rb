718fcdab3b8a7cb530a1ee4fc80d9af52d364e3c
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-13 20:16:12 +0100

    more spec fix

--- app/jobs/outlook/mass_create_event_job.rb
+++ app/jobs/outlook/mass_create_event_job.rb
@@ -5,7 +5,7 @@ module Outlook
     queue_as :outlook_sync
 
     def perform(agent)
-      agent.agents_rdvs.future.each do |agents_rdv|
+      agent.agents_rdvs.future.find_each do |agents_rdv|
         Outlook::SyncEventJob.perform_later_for(agents_rdv)
       end
     end
