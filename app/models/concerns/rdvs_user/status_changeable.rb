fcc12a90296a7473daa9adeed491cf37c52a5281
Neuville Romain <neuvilleromain@gmail.com>
2023-03-15 21:24:24 +0100

    Reprise, Simplification des status pour les rdv (#3354)
    * improve/refacto dropdown messages with notif mention
    
    * simplify rdv statuses behavior for collectives rdvs and test
    
    * add migration for prod datas
    
    * change wording and separate templates for collective rdvs

--- app/models/concerns/rdvs_user/status_changeable.rb
+++ app/models/concerns/rdvs_user/status_changeable.rb
@@ -8,6 +8,7 @@ module RdvsUser::StatusChangeable
 
     RdvsUser.transaction do
       if update(status: status)
+        rdv.update_rdv_status_from_participation
         notify_update!(author)
         true
       else
