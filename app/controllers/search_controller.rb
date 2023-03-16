686296eb7adc33c1e41fc54248301f3f2e690a85
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-16 13:58:15 +0100

    fix another merge mistake

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -54,7 +54,7 @@ class SearchController < ApplicationController
   def search_params
     params.permit(
       :latitude, :longitude, :address, :city_code, :departement, :street_ban_id,
-      :service_id, :lieu_id, :date, :motif_search_terms, :motif_name_with_location_type, :motif_category, :motif_category_short_name,
+      :service_id, :lieu_id, :date, :motif_search_terms, :motif_name_with_location_type, :motif_category_short_name,
       :invitation_token, :motif_id, :public_link_organisation_id, :user_selected_organisation_id, :prescripteur,
       organisation_ids: [], referent_ids: [], external_organisation_ids: []
     )
