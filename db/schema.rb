4129b775cbe97f8226783a7b8d5ffc3d910c4155
Yannick François <yannick.francois@beta.gouv.fr>
2023-03-16 20:09:33 +0100

    Update organisations's agent without territorial access right

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_131846) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_102739) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
