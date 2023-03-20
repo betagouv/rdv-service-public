5cdfad85df6fc3ebef2b771bc58f3908a744c510
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-20 17:13:55 +0100

    lint fix

--- spec/support/expect_redis_not_to_leak.rb
+++ spec/support/expect_redis_not_to_leak.rb
@@ -2,7 +2,9 @@ class ExpectRedisNotToLeak
   def self.run!
     redis = Redis.new(url: Rails.configuration.x.redis_url)
 
-    redis.keys.each do |key|
+    keys = redis.keys
+
+    keys.each do |key|
       if redis.expiretime(key) == -1
         raise "Redis leak risk: the key #{key} has been set without an expiration time"
       end
