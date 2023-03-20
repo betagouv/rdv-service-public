cacd3fd1df03e6892634f3ea0d126c710d415e3d
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-20 14:40:07 +0100

    Add spec to make sure users can't book

--- spec/features/prescripteurs/motifs_for_prescripteur_only_spec.rb
+++ spec/features/prescripteurs/motifs_for_prescripteur_only_spec.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe "motifs for prescripteurs only" do
-  let(:organisation) { create(:organisation, territory: territory_with_beta_prescripteur_enabled) }
-  let(:territory_with_beta_prescripteur_enabled) { create(:territory, departement_number: "83") }
+RSpec.describe "motifs for prescripteurs only", js: true do
+  let(:organisation) { create(:organisation, territory: territory, name: "MDS du quartier") }
   let(:lieu) { create(:lieu, organisation: organisation, name: "Bureau") }
   let(:agent) { create(:agent, admin_role_in_organisations: [organisation]) }
   let(:motif) { create(:motif, name: "Accompagnement individuel", organisation: organisation, bookable_by: :agents) }
@@ -11,11 +10,38 @@ RSpec.describe "motifs for prescripteurs only" do
     login_as(agent, scope: :agent)
   end
 
-  specify "setting a motif as prescripteurs only from the agents form", js: true do
-    visit edit_admin_organisation_motif_path(organisation, motif)
-    expect(page).not_to have_content("Délai minimum de réservation")
+  context "when the territory has the prescripteur beta feature enabled" do
+    let(:territory) { create(:territory, departement_number: "83") }
 
-    choose("Ouvert aux agents et aux prescripteurs")
-    expect(page).to have_content("Délai minimum de réservation")
+    specify "setting a motif as prescripteurs only from the agents form" do
+      visit edit_admin_organisation_motif_path(organisation, motif)
+      expect(page).not_to have_content("Délai minimum de réservation")
+
+      choose("Ouvert aux agents et aux prescripteurs")
+      expect(page).to have_content("Délai minimum de réservation")
+
+      click_button("Enregistrer")
+
+      expect(page).to have_content("Ouvert aux agents et aux prescripteurs")
+    end
+  end
+
+  context "when the territory doesn't have that feature enabled" do
+    let(:territory) { create(:territory, departement_number: "12") }
+
+    it "doesn't show the possibility to have prescripteurs and toggles the online booking options properly" do
+      visit edit_admin_organisation_motif_path(organisation, motif)
+      expect(page).not_to have_content("Ouvert aux agents et aux prescripteurs")
+
+      expect(page).not_to have_content("Délai minimum de réservation")
+
+      choose("Ouvert aux agents et aux usagers")
+
+      expect(page).to have_content("Délai minimum de réservation")
+
+      choose("Uniquement les agents de MDS du quartier")
+
+      expect(page).not_to have_content("Délai minimum de réservation")
+    end
   end
 end
