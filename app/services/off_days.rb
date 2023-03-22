4460c213dd8aca62716951fa7354c2aba92e1835
Victor Mours <victor.mours@beta.gouv.fr>
2023-03-22 14:34:33 +0100

    Inline jours feries to avoid a lot of requests

--- app/services/off_days.rb
+++ app/services/off_days.rb
@@ -3,42 +3,6 @@
 class OffDays
   # https://www.service-public.fr/particuliers/vosdroits/F2405
   JOURS_FERIES = [
-    Date.new(2020, 1, 1),
-    Date.new(2020, 4, 13),
-    Date.new(2020, 5, 1),
-    Date.new(2020, 5, 8),
-    Date.new(2020, 5, 21),
-    Date.new(2020, 6, 1),
-    Date.new(2020, 7, 14),
-    Date.new(2020, 8, 15),
-    Date.new(2020, 11, 1),
-    Date.new(2020, 11, 11),
-    Date.new(2020, 12, 25),
-
-    Date.new(2021, 1, 1),
-    Date.new(2021, 4, 5),
-    Date.new(2021, 5, 1),
-    Date.new(2021, 5, 8),
-    Date.new(2021, 5, 13),
-    Date.new(2021, 5, 24),
-    Date.new(2021, 7, 14),
-    Date.new(2021, 8, 15),
-    Date.new(2021, 11, 1),
-    Date.new(2021, 11, 11),
-    Date.new(2021, 12, 25),
-
-    Date.new(2022, 1, 1),
-    Date.new(2022, 4, 18),
-    Date.new(2022, 5, 1),
-    Date.new(2022, 5, 8),
-    Date.new(2022, 5, 26),
-    Date.new(2022, 6, 6),
-    Date.new(2022, 7, 14),
-    Date.new(2022, 8, 15),
-    Date.new(2022, 11, 1),
-    Date.new(2022, 11, 11),
-    Date.new(2022, 12, 25),
-
     Date.new(2023, 1, 1),
     Date.new(2023, 4, 10),
     Date.new(2023, 5, 1),
@@ -61,5 +25,18 @@ class OffDays
     date_range = Lapin::Range.ensure_range_is_date(date_range)
 
     JOURS_FERIES.intersection(date_range)
+  end
+
+  def self.to_a
+    JOURS_FERIES.map do |jour_ferie|
+      {
+        title: "Jour férié 🎉",
+        start: jour_ferie.beginning_of_day.as_json,
+        end: jour_ferie.end_of_day.as_json,
+        backgroundColor: "#6F6F71",
+        allDay: true,
+        extendedProps: { jour_feries: true },
+      }
+    end
   end
 end
