b2c34fd2866ecb826c15b5f5da576ca1669d45c4
Victor Mours <victor.mours@beta.gouv.fr>
2023-04-12 17:02:28 +0200

    refactor to bring outlook_create_in_progress closer to where it is used and less duplicated

--- app/jobs/outlook/mass_create_event_job.rb
+++ app/jobs/outlook/mass_create_event_job.rb
@@ -6,9 +6,6 @@ module Outlook
 
     def perform(agent)
       agent.agents_rdvs.includes(:rdv).where(rdv: { starts_at: 1.month.ago.. }).find_each do |agents_rdv|
-        if agents_rdv.outlook_id.nil?
-          agents_rdv.update_columns(outlook_create_in_progress: true) # rubocop:disable Rails/SkipsModelValidations
-        end
         Outlook::SyncEventJob.perform_later_for(agents_rdv)
       end
     end
