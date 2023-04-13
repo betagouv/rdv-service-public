e4b2e8f47f711e317cc78bba445169b78a968526
Victor Mours <victor.mours@beta.gouv.fr>
2023-04-13 12:15:42 +0200

    Correctif de la synchro initiale avec outlook (#3447)
    * Add failing spec
    
    * fix bug
    
    * Also sync rdvs from a month ago
    
    * spec and lint fix
    
    * refactor to bring outlook_create_in_progress closer to where it is used and less duplicated
    
    * Update app/jobs/outlook/mass_create_event_job.rb
    
    Co-authored-by: François Ferrandis <francois.ferrandis@beta.gouv.fr>
    
    * Update app/jobs/outlook/sync_event_job.rb
    
    Co-authored-by: François Ferrandis <francois.ferrandis@beta.gouv.fr>
    
    ---------
    
    Co-authored-by: François Ferrandis <francois.ferrandis@beta.gouv.fr>

--- spec/jobs/outlook/mass_create_event_job_spec.rb
+++ spec/jobs/outlook/mass_create_event_job_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Outlook::MassCreateEventJob do
 
   let!(:future_rdv1) { create(:rdv, organisation: organisation, starts_at: 1.day.from_now, agents: [agent]) }
   let!(:future_rdv2) { create(:rdv, organisation: organisation, starts_at: 2.days.from_now, agents: [agent]) }
-  let!(:past_rdv1) { create(:rdv, organisation: organisation, starts_at: 1.day.ago, agents: [agent]) }
-  let!(:past_rdv2) { create(:rdv, organisation: organisation, starts_at: 2.days.ago, agents: [agent]) }
+  let!(:recent_past_rdv) { create(:rdv, organisation: organisation, starts_at: 10.days.ago, agents: [agent]) }
+  let!(:distant_past_rdv) { create(:rdv, organisation: organisation, starts_at: 200.days.ago, agents: [agent]) }
 
   before { allow(Outlook::SyncEventJob).to receive(:perform_later_for) }
 
@@ -17,7 +17,7 @@ RSpec.describe Outlook::MassCreateEventJob do
     described_class.perform_now(agent)
     expect(Outlook::SyncEventJob).to have_received(:perform_later_for).with(future_rdv1.agents_rdvs.first)
     expect(Outlook::SyncEventJob).to have_received(:perform_later_for).with(future_rdv2.agents_rdvs.first)
-    expect(Outlook::SyncEventJob).not_to have_received(:perform_later_for).with(past_rdv1.agents_rdvs.first)
-    expect(Outlook::SyncEventJob).not_to have_received(:perform_later_for).with(past_rdv2.agents_rdvs.first)
+    expect(Outlook::SyncEventJob).to have_received(:perform_later_for).with(recent_past_rdv.agents_rdvs.first)
+    expect(Outlook::SyncEventJob).not_to have_received(:perform_later_for).with(distant_past_rdv.agents_rdvs.first)
   end
 end
