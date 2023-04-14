6303845054568afaba488b878ce789277d2fc1f8
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-04-14 15:22:48 +0200

    Use proper migration name

--- db/migrate/20230328083444_remove_organisation_id_from_absences.rb
+++ db/migrate/20230328083444_remove_organisation_id_from_absences.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class SetAbsenceOrganisationIdNull < ActiveRecord::Migration[7.0]
+class RemoveOrganisationIdFromAbsences < ActiveRecord::Migration[7.0]
   def change
     remove_column :absences, :organisation_id, :boolean
   end
