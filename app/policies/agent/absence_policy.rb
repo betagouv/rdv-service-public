996a05e9fcdfe168071af753dd8bc45d7a141aa2
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-04-18 11:17:20 +0200

    Further simplify absence_policy.rb

--- app/policies/agent/absence_policy.rb
+++ app/policies/agent/absence_policy.rb
@@ -3,32 +3,26 @@
 class Agent::AbsencePolicy < ApplicationPolicy
   include CurrentAgentInPolicyConcern
 
-  alias absence record
-
-  def same_agent_or_has_access?
-    return true if absence.agent_id == current_agent.id
-
-    agents_i_can_handle.exists?(absence.agent_id)
+  def can_manage_agent?
+    # delegate to AgentPolicy: if I can manage an agent, I can manage her absences
+    ::Agent::AgentPolicy.new(pundit_user, record.agent).current_agent_or_admin_in_record_organisation?
   end
 
-  alias show? same_agent_or_has_access?
-  alias create? same_agent_or_has_access?
-  alias cancel? same_agent_or_has_access?
-  alias new? same_agent_or_has_access?
-  alias update? same_agent_or_has_access?
-  alias edit? same_agent_or_has_access?
-  alias destroy? same_agent_or_has_access?
-  alias versions? same_agent_or_has_access?
+  alias show? can_manage_agent?
+  alias create? can_manage_agent?
+  alias cancel? can_manage_agent?
+  alias new? can_manage_agent?
+  alias update? can_manage_agent?
+  alias edit? can_manage_agent?
+  alias destroy? can_manage_agent?
+  alias versions? can_manage_agent?
 
   class Scope < Scope
     include CurrentAgentInPolicyConcern
 
     def resolve
-      if current_agent.service.secretariat?
-        scope.where(agent: agents_of_my_orgs)
-      else
-        scope.where(agent: agents_i_can_handle + [current_agent.id])
-      end
+      # delegate to AgentPolicy: if I can manage an agent, I can manage her absences
+      ::Agent::AgentPolicy::Scope.new(pundit_user, Agent.all).resolve
     end
   end
 end
