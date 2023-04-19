df37f82625372dbdb61a7193f710cbdae4a7a634
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-04-19 15:48:42 +0200

    WIP

--- app/policies/agent/absence_policy.rb
+++ app/policies/agent/absence_policy.rb
@@ -3,19 +3,24 @@
 class Agent::AbsencePolicy < ApplicationPolicy
   include CurrentAgentInPolicyConcern
 
-  # delegate to AgentPolicy: if I can manage an agent, I can manage her absences
-  delegate :can_manage_agent?, to: :agent_policy
+  def can_manage_absence?
+    agent_policy.can_manage_agent? || same_service_and_org_in_common?
+  end
 
-  alias show? can_manage_agent?
-  alias create? can_manage_agent?
-  alias cancel? can_manage_agent?
-  alias new? can_manage_agent?
-  alias update? can_manage_agent?
-  alias edit? can_manage_agent?
-  alias destroy? can_manage_agent?
-  alias versions? can_manage_agent?
+  alias show? can_manage_absence?
+  alias create? can_manage_absence?
+  alias cancel? can_manage_absence?
+  alias new? can_manage_absence?
+  alias update? can_manage_absence?
+  alias edit? can_manage_absence?
+  alias destroy? can_manage_absence?
+  alias versions? can_manage_absence?
 
   private
+
+  def same_service_and_org_in_common?
+    ___
+  end
 
   def agent_policy
     ::Agent::AgentPolicy.new(pundit_user, record.agent)
