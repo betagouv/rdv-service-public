35b1f8f9fd2c5028e501e93e1c236d51d5bfc7b3
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-04-19 16:54:00 +0200

    Keep an organisation in the webhook blueprint

--- app/blueprints/absence_blueprint.rb
+++ app/blueprints/absence_blueprint.rb
@@ -6,6 +6,13 @@ class AbsenceBlueprint < Blueprinter::Base
   fields :ical_uid, :title, :first_day, :end_day, :start_time, :end_time
   association :agent, blueprint: AgentBlueprint
 
+  # TODO: Supprimer quand la solution à ce problème est mise en place:
+  #   https://github.com/betagouv/rdv-solidarites.fr/pull/3456
+  field(:organisation) do |absence|
+    organisation = absence.agent.organisations.first
+    OrganisationBlueprint.render_as_hash(organisation) if organisation
+  end
+
   # rubocop:disable Style/SymbolProc
   field(:rrule) { _1.rrule }
   field(:ical) { _1.to_ical }
