c2bfe7611e8bed989c984966bf7fc1ccc03ebeaf
Victor Mours <victor.mours@beta.gouv.fr>
2023-05-05 16:26:09 +0200

    Correctif pour la suppression d'agents (#3502)
    * Add failing spec
    
    * green spec
    
    * more efficient implementation

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -127,7 +127,9 @@ class Agent < ApplicationRecord
   end
 
   def soft_delete
-    raise SoftDeleteError, "agent still has attached resources" if organisations.any? || plage_ouvertures.any? || absences.any?
+    still_has_attached_resources = organisations.any? || plage_ouvertures.any? { |r| !r.destroyed? } || absences.any? { |r| !r.destroyed? }
+
+    raise SoftDeleteError, "agent still has attached resources" if still_has_attached_resources
 
     sector_attributions.destroy_all
     update_columns(deleted_at: Time.zone.now, email_original: email, email: deleted_email, uid: deleted_email)
