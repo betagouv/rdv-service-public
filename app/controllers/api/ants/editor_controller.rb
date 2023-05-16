8e4fe185497f49cf651d7ab03a919dc751bc8757
Rahmane Ousmane <ousmanedev@gmail.com>
2023-05-16 10:36:01 +0200

    Improve coding style in Api::Ants::EditorController

--- app/controllers/api/ants/editor_controller.rb
+++ app/controllers/api/ants/editor_controller.rb
@@ -2,52 +2,53 @@
 
 class Api::Ants::EditorController < Api::Ants::BaseController
   def get_managed_meeting_points # rubocop:disable Naming/AccessorMethodName
-    response_body = lieux.map do |lieu|
-      public_entry_address, city_name, zip_code = lieu.address.split(", ")
-      {
-        id: lieu.id.to_s,
-        name: lieu.name,
-        longitude: lieu.longitude,
-        latitude: lieu.latitude,
-        public_entry_address: public_entry_address,
-        zip_code: zip_code,
-        city_name: city_name,
-      }
-    end
-
-    render json: response_body
+    render json: lieux.map { |lieu| lieu_infos(lieu) }
   end
 
   def available_time_slots
-    response_hash = {}
-
-    lieux.where(id: params[:meeting_point_ids]).each do |lieu|
-      response_hash[lieu.id] = creneaux(
-        lieu,
-        Date.parse(params[:start_date]),
-        Date.parse(params[:end_date])
-      ).map do |creneau|
-        { datetime: creneau.starts_at.strftime("%Y-%m-%dT%H:%MZ") }
-      end
-    end
-
-    render json: response_hash
+    render json: lieux.map { |lieu| [lieu.id, time_slots(lieu)] }.to_h
   end
 
   private
 
   def lieux
-    @lieux ||= Lieu.joins(:organisation).where(organisations: { verticale: :rdv_mairie })
+    lieux = Lieu.joins(:organisation).where(organisations: { verticale: :rdv_mairie })
+    lieux = lieux.where(id: params[:meeting_point_ids]) if params[:meeting_point_ids]
+    lieux
   end
 
-  def creneaux(lieu, start_date, end_date)
-    motif = lieu.organisation.motifs.first
+  def time_slots(lieu)
+    creneaux(lieu).map { |creneau| { datetime: creneau.starts_at.strftime("%Y-%m-%dT%H:%MZ") } }
+  end
 
+  def creneaux(lieu)
     Users::CreneauxSearch.new(
+      lieu: lieu,
       user: @current_user,
       motif: motif,
-      lieu: lieu,
-      date_range: (start_date..end_date)
+      date_range: date_range
     ).creneaux
+  end
+
+  def date_range
+    @date_range ||= (Date.parse(params[:start_date])..Date.parse(params[:end_date]))
+  end
+
+  def motif
+    @motif ||= lieux.first.organisation.motifs.first
+  end
+
+  def lieu_infos(lieu)
+    public_entry_address, city_name, zip_code = lieu.address.split(", ")
+
+    {
+      id: lieu.id.to_s,
+      name: lieu.name,
+      longitude: lieu.longitude,
+      latitude: lieu.latitude,
+      public_entry_address: public_entry_address,
+      zip_code: zip_code,
+      city_name: city_name,
+    }
   end
 end
