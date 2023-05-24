bfaefbbf4727444e22127eb248d6ce25c553b6f6
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-05-24 14:46:07 +0200

    Import icons

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -4,3 +4,4 @@
 //= link @gouvfr/dsfr/dist/dsfr.min.css
 //= link @gouvfr/dsfr/dist/dsfr.module.min.js
 //= link @gouvfr/dsfr/dist/dsfr.nomodule.min.js
+//= link @gouvfr/dsfr/dist/utility/icons/icons.min.css
