90a4fca5ae1fb76a442ffeac06379ea4bdbf0396
Romain Neuville <neuvilleromain@gmail.com>
2023-06-01 14:05:57 +0200

    with a new bookable_by option

--- spec/features/users/user_can_manage_collective_rdv_spec.rb
+++ spec/features/users/user_can_manage_collective_rdv_spec.rb
@@ -129,6 +129,7 @@ RSpec.describe "Adding a user to a collective RDV" do
     end
 
     it "with an invited user (Token), redirect to rdv with invitaiton_token refreshed" do
+      motif.update!(bookable_by: "agents_and_prescripteurs_and_invited_users")
       params.merge!(invitation_token: invitation_token)
       visit prendre_rdv_path(params)
 
@@ -147,6 +148,8 @@ RSpec.describe "Adding a user to a collective RDV" do
   context "Specific cases" do
     context "Invited User" do
       let(:user) { invited_user }
+      let!(:motif) { create(:motif, :collectif, bookable_by: "agents_and_prescripteurs_and_invited_users", organisation: organisation, service: service) }
+
 
       it "do not display revoked or full rdvs for reservation (invitation error)" do
         params.merge!(invitation_token: invitation_token)
