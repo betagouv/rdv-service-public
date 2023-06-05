d3e9e428301e3a0ec74eeced7dc95d3fb5c83d1a
Victor Mours <victor.mours@gmail.com>
2023-06-05 18:14:24 +0200

    Use external_link_to helper for simpler code and better a11y

--- app/helpers/dsfr_helper.rb
+++ app/helpers/dsfr_helper.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 module DsfrHelper
-  def dsfr_image_tag(path, alt:, classes: "")
+  def dsfr_image_tag(path, alt, html_options = {})
     tag.figure(class: "fr-content-media") do
       tag.div(class: "fr-content-media__img") do
-        image_tag(path, alt: alt, class: "fr-responsive-img #{classes}")
+        image_tag(path, alt: alt, class: "fr-responsive-img #{html_options[:class]}")
       end
     end
+  end
+
+  def external_link_to(name, url, html_options = {})
+    link_to(name, url, { target: "_blank", rel: "noopener", title: "#{name} - nouvel onglet" }.merge(html_options))
   end
 end
