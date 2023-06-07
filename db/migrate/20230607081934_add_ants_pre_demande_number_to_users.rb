1afb25afcfc7f5c53fec241e30eb0c492c075199
Rahmane Ousmane <ousmanedev@gmail.com>
2023-06-07 10:21:00 +0200

    Fix migration file

--- db/migrate/20230607081934_add_ants_pre_demande_number_to_users.rb
+++ db/migrate/20230607081934_add_ants_pre_demande_number_to_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddRdvExternalNumberToUser < ActiveRecord::Migration[7.0]
+class AddAntsPreDemandeNumberToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :ants_pre_demande_number, :string
   end
