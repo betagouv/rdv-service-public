a0209c8bdc52e831bd4e5550f43b8cbe0e677091
Rahmane Ousmane <ousmanedev@gmail.com>
2023-06-14 17:46:54 +0200

    Create/Delete appointments for all RDV users

--- app/models/concerns/ants/sync_rdv.rb
+++ app/models/concerns/ants/sync_rdv.rb
@@ -6,30 +6,34 @@ module Ants
     include Rails.application.routes.url_helpers
 
     included do
-      after_commit :create_appointment, on: :create, if: -> { organisation.rdv_mairie? }
-      before_commit :delete_appointment, on: :destroy, if: -> { organisation.rdv_mairie? }
-      after_commit :create_or_delete_appointment, on: :update, if: -> { organisation.rdv_mairie? }
+      after_commit :create_appointments, on: :create, if: -> { organisation.rdv_mairie? }
+      before_commit :delete_appointments, on: :destroy, if: -> { organisation.rdv_mairie? }
+      after_commit :create_or_delete_appointments, on: :update, if: -> { organisation.rdv_mairie? }
     end
 
     private
 
-    def create_appointment
-      Ants::CreateAppointment.perform_later(rdv_params)
+    def create_appointments
+      users.each do |user|
+        Ants::CreateAppointment.perform_later(rdv_params(user))
+      end
     end
 
-    def delete_appointment
-      Ants::DeleteAppointment.perform_later(rdv_params.except(:management_url))
+    def delete_appointments
+      users.each do |user|
+        Ants::DeleteAppointment.perform_later(rdv_params(user).except(:management_url))
+      end
     end
 
-    def create_or_delete_appointment
+    def create_or_delete_appointments
       return unless saved_change_to_status?
 
-      cancelled? ? delete_appointment : create_appointment
+      cancelled? ? delete_appointments : create_appointments
     end
 
-    def rdv_params
+    def rdv_params(user)
       {
-        application_id: users.first.ants_pre_demande_number,
+        application_id: user.ants_pre_demande_number,
         meeting_point: lieu.name,
         appointment_date: starts_at.strftime("%Y-%m-%d %H:%M:%S"),
         management_url: rdvs_short_url(self, host: organisation.domain.host_name),
