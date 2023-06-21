d0bc8f388f4f6a37114566794764445d6e1f0078
Victor Mours <victor.mours@gmail.com>
2023-06-21 11:28:37 +0200

    fix

--- scripts/clone_motifs.rb
+++ scripts/clone_motifs.rb
@@ -2,19 +2,19 @@
 
 # Permet de passer de 1 motif à 3 motifs pour les mairies
 def duplicate_motifs(source_motif)
-  motif_cni = source_motif.clone
+  motif_cni = source_motif.dup
   motif_cni.update!(
     name: "Carte d'identité",
     motif_category: MotifCategory.find_by(name: "Carte d'identité disponible sur le site de l'ANTS")
   )
 
-  motif_passport = source_motif.clone
+  motif_passport = source_motif.dup
   motif_passport.update!(
     name: "Passeport",
     motif_category: MotifCategory.find_by(name: "Passeport disponible sur le site de l'ANTS")
   )
 
-  motif_cni_passport = source_motif.clone
+  motif_cni_passport = source_motif.dup
   motif_cni_passport.update!(
     name: "Carte d'identité et passeport",
     motif_category: MotifCategory.find_by(name: "Carte d'identité et passeport disponible sur le site de l'ANTS")
