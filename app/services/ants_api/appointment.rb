ee828460d7ae2b97735de552c70d89037bc13de6
Rahmane Ousmane <ousmanedev@rahmanes-macbook-pro-2.home>
2023-07-18 21:16:03 +0200

    Raise error when Api request fails

--- app/services/ants_api/appointment.rb
+++ app/services/ants_api/appointment.rb
@@ -2,6 +2,8 @@
 
 module AntsApi
   class Appointment
+    class ApiRequestError < StandardError; end
+
     attr_reader :application_id, :meeting_point, :appointment_date, :management_url
 
     def initialize(application_id:, meeting_point:, appointment_date:, management_url:, editor_comment: nil)
@@ -22,19 +24,29 @@ module AntsApi
     end
 
     def create
-      Typhoeus.post(
-        "#{ENV['ANTS_RDV_API_URL']}/appointments",
-        params: to_request_params,
-        headers: self.class.headers
-      )
+      request do
+        Typhoeus.post(
+          "#{ENV['ANTS_RDV_API_URL']}/appointments",
+          params: to_request_params,
+          headers: self.class.headers
+        )
+      end
     end
 
     def delete
-      Typhoeus.delete(
-        "#{ENV['ANTS_RDV_API_URL']}/appointments",
-        params: to_request_params.except(:management_url),
-        headers: self.class.headers
-      )
+      request do
+        Typhoeus.delete(
+          "#{ENV['ANTS_RDV_API_URL']}/appointments",
+          params: to_request_params.except(:management_url),
+          headers: self.class.headers
+        )
+      end
+    end
+
+    private
+
+    def request(&block)
+      self.class.request(&block)
     end
 
     class << self
@@ -57,15 +69,26 @@ module AntsApi
         }
       end
 
+      def request(&block)
+        response = block.call
+        if response.failure?
+          raise(ApiRequestError, "code:#{response.response_code}, body:#{response.response_body}")
+        end
+
+        response.body.empty? ? {} : JSON.parse(response.body)
+      end
+
       private
 
       def load_appointments(application_id)
-        response = Typhoeus.get(
-          "#{ENV['ANTS_RDV_API_URL']}/status",
-          params: { application_ids: application_id },
-          headers: headers
-        )
-        response_body = response.body.empty? ? {} : JSON.parse(response.body)
+        response_body = request do
+          Typhoeus.get(
+            "#{ENV['ANTS_RDV_API_URL']}/status",
+            params: { application_ids: application_id },
+            headers: headers
+          )
+        end
+
         response_body.fetch(application_id, {}).fetch("appointments", [])
       end
     end
