467fdaf9fa6ac622c0f425e49484d68013948c4b
Victor Mours <victor.mours@gmail.com>
2023-07-18 11:51:46 +0200

    wip: adding spec to find offending translation keys

--- spec/lib/html_in_translation_keys_spec.rb
+++ spec/lib/html_in_translation_keys_spec.rb
@@ -1,13 +1,18 @@
 # frozen_string_literal: true
 
 RSpec.describe "Avoid usage of html_safe on translation keys" do
-  specify do
+  it "ne contient pas de clés de traduction qui n'utilise pas le suffixe _html mais qui contient du html" do
     I18n.backend.eager_load!
-    translations_json = I18n.backend.translations.to_json
-    translation_key_with_html_regex = /".*":".*<.*>.*"/
 
-    regex_matches = translation_key_with_html_regex.match(translations_json)
+    # Un bricolage pour trouver les clés potentielles qui pourraint necessiter un html_safe
+    translations_string = I18n.backend.translations[:fr].to_s
+    translation_key_with_html_regex = /:[^=]*[^(_html)]=>"[^"]*<[^"]*>[^"]*"/
 
-    byebug
+    regex_matches = translations_string.scan(translation_key_with_html_regex)
+    if regex_matches.any?
+      puts regex_matches
+    end
+
+    expect(regex_matches).to be_empty
   end
 end
