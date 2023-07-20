8b5a322df6b342d4bfa26e84cf73acdf9e5c829f
Romain Neuville <neuvilleromain@gmail.com>
2023-07-20 14:08:28 +0100

    fix misstake

--- app/controllers/api/v1/invitations_controller.rb
+++ app/controllers/api/v1/invitations_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::InvitationsController < Api::V1::AgentAuthBaseController
   end
 
   def set_user
-    @user = User.find_by(params[:user_id])
+    @user = User.find(params[:user_id])
     authorize(@user)
   rescue ActiveRecord::RecordNotFound
     render_error :not_found, not_found: :user
