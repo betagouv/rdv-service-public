195af20affbe6d6daaea25f80939dfac5b37c9f8
Romain Neuville <neuvilleromain@gmail.com>
2023-07-20 15:38:22 +0100

    replace alias with method (cause before_action and rubocop)

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@
 
 class Api::V1::UsersController < Api::V1::AgentAuthBaseController
   before_action :set_organisation, only: %i[show update]
-  before_action :set_user, only: %i[show update invite_get rdv_invitation_token]
+  before_action :set_user, only: %i[show update invite_get invite_post rdv_invitation_token]
 
   def index
     users = policy_scope(User)
@@ -53,8 +53,11 @@ class Api::V1::UsersController < Api::V1::AgentAuthBaseController
     assign_rdv_invitation_token if @user.rdv_invitation_token.nil?
     render json: { invitation_token: @user.rdv_invitation_token }
   end
-  # Todo remove this alias after rdvi migrated to the new rdv_invitation_token endpoint
-  alias invite_post rdv_invitation_token
+
+  # Todo remove this method after rdvi migrated to the new rdv_invitation_token endpoint
+  def invite_post
+    rdv_invitation_token
+  end
 
   private
 
