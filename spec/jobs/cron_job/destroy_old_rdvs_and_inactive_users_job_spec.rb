808cb7619360e522694e32453c2e9358df5091c4
Victor Mours <victor.mours@gmail.com>
2023-08-16 14:06:21 +0200

    destroy old users

--- spec/jobs/cron_job/destroy_old_rdvs_and_inactive_users_job_spec.rb
+++ spec/jobs/cron_job/destroy_old_rdvs_and_inactive_users_job_spec.rb
@@ -1,22 +1,27 @@
 # frozen_string_literal: true
 
-describe CronJob::DestroyOldRdvsJob do
-  it "only destroys Rdvs that are more than 2 years old" do
+describe CronJob::DestroyOldRdvsAndInactiveUsersJob do
+  it "only destroys Rdvs that are more than 2 years old and inactive users created 2 more than years ago" do
     rdv_occurring_25_months_ago = travel_to(25.months.ago) { create(:rdv, starts_at: Time.zone.today.change(hour: 16)) }
     rdv_occurring_23_months_ago = travel_to(23.months.ago) { create(:rdv, starts_at: Time.zone.today.change(hour: 16)) }
     rdv_occurring_11_months_ago = travel_to(11.months.ago) { create(:rdv, starts_at: Time.zone.today.change(hour: 16)) }
 
+    user_without_rdv_created_23_months_ago = travel_to(23.months.ago) { create(:user) }
+
     described_class.new.perform
+    perform_enqueued_jobs # to perform the DestroyInactiveUsers job
 
     expect(Rdv.all).to include(rdv_occurring_23_months_ago, rdv_occurring_11_months_ago)
     expect(Rdv.all).not_to include(rdv_occurring_25_months_ago)
+
+    expect(User.all).to match_array([user_without_rdv_created_23_months_ago, rdv_occurring_11_months_ago.users.first, rdv_occurring_23_months_ago.users.first])
   end
 
   it "does not call any webhook" do
     travel_to(25.months.ago) { create(:rdv, starts_at: Time.zone.today.change(hour: 16)) }
     expect do
       described_class.new.perform
-    end.not_to have_enqueued_job
+    end.not_to have_enqueued_job(WebhookJob)
   end
 
   it "actually deletes RDVs that were soft_deleted" do
