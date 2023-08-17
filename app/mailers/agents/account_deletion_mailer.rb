b0eefe8041bf4309b49ee69a0b7ca3bb9ca1b3fb
Victor Mours <victor.mours@gmail.com>
2023-08-17 10:54:17 +0200

    Add deletion warning email

---
+++ app/mailers/agents/account_deletion_mailer.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Agents::AccountDeletionMailer < ApplicationMailer
+  before_action do
+    @agent = params[:agent]
+  end
+
+  default to: -> { @agent.email }
+
+  def upcoming_deletion_warning
+    mail(subject: "Votre compte sur #{domain.name} sera supprimé dans 1 mois", domain_name: domain.name)
+  end
+
+  private
+
+  def domain
+    @agent.domain
+  end
+end
