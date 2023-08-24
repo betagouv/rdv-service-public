bb2dae21b4685b61b4b29471894906aed0a0b6a4
Victor Mours <victor.mours@gmail.com>
2023-08-24 14:27:14 +0200

    improve spec

--- spec/jobs/cron_job/inactive_agents_spec.rb
+++ spec/jobs/cron_job/inactive_agents_spec.rb
@@ -7,11 +7,18 @@ describe CronJob do
     agent_created_25_months_ago_with_warning = travel_to(25.months.ago) { create(:agent) }
     agent_created_25_months_ago_with_warning.update!(account_deletion_warning_sent_at: 33.days.ago)
 
+    agent_created_12_months_ago_with_warning = travel_to(12.months.ago) { create(:agent, account_deletion_warning_sent_at: Time.zone.now) }
+    agent_created_12_months_ago_without_warning = travel_to(12.months.ago) { create(:agent) }
+
     two_years_ago = 2.years.ago
     CronJob::WarnInactiveAgentsOfAccountDeletion.new.perform(two_years_ago)
     CronJob::DestroyInactiveAgents.new.perform(two_years_ago)
 
-    expect(Agent.all).to eq [agent_created_25_months_ago_without_warning]
+    expect(Agent.all).to match_array([
+                                       agent_created_25_months_ago_without_warning,
+                                       agent_created_12_months_ago_without_warning,
+                                       agent_created_12_months_ago_with_warning,
+                                     ])
 
     expect(agent_created_25_months_ago_without_warning.reload.account_deletion_warning_sent_at).to be_present
   end
