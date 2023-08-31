e38298c51779e7ea0cc4947ece6551643c69826e
Victor Mours <victor.mours@beta.gouv.fr>
2023-08-31 14:57:52 +0200

    Ajoute l'adresse email de l'agent dans le mail d'avertissement de suppression de compte (#3760)
    Add agent email adresse in warning email

--- spec/mailers/previews/agents/account_deletion_mailer_preview.rb
+++ spec/mailers/previews/agents/account_deletion_mailer_preview.rb
@@ -2,6 +2,6 @@
 
 class Agents::AccountDeletionMailerPreview < ActionMailer::Preview
   def upcoming_deletion_warning
-    Agents::AccountDeletionMailer.with(agent: Agent.last).upcoming_deletion_warning
+    Agents::AccountDeletionMailer.with(agent: Agent.where.not(email: nil).last).upcoming_deletion_warning
   end
 end
