8cf5f78948dc79319b5619313f1c87c14ce27767
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-09-05 15:42:20 +0200

    Fix specs

--- db/migrate/20230905090020_remove_deleted_at_from_rdvs.rb
+++ db/migrate/20230905090020_remove_deleted_at_from_rdvs.rb
@@ -2,9 +2,14 @@
 
 class RemoveDeletedAtFromRdvs < ActiveRecord::Migration[7.0]
   def change
+    change_column_null :receipts, :rdv_id, true
+
     reversible do |direction|
       direction.up do
-        Rdv.where.not(deleted_at: nil).destroy_all
+        Rdv.where.not(deleted_at: nil).each do |rdv|
+          rdv.skip_webhooks = true
+          rdv.destroy
+        end
       end
 
       remove_column :rdvs, :deleted_at, :datetime
