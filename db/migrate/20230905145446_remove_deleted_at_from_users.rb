af6e274c9ab523fd2a681a5336e3c27c534829bd
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-09-06 17:20:55 +0200

    Actually delete currently soft deleted users

--- db/migrate/20230905145446_remove_deleted_at_from_users.rb
+++ db/migrate/20230905145446_remove_deleted_at_from_users.rb
@@ -2,6 +2,15 @@
 
 class RemoveDeletedAtFromUsers < ActiveRecord::Migration[7.0]
   def change
+    reversible do |direction|
+      direction.up do
+        User.where.not(deleted_at: nil).each do
+          user.skip_webhooks = true
+          user.destroy
+        end
+      end
+    end
+
     remove_column :users, :deleted_at, :datetime
   end
 end
