d0bd9bc5161b2fe28aa6c5f6c1db8ad9d09821eb
Victor Mours <victor.mours@gmail.com>
2023-09-11 15:24:00 +0200

    wip

--- spec/features/agents/agents/invite_agents_spec.rb
+++ spec/features/agents/agents/invite_agents_spec.rb
@@ -1,9 +1,8 @@
 # frozen_string_literal: true
 
 RSpec.describe "Agent can incite another agent" do
+  let(:territory) { create(:territory) }
 
   context "in two different organisations" do
-
   end
-
 end
