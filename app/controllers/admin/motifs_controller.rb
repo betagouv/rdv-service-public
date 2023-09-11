5d6b13449a3a1dd1f5fbb1906663639df5f939e4
Victor Mours <victor.mours@beta.gouv.fr>
2023-09-11 15:11:21 +0200

    Correctif sur l'affichage de la sectorisation dans l'index des motifs (#3769)
    * Add failing spec
    
    * Fix inverted condition
    
    * Add missing scope and expand test

--- app/controllers/admin/motifs_controller.rb
+++ app/controllers/admin/motifs_controller.rb
@@ -14,7 +14,7 @@ class Admin::MotifsController < AgentAuthController
 
     @sectors_attributed_to_organisation_count = Sector.attributed_to_organisation(current_organisation).count
     @sectorisation_level_agent_counts_by_service = SectorAttribution.level_agent_grouped_by_service(current_organisation)
-    @display_sectorisation_level = current_organisation.motifs.where.not(sectorisation_level: Motif::SECTORISATION_LEVEL_DEPARTEMENT).any?
+    @display_sectorisation_level = current_organisation.motifs.active.where.not(sectorisation_level: Motif::SECTORISATION_LEVEL_DEPARTEMENT).any?
   end
 
   def new
