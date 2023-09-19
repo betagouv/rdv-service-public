1a16ef616410ecdcfb2ce7dc6f0cf9574a4cb84e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-09-19 16:02:30 +0200

    Move priority to constant

--- app/jobs/concerns/default_job_behaviour.rb
+++ app/jobs/concerns/default_job_behaviour.rb
@@ -3,6 +3,8 @@
 module DefaultJobBehaviour
   extend ActiveSupport::Concern
 
+  PRIORITY_OF_RETRIES = -20
+
   included do
     # Include job metadata in Sentry context
     around_perform do |job, block|
@@ -28,7 +30,7 @@ module DefaultJobBehaviour
     # Exponential backoff is n^4, so wait times between retries will be as follows:
     # attempt:  1   2    3    4   5    6    7    8    9     10    11  12  13  14   15   16   17   18   19   20
     # backoff:  1s, 16s, 81s, 4m, 10m, 21m, 40m, 68m, 109m, 166m, 4h, 6h, 8h, 11h, 14h, 18h, 23h, 29h, 36h, 44h
-    retry_on(StandardError, wait: :exponentially_longer, attempts: 20, priority: -20)
+    retry_on(StandardError, wait: :exponentially_longer, attempts: 20, priority: PRIORITY_OF_RETRIES)
 
     # Makes sure every failed attempt is logged to Sentry
     # (see: https://github.com/bensheldon/good_job#retries)
