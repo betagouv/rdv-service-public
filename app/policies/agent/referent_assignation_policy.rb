97ee2e8a7e43f4a8315606b0356c1a136deef665
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-09-20 17:02:05 +0200

    Supprime les branches `if current_organisation` qui ne sont jamais utilisés
    Elles avaient été ajoutées par anticipation mais créent finalement de la confusion
    car on se demande à quoi elles servent, et ça rend la policy beaucoup moins lisible.

--- app/policies/agent/referent_assignation_policy.rb
+++ app/policies/agent/referent_assignation_policy.rb
@@ -11,22 +11,10 @@ class Agent::ReferentAssignationPolicy < DefaultAgentPolicy
   protected
 
   def same_agent_territory?
-    if current_organisation.present?
-      @record.agent.organisations.map(&:territory_id).include? current_organisation.territory_id
-    else
-      (@record.agent.organisations.map(&:territory_id) & current_agent.organisations.map(&:territory_id)).any?
-    end
+    (@record.agent.organisations.map(&:territory_id) & current_agent.organisations.map(&:territory_id)).any?
   end
 
   def same_user_org?
-    if current_organisation.present?
-      if current_organisation.territory.visible_users_throughout_the_territory
-        (@record.user.organisation_ids & current_organisation.territory.organisation_ids).any?
-      else
-        @record.user.organisation_ids.include?(current_organisation.id)
-      end
-    else
-      (@record.user.organisation_ids & current_agent.organisation_ids).any?
-    end
+    (@record.user.organisation_ids & current_agent.organisation_ids).any?
   end
 end
