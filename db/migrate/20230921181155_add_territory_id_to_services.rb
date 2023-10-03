c7b0c6b0ef1c9294fbf2d89d9c74ee4ecf69aa32
Rahmane Ousmane <ousmanedev@gmail.com>
2023-10-03 08:37:09 +0100

    Add foreign key on services#territory_id

--- db/migrate/20230921181155_add_territory_id_to_services.rb
+++ db/migrate/20230921181155_add_territory_id_to_services.rb
@@ -2,6 +2,6 @@
 
 class AddTerritoryIdToServices < ActiveRecord::Migration[7.0]
   def change
-    add_column :services, :territory_id, :integer
+    add_reference :services, :territory, foreign_key: true, index: true
   end
 end
