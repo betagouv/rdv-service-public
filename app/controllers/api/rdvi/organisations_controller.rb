0463d724531fbf9ec490f767720da9a3fe571f6f
Romain Neuville <neuvilleromain@gmail.com>
2023-10-03 09:36:58 +0200

    remove commentary

--- app/controllers/api/rdvi/organisations_controller.rb
+++ app/controllers/api/rdvi/organisations_controller.rb
@@ -4,14 +4,8 @@ class Api::Rdvi::OrganisationsController < Api::V1::AgentAuthBaseController
   before_action :set_organisation
 
   def available_creneaux_count
-    # render json: { available_creneaux_count: 0 }
-    # return
-
     total_creneaux = calculate_total_creneaux
     render json: { available_creneaux_count: total_creneaux }
-
-    # Quid de l'affichage dynamique dans rdvi si ils créent de nouveaux créneaux entre temps ? webhooks ?
-    # Pas de cache possible car recalcul si nvx rdv collectif ou plage d'ouverture créée entre temps
   end
 
   private
@@ -56,8 +50,7 @@ class Api::Rdvi::OrganisationsController < Api::V1::AgentAuthBaseController
   end
 
   def calculate_collective_creneaux(motif, date_range)
-    rdvs = Rdv.collectif_and_available_for_reservation.where(motif: motif, starts_at: date_range)
-    rdvs.sum(&:remaining_seats)
+    Rdv.collectif_and_available_for_reservation.where(motif: motif, starts_at: date_range).sum(&:remaining_seats)
   end
 
   def calculate_individual_creneaux(motif, date_range)
