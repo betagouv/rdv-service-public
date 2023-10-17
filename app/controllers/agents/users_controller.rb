71e12fea71bb57e65e6ed42c292ecf7c713f7363
Victor Mours <victor.mours@gmail.com>
2023-10-17 15:30:56 +0200

    refacto

--- app/controllers/agents/users_controller.rb
+++ app/controllers/agents/users_controller.rb
@@ -7,6 +7,13 @@ class Agents::UsersController < AgentAuthController
 
   def search
     skip_authorization # On scope les usagers par organisation puis par territoire via de la logique métier plutôt qu'une policy Pundit
+    # Dans cette recherche, on autorise un périmètre plus grand que la policy de base, puisqu'on est en train d'ajouter un usager à l'organisation en créant un rdv
+
+    # On veut quand même vérifier que l'organisation demandée fait partie des organisations de l'agent
+    if current_agent.organisations.find_by(id: params[:organisation_id]).nil?
+      return head :forbidden
+    end
+
     user_scope = User.where.not(id: params[:exclude_ids]).search_by_text(params[:term])
 
     users_from_organisation = user_scope.joins(:user_profiles).where(user_profiles: { organisation_id: params[:organisation_id] }).limit(MAX_RESULTS)
@@ -14,38 +21,38 @@ class Agents::UsersController < AgentAuthController
     results_count = users_from_organisation.count
 
     users_from_territory = if results_count < MAX_RESULTS
-                             user_scope.joins(:territories).where(territories: current_agent.territories).limit(MAX_RESULTS - results_count).where.not(id: users_from_organisation.ids)
+                             user_scope.joins(:territories).where(territories: current_agent.territories)
+                               .where.not(id: users_from_organisation.ids)
+                               .limit(MAX_RESULTS - results_count)
                            else
                              []
                            end
-    if users_from_organisation.none? && users_from_territory.none?
-      render json: { results: [] }
-    else
 
-      render json: {
-        results: [
-          {
-            text: nil,
-            children: users_from_organisation.map do |user|
-              {
-                id: user.id,
-                text: UsersHelper.reverse_full_name_and_notification_coordinates(user),
-              }
-            end,
-          },
-          users_from_territory.first && {
-            text: "Usagers des autres organisations",
-            children: users_from_territory.map do |user|
-              {
-                id: user.id,
-                text: UsersHelper.reverse_full_name_and_notification_coordinates(user),
-              }
-            end,
+    results = []
 
-          },
-        ].compact,
-
+    if users_from_organisation.any?
+      results << {
+        text: nil,
+        children: users_from_organisation.map { |user| serialize(user) },
       }
     end
+
+    if users_from_territory.any?
+      results << {
+        text: "Usagers des autres organisations",
+        children: users_from_territory.map { |user| serialize(user) },
+      }
+    end
+
+    render json: { results: results }
+  end
+
+  private
+
+  def serialize(user)
+    {
+      id: user.id,
+      text: UsersHelper.reverse_full_name_and_notification_coordinates(user),
+    }
   end
 end
--- app/views/agents/users/search.json.jbuilder
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-json.results @users do |user|
-  json.id user.id
-  json.text reverse_full_name_and_notification_coordinates(user)
-end
