7025bbaf842fade4d1234851645e2858f99dda1e
Victor Mours <victor.mours@gmail.com>
2023-10-17 16:43:28 +0200

    fix specs

--- spec/support/select2_spec_helper.rb
+++ spec/support/select2_spec_helper.rb
@@ -2,7 +2,10 @@ module Select2SpecHelper
   def select_user(user)
     find(".collapse-add-user-selection .select2-selection").click
     find(".select2-search__field").send_keys(user.last_name[0..2])
-    find(".select2-results li.select2-results__option", text: user.reverse_full_name).click
+
+    result_selector = ".select2-results li.select2-results__option li"
+
+    find(result_selector, text: user.reverse_full_name).click
     expect(page).to have_selector("a[title='Modifier']")
   end
 
