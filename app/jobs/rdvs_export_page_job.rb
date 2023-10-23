a0e4a2eac02807bfd74c0c98647dcfd55aecfb3a
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-10-23 16:41:42 +0200

    Fix crash (calling `order` on Array instead of AR relation)

--- app/jobs/rdvs_export_page_job.rb
+++ app/jobs/rdvs_export_page_job.rb
@@ -2,7 +2,7 @@
 
 class RdvsExportPageJob < ExportJob
   def perform(rdv_ids, page_index, redis_key)
-    rows = RdvExporter.rows_from_rdvs(Rdv.where(id: rdv_ids)).order(starts_at: :desc)
+    rows = RdvExporter.rows_from_rdvs(Rdv.where(id: rdv_ids).order(starts_at: :desc))
 
     redis_connection = Redis.new(url: Rails.configuration.x.redis_url)
     redis_connection.hset(redis_key, page_index, rows.to_json)
