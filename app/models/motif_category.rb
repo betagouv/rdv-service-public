8b021d519c9100b39b5245e3e245069167878af0
Victor Mours <victor.mours@gmail.com>
2023-10-26 15:57:05 +0200

    fix typo

--- app/models/motif_category.rb
+++ app/models/motif_category.rb
@@ -9,9 +9,9 @@ class MotifCategory < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :short_name, presence: true, uniqueness: true
 
-  scope :requires_ants_predemande_number, -> { where(name: Api::Ants::EditorController::ANTS_MOTIF_CATEROGY_NAMES) }
+  scope :requires_ants_predemande_number, -> { where(name: Api::Ants::EditorController::ANTS_MOTIF_CATEGORY_NAMES) }
 
   def requires_ants_predemande_number?
-    name.in?(Api::Ants::EditorController::ANTS_MOTIF_CATEROGY_NAMES)
+    name.in?(Api::Ants::EditorController::ANTS_MOTIF_CATEGORY_NAMES)
   end
 end
