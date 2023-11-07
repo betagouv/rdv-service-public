89c9621164f495a913ac4b208ad88b1d0bc5c336
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-11-07 16:25:55 +0100

    Rename ambiguous method

--- app/models/service.rb
+++ app/models/service.rb
@@ -27,7 +27,8 @@ class Service < ApplicationRecord
 
   ## -
 
-  def self.all_for_territory(territory)
+  # Retourne les services des agents des orgas du territoire
+  def self.used_by_agents_of_territory(territory)
     agents_of_territory = Agent.joins(:organisations).merge(territory.organisations)
     joins(:agent_services).where(agent_services: { agents: agents_of_territory }).distinct
   end
