cf5a5b5ecf99e36481fd98d81b532b23031331de
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-11-08 12:57:47 +0100

    Add specs for service change

--- app/services/admin_changes_agent_services.rb
+++ app/services/admin_changes_agent_services.rb
@@ -7,9 +7,16 @@ class AdminChangesAgentServices
     @new_services = Service.where(id: new_service_ids)
   end
 
+  validate :at_least_one_service
   validate :removed_services_dont_have_plages
 
   private
+
+  def at_least_one_service
+    if @new_services.empty?
+      errors.add(:service_ids, "Un agent doit avoir au moins un service")
+    end
+  end
 
   def removed_services_dont_have_plages
     removed_services.each do |removed_service|
--- spec/features/territory_admins/territory_admin_can_manage_agents_spec.rb
+++ spec/features/territory_admins/territory_admin_can_manage_agents_spec.rb
@@ -40,4 +40,44 @@ describe "territory admin can manage agents", type: :feature do
       expect { click_on "Enregistrer" }.to change { agent.reload.teams }.to([team_b])
     end
   end
+
+  describe "changing agent service" do
+    let(:service_a) { create(:service, name: "A") }
+    let(:service_b) { create(:service, name: "B") }
+    let(:service_c) { create(:service, name: "C") }
+    let!(:edited_agent) { create(:agent, admin_role_in_organisations: [organisation], services: [service_a, service_b]) }
+
+    before do
+      current_agent = create(:agent, admin_role_in_organisations: [organisation], role_in_territories: [territory], services: [service_c])
+      # l'agent qui édite doit avoir les droits d'édition
+      create(:agent_territorial_access_right, agent: current_agent, territory: territory, allow_to_manage_access_rights: true)
+      # l'agent édité doit avoir un agent_territorial_access_right car sinon le formulaire plante
+      create(:agent_territorial_access_right, agent: edited_agent, territory: territory)
+
+      login_as(current_agent, scope: :agent)
+      visit edit_admin_territory_agent_path(territory_id: territory.id, id: edited_agent.id)
+    end
+
+    it "allows adding and removing services" do
+      select service_c.name, from: "Services"
+      unselect service_b.name, from: "Services"
+      expect { click_on "Modifier les services" }.to change { edited_agent.reload.services.to_set }
+        .from([service_a, service_b].to_set)
+        .to([service_a, service_c].to_set)
+    end
+
+    it "forbids removing a service that still have plages" do
+      create(:plage_ouverture, agent: edited_agent, motifs: [create(:motif, service: service_b)])
+      unselect service_b.name, from: "Services"
+      expect { click_on "Modifier les services" }.not_to change { edited_agent.reload.services.to_set }
+      expect(page).to have_content("L'agent a toujours des plages d'ouverture pour le service : B")
+    end
+
+    it "forbids removing last service" do
+      unselect service_a.name, from: "Services"
+      unselect service_b.name, from: "Services"
+      expect { click_on "Modifier les services" }.not_to change { edited_agent.reload.services.to_set }
+      expect(page).to have_content("Un agent doit avoir au moins un service")
+    end
+  end
 end
