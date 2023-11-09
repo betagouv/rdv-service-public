c1584196c96fe0ed05d0eb8661cad3c2c81a1c7a
Neuville Romain <neuvilleromain@gmail.com>
2023-11-09 14:11:03 +0100

    Participations Policy fix pour l'api (#3864)
    Participations Policy fix pour l'api (#3864)

--- app/controllers/api/v1/participations_controller.rb
+++ app/controllers/api/v1/participations_controller.rb
@@ -11,6 +11,14 @@ class Api::V1::ParticipationsController < Api::V1::AgentAuthBaseController
 
   private
 
+  # On fait cela sinon current_organisation est nil
+  # Cela empêche can_access_others_planning? de renvoyer une valeur dans le scope de la policy RDV et on limite les rdvs visibles même quand on doit y avoir accés
+  # TODO : Revoir le fonctionnement de current_organisation dans les scopes
+
+  def current_organisation
+    @current_organisation ||= Participation.find(params[:id]).rdv.organisation
+  end
+
   def participation_params
     params.require(:participation).permit(:status)
   end
