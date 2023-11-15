1f426852cb795b86bb79bfa33f2e28add3241b27
Victor Mours <victor.mours@gmail.com>
2023-11-15 15:08:34 +0100

    sort services by activation status and display agents count

--- app/controllers/admin/territories/services_controller.rb
+++ app/controllers/admin/territories/services_controller.rb
@@ -1,6 +1,11 @@
 class Admin::Territories::ServicesController < Admin::Territories::BaseController
   def edit
     authorize current_territory
+
+    activated_services = sort_and_format(current_territory.services)
+    other_services = sort_and_format(Service.where.not(id: current_territory.service_ids))
+
+    @services = activated_services + other_services
   end
 
   def update
@@ -14,5 +19,18 @@ class Admin::Territories::ServicesController < Admin::Territories::BaseControlle
 
   def services_params
     params.require(:territory).permit(service_ids: [])
+  end
+
+  def sort_and_format(services)
+    services.sort_by { |s| I18n.transliterate(s.name).downcase }.map do |service|
+      label = service.name
+
+      agents_count = service.agents.active.merge(current_territory.organisations_agents).count
+      if agents_count > 0
+        label += " (#{agents_count} #{'agent'.pluralize(agents_count)})"
+      end
+
+      [label, service.id]
+    end
   end
 end
