0045f6488e3da828358bb510de73fb40c2960c4a
Victor Mours <victor.mours@gmail.com>
2023-11-20 11:00:40 +0100

    fix

--- spec/features/users/user_can_search_rdv_on_rdv_mairie_spec.rb
+++ spec/features/users/user_can_search_rdv_on_rdv_mairie_spec.rb
@@ -2,7 +2,7 @@ describe "User can search rdv on rdv mairie" do
   include_context "rdv_mairie_api_authentication"
 
   let(:now) { Time.zone.parse("2021-12-13 8:00") }
-  let!(:territory) { create(:territory, :mairies) }
+  let!(:territory) { create(:territory, :mairies, departement_number: "MA") }
   let!(:organisation) { create(:organisation, :with_contact, territory: territory) }
   let(:service) { create(:service) }
   let!(:cni_motif) do
