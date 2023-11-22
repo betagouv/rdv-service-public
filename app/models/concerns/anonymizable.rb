7fecad88967be5f33519274b17e515fb68fbfec4
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-11-22 19:41:09 +0100

    Ajout d'un script d'anonymisation / troncage de la base + doc

--- app/models/concerns/anonymizable.rb
+++ app/models/concerns/anonymizable.rb
@@ -1,21 +1,38 @@
 module Anonymizable
+  extend ActiveSupport::Concern
+
   def anonymize_personal_data_columns!
-    personal_data_columns = self.class.columns.select do |column|
-      column.name.in?(self.class.personal_data_column_names)
-    end
-
-    anonymized_attributes = personal_data_columns.to_h do |column|
-      [column.name, anonymous_value(column)]
-    end
-
-    update_columns(anonymized_attributes) # rubocop:disable Rails/SkipsModelValidations
+    update_columns(self.class.anonymized_attributes) # rubocop:disable Rails/SkipsModelValidations
   end
 
-  private
+  class_methods do
+    def anonymize_all!(scope)
+      raise "L'anonymisation en masse est désactivée en production pour éviter les catastrophes" if Rails.env.production?
 
-  def anonymous_value(column)
-    if column.type.in?(%i[string text])
-      "[valeur anonymisée]"
+      scope.update_all(anonymized_attributes) # rubocop:disable Rails/SkipsModelValidations
+    end
+
+    def anonymized_attributes
+      personal_data_columns = columns.select do |column|
+        column.name.in?(personal_data_column_names)
+      end
+
+      personal_data_columns.to_h do |column|
+        [column.name, anonymous_value(column)]
+      end
+    end
+
+    def anonymous_value(column)
+      case column.type.to_sym
+      when :string, :text
+        "[valeur anonymisée]"
+      when :date
+        column.null ? nil : Date.new(1900, 1, 1)
+      when :integer
+        column.null ? nil : 0
+      else
+        raise "Don't know how to anonymize column #{column.name} (#{column.type})"
+      end
     end
   end
 end
