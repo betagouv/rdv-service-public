94bd07630d92daa2f5adba367a17c8c62f11ba9e
Victor Mours <victor.mours@gmail.com>
2023-11-28 18:54:11 +0100

    fix

--- app/controllers/admin/rdvs_collectifs/motifs_controller.rb
+++ app/controllers/admin/rdvs_collectifs/motifs_controller.rb
@@ -1,6 +1,6 @@
 class Admin::RdvsCollectifs::MotifsController < AgentAuthController
   def index
-    @motifs = policy_scope(Motif, policy_scope_class: Agent::MotifPolicy)
+    @motifs = policy_scope(Motif, policy_scope_class: Agent::MotifPolicy::Scope)
       .available_motifs_for_organisation_and_agent(current_organisation, current_agent).collectif
   end
 
