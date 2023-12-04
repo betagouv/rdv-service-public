2a91efd1a37e89d8f550538ee5af4285d4e16088
Victor Mours <victor.mours@gmail.com>
2023-12-04 12:18:06 +0100

    lint fix

--- app/lib/anonymizer.rb
+++ app/lib/anonymizer.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 class Anonymizer
   def self.anonymize_all_data!
     anonymize_user_data!
--- spec/lib/anonymizer_spec.rb
+++ spec/lib/anonymizer_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe Anonymizer do
   let!(:user) { create(:user) }
   let!(:prescripteur) { create(:prescripteur) }
