3c548d5d5d43b354bc2cf6f70c3fb1aa6bde759c
Victor Mours <victor.mours@gmail.com>
2023-12-07 12:15:41 +0100

    Add sanity checks and anonymize data when loading locally

--- scripts/anonymize_database.rb
+++ scripts/anonymize_database.rb
@@ -1,6 +1,18 @@
-# commencer par charger en local un dump de production
 Anonymizer.anonymize_all_data!
 
-# en local, faire un pg_dump -d lapin_development --file=tmp/anonymized_dump.sql
-# puis compresser le dump
-# puis scalingo --app=rdv-service-public-etl --region=osc-secnum-fr1 run --file=tmp/anonymized_dump.sql.zip "bash"
+# Sanity checks
+if User.where.not(first_name: "[valeur anonymisée]").any?
+  raise "Certains usagers n'ont pas été anonymisés !"
+end
+
+if Agent.where.not(last_name: "[valeur anonymisée]").any?
+  raise "Certains agents n'ont pas été anonymisés !"
+end
+
+if Prescripteur.where.not(first_name: "[valeur anonymisée]").any?
+  raise "Certains prescripteurs n'ont pas été anonymisés !"
+end
+
+if Receipt.where.not(content: "[valeur anonymisée]").any?
+  raise "Certains receipts n'ont pas été anonymisés !"
+end
