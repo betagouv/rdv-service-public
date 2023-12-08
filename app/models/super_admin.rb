97f5065559745ee0205683a0a1b6eee9163d5f8f
Romain Neuville <neuvilleromain@gmail.com>
2023-12-08 11:54:07 +0100

    add fields for super admin dashboard, sign_out all scopes when super_admin disconnect

--- app/models/super_admin.rb
+++ app/models/super_admin.rb
@@ -6,6 +6,7 @@ class SuperAdmin < ApplicationRecord
   # Validations
   validates :first_name, presence: true
   validates :last_name, presence: true
+  validates :email, presence: true
 
   devise :authenticatable
 
