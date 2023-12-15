e792a734a74ec83de13276112f4f4f2feac60e7b
Romain Neuville <neuvilleromain@gmail.com>
2023-12-15 22:50:35 +0100

    fix method

--- app/policies/super_admin/super_admin_policy.rb
+++ app/policies/super_admin/super_admin_policy.rb
@@ -1,4 +1,8 @@
 class SuperAdmin::SuperAdminPolicy < DefaultSuperAdminPolicy
+  def privileges_for_record?
+    super_admin_member? || (record.support_member? && support_member?)
+  end
+
   alias index? team_member?
   alias show? team_member?
   alias new? team_member?
@@ -6,10 +10,4 @@ class SuperAdmin::SuperAdminPolicy < DefaultSuperAdminPolicy
   alias edit? privileges_for_record?
   alias update? privileges_for_record?
   alias destroy? privileges_for_record?
-
-  private
-
-  def privileges_for_record?
-    super_admin_member? || (record.support_member? && support_member?)
-  end
 end
