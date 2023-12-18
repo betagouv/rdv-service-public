bcb4569eb311098b45dd201e011cf39e57f584a7
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2023-12-18 17:30:24 +0100

    Suite de l'ajout des noms sur les super admins (#3930)
    * Extract names from GitHub data when creating first super admin in dev
    
    * Add NOT NULL on SuperAdmin names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_14_104204) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_14_155817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
@@ -584,8 +584,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_14_104204) do
     t.string "email", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "first_name"
-    t.string "last_name"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
   end
 
   create_table "teams", force: :cascade do |t|
