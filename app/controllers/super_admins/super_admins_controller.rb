903931cc69ff49564c048c39ff67ef28006d7190
Romain Neuville <neuvilleromain@gmail.com>
2023-12-19 10:19:16 +0100

    fix links for delete agent and good_job, avoid privilége escalation

--- app/controllers/super_admins/super_admins_controller.rb
+++ app/controllers/super_admins/super_admins_controller.rb
@@ -1,4 +1,20 @@
 module SuperAdmins
   class SuperAdminsController < SuperAdmins::ApplicationController
+    before_action :check_privilege_escalation, only: %i[update]
+
+    def check_privilege_escalation
+      return not_authorized_to_update if privilege_escalation?
+    end
+
+    private
+
+    def not_authorized_to_update
+      flash[:error] = "Vous n'êtes pas autorisé à modifier le role de super_admin"
+      redirect_to(request.referer)
+    end
+
+    def privilege_escalation?
+      current_super_admin.support_member? && resource_params[:role] == "super_admin"
+    end
   end
 end
