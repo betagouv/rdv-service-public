eae223c4d0e1049696e1e782a5216973cfb80022
Victor Mours <victor.mours@gmail.com>
2023-12-20 12:15:29 +0100

    cleanup

--- app/services/agent_prescription_search_context.rb
+++ app/services/agent_prescription_search_context.rb
@@ -8,18 +8,4 @@ class AgentPrescriptionSearchContext < WebSearchContext
     url_helpers = Rails.application.routes.url_helpers
     url_helpers.recapitulatif_admin_prescription_path(params)
   end
-
-  def current_step
-    if !service_selected?
-      :service_selection
-    elsif !motif_name_and_type_selected?
-      :motif_selection
-    elsif requires_lieu_selection?
-      :lieu_selection
-    elsif requires_organisation_selection?
-      :organisation_selection
-    else
-      :creneau_selection
-    end
-  end
 end
