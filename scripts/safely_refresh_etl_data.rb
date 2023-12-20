e66ba62dd533a7cead4956e58f58d54bc462bb57
Victor Mours <victor.mours@gmail.com>
2023-12-20 15:58:08 +0100

    wip

--- scripts/safely_refresh_etl_data.rb
+++ scripts/safely_refresh_etl_data.rb
@@ -1,1 +1,7 @@
-scalingo_api_token = 
+home_path = `echo $HOME`.squish
+
+scalingo_auth = JSON.parse(File.read("#{home_path}/.config/scalingo/auth"))
+
+scalingo_api_token = scalingo_auth.dig("auth_config_data", "auth.scalingo.com", "tokens", "token")
+
+puts scalingo_api_token
