9040fb53a8aa95794bc520d8b01abb34bef07aa8
Romain Neuville <neuvilleromain@gmail.com>
2024-01-05 17:11:20 +0100

    basic test happy path

--- spec/features/agents/agent_can_prescribe_rdvs_spec.rb
+++ spec/features/agents/agent_can_prescribe_rdvs_spec.rb
@@ -1,31 +1,59 @@
-describe "Agents can try the user-facing online booking pages" do
+describe "Agents can prescribe rdvs for a selected user" do
+  let(:now) { Time.zone.parse("2024-01-05 16h00") }
   let!(:territory) { create(:territory) }
   let!(:organisation1) { create(:organisation, territory: territory) }
   let!(:organisation2) { create(:organisation, territory: territory) }
-  let!(:agent) { create(:agent, admin_role_in_organisations: [organisation]) }
+  let!(:agent) { create(:agent, admin_role_in_organisations: [organisation1]) }
+  let!(:user) { create(:user, organisations: [organisation1]) }
+  let!(:motif1) { create(:motif, organisation: organisation1, service: agent.services.first) }
+  let!(:motif2) { create(:motif, organisation: organisation2, service: agent.services.first) }
+  let!(:motif3) { create(:motif, organisation: organisation2) }
+  let!(:lieu1) { create(:lieu, organisation: organisation1) }
+  let!(:lieu2) { create(:lieu, organisation: organisation1) }
+  let!(:lieu_org2) { create(:lieu, organisation: organisation1) }
+  let!(:plage_ouverture1) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif1], lieu: lieu1, organisation: organisation1) }
+  let!(:plage_ouverture2) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif1], lieu: lieu2, organisation: organisation1) }
+  let!(:plage_ouverture3) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif2], lieu: lieu_org2, organisation: organisation2) }
+  let!(:plage_ouverture4) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif3], lieu: lieu_org2, organisation: organisation2) }
 
-  before do
-    first_day = Date.parse("2023/08/01")
-    travel_to(first_day.beginning_of_day)
-    motif = create(:motif, organisation: organisation, service: agent.services.first)
-    motif.plage_ouvertures << create(:plage_ouverture, first_day: first_day, organisation: organisation, agent: agent)
-  end
-
-  it "shows the online booking forms, until the creneau selection" do
+  it "works, happy path", js: true do
     login_as(agent, scope: :agent)
-    visit public_link_to_org_path(organisation_id: organisation.id)
+    visit admin_organisation_agent_searches_path(organisation1, user_ids: [user.id])
+    click_link "élargir votre recherche"
+    expect(page).to have_content("Prescription")
+    expect(page).to have_content("pour #{user.full_name}")
+    # Select Service
     expect(page).to have_content("Sélectionnez le service avec qui vous voulez prendre un RDV")
-    click_link(agent.services.first.name)
-    expect(page).to have_content("Sélectionnez un lieu de RDV :")
-    click_link("Prochaine disponibilité")
-    expect(page).to have_content("Sélectionnez un créneau :")
-  end
-
-  it "works on the RDV_MAIRIE domain" do
-    login_as(agent, scope: :agent)
-    visit "http://www.rdv-mairie-test.localhost/#{public_link_to_org_path(organisation_id: organisation.id)}"
-    expect(page).to have_content("Sélectionnez le service avec qui vous voulez prendre un RDV")
+    expect(page).to have_content(motif1.service.name)
+    expect(page).to have_content(motif2.service.name)
+    find("h3", text: motif1.service.name).click
+    # Select Motif
+    expect(page).to have_content("Sélectionnez le motif de votre RDV")
+    expect(page).to have_content(motif1.name)
+    expect(page).to have_content(motif2.name)
+    find("h3", text: motif1.name).click
+    # Select Lieu
+    expect(page).to have_content(lieu1.name)
+    expect(page).to have_content(lieu2.name)
+    find(".card-title", text: /#{lieu1.name}/).ancestor(".card").find("a.stretched-link").click
+    expect(page).to have_content(lieu1.name)
+    # Select créneau
+    first(:link, "11:00").click
+    # Display Recapitulatif
+    expect(page).to have_content("Motif : #{motif1.name}")
+    expect(page).to have_content("Lieu : #{lieu1.name}")
+    expect(page).to have_content("Date du rendez-vous :")
+    expect(page).to have_content("Usager : #{user.full_name}")
+    click_button "Confirmer le rdv"
+    # Display Confirmation
+    expect(page).to have_content("Rendez-vous confirmé")
+    expect(Rdv.count).to eq(1)
+    expect(Rdv.last.users.first).to eq(user)
+    expect(Rdv.last.organisation).to eq(organisation1)
+    expect(Rdv.last.motif).to eq(motif1)
+    expect(Rdv.last.lieu).to eq(lieu1)
+    expect(Rdv.last.starts_at).to eq(Time.zone.parse("2024-02-05 11:00"))
+    expect(Rdv.last.created_by).to eq("prescripteur")
+    expect(Rdv.last.participations.last.created_by).to eq("prescripteur")
   end
 end
-
-# Revoir ce test pour le happy path de agents prescripteurs
