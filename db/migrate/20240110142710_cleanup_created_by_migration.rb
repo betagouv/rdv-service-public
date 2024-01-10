53ef1c1ca46d1b2880b558ee3d0298972d22afcf
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-10 16:13:08 +0100

    Follow safety instructions: https://github.com/ankane/strong_migrations#setting-not-null-on-an-existing-column

--- db/migrate/20240110142710_cleanup_created_by_migration.rb
+++ db/migrate/20240110142710_cleanup_created_by_migration.rb
@@ -1,8 +1,8 @@
 class CleanupCreatedByMigration < ActiveRecord::Migration[7.0]
   def change
     safety_assured do
-      change_column_null :rdvs, :created_by_type, false
-      change_column_null :participations, :created_by_type, false
+      add_check_constraint :rdvs, "created_by_type IS NOT NULL", name: "rdvs_created_by_type_null", validate: false
+      add_check_constraint :participations, "created_by_type IS NOT NULL", name: "participations_created_by_type_null", validate: false
 
       remove_column :rdvs, :created_by, :integer, index: true
       remove_column :participations, :created_by, :created_by
