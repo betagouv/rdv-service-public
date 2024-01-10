cf7200a623d69022d3e77715af43af03e1a81471
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-10 17:41:05 +0100

    Improve comments

--- db/migrate/20240110142710_cleanup_created_by_migration.rb
+++ db/migrate/20240110142710_cleanup_created_by_migration.rb
@@ -1,10 +1,6 @@
 class CleanupCreatedByMigration < ActiveRecord::Migration[7.0]
   def change
     safety_assured do
-      # See https://github.com/ankane/strong_migrations#setting-not-null-on-an-existing-column
-      add_check_constraint :rdvs, "created_by_type IS NOT NULL", name: "rdvs_created_by_type_null", validate: false
-      add_check_constraint :participations, "created_by_type IS NOT NULL", name: "participations_created_by_type_null", validate: false
-
       remove_column :rdvs, :created_by, :integer, index: true
       remove_column :participations, :created_by, :created_by
 
@@ -19,5 +15,10 @@ class CleanupCreatedByMigration < ActiveRecord::Migration[7.0]
         end
       end
     end
+
+    # Première étape de :
+    # https://github.com/ankane/strong_migrations#setting-not-null-on-an-existing-column
+    add_check_constraint :rdvs, "created_by_type IS NOT NULL", name: "rdvs_created_by_type_null", validate: false
+    add_check_constraint :participations, "created_by_type IS NOT NULL", name: "participations_created_by_type_null", validate: false
   end
 end
--- db/migrate/20240110163312_cleanup_created_by_migration_episode_two.rb
+++ db/migrate/20240110163312_cleanup_created_by_migration_episode_two.rb
@@ -1,5 +1,6 @@
 class CleanupCreatedByMigrationEpisodeTwo < ActiveRecord::Migration[7.0]
-  # See https://github.com/ankane/strong_migrations#setting-not-null-on-an-existing-column
+  # Seconde étape de :
+  # https://github.com/ankane/strong_migrations#setting-not-null-on-an-existing-column
   def change
     validate_check_constraint :rdvs, name: "rdvs_created_by_type_null"
     validate_check_constraint :participations, name: "participations_created_by_type_null"
