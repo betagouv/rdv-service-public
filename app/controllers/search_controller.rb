1166f2a819cad324ad450e99c4e053871ccd9c22
Victor Mours <victor.mours@beta.gouv.fr>
2024-01-11 18:01:14 +0100

    Permet aux agents prescripteurs de continuer à utiliser le front (#3972)
    dont redirect agent prescripteur using the front end

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,7 @@ class SearchController < ApplicationController
 
   def search_rdv
     # TODO : public_link_organisation_id has to work if agent is logged in ?
-    if current_agent && query_params[:public_link_organisation_id].nil?
+    if current_agent && params[:user_ids].present?
       redirect_to search_creneau_admin_organisation_prescription_path(session[:agent_prescripteur_organisation_id], agent_search_params)
     end
     @context = if invitation?
