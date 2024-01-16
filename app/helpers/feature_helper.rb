b169bb138bd25bf93543374282f13a70654de176
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-16 17:32:45 +0100

    Activer prescription interne pour les territoires CDAD (#3981)

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -1,5 +1,8 @@
 module FeatureHelper
   def show_agent_prescription_feature?
-    current_organisation.territory.departement_number.in?(%w[83]) || Rails.env.development? || ENV["RDV_SOLIDARITES_INSTANCE_NAME"] == "DEMO"
+    current_organisation.territory.name.starts_with?("CDAD") ||
+      current_organisation.territory.departement_number.in?(%w[83]) ||
+      Rails.env.development? ||
+      ENV["RDV_SOLIDARITES_INSTANCE_NAME"] == "DEMO"
   end
 end
