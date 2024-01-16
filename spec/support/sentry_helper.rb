a603676a1404445c0242485d1a6cfb729cdbd6fd
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-16 18:15:46 +0100

    Try a hopeless thing

--- spec/support/sentry_helper.rb
+++ spec/support/sentry_helper.rb
@@ -1,9 +1,11 @@
 # Sentry test helper setup, see:
 # https://github.com/getsentry/sentry-ruby/blob/5fc11d9/sentry-ruby/lib/sentry/test_helper.rb
 def stub_sentry_events
-  around do |example|
+  before do
     setup_sentry_test
-    example.run
+  end
+
+  after do
     teardown_sentry_test
   end
 end
