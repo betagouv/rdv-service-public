50f6264775669f8aacc5b4afcabd87528220b139
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-18 15:25:58 +0100

    Revert "Rails 7.1 (étape 1) (#3925)"
    This reverts commit 927e03f3

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -4,15 +4,15 @@ RSpec.configure do |config|
   # Specify a root folder where Swagger JSON files are generated
   # NOTE: If you're using the rswag-api to serve API descriptions, you'll need
   # to ensure that it's configured to serve Swagger from the same folder
-  config.openapi_root = Rails.root.join("swagger").to_s
+  config.swagger_root = Rails.root.join("swagger").to_s
 
   # Define one or more Swagger documents and provide global metadata for each one
   # When you run the 'rswag:specs:swaggerize' rake task, the complete Swagger will
-  # be generated at the provided relative path under openapi_root
+  # be generated at the provided relative path under swagger_root
   # By default, the operations defined in spec files are added to the first
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
-  config.openapi_specs = {
+  config.swagger_docs = {
     "v1/api.json" => {
       openapi: "3.0.1",
       info: {
@@ -561,8 +561,8 @@ RSpec.configure do |config|
   }
 
   # Specify the format of the output Swagger file when running 'rswag:specs:swaggerize'.
-  # The openapi_specs configuration option has the filename including format in
+  # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.openapi_format = :json
+  config.swagger_format = :json
 end
