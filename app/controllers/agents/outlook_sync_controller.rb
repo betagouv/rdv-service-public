27273b3080e7fe5b971f84d6bd21a10b46906fd9
Victor Mours <victor.mours@gmail.com>
2024-01-19 15:29:47 +0100

    Better copy when Outlook is synced

--- app/controllers/agents/outlook_sync_controller.rb
+++ app/controllers/agents/outlook_sync_controller.rb
@@ -19,9 +19,7 @@ class Agents::OutlookSyncController < AgentAuthController
     authorize current_agent, :current_agent_or_admin_in_record_organisation?
     current_agent.update!(outlook_disconnect_in_progress: true)
     Outlook::MassDestroyEventJob.perform_later(current_agent)
-    flash[:notice] = "Votre compte Outlook est bien en cours de déconnexion. " \
-                     "Cette action peut prendre plusieurs minutes, nécessaires à la suppression de vos événements dans votre agenda. " \
-                     "Rechargez la page un peu plus tard pour voir l'état de la déconnexion."
+    flash[:notice] = "Votre compte Outlook est bien en cours de déconnexion. "
     redirect_to agents_calendar_sync_outlook_sync_path
   end
 
