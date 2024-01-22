4c31d9ecd4dbf9fe85ac09dcbd233211c69dbfc8
Victor Mours <victor.mours@gmail.com>
2024-01-22 11:48:34 +0100

    using 4 processes makes the pg_restore faster on scalingo

--- scripts/etl.sh
+++ scripts/etl.sh
@@ -42,12 +42,11 @@ etl_addon_id="$( scalingo --region osc-secnum-fr1 --app rdv-service-public-etl a
 
 
 echo "Chargement du dump..."
-# TODO: try speeding up the process by using the --jobs option
 # voir https://stackoverflow.com/questions/37038193/exclude-table-during-pg-restore pour l'explication des tables à exclure
 # TODO: réutiliser AnonymizerRules::TRUNCATED_TABLES ici
 # C'est compliqué à écrire en bash, et il vaudrait mieux utiliser du ruby pour ce genre de logique
 # tables_to_exclude="$(bundle exec rails runner \"puts AnonymizerRules::TRUNCATED_TABLES.join\(\'\|\'\)\") | tail -n1"
-time pg_restore --clean --if-exists --no-owner --no-privileges --jobs=2 --dbname "${DATABASE_URL}" -L <(pg_restore -l /app/*.pgsql | grep -vE 'TABLE DATA public (versions|good_jobs|good_job_settings|good_job_batches|good_job_processes)') /app/*.pgsql
+time pg_restore --clean --if-exists --no-owner --no-privileges --jobs=4 --dbname "${DATABASE_URL}" -L <(pg_restore -l /app/*.pgsql | grep -vE 'TABLE DATA public (versions|good_jobs|good_job_settings|good_job_batches|good_job_processes)') /app/*.pgsql
 
 
 echo "Anonymisation de la base"
