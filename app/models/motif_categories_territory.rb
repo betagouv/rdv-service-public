5bb8e23450edad168fb882aa7e96f8764f122e0c
Victor Mours <victor.mours@gmail.com>
2024-01-24 16:09:51 +0100

    theoretically working version

---
+++ app/models/motif_categories_territory.rb
@@ -0,0 +1,4 @@
+class MotifCategoriesTerritory < ApplicationRecord
+  belongs_to :motif_category
+  belongs_to :territory
+end
--- scripts/split_territory.rb
+++ scripts/split_territory.rb
@@ -11,8 +11,16 @@ class SplitTerritory
       @new_territory = Territory.create(territory_attributes)
 
       move_organisations
+
       move_or_duplicate_agent_territorial_roles
       move_territorial_access_rights
+
+      move_or_duplicate_motif_categories_territories
+      move_or_duplicate_territory_services
+
+      move_sectors
+      move_teams
+      raise "rolling back transaction for dry run"
     end
   end
 
@@ -69,6 +77,69 @@ class SplitTerritory
           agent_territorial_role.update!(territory: new_territory)
         end
       end
+    end
+  end
+
+  def move_or_duplicate_motif_categories_territories
+    MotifCategoriesTerritory.where(territory: old_territory).find_each do |motif_categories_territory|
+      present_in_old_territory = motif_category_present_in_territory?(motif_categories_territory.motif_category_id, old_territory)
+      present_in_new_territory = motif_category_present_in_territory?(motif_categories_territory.motif_category_id, new_territory)
+
+      if present_in_new_territory
+        if present_in_old_territory
+          MotifCategoriesTerritory.create!(territory: new_territory, motif_category: motif_categories_territory.motif_category)
+        else
+          motif_categories_territory.update!(territory: new_territory)
+        end
+      end
+    end
+  end
+
+  def motif_category_present_in_territory?(motif_category_id, territory)
+    Motif.joins(:organisation).where(
+      motif_category_id: motif_category_id
+    ).where(
+      organisations: { territory_id: territory.id }
+    ).any?
+  end
+
+  def move_or_duplicate_territory_services
+    TerritoryService.where(territory: old_territory).each do |territory_service|
+      present_in_old_territory = service_present_in?(territory_service.service_id, old_territory)
+      present_in_new_territory = service_present_in?(territory_service.service_id, new_territory)
+
+      if present_in_new_territory
+        if present_in_old_territory
+          TerritoryService.create!(territory: new_territory, service_id: territory_service.service_id)
+        else
+          territory_service.update!(territory: new_territory)
+        end
+      end
+    end
+  end
+
+  def service_present_in?(service_id, territory)
+    Agent.joins(:services).where(services: { id: service_id })
+      .joins(:organisations).where(organisations: { territory_id: territory.id }).any?
+  end
+
+  def move_sectors
+    old_territory.sectors.each do |sector|
+      territory_ids = sector.organisations.pluck(:territory_id).uniq + sectors.attributions.joins(agent: :organisations).pluck("organisations.territory_id").uniq
+
+      if territory_ids.count > 1
+        raise "Shared sectors can't be handled"
+      end
+
+      if territory_ids == [new_territory.id]
+        sector.update!(territory: new_territory)
+      end
+    end
+  end
+
+  def move_teams
+    if old_territory.teams.any? # false pour la plupart des territoires, dont la Drome
+      raise "This script doesn't have the logic to move teams yet!"
     end
   end
 
