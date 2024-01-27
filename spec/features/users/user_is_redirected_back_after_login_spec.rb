58e33549e29ce0acaf5221ad02fcf77dda24a7fc
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-27 09:16:21 +0100

    Use middleware: cleaner and *actually works*

---
+++ spec/features/users/user_is_redirected_back_after_login_spec.rb
@@ -0,0 +1,30 @@
+require "rails_helper"
+
+RSpec.describe "User is redirected back to requested page after login" do
+  stub_sentry_events
+
+  let!(:user) { create(:user) }
+
+  it "works" do
+    visit "/users/rdvs"
+    expect(page).to have_current_path("/users/sign_in")
+    expect(page).to have_content("Vous devez vous connecter ou vous inscrire pour continuer")
+
+    fill_in("user_email", with: user.email)
+    fill_in("password", with: user.password)
+    click_button("Se connecter")
+    expect(page).to have_current_path("/users/rdvs")
+  end
+
+  context "when visiting a very very long URL" do
+    it "reports ActionDispatch::Cookies::CookieOverflow to Sentry" do
+      long_path = "/users/rdvs?bogus_param=#{'coucou' * 1000}"
+      expect do
+        visit long_path
+      end.to raise_error(ActionDispatch::Cookies::CookieOverflow)
+
+      expect(sentry_events.last.exception.values.first.type).to eq("ActionDispatch::Cookies::CookieOverflow")
+      expect(sentry_events.last.contexts["path"][:original_fullpath]).to eq(long_path)
+    end
+  end
+end
