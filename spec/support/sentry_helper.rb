b2067404f545abe5a04c85ffd9e3ff59092d6fbb
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-27 15:06:10 +0100

    Introduce hack that allows un-xitting specs!

--- spec/support/sentry_helper.rb
+++ spec/support/sentry_helper.rb
@@ -1,9 +1,17 @@
 # Sentry test helper setup, see:
-# https://github.com/getsentry/sentry-ruby/blob/5fc11d9/sentry-ruby/lib/sentry/test_helper.rb
+# https://github.com/getsentry/sentry-ruby/blob/5.16.1/sentry-ruby/lib/sentry/test_helper.rb
 def stub_sentry_events
-  around do |example|
+  before do
     setup_sentry_test
-    example.run
+
+    # Le middleware Rack `Sentry::Rack::CaptureExceptions` fait un appel à `clone_hub_to_current_thread`.
+    # Cet appel a pour effet de remplacer le hub en place. On perd alors la référence des stubs utilisés
+    # par le helper de test dans `setup_sentry_test`.
+    # TODO: Créer une issue sur le repo de sentry-ruby
+    allow(Sentry).to receive(:clone_hub_to_current_thread)
+  end
+
+  after do
     teardown_sentry_test
   end
 end
