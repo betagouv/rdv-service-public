af12d58f1744a54b49b14c707032dcd0a0b017fa
Victor Mours <victor.mours@gmail.com>
2024-01-30 16:47:46 +0100

    ouverture de la prescription interne à la Drôme

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -1,7 +1,7 @@
 module FeatureHelper
   def show_agent_prescription_feature?
     current_organisation.territory.name.starts_with?("CDAD") ||
-      current_organisation.territory.departement_number.in?(%w[83]) ||
+      current_organisation.territory.departement_number.in?(%w[83 26]) ||
       Rails.env.development? ||
       ENV["RDV_SOLIDARITES_INSTANCE_NAME"] == "DEMO"
   end
