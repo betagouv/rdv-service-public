5c10360d3a3f3698a69e811c14302998dab663dd
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-01-30 17:43:24 +0100

    Utiliser de bien meilleurs  noms + check orgs user

--- spec/features/agents/agent_can_prescribe_rdvs_spec.rb
+++ spec/features/agents/agent_can_prescribe_rdvs_spec.rb
@@ -1,52 +1,61 @@
-describe "Agents can prescribe rdvs" do
-  let(:now) { Time.zone.parse("2024-01-05 16h00") }
-  let!(:territory) { create(:territory, departement_number: "83") }
-  let!(:organisation1) { create(:organisation, territory: territory) }
-  let!(:organisation2) { create(:organisation, territory: territory) }
-  let!(:agent) { create(:agent, admin_role_in_organisations: [organisation1]) }
-  let!(:motif1) { create(:motif, organisation: organisation1, service: agent.services.first) }
-  let!(:motif2) { create(:motif, organisation: organisation2, service: agent.services.first) }
-  let!(:motif3) { create(:motif, organisation: organisation2) }
-  let!(:lieu1) { create(:lieu, organisation: organisation1) }
-  let!(:lieu2) { create(:lieu, organisation: organisation1) }
-  let!(:lieu_org2) { create(:lieu, organisation: organisation1) }
-  let!(:plage_ouverture1) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif1], lieu: lieu1, organisation: organisation1) }
-  let!(:plage_ouverture2) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif1], lieu: lieu2, organisation: organisation1) }
-  let!(:plage_ouverture3) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif2], lieu: lieu_org2, organisation: organisation2) }
-  let!(:plage_ouverture4) { create(:plage_ouverture, :daily, first_day: (now + 1.month).to_date, motifs: [motif3], lieu: lieu_org2, organisation: organisation2) }
-
+describe "agent_mdss can prescribe rdvs" do
   before do
+    travel_to(now)
     stub_request(
       :get,
       "https://api-adresse.data.gouv.fr/search/?q=20%20avenue%20de%20S%C3%A9gur,%20Paris"
     ).to_return(status: 200, body: file_fixture("geocode_result.json").read, headers: {})
   end
 
+  let(:now) { Time.zone.parse("2024-01-05 16h00") }
+  let!(:territory) { create(:territory, departement_number: "83") }
+  let!(:org_mds) { create(:organisation, territory: territory) }
+  let!(:org_insertion) { create(:organisation, territory: territory) }
+
+  let!(:agent_mds) { create(:agent, admin_role_in_organisations: [org_mds]) }
+  let!(:agent_insertion) { create(:agent, admin_role_in_organisations: [org_insertion]) }
+
+  let!(:motif_mds) { create(:motif, organisation: org_mds, service: agent_mds.services.first) }
+  let!(:motif_insertion) { create(:motif, organisation: org_insertion, service: agent_mds.services.first) }
+  let!(:motif_autre_service) { create(:motif, organisation: org_insertion) }
+
+  let!(:mds_paris_nord) { create(:lieu, organisation: org_mds) }
+  let!(:mission_locale_paris_nord) { create(:lieu, organisation: org_insertion) }
+  let!(:mission_locale_paris_sud) { create(:lieu, organisation: org_insertion) }
+
+  before do
+    next_month = (now + 1.month).to_date
+    create(:plage_ouverture, :daily, first_day: next_month, motifs: [motif_mds], lieu: mds_paris_nord, organisation: org_mds)
+    create(:plage_ouverture, :daily, first_day: next_month, motifs: [motif_insertion], lieu: mission_locale_paris_sud, organisation: org_insertion, agent: agent_insertion)
+    create(:plage_ouverture, :daily, first_day: next_month, motifs: [motif_insertion], lieu: mission_locale_paris_nord, organisation: org_insertion)
+    create(:plage_ouverture, :daily, first_day: next_month, motifs: [motif_autre_service], lieu: mission_locale_paris_sud, organisation: org_insertion)
+  end
+
   describe 'using "Trouver un RDV"' do
-    let!(:existing_user) { create(:user, first_name: "Francis", last_name: "FACTICE", organisations: [organisation1]) }
+    let!(:existing_user) { create(:user, first_name: "Francis", last_name: "FACTICE", organisations: [org_mds]) }
 
     it "works (happy path)", js: true do
-      login_as(agent, scope: :agent)
+      login_as(agent_mds, scope: :agent)
       visit root_path
       within(".left-side-menu") { click_on "Trouver un RDV" }
       click_link "élargir votre recherche"
       expect(page).to have_content("Prescription")
       # Select Service
       expect(page).to have_content("Sélectionnez le service avec qui vous voulez prendre un RDV")
-      expect(page).to have_content(motif1.service.name)
-      expect(page).to have_content(motif2.service.name)
-      find("h3", text: motif1.service.name).ancestor("a").click
+      expect(page).to have_content(motif_mds.service.name)
+      expect(page).to have_content(motif_autre_service.service.name)
+      find("h3", text: motif_mds.service.name).ancestor("a").click
       # Select Motif
       expect(page).to have_content("Sélectionnez le motif de votre RDV")
-      expect(page).to have_content(motif1.name)
-      expect(page).to have_content(motif2.name)
-      find("h3", text: motif1.name).ancestor("a").click
+      expect(page).to have_content(motif_mds.name)
+      expect(page).to have_content(motif_insertion.name)
+      find("h3", text: motif_insertion.name).ancestor("a").click
       # Select Lieu
-      expect(page).to have_content(lieu1.name)
-      expect(page).to have_content(lieu2.name)
-      find(".card-title", text: /#{lieu1.name}/).ancestor(".card").find("a.stretched-link").click
-      expect(page).to have_content(lieu1.name)
+      expect(page).to have_content(mission_locale_paris_sud.name)
+      expect(page).to have_content(mission_locale_paris_nord.name)
+      find(".card-title", text: /#{mission_locale_paris_nord.name}/).ancestor(".card").find("a.stretched-link").click
       # Select créneau
+      expect(page).to have_content(mission_locale_paris_nord.name)
       first(:link, "11:00").click
       # Display User selection
       find(".select2-selection[aria-labelledby=select2-user_ids_-container]").click
@@ -55,8 +64,8 @@ describe "Agents can prescribe rdvs" do
       first(".select2-results ul.select2-results__options li").click
       click_on "Continuer"
       # Display Récapitulatif
-      expect(page).to have_content("Motif : #{motif1.name}")
-      expect(page).to have_content("Lieu : #{lieu1.name}")
+      expect(page).to have_content("Motif : #{motif_insertion.name}")
+      expect(page).to have_content("Lieu : #{mission_locale_paris_nord.name}")
       expect(page).to have_content("Date du rendez-vous :")
       expect(page).to have_content("Usager : Francis FACTICE")
       click_button "Confirmer le rdv"
@@ -65,33 +74,34 @@ describe "Agents can prescribe rdvs" do
       expect(Rdv.count).to eq(1)
       created_rdv = Rdv.last
       expect(created_rdv.users.first).to eq(existing_user)
-      expect(created_rdv.organisation).to eq(organisation1)
-      expect(created_rdv.motif).to eq(motif1)
-      expect(created_rdv.lieu).to eq(lieu1)
+      expect(created_rdv.users.first.organisations).to match_array([org_mds, org_insertion])
+      expect(created_rdv.organisation).to eq(org_insertion)
+      expect(created_rdv.motif).to eq(motif_insertion)
+      expect(created_rdv.lieu).to eq(mission_locale_paris_nord)
       expect(created_rdv.starts_at).to eq(Time.zone.parse("2024-02-05 11:00"))
-      expect(created_rdv.created_by).to eq(agent)
-      expect(created_rdv.participations.last.created_by).to eq(agent)
+      expect(created_rdv.created_by).to eq(agent_mds)
+      expect(created_rdv.participations.last.created_by).to eq(agent_mds)
     end
   end
 
   describe "when starting from a user profile" do
-    let!(:user) { create(:user, organisations: [organisation1]) }
+    let!(:user) { create(:user, organisations: [org_mds]) }
 
     it "pre-fills the user" do
-      login_as(agent, scope: :agent)
-      visit admin_organisation_user_path(organisation1, id: user.id)
+      login_as(agent_mds, scope: :agent)
+      visit admin_organisation_user_path(org_mds, id: user.id)
       within(".content") { click_on "Trouver un RDV" } # Trouver un RDV pour l'usager
       click_link "élargir votre recherche"
       expect(page).to have_content("Prescription")
       expect(page).to have_content("pour #{user.full_name}")
       # Select Service
-      find("h3", text: motif1.service.name).ancestor("a").click
+      find("h3", text: motif_mds.service.name).ancestor("a").click
       # Select Motif
       expect(page).to have_content("Sélectionnez le motif de votre RDV")
-      find("h3", text: motif1.name).ancestor("a").click
+      find("h3", text: motif_mds.name).ancestor("a").click
       # Select Lieu
-      find(".card-title", text: /#{lieu1.name}/).ancestor(".card").find("a.stretched-link").click
-      expect(page).to have_content(lieu1.name)
+      find(".card-title", text: /#{mds_paris_nord.name}/).ancestor(".card").find("a.stretched-link").click
+      expect(page).to have_content(mds_paris_nord.name)
       # Select créneau
       first(:link, "11:00").click
       # Display Récapitulatif
