3cae1bc9601e2209cf26dc371f1d10fa60a370c1
Victor Mours <victor.mours@gmail.com>
2024-01-31 15:46:17 +0100

    lint fix

--- app/dashboards/compte_dashboard.rb
+++ app/dashboards/compte_dashboard.rb
@@ -3,10 +3,10 @@ require "administrate/base_dashboard"
 class CompteDashboard < Administrate::BaseDashboard
   ATTRIBUTE_TYPES = {
     territory_name: Field::String,
-  }
+  }.freeze
 
   COLLECTION_ATTRIBUTES = %i[
   ].freeze
 
-  FORM_ATTRIBUTES = %i[territory_name]
+  FORM_ATTRIBUTES = %i[territory_name].freeze
 end
