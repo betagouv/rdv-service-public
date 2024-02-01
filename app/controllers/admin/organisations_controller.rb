3537f60cb34cc8f954236f8cea2bf2179b869625
Victor Mours <victor.mours@gmail.com>
2024-02-01 17:38:09 +0100

    Suppression de la page premiers pas

--- app/controllers/admin/organisations_controller.rb
+++ app/controllers/admin/organisations_controller.rb
@@ -48,7 +48,7 @@ class Admin::OrganisationsController < AgentAuthController
     )
     authorize(@organisation)
     if @organisation.save
-      redirect_to organisation_home_path(@organisation, current_agent), flash: { success: "Organisation créée !" }
+      redirect_to admin_organisation_agent_agenda_path(@organisation, current_agent), flash: { success: "Organisation créée !" }
     else
       render :new, layout: "registration"
     end
@@ -76,6 +76,6 @@ class Admin::OrganisationsController < AgentAuthController
   def follow_unique
     return if params[:follow_unique].blank? || policy_scope(Organisation).count != 1
 
-    redirect_to organisation_home_path(policy_scope(Organisation).first, current_agent)
+    redirect_to admin_organisation_agent_agenda_path(policy_scope(Organisation).first, current_agent)
   end
 end
