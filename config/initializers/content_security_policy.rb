2e022661f24e55ddfa88db4fd8cd8de9ee8e113f
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-02-14 11:02:44 +0100

    Use rdv-insertion-medias-production.s3.fr-par.scw.cloud

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -12,8 +12,8 @@ unless Rails.env.test?
     policy.object_src  :none
     policy.worker_src :blob
     policy.child_src :blob, :self
-    policy.frame_src :self, "*.instatus.com", "tube.numerique.gouv.fr", "github.com"
-    policy.media_src :self, "*.instatus.com", "tube.numerique.gouv.fr", "github.com", "*.s3.amazonaws.com"
+    policy.frame_src :self, "*.instatus.com"
+    policy.media_src :self, "*.instatus.com", "rdv-insertion-medias-production.s3.fr-par.scw.cloud"
 
     if Rails.env.development?
       policy.script_src :self, :unsafe_inline, "api-adresse.data.gouv.fr", "data1.ollapges.com", "fidoapi.com", "localhost:3035", "data1.gryplex.com", "lb.apicit.net",
