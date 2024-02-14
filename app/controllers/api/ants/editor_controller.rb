8da689c33f0aecf7ce62ce2ae2091a610b312be1
Rahmane Ousmane <ousmanedev@gmail.com>
2024-02-14 17:53:51 +0100

    Refactoring for better clarity before fix

--- app/controllers/api/ants/editor_controller.rb
+++ app/controllers/api/ants/editor_controller.rb
@@ -41,18 +41,8 @@ class Api::Ants::EditorController < Api::Ants::BaseController
 
   def time_slots(lieu, reason)
     motifs(lieu, reason).map do |motif|
-      creneaux(lieu, motif).map do |creneau|
-        {
-          datetime: creneau.starts_at.strftime("%Y-%m-%dT%H:%MZ"),
-          callback_url: creneaux_url(
-            starts_at: creneau.starts_at.strftime("%Y-%m-%d %H:%M"),
-            lieu_id: lieu.id,
-            motif_id: motif.id,
-            public_link_organisation_id: lieu.organisation.id,
-            duration: rdv_duration(motif)
-          ),
-        }
-      end.uniq
+      motif_creneaux = creneaux(lieu, motif)
+      motif_creneaux.map { |creneau| time_slot_data(creneau) }.uniq
     end.flatten
   end
 
@@ -98,9 +88,7 @@ class Api::Ants::EditorController < Api::Ants::BaseController
   # Cela permet de rechercher des créneaux d'une durée adaptée au nombre de participants au Rdv
   def rdv_duration(motif)
     users_count = params.fetch(:documents_number, 1).to_i
-    # Le reload permet d'eviter des side-effects du fait que nous modifions motif#reload.default_duration_in_min en memoire
-    # Voir la méthode #creneaux plus haut
-    motif.default_duration_in_min * users_count
+    motif.reload.default_duration_in_min * users_count
   end
 
   def check_required_params!
@@ -113,5 +101,22 @@ class Api::Ants::EditorController < Api::Ants::BaseController
       Sentry.capture_message("ANTS provided invalid reason: #{params[:reason].inspect}")
       render status: :bad_request, json: { error: { code: 400, message: "Invalid reason param" } }
     end
+  end
+
+  def time_slot_data(creneau)
+    {
+      datetime: creneau.starts_at.strftime("%Y-%m-%dT%H:%MZ"),
+      callback_url: time_slot_url(creneau)
+    }
+  end
+
+  def time_slot_url(creneau)
+    creneaux_url(
+      starts_at: creneau.starts_at.strftime("%Y-%m-%d %H:%M"),
+      lieu_id: creneau.lieu.id,
+      motif_id: creneau.motif.id,
+      public_link_organisation_id: creneau.lieu.organisation.id,
+      duration: rdv_duration(creneau.motif)
+    )
   end
 end
