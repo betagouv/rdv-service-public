2f59ff2c209c9946c28352e0486067ad50eb0b74
Victor Mours <victor.mours@gmail.com>
2024-02-21 11:21:22 +0100

    Set breadcrumbs for configuration nav

--- app/helpers/configuration_helper.rb
+++ app/helpers/configuration_helper.rb
@@ -1,20 +1,21 @@
 module ConfigurationHelper
   def territory_navigation(title = nil, previous_links = [])
-    tag.nav class: "border-bottom configuration-title border-bottom pb-2 configuration-title mb-2" do
+    tag.nav class: "configuration-title pb-2 mb-2" do
       tag.ol class: "breadcrumb m-0 p-0" do
         concat(tag.li(class: "breadcrumb-item") do
+          link_to root_path do
+            concat(current_domain.name)
+          end
+        end)
+        concat(tag.li(class: "breadcrumb-item") do
           link_to admin_territory_path(current_territory) do
-            concat(tag.i(class: "fa fa-cogs"))
-            concat(" ")
             concat(t("admin.territories.nav.configuration_title"))
           end
         end)
-        if previous_links.any?
-          previous_links.each do |prev_link|
-            concat(tag.li(class: "breadcrumb-item") do
-              prev_link
-            end)
-          end
+        previous_links.each do |prev_link|
+          concat(tag.li(class: "breadcrumb-item") do
+            prev_link
+          end)
         end
 
         if title.present?
