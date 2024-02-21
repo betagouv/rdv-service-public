21621135c0d5fddead47611a8d18a2119df7cc82
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-02-21 09:45:32 +0100

    Clarifier la méthode `Users::CreneauxWizardConcern#service`
    + ne plus skipper l'étape quand les motifs n'ont qu'un seul service ?

--- app/services/concerns/users/creneaux_wizard_concern.rb
+++ app/services/concerns/users/creneaux_wizard_concern.rb
@@ -6,7 +6,7 @@ module Users::CreneauxWizardConcern
   def current_step
     if departement.blank?
       :address_selection
-    elsif !service_selected?
+    elsif !selected_service
       :service_selection
     elsif !motif_name_and_type_selected?
       :motif_selection
@@ -52,16 +52,12 @@ module Users::CreneauxWizardConcern
     end.compact.sort_by(&:last).to_h
   end
 
-  def service
-    @service ||= if @service_id.present?
-                   Service.find(@service_id)
-                 elsif services.count == 1
-                   services.first
-                 end
+  def selected_service
+    Service.find(@service_id) if @service_id
   end
 
-  def services
-    @services ||= matching_motifs.includes(:service).map(&:service).uniq.sort_by(&:name)
+  def services_of_motifs
+    @services_of_motifs ||= matching_motifs.includes(:service).map(&:service).uniq.sort_by(&:name)
   end
 
   def next_availability_by_lieux
@@ -101,10 +97,6 @@ module Users::CreneauxWizardConcern
 
   def motif_name_and_type_selected?
     unique_motifs_by_name_and_location_type.length == 1
-  end
-
-  def service_selected?
-    service.present?
   end
 
   def requires_lieu_selection?
