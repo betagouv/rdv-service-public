3e96313b3c67468ad4ee08735b7b6eef5bf643c9
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-02-23 17:27:40 +0100

    Encrypt content

--- app/models/export.rb
+++ app/models/export.rb
@@ -1,8 +1,14 @@
 class Export < ApplicationRecord
+  # Attributes
+  encrypts :content
+
+  # Relations
   belongs_to :agent
 
+  # Hooks
+  after_create { CleanupExportJob.set(wait_until: expires_at).perform_later(id) }
+
+  # Scopes
   scope :not_expired, -> { where("expires_at > ?", Time.zone.now) }
   scope :available, -> { not_expired.where.not(content: nil) }
-
-  after_create { CleanupExportJob.set(wait_until: expires_at).perform_later(id) }
 end
