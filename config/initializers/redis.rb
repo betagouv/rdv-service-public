95cb2dfa80a56dc63b2762ca0cf1d65b683ce101
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-02-28 17:00:40 +0100

    Define namespace in all envs to easily find app writes

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,7 +3,7 @@ raise "Prevent monkey patch" if Redis.respond_to?(:with_connection)
 class Redis
   def self.with_connection
     redis_connection = new(url: Rails.configuration.x.redis_url)
-    redis_connection = Redis::Namespace.new(Rails.configuration.x.redis_namespace, redis: redis_connection) if Rails.configuration.x.redis_namespace
+    redis_connection = Redis::Namespace.new(Rails.configuration.x.redis_namespace, redis: redis_connection)
     yield(redis_connection)
   ensure
     redis_connection&.close
