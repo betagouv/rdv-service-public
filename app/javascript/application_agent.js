022b7761cad2dc2e1a650f001b249d4fa2cdabb5
Victor Mours <victor.mours@gmail.com>
2024-02-28 16:55:34 +0100

    save tooltip status in local storage

--- app/javascript/application_agent.js
+++ app/javascript/application_agent.js
@@ -26,6 +26,8 @@ import { Select2Inputs } from './components/select2-inputs'
 import { PlanningAgentSelect } from './components/planning-agent-select'
 import { ParticipationSelect } from './components/rdv-user-select'
 import { DestroyButton } from './components/destroy-button'
+import { Tooltips } from './components/tooltips'
+import { HeaderTooltip } from './components/header_tooltip'
 import './components/calendar'
 import './components/browser-detection'
 
@@ -102,10 +104,6 @@ $(document).on('turbolinks:load', function() {
 
   new DestroyButton()
 
-  // TODO: clean this up before real review
-  $(".agent-account-link").tooltip("show")
-  setTimeout(()=> {
-    console.log("hiding tooltip")
-    $(".agent-account-link").tooltip("hide")
-  }, 5000)
+  Tooltips()
+  HeaderTooltip()
 })
