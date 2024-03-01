a5fc1df63dcf0a8e14d1b591aafa85eb4a259918
Romain Neuville <neuvilleromain@gmail.com>
2024-03-01 16:49:17 +0100

    update inclusion connect values

--- app/services/inclusion_connect.rb
+++ app/services/inclusion_connect.rb
@@ -20,13 +20,13 @@ class InclusionConnect
     token = get_token(code, inclusion_connect_callback_url)
     return if token.blank?
 
-    user_info = get_user_info(token)
-    return if user_info.blank?
+    @user_info = get_user_info(token)
+    return if @user_info.blank?
 
-    agent = find_agent(user_info)
+    agent = find_agent
     return unless agent
 
-    update_agent(agent, user_info)
+    update_agent(agent)
     agent
   end
 
@@ -64,28 +64,57 @@ class InclusionConnect
     JSON.parse(res.body)
   end
 
-  def update_agent(agent, user_info)
-    agent.inclusion_connect_open_id_sub ||= user_info["sub"]
-    agent.first_name ||= user_info["given_name"]
-    agent.last_name ||= user_info["family_name"]
+  def update_agent(agent)
+    # We dont want to update one of the agents if we have a mismatch
+    return handle_agent_mismatch if agent_mismatch?
 
-    agent.invitation_accepted_at ||= Time.zone.now
-    agent.confirmed_at ||= Time.zone.now
-
-    agent.last_sign_in_at = Time.zone.now
-    agent.save!
+    update_basic_info(agent)
+    update_email_and_uid(agent) if agent.email != @user_info["email"]
   end
 
-  def find_agent(user_info)
-    found_by_sub = Agent.find_by(inclusion_connect_open_id_sub: user_info["sub"])
-    found_by_email = Agent.find_by(email: user_info["email"])
+  def update_basic_info(agent)
+    agent.assign_attributes({
+                              inclusion_connect_open_id_sub: agent.inclusion_connect_open_id_sub || @user_info["sub"],
+                              first_name: @user_info["given_name"],
+                              last_name: @user_info["family_name"],
+                              invitation_accepted_at: agent.invitation_accepted_at || Time.zone.now,
+                              confirmed_at: agent.confirmed_at || Time.zone.now,
+                              last_sign_in_at: Time.zone.now,
+                            })
+    agent.save! if agent.changed?
+  end
 
-    if found_by_sub && found_by_email && found_by_sub != found_by_email
-      Sentry.add_breadcrumb(Sentry::Breadcrumb.new(message: "Found agent with sub", data: { sub: user_info["sub"], agent_id: found_by_sub.id }))
-      Sentry.add_breadcrumb(Sentry::Breadcrumb.new(message: "Found agent with email", data: { email: user_info["email"], agent_id: found_by_email.id }))
-      Sentry.capture_message("InclusionConnect sub and email mismatch", fingerprint: "ic_agent_sub_email_mismatch")
+  def update_email_and_uid(agent)
+    agent.update_columns(email: @user_info["email"], uid: @user_info["email"]) # rubocop:disable Rails/SkipsModelValidations
+  end
+
+  def find_agent
+    # Dans le cas ou la migration vers francetravail a été faite mais que son email pole-emploi.fr est encore dans la base
+    # Enlever cette condition après la migration
+    if @user_info["email"].split("@").last == "francetravail.fr" && found_by_email.nil? && found_by_sub.nil?
+      @found_by_email = Agent.find_by(email: @user_info["email"].gsub("francetravail.fr", "pole-emploi.fr"))
     end
 
+    handle_agent_mismatch if agent_mismatch?
+
     found_by_sub || found_by_email
+  end
+
+  def handle_agent_mismatch
+    Sentry.add_breadcrumb(Sentry::Breadcrumb.new(message: "Found agent with sub", data: { sub: @user_info["sub"], agent_id: found_by_sub.id }))
+    Sentry.add_breadcrumb(Sentry::Breadcrumb.new(message: "Found agent with email", data: { email: @user_info["email"], agent_id: found_by_email.id }))
+    Sentry.capture_message("InclusionConnect sub and email mismatch", fingerprint: "ic_agent_sub_email_mismatch")
+  end
+
+  def found_by_email
+    @found_by_email ||= Agent.find_by(email: @user_info["email"])
+  end
+
+  def found_by_sub
+    @found_by_sub ||= Agent.find_by(inclusion_connect_open_id_sub: @user_info["sub"])
+  end
+
+  def agent_mismatch?
+    found_by_sub && found_by_email && found_by_sub != found_by_email
   end
 end
