dface4ebae862faef3db4eda4984c2c400f1f101
Victor Mours <victor.mours@beta.gouv.fr>
2024-03-04 10:21:30 +0100

    Update spec/features/agents/rdvs_collectifs/full_lifecycle_spec.rb
    Co-authored-by: Neuville Romain <neuvilleromain@gmail.com>

--- spec/features/agents/rdvs_collectifs/full_lifecycle_spec.rb
+++ spec/features/agents/rdvs_collectifs/full_lifecycle_spec.rb
@@ -21,7 +21,8 @@ RSpec.describe "Agent can organize a rdv collectif", js: true do
   def create_rdv_collectif(lieu_availability)
     # Creating a new RDV Collectif
     visit admin_organisation_rdvs_collectifs_path(organisation)
-    visit admin_organisation_rdvs_collectifs_path(organisation) # Second visit to hide the new header tooltip
+    visit admin_organisation_rdvs_collectifs_path(organisation) # TODO: supprimer en même temps que app/javascript/components/header_tooltip.js
+    # Second visit to hide the new header tooltip
     expect(page).to have_content("Aucun RDV")
 
     click_link "Nouveau RDV Collectif"
