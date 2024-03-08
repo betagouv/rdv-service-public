bed75a5d7579eb52f0d60bc7df50e4b5d237aa2e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-03-08 12:19:09 +0100

    Please rubocop

--- app/services/web_search_context.rb
+++ app/services/web_search_context.rb
@@ -37,7 +37,6 @@ class WebSearchContext < SearchContext
     @public_link_organisation_id || @user_selected_organisation_id
   end
 
-  # rubocop:disable Metrics/PerceivedComplexity
   def filter_motifs(available_motifs)
     motifs = super
     motifs = if prescripteur?
@@ -55,7 +54,6 @@ class WebSearchContext < SearchContext
     motifs = motifs.where(id: @motif_id) if @motif_id.present?
     motifs
   end
-  # rubocop:enable Metrics/PerceivedComplexity
 
   # TODO : move this to a specific search context https://github.com/betagouv/rdv-solidarites.fr/pull/3827#discussion_r1351988739
   def public_link_organisation
