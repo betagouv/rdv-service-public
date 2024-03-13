0b66c08ea697297ea94ef1ffc785013249a5a838
Victor Mours <victor.mours@gmail.com>
2024-03-13 15:36:44 +0100

    Fix and additional spec to document behavior

--- spec/mailers/custom_devise_mailer_domain_spec.rb
+++ spec/mailers/custom_devise_mailer_domain_spec.rb
@@ -108,11 +108,22 @@ RSpec.describe CustomDeviseMailer, "#domain" do
     subject(:sent_email) { described_class.reset_password_instructions(agent, "t0k3n") }
 
     let(:inviter) { create(:agent) }
-    let(:agent) { create(:agent, invited_by: inviter) }
+    let(:agent) { create(:agent, invited_by: inviter, invitation_created_at: Time.zone.now) }
 
     it "doesn't override the reply-to address" do
       perform_enqueued_jobs
-      expect(sent_email.reply_to).to eq Domain::RDV_SOLIDARITES.support_email
+      expect(sent_email.from).to eq [Domain::RDV_SOLIDARITES.support_email]
+      expect(sent_email.reply_to).to be_blank
+    end
+
+    context "for an invitation" do
+      subject(:sent_email) { described_class.invitation_instructions(agent, "t0k3n") }
+
+      it "sets the reply to as the inviter email so that the inviter can resend the invitation if necessary" do
+        # Une meilleure solution serait de permettre à l'agent de renvoyer une invitation en autonomie, et on pourra supprimer ce test quand ça sera implémenté
+        perform_enqueued_jobs
+        expect(sent_email.reply_to).to eq [inviter.email]
+      end
     end
   end
 end
