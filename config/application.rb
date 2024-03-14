9fd26935459e5225c0311e2858a6725da85ee929
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-03-14 17:49:33 +0100

    Revert to using warning now that they are fixed

--- config/application.rb
+++ config/application.rb
@@ -20,7 +20,6 @@ module Lapin
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.active_record.allow_deprecated_singular_associations_name = false
 
     # Configuration for the application, engines, and railties goes here.
     #
