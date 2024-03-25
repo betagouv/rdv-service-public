00b536f725a0f83302a70537851df04f605ecd20
Victor Mours <victor.mours@gmail.com>
2024-03-25 16:23:40 +0100

    fix error case

--- spec/features/agents/rdv_wizard/step2_spec.rb
+++ spec/features/agents/rdv_wizard/step2_spec.rb
@@ -16,12 +16,14 @@ RSpec.describe "Step 2 of the rdv wizard" do
   let(:other_organisation) { create(:organisation, territory: territory) }
   let(:agent) { create(:agent, service: motif.service, basic_role_in_organisations: [organisation]) }
 
-  before { create(:agent_territorial_access_right, agent: agent, territory: territory) }
+  before do
+    create(:agent_territorial_access_right, agent: agent, territory: territory)
 
-  it "allows searching for users", js: true do
     login_as(agent, scope: :agent)
     visit new_admin_organisation_rdv_wizard_step_path(params)
+  end
 
+  it "allows searching for users", js: true do
     # Click on the search field
     find(".collapse-add-user-selection .select2-selection").click
 
@@ -49,5 +51,80 @@ RSpec.describe "Step 2 of the rdv wizard" do
     find(".select2-search__field").send_keys("k")
 
     expect(page).to have_content("Aucun résultat")
+  end
+
+  describe "ANTS pre-demande field" do
+    let(:motif) do
+      create(:motif, name: "Carte d'identité", organisation: organisation, restriction_for_rdv: nil, motif_category: cni_motif_category, default_duration_in_min: 25)
+    end
+
+    let!(:cni_motif_category) { create(:motif_category, name: Api::Ants::EditorController::CNI_MOTIF_CATEGORY_NAME) }
+    let(:ants_pre_demande_number) { "1122334455" }
+
+    context "ants_pre_demander number is not used" do
+      before do
+        stub_request(:get, %r{https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/status}).to_return(
+          status: 200,
+          body: { ants_pre_demande_number => { appointments: [] } }.to_json
+        )
+      end
+
+      it "creates user with no warning", js: true do
+        click_link "Créer un usager"
+        fill_in :user_first_name, with: "Marco"
+        fill_in :user_last_name, with: "Lebreton"
+        fill_in :user_ants_pre_demande_number, with: ants_pre_demande_number
+        click_button "Créer"
+        expect(page).not_to have_content(
+          "Ce numéro de pré-demande ANTS est déjà utilisé pour un RDV auprès de Mairie de Sannois. Veuillez annuler ce RDV avant d'en prendre un nouveau"
+        )
+
+        expect(page).to have_content("LEBRETON Marco")
+
+        expect(User.last).to have_attributes(
+          first_name: "Marco",
+          last_name: "Lebreton",
+          ants_pre_demande_number: ants_pre_demande_number
+        )
+      end
+    end
+
+    context "ants_pre_demander is already used" do
+      before do
+        stub_request(:get, %r{https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/status}).to_return(
+          status: 200,
+          body: {
+            ants_pre_demande_number => {
+              appointments: [
+                {
+                  management_url: "https://gerer-rdv.com",
+                  meeting_point: "Mairie de Sannois",
+                  appointment_date: "01/01/2030",
+                },
+              ],
+            },
+          }.to_json
+        )
+      end
+
+      it "creates user with a warning", js: true do
+        click_link "Créer un usager"
+        fill_in :user_first_name, with: "Marco"
+        fill_in :user_last_name, with: "Lebreton"
+        fill_in :user_ants_pre_demande_number, with: ants_pre_demande_number
+        click_button "Créer"
+        expect(page).to have_content(
+          "Ce numéro de pré-demande ANTS est déjà utilisé pour un RDV auprès de Mairie de Sannois. Veuillez annuler ce RDV avant d'en prendre un nouveau"
+        )
+        click_button("Confirmer en ignorant les avertissements")
+        expect(page).to have_content("LEBRETON Marco")
+
+        expect(User.last).to have_attributes(
+          first_name: "Marco",
+          last_name: "Lebreton",
+          ants_pre_demande_number: ants_pre_demande_number
+        )
+      end
+    end
   end
 end
