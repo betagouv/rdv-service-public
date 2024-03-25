d075756cc71a381852a5fe16f1d6ebb142967027
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-03-25 09:18:15 +0100

    Handle new exception in sync_appointment_job.rb

--- app/jobs/ants/sync_appointment_job.rb
+++ app/jobs/ants/sync_appointment_job.rb
@@ -37,11 +37,15 @@ module Ants
 
         obsolete_appointment&.delete
       end
+    rescue AntsApi::Appointment::InvalidApplicationError => e
+      Rails.logger.warn("Can't delete ANTS appointment for application #{@rdv_attributes[:obsolete_application_id]}: #{e}")
     end
 
     def delete_appointments
       users.each do |user|
         existing_appointment(user)&.delete
+      rescue AntsApi::Appointment::InvalidApplicationError => e
+        Rails.logger.warn("Can't delete ANTS appointment for application #{user.ants_pre_demande_number}: #{e}")
       end
     end
 
@@ -50,6 +54,8 @@ module Ants
         existing_appointment(user)&.delete
 
         AntsApi::Appointment.new(application_id: user.ants_pre_demande_number, **@appointment_data).create
+      rescue AntsApi::Appointment::InvalidApplicationError => e
+        Rails.logger.warn("Can't delete ANTS appointment for application #{user.ants_pre_demande_number}: #{e}")
       end
     end
 
