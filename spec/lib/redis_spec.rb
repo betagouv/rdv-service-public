9835b3fbf354ee1f5992a25093b0101ea6fed782
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-03-25 15:48:50 +0100

    Faire en sorte que le test vérifie simplement qu'on utilise une pool

--- spec/lib/redis_spec.rb
+++ spec/lib/redis_spec.rb
@@ -1,21 +1,14 @@
 RSpec.describe Redis do
   describe "#with_connection" do
-    it "is fast because it is pooled" do
-      starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    # L'usage d'une pool de connexion offre de bien meilleures performances,
+    # ce test a donc été ajouté pour inciter à continuer d'en utiliser une.
+    it "is pooled" do
+      allow(Redis::CONNECTION_POOL).to receive(:with).and_call_original
 
-      10000.times do
-        described_class.with_connection do |redis|
-          redis.set("key", "value")
-        end
-      end
+      described_class.with_connection { |redis| redis.set("key", "value") }
+      described_class.with_connection { |redis| expect(redis.get("key")).to eq("value") }
 
-      ending_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      elapsed_time = ending_time - starting_time
-
-      # Without using a connection pool, 10000 connection checkouts + SET takes about 3000ms.
-      # With a connection pool, 10000 connection checkouts + SET takes about 400ms.
-      # This test ensures that 10000 checkouts + SET stays under 1000ms
-      expect(elapsed_time).to be < 1.0
+      expect(Redis::CONNECTION_POOL).to have_received(:with).twice
     end
   end
 end
