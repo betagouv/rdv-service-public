bb8e62e4e728cb5db3bd4cd7591ded4a4c4c9665
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-03-25 14:39:51 +0100

    Fiddle values to prevent flaky spec

--- spec/lib/redis_spec.rb
+++ spec/lib/redis_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe Redis do
     it "is fast because it is pooled" do
       starting_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
-      1000.times do
+      10000.times do
         described_class.with_connection do |redis|
           redis.set("key", "value")
         end
@@ -12,10 +12,10 @@ RSpec.describe Redis do
       ending_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       elapsed_time = ending_time - starting_time
 
-      # Without using a connection pool, 1000 connection checkouts + SET takes about 300ms.
-      # With a connection pool, 1000 connection checkouts + SET takes about 45ms.
-      # This test ensures that 1000 checkouts + SET stays under 100ms
-      expect(elapsed_time).to be < 0.1
+      # Without using a connection pool, 10000 connection checkouts + SET takes about 3000ms.
+      # With a connection pool, 10000 connection checkouts + SET takes about 400ms.
+      # This test ensures that 10000 checkouts + SET stays under 1000ms
+      expect(elapsed_time).to be < 1.0
     end
   end
 end
