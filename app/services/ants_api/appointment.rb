532af1a31d525dac170c844ffd14165e09996837
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-04-10 18:04:47 +0200

    Spécifier `meeting_point_id` dans nos déclarations à l'ANTS

--- app/services/ants_api/appointment.rb
+++ app/services/ants_api/appointment.rb
@@ -2,19 +2,20 @@ module AntsApi
   class Appointment
     class ApiRequestError < StandardError; end
 
-    attr_reader :application_id, :meeting_point, :appointment_date, :management_url
+    attr_reader :application_id, :meeting_point_id, :meeting_point, :appointment_date, :management_url
 
-    def initialize(application_id:, meeting_point:, appointment_date:, management_url:, editor_comment: nil)
+    def initialize(application_id:, meeting_point:, appointment_date:, management_url:, meeting_point_id: nil)
       @application_id = application_id
+      @meeting_point_id = meeting_point_id
       @meeting_point = meeting_point
       @appointment_date = appointment_date
       @management_url = management_url
-      @editor_comment = editor_comment
     end
 
     def to_request_params
       {
         application_id: application_id,
+        meeting_point_id: meeting_point_id,
         meeting_point: meeting_point,
         appointment_date: appointment_date,
         management_url: management_url,
