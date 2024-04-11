0c81f97c466f5c33cf0edc35a1b3bd8e0a152996
Romain Neuville <neuvilleromain@gmail.com>
2024-04-11 12:10:03 +0200

    do not display internal prescription feature if there is no opened motifs in the territory

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -2,6 +2,7 @@ module FeatureHelper
   def show_agent_prescription_feature?(user_provided:)
     # TODO: faire sauter ce return une fois que nous avons implémenté la saisie d'adresse
     return false if current_organisation.territory.sectorized? && !user_provided
+    return false unless current_organisation.territory.any_motifs_opened_for_prescription?
 
     current_organisation.territory.name.starts_with?("CDAD") ||
       current_organisation.territory.departement_number.in?(%w[83 26 13 55]) ||
