54c17e67d309b7c1e76187a9e34b5c7b482883ba
Romain Neuville <neuvilleromain@gmail.com>
2024-04-11 12:11:11 +0200

    open internal prescription to all territories

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -4,9 +4,6 @@ module FeatureHelper
     return false if current_organisation.territory.sectorized? && !user_provided
     return false unless current_organisation.territory.any_motifs_opened_for_prescription?
 
-    current_organisation.territory.name.starts_with?("CDAD") ||
-      current_organisation.territory.departement_number.in?(%w[83 26 13 55]) ||
-      Rails.env.development? ||
-      ENV["RDV_SOLIDARITES_INSTANCE_NAME"] == "DEMO"
+    true
   end
 end
