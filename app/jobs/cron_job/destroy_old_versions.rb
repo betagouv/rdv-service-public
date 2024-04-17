41fc526988dad55dbdf22431b055a9bbdf16f54d
Victor Mours <victor.mours@gmail.com>
2024-04-17 16:30:05 +0200

    Display information in UI

--- app/jobs/cron_job/destroy_old_versions.rb
+++ app/jobs/cron_job/destroy_old_versions.rb
@@ -1,4 +1,17 @@
 class CronJob::DestroyOldVersions < CronJob
+  MODEL_NAMES_WITH_NO_PERSONAL_INFORMATION = %w[
+    WebhookEndpoint
+    Service
+    Lieu
+    Organisation
+    Territory
+    Team
+    MotifCategory
+    Motif
+    Plage_ouverture
+    Territory_service
+  ]
+
   def perform
     delete_old_versions_with_personal_information
 
@@ -10,27 +23,12 @@ class CronJob::DestroyOldVersions < CronJob
   private
 
   def delete_old_versions_with_personal_information
-    PaperTrail::Version.where.not(item_type: model_names_with_no_personal_information)
+    PaperTrail::Version.where.not(item_type: MODEL_NAMES_WITH_NO_PERSONAL_INFORMATION)
       .where("created_at < ?", 1.year.ago).delete_all
   end
 
   def remove_personal_information_from_old_versions
     PaperTrail::Version.where("created_at < ?", 1.year.ago)
       .where.not(whodunnit: nil).update_all(whodunnit: nil) # rubocop:disable Rails/SkipsModelValidations
-  end
-
-  def model_names_with_no_personal_information
-    %w[
-      WebhookEndpoint
-      Service
-      Lieu
-      Organisation
-      Territory
-      Team
-      MotifCategory
-      Motif
-      Plage_ouverture
-      Territory_service
-    ]
   end
 end
