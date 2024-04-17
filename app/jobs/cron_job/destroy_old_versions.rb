981ea0501f6ded4fad2342eeba4ed054100e0b80
Victor Mours <victor.mours@gmail.com>
2024-04-17 16:11:10 +0200

    Green spec

--- app/jobs/cron_job/destroy_old_versions.rb
+++ app/jobs/cron_job/destroy_old_versions.rb
@@ -1,10 +1,23 @@
 class CronJob::DestroyOldVersions < CronJob
   def perform
-    # Versions are used in RDV exports, and RDVs are currently kept for 2 years.
-    PaperTrail::Version.where("created_at < ?", 2.years.ago).delete_all
+    delete_old_versions_with_personal_information
+
+    remove_personal_information_from_old_versions
+
+    PaperTrail::Version.where("created_at < ?", 5.years.ago).delete_all
   end
 
   private
+
+  def delete_old_versions_with_personal_information
+    PaperTrail::Version.where.not(item_type: model_names_with_no_personal_information)
+      .where("created_at < ?", 1.year.ago).delete_all
+  end
+
+  def remove_personal_information_from_old_versions
+    PaperTrail::Version.where("created_at < ?", 1.year.ago)
+      .where.not(whodunnit: nil).update_all(whodunnit: nil) # rubocop:disable Rails/SkipsModelValidations
+  end
 
   def model_names_with_no_personal_information
     %w[
--- spec/jobs/cron_job/destroy_old_versions_spec.rb
+++ spec/jobs/cron_job/destroy_old_versions_spec.rb
@@ -1,4 +1,8 @@
 RSpec.describe CronJob::DestroyOldVersions do
+  before do
+    PaperTrail.request.whodunnit = "Francis Factice"
+  end
+
   context "for a table with personal information" do
     let!(:recent_user) do
       travel_to(11.months.ago) { create(:user) }
@@ -17,6 +21,10 @@ RSpec.describe CronJob::DestroyOldVersions do
   end
 
   context "for a table with no personal information" do
+    let!(:very_recent_motif) do
+      travel_to(11.months.ago) { create(:motif) }
+    end
+
     let!(:recent_motif) do
       travel_to((4.years + 11.months).ago) { create(:motif) }
     end
@@ -28,11 +36,12 @@ RSpec.describe CronJob::DestroyOldVersions do
     it "keeps the records for a very long time, but deletes the personal information in the version" do
       described_class.new.perform
 
+      expect(very_recent_motif.versions).to be_present
       expect(recent_motif.versions).to be_present
-      expect(old_motif.versions).to be_present
+      expect(old_motif.versions).to be_blank
 
-      expect(recent_motif.versions.first.whodunnit).to be_present
-      expect(old_motif.versions.first.whodunnit).to be_blank
+      expect(very_recent_motif.versions.first.whodunnit).to be_present
+      expect(recent_motif.versions.first.whodunnit).to be_blank
     end
   end
 end
