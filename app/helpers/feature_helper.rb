274239f5a2b5700d8b11cd2004c47aa57be0db88
Romain Neuville <neuvilleromain@gmail.com>
2024-04-23 18:03:09 +0200

    useless code

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -1,6 +1,4 @@
 module FeatureHelper
-  # Prescription
-
   def show_agent_prescription_feature?(user_provided:)
     # TODO: faire sauter ce return une fois que nous avons implémenté la saisie d'adresse
     return false if current_organisation.territory.sectorized? && !user_provided
