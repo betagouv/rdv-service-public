6076c232f9dd2889fa63ec4d6703dd2bb3c183ff
Romain Neuville <neuvilleromain@gmail.com>
2024-04-23 17:51:34 +0200

    improve link conditions to avoid 404

--- app/helpers/feature_helper.rb
+++ app/helpers/feature_helper.rb
@@ -1,4 +1,6 @@
 module FeatureHelper
+  # Prescription
+
   def show_agent_prescription_feature?(user_provided:)
     # TODO: faire sauter ce return une fois que nous avons implémenté la saisie d'adresse
     return false if current_organisation.territory.sectorized? && !user_provided
@@ -9,5 +11,13 @@ module FeatureHelper
 
   def show_agent_prescription_incitation?
     current_agent.present? && current_agent.territories.any?(&:any_motifs_opened_for_prescription?)
+  end
+
+  def current_agent_can_prescribe_in_territory?(territory)
+    current_agent.territories.include?(territory) && territory.any_motifs_opened_for_prescription?
+  end
+
+  def current_agent_first_organisation_in_territory(territory)
+    current_agent.organisations.find_by(territory: territory)
   end
 end
