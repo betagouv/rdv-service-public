c28f4fe6b5db31ca38c30bd4a83285f37d0cc000
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-05-02 12:38:39 +0200

    Simplification du formulaire de motif (#4135)
    * Add tab markup
    
    * Move each tab to a partial
    
    * Implement first tab
    
    * Fix spec
    
    * Fix more specs
    
    * First tab OK
    
    * Ça avance !
    
    * Add placeholders
    
    * WIP
    
    * WIP
    
    * WIP
    
    * Step 2 OK (except JS refactor)
    
    * Step 3 OK
    
    * Fix some specs
    
    * Ça avance sur stimulus
    
    * Refctor a bit
    
    * Don't show secretariat service
    
    * Woops
    
    * Cocher "RDVs modifiables" quand on ouvre à la résa en ligne
    
    * La petite ternaire plus compacte
    
    * Disable submit button until all required fields are filled
    
    * Gérer le lien avec ancre
    
    * Supprimer motif-form.js
    
    * WIP
    
    * Fix specs that were false positive anyway
    
    * Forgot to remove that
    
    * Gérer le cas où on vide le service ou nom en éditant
    
    * Rétablir "Intitulé du motif" en "Nom"
    
    * Ajout des screenshots pour les instructions
    
    * Mission renommage pour coller à la maquette
    
    * Afficher un bouton "Créer le motif" plutôt que "Enregistrer"
    
    * Corrections des labels de bookable_by
    
    * Update label in spec
    
    * Revert vers "RDV individuel" et "RDV collectif"
    
    * Fix spec
    
    * Fix specs?
    
    * Afficher les radio buttons en une seule colonne
    
    * Update config/locales/models/motif.fr.yml
    
    Co-authored-by: Victor Mours <victor.mours@beta.gouv.fr>
    
    * phrasing
    
    * Simplifier ?
    
    * Retours Victor
    
    * Fix specs
    
    ---------
    
    Co-authored-by: Victor Mours <victor.mours@beta.gouv.fr>

---
+++ app/javascript/controllers/motif_form_controller.js
@@ -0,0 +1,86 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [
+    "locationTypeRadios",
+    "bookableByRadios",
+    "bookingDelaySection",
+    "sectoSection",
+    "secretariatSection",
+    "followUpCheckbox"
+  ]
+
+  connect() {
+    this.refreshSections()
+
+    // Permet de pointer vers l'onglet de résa en ligne via un lien avec ancre
+    if(window.location.hash === "#tab_resa_en_ligne") {
+      this.element.querySelector("button#tab_resa_en_ligne").click();
+    }
+  }
+
+  refreshSections() {
+    this.refreshSection(this.bookingDelaySectionTarget, this.reasonsToDisableBookingDelay())
+    this.refreshSection(this.sectoSectionTarget, this.reasonsToDisableSecto())
+    this.refreshSection(this.secretariatSectionTarget, this.reasonsToDisableSecretariat())
+  }
+
+  refreshSection(section, reasons) {
+    if(reasons.length === 0) {
+      this.enableSection(section)
+    }
+    else {
+      this.disableSection(section, reasons)
+    }
+  }
+
+  reasonsToDisableBookingDelay() {
+    return this.bookableBy === "agents" ? ["les créneaux ne sont pas ouverts à la réservation en ligne"] : []
+  }
+
+  reasonsToDisableSecto() {
+    const reasons = []
+    if(this.bookableBy === "agents") {
+      reasons.push("les créneaux ne sont pas ouverts à la réservation en ligne")
+    }
+    if(this.followUpCheckbox.checked) {
+      reasons.push(`l'option "RDV de suivi" est cochée`)
+    }
+    return reasons
+  }
+
+  reasonsToDisableSecretariat() {
+    const reasons = []
+    if(this.locationType === "home") {
+      reasons.push("le RDV est à domicile")
+    }
+    if(this.followUpCheckbox.checked) {
+      reasons.push(`l'option "RDV de suivi" est cochée`)
+    }
+    return reasons
+  }
+
+  enableSection(sectionRoot) {
+    sectionRoot.querySelectorAll("input, select").forEach(i => i.disabled = false)
+    sectionRoot.querySelector(".js-reasons-for-disabled-section").classList.add("hidden")
+    sectionRoot.querySelectorAll(".js-check-on-section-enable").forEach(box => box.checked = true)
+    sectionRoot.classList.remove("disabled-card")
+  }
+  disableSection(sectionRoot, reasons) {
+    sectionRoot.querySelectorAll("input, select").forEach(i => i.disabled = true)
+    sectionRoot.querySelector(".js-reasons-for-disabled-section").innerText = `Vous ne pouvez pas modifier ce paramètre car ${reasons.join(" et ")}.`;
+    sectionRoot.querySelector(".js-reasons-for-disabled-section").classList.remove("hidden")
+    sectionRoot.querySelectorAll(".js-uncheck-on-section-disable").forEach(box => box.checked = false)
+    sectionRoot.classList.add("disabled-card")
+  }
+
+  get locationType() {
+    return this.locationTypeRadiosTargets.find(radio => radio.checked).value
+  }
+  get bookableBy() {
+    return this.bookableByRadiosTargets.find(radio => radio.checked).value
+  }
+  get followUpCheckbox() {
+    return this.followUpCheckboxTarget
+  }
+}
