8c4dd6c24c24e067fc8b2eb5cbca623ac6bd9904
Victor Mours <victor.mours@gmail.com>
2024-05-06 17:46:56 +0200

    Add a minimum session duration

--- config/application.rb
+++ config/application.rb
@@ -58,9 +58,10 @@ module Lapin
 
     config.x.redis_namespace = "app"
 
-    # Avec cette configuration, on crée un cookie qvec une expiration dans 8 heures
+    # Avec cette configuration, on crée un cookie qui expire au bout de 8 heures
     # Cette date d'expiration est mise à jour à chaque requête.
-    # L'expiration est gérée avec le module Timeoutable de Devise.
+    # L'expiration est aussi gérée avec le module Timeoutable de Devise, qui indique le temps entre le login et la déconnexion.
+    # L'utilisateur peut être obligé à se reconnecter par Devise::Timeoutable même si le cookie n'a pas expiré.
     config.session_store :cookie_store, key: "_rdv_sp_session", expire_after: 8.hours
 
     # Devise layout
