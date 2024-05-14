9409b168e28446acbfa0f5ab7f5e54a4407faf83
Victor Mours <victor.mours@gmail.com>
2024-05-14 10:56:03 +0200

    Add constraint for digit

--- app/javascript/components/dsfr-new-password.js
+++ app/javascript/components/dsfr-new-password.js
@@ -5,6 +5,8 @@ export default function DsfrNewPassword() {
   const input = component.querySelector(`input[type="password"]`)
   const minLengthMessage = component.querySelector(`[data-component="js_dsfr_new_password__min_length_message"]`)
 
+  const atLeastOneDigitMessage = component.querySelector(`[data-component="js_dsfr_new_password__at_least_one_digit_message"]`)
+
   const minLength = parseInt(minLengthMessage.dataset.minLength)
 
   input.addEventListener('input', event=> {
@@ -14,6 +16,14 @@ export default function DsfrNewPassword() {
     } else {
       minLengthMessage.classList.add("fr-message--info")
       minLengthMessage.classList.remove("fr-message--valid")
+    }
+
+    if(input.value.match(/\d/g)) {
+      atLeastOneDigitMessage.classList.remove("fr-message--info")
+      atLeastOneDigitMessage.classList.add("fr-message--valid")
+    } else {
+      atLeastOneDigitMessage.classList.add("fr-message--info")
+      atLeastOneDigitMessage.classList.remove("fr-message--valid")
     }
   })
 }
