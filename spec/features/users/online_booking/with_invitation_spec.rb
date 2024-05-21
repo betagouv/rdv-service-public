9bfa708f35df36cc865202ebf4dcd2d8ec468f9f
Romain Neuville <neuvilleromain@gmail.com>
2024-05-21 16:41:50 +0200

    apply review

--- spec/features/users/online_booking/with_invitation_spec.rb
+++ spec/features/users/online_booking/with_invitation_spec.rb
@@ -136,7 +136,7 @@ RSpec.describe "User can be invited" do
         expect(page).to have_content("Malheureusement, aucun créneau correspondant à votre invitation n'a été trouvé.")
         expect(page).to have_content("Toutes nos excuses pour cela.")
         expect(page).to have_content(organisation.humanized_phone_number)
-        expect(page).to have_link("Envoyer une demande d'ouverture de créneaux à l'organisation")
+        expect(page).to have_link("Envoyer une demande d'ouverture de créneaux")
         expect(page).to have_css("a[href*='mailto:organisation@test.fr']")
         expect(page).to have_css("a[href*='cc=support%40rdv-insertion.fr']")
       end
