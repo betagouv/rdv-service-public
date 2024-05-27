ec92eef79108b4a273ec57db2f7efa2313aabf80
Victor Mours <victor.mours@gmail.com>
2024-05-27 12:14:45 +0200

    Add spec

---
+++ spec/features/users/account/change_password_spec.rb
@@ -0,0 +1,22 @@
+RSpec.describe "Changing user password" do
+  let(:user) { create(:user, password: "Rdvservicepublictest1!") }
+
+  it "shows validation errors if the password is too weak, and accepts if it is strong enough" do
+    login_as(user, scope: :user)
+    visit root_path
+    click_link "Votre compte"
+    click_link "Changer de mot de passe"
+
+    fill_in "Nouveau mot de passe", with: "tropfaible"
+    fill_in "Mot de passe actuel", with: "Rdvservicepublictest1!"
+
+    expect { click_button "Enregistrer" }.not_to change { user.reload.encrypted_password }
+
+    expect(page).to have_content("votre mot de passe doit faire au moins 12 caractères")
+
+    fill_in "Nouveau mot de passe", with: "NouveauRdvservicepublictest1!"
+    fill_in "Mot de passe actuel", with: "Rdvservicepublictest1!"
+
+    expect { click_button "Enregistrer" }.to change { user.reload.encrypted_password }
+  end
+end
