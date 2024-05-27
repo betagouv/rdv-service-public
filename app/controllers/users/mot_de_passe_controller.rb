2fdd3093bc2f62867a8c3e369a111360905ece60
Victor Mours <victor.mours@gmail.com>
2024-05-27 13:58:59 +0200

    fix

--- app/controllers/users/mot_de_passe_controller.rb
+++ app/controllers/users/mot_de_passe_controller.rb
@@ -9,7 +9,7 @@ class Users::MotDePasseController < UserAuthController
   def update
     authorize(current_user)
     if current_user.update_with_password(user_params)
-      bypass_sign_in(current_user) # Pour des raisons mystérieuses, Devise déconnecte l'agent après un changement de mot de passe
+      bypass_sign_in(current_user) # Devise déconnecte l'usager après un changement de mot de passe
       flash[:notice] = "Votre mot de passe a été changé"
       redirect_to edit_user_registration_path
     else
