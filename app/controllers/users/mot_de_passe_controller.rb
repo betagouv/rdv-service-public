d696c41341cbdf0e9ca2b964760bbf325fe79c08
Victor Mours <victor.mours@gmail.com>
2024-05-27 17:23:44 +0200

    remove unused controller

--- app/controllers/users/mot_de_passe_controller.rb
+++
@@ -1,6 +0,0 @@
-# Contrairement au User::PasswordsController qui gère les reset de mot de passe via les mécanismes custom de devise
-# et qui ne nécessite pas que l'usager soit connecté,
-# ce controller gère la modification de mot de passe pour un usager connecté, sans utiliser Devise.
-class Users::MotDePasseController < UserAuthController
-  end
-end
