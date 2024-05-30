0437d79f2cd15a4c566f11852ddc6f5182fac1dd
Victor Mours <victor.mours@gmail.com>
2024-05-30 10:46:43 +0200

    Additional cleanup on controller

--- app/controllers/agent_connect_controller.rb
+++ app/controllers/agent_connect_controller.rb
@@ -22,10 +22,13 @@ class AgentConnectController < ApplicationController
   def callback
     agent_connect_state = session.delete(:agent_connect_state)
     if agent_connect_state.blank? || params[:state] != agent_connect_state
-      Sentry.capture_message("Agent Connect states is session and params do not match", extra: { params_state: params[:state], session_ic_state: agent_connect_state },
-                                                                                        fingerprint: ["agent_connect_state_mismatch"])
+      Sentry.capture_message(
+        "Agent Connect states is session and params do not match",
+        extra: { params_state: params[:state], session_agent_connect_state: agent_connect_state },
+        fingerprint: ["agent_connect_state_mismatch"]
+      )
       flash[:error] = error_message
-      redirect_to new_agent_session_path and return
+      redirect_to(new_agent_session_path) and return
     end
 
     agent = AgentConnect.new(params[:code], agent_connect_callback_url).authenticate_and_find_agent
@@ -38,15 +41,15 @@ class AgentConnectController < ApplicationController
       Sentry.capture_message("Failed to authenticate agent with Agent Connect", fingerprint: ["agent_connect_other_error"])
       redirect_to new_agent_session_path
     end
-  rescue InclusionConnect::AgentNotFoundError => e
+  rescue AgentConnect::AgentNotFoundError => e
     flash[:error] = "Il n'y a pas de compte agent pour l'adresse mail #{e.message}.<br />" \
-                    "Vous devez utiliser Inclusion Connect avec l'adresse mail à laquelle vous avez reçu votre invitation sur #{current_domain.name}.<br />" \
+                    "Vous devez utiliser Agent Connect avec l'adresse mail à laquelle vous avez reçu votre invitation sur #{current_domain.name}.<br />" \
                     "Vous pouvez également contacter le support à l'adresse <a href='mailto:#{current_domain.support_email}'>#{current_domain.support_email}</a> si le problème persiste."
-    Sentry.capture_message("Failed to authenticate agent with InclusionConnect - Agent not found", extra: { exception_message: e.message }, fingerprint: ["ic_agent_not_found"])
+    Sentry.capture_message("Failed to authenticate agent with Agent Connect - Agent not found", extra: { exception_message: e.message }, fingerprint: ["agent_connect_agent_not_found"])
     redirect_to new_agent_session_path
-  rescue InclusionConnect::ApiRequestError => e
+  rescue AgentConnect::ApiRequestError => e
     flash[:error] = error_message
-    Sentry.capture_message("Failed to authenticate agent with InclusionConnect - Api error", extra: { exception_message: e.message }, fingerprint: ["ic_api_error"])
+    Sentry.capture_message("Failed to authenticate agent with Agent Connect - Api error", extra: { exception_message: e.message }, fingerprint: ["agent_connect_api_error"])
     redirect_to new_agent_session_path
   end
 
