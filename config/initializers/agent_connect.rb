e08db423bc375f4c1f817663cf835873974157d4
Victor Mours <victor.mours@gmail.com>
2024-06-03 16:05:19 +0200

    separate agent connect service in openid and rdvsp parts

--- config/initializers/agent_connect.rb
+++ config/initializers/agent_connect.rb
@@ -1,3 +1,3 @@
-if ENV['AGENT_CONNECT_BASE_URL'].present?
-  AGENT_CONNECT_CONFIG = OpenIDConnect::Discovery::Provider::Config.discover!(ENV.fetch('AGENT_CONNECT_BASE_URL'))
+if ENV['AGENT_CONNECT_BASE_URL'].present? && !ENV['DISABLE_AGENT_CONNECT']
+  AgentConnectOpenIdClient::AGENT_CONNECT_CONFIG = OpenIDConnect::Discovery::Provider::Config.discover!(ENV.fetch('AGENT_CONNECT_BASE_URL'))
 end
