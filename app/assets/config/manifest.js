8d18c51232a544bc3040005b1e3746a6a78b9002
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-06-03 15:55:50 +0200

    add JS

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,11 +1,19 @@
 //= link_tree ../images
 //= link_tree ../builds
 
-//= link @gouvfr/dsfr/dist/dsfr.min.css
 //= link @gouvfr/dsfr/dist/core/core.min.css
+//= link @gouvfr/dsfr/dist/core/core.module.min.js
+//= link @gouvfr/dsfr/dist/core/core.nomodule.min.js
 //= link @gouvfr/dsfr/dist/core/core.legacy.min.css
+
 //= link @gouvfr/dsfr/dist/component/header/header.min.css
+//= link @gouvfr/dsfr/dist/component/header/header.module.min.js
+//= link @gouvfr/dsfr/dist/component/header/header.nomodule.min.js
+
 //= link @gouvfr/dsfr/dist/component/logo/logo.min.css
-//= link @gouvfr/dsfr/dist/dsfr.module.min.js
-//= link @gouvfr/dsfr/dist/dsfr.nomodule.min.js
+
+//= link @gouvfr/dsfr/dist/component/modal/modal.min.css
+//= link @gouvfr/dsfr/dist/component/modal/modal.module.min.js
+//= link @gouvfr/dsfr/dist/component/modal/modal.nomodule.min.js
+
 //= link @gouvfr/dsfr/dist/utility/icons/icons.min.css
