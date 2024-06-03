f9521bfca196f9f71fc8074d733931c9cd89e054
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-06-03 16:12:42 +0200

    fix specs

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,10 @@
 //= link_tree ../images
 //= link_tree ../builds
 
+//= link @gouvfr/dsfr/dist/dsfr.min.css
+//= link @gouvfr/dsfr/dist/dsfr.module.min.js
+//= link @gouvfr/dsfr/dist/dsfr.nomodule.min.js
+
 //= link @gouvfr/dsfr/dist/core/core.min.css
 //= link @gouvfr/dsfr/dist/core/core.module.min.js
 //= link @gouvfr/dsfr/dist/core/core.nomodule.min.js
