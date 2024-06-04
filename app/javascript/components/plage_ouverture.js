944e756788c7e6d19d53c6321d2c17c9f93b442a
Rahmane Ousmane <ousmanedev@gmail.com>
2024-06-04 17:44:53 +0200

    Do not show lieu selection field if no public office motif is selected

---
+++ app/javascript/components/plage_ouverture.js
@@ -0,0 +1,22 @@
+
+class PlageOuverture {
+  constructor() {
+    this.toggleLieuSelectionField();
+
+    let that = this;
+    $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("change", () => { that.toggleLieuSelectionField(); })
+  }
+
+  toggleLieuSelectionField() {
+    let selectedMotifsPublicOffice = $(".plage-ouverture-form .form-check-input.public_office[name='plage_ouverture[motif_ids][]']:checked");
+
+    if (selectedMotifsPublicOffice.length > 0) {
+      $(".plage-ouverture-form .collapse.lieu-field").collapse("show");
+    } else {
+      $(".plage-ouverture-form .collapse.lieu-field .select2-input").val(null).trigger('change');
+      $(".plage-ouverture-form .collapse.lieu-field").collapse("hide");
+    }
+  }
+}
+
+export { PlageOuverture };
