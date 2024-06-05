07cda22029d8442dbc33fa485b939320e15f9f47
Rahmane Ousmane <ousmanedev@gmail.com>
2024-06-05 11:13:51 +0200

    Improve coding style

--- app/javascript/components/plage_ouverture.js
+++ app/javascript/components/plage_ouverture.js
@@ -2,18 +2,17 @@ class PlageOuverture {
   constructor() {
     this.toggleLieuSelectionField();
 
-    let that = this;
-    $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("change", () => { that.toggleLieuSelectionField(); })
+    $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("change", () => { this.toggleLieuSelectionField(); })
   }
 
   toggleLieuSelectionField() {
-    let selectedMotifsPublicOffice = $(".plage-ouverture-form .form-check-input.public_office[name='plage_ouverture[motif_ids][]']:checked");
+    const selectedMotifsPublicOffice = $(".plage-ouverture-form .form-check-input.public_office[name='plage_ouverture[motif_ids][]']:checked");
 
     if (selectedMotifsPublicOffice.length > 0) {
-      $(".plage-ouverture-form .collapse.lieu-field").collapse("show");
+      $(".plage-ouverture-form .collapse.js-lieu-field").collapse("show");
     } else {
-      $(".plage-ouverture-form .collapse.lieu-field .select2-input").val(null).trigger('change');
-      $(".plage-ouverture-form .collapse.lieu-field").collapse("hide");
+      $(".plage-ouverture-form .collapse.js-lieu-field .select2-input").val(null).trigger('change');
+      $(".plage-ouverture-form .collapse.js-lieu-field").collapse("hide");
     }
   }
 }
