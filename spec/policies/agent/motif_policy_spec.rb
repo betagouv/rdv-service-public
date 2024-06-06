7ed6529912b5e28a61733478a15e7c18ae4a8a8b
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-06-06 17:07:20 +0200

    Write specs for motif policy scopes

--- spec/policies/agent/motif_policy_spec.rb
+++ spec/policies/agent/motif_policy_spec.rb
@@ -2,7 +2,7 @@
 RSpec.describe Agent::MotifPolicy do
   subject { described_class }
 
-  let!(:motif) { create(:motif) }
+  let(:motif) { create(:motif) }
 
   context "for a basic agent of the same service" do
     let(:agent) { create(:agent, basic_role_in_organisations: [motif.organisation], service: motif.service) }
@@ -86,6 +86,134 @@ RSpec.describe Agent::MotifPolicy do
       expect(policy.destroy?).to be_falsey
 
       expect(policy.versions?).to be_falsey
+    end
+  end
+
+  describe Agent::MotifPolicy::ManageScope do
+    context "when I am a territory admin" do
+      let!(:territory_admin) { create(:agent, role_in_territories: [territory]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation_of_other_territory) { create(:organisation, territory: create(:territory)) }
+      let!(:motif_in_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: organisation_of_other_territory) }
+
+      it "includes all motifs from the territory's orgs" do
+        manageable_motifs = described_class.apply(territory_admin, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_territory)
+        expect(manageable_motifs).not_to include(motif_in_other_territory)
+      end
+    end
+
+    context "when I am an organisation admin" do
+      let!(:organisation_admin) { create(:agent, admin_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org) { create(:motif, organisation: organisation) }
+      let!(:motif_in_other_org_of_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: create(:organisation, territory: create(:territory))) }
+
+      it "includes motifs from my admin orgs, but not from other orgs" do
+        manageable_motifs = described_class.apply(organisation_admin, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_org)
+        expect(manageable_motifs).not_to include(motif_in_other_org_of_my_territory)
+        expect(manageable_motifs).not_to include(motif_in_other_territory)
+      end
+    end
+
+    context "when I am a secretaire" do
+      let!(:secretaire) { create(:agent, :secretaire, basic_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org) { create(:motif, organisation: organisation) }
+      let!(:motif_in_other_org_of_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: create(:organisation, territory: create(:territory))) }
+
+      it "does not allow me to manage any motif" do
+        manageable_motifs = described_class.apply(secretaire, Motif.all)
+        expect(manageable_motifs).to eq([])
+      end
+    end
+
+    context "when I am a basic agent" do
+      let!(:basic_agent) { create(:agent, basic_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org) { create(:motif, organisation: organisation) }
+      let!(:motif_in_other_org_of_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: create(:organisation, territory: create(:territory))) }
+
+      it "does not allow me to manage any motif" do
+        manageable_motifs = described_class.apply(basic_agent, Motif.all)
+        expect(manageable_motifs).to eq([])
+      end
+    end
+  end
+
+  describe Agent::MotifPolicy::UseScope do
+    context "when I am a territory admin" do
+      let!(:territory_admin) { create(:agent, role_in_territories: [territory]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation_of_other_territory) { create(:organisation, territory: create(:territory)) }
+      let!(:motif_in_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: organisation_of_other_territory) }
+
+      it "includes all motifs from the territory's orgs" do
+        manageable_motifs = described_class.apply(territory_admin, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_territory)
+        expect(manageable_motifs).not_to include(motif_in_other_territory)
+      end
+    end
+
+    context "when I am an organisation admin" do
+      let!(:organisation_admin) { create(:agent, admin_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org) { create(:motif, organisation: organisation) }
+      let!(:motif_in_other_org_of_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+      let!(:motif_in_other_territory) { create(:motif, organisation: create(:organisation, territory: create(:territory))) }
+
+      it "includes motifs from my admin orgs, but not from other orgs" do
+        manageable_motifs = described_class.apply(organisation_admin, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_org)
+        expect(manageable_motifs).not_to include(motif_in_other_org_of_my_territory)
+        expect(manageable_motifs).not_to include(motif_in_other_territory)
+      end
+    end
+
+    context "when I am a secretaire" do
+      let!(:secretaire) { create(:agent, :secretaire, basic_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org) { create(:motif, organisation: organisation) }
+      let!(:motif_in_other_org_of_my_territory) { create(:motif, organisation: create(:organisation, territory: territory)) }
+
+      it "allows me to use motifs from my orgs, albeit basic" do
+        manageable_motifs = described_class.apply(secretaire, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_org)
+        expect(manageable_motifs).not_to include(motif_in_other_org_of_my_territory)
+      end
+    end
+
+    context "when I am a basic agent" do
+      let!(:basic_agent) { create(:agent, basic_role_in_organisations: [organisation]) }
+
+      let!(:territory) { create(:territory) }
+      let!(:organisation) { create(:organisation, territory: territory) }
+      let!(:motif_in_my_org_same_service) { create(:motif, organisation: organisation, service: basic_agent.services.first) }
+      let!(:motif_in_my_org_other_service) { create(:motif, organisation: organisation, service: create(:service)) }
+
+      it "allows me to use motifs from my orgs if they are from the ame service" do
+        manageable_motifs = described_class.apply(basic_agent, Motif.all)
+        expect(manageable_motifs).to include(motif_in_my_org_same_service)
+        expect(manageable_motifs).not_to include(motif_in_my_org_other_service)
+      end
     end
   end
 end
