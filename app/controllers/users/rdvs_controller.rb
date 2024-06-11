2e1538c61500083dfa4698ce60f2e6fca9ddf928
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-06-11 18:03:35 +0200

    improve

--- app/controllers/users/rdvs_controller.rb
+++ app/controllers/users/rdvs_controller.rb
@@ -6,7 +6,7 @@ class Users::RdvsController < UserAuthController
   before_action :build_creneau, :redirect_if_creneau_not_available, only: %i[edit update]
   after_action :allow_iframe
 
-  layout "application_narrow", only: %i[show edit]
+  layout "application_narrow", only: %i[show]
 
   include TokenInvitable
 
