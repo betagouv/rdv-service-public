040ba5813d6d3935ddc48d8bcc50cbfd54142c38
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-07-02 13:22:48 +0200

    fix

--- spec/models/concerns/rdv/hardcoded_attribute_names_concern_spec.rb
+++ spec/models/concerns/rdv/hardcoded_attribute_names_concern_spec.rb
@@ -1,6 +1,5 @@
 RSpec.describe Rdv::HardcodedAttributeNamesConcern, type: :concern do
   describe ".hardcoded_attribute_names" do
-
     it "is always equal to attribute_names" do
       expect(Rdv.hardcoded_attribute_names).to eq(Rdv.attribute_names)
     end
