0d2de166496a9db4ed3170036abbb2825953c7eb
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-07-02 15:09:35 +0200

    fix feature spec

--- spec/requests/agents/users_controller_search_spec.rb
+++ spec/requests/agents/users_controller_search_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe Agents::UsersController, "#search" do
       sign_in agent
       get search_agents_users_path(organisation_id: other_organisation)
       expect(response).to have_http_status(:found) # 302 redirects
-      expect(flash[:error]).to eq("Vous ne pouvez pas effectuer cette action.")
+      expect(flash[:error]).to eq("Vous ne pouvez pas accéder à cette organisation")
     end
   end
 end
