a806af850e3b330fc5194d7722d9d2ba1d3f5933
Victor Mours <victor.mours@gmail.com>
2024-07-03 15:21:57 +0200

    Add warning if logger and fix linter

--- config/initializers/agent_connect.rb
+++ config/initializers/agent_connect.rb
@@ -2,16 +2,19 @@ require_relative "sentry"
 
 Rails.configuration.x.agent_connect_unreachable_at_boot_time = false
 
-if ENV['AGENT_CONNECT_BASE_URL'].present? && !ENV['AGENT_CONNECT_DISABLED']
+if ENV["AGENT_CONNECT_BASE_URL"].present? && !ENV["AGENT_CONNECT_DISABLED"]
   begin
     # la méthode .discover! fait un appel à l'api d'Agent Connect
-    Rails.configuration.x.agent_connect_config = OpenIDConnect::Discovery::Provider::Config.discover!(ENV['AGENT_CONNECT_BASE_URL'])
+    Rails.configuration.x.agent_connect_config = OpenIDConnect::Discovery::Provider::Config.discover!(ENV["AGENT_CONNECT_BASE_URL"])
   rescue StandardError => e
-    Sentry.capture_exception(e)
-    Sentry.capture_message <<~MSG
+    error_message = <<~MSG
       Agent Connect n'est pas joignable au démarrage de l'application.
       Elle a été démarrée en désactivant le bouton Agent Connect, mais elle aura besoin d'être redémarrée quand Agent Connect sera à nouveau joignable."
     MSG
+
+    Rails.logger.warn(error_message)
+    Sentry.capture_exception(e)
+    Sentry.capture_message(error_message)
     Rails.configuration.x.agent_connect_unreachable_at_boot_time = true
   end
 end
