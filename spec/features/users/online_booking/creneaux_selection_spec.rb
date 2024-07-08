a25ca9a7249a42931a3c1a38de04d335c964fcf9
aminedhobb <amine.dhobb@gmail.com>
2024-07-08 12:37:58 +0200

    fix failing test after rebase

--- spec/features/users/online_booking/creneaux_selection_spec.rb
+++ spec/features/users/online_booking/creneaux_selection_spec.rb
@@ -52,7 +52,7 @@ RSpec.describe "User can select a creneau" do
     it "does not show duplicate creneaux" do
       visit public_link_to_org_path(organisation_id: organisation.id)
 
-      click_on("Vaccination") # choix du motif
+      click_on("RSA Orientation") # choix du motif
       click_on("Prochaine disponibilité le") # choix du lieu
       displayed_creneaux = page.all("a", text: "08:00")
       expect(displayed_creneaux.size).to eq(1)
