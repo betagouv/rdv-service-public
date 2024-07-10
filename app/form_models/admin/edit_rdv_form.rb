cfac10d169520e584ec37d21f5fd3eecc28fccb5
Victor Mours <victor.mours@gmail.com>
2024-07-10 17:38:25 +0200

    Validation of rdv is done in different ways

--- app/form_models/admin/edit_rdv_form.rb
+++ app/form_models/admin/edit_rdv_form.rb
@@ -10,12 +10,6 @@ class Admin::EditRdvForm
   end
 
   def update(**rdv_attributes)
-    @rdv.assign_attributes(rdv_attributes)
-
-    if valid?
-      @rdv.save_and_notify(agent_context.agent)
-    else
-      false
-    end
+    valid? && @rdv.update_and_notify(agent_context.agent, rdv_attributes)
   end
 end
