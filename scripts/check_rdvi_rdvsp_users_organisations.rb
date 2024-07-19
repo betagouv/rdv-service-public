8fff0fa75587b4804ab4e6768cdbc9a3538eaf2a
Romain Neuville <neuvilleromain@gmail.com>
2024-07-19 15:19:09 +0200

    use 2 json files instead of log

--- scripts/check_rdvi_rdvsp_users_organisations.rb
+++ scripts/check_rdvi_rdvsp_users_organisations.rb
@@ -12,6 +12,9 @@ file_path = Rails.root.join("rdvsp_users_ids.json")
 json_data = File.read(file_path)
 users_data = JSON.parse(json_data)
 
+organisations_only_in_rdvsp = Hash.new { |hash, key| hash[key] = [] }
+organisations_not_in_rdvsp = Hash.new { |hash, key| hash[key] = [] }
+
 batch_size = 1000
 
 users_data.each_slice(batch_size) do |user_batch|
@@ -32,17 +35,27 @@ users_data.each_slice(batch_size) do |user_batch|
     end
 
     organisation_ids_from_rdvsp = user.organisations.where(verticale: "rdv_insertion").pluck(:id)
-    organisation_not_in_rdvsp = organisation_ids_from_rdvi - organisation_ids_from_rdvsp
-    organisation_only_in_rdvsp = organisation_ids_from_rdvsp - organisation_ids_from_rdvi
+    organisations_not_in_rdvsp_current = organisation_ids_from_rdvi - organisation_ids_from_rdvsp
+    organisations_only_in_rdvsp_current = organisation_ids_from_rdvsp - organisation_ids_from_rdvi
 
-    unless organisation_only_in_rdvsp.empty?
-      logger.info "User #{user.id} a des organisations en plus dans RDVSP : #{organisation_only_in_rdvsp.join(', ')}"
+    organisations_not_in_rdvsp_current.each do |org_id|
+      organisations_not_in_rdvsp[org_id] << user.id
     end
 
-    unless organisation_not_in_rdvsp.empty?
-      logger.info "User #{user.id} a des organisations en moins dans RDVSP : #{organisation_not_in_rdvsp.join(', ')}"
+    organisations_only_in_rdvsp_current.each do |org_id|
+      organisations_only_in_rdvsp[org_id] << user.id
     end
   end
 end
 
-logger.info "Vérification des données terminée."
+file_path_not_in_rdvsp = Rails.root.join("organisations_not_in_rdvsp.json")
+file_path_only_in_rdvsp = Rails.root.join("organisations_only_in_rdvsp.json")
+
+File.write(file_path_not_in_rdvsp, JSON.pretty_generate(organisations_not_in_rdvsp))
+File.write(file_path_only_in_rdvsp, JSON.pretty_generate(organisations_only_in_rdvsp))
+
+logger.info "Vérification des données terminée et fichiers JSON générés."
+
+puts "Les fichiers JSON ont été générés et sauvegardés aux emplacements suivants :"
+puts "Organisations en moins dans RDVSP : #{file_path_not_in_rdvsp}"
+puts "Organisations en plus dans RDVSP : #{file_path_only_in_rdvsp}"
