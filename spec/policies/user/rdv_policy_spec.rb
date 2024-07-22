1d17747c61a15c2c507bba1ca15ffde4880aeff5
Victor Mours <victor.mours@gmail.com>
2024-07-22 18:10:18 +0200

    fix policy spec

--- spec/policies/user/rdv_policy_spec.rb
+++ spec/policies/user/rdv_policy_spec.rb
@@ -26,14 +26,14 @@ RSpec.describe User::RdvPolicy, type: :policy do
   let!(:rdv) { create(:rdv, organisation: organisation, agents: [agent], motif: motif, users: [user], created_by: user) }
 
   context "Rdv belongs to user" do
-    it_behaves_like "permit actions", :rdv, :show?, :index?, :new?, :edit?, :update?, :create?, :creneaux?, :cancel?
+    it_behaves_like "permit actions", :rdv, :show?, :index?, :new?, :edit?, :update?, :create?, :deplacer?, :cancel?
     it_behaves_like "included in scope"
   end
 
   context "Rdv belongs to user for a relative" do
     let(:rdv) { create(:rdv, organisation: organisation, agents: [agent], motif: motif, users: [relative], created_by: user) }
 
-    it_behaves_like "permit actions", :rdv, :show?, :index?, :new?, :edit?, :update?, :create?, :creneaux?, :cancel?
+    it_behaves_like "permit actions", :rdv, :show?, :index?, :new?, :edit?, :update?, :create?, :deplacer?, :cancel?
     it_behaves_like "included in scope"
   end
 
@@ -41,7 +41,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
     let(:rdv) { create(:rdv, organisation: organisation, agents: [agent], motif: motif, users: [user2], created_by: user) }
 
     it_behaves_like "permit actions", :rdv, :index?
-    it_behaves_like "not permit actions", :rdv, :show?, :new?, :edit?, :update?, :create?, :creneaux?, :cancel?
+    it_behaves_like "not permit actions", :rdv, :show?, :new?, :edit?, :update?, :create?, :deplacer?, :cancel?
     it_behaves_like "not included in scope"
   end
 
@@ -51,7 +51,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
     end
 
     it_behaves_like "permit actions", :rdv, :new?, :create?
-    it_behaves_like "not permit actions", :rdv, :index?, :edit?, :update?, :creneaux?, :cancel?, :show?
+    it_behaves_like "not permit actions", :rdv, :index?, :edit?, :update?, :deplacer?, :cancel?, :show?
     it_behaves_like "included in scope"
   end
 
@@ -60,7 +60,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
       let!(:rdv) { create(:rdv, :collectif, organisation: organisation, agents: [agent], users: [user]) }
 
       it_behaves_like "permit actions", :rdv, :show?, :index?, :new?
-      it_behaves_like "not permit actions", :rdv, :edit?, :update?, :creneaux?, :create?, :cancel?
+      it_behaves_like "not permit actions", :rdv, :edit?, :update?, :deplacer?, :create?, :cancel?
       it_behaves_like "included in scope"
     end
 
@@ -68,7 +68,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
       let(:rdv) { create(:rdv, :collectif, organisation: organisation, agents: [agent], users: [relative]) }
 
       it_behaves_like "permit actions", :rdv, :show?, :index?, :new?
-      it_behaves_like "not permit actions", :rdv, :edit?, :update?, :creneaux?, :create?, :cancel?
+      it_behaves_like "not permit actions", :rdv, :edit?, :update?, :deplacer?, :create?, :cancel?
       it_behaves_like "included in scope"
     end
 
@@ -76,7 +76,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
       let(:rdv) { create(:rdv, :collectif, organisation: organisation, agents: [agent], users: [user2]) }
 
       it_behaves_like "permit actions", :rdv, :index?, :new?
-      it_behaves_like "not permit actions", :rdv, :show?, :edit?, :update?, :creneaux?, :create?, :cancel?
+      it_behaves_like "not permit actions", :rdv, :show?, :edit?, :update?, :deplacer?, :create?, :cancel?
       it_behaves_like "included in scope"
     end
 
@@ -88,7 +88,7 @@ RSpec.describe User::RdvPolicy, type: :policy do
       end
 
       it_behaves_like "permit actions", :rdv, :new?
-      it_behaves_like "not permit actions", :rdv, :index?, :edit?, :update?, :creneaux?, :cancel?, :show?, :create?
+      it_behaves_like "not permit actions", :rdv, :index?, :edit?, :update?, :deplacer?, :cancel?, :show?, :create?
       it_behaves_like "included in scope"
     end
   end
