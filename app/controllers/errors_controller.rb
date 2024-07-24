ccf990984bb04a740643889e696f625266a22887
Victor Mours <victor.mours@gmail.com>
2024-07-24 14:06:59 +0200

    additional improvements

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,6 @@
 class ErrorsController < ApplicationController
   def not_found
-    render status: :not_found
+    rendered_layout = current_agent ? "application_configuration" : "application"
+    render status: :not_found, layout: rendered_layout
   end
 end
