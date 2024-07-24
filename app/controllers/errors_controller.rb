d1984c8271ac236e1c13e179f33df757e91a2f27
Victor Mours <victor.mours@gmail.com>
2024-07-24 14:21:35 +0200

    make it work for 500 page as well

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,16 @@
 class ErrorsController < ApplicationController
   def not_found
-    rendered_layout = current_agent ? "application_configuration" : "application"
     render status: :not_found, layout: rendered_layout
+  end
+
+  def internal_server_error
+    render status: :internal_server_error, layout: rendered_layout
+  end
+
+  private
+
+  def rendered_layout
+    # Le layout application_configuration permet d'avoir le header des agents sans le menu de gauche
+    current_agent ? "application_configuration" : "application"
   end
 end
