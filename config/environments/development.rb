41d84872d8679e72711a8e290d18f62e209dce0f
Victor Mours <victor.mours@gmail.com>
2024-07-24 14:22:04 +0200

    cleanup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,9 +12,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  # TODO: remettre à true avant PR
-  # config.consider_all_requests_local = true
-  config.consider_all_requests_local = false
+  config.consider_all_requests_local = true
 
   # Enable server timing
   config.server_timing = true
