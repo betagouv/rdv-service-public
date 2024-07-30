d6c9178072b712f1c4ef7281443659d7ed552381
Victor Mours <victor.mours@gmail.com>
2024-07-30 14:13:58 +0200

    update specs

--- spec/policies/agent/team_policy_spec.rb
+++ spec/policies/agent/team_policy_spec.rb
@@ -1,54 +1,47 @@
-RSpec.describe Agent::TeamPolicy, type: :policy do
-  %i[new? destroy? edit? update?].each do |action|
+RSpec.describe Agent::TeamPolicy do
+  let(:team) { create(:team, territory: territory) }
+  let(:territory) { create(:territory) }
+
+  %i[create? new? destroy? edit? update? versions?].each do |action|
     describe "##{action}" do
-      it "returns false with agent disllowed to manage teams" do
-        access_right = create(:agent_territorial_access_right, allow_to_manage_teams: false)
+      it "returns false with agent not allowed to manage teams" do
+        access_right = create(:agent_territorial_access_right, territory: territory, allow_to_manage_teams: false)
         agent = access_right.agent
-        territory = access_right.territory
-        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
         team = create(:team, territory: territory)
-        expect(described_class.new(agent_territorial_context, team).send(action)).to be false
+        expect(described_class.new(agent, team).send(action)).to be_falsey
       end
 
-      it "returns true with agent with admin access to this territory" do
-        access_right = create(:agent_territorial_access_right, allow_to_manage_teams: true)
+      it "returns true with agent allowed to manage teams in this territory access to this territory" do
+        access_right = create(:agent_territorial_access_right, territory: territory, allow_to_manage_teams: true)
         agent = access_right.agent
-        territory = access_right.territory
-        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        team = create(:team, territory: territory)
-        expect(described_class.new(agent_territorial_context, team).send(action)).to be true
+        expect(described_class.new(agent, team).send(action)).to be true
       end
 
-      it "returns false if team is from another territory" do
-        access_right = create(:agent_territorial_access_right, allow_to_manage_teams: true)
-        agent = access_right.agent
-        territory = access_right.territory
-        team = create(:team, territory: create(:territory))
-        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        expect(described_class.new(agent_territorial_context, team).send(action)).to be false
+      context "when team is from another territory" do
+        let(:team) { create(:team, territory: create(:territory)) }
+
+        it "returns false" do
+          access_right = create(:agent_territorial_access_right, territory: territory, allow_to_manage_teams: true)
+          agent = access_right.agent
+          expect(described_class.new(agent, team).send(action)).to be_falsey
+        end
       end
     end
   end
 
-  %i[display? create?].each do |action|
-    describe "##{action}" do
-      it "returns false with agent disllowed to manage teams" do
-        access_right = create(:agent_territorial_access_right, allow_to_manage_teams: false)
-        agent = access_right.agent
-        territory = access_right.territory
-        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        team = create(:team, territory: territory)
-        expect(described_class.new(agent_territorial_context, team).send(action)).to be false
-      end
+  describe ".allowed_to_manage_teams_in?" do
+    it "returns false with agent not allowed to manage teams" do
+      access_right = create(:agent_territorial_access_right, allow_to_manage_teams: false)
+      agent = access_right.agent
+      territory = access_right.territory
+      expect(described_class.allowed_to_manage_teams_in?(territory, agent)).to be false
+    end
 
-      it "returns true with agent with admin access to this territory" do
-        access_right = create(:agent_territorial_access_right, allow_to_manage_teams: true)
-        agent = access_right.agent
-        territory = access_right.territory
-        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        team = create(:team, territory: territory)
-        expect(described_class.new(agent_territorial_context, team).send(action)).to be true
-      end
+    it "returns true with agent with agent allowed to manage teams to this territory" do
+      access_right = create(:agent_territorial_access_right, allow_to_manage_teams: true)
+      agent = access_right.agent
+      territory = access_right.territory
+      expect(described_class.allowed_to_manage_teams_in?(territory, agent)).to be true
     end
   end
 end
