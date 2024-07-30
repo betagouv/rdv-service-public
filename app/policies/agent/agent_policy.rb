c435e9d3ddd4534a2345682f40596e034ec15ca5
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-07-30 11:19:49 +0200

    Try to make the method more readable

--- app/policies/agent/agent_policy.rb
+++ app/policies/agent/agent_policy.rb
@@ -36,14 +36,12 @@ class Agent::AgentPolicy < ApplicationPolicy
     include CurrentAgentInPolicyConcern
 
     def resolve
-      agents_i_can_see_as_secretaire = current_agent.secretaire? ? scope.where(id: AgentRole.where(organisation_id: current_agent.organisations).select(:agent_id)) : scope.none
+      scope = scope.joins(:organisations) # JOINing on :organisations allows us to #merge Organisation scopes
 
-      agents_of_territories_i_admin = scope.joins(:organisations).merge(current_agent.organisations_of_territorial_roles)
-
-      agents_of_orgs_i_admin = scope.joins(:organisations).merge(current_agent.admin_orgs)
-
-      agents_of_orgs_i_basic_same_service = scope.joins(:organisations).merge(current_agent.basic_orgs)
-        .merge(current_agent.confreres)
+      agents_i_can_see_as_secretaire = current_agent.secretaire? ? scope.merge(current_agent.organisations) : scope.none
+      agents_of_territories_i_admin = scope.merge(current_agent.organisations_of_territorial_roles)
+      agents_of_orgs_i_admin = scope.merge(current_agent.admin_orgs)
+      agents_of_orgs_i_basic_same_service = scope.merge(current_agent.basic_orgs).merge(current_agent.confreres)
 
       scope.where_id_in_subqueries([agents_i_can_see_as_secretaire, agents_of_territories_i_admin, agents_of_orgs_i_admin, agents_of_orgs_i_basic_same_service])
     end
