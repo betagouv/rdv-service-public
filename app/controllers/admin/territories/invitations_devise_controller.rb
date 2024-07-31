a48de21ab067ccc353c09590f719939b31809260
Victor Mours <victor.mours@gmail.com>
2024-07-31 14:49:54 +0200

    block other controller actions

--- app/controllers/admin/territories/invitations_devise_controller.rb
+++ app/controllers/admin/territories/invitations_devise_controller.rb
@@ -1,4 +1,11 @@
 class Admin::Territories::InvitationsDeviseController < Devise::InvitationsController
   # Ce controller est uniquement utilisé pour permettre aux agents d'accepter les invitations
   layout "application_dsfr"
+
+  # Bloque l'accès aux méthodes du controller parent
+  before_action :block_controller_action, except: [:edit]
+
+  def block_controller_action
+    raise Pundit::NotAuthorizedError, "not authorized"
+  end
 end
