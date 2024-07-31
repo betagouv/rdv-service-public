a6cc557150641a69b8227a2742948962b4a714f0
Victor Mours <victor.mours@beta.gouv.fr>
2024-07-31 17:54:45 +0200

    Empêcher les créations d'invitation par défaut de devise_invitable (#4510)
    block more requests to invitation

--- app/controllers/admin/territories/invitations_devise_controller.rb
+++ app/controllers/admin/territories/invitations_devise_controller.rb
@@ -2,7 +2,7 @@ class Admin::Territories::InvitationsDeviseController < Devise::InvitationsContr
   # Ce controller est uniquement utilisé pour permettre aux agents d'accepter les invitations
   layout "application_dsfr"
 
-  # Bloque l'accès aux méthodes du controller parent
+  # Bloque l'accès aux méthodes du controller parent pour éviter de permettre d'envoyer des invitations n'importe comment
   before_action :block_controller_action, except: %i[edit update] # rubocop:disable Rails/LexicallyScopedActionFilter
 
   def block_controller_action
