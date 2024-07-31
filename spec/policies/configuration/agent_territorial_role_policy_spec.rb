17b96baeb79eea5d2fa2d2e10b62c3572ec537f8
Victor Mours <victor.mours@gmail.com>
2024-07-31 16:01:58 +0200

    wip

--- spec/policies/configuration/agent_territorial_role_policy_spec.rb
+++ spec/policies/configuration/agent_territorial_role_policy_spec.rb
@@ -5,14 +5,27 @@ RSpec.describe Configuration::AgentTerritorialRolePolicy, type: :policy do
         territory = create(:territory)
         agent = create(:agent, role_in_territories: [])
         agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        expect(described_class.new(agent_territorial_context, territory).send(action)).to be false
+
+        territorial_role = create(:agent_territorial_role, territory: territory)
+        expect(described_class.new(agent_territorial_context, territorial_role).send(action)).to be false
+      end
+
+      it "returns false with agent with admin access to a different territory" do
+        territory = create(:territory)
+        agent = create(:agent, role_in_territories: [create(:territory)])
+        agent_territorial_context = AgentTerritorialContext.new(agent, territory)
+
+        territorial_role = create(:agent_territorial_role, territory: territory)
+        expect(described_class.new(agent_territorial_context, territorial_role).send(action)).to be false
       end
 
       it "returns true with agent with admin access to this territory" do
         territory = create(:territory)
         agent = create(:agent, role_in_territories: [territory])
         agent_territorial_context = AgentTerritorialContext.new(agent, territory)
-        expect(described_class.new(agent_territorial_context, territory).send(action)).to be true
+
+        territorial_role = create(:agent_territorial_role, territory: territory)
+        expect(described_class.new(agent_territorial_context, territorial_role).send(action)).to be true
       end
     end
   end
