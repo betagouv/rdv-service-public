cce261479933722f579896fe49ac127958cbfd78
Victor Mours <victor.mours@gmail.com>
2024-08-01 18:27:27 +0200

    use organisation scope, which is better

--- app/policies/agent/agent_role_policy.rb
+++ app/policies/agent/agent_role_policy.rb
@@ -14,15 +14,4 @@ class Agent::AgentRolePolicy
   alias edit? territorial_admin_or_can_invite_agents?
   alias create? territorial_admin_or_can_invite_agents?
   alias destroy? territorial_admin_or_can_invite_agents?
-
-  class Scope < ApplicationPolicy::Scope
-    include CurrentAgentInPolicyConcern
-
-    def resolve
-      my_roles = scope.merge(current_agent.roles)
-      roles_of_orgs_i_admin = scope.where(organisation: current_agent.admin_orgs)
-
-      my_roles.or roles_of_orgs_i_admin
-    end
-  end
 end
