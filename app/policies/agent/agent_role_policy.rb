bbe6218b3c114a6151045ceaa0e1aced9b08f751
Victor Mours <victor.mours@gmail.com>
2024-08-01 17:10:33 +0200

    merge policies

--- app/policies/agent/agent_role_policy.rb
+++ app/policies/agent/agent_role_policy.rb
@@ -1,4 +1,20 @@
 class Agent::AgentRolePolicy
+  def initialize(context, agent_role)
+    @current_agent = context.agent
+    @agent_role = agent_role
+    @access_rights = @current_agent.access_rights_for_territory(agent_role.organisation.territory)
+  end
+
+  def territorial_admin_or_can_invite_agents?
+    @current_agent.territorial_admin_in?(@agent_role.organisation.territory) ||
+      @access_rights&.allow_to_invite_agents?
+  end
+
+  alias update? territorial_admin_or_can_invite_agents?
+  alias edit? territorial_admin_or_can_invite_agents?
+  alias create? territorial_admin_or_can_invite_agents?
+  alias destroy? territorial_admin_or_can_invite_agents?
+
   class Scope < ApplicationPolicy::Scope
     include CurrentAgentInPolicyConcern
 
