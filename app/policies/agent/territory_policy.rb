c66cf9b274e8e071175fda99293a1a376ec05582
Victor Mours <victor.mours@gmail.com>
2024-08-01 12:46:28 +0200

    wip

--- app/policies/agent/territory_policy.rb
+++ app/policies/agent/territory_policy.rb
@@ -10,6 +10,13 @@ class Agent::TerritoryPolicy < ApplicationPolicy
   alias update? agent_has_role_in_record_territory?
   alias edit? agent_has_role_in_record_territory?
 
+  def show?
+    agent_has_role_in_record_territory? ||
+      allow_to_manage_teams? ||
+      allow_to_manage_access_rights? ||
+      allow_to_invite_agents?
+  end
+
   class Scope < Scope
     alias context pundit_user
     delegate :agent, to: :context, prefix: :current # defines current_agent
@@ -17,5 +24,23 @@ class Agent::TerritoryPolicy < ApplicationPolicy
     def resolve
       scope.joins(:roles).where(roles: { agent: current_agent })
     end
+  end
+
+  private
+
+  def access_rights
+    @access_rights ||= access_rights.where(territory: record)
+  end
+
+  def allow_to_manage_access_rights?
+    access_rights&.allow_to_manage_access_rights?
+  end
+
+  def allow_to_invite_agents?
+    access_rights&.allow_to_invite_agents?
+  end
+
+  def allow_to_manage_teams?
+    access_rights&.allow_to_manage_teams?
   end
 end
