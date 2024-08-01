a53f00c0fac38cb0d2994a993469bbf4b0cb80a8
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-08-01 10:13:14 +0200

    refactor ZonePolicy specs

--- spec/policies/configuration/zone_policy_spec.rb
+++ spec/policies/configuration/zone_policy_spec.rb
@@ -1,53 +1,59 @@
 RSpec.describe Configuration::ZonePolicy, type: :policy do
-  %i[new? create? destroy?].each do |action|
-    describe "##{action}" do
-      context "agent does not have any territorial role" do
-        let(:territory) { create(:territory) }
-        let(:sector) { create(:sector, territory:) }
-        let(:zone) { create(:zone, sector:) }
-        let(:agent) { create(:agent) }
-        let(:agent_territorial_context) { AgentTerritorialContext.new(agent, territory) }
+  subject { described_class }
 
-        it "does not permit #{action}" do
-          expect(described_class.new(agent_territorial_context, zone).send(action)).to eq false
-        end
-      end
+  context "agent does not have any territorial role" do
+    let(:territory) { create(:territory) }
+    let(:sector) { create(:sector, territory:) }
+    let(:zone) { create(:zone, sector:) }
+    let(:agent) { create(:agent) }
+    let(:pundit_context) { AgentTerritorialContext.new(agent, territory) }
 
-      context "agent has territorial role in zone territory" do
-        let(:territory) { create(:territory) }
-        let(:sector) { create(:sector, territory:) }
-        let(:zone) { create(:zone, sector:) }
-        let(:agent) { create(:agent, role_in_territories: [territory]) }
-        let(:agent_territorial_context) { AgentTerritorialContext.new(agent, territory) }
+    it_behaves_like "not permit actions",
+                    :zone,
+                    :new?,
+                    :create?,
+                    :destroy?
+  end
 
-        it "permits #{action}" do
-          expect(described_class.new(agent_territorial_context, zone).send(action)).to eq true
-        end
-      end
+  context "agent has territorial role in zone territory" do
+    let(:territory) { create(:territory) }
+    let(:sector) { create(:sector, territory:) }
+    let(:zone) { create(:zone, sector:) }
+    let(:agent) { create(:agent, role_in_territories: [territory]) }
+    let(:pundit_context) { AgentTerritorialContext.new(agent, territory) }
 
-      context "agent has territorial role in other territory" do
-        let(:territory_zone) { create(:territory) }
-        let(:territory_agent) { create(:territory) }
-        let(:sector) { create(:sector, territory: territory_zone) }
-        let(:zone) { create(:zone, sector:) }
-        let(:agent) { create(:agent, role_in_territories: [territory_agent]) }
+    it_behaves_like "permit actions",
+                    :zone,
+                    :new?,
+                    :create?,
+                    :destroy?
+  end
 
-        context "context uses agent's territory" do
-          let(:agent_territorial_context) { AgentTerritorialContext.new(agent, territory_agent) }
+  context "agent has territorial role in other territory" do
+    let(:territory_zone) { create(:territory) }
+    let(:territory_agent) { create(:territory) }
+    let(:sector) { create(:sector, territory: territory_zone) }
+    let(:zone) { create(:zone, sector:) }
+    let(:agent) { create(:agent, role_in_territories: [territory_agent]) }
 
-          it "does not permit #{action}" do
-            expect(described_class.new(agent_territorial_context, zone).send(action)).to eq false
-          end
-        end
+    context "context uses agent’s territory" do
+      let(:pundit_context) { AgentTerritorialContext.new(agent, territory_agent) }
 
-        context "context uses zone's territory" do
-          let(:agent_territorial_context) { AgentTerritorialContext.new(agent, territory_zone) }
+      it_behaves_like "not permit actions",
+                      :zone,
+                      :new?,
+                      :create?,
+                      :destroy?
+    end
 
-          it "does not permit #{action}" do
-            expect(described_class.new(agent_territorial_context, zone).send(action)).to eq false
-          end
-        end
-      end
+    context "context uses zone's territory" do
+      let(:pundit_context) { AgentTerritorialContext.new(agent, territory_zone) }
+
+      it_behaves_like "not permit actions",
+                      :zone,
+                      :new?,
+                      :create?,
+                      :destroy?
     end
   end
 end
