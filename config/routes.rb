307d6ea0b0e505b69ce629761a063d745908b479
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2024-08-05 16:26:04 +0200

    renommage de la route pour mettre à jour les équipes d’un agent depuis l’admin de territoire (#4527)
    * renommage de la route pour mettre à jour les équipes d’un agent
    
    * fix
    
    * Update app/controllers/admin/territories/agents_controller.rb
    
    Co-authored-by: Victor Mours <victor.mours@beta.gouv.fr>
    
    ---------
    
    Co-authored-by: Victor Mours <victor.mours@beta.gouv.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -126,10 +126,11 @@ Rails.application.routes.draw do
           resources :agent_roles, only: %i[update create destroy]
           resources :agent_territorial_access_rights, only: %i[update]
           resources :webhook_endpoints, except: %i[show]
-          resources :agents, only: %i[index new create update edit] do
+          resources :agents, only: %i[index new create edit] do
             member do
               put :territory_admin
               patch :update_services
+              patch :update_teams
             end
           end
           resources :teams, except: :show
--- spec/policies/configuration/agent_policy_spec.rb
+++ spec/policies/configuration/agent_policy_spec.rb
@@ -9,35 +9,35 @@ RSpec.describe Configuration::AgentPolicy, type: :policy do
       let(:agent) { create(:agent, role_in_territories: []) }
       let!(:access_rights) { create(:agent_territorial_access_right, agent: agent, territory: territory) }
 
-      it_behaves_like "not permit actions", :agent, :display?, :edit?, :create?, :update?
+      it_behaves_like "not permit actions", :agent, :display?, :edit?, :create?, :update_teams?
     end
 
     context "admin access to this territory" do
       let(:agent) { create(:agent, role_in_territories: [territory]) }
       let!(:access_rights) { create(:agent_territorial_access_right, agent: agent, territory: territory) }
 
-      it_behaves_like "permit actions", :agent, :display?, :edit?, :create?, :update?
+      it_behaves_like "permit actions", :agent, :display?, :edit?, :create?, :update_teams?
     end
 
     context "allowed to manage teams agent" do
       let(:agent) { create(:agent, role_in_territories: []) }
       let!(:access_rights) { create(:agent_territorial_access_right, agent: agent, territory: territory, allow_to_manage_teams: true) }
 
-      it_behaves_like "permit actions", :agent, :display?, :edit?, :update?
+      it_behaves_like "permit actions", :agent, :display?, :edit?, :update_teams?
     end
 
     context "allowed to invite agents agent" do
       let(:agent) { create(:agent, role_in_territories: []) }
       let!(:access_rights) { create(:agent_territorial_access_right, agent: agent, territory: territory, allow_to_invite_agents: true) }
 
-      it_behaves_like "permit actions", :agent, :display?, :edit?, :create?, :update?
+      it_behaves_like "permit actions", :agent, :display?, :edit?, :create?, :update_teams?
     end
 
     context "allowed to manage access rights agent" do
       let(:agent) { create(:agent, role_in_territories: []) }
       let!(:access_rights) { create(:agent_territorial_access_right, agent: agent, territory: territory, allow_to_manage_access_rights: true) }
 
-      it_behaves_like "permit actions", :agent, :display?, :edit?, :update?
+      it_behaves_like "permit actions", :agent, :display?, :edit?, :update_teams?
     end
   end
 end
