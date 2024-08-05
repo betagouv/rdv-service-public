f55186ecceb72031f853dacb7a2fa1869ec0b773
Victor Mours <victor.mours@gmail.com>
2024-08-05 11:19:14 +0200

    Extract edit method

--- app/policies/agent/agent_territorial_access_right_policy.rb
+++ app/policies/agent/agent_territorial_access_right_policy.rb
@@ -4,6 +4,11 @@ class Agent::AgentTerritorialAccessRightPolicy
     @agent_territorial_access_right = agent_territorial_access_right
   end
 
+  def edit?
+    policy = Agent::TerritoryPolicy.new(@current_agent, @agent_territorial_access_right.territory)
+    policy.allow_to_manage_access_rights?
+  end
+
   def update?
     @current_agent.territorial_admin_in?(@agent_territorial_access_right.territory)
   end
