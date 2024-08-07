fabb6cf85f226b53735cedf43e21a7642c58a918
Victor Mours <victor.mours@gmail.com>
2024-08-07 16:26:44 +0200

    wip

--- app/models/concerns/rdv/authored_concern.rb
+++ app/models/concerns/rdv/authored_concern.rb
@@ -9,6 +9,13 @@ module Rdv::AuthoredConcern
   end
 
   def author
+    if created_by
+      return created_by.full_name
+    end
+
+    # TODO: a partir du 10/01/2025, on pourra supprimer toute cette partie de la méthode qui utilise les versions, puisque
+    # ça fera un an qu'on aura mis en production la pr https://github.com/betagouv/rdv-service-public/pull/3946
+    # qui écrit les created_by
     creation_event = versions_where_event_eq_create.loaded? ? versions_where_event_eq_create.first : versions.where(event: "create").first
     whodunnit = creation_event&.whodunnit
     if whodunnit.blank?
@@ -16,9 +23,9 @@ module Rdv::AuthoredConcern
     end
 
     if whodunnit.starts_with?("[User] ")
-      whodunnit.gsub("[User] ", "")
+      whodunnit.gsub(/[User] \d* ?/, "")
     elsif whodunnit.starts_with?("[Agent] ")
-      whodunnit.gsub("[Agent] ", "")
+      whodunnit.gsub(/\[Agent\] \d* ?/, "")
     else
       whodunnit
     end
