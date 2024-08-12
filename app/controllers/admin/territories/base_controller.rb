8a04a0d2b40e22b5fcda336be96679f9a47a241d
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-08-12 21:20:08 +0200

    autocorrect Lint/UselessMethodDefinition

--- app/controllers/admin/territories/base_controller.rb
+++ app/controllers/admin/territories/base_controller.rb
@@ -21,9 +21,6 @@ class Admin::Territories::BaseController < ApplicationController
   helper_method :pundit_user
 
   # L'usage recommandé est de passer explicitement une policy_scope_class pour savoir quelle policy est utilisé
-  def policy_scope(scope, policy_scope_class:)
-    super
-  end
 
   private
 
