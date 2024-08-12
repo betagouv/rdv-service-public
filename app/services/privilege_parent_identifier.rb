7c419d1d1899d3ecb1ed1b7dd39262a5edb38ad5
Victor Mours <victor.mours@gmail.com>
2024-08-12 14:34:11 +0200

    fix

--- app/services/privilege_parent_identifier.rb
+++ app/services/privilege_parent_identifier.rb
@@ -59,7 +59,9 @@ class PrivilegeParentIdentifier
     end
 
     if agents.empty?
-      versions = PaperTrail::Version.where(item_type: :Agent, event: :update).where("object_changes->'last_name' is not null").select { |v| v.object_changes.to_s[agent_last_name].present? }
+      versions = PaperTrail::Version.where(item_type: :Agent, event: :update).where("object_changes->'last_name' is not null").select do |v|
+        v.object_changes.to_s.upcase[agent_last_name.upcase].present?
+      end
       agents += Agent.where(id: versions.map(&:item_id)).select do |agent|
         agent.paper_trail.version_at(version.created_at).full_name == whodunnit_full_name
       end
