a583694229970d7a27cef0a73b34b06d83b9de41
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-08-13 11:58:20 +0200

    ajout failing spec

--- spec/features/agents/sectorisation/sectorisation_setup_spec.rb
+++ spec/features/agents/sectorisation/sectorisation_setup_spec.rb
@@ -1,12 +1,14 @@
 RSpec.describe "Agent can setup sectorisation", type: :feature do
   let(:territory) { create(:territory, departement_number: "26") }
+  let!(:service) { create(:service, name: "Service social") }
   let(:ccas_valentinois) { create(:organisation, territory: territory, name: "CCAS Valentinois") }
   let(:mds_drome) { create(:organisation, territory: territory, name: "MDS Drôme") }
-  let(:agent) { create(:agent, role_in_territories: [territory], admin_role_in_organisations: [ccas_valentinois, mds_drome]) }
+  let(:lea) { create(:agent, first_name: "lea", last_name: "Dupont", services: [service], role_in_territories: [territory], admin_role_in_organisations: [ccas_valentinois, mds_drome]) }
+  let!(:marguerite) { create(:agent, first_name: "Marguerite", last_name: "Duras", services: [service], role_in_territories: [territory], basic_role_in_organisations: [ccas_valentinois, mds_drome]) }
 
-  before { login_as(agent, scope: :agent) }
+  before { login_as(lea, scope: :agent) }
 
-  it "works" do
+  it "works", :js do
     visit admin_territory_sectorization_path(territory)
     find("a", text: "Secteurs").click
     click_on "Créer un nouveau secteur"
@@ -20,5 +22,22 @@ RSpec.describe "Agent can setup sectorisation", type: :feature do
     click_on "Attribuer une organisation ou un agent"
     select "MDS Drôme", from: "Organisation"
     click_on "Ajouter"
+    expect(page).to have_content("Attribution ajoutée")
+
+    click_on "Secteurs"
+    click_on "Créer un nouveau secteur"
+    fill_in :sector_name, with: "Secteur Sud"
+    fill_in :sector_human_id, with: "sud"
+    click_on "Enregistrer"
+    click_on "Ajouter une commune ou une rue"
+    fill_in_readonly_input("#zone_city_name", "Albon")
+    fill_in_readonly_input("#zone_city_code", "26004")
+    click_on "Enregistrer"
+    click_on "Attribuer une organisation ou un agent"
+    choose "Agent désigné"
+    select "MDS Drôme", from: "Organisation"
+    select "DURAS Marguerite (Service social)", from: "Agent"
+    click_on "Ajouter"
+    expect(page).to have_content("Attribution ajoutée")
   end
 end
--- spec/support/fill_in_readonly_input_helper.rb
+++ spec/support/fill_in_readonly_input_helper.rb
@@ -1,7 +1,11 @@
 module FillInReadOnlyInputHelper
   def fill_in_readonly_input(selector, value)
-    # cf https://github.com/teamcapybara/capybara/issues/1178
-    # this should work both with js: true and without
-    find(selector).native["value"] = value
+    if RSpec.current_example.metadata[:js]
+      find(selector) # so that it waits for the page to load
+      page.execute_script("document.querySelector('#{selector}').value = '#{value}'")
+    else
+      # cf https://github.com/teamcapybara/capybara/issues/1178
+      find(selector).native["value"] = value
+    end
   end
 end
