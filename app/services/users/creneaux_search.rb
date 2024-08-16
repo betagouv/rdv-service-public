27f213f98391b137b14732f82b22963aaf095fd4
Victor Mours <victor.mours@gmail.com>
2024-08-16 17:17:28 +0200

    inline concern

--- app/services/users/creneaux_search.rb
+++ app/services/users/creneaux_search.rb
@@ -1,6 +1,4 @@
 class Users::CreneauxSearch
-  include Users::CreneauxSearchConcern
-
   attr_reader :motif, :date_range, :user, :lieu
 
   delegate :start_booking_delay, :end_booking_delay, to: :motif
@@ -23,5 +21,46 @@ class Users::CreneauxSearch
     )
 
     search.creneaux.select { _1.starts_at == starts_at }.sample
+  end
+
+  def next_availability
+    return available_collective_rdvs.first if motif.collectif?
+
+    NextAvailabilityService.find(motif, @lieu, attributed_agents, from: start_booking_delay, to: end_booking_delay)
+  end
+
+  def creneaux
+    return available_collective_rdvs if motif.collectif?
+
+    reduced_date_range = Lapin::Range.reduce_range_to_delay(motif, date_range) # réduit le range en fonction du délay
+    return [] if reduced_date_range.blank?
+
+    SlotBuilder.available_slots(motif, @lieu, reduced_date_range, attributed_agents)
+  end
+
+  def available_collective_rdvs
+    rdvs = Rdv.collectif_and_available_for_reservation
+      .where(motif: motif, lieu: @lieu, starts_at: @motif.start_booking_delay..@motif.end_booking_delay)
+      .order(:starts_at)
+
+    rdvs = rdvs.joins(:agents).where(agents: attributed_agents) if attributed_agents.any?
+    rdvs
+  end
+
+  private
+
+  def attributed_agents
+    @attributed_agents ||= retrieve_attributed_agents
+  end
+
+  def retrieve_attributed_agents
+    return @user.referent_agents if motif.follow_up?
+    return geo_attributed_agents if @geo_search.present? && motif.sectorisation_level_agent?
+
+    []
+  end
+
+  def geo_attributed_agents
+    @geo_search.attributed_agents_by_organisation[@motif.organisation].presence || []
   end
 end
