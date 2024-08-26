e782dbd140535e9be5072b45ad1031bdf61d15c0
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-08-26 18:40:24 +0200

    autocorrect Layout/HashAlignment

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,8 @@ class User < ApplicationRecord
   enum :caisse_affiliation, { aucune: 0, caf: 1, msa: 2 }
   enum :family_situation, { single: 0, in_a_relationship: 1, divorced: 2 }
   enum :created_through, { agent_creation: "agent_creation", user_sign_up: "user_sign_up",
-                          franceconnect_sign_up: "franceconnect_sign_up", user_relative_creation: "user_relative_creation",
-                          unknown: "unknown", agent_creation_api: "agent_creation_api", prescripteur: "prescripteur", }
+                           franceconnect_sign_up: "franceconnect_sign_up", user_relative_creation: "user_relative_creation",
+                           unknown: "unknown", agent_creation_api: "agent_creation_api", prescripteur: "prescripteur", }
   enum :invited_through, { devise_email: "devise_email", external: "external" }
   enum :logement, { sdf: 0, heberge: 1, en_accession_propriete: 2, proprietaire: 3, autre: 4, locataire: 5 }
 
