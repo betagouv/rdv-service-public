680bf2b750f523ca51c33581bb1e2cfe6865a12e
Victor Mours <victor.mours@gmail.com>
2024-08-26 11:47:25 +0200

    move prochain creneau spec to swagger directory

--- spec/requests/api/visioplainte/swagger_doc/creneaux_spec.rb
+++ spec/requests/api/visioplainte/swagger_doc/creneaux_spec.rb
@@ -48,4 +48,52 @@ RSpec.describe "Visioplainte API", swagger_doc: "visioplainte/api.json" do
       end
     end
   end
+
+  path "/api/visioplainte/creneaux/prochain" do
+    get "Indique la date du prochain créneau" do
+      with_visioplainte_authentication
+
+      description "Renvoie le prochain créneau disponible"
+      parameter name: :service, in: :query, type: :string,
+        description: "Indique si on souhaite obtenir les créneaux de la plateforme de la gendarmerie ou de la police. " \
+        "Les deux valeurs possibles sont donc 'Police' ou 'Gendarmerie'",
+        example: "Police", required: true
+
+      parameter name: "date_debut", in: :query, type: :string, description: "date au format iso8601 (YYYY-MM-DD), date à partir de laquelle on cherche des créneaux",
+        example: "2024-12-22", required: true
+
+      let(:date_debut) { "2024-12-22" }
+      let(:service) { "Police" }
+
+      response 200, "Renvoie le prochain créneau disponible" do
+        run_test!
+        schema type: :object, properties: { starts_at: { type: :string }, duration_in_min: { type: :integer } }, required: %w[starts_at duration_in_min]
+
+        specify do
+          expect(parsed_response_body).to eq(
+            {
+              starts_at: "2024-12-22T10:00:00+02:00",
+              duration_in_min: 30,
+            }.deep_stringify_keys
+          )
+        end
+      end
+
+      response 404, "Renvoie un message d'erreur si aucun créneau n'est disponible" do
+        run_test!
+        schema type: :object, properties: { erreur: { type: :string } }, required: %w[erreur]
+
+        let(:service) { "Gendarmerie" }
+
+        specify do
+          expect(parsed_response_body).to eq(
+            {
+              erreur: "Aucun créneau n'est disponible après cette date pour ce service.",
+            }.deep_stringify_keys
+          )
+        end
+      end
+    end
+  end
+
 end
