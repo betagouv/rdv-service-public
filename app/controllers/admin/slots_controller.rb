d8a5d42e11eefa832196296ad3a1c5b2a28b750f
Victor Mours <victor.mours@gmail.com>
2024-08-30 17:14:41 +0200

    fix

--- app/controllers/admin/slots_controller.rb
+++ app/controllers/admin/slots_controller.rb
@@ -1,6 +1,7 @@
 class Admin::SlotsController < AgentAuthController
   def index
     @form = helpers.build_agent_creneaux_search_form(current_organisation, params)
+    skip_policy_scope # La construction du formulaire filtre sur l'orga courante
 
     @search_result = if @form.motif.individuel?
                        CreneauxSearch::ForAgent.new(@form).build_result
