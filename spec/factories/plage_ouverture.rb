baa44ee78ceded66ea1d31714968d9c3e5fa713e
Victor Mours <victor.mours@gmail.com>
2024-09-09 16:06:48 +0200

    add daily plage ouverture factory back temporarily

--- spec/factories/plage_ouverture.rb
+++ spec/factories/plage_ouverture.rb
@@ -16,6 +16,9 @@ FactoryBot.define do
       recurrence { nil }
     end
 
+    trait :daily do # Cette option n'existe pas dans l'interface, on pourrait simplifier le code en la supprimant
+      recurrence { Montrose.every(:day, starts: first_day) }
+    end
     trait :weekly do
       recurrence { Montrose.every(:week, on: [:monday], starts: first_day) }
     end
