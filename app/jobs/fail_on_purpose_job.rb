716ea021c05224ca1a6eea2fdfc307c36c6d1d05
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-09-11 14:30:47 +0200

    générisation de la capture des exceptions Sentry depuis les jobs

--- app/jobs/fail_on_purpose_job.rb
+++ app/jobs/fail_on_purpose_job.rb
@@ -3,6 +3,10 @@ class FailOnPurposeError < StandardError; end
 class FailOnPurposeJob < ApplicationJob
   # self.log_arguments = false
 
+  # retry_on(StandardError, wait: 20.seconds, attempts: 3, priority: DefaultJobBehaviour::PRIORITY_OF_RETRIES)
+
+  # discard_on(FailOnPurposeError) { |job, error| job.capture_sentry_exception(error) }
+
   def perform(message: "This error was raised on purpose")
     Sentry.get_current_scope.set_fingerprint(["FailOnPurposeError", message])
     raise FailOnPurposeError, message
