5069e0de05c22aa63cf8a012c3d0906184dad5f5
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-09-11 09:42:21 +0200

    Remove boilerplate comments

--- config/initializers/custom_serializers.rb
+++ config/initializers/custom_serializers.rb
@@ -2,43 +2,31 @@
 # transmettre aux jobs les attributs des absences et plages d'ouverture.
 
 class TodSerializer < ActiveJob::Serializers::ObjectSerializer
-  # Checks if an argument should be serialized by this serializer.
   def serialize?(argument)
     argument.is_a? Tod::TimeOfDay
   end
 
-  # Converts an object to a simpler representative using supported object types.
-  # The recommended representative is a Hash with a specific key. Keys can be of basic types only.
-  # You should call `super` to add the custom serializer type to the hash.
   def serialize(tod)
     super(tod_str: Tod::TimeOfDay.dump(tod))
   end
 
-  # Converts serialized value into a proper object.
   def deserialize(hash)
     Tod::TimeOfDay.load(hash[:tod_str])
   end
 end
-
 Rails.application.config.active_job.custom_serializers << TodSerializer
 
 class RecurrenceSerializer < ActiveJob::Serializers::ObjectSerializer
-  # Checks if an argument should be serialized by this serializer.
   def serialize?(argument)
     argument.is_a? Montrose::Recurrence
   end
 
-  # Converts an object to a simpler representative using supported object types.
-  # The recommended representative is a Hash with a specific key. Keys can be of basic types only.
-  # You should call `super` to add the custom serializer type to the hash.
   def serialize(recurrence)
     super(recurrence_str: Montrose::Recurrence.dump(recurrence))
   end
 
-  # Converts serialized value into a proper object.
   def deserialize(hash)
     Montrose::Recurrence.load(hash[:recurrence_str])
   end
 end
-
 Rails.application.config.active_job.custom_serializers << RecurrenceSerializer
