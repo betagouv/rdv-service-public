4fa29d264c31f147ef733359e8e931201c2a1a79
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-09-11 13:42:44 +0200

    stratégie de retry courte pour les RdvUpcomingReminderJob + améliorer les discard_on

--- app/jobs/rdv_upcoming_reminder_job.rb
+++ app/jobs/rdv_upcoming_reminder_job.rb
@@ -1,8 +1,17 @@
 class RdvUpcomingReminderJob < ApplicationJob
   queue_as :reminders
 
+  # Ces jobs sont enqueued 48 heures avant le début du RDV
+  # La stratégie de retries par défaut jusqu’à 8 jours ne convient donc pas
+  # Ce retry_on a précédence sur celui du DefaultJobBehaviour
+  # Les handlers retry_on et discard_on sont parcourus de bas en haut du code puis en remontant les classes parentes
+  retry_on StandardError, wait: 2.hours, attempts: 20, priority: DefaultJobBehaviour::PRIORITY_OF_RETRIES
+
   class TooLateError < StandardError; end
-  discard_on(TooLateError) { |_job, error| Sentry.capture_exception(error) }
+
+  discard_on(TooLateError) { |job, error| job.capture_sentry_exception(error) }
+
+  discard_on(ActiveJob::DeserializationError) { |job, error| job.capture_sentry_exception(error) }
 
   def perform(rdv)
     if rdv.ends_at < Time.zone.now
