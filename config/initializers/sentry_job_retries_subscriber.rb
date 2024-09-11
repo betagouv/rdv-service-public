f1fc337bcbb5cc708f2a503f10f07a62892c4442
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-09-11 14:34:02 +0200

    forgot file

--- config/initializers/sentry_job_retries_subscriber.rb
+++ config/initializers/sentry_job_retries_subscriber.rb
@@ -6,14 +6,5 @@ ActiveSupport::Notifications.subscribe("enqueue_retry.active_job") do |_name, _s
 
   next if !exception || !job.capture_sentry_warning_for_retry?(exception)
 
-  # adapted from https://github.com/getsentry/sentry-ruby/blob/master/sentry-rails/lib/sentry/rails/active_job.rb#L47-L54
-  Sentry.capture_exception(
-    exception,
-    extra: Sentry::Rails::ActiveJobExtensions::SentryReporter.sentry_context(job),
-    level: :warning,
-    tags: {
-      job_id: job.job_id,
-      provider_job_id: job.provider_job_id,
-    }
-  )
+  job.capture_sentry_exception(exception, level: :warning)
 end
