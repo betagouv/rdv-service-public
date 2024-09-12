adeb666c969bc83f39cdc2090cd153a447b9d08b
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-09-12 13:10:09 +0200

    Nous n'avons plus besoin de ces beaux serializers

--- config/initializers/custom_serializers.rb
+++
@@ -1,5 +0,0 @@
-# Ces serializers sont utilisés par ActiveSupport pour automatiquement
-# transmettre aux jobs les attributs des absences et plages d'ouverture.
-
-# Rails.application.config.active_job.custom_serializers << TodSerializer
-# Rails.application.config.active_job.custom_serializers << RecurrenceSerializer
