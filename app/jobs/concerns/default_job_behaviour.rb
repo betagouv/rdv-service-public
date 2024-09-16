9ea29a689c31ad85badff62983d0dc1acbcc79d8
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-09-16 16:19:06 +0200

    fix specs

--- app/jobs/concerns/default_job_behaviour.rb
+++ app/jobs/concerns/default_job_behaviour.rb
@@ -11,7 +11,7 @@ module DefaultJobBehaviour
     # Exponential backoff is n^4, so wait times between retries will be as follows:
     # attempt:  1   2    3    4   5    6    7    8    9     10    11  12  13  14   15   16   17   18   19   20
     # backoff:  1s, 16s, 81s, 4m, 10m, 21m, 40m, 68m, 109m, 166m, 4h, 6h, 8h, 11h, 14h, 18h, 23h, 29h, 36h, 44h
-    # sum: (1..20).map { _1 ** 4 }.sum.to_f / 60 / 60 / 24 ~= 8
+    # sum: (1..20).map { _1 ** 4 }.sum.to_f / 60 / 60 / 24 ~= 8 hours
     # it therefore takes more than 8 days for a job to be discarded
     retry_on(StandardError, wait: :exponentially_longer, attempts: MAX_ATTEMPTS, priority: PRIORITY_OF_RETRIES)
 
@@ -36,13 +36,13 @@ module DefaultJobBehaviour
       job_id:,
       job_link:,
       queue_name:,
-      scheduled_at: scheduled_at,
+      scheduled_at:,
     }
 
     if self.class.log_arguments
       context[:arguments] = Sentry::Rails::ActiveJobExtensions::SentryReporter.sentry_serialize_arguments(arguments)
     end
 
-    Sentry.set_context "job", context
+    Sentry.set_context :job, context
   end
 end
