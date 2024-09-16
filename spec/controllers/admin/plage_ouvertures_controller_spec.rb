d1d0914de8aa3d320ea303f60d238b4b994874a8
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-09-16 11:26:06 +0200

    Reapply de #4612 (#4625)
    * Reapply "Suppression d'absence / plage : sérialiser pour un envoi asynchrone" (#4624)
    
    This reverts commit 8ed7526a37b11f044b92cdedcc77db3f97573ba0.
    
    * Mark hydrated record as read-only
    
    * Remove boilerplate comments
    
    * Clarify specs
    
    * Try to follow advice from https://guides.rubyonrails.org/v7.0/active_job_basics.html#serializers
    
    * Fix rubocop offences
    
    * On tente de sérialiser à la main
    
    * Toujours aussi pénible les clés de hash en symboles / strings
    
    * Nous n'avons plus besoin de ces beaux serializers

--- spec/controllers/admin/plage_ouvertures_controller_spec.rb
+++ spec/controllers/admin/plage_ouvertures_controller_spec.rb
@@ -234,7 +234,7 @@ RSpec.describe Admin::PlageOuverturesController, type: :controller do
       it "send notification after destroy" do
         ActionMailer::Base.deliveries.clear
         delete :destroy, params: { organisation_id: organisation.id, id: plage_ouverture.id }
-        expect(ActionMailer::Base.deliveries.size).to eq(1)
+        expect { perform_enqueued_jobs }.to change { ActionMailer::Base.deliveries.size }.by(1)
         expect(ActionMailer::Base.deliveries.last.subject).to include("RDV Solidarités - Plage d’ouverture supprimée")
       end
 
@@ -243,17 +243,6 @@ RSpec.describe Admin::PlageOuverturesController, type: :controller do
         ActionMailer::Base.deliveries.clear
         delete :destroy, params: { organisation_id: organisation.id, id: plage_ouverture.id }
         expect(ActionMailer::Base.deliveries.size).to eq(0)
-      end
-
-      it "remove plage ouverture without delay" do
-        # Difficile de tester le delai qui avait été
-        # introduit ici, c'est pourtant la source d'un problème.
-        # En attendant de pouvoir tester dans une spec de feature
-        # je pose ce test comme justification de la suppresion de
-        # l'utilisation du `delay`
-        expect do
-          delete :destroy, params: { organisation_id: organisation.id, id: plage_ouverture.id }
-        end.not_to have_enqueued_job
       end
     end
   end
