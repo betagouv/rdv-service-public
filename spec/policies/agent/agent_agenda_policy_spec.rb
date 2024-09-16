920a0670848e46efb849226c2ed6279221866e73
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2024-09-16 13:43:48 +0200

    Suppression d’un scoping pundit redondant sur les agendas des agents (#4639)

--- spec/policies/agent/agent_agenda_policy_spec.rb
+++ spec/policies/agent/agent_agenda_policy_spec.rb
@@ -30,6 +30,16 @@ RSpec.describe Agent::AgentAgendaPolicy, type: :policy do
       permissions(:show?) { it { is_expected.not_to permit(pundit_context, agenda) } }
     end
 
+    context "regular agent, other agent does not belong to current organisation" do
+      let!(:service) { create(:service) }
+      let!(:agent) { create(:agent, basic_role_in_organisations: [organisation], service: service) }
+      let!(:organisation2) { create(:organisation) }
+      let!(:other_agent) { create(:agent, basic_role_in_organisations: [organisation2], service: create(:service)) }
+      let(:agenda) { AgentAgenda.new(agent: other_agent, organisation: organisation) }
+
+      permissions(:show?) { it { is_expected.not_to permit(pundit_context, agenda) } }
+    end
+
     context "admin agent, other agent's absence, different service" do
       let!(:service) { create(:service) }
       let!(:agent) { create(:agent, admin_role_in_organisations: [organisation], service: service) }
