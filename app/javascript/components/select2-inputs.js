5c64f9586dea84a429fb3f05dbd2358bcc40b442
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-09-19 12:01:20 +0200

    Rename variables and methods in JS file

--- app/javascript/components/select2-inputs.js
+++ app/javascript/components/select2-inputs.js
@@ -20,39 +20,39 @@ class Select2Inputs {
   }
 
   initInput = (elt) => {
-    const options = this.getInputOptions(elt)
-    $(elt).select2(options)
+    const config = this.getInputConfig(elt)
+    $(elt).select2(config)
     if (elt.dataset.autoSelectSoleOption) {
-      this.autoSelectSoleOption(elt, options)
+      this.autoSelectSoleOption(elt, config)
     }
   }
 
-  getInputOptions = elt => {
-    let options = {}
+  getInputConfig = elt => {
+    let config = {}
     if (elt.dataset.select2Config !== undefined)
-      options = JSON.parse(elt.dataset.select2Config)
+      config = JSON.parse(elt.dataset.select2Config)
 
-    if (options.disableSearch)
-      options.minimumResultsForSearch = Infinity // cf https://select2.org/searching
+    if (config.disableSearch)
+      config.minimumResultsForSearch = Infinity // cf https://select2.org/searching
 
     // Make sure select2 works correctly inside a modal
     // https://select2.org/troubleshooting/common-problems#select2-does-not-function-properly-when-i-use-it-inside-a-bootst
     let modal = $(elt).closest(".modal")[0]
     if (modal !== undefined)
-      options.dropdownParent = modal
+      config.dropdownParent = modal
 
-    // Lorsque le select est configuré en AJAX **et** qu'aucune option n'est pré-injectée dans le HTML,
+    // Lorsque le select est configuré en AJAX **et** qu'aucune <option> n'est pré-injectée dans le HTML,
     // l'utilisateur⋅ice voit seulement un champ de recherche et une mention "Aucun résultat trouvé".
     // Afin d'indiquer clairement qu'il est attendu de commencer à saisir quelque-chose, ce code fait en
     // sorte que la mention soit plutot "Commencez à taper pour rechercher".
     const isAjax = elt.dataset.select2Config?.includes("ajax");
     const hasAnyOption = Array.from(elt.options).some(opt => opt.value); // we rule out options without value, they are usually placeholders
     if (isAjax && !hasAnyOption) {
-      options.minimumInputLength = 1
-      options.language = { inputTooShort: () => "Commencez à taper pour rechercher" } // Overrides select2/i18n/fr.js
+      config.minimumInputLength = 1
+      config.language = { inputTooShort: () => "Commencez à taper pour rechercher" } // Overrides select2/i18n/fr.js
     }
 
-    return options
+    return config
   }
 
   destroyInputs = () => {
