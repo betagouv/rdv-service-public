b952f6560e484b6d2260bf7a324e35551c8ec0f5
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2024-09-19 11:54:18 +0200

    nouvelle route de contrôle vérifiant que les jobs se dépilent (#4650)

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,30 @@
+class HealthController < ApplicationController
+  def db_connection
+    Territory.count # cette ligne raisera en cas de problème de connexion
+    render status: :ok, plain: "health OK"
+  end
+
+  def jobs_queues
+    counts1 = compute_enqueued_jobs_count_by_queue
+    queues_with_many_jobs = counts1.select { |_queue, count| count > 10 }
+    return render(status: :ok, json: {}) if queues_with_many_jobs.none?
+
+    sleep(5) # leave some time for some jobs to be performed
+    counts2 = compute_enqueued_jobs_count_by_queue
+    congested_queues = queues_with_many_jobs.select { |queue, count1| counts2.fetch(queue, 0) >= count1 }.keys
+
+    return render(status: :service_unavailable, json: { congested_queues: }) if congested_queues
+
+    render(status: :ok, json: {})
+  end
+
+  private
+
+  def compute_enqueued_jobs_count_by_queue
+    GoodJob::Job
+      .group(:queue_name)
+      .where("scheduled_at < ?", Time.zone.now)
+      .where(finished_at: nil)
+      .count
+  end
+end
