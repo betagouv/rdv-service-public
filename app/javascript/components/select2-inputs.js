e1b9bfe979bd89292bd4db028c75beee352308d3
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-09-19 11:55:26 +0200

    Renommer "select-options" pour éviter l'ambiguïté avec les <option>

--- app/javascript/components/select2-inputs.js
+++ app/javascript/components/select2-inputs.js
@@ -29,10 +29,12 @@ class Select2Inputs {
 
   getInputOptions = elt => {
     let options = {}
-    if (elt.dataset.selectOptions !== undefined)
-      options = JSON.parse(elt.dataset.selectOptions)
+    if (elt.dataset.select2Config !== undefined)
+      options = JSON.parse(elt.dataset.select2Config)
+
     if (options.disableSearch)
       options.minimumResultsForSearch = Infinity // cf https://select2.org/searching
+
     // Make sure select2 works correctly inside a modal
     // https://select2.org/troubleshooting/common-problems#select2-does-not-function-properly-when-i-use-it-inside-a-bootst
     let modal = $(elt).closest(".modal")[0]
@@ -43,7 +45,7 @@ class Select2Inputs {
     // l'utilisateur⋅ice voit seulement un champ de recherche et une mention "Aucun résultat trouvé".
     // Afin d'indiquer clairement qu'il est attendu de commencer à saisir quelque-chose, ce code fait en
     // sorte que la mention soit plutot "Commencez à taper pour rechercher".
-    const isAjax = elt.dataset.selectOptions?.includes("ajax");
+    const isAjax = elt.dataset.select2Config?.includes("ajax");
     const hasAnyOption = Array.from(elt.options).some(opt => opt.value); // we rule out options without value, they are usually placeholders
     if (isAjax && !hasAnyOption) {
       options.minimumInputLength = 1
@@ -61,7 +63,7 @@ class Select2Inputs {
   autoSelectSoleOption = (elt, options) => {
     // This code checks if a select element (represented by the `elt` variable) has only one option.
     // return all options if it is ajax
-    const isAjax = elt.dataset.selectOptions?.includes("ajax");
+    const isAjax = elt.dataset.select2Config?.includes("ajax");
     if (isAjax) return;
 
     // Get all options and remove blank values if exists (placeholders)
