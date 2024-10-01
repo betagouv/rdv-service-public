8800d523cd7bb16e6ce3b0edb54774a57b41d87d
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-01 18:18:39 +0200

    refactor: inline all ants_stub requests

--- spec/models/concerns/ants/appointment_serializer_and_listener_spec.rb
+++ spec/models/concerns/ants/appointment_serializer_and_listener_spec.rb
@@ -22,7 +22,12 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
         let!(:rdv) { build(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
 
         it "créé l’appointment via l’API ANTS" do
-          stub_ants_status("A123456789", status: "validated", appointments: [])
+          stub_request(:get, "#{API_URL}/status")
+            .with(query: { application_ids: "A123456789" })
+            .to_return(
+              status: 200,
+              body: { "A123456789" => { status: "validated", appointments: [] } }.to_json
+            )
           stub_request(:post, "#{API_URL}/appointments")
             .with(query: hash_including(application_id: "A123456789")) # Webmock ne répond pas à la requête POST avec des query params sans cette ligne
             .to_return(status: 200, body: { success: true }.to_json)
@@ -81,21 +86,32 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
       let!(:rdv) { create(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
 
       it "supprime l’appointment via l’API ANTS" do
-        stub_ants_delete("A123456789")
-        stub_ants_status(
-          "A123456789",
-          appointments: [
-            {
-              management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
-              meeting_point: "MDS Soleil",
-              meeting_point_id: rdv.lieu.id,
-              appointment_date: "2020-04-20 08:00:00",
-            },
-          ]
-        )
+        stub_request(:delete, "#{API_URL}/appointments")
+          .with(query: hash_including(application_id: "A123456789"))
+          .to_return(status: 200, body: { rowcount: 1 }.to_json)
+        stub_request(:get, "#{API_URL}/status")
+          .with(query: { application_ids: "A123456789" })
+          .to_return(
+            status: 200,
+            body: {
+              "A123456789" => {
+                status: "validated",
+                appointments: [
+                  {
+                    management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
+                    meeting_point: "MDS Soleil",
+                    meeting_point_id: rdv.lieu.id,
+                    appointment_date: "2020-04-20 08:00:00",
+                  },
+                ],
+              },
+            }.to_json
+          )
+
         perform_enqueued_jobs do
           rdv.destroy
         end
+
         expect(WebMock).to have_requested(:delete, "#{API_URL}/appointments")
           .with(
             query: {
@@ -118,21 +134,32 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
         let!(:rdv) { create(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
 
         it "supprime l’appointment via l’API ANTS" do
-          stub_ants_status(
-            "A123456789",
-            appointments: [
-              {
-                management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
-                meeting_point: "MDS Soleil",
-                meeting_point_id: rdv.lieu.id,
-                appointment_date: "2020-04-20 08:00:00",
-              },
-            ]
-          )
-          stub_ants_delete("A123456789")
+          stub_request(:get, "#{API_URL}/status")
+            .with(query: { application_ids: "A123456789" })
+            .to_return(
+              status: 200,
+              body: {
+                "A123456789" => {
+                  status: "validated",
+                  appointments: [
+                    {
+                      management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
+                      meeting_point: "MDS Soleil",
+                      meeting_point_id: rdv.lieu.id,
+                      appointment_date: "2020-04-20 08:00:00",
+                    },
+                  ],
+                },
+              }.to_json
+            )
+          stub_request(:delete, "#{API_URL}/appointments")
+            .with(query: hash_including(application_id: "A123456789"))
+            .to_return(status: 200, body: { rowcount: 1 }.to_json)
+
           perform_enqueued_jobs do
             rdv.excused!
           end
+
           expect(WebMock).to have_requested(:delete, "#{API_URL}/appointments")
             .with(
               query: {
@@ -156,22 +183,35 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
         before { rdv.excused! }
 
         it "créé l’appointment via l’API ANTS" do
-          stub_ants_status(
-            "A123456789",
-            appointments: [
-              {
-                management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
-                meeting_point: "MDS Soleil",
-                meeting_point_id: rdv.lieu.id,
-                appointment_date: "2020-04-20 08:00:00",
-              },
-            ]
-          )
-          stub_ants_delete("A123456789")
-          stub_ants_create("A123456789")
+          stub_request(:get, "#{API_URL}/status")
+            .with(query: { application_ids: "A123456789" })
+            .to_return(
+              status: 200,
+              body: {
+                "A123456789" => {
+                  status: "validated",
+                  appointments: [
+                    {
+                      management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
+                      meeting_point: "MDS Soleil",
+                      meeting_point_id: rdv.lieu.id,
+                      appointment_date: "2020-04-20 08:00:00",
+                    },
+                  ],
+                },
+              }.to_json
+            )
+          stub_request(:delete, "#{API_URL}/appointments")
+            .with(query: hash_including(application_id: "A123456789"))
+            .to_return(status: 200, body: { rowcount: 1 }.to_json)
+          stub_request(:post, "#{API_URL}/appointments")
+            .with(query: hash_including(application_id: "A123456789"))
+            .to_return(status: 200, body: { success: true }.to_json)
+
           perform_enqueued_jobs do
             rdv.seen!
           end
+
           expect(WebMock).to have_requested(:post, "#{API_URL}/appointments")
             .with(
               query: {
@@ -198,11 +238,20 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
 
       it "créé un nouvel appointment via l’API ANTS" do
         user.reload
-        create_appointment_stub = stub_ants_create("AABBCCDDEE")
-        stub_ants_status("AABBCCDDEE", status: "validated", appointments: [])
+        create_appointment_stub = stub_request(:post, "#{API_URL}/appointments")
+          .with(query: hash_including(application_id: "AABBCCDDEE"))
+          .to_return(status: 200, body: { success: true }.to_json)
+        stub_request(:get, "#{API_URL}/status")
+          .with(query: { application_ids: "AABBCCDDEE" })
+          .to_return(
+            status: 200,
+            body: { "AABBCCDDEE" => { status: "validated", appointments: [] } }.to_json
+          )
+
         perform_enqueued_jobs do
           user.update(ants_pre_demande_number: "AABBCCDDEE")
         end
+
         expect(create_appointment_stub).to have_been_requested.at_least_once
       end
     end
@@ -218,11 +267,20 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
 
       it "déclenche une synchronisation avec l’ANTS" do
         lieu.reload
-        create_appointment_stub = stub_ants_create("A123456789")
-        stub_ants_status("A123456789", status: "validated", appointments: [])
+        create_appointment_stub = stub_request(:post, "#{API_URL}/appointments")
+          .with(query: hash_including(application_id: "A123456789"))
+          .to_return(status: 200, body: { success: true }.to_json)
+        stub_request(:get, "#{API_URL}/status")
+          .with(query: { application_ids: "A123456789" })
+          .to_return(
+            status: 200,
+            body: { "A123456789" => { status: "validated", appointments: [] } }.to_json
+          )
+
         perform_enqueued_jobs do
           lieu.update(name: "Nouveau Lieu")
         end
+
         expect(create_appointment_stub).to have_been_requested.at_least_once
       end
     end
@@ -238,19 +296,28 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
 
       it "supprime l’appointment via l’API ANTS" do
         user.reload
-        stub_ants_status("A123456789", status: "validated", appointments: [])
-        stub_ants_status(
-          "A123456789",
-          appointments: [
-            {
-              management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
-              meeting_point: "MDS Soleil",
-              meeting_point_id: rdv.lieu.id,
-              appointment_date: "2020-04-20 08:00:00",
-            },
-          ]
-        )
-        stub_ants_delete("A123456789")
+        stub_request(:get, "#{API_URL}/status")
+          .with(query: { application_ids: "A123456789" })
+          .to_return(
+            status: 200,
+            body: {
+              "A123456789" => {
+                status: "validated",
+                appointments: [
+                  {
+                    management_url: "http://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
+                    meeting_point: "MDS Soleil",
+                    meeting_point_id: rdv.lieu.id,
+                    appointment_date: "2020-04-20 08:00:00",
+                  },
+                ],
+              },
+            }.to_json
+          )
+        stub_request(:delete, "#{API_URL}/appointments")
+          .with(query: hash_including(application_id: "A123456789"))
+          .to_return(status: 200, body: { rowcount: 1 }.to_json)
+
         perform_enqueued_jobs do
           user.participations.first.destroy
         end
@@ -268,10 +335,17 @@ RSpec.describe Ants::AppointmentSerializerAndListener do
         let!(:rdv) { create(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
 
         it "ne déclenche pas la création d’un nouvel appointment via l’API ANTS" do
-          stub_ants_status("A123456789", status: "consumed", appointments: [])
+          stub_request(:get, "#{API_URL}/status")
+            .with(query: { application_ids: "A123456789" })
+            .to_return(
+              status: 200,
+              body: { "A123456789" => { status: "consumed", appointments: [] } }.to_json
+            )
+
           perform_enqueued_jobs do
             rdv.excused!
           end
+
           expect(WebMock).not_to have_requested(:post, "#{API_URL}/appointments").with(headers: ants_api_headers)
         end
       end
