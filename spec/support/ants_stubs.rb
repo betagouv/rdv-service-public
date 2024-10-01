c06b1a7b19b9657272dee716460f04f13a27b2da
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-01 14:28:53 +0200

    refacto: légères améliorations du helper de stubs

--- spec/support/ants_stubs.rb
+++ spec/support/ants_stubs.rb
@@ -5,17 +5,14 @@ def stub_ants_status(application_id, status: "validated", appointments: [])
     .with(query: { application_ids: application_id })
     .to_return(
       status: 200,
-      body: { application_id => { status: status, appointments: appointments } }.to_json
+      body: { application_id => { status:, appointments: } }.to_json
     )
 end
 
 def stub_ants_create(application_id)
   stub_request(:post, "#{API_URL}/appointments")
-    .with(query: hash_including({ "application_id" => application_id }))
-    .to_return(
-      status: 200,
-      body: { success: true }.to_json
-    )
+    .with(query: hash_including(application_id:))
+    .to_return(status: 200, body: { success: true }.to_json)
 end
 
 def stub_ants_delete(application_id)
