fa5629c466e997602d664bc87c26d77a4e894505
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-02 09:19:56 +0200

    refactor des stubs ants dans les specs

--- spec/support/ants_stubs.rb
+++ spec/support/ants_stubs.rb
@@ -1,24 +1,22 @@
+API_URL = "https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api".freeze
+
 def stub_ants_status(application_id, status: "validated", appointments: [])
-  stub_request(:get, "https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/status?application_ids=#{application_id}").to_return(
-    status: 200,
-    body: { application_id => { status: status, appointments: appointments } }.to_json
-  )
+  stub_request(:get, "#{API_URL}/status")
+    .with(query: { application_ids: application_id })
+    .to_return(
+      status: 200,
+      body: { application_id => { status:, appointments: } }.to_json
+    )
 end
 
 def stub_ants_create(application_id)
-  stub_request(:post, %r{https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/appointments/*})
-    .with(query: hash_including({ "application_id" => application_id }))
-    .to_return(
-      status: 200,
-      body: { success: true }.to_json
-    )
+  stub_request(:post, "#{API_URL}/appointments")
+    .with(query: hash_including(application_id:))
+    .to_return(status: 200, body: { success: true }.to_json)
 end
 
 def stub_ants_delete(application_id)
-  stub_request(:delete, %r{https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/appointments/*})
-    .with(query: hash_including({ "application_id" => application_id }))
-    .to_return(
-      status: 200,
-      body: { rowcount: 1 }.to_json
-    )
+  stub_request(:delete, "#{API_URL}/appointments")
+    .with(query: hash_including(application_id:))
+    .to_return(status: 200, body: { rowcount: 1 }.to_json)
 end
