009fd518df04e9a95ccb75b52764e2e0f5aa75cb
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-04 14:50:34 +0200

    Stop using a "current organisation" in Agent::PlageOuverturePolicy

--- app/policies/agent/plage_ouverture_policy.rb
+++ app/policies/agent/plage_ouverture_policy.rb
@@ -1,29 +1,55 @@
-class Agent::PlageOuverturePolicy < DefaultAgentPolicy
-  class Scope < Scope
-    def resolve
-      if context.can_access_others_planning?
-        scope.where(organisation: current_organisation)
-      else
-        scope.joins(:agent).where(organisation: current_organisation).merge(current_agent.confreres)
-      end
-    end
+class Agent::PlageOuverturePolicy < ApplicationPolicy
+  include CurrentAgentInPolicyConcern
+
+  def show?
+    same_agent_or_has_access?
   end
+  alias versions? show?
+
+  def update?
+    same_agent_or_has_access?
+  end
+  alias new? update?
+  alias create? update?
+  alias edit? update?
+  alias destroy? update?
 
   private
+
+  def same_agent_or_has_access?
+    return true if @record.agent == current_agent
+
+    case current_agent.access_level_in(@record.organisation)
+    when AgentRole::ACCESS_LEVEL_ADMIN
+      true
+    when AgentRole::ACCESS_LEVEL_BASIC
+      same_service? || current_agent.secretaire?
+    else
+      false
+    end
+  end
 
   def same_service?
     @record.agent.confrere_of?(current_agent)
   end
 
-  class DepartementScope < Scope
+  class Scope < Scope
+    include CurrentAgentInPolicyConcern
+
     def resolve
-      if context.can_access_others_planning?
-        scope.where(organisation: current_agent.organisations)
+      my_plages = PlageOuverture.where(agent: current_agent)
+      if current_agent.secretaire?
+        plages_of_all_my_orgs = PlageOuverture.where(organisation: current_agent.organisations)
+        scope.where_id_in_subqueries([my_plages, plages_of_all_my_orgs])
       else
-        scope.joins(:agent)
-          .where(organisation: current_agent.organisations)
-          .merge(current_agent.confreres)
+        plages_of_my_admin_orgs = PlageOuverture.where(organisation: current_agent.admin_orgs)
+        plages_of_confreres_in_my_basic_orgs = PlageOuverture.where(organisation: current_agent.basic_orgs)
+          .joins(:agent).merge(current_agent.confreres)
+        scope.where_id_in_subqueries([my_plages, plages_of_my_admin_orgs, plages_of_confreres_in_my_basic_orgs])
       end
     end
+  end
+
+  class DepartementScope < Scope
   end
 end
