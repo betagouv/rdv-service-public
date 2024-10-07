4e4ee3aff33bff918faf4f411b37a5638bb41369
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-07 17:09:05 +0200

    Explicitly pass policy_class to policy(record, policy_class:)

--- app/controllers/concerns/explicit_pundit_concern.rb
+++ app/controllers/concerns/explicit_pundit_concern.rb
@@ -12,4 +12,9 @@ module ExplicitPunditConcern
   def policy_scope(scope, policy_scope_class:)
     super(scope, policy_scope_class: policy_scope_class) # rubocop:disable Style/SuperArguments
   end
+
+  # NOTE: it is a project-specific choice to always pass `policy_class` explicitly
+  def policy(record, policy_class:)
+    policy_class.new(pundit_user, record)
+  end
 end
