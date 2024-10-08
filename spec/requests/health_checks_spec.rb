1fcecea209bdf9b6b6474abb9fe177c69498e895
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-08 11:35:33 +0200

    Retirer une config ajoutée pour la stack OVH (health check HTTP) (#4687)

--- spec/requests/health_checks_spec.rb
+++ spec/requests/health_checks_spec.rb
@@ -1,14 +1,4 @@
 RSpec.describe "health checks" do
-  #
-  # Cet endpoint a été ajouté lors de la migration de Scalingo vers OVH.
-  # En effet, la nouvelle stack a besoin d'un endpoint de health check pour
-  # savoir si l'appli tourne, et la redémarrer si nécessaire.
-  #
-  # La particularité de cette route est qu'elle doit être accessible en
-  # production en HTTP plutôt qu'en HTTP, car l'agent qui fait le health
-  # check est interne au réseau Kubernetes (le HTTPS en géré à l'extérieur).
-  # Nous avons donc défini une config.ssl_options qui exclut cette route.
-  #
   describe "/health_check" do
     it "returns HTTP 200" do
       get "/health_check"
