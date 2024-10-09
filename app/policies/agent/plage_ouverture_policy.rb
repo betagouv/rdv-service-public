fa29a79151d20ae0d17f11db4490b68d2ffdb68d
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-09 12:07:25 +0200

    Use OR instead of subqueries

--- app/policies/agent/plage_ouverture_policy.rb
+++ app/policies/agent/plage_ouverture_policy.rb
@@ -36,15 +36,20 @@ class Agent::PlageOuverturePolicy < ApplicationPolicy
     include CurrentAgentInPolicyConcern
 
     def resolve
-      my_plages = PlageOuverture.where(agent: current_agent)
       if current_agent.secretaire?
-        plages_of_all_my_orgs = PlageOuverture.where(organisation: current_agent.organisations)
-        scope.where_id_in_subqueries([my_plages, plages_of_all_my_orgs])
+        scope.joins("INNER JOIN agent_roles ON agent_roles.organisation_id = plage_ouvertures.organisation_id")
+          .where(agent_roles: { agent_id: current_agent.id }) # plages des organisations dans lesquelles j'ai un role
       else
-        plages_of_my_admin_orgs = PlageOuverture.where(organisation: current_agent.admin_orgs)
-        plages_of_confreres_in_my_basic_orgs = PlageOuverture.where(organisation: current_agent.basic_orgs)
-          .joins(:agent).merge(current_agent.confreres)
-        scope.where_id_in_subqueries([my_plages, plages_of_my_admin_orgs, plages_of_confreres_in_my_basic_orgs])
+        confreres_of_my_orgs = current_agent.confreres.joins(:roles)
+          .where(agent_roles: { organisation_id: current_agent.organisations }).ids
+        scope.joins("INNER JOIN agent_roles ON agent_roles.organisation_id = plage_ouvertures.organisation_id")
+          .where(agent_roles: { agent_id: current_agent.id }) # plages des organisations dans lesquelles j'ai un role
+          .where(
+            "plage_ouvertures.agent_id = ?
+              OR (plage_ouvertures.agent_id IN (?) AND agent_roles.access_level = 'basic')
+              OR (agent_roles.access_level = 'admin')",
+            current_agent.id, confreres_of_my_orgs
+          )
       end
     end
   end
