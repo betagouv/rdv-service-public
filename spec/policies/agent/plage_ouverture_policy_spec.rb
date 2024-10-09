ca0dfe26114543378519d73b84a50121506f979c
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-09 23:14:37 +0200

    Don't use cryptic shared_examples for policy spec

--- spec/policies/agent/plage_ouverture_policy_spec.rb
+++ spec/policies/agent/plage_ouverture_policy_spec.rb
@@ -1,48 +1,72 @@
 RSpec.describe Agent::PlageOuverturePolicy, type: :policy do
-  subject { described_class }
+  let(:policy) { described_class.new(agent, plage_ouverture) }
+  let(:policy_scope) { described_class::Scope.new(agent, PlageOuverture.all).resolve }
 
   let(:plage_ouverture) { create(:plage_ouverture) }
-  let(:pundit_context) { AgentOrganisationContext.new(agent, plage_ouverture.organisation) }
-
-  shared_examples "included in scope" do
-    it "is included in scope" do
-      expect(described_class::Scope.new(pundit_context, PlageOuverture).resolve).to include(plage_ouverture)
-    end
-  end
-
-  shared_examples "not included in scope" do
-    it "is not included in scope" do
-      expect(described_class::Scope.new(pundit_context, PlageOuverture).resolve).not_to include(plage_ouverture)
-    end
-  end
 
   context "when the plage belongs to the given agent" do
     let(:agent) { plage_ouverture.agent }
 
-    it_behaves_like "permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-    it_behaves_like "included in scope"
+    it "allows read/write operations" do
+      expect(policy.new?).to       be true
+      expect(policy.create?).to    be true
+      expect(policy.edit?).to      be true
+      expect(policy.update?).to    be true
+      expect(policy.destroy?).to   be true
+      expect(policy.show?).to      be true
+      expect(policy.versions?).to  be true
+
+      expect(policy_scope).to include(plage_ouverture)
+    end
   end
 
   context "when given agent is secrétaire" do
     context "when she does not belong to the plage's organisation" do
       let(:agent) { create(:agent, :secretaire, basic_role_in_organisations: [create(:organisation)]) }
 
-      it_behaves_like "not permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-      it_behaves_like "not included in scope"
+      it "allows read/write operations" do
+        expect(policy.new?).to       be false
+        expect(policy.create?).to    be false
+        expect(policy.edit?).to      be false
+        expect(policy.update?).to    be false
+        expect(policy.destroy?).to   be false
+        expect(policy.show?).to      be false
+        expect(policy.versions?).to  be false
+
+        expect(policy_scope).not_to include(plage_ouverture)
+      end
     end
 
     context "when she belongs to the plage's organisation as basic member" do
       let(:agent) { create(:agent, :secretaire, basic_role_in_organisations: [plage_ouverture.organisation]) }
 
-      it_behaves_like "permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-      it_behaves_like "included in scope"
+      it "allows read/write operations" do
+        expect(policy.new?).to       be true
+        expect(policy.create?).to    be true
+        expect(policy.edit?).to      be true
+        expect(policy.update?).to    be true
+        expect(policy.destroy?).to   be true
+        expect(policy.show?).to      be true
+        expect(policy.versions?).to  be true
+
+        expect(policy_scope).to include(plage_ouverture)
+      end
     end
 
     context "when she belongs to the plage's organisation as admin member" do
       let(:agent) { create(:agent, :secretaire, admin_role_in_organisations: [plage_ouverture.organisation]) }
 
-      it_behaves_like "permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-      it_behaves_like "included in scope"
+      it "allows read/write operations" do
+        expect(policy.new?).to       be true
+        expect(policy.create?).to    be true
+        expect(policy.edit?).to      be true
+        expect(policy.update?).to    be true
+        expect(policy.destroy?).to   be true
+        expect(policy.show?).to      be true
+        expect(policy.versions?).to  be true
+
+        expect(policy_scope).to include(plage_ouverture)
+      end
     end
   end
 
@@ -50,8 +74,17 @@ RSpec.describe Agent::PlageOuverturePolicy, type: :policy do
     context "when she does not belong to the plage's organisation" do
       let(:agent) { create(:agent, basic_role_in_organisations: [create(:organisation)]) }
 
-      it_behaves_like "not permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-      it_behaves_like "not included in scope"
+      it "allows read/write operations" do
+        expect(policy.new?).to       be false
+        expect(policy.create?).to    be false
+        expect(policy.edit?).to      be false
+        expect(policy.update?).to    be false
+        expect(policy.destroy?).to   be false
+        expect(policy.show?).to      be false
+        expect(policy.versions?).to  be false
+
+        expect(policy_scope).not_to include(plage_ouverture)
+      end
     end
 
     context "when she belongs to the plage's organisation as basic member" do
@@ -65,8 +98,17 @@ RSpec.describe Agent::PlageOuverturePolicy, type: :policy do
           expect(agent.services.to_set.intersection(plage_ouverture.agent.services.to_set)).not_to be_empty # rubocop:disable RSpec/ExpectInHook
         end
 
-        it_behaves_like "permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-        it_behaves_like "included in scope"
+        it "allows read/write operations" do
+          expect(policy.new?).to       be true
+          expect(policy.show?).to      be true
+          expect(policy.versions?).to  be true
+          expect(policy.create?).to    be true
+          expect(policy.edit?).to      be true
+          expect(policy.update?).to    be true
+          expect(policy.destroy?).to   be true
+
+          expect(policy_scope).to include(plage_ouverture)
+        end
       end
 
       context "when she shares no service with the plage's agent" do
@@ -74,16 +116,34 @@ RSpec.describe Agent::PlageOuverturePolicy, type: :policy do
           expect(agent.services.to_set.intersection(plage_ouverture.agent.services.to_set)).to be_empty # rubocop:disable RSpec/ExpectInHook
         end
 
-        it_behaves_like "not permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-        it_behaves_like "not included in scope"
+        it "allows read/write operations" do
+          expect(policy.new?).to       be false
+          expect(policy.create?).to    be false
+          expect(policy.edit?).to      be false
+          expect(policy.update?).to    be false
+          expect(policy.destroy?).to   be false
+          expect(policy.show?).to      be false
+          expect(policy.versions?).to  be false
+
+          expect(policy_scope).not_to include(plage_ouverture)
+        end
       end
     end
 
     context "when she belongs to the plage's organisation as admin member" do
       let(:agent) { create(:agent, admin_role_in_organisations: [plage_ouverture.organisation]) }
 
-      it_behaves_like "permit actions", :plage_ouverture, :new?, :show?, :versions?, :create?, :edit?, :update?, :destroy?
-      it_behaves_like "included in scope"
+      it "allows read/write operations" do
+        expect(policy.new?).to be true
+        expect(policy.create?).to    be true
+        expect(policy.edit?).to      be true
+        expect(policy.update?).to    be true
+        expect(policy.destroy?).to   be true
+        expect(policy.show?).to      be true
+        expect(policy.versions?).to  be true
+
+        expect(policy_scope).to include(plage_ouverture)
+      end
     end
   end
 end
