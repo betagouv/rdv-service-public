57296bc9ad5852119509309bc0edf41366aba0ea
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-15 17:56:53 +0200

    Reapply all changes of reverted PRs

--- app/controllers/admin/agenda/rdvs_controller.rb
+++ app/controllers/admin/agenda/rdvs_controller.rb
@@ -2,8 +2,7 @@ class Admin::Agenda::RdvsController < Admin::Agenda::BaseController
   def index
     agent = Agent.find(params[:agent_id])
     @organisation = Organisation.find(params[:organisation_id])
-    @rdvs = custom_policy
-      .merge(agent.rdvs)
+    @rdvs = policy_scope(agent.rdvs, policy_scope_class: Agent::RdvPolicy::Scope)
       .includes(%i[organisation lieu motif users participations])
     @rdvs = @rdvs.where(starts_at: time_range_params)
     @rdvs = @rdvs.where(status: Rdv::NOT_CANCELLED_STATUSES) unless current_agent.display_cancelled_rdv
@@ -11,10 +10,7 @@ class Admin::Agenda::RdvsController < Admin::Agenda::BaseController
 
   private
 
-  # TODO: custom policy waiting for policies refactoring
-  def custom_policy
-    context = AgentOrganisationContext.new(current_agent, @organisation)
-    Agent::RdvPolicy::DepartementScope.new(context, Rdv)
-      .resolve
+  def pundit_user
+    current_agent
   end
 end
