a0429973a774b902c6331c1bf96f640431299f55
Victor Mours <victor.mours@gmail.com>
2024-10-17 18:39:08 +0200

    wip

--- db/migrate/20241017163711_add_creneaux_search_rdv_index.rb
+++ db/migrate/20241017163711_add_creneaux_search_rdv_index.rb
@@ -1,4 +1,7 @@
 class AddCreneauxSearchRdvIndex < ActiveRecord::Migration[7.0]
+  disable_ddl_transaction!
+
   def change
+    add_index :rdvs, %i[status starts_at ends_at], algorithm: :concurrently
   end
 end
