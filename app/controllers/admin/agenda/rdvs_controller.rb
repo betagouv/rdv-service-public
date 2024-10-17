765c4604600e06eaeb655fd06a2eb32386b15f4e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-17 09:08:46 +0200

    Try to introduce a new class to prevent accidental leak

--- app/controllers/admin/agenda/rdvs_controller.rb
+++ app/controllers/admin/agenda/rdvs_controller.rb
@@ -7,9 +7,27 @@ class Admin::Agenda::RdvsController < Admin::Agenda::BaseController
     # Pour chacun de ces RDVs, nous faisons appel à `Agent::RdvPolicy#show`
     # dans la vue pour déterminer si il faut afficher les infos du RDV ou non.
     skip_authorization
-    @rdvs = agent.rdvs.includes(:organisation, :lieu, :users, :agents, :participations, motif: [:service])
+    rdvs = agent.rdvs.includes(:organisation, :lieu, :users, :agents, :participations, motif: [:service])
 
-    @rdvs = @rdvs.where(starts_at: time_range_params)
-    @rdvs = @rdvs.where(status: Rdv::NOT_CANCELLED_STATUSES) unless current_agent.display_cancelled_rdv
+    rdvs = rdvs.where(starts_at: time_range_params)
+    @rdvs = rdvs.map do |rdv|
+      if Agent::RdvPolicy.new(current_agent, rdv).show?
+        rdv if rdv.not_cancelled? || current_agent.display_cancelled_rdv
+      else
+        UnauthorizedRdv.new(rdv)
+      end
+    end.compact
+  end
+
+  class UnauthorizedRdv
+    def initialize(rdv)
+      @rdv = rdv
+    end
+
+    delegate :starts_at, :ends_at, to: :@rdv
+
+    def to_partial_path
+      "unauthorized_rdv"
+    end
   end
 end
---
+++ app/views/admin/agenda/rdvs/_unauthorized_rdv.json.jbuilder
@@ -0,0 +1,7 @@
+json.id unauthorized_rdv.id
+json.title "Occupé⋅e (en RDV)"
+json.textColor "black"
+json.backgroundColor "#dadada" # Overridden by striped background in CSS
+json.extendedProps do
+  json.unauthorizedRdvExplanation "Vous ne pouvez pas voir ce RDV parce qu'il a lieu dans un autre service ou une autre organisation"
+end
