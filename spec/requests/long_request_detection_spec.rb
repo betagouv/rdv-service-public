50c7cadb20f368c3ea243cfb64c14b0bf8830d6d
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-10-18 18:11:45 +0200

    Clarify expected error message

--- spec/requests/long_request_detection_spec.rb
+++ spec/requests/long_request_detection_spec.rb
@@ -2,6 +2,6 @@ RSpec.describe "detecting long-running requests" do
   it "works" do
     stub_const "LongRequestNotifyMiddleware::TIME_LIMIT", 0.00001.seconds
     get "/health_check"
-    expect(sentry_events.last.message).to start_with("Long request detected: /health_check took")
+    expect(sentry_events.last.message).to match(%r{Long request detected: /health_check took .* seconds})
   end
 end
