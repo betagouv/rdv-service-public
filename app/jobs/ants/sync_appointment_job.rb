ddbfd1016c7f58b8c1e8889e2c6f7b44d6493d66
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-18 14:41:26 +0200

    improvements

--- app/jobs/ants/sync_appointment_job.rb
+++ app/jobs/ants/sync_appointment_job.rb
@@ -7,34 +7,34 @@ module Ants
       key: -> { "#{self.class.name}-rdv-#{arguments.last[:application_id]}" }
     )
     # useful to debug tests and avoid retries
-    discard_on(StandardError) { |_job, ex| raise ex }
+    # discard_on(StandardError) { |_job, ex| raise ex }
 
     def perform(application_id:)
-      ants_status = AntsApi.status(application_id:, timeout: 4).deep_symbolize_keys
+      ants_status = AntsApi.status(application_id:, timeout: 4)
 
-      return false unless ants_status[:status] == "validated"
+      return false unless ants_status["status"] == "validated"
 
-      ants_appointments = ants_status[:appointments]
-        .select { _1[:management_url].include?("rdv") } # TODO
-      puts "ants_appointments is #{ants_appointments}"
+      ants_appointments = ants_status["appointments"]
+        .select { _1["management_url"].include?("rdv") } # TODO
 
       rdv = Rdv.joins(:users)
         .where(users: { ants_pre_demande_number: application_id })
         .where.not(status: Rdv::CANCELLED_STATUSES)
         .first
 
-      # puts "comparing \n#{ants_appointments} and #{rdv&.serialize_for_ants_api}"
       # on ne fait rien si les infos sont déjà identiques
-      return if ants_appointments == [rdv&.serialize_for_ants_api]
+      return true if ants_appointments == [rdv&.serialize_for_ants_api]
 
-      # Create : cette ligne ne fera rien
-      # Update : l’API de l’ANTS ne permet pas de faire de mises à jour, on fait donc un delete puis un update
-      # Delete : cette ligne
+      # on déclenche la suppression des appointments existants dans tous les cas, qu’il s’agisse d’une mise à jour ou d’une suppression
+      # en effet l’API de l’ANTS ne permet pas de faire de mises à jour, on fait donc un delete puis un update
       ants_appointments.each do |appointment|
-        AntsApi.delete(application_id:, **appointment.slice(:meeting_point, :appointment_date, :meeting_point_id))
+        AntsApi.delete(
+          application_id:,
+          **appointment.symbolize_keys.slice(:meeting_point, :appointment_date, :meeting_point_id)
+        )
       end
 
-      # S’il n’y a aucun RDV non-annulé dans notre DB, on s’arrête ici, il n’y a plus aucun appointments ANTS
+      # S’il n’y a aucun RDV non-annulé dans notre DB, on s’arrête ici. Il n’y a plus aucun appointment ANTS
       return unless rdv
 
       AntsApi.create(application_id:, **rdv.serialize_for_ants_api)
