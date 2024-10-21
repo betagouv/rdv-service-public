ca9bc2f84958f6eb722997201bfd7298ce01983f
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-21 14:02:15 +0200

    petit refactor de conditions dans User::RdvPolicy

--- app/policies/user/rdv_policy.rb
+++ app/policies/user/rdv_policy.rb
@@ -27,9 +27,11 @@ class User::RdvPolicy < ApplicationPolicy
   end
 
   def show?
-    return true if record.collectif? && record.bookable_by_everyone_or_bookable_by_invited_users? && rdv_belongs_to_user_or_relatives?
-
-    rdv_belongs_to_user_or_relatives? && (!current_user.signed_in_with_invitation_token? || current_user.invited_for_rdv?(record))
+    rdv_belongs_to_user_or_relatives? && (
+      (record.collectif? && record.bookable_by_everyone_or_bookable_by_invited_users?) ||
+      !current_user.signed_in_with_invitation_token? ||
+      current_user.invited_for_rdv?(record)
+    )
   end
 
   def cancel?
@@ -41,7 +43,9 @@ class User::RdvPolicy < ApplicationPolicy
   end
 
   def can_change_participants?
-    !current_user.signed_in_with_invitation_token? && current_user.participation_for(record).not_cancelled? && !record.in_the_past?
+    !current_user.signed_in_with_invitation_token? &&
+      current_user.participation_for(record).not_cancelled? &&
+      !record.in_the_past?
   end
 
   alias creneaux? edit?
