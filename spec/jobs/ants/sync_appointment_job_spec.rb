589b348fc41685c71d8bea69908655931ef329ba
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-10-28 10:28:58 +0100

    ajout de specs au job sync appointment, dont une qui échoue pour la catégorie de motif

--- spec/jobs/ants/sync_appointment_job_spec.rb
+++ spec/jobs/ants/sync_appointment_job_spec.rb
@@ -16,4 +16,80 @@ RSpec.describe Ants::SyncAppointmentJob do
       )
     end
   end
+
+  context "Nouveau RDV ANTS" do
+    let!(:organisation) { create(:organisation, verticale: :rdv_mairie) }
+    let!(:lieu) { create(:lieu, organisation:, name: "Mairie de Saumur") }
+    let!(:motif) { create(:motif, motif_category: create(:motif_category, :passeport)) }
+    let!(:user) { create(:user, ants_pre_demande_number: "A123456789", organisations: [organisation]) }
+    let!(:rdv) { create(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
+
+    before { travel_to(Time.zone.parse("2020-02-10")) } # le RDV est dans le futur
+
+    it "créé un appointment" do
+      allow(AntsApi).to receive(:status)
+        .with(hash_including(application_id: "A123456789"))
+        .and_return({ "status" => "validated", "appointments" => [] })
+      expect(AntsApi).not_to receive(:delete)
+      expect(AntsApi).to receive(:create).with(hash_including(application_id: "A123456789"))
+      described_class.perform_now(application_id: "A123456789")
+    end
+  end
+
+  context "Synchro pour un RDV ANTS dans le passé" do
+    let!(:organisation) { create(:organisation, verticale: :rdv_mairie) }
+    let!(:lieu) { create(:lieu, organisation:, name: "Mairie de Saumur") }
+    let!(:motif) { create(:motif, motif_category: create(:motif_category, :passeport)) }
+    let!(:user) { create(:user, ants_pre_demande_number: "A123456789", organisations: [organisation]) }
+    let!(:rdv) { create(:rdv, motif:, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
+
+    before { travel_to(Time.zone.parse("2020-06-30")) } # le RDV est dans le passé
+
+    it "supprime l’appointment existant et n’en re-créé pas" do
+      allow(AntsApi).to receive(:status)
+        .with(hash_including(application_id: "A123456789"))
+        .and_return(
+          {
+            "status" => "validated",
+            "appointments" => [
+              {
+                "management_url" => "https://www.rdv-mairie-test.localhost/users/rdvs/#{rdv.id}",
+                "meeting_point" => "Mairie de Saumur",
+                "meeting_point_id" => lieu.id,
+                "appointment_date" => "2020-04-20 08:00:00",
+              },
+            ],
+          }
+        )
+      expect(AntsApi).to receive(:delete)
+        .with(
+          {
+            application_id: "A123456789",
+            meeting_point: "Mairie de Saumur",
+            meeting_point_id: lieu.id,
+            appointment_date: "2020-04-20 08:00:00",
+          }
+        )
+      expect(AntsApi).not_to receive(:create)
+      described_class.perform_now(application_id: "A123456789")
+    end
+  end
+
+  context "Synchro pour un application_id correspondant à un usager ayant un RDV mais qui n’est pas un RDV ANTS" do
+    let!(:organisation) { create(:organisation, verticale: :rdv_mairie) }
+    let!(:lieu) { create(:lieu, organisation:, name: "Mairie de Saumur") }
+    let!(:user) { create(:user, ants_pre_demande_number: "A123456789", organisations: [organisation]) }
+    let!(:rdv) { create(:rdv, users: [user], lieu:, organisation:, starts_at: Time.zone.parse("2020-04-20 08:00:00")) }
+
+    before { travel_to(Time.zone.parse("2020-03-10")) } # le RDV est dans le futur
+
+    it "ne créé pas d’appointment" do
+      allow(AntsApi).to receive(:status)
+        .with(hash_including(application_id: "A123456789"))
+        .and_return({ "status" => "validated", "appointments" => [] })
+      expect(AntsApi).not_to receive(:delete)
+      expect(AntsApi).not_to receive(:create)
+      described_class.perform_now(application_id: "A123456789")
+    end
+  end
 end
