d9734f0f61ae99fb69c8a5cf8967a84a6b2cb8df
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-11-04 12:24:10 +0100

    Éviter le bruit dans Sentry en cas de rate-limiting de Outlook (#4772)
    * Log executions to Sentry
    
    * Fix nasty typo in comment
    
    * Éviter le bruit dans Sentry en cas de rate-limiting de Outlook
    
    * Alerter au 3ème retry, on verra si ça fait trop de bruit

--- app/jobs/concerns/default_job_behaviour.rb
+++ app/jobs/concerns/default_job_behaviour.rb
@@ -11,7 +11,7 @@ module DefaultJobBehaviour
     # Exponential backoff is n^4, so wait times between retries will be as follows:
     # attempt:  1   2    3    4   5    6    7    8    9     10    11  12  13  14   15   16   17   18   19   20
     # backoff:  1s, 16s, 81s, 4m, 10m, 21m, 40m, 68m, 109m, 166m, 4h, 6h, 8h, 11h, 14h, 18h, 23h, 29h, 36h, 44h
-    # sum: (1..20).map { _1 ** 4 }.sum.to_f / 60 / 60 / 24 ~= 8 hours
+    # sum: (1..20).map { _1 ** 4 }.sum.to_f / 60 / 60 / 24 ~= 8 days
     # it therefore takes more than 8 days for a job to be discarded
     retry_on(StandardError, wait: :polynomially_longer, attempts: MAX_ATTEMPTS, priority: PRIORITY_OF_RETRIES)
 
@@ -44,5 +44,6 @@ module DefaultJobBehaviour
     end
 
     Sentry.set_context :job, context
+    Sentry.set_tags(executions: executions)
   end
 end
