1908f9b9d57290606e13c1752cb8592684239c3b
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-11-06 16:00:36 +0100

    Fix merge

--- spec/controllers/admin/agents_controller_spec.rb
+++ spec/controllers/admin/agents_controller_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe Admin::AgentsController, type: :controller do
         expect(response.body).to include unconfirmed_agent.email
       end
     end
-  end
 
     describe "JSON version" do
       it "returns agents as JSON" do
@@ -259,7 +258,7 @@ RSpec.describe Admin::AgentsController, type: :controller do
 
         it "displays an error about the mismatch" do
           subject
-          expect(flash[:alert]).to match(/Attention : le\(s\) service\(s\) demandé\(s\) .* ne correspondent pas/)
+          expect(flash[:alert]).to match(/Attention : le\(s\) service\(s\) demandé\(s\) .* ne correspondent pas/)
         end
       end
 
