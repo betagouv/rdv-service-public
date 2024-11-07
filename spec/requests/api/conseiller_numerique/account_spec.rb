55991f9a80d0ee60ef91b0fb7c9ac7da37b8ff34
Victor Mours <victor.mours@gmail.com>
2024-11-07 15:51:02 +0100

    fix auth specs

--- spec/requests/api/conseiller_numerique/account_spec.rb
+++ spec/requests/api/conseiller_numerique/account_spec.rb
@@ -1,9 +1,37 @@
 RSpec.describe "Création de comptes de conseillers numériques" do
+  let(:params) do
+    {
+      agent: {
+        email: "francis.factice@france-service.fr",
+        first_name: "Francis",
+        last_name: "Factice",
+        external_id: "123456",
+      },
+      organisation: {
+        external_id: "123456",
+        name: "ANCT",
+      },
+      lieux: [{
+        name: "Bureaux PIX",
+        address: "21 rue des Ardennes, Paris, 75019",
+      }],
+    }
+  end
+
+  before do
+    create(:territory, :conseillers_numeriques)
+    create(:service, :conseiller_numerique)
+    stub_request(
+      :get,
+      "https://api-adresse.data.gouv.fr/search/?postcode=75019&q=21%20rue%20des%20Ardennes,%20Paris,%2075019"
+    ).to_return(status: 200, body: file_fixture("geocode_result.json").read, headers: {})
+  end
+
   context "when the api key is configured properly" do
-    stub_env_with(COOP_MEDIATION_NUMERIQUE_API_KEY: "visioplainte-api-test-key-123456")
+    stub_env_with(COOP_MEDIATION_NUMERIQUE_API_KEY: "coop-mediation-numerique-api-test-key-123456")
     context "without the api key header" do
       before do
-        get "/api/visioplainte/creneaux"
+        post "/api/accounts", params: params
       end
 
       it "returns a 401 response" do
@@ -14,7 +42,7 @@ RSpec.describe "Création de comptes de conseillers numériques" do
 
     context "with the wrong api key" do
       before do
-        get "/api/visioplainte/creneaux", headers: { "X-COOP-MEDIATION-NUMERIQUE-API-KEY": "wrong key" }
+        post "/api/accounts", params: params, headers: { "X-COOP-MEDIATION-NUMERIQUE-API-KEY": "wrong key" }
       end
 
       it "returns a 401 response" do
@@ -24,9 +52,13 @@ RSpec.describe "Création de comptes de conseillers numériques" do
     end
 
     context "with the correct api key" do
-      it "returns a 200 response" do
-        expect(response.status).to eq 200
-        expect(response.parsed_body.keys).to eq ["creneaux"]
+      before do
+        post "/api/accounts", params: params, headers: { "X-COOP-MEDIATION-NUMERIQUE-API-KEY": "coop-mediation-numerique-api-test-key-123456" }
+      end
+
+      it "returns a 201 response" do
+        expect(response.status).to eq 201
+        expect(response.parsed_body.keys).to eq ["id"]
       end
     end
   end
@@ -36,7 +68,7 @@ RSpec.describe "Création de comptes de conseillers numériques" do
 
     it "raises an error" do
       expect do
-        get "/api/visioplainte/creneaux", headers: { "X-COOP-MEDIATION-NUMERIQUE-API-KEY": "test-api-key" }
+        post "/api/accounts", params: params, headers: { "X-COOP-MEDIATION-NUMERIQUE-API-KEY": "test-api-key" }
       end.to raise_error(KeyError)
 
       expect(response).to be_nil
