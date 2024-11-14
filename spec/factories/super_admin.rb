fad4b5dc884268d77e9df0901c3268eb044149d0
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-11-14 13:37:38 +0100

    Plus lisible

--- spec/factories/super_admin.rb
+++ spec/factories/super_admin.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :super_admin do
-    email { Faker::Internet.email(domain: "superadmin.fr") }
+    email { Faker::Internet.email(domain: "super-admin.fr") }
     role { :legacy_admin }
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
