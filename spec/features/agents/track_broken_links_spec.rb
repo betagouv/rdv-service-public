762204e6ed4975bc7b05f2ad985040fc0b184a4f
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-11-15 10:57:25 +0100

    Fix another spec that relied on factory defaults

--- spec/features/agents/track_broken_links_spec.rb
+++ spec/features/agents/track_broken_links_spec.rb
@@ -23,7 +23,7 @@ RSpec.describe "Broken links in the application are visible in Sentry" do
         expect_any_instance_of(Sentry::Client).to receive(:log_debug).with("Discarded event because before_send returned nil") # rubocop:disable RSpec/AnyInstance
         visit broken_agenda_path
         fill_in "Email", with: agent.email
-        fill_in :password, with: "Correcth0rse!"
+        fill_in :password, with: agent.password
         expect { click_on "Se connecter" }.to raise_error(ActiveRecord::RecordNotFound)
       end
     end
