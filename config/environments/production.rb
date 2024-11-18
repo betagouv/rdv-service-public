17f566cddeff839964e60bf5012bda7fe4161e89
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-11-18 08:30:34 +0100

    suppression env var RAILS_SERVE_STATIC_FILES

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,8 @@ Rails.application.configure do
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
 
-  # Disable serving static files from the `/public` folder by default since
-  # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
+  # Rails should serve static files itself, we don’t have a NGINX or Apache setup in prod
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
