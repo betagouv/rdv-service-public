c6aba326802959ad01863c0b5daf43ce0192b8f6
Victor Mours <victor.mours@gmail.com>
2024-11-26 16:00:13 +0100

    finish spec

--- spec/features/agents/visioplainte/sundays_spec.rb
+++ spec/features/agents/visioplainte/sundays_spec.rb
@@ -38,25 +38,13 @@ RSpec.describe "Visioplainte agents can work on sunday" do
     end
 
     describe "rdvs" do
-      before do
-        create(:plage_ouverture,
-               organisation: organisation,
-               agent: organisation.agents.first,
-               motifs: organisation.motifs,
-               first_day: Date.tomorrow,
-               start_time: Tod::TimeOfDay.new(14),
-               end_time: Tod::TimeOfDay.new(18),
-               recurrence: Montrose.every(:week, day: [1, 2, 3, 4, 5, 6, 0], interval: 1, starts: Date.tomorrow, on: %i[monday tuesday thursday friday saturday sunday]))
+      let!(:rdv) do
+        create(:rdv, agents: [superviseur], starts_at: Time.zone.local(2024, 12, 1, 15, 0, 0))
       end
 
-      include_context "Visioplainte Auth"
-
-      it "can be created and displayed on sunday", js: true do
-        Faraday.post "#{Capybara.app_host}/api/visioplainte/rdvs", headers: auth_header, params: { starts_at: "2024-11-30T08:00:00+02:00" }
-
-        admin_organisation_agent_agenda_path(organisation_id: organisation.id, agent_id: superviseur.id)
-        require "byebug"
-        byebug
+      it "can be displayed on sunday", js: true do
+        visit admin_organisation_agent_agenda_path(organisation, superviseur)
+        expect(page).to have_content(rdv.users.first.first_name)
       end
     end
   end
@@ -64,6 +52,7 @@ RSpec.describe "Visioplainte agents can work on sunday" do
   context "for a normal agent" do
     let(:agent) { create(:agent, basic_role_in_organisations: [organisation]) }
     let(:organisation) { create(:organisation) }
+    let!(:motif) { create(:motif, organisation: organisation, service: agent.services.first) }
 
     before { login_as(agent, scope: :agent) }
 
