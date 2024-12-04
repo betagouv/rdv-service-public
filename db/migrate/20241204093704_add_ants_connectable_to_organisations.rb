493f448f016c8b899a72658d80f676cc16da219c
Victor Mours <victor.mours@gmail.com>
2024-12-04 10:46:38 +0100

    backfill

--- db/migrate/20241204093704_add_ants_connectable_to_organisations.rb
+++ db/migrate/20241204093704_add_ants_connectable_to_organisations.rb
@@ -5,5 +5,12 @@ class AddAntsConnectableToOrganisations < ActiveRecord::Migration[7.1]
     change_column_comment :organisations, :ants_connectable, from: nil, to: <<~COMMENT
       Autorise l'organisation à être branchée sur le moteur de recherche de l'ANTS sur https://rendezvouspasseport.ants.gouv.fr/. Pour éviter de brancher n'importe qui sur ce moteur de recherche, cette option n'est pas activable par les agents.
     COMMENT
+
+    up_only do
+      mairies_territory = Territory.find_by(name: "Mairies")
+      if mairies_territory # La migration tourne aussi sur l'instance historique qui n'a pas ce territoire
+        Organisation.where(territory_id: mairies_territory.id).update_all(ants_connectable: true)
+      end
+    end
   end
 end
