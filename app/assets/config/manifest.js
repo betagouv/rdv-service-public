b32e53498a62d1e9887634677cff445d31c16d0b
Victor Mours <victor.mours@gmail.com>
2024-12-09 10:43:17 +0100

    undo stylesheet change

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,7 +2,6 @@
 //= link_tree ../builds
 
 //= link @gouvfr/dsfr/dist/dsfr.min.css
-//= link @gouvfr/dsfr/dist/dsfr.main.min.css
 //= link @gouvfr/dsfr/dist/dsfr.module.min.js
 //= link @gouvfr/dsfr/dist/dsfr.nomodule.min.js
 //= link @gouvfr/dsfr/dist/utility/icons/icons.min.css
