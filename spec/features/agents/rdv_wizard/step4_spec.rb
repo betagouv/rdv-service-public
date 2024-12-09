cf891905b2e0c90bdb503ef43272dae70c808239
Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>
2024-12-09 10:47:38 +0100

    On réduit la taille des SMS (#4846)

--- spec/features/agents/rdv_wizard/step4_spec.rb
+++ spec/features/agents/rdv_wizard/step4_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe "Step 4 of the rdv wizard" do
       visit new_admin_organisation_rdv_wizard_step_path(params)
       click_button "Créer RDV"
       expect(page).to have_content("Le rendez-vous a été créé.")
-      rdv_url = rdv_short_url(Rdv.last, host: Domain::RDV_SOLIDARITES.host_name, tkn: "12345")
+      rdv_url = rdv_short_url(Rdv.last, host: Domain::RDV_SOLIDARITES.host_name, tkn: "12345").sub(%r{https?://}, "")
       expect_sms_enqueued(content: /#{rdv_url}/)
     end
   end
