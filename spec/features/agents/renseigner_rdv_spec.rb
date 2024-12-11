59824d428cbfcaddec5ce0e5f5e0e2eb1649063d
Victor Mours <victor.mours@gmail.com>
2024-12-11 14:46:21 +0100

    Add spec for cancelled rdv

--- spec/features/agents/renseigner_rdv_spec.rb
+++ spec/features/agents/renseigner_rdv_spec.rb
@@ -25,6 +25,19 @@ RSpec.describe "Les agents peuvent renseigner le statut des rendez-vous pour nou
     expect(rdv.reload.status).to eq("unknown")
   end
 
+  context "for a cancelled rdv" do
+    it "works from the main page", js: true do
+      visit a_renseigner_admin_organisation_rdvs_path(rdv.organisation)
+
+      find(".fr-btn", text: "Autre").click
+      find("span", text: "Annulé à l’initiative du service").click
+
+      sleep 1 # Pour attendre que la requête ajax se finisse
+      expect(page).to have_content("Rendez-vous mis à jour")
+      expect(rdv.reload.status).to eq("revoked")
+    end
+  end
+
   it "works from the rdv details page", js: true do
     visit admin_organisation_rdv_path(rdv.organisation, rdv)
     find(".btn", text: "À renseigner").click
