698b2ab94f644d689a9902541fd97b79c0ba382c
Victor Mours <victor.mours@gmail.com>
2024-12-16 19:31:12 +0100

    fix

--- app/models/rdv_plan.rb
+++ app/models/rdv_plan.rb
@@ -2,6 +2,7 @@ class RdvPlan < ApplicationRecord
   belongs_to :agent
   belongs_to :motif
   belongs_to :lieu, optional: true
+  belongs_to :rdv_agent, class_name: "Agent", optional: true
 
   has_many :participations, class_name: "RdvPlanParticipation", dependent: :destroy
   has_many :users, through: :participations
