fc43b52b9aaf4a933184ddd65f69df73f5ac2af2
Victor Mours <victor.mours@gmail.com>
2024-12-17 14:50:36 +0100

    move rdv a renseigner to header

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -233,7 +233,7 @@ class Agent < ApplicationRecord
   end
 
   def update_unknown_past_rdv_count!
-    update_column(:unknown_past_rdv_count, rdvs.status(:unknown_past).count) if persisted?
+    update_column(:unknown_past_rdv_count, rdvs.status(:unknown_past).where("starts_at > ?", 2.weeks.ago).count) if persisted?
   end
 
   # This method is called when calling #current_agent on a controller action that is automatically generated
