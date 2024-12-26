2260abbd5cde32c7bd59507bb69366c226c3084f
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-12-26 15:01:54 +0100

    fix speccs

--- spec/services/off_days_spec.rb
+++ spec/services/off_days_spec.rb
@@ -41,8 +41,8 @@ RSpec.describe OffDays, type: :service do
   describe ".to_full_calendar_array" do
     it "returns the proper format for full calendar" do
       array = described_class.to_full_calendar_array
-      expect(array[0].keys).to match_array(%i[title start end backgroundColor])
-      expect(array[0][:backgroundColor]).to eq "rgba(127, 140, 141, 0.7)"
+      expect(array[0].keys).to match_array(%i[title start end backgroundColor textColor])
+      expect(array[0][:backgroundColor]).to eq "rgba(52, 57, 58, 0.7)"
     end
   end
 end
