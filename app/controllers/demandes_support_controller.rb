b3806941d3a3735b8df06497fb72b1c87dae10b6
Adrien Di Pasquale <adrien@dipasquale.fr>
2024-12-30 09:58:26 +0100

    WIP

--- app/controllers/demandes_support_controller.rb
+++ app/controllers/demandes_support_controller.rb
@@ -12,7 +12,6 @@ class DemandesSupportController < ApplicationController
 
   def demande_params
     {
-      role: params.dig(:demande_support_form, :role) || params[:role],
       raison: params.dig(:demande_support_form, :raison) || params[:raison],
       message: params.dig(:demande_support_form, :message) || params[:message],
       current_domain:,
