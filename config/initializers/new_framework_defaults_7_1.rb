200d2bb25e8dfccd863786536af63a6cac62c35b
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2024-12-31 16:32:46 +0100

    Cette config sqlite ne devrait avoir aucun impact chez nous

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -75,7 +75,7 @@ Rails.application.config.action_dispatch.default_headers = {
 # For example, it is possible to create an index for a non existing column.
 # See https://www.sqlite.org/quirks.html#double_quoted_string_literals_are_accepted for more details.
 #++
-# Rails.application.config.active_record.sqlite3_adapter_strict_strings_by_default = true
+Rails.application.config.active_record.sqlite3_adapter_strict_strings_by_default = true
 
 ###
 # Disable deprecated singular associations names.
