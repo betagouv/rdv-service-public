0d1d294b86d67a358f97d67c57e44d998e84f2d7
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-03 15:35:45 +0100

    Prevent useless db round trips

--- app/form_models/agent_creneaux_search_form.rb
+++ app/form_models/agent_creneaux_search_form.rb
@@ -7,23 +7,23 @@ class AgentCreneauxSearchForm
   validates :organisation_id, :motif, presence: true
 
   def organisation
-    Organisation.find_by(id: organisation_id)
+    Organisation.find_by(id: organisation_id) if organisation_id.present?
   end
 
   def service
-    Service.find_by(id: service_id)
+    Service.find_by(id: service_id) if service_id.present?
   end
 
   def motif
-    organisation.motifs.find_by(id: motif_id)
+    organisation.motifs.find_by(id: motif_id) if motif_id.present?
   end
 
   def users
-    organisation.users.where(id: user_ids)
+    organisation.users.where(id: user_ids) if user_ids.present?
   end
 
   def teams
-    organisation.territory.teams.where(id: team_ids)
+    organisation.territory.teams.where(id: team_ids) if team_ids.present?
   end
 
   def date_range
