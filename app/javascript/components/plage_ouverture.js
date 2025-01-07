ca92c8531271f8577031060b5e9f1ebb033715f1
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-07 12:30:32 +0100

    Add comments

--- app/javascript/components/plage_ouverture.js
+++ app/javascript/components/plage_ouverture.js
@@ -4,8 +4,11 @@ class PlageOuverture {
 
     $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("change", () => { this.toggleLieuSelectionField(); })
 
+    // On veut aussi refresh le select de lieu lorsque l'on utilise le select-all.
+    // L'événement js "input" est émis par '@stimulus-components/checkbox-select-all'.
     document.addEventListener("input", (e) => {
       if(e.target.dataset.checkboxSelectAllTarget === "checkboxAll") {
+        // setTimeout permet d'évaluer l'état des checkboxes **après** que le select-all ait fait effet
         setTimeout(this.toggleLieuSelectionField)
       }
     })
