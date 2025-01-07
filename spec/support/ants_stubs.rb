30e28c87d11bc201c48f94a477c3f7a96c94a076
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-01-07 13:32:42 +0100

    introduction nouvelle méthode stub_request_ants_status

--- spec/support/ants_stubs.rb
+++ spec/support/ants_stubs.rb
@@ -1,10 +1,13 @@
-def stub_ants_status_ok(application_id, status: "validated", appointments: [])
+def stub_request_ants_status(application_id)
   stub_request(:get, "https://int.api-coordination.rendezvouspasseport.ants.gouv.fr/api/status")
     .with(query: hash_including(application_ids: application_id))
-    .to_return(
-      status: 200,
-      body: { application_id => { status: status, appointments: appointments } }.to_json
-    )
+end
+
+def stub_ants_status_ok(application_id, status: "validated", appointments: [])
+  stub_request_ants_status(application_id).to_return(
+    status: 200,
+    body: { application_id => { status: status, appointments: appointments } }.to_json
+  )
 end
 
 def stub_ants_create(application_id)
