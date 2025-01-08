ff6328c86f714ffef96c2b6a79e180e33d9825d0
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-01-08 09:43:32 +0100

    fix specs

--- spec/features/users/user_can_manage_rdv_spec.rb
+++ spec/features/users/user_can_manage_rdv_spec.rb
@@ -34,10 +34,10 @@ RSpec.describe "User can manage their rdvs" do
       let(:starts_at) { 15.days.from_now }
 
       it "default", js: true do
-        expect(page).to have_content("Je souhaite être prévenu si un créneau se libère.")
-        check "Je souhaite être prévenu si un créneau se libère."
+        expect(page).to have_content("Je souhaite être prévenu(e) si un créneau se libère.")
+        check "Je souhaite être prévenu(e) si un créneau se libère."
         expect(page).to have_content("Vous êtes à présent sur la liste d'attente")
-        uncheck "Je souhaite être prévenu si un créneau se libère."
+        uncheck "Je souhaite être prévenu(e) si un créneau se libère."
         expect(page).to have_content("Vous n'êtes plus sur la liste d'attente")
       end
     end
