e3bf7f6d5febc45e4948fc312ed7d4e4c0a8e8f9
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-09 10:21:53 +0100

    Mieux ?

--- app/controllers/admin/api/agenda/plage_ouvertures_controller.rb
+++ app/controllers/admin/api/agenda/plage_ouvertures_controller.rb
@@ -3,9 +3,10 @@ class Admin::Api::Agenda::PlageOuverturesController < Admin::Api::BaseController
     @agent = Agent.find(params[:agent_id])
     @organisation = Organisation.find(params[:organisation_id])
 
-    if date_range_params.to_a.size > 7
-      @month_view_detected = true
-    end
+    @in_background = params[:in_background].to_b
+    # en vue mensuelle, les plages ne doivent pas être en background
+    # https://github.com/betagouv/rdv-service-public/pull/2775
+    @in_background = false if date_range_params.to_a.size > 7
 
     plage_ouvertures = policy_scope(@agent.plage_ouvertures, policy_scope_class: Agent::PlageOuverturePolicy::Scope)
       .includes(:lieu, :organisation)
