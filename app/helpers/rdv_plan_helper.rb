c67bd63e3e1178dfa16697c9e4ee67d74fc114a9
Victor Mours <victor.mours@gmail.com>
2025-01-09 18:04:15 +0100

    fix

--- app/helpers/rdv_plan_helper.rb
+++ app/helpers/rdv_plan_helper.rb
@@ -2,7 +2,7 @@ module RdvPlanHelper
   def modalites_collection
     result = []
 
-    available_location_types = current_agent.motifs.where(organisation_id: current_agent.roles.select(:organisation_id)).pluck(:location_type)
+    available_location_types = current_agent.motifs.active.where(organisation_id: current_agent.roles.select(:organisation_id)).pluck(:location_type)
 
     if available_location_types.include?("public_office")
       result += Lieu.enabled.where(organisation_id: current_agent.organisations.select(:id)).map do |l|
