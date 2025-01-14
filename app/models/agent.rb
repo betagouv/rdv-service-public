73bc3c6180607ea78edc9242f7fe7e5fc3435940
Victor Mours <victor.mours@gmail.com>
2025-01-14 12:08:01 +0100

    remove ambiguous association

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -68,7 +68,6 @@ class Agent < ApplicationRecord
   has_many :sector_attributions, dependent: :destroy
   has_many :agent_teams, dependent: :destroy
   has_many :referent_assignations, dependent: :destroy
-  has_many :rdv_plans, dependent: :destroy, inverse_of: :planning_agent
 
   accepts_nested_attributes_for :roles, :agent_territorial_access_rights
 
