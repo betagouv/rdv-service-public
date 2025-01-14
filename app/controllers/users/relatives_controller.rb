28ade674e79abc8eaf88871e5856b8c75b074ebc
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-01-14 16:28:40 +0100

    fix specs

--- app/controllers/users/relatives_controller.rb
+++ app/controllers/users/relatives_controller.rb
@@ -6,7 +6,7 @@ class Users::RelativesController < UserAuthController
   def new
     user = current_user.relatives.new
     authorize(user, policy_class: User::UserPolicy)
-    @form = RelativeUserForm.new(user:, ants_pre_demande_number_required: params[:ants_pre_demande_number_required])
+    @form = RelativeUserForm.new(user:, ants_pre_demande_number_required: params[:ants_pre_demande_number_required].to_b)
     respond_modal_with @form
   end
 
@@ -19,7 +19,7 @@ class Users::RelativesController < UserAuthController
     authorize(user, policy_class: User::UserPolicy)
     return_location = request.referer
     @form = RelativeUserForm.new(user:)
-    if @form.submit(**form_params.to_h.symbolize_keys)
+    if @form.submit(**form_params)
       flash[:success] = "#{@form.user.full_name} a été ajouté comme proche."
       return_location = add_query_string_params_to_url(request.referer, created_user_id: @form.user.id)
     end
@@ -29,14 +29,14 @@ class Users::RelativesController < UserAuthController
   def edit
     user = User.find(params.require(:id))
     authorize(user, policy_class: User::UserPolicy)
-    @form = RelativeUserForm.new(user:, ants_pre_demande_number_required: params[:ants_pre_demande_number_required])
+    @form = RelativeUserForm.new(user:, ants_pre_demande_number_required: params[:ants_pre_demande_number_required].to_b)
   end
 
   def update
     user = User.find(params.require(:id))
     authorize(user, policy_class: User::UserPolicy)
     @form = RelativeUserForm.new(user:)
-    if @form.submit(**form_params.to_h.symbolize_keys)
+    if @form.submit(**form_params)
       flash[:success] = "Les informations de votre proche #{@form.user.full_name} ont été mises à jour."
       redirect_to users_informations_path
     else
@@ -54,6 +54,10 @@ class Users::RelativesController < UserAuthController
   private
 
   def form_params
-    params.require(:relative_user_form).permit(:first_name, :last_name, :birth_date, :ants_pre_demande_number, :ants_pre_demande_number_required)
+    p = params.require(:relative_user_form)
+      .permit(:first_name, :last_name, :birth_date, :ants_pre_demande_number, :ants_pre_demande_number_required)
+      .to_h.symbolize_keys
+    p[:ants_pre_demande_number_required] = p[:ants_pre_demande_number_required].to_b
+    p
   end
 end
