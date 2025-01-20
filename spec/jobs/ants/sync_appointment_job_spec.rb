cf04ce718d569bfc0a50ebdd870aef5945fac824
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-20 12:51:30 +0100

    Débloquer les jobs de synchro ANTS dont le num de dossier finit par un espace (#4988)
    * Supprime le code de transition
    
    * Débloque les jobs actuellement en retry
    
    * Répare la recherche de RDV dans le job

--- spec/jobs/ants/sync_appointment_job_spec.rb
+++ spec/jobs/ants/sync_appointment_job_spec.rb
@@ -1,22 +1,4 @@
 RSpec.describe Ants::SyncAppointmentJob do
-  context "job mis en attente avec des arguments dépréciés" do
-    let!(:user1) { create(:user, ants_pre_demande_number: "AABBCCDDEE") }
-    let!(:user2) { create(:user, ants_pre_demande_number: nil) }
-    let!(:user3) { create(:user, ants_pre_demande_number: "1020304050") }
-
-    it "met en attente de nouveaux jobs avec les nouveaux arguments" do
-      expect(described_class).to receive(:perform_later).with(ants_pre_demande_number: "AABBCCDDEE")
-      expect(described_class).not_to receive(:perform_later).with(ants_pre_demande_number: nil)
-      expect(described_class).to receive(:perform_later).with(ants_pre_demande_number: "1020304050")
-      expect(described_class).to receive(:perform_later).with(ants_pre_demande_number: "FOOBARFOO1")
-
-      described_class.perform_now(
-        rdv_attributes: { id: 10, status: "canceled", users_ids: [user1.id, user2.id, user3.id], obsolete_ants_pre_demande_number: "FOOBARFOO1" },
-        appointment_data: { meeting_point: "gare du nord" }
-      )
-    end
-  end
-
   context "Nouveau RDV ANTS" do
     let!(:organisation) { create(:organisation, verticale: :rdv_mairie) }
     let!(:lieu) { create(:lieu, organisation:, name: "Mairie de Saumur") }
