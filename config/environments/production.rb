2ffe8be631917a351610516604f849a8c1221783
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-01-20 12:38:24 +0100

    disable assets precompile in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # Assets are already compiled by default by the nodejs-buildpack
+  config.assets.enabled = false
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
