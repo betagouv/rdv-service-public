9f3606fbf28f9459ddc4fb27f4780cc283f4f7c2
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-01-20 12:05:18 +0100

    Fix - Synchro CRM - ID Territoire ou Organisation invalide

--- app/jobs/cron_job/synchronize_crm.rb
+++ app/jobs/cron_job/synchronize_crm.rb
@@ -20,28 +20,36 @@ class CronJob::SynchronizeCrm < ApplicationJob
 
     client.database_query(database_id: NOTION_DATABASE_ID, filter:) do |page|
       page.results.each do |notion_page|
-        rdv_count = Rdv.where(organisation: organisations_ids(notion_page.properties["COMPTE PROD"].url)).count
-        client.update_page(page_id: notion_page.id, properties: { "NOMBRE DE RDV" => rdv_count })
+        ids = organisations_ids(notion_page.properties["COMPTE PROD"].url)
+        if ids.blank?
+          client.update_page(page_id: notion_page.id, properties: { "NOMBRE DE RDV" => nil })
+        else
+          rdv_count = Rdv.where(organisation: ids).count
+          client.update_page(page_id: notion_page.id, properties: { "NOMBRE DE RDV" => rdv_count })
+        end
       end
     end
   end
 
   private
 
+  # Prends une URL de compte au format '/organisations/1' ou '/territories/1' et retourne les IDs des organisations correspondantes
+  # On ne retourne les IDs que si les organisations existent dans la base de données
   def organisations_ids(account_url)
     if account_url.match('territories/(\d+)')
       territory_id = account_url.match('territories/(\d+)')[1]
-      territory = Territory.find(territory_id)
+      territory = Territory.find_by(id: territory_id)
       Organisation.where(territory: territory).pluck(:id)
     elsif account_url.match('organisations/(\d+)')
-      [account_url.match('organisations/(\d+)')[1]]
+      Organisation.where(id: account_url.match('organisations/(\d+)')[1]).pluck(:id)
     else
       Sentry.capture_message("Unrecognized account URL: #{account_url}", fingerprint: ["CronJob::SynchronizeCrm"])
+      []
     end
   end
 
   def instance_hostname
-    ENV["HOST"].sub("https://", "")
+    ENV["HOST"]
   end
 
   def client
