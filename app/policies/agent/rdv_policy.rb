741d96d3d61c4f00820165bfc0b61db23f031ec9
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-22 17:54:12 +0100

    Corrige une offense Rubocop

--- app/policies/agent/rdv_policy.rb
+++ app/policies/agent/rdv_policy.rb
@@ -19,7 +19,6 @@ class Agent::RdvPolicy < ApplicationPolicy
     same_agent_or_has_access? && users_authorized?
   end
 
-
   # Pour le moment nous n'avons qu'un seul niveau d'accès à un RDV,
   # qui permet à la fois de l'afficher et de le modifier
   alias show? update?
