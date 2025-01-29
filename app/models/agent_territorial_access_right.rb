83a6f2bb2f9cc406994032bff3722d3486f7eb2e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-01-29 15:40:11 +0100

    Mise en place d'une scope de tous les territoires que je peux gérer

--- app/models/agent_territorial_access_right.rb
+++ app/models/agent_territorial_access_right.rb
@@ -5,4 +5,10 @@ class AgentTerritorialAccessRight < ApplicationRecord
   # Relations
   belongs_to :agent
   belongs_to :territory
+
+  scope :allowing_anything, lambda {
+    where("agent_territorial_access_rights.allow_to_manage_teams         IS TRUE OR
+           agent_territorial_access_rights.allow_to_manage_access_rights IS TRUE OR
+           agent_territorial_access_rights.allow_to_invite_agents        IS TRUE")
+  }
 end
