0e3bccb4ac1b5475b98d46c086401b6126c90091
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-02-03 17:48:15 +0100

    correction des specs

--- spec/features/anybody/anybody_can_see_stats_spec.rb
+++ spec/features/anybody/anybody_can_see_stats_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe "Anybody can see stats" do
     click_link "Statistiques"
     expect(page).to have_current_path(stats_path)
     expect(page).to have_content("Statistiques")
+    # cette page contient l’iframe metabase
+    expect(page).to have_css("iframe")
   end
 
-  it "displays the number of agents with public plages or RDV collectif", js: true do
+  it "displays the number of agents with public plages or RDV collectif on the territory scoped page" do
     visit stats_territory_path(organisation.territory)
     expect(page).to have_content("0 ont des créneaux ouverts au public")
 
-    create(:plage_ouverture, motifs: [create(:motif)]) # reservable online plage
+    create(:plage_ouverture, motifs: [create(:motif, organisation:)], organisation:) # reservable online plage
     create(:rdv, motif:, organisation:) # reservable online RDV collectif
 
     visit stats_territory_path(organisation.territory)
