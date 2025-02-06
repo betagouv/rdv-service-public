dd289146c5a8cd742cdf90ce4b1e3e9b5c4c1c65
Romain Neuville <neuvilleromain@gmail.com>
2025-02-06 17:27:07 +0100

    fix angry linter

--- spec/requests/api/v1/swagger_doc/users_request_spec.rb
+++ spec/requests/api/v1/swagger_doc/users_request_spec.rb
@@ -188,17 +188,15 @@ RSpec.describe "Users API", swagger_doc: "v1/api.json" do
 
         run_test!
 
-        it "returns error message" do
+        it {
           expect(parsed_response_body).to include(
             success: false,
             errors: {},
             error_messages: ["Vous ne pouvez pas modifier l'email d'un usager ayant validé son compte."]
           )
-        end
+        }
 
-        it "doesn't update the email" do
-          expect(user.reload.email).not_to eq(email)
-        end
+        it { expect(user.reload.email).not_to eq(email) }
       end
 
       response 422, "can't nullify email of confirmed user" do
@@ -207,17 +205,15 @@ RSpec.describe "Users API", swagger_doc: "v1/api.json" do
 
         run_test!
 
-        it "returns error message" do
+        it {
           expect(parsed_response_body).to include(
             success: false,
             errors: {},
             error_messages: ["Vous ne pouvez pas modifier l'email d'un usager ayant validé son compte."]
           )
-        end
+        }
 
-        it "doesn't update the email" do
-          expect(user.reload.email).not_to eq(email)
-        end
+        it { expect(user.reload.email).not_to eq(email) }
       end
 
       it_behaves_like "an endpoint that returns 401 - unauthorized"
