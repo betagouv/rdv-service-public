68df4a5ee5884d3d032dab5fc72d7123907e7b20
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-02-10 08:36:42 +0100

    Nouvelles stats publiques inter-instances via embed metabase (#5043)

--- spec/features/anybody/anybody_can_see_stats_spec.rb
+++ spec/features/anybody/anybody_can_see_stats_spec.rb
@@ -7,17 +7,18 @@ RSpec.describe "Anybody can see stats" do
     click_link "Statistiques"
     expect(page).to have_current_path(stats_path)
     expect(page).to have_content("Statistiques")
-    expect(page).to have_content("RDV créés")
+    # cette page contient l’iframe metabase
+    expect(page).to have_css("iframe")
   end
 
-  it "displays the number of agents with public plages or RDV collectif" do
-    visit stats_path
+  it "displays the number of agents with public plages or RDV collectif on the territory scoped page" do
+    visit stats_territory_path(territory: organisation.territory)
     expect(page).to have_content("0 ont des créneaux ouverts au public")
 
-    create(:plage_ouverture, motifs: [create(:motif)]) # reservable online plage
+    create(:plage_ouverture, motifs: [create(:motif, organisation:)], organisation:) # reservable online plage
     create(:rdv, motif:, organisation:) # reservable online RDV collectif
 
-    visit stats_path
+    visit stats_territory_path(territory: organisation.territory)
     expect(page).to have_content("2 ont des créneaux ouverts au public")
   end
 end
