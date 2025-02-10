f694b5a5f887e12a92b59fdb1c79b563c4c59cce
Victor Mours <victor.mours@gmail.com>
2025-02-10 15:03:33 +0100

    fix factory

--- spec/factories/oauth_application.rb
+++ spec/factories/oauth_application.rb
@@ -3,5 +3,6 @@ FactoryBot.define do
     name { "Démarches Simplifiées" }
     uid { "fake_app_id" }
     logo_base64 { "" }
+    redirect_uri { "http://localhost:4567/omniauth/rdvservicepublic/callback\nhttps://demo.demarches-simplifiees.fr/omniauth/rdvservicepublic/callback" }
   end
 end
