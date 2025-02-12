1f6480f97c02081a3e376f2eecc45fc6d0e3eec9
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-02-12 15:53:38 +0100

    Ajoute les logiques de formulaire pour ajouter le créneau secondaire

--- app/javascript/components/plage_ouverture.js
+++ app/javascript/components/plage_ouverture.js
@@ -1,7 +1,8 @@
 class PlageOuverture {
   constructor() {
     this.toggleLieuSelectionField(true);
-    $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("input", () => { this.toggleLieuSelectionField(); })
+    $(".plage-ouverture-form .form-check-input[name='plage_ouverture[motif_ids][]']").on("input", () => { this.toggleLieuSelectionField(); });
+    this.armSecondaryTimesToggle();
   }
 
   toggleLieuSelectionField(noTransition = false) {
@@ -13,6 +14,61 @@ class PlageOuverture {
     } else {
       $(lieuSelectionField).find(".select2-input").val(null).trigger('change');
       lieuSelectionField.collapse("hide");
+    }
+  }
+
+  armSecondaryTimesToggle() {
+    const addSecondaryTimesButton = document.querySelector(".js-plages-form-add-secondary-times-button")
+    const removeSecondaryTimesButton = document.querySelector(".js-plages-form-remove-secondary-times-button")
+    const secondaryTimesContainer = document.querySelector(".js-plages-form-secondary-times-container")
+    const secondaryTimesShownInitially = document.querySelector(".js-plages-form-secondary-times-initial-state").dataset.state === "true"
+
+    const secondaryTimesStartsAtHours = document.querySelector("#plage_ouverture_afternoon_start_time_4i")
+    const secondaryTimesStartsAtMinutes = document.querySelector("#plage_ouverture_afternoon_start_time_5i")
+    const secondaryTimesEndsAtHours = document.querySelector("#plage_ouverture_afternoon_end_time_4i")
+    const secondaryTimesEndsAtMinutes = document.querySelector("#plage_ouverture_afternoon_end_time_5i")
+
+    console.log(addSecondaryTimesButton)
+    console.log(removeSecondaryTimesButton)
+    console.log(secondaryTimesContainer)
+    console.log(secondaryTimesShownInitially)
+
+    const showSecondaryTimes = () => {
+      addSecondaryTimesButton.classList.add("hidden")
+      removeSecondaryTimesButton.classList.remove("hidden")
+      secondaryTimesContainer.classList.remove("hidden")
+
+      secondaryTimesStartsAtHours.required = true
+      secondaryTimesStartsAtMinutes.required = true
+      secondaryTimesEndsAtHours.required = true
+      secondaryTimesEndsAtMinutes.required = true
+
+      secondaryTimesStartsAtHours.value = ""
+      secondaryTimesStartsAtMinutes.value = "00"
+      secondaryTimesEndsAtHours.value = ""
+      secondaryTimesEndsAtMinutes.value = "00"
+    }
+    const hideSecondaryTimes = () => {
+      addSecondaryTimesButton.classList.remove("hidden")
+      removeSecondaryTimesButton.classList.add("hidden")
+      secondaryTimesContainer.classList.add("hidden")
+
+      secondaryTimesStartsAtHours.required = false
+      secondaryTimesStartsAtMinutes.required = false
+      secondaryTimesEndsAtHours.required = false
+      secondaryTimesEndsAtMinutes.required = false
+
+      secondaryTimesStartsAtHours.value = ""
+      secondaryTimesStartsAtMinutes.value = ""
+      secondaryTimesEndsAtHours.value = ""
+      secondaryTimesEndsAtMinutes.value = ""
+    }
+    addSecondaryTimesButton.addEventListener("click", showSecondaryTimes)
+
+    removeSecondaryTimesButton.addEventListener("click", hideSecondaryTimes)
+
+    if(secondaryTimesShownInitially) {
+      showSecondaryTimes()
     }
   }
 }
