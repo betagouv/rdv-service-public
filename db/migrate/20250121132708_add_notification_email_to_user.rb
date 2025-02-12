22591de56730bd0eed7c612a88e1836eec6b6ce8
Romain Neuville <neuvilleromain@gmail.com>
2025-02-12 15:09:24 +0100

    apply Victors suggestion

--- db/migrate/20250121132708_add_notification_email_to_user.rb
+++ db/migrate/20250121132708_add_notification_email_to_user.rb
@@ -2,7 +2,7 @@ class AddNotificationEmailToUser < ActiveRecord::Migration[7.1]
   disable_ddl_transaction!
 
   def change
-    add_column :users, :notification_email, :string
+    add_column :users, :notification_email, :string, comment: "Used for notifications only, multiple users can share the same email notification address"
     add_index :users, :notification_email,
               where: "notification_email IS NOT NULL",
               algorithm: :concurrently
