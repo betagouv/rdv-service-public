408c9b300dcc5a10b39db7b9498614c1cb7d6aba
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-02-13 17:42:12 +0100

    Renomme le paramètre "in_background" afin qu'il exprime mieux son sens

--- app/controllers/admin/api/agenda/plage_ouvertures_controller.rb
+++ app/controllers/admin/api/agenda/plage_ouvertures_controller.rb
@@ -3,9 +3,14 @@ class Admin::Api::Agenda::PlageOuverturesController < Admin::Api::BaseController
     @agent = Agent.find(params[:agent_id])
     @organisation = Organisation.find(params[:organisation_id])
 
-    if date_range_params.to_a.size.between?(28, 45)
-      @month_view_detected = true
-    end
+    # transition de renommage du paramètre "in_background" en "mixed_with_rdvs"
+    params[:mixed_with_rdvs] ||= params[:in_background]
+
+    # Lorsque les plages sont affichées parmi des RDVs, nous voulons les afficher
+    # en arrière-plan afin d'améliorer la lisibilité.
+    # Excepté sur la vue mensuelle, dans laquelle les éléments sont affichés à la suite.
+    month_view_detected = date_range_params.to_a.size.between?(28, 45)
+    @display_in_background = params[:mixed_with_rdvs] && !month_view_detected
 
     plage_ouvertures = policy_scope(@agent.plage_ouvertures, policy_scope_class: Agent::PlageOuverturePolicy::Scope)
       .includes(:lieu, :organisation)
