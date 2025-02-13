267f32f2e779b83d57f7aaece7be5122940044f9
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-02-13 17:00:41 +0100

    Utilisation d’une variable d’environnement pour la DB du CRM

--- app/jobs/cron_job/synchronize_crm.rb
+++ app/jobs/cron_job/synchronize_crm.rb
@@ -2,7 +2,7 @@ class CronJob::SynchronizeCrm < ApplicationJob
   # Base de données "Activation"
   # ID récupéré dans l’URL de la page
   # Cf: https://developers.notion.com/reference/retrieve-a-database
-  NOTION_DATABASE_ID = "81a35c7b2490464590a408bbbb78ca2e".freeze
+  NOTION_DATABASE_ID = ENV["NOTION_DATABASE_ID"].freeze
 
   def perform
     return unless ENV["NOTION_API_SECRET"]
