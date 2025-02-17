3f9dd24f38a8a79dcbe221e174729fb9f4a484f2
Victor Mours <victor.mours@gmail.com>
2025-02-17 12:18:48 +0000

    Add failing spec for creation

--- spec/features/agents/users/agent_can_create_user_spec.rb
+++ spec/features/agents/users/agent_can_create_user_spec.rb
@@ -1,5 +1,6 @@
 RSpec.describe "Agent can create user" do
-  let!(:organisation) { create(:organisation, name: "MDS des Champs") }
+  let!(:organisation) { create(:organisation, name: "MDS des Champs", territory: territory) }
+  let!(:territory) { create(:territory, enable_notes_field: true) }
   let!(:agent) { create(:agent, basic_role_in_organisations: [organisation]) }
   let!(:user) do
     create(:user, first_name: "Jean", last_name: "LEGENDE", email: "jean@legende.com", organisations: [organisation])
@@ -18,6 +19,7 @@ RSpec.describe "Agent can create user" do
   it "works" do
     fill_in :user_first_name, with: "Marco"
     fill_in :user_last_name, with: "Lebreton"
+    fill_in "Remarques", with: "souhaite participer au prochain atelier collectif"
     click_button "Créer"
     expect_page_title("Marco LEBRETON")
     expect(page).to have_no_content("Inviter")
@@ -27,6 +29,10 @@ RSpec.describe "Agent can create user" do
     click_link "Inviter"
     open_email("marco@lebreton.bzh")
     expect(current_email.subject).to eq("Vous avez été invité sur RDV Aide Numérique")
+
+    user = User.last
+    expect(user.notes).to be_blank
+    expect(user.annotation_for(organisation.territory)).to eq "souhaite participer au prochain atelier collectif"
   end
 
   context "user already exists in other organisation" do
