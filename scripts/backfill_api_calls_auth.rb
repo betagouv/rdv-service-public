23cfa42fe977430c26475fbba5dda6c7e250077d
Victor Mours <victor.mours@gmail.com>
2025-02-18 16:15:10 +0100

    fix header name

--- scripts/backfill_api_calls_auth.rb
+++ scripts/backfill_api_calls_auth.rb
@@ -7,7 +7,7 @@ ApiCall.where(authentication_type: nil).find_each do |api_call|
     api_call.authentication_type = "OAuth"
   elsif headers["HTTP_ACCESS_TOKEN"] && headers["HTTP_UID"]
     api_call.authentication_type = "DeviseTokenAuth"
-  elsif headers["X-Agent-Auth-Signature"]
+  elsif headers["HTTP_X_AGENT_AUTH_SIGNATURE"]
     api_call.authentication_type = "SharedSecret"
   end
 
