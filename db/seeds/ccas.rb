f975c3ea80ba06d50f4a5c4d6ca5427d994d0535
Victor Mours <victor.mours@gmail.com>
2025-02-19 12:16:33 +0100

    Add agent without orga or service in seed

--- db/seeds/ccas.rb
+++ db/seeds/ccas.rb
@@ -49,3 +49,18 @@ user = User.new(
 
 user.skip_confirmation!
 user.save!
+
+# Un agent pour tester l'absence d'orga et de services
+agent = Agent.new(
+  email: "bob-sans-orga@demo.rdv-solidarites.fr",
+  uid: "bob-sans-orga@demo.rdv-solidarites.fr",
+  first_name: "Bob",
+  last_name: "Sans Organisation",
+  password: "Rdvservicepublictest1!",
+  services: [],
+  invitation_accepted_at: 1.day.ago,
+  roles_attributes: [],
+  agent_territorial_access_rights_attributes: []
+)
+agent.skip_confirmation!
+agent.save!
