383fd0517e10e1af27771ff2a9361d99b5f835dd
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-02-24 10:51:36 +0100

    Bugfix: répare la recherche agent par e-mail

--- app/models/concerns/text_search.rb
+++ app/models/concerns/text_search.rb
@@ -38,8 +38,11 @@ module TextSearch
 
       term = clean_search_term(term)
 
-      if columns.map(&:name).any? { |column| column =~ /email/i } && looks_like_email(term)
-        where("\"#{table_name}\".\"email\" LIKE ? OR \"#{table_name}\".\"notification_email\" LIKE ?", "#{term}%", "#{term}%")
+      email_columns = columns.map(&:name).intersection(%w[email notification_email])
+      if email_columns.any? && looks_like_email(term)
+        email_columns.map do |email_column|
+          where("\"#{table_name}\".\"#{email_column}\" LIKE ?", "#{term}%")
+        end.reduce(:or)
       else
         full_text_search(term)
       end
