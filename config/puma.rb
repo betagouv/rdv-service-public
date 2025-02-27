2f777c391cde8397a6a65160b9678c1391468c58
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-02-27 18:00:15 +0100

    MàJ WEB_CONCURRENCY

--- config/puma.rb
+++ config/puma.rb
@@ -36,7 +36,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # NOTE: Cette variable d’env est quand même respectée même quand la ligne au dessus est commentée !
-# Elle est actuellement définie à 2 ou 3 sur les instances de production.
+# Elle n’est actuellement pas définie sur les instances de production (2024-02-27)
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
