c97a127e7e23c7192c22dac84c10e5d747c1b012
Victor Mours <victor.mours@beta.gouv.fr>
2025-03-04 11:48:29 +0100

    Permettre aux agents de se créer un compte sans organisation via Proconnect (#5102)
    * Add agent without orga or service in seed
    
    * Set agent root to its own controller
    
    * fix
    
    * fix
    
    * Add page
    
    * minor improvements
    
    * fix
    
    * wip: adding form
    
    * wip
    
    * working account creation
    
    * allow inviting agent and adding services
    
    * working account creation from proconnect
    
    * udpate spec
    
    * fix route
    
    * fix
    
    * fix
    
    * improve copy, and use sanitize over html_safe
    
    * fix
    
    * lint fix
    
    * Update copywriting
    
    * improve agent factory

---
+++ app/controllers/agents/pages_controller.rb
@@ -0,0 +1,23 @@
+class Agents::PagesController < AgentAuthController
+  layout "application"
+
+  CONTACT_TEAM_URL = "https://cal.com/forms/937585aa-48a4-4efd-a642-961fad79c9c5".freeze
+
+  def home
+    skip_authorization
+
+    accessible_organisations = policy_scope(Organisation, policy_scope_class: Agent::OrganisationPolicy::Scope)
+
+    if accessible_organisations.count == 1
+      redirect_to admin_organisation_agent_agenda_path(accessible_organisations.first, current_agent)
+    elsif accessible_organisations.count > 1
+      redirect_to admin_organisations_path
+    end
+  end
+
+  private
+
+  def pundit_user
+    AgentContext.new(current_agent)
+  end
+end
---
+++ spec/services/admin_creates_agent_spec.rb
@@ -0,0 +1,27 @@
+RSpec.describe AdminCreatesAgent do
+  context "when inviting an agent that doesn't have any services" do
+    let(:agent) do
+      create(:agent, :no_services, organisations: [])
+    end
+    let(:service1) { create(:service) }
+    let(:service2) { create(:service) }
+    let(:organisation) do
+      create(:organisation)
+    end
+    let(:admin) do
+      create(:agent, admin_role_in_organisations: [organisation])
+    end
+
+    it "adds the services to the agent" do
+      described_class.new(
+        agent_params: { email: agent.email, service_ids: [service1.id, service2.id] },
+        current_agent: admin,
+        organisations: [organisation],
+        access_level: :basic
+      ).call
+
+      expect(agent.reload.services).to contain_exactly(service1, service2)
+      expect(agent.organisations).to eq [organisation]
+    end
+  end
+end
