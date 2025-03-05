372ef5f843347bf55e034558c6d298e3499450a6
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-03-05 15:34:57 +0100

    Nettoyage des `ignored_columns` de colonnes déjà supprimées (#5146)

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,7 +1,6 @@
 class SoftDeleteError < StandardError; end
 
 class Agent < ApplicationRecord
-  self.ignored_columns += %i[inclusion_connect_open_id_sub invitations_count]
   # Mixins
   has_paper_trail(
     only: %w[email first_name last_name starts_at invitation_sent_at invitation_accepted_at]
