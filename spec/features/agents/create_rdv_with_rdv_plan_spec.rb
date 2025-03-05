628f7d481f048890ab6d4e328902211c1f89d500
Victor Mours <victor.mours@gmail.com>
2025-03-05 12:11:00 +0100

    lint fix

--- spec/features/agents/create_rdv_with_rdv_plan_spec.rb
+++ spec/features/agents/create_rdv_with_rdv_plan_spec.rb
@@ -67,7 +67,8 @@ RSpec.describe "Les agents peuvent prendre un rendez-vous en passant par l'inter
     expect(page).to have_content("Retour sur Démarches Simplifiées")
   end
 
-  context "quand l'agent n'appartient à aucune organisation parce qu'il n'a pas encore fait d'ouverture de compte avec notre équipe déploiement, ou qu'il n'a pas été invité à rejoindre son organisation par ses collègues" do
+  context "quand l'agent n'appartient à aucune organisation" do
+    # Ça arrive s'il n'a pas encore fait d'ouverture de compte avec notre équipe déploiement, ou qu'il n'a pas été invité à rejoindre son organisation par ses collègues
     let!(:agent) do
       create(:agent, basic_role_in_organisations: [])
     end
