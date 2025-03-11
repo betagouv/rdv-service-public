b4958bc9455954c8bf9803d39c30ea03d2706c15
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-03-11 10:07:34 +0100

    Revert la détection d'occurrence invalide (#5163)

--- app/models/recurrence/occurrence.rb
+++ app/models/recurrence/occurrence.rb
@@ -4,8 +4,6 @@ class Recurrence::Occurrence
   attr_reader :starts_at, :ends_at
 
   def initialize(starts_at:, ends_at:)
-    raise "Invalid times: #{starts_at} -> #{ends_at}" if starts_at >= ends_at
-
     @starts_at = starts_at
     @ends_at = ends_at
   end
