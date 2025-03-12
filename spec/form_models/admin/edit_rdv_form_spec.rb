e44a27dd35f2b1423bf8fddb55255a3b4aa313d4
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-03-12 18:53:18 +0100

    Fix: mise à jour de la liste des agents d’un RDV

--- spec/form_models/admin/edit_rdv_form_spec.rb
+++ spec/form_models/admin/edit_rdv_form_spec.rb
@@ -45,5 +45,25 @@ RSpec.describe Admin::EditRdvForm, type: :form do
       expect(rdv.cancelled_at).to be_nil
       expect(rdv.status).to eq("unknown")
     end
+
+    context "lorsque le RDV est invalide" do
+      let(:rdv) { create(:rdv, agents: [agent], organisation: organisation) }
+      let(:another_rdv) { create(:rdv, agents: [agent], organisation: organisation, starts_at: rdv.starts_at) }
+      let(:another_agent) { create(:agent) }
+
+      it "on ne met pas à jour la liste des agents si l’agent n’a pas bypass l’avertissement" do
+        edit_rdv_form = described_class.new(rdv, agent_context)
+        edit_rdv_form.submit({ agents: [agent, another_agent] })
+
+        expect(rdv.reload.agents).to eq([agent])
+      end
+
+      it "on met à jour la liste des agents si l’agent a bypass l’avertissement" do
+        edit_rdv_form = described_class.new(rdv, agent_context)
+        edit_rdv_form.submit({ agents: [agent, another_agent], ignore_benign_errors: "1" })
+
+        expect(rdv.reload.agents).to eq([agent, another_agent])
+      end
+    end
   end
 end
