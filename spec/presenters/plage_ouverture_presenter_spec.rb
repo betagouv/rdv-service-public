38daef8d34f7c5acdf8bfb8b7e89cbfe1d8b68e6
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-03-17 11:05:23 +0100

    Fix spec

--- spec/presenters/plage_ouverture_presenter_spec.rb
+++ spec/presenters/plage_ouverture_presenter_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe PlageOuverturePresenter, type: :presenter do
       end
       let(:presenter) { described_class.new(plage_ouverture, agent1_context) }
 
-      it { is_expected.to match %r{Jeanne LONGO a <a href=.*>une plage d'ouverture</a> à MDS du coin mercredi 09 décembre 2020de 07:00 à 10:00} }
+      it { is_expected.to match %r{Jeanne LONGO a <a href=.*>une plage d'ouverture</a> à MDS du coin mercredi 09 décembre 2020 de 07:00 à 10:00} }
     end
   end
 end
