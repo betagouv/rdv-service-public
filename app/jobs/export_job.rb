4f522c0558cb659cd81903ab040af19c75af6e79
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-03-20 11:32:19 +0100

    Éviter que les exports bloquent les autres jobs

--- app/jobs/export_job.rb
+++ app/jobs/export_job.rb
@@ -1,6 +1,10 @@
 class ExportJob < ApplicationJob
   queue_as :exports
 
+  # Les exports ne sont pas urgents, et ils sont longs à exécuter,
+  # donc il est raisonnable qu'ils laissent la priorité aux autres jobs.
+  queue_with_priority 10
+
   private
 
   def redis_key(export_id)
