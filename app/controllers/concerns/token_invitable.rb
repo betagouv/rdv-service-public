a46d8007f57e9660959b2dfc691a86c6161a85f5
Victor Mours <victor.mours@gmail.com>
2025-03-31 16:07:36 +0200

    remove unused return value

--- app/controllers/concerns/token_invitable.rb
+++ app/controllers/concerns/token_invitable.rb
@@ -12,7 +12,7 @@ module TokenInvitable
   private
 
   def store_invitation_in_session_and_redirect
-    return true if params[:invitation_token].blank?
+    return if params[:invitation_token].blank?
 
     invitation = Invitation.new(current_url_params)
     return redirect_with_error(t("devise.invitations.invitation_token_invalid")) unless invitation.token_valid?
