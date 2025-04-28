b28dbb1f1dc5938f2a0c6d01e32a0fff7b6447ac
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-04-28 15:20:13 +0200

    specs EditRdvForm : ajout et suppression agents
    partie refacto :
    - les last_names n’étaient pas utiles à la lecture
    - inlining des submits
    
    - ajout de specs pour les cas de suppressions d’agents

--- spec/form_models/admin/edit_rdv_form_spec.rb
+++ spec/form_models/admin/edit_rdv_form_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe Admin::EditRdvForm, type: :form do
   let(:organisation) { create(:organisation) }
-  let(:agent) { create(:agent) }
+  let(:agent) { create(:agent, basic_role_in_organisations: [organisation]) }
   let(:agent_context) { instance_double(AgentOrganisationContext, agent: agent, organisation: organisation) }
 
   before { stub_netsize_ok }
@@ -47,36 +47,86 @@ RSpec.describe Admin::EditRdvForm, type: :form do
     end
 
     context "ajout d’un agent à un RDV avec une erreur contournable" do
-      subject do
-        described_class.new(rdv2, agent_context).submit(attributes)
-        rdv2.reload.agents.map(&:full_name).sort.to_sentence
-      end
-
       let!(:organisation) { create(:organisation) }
-      let!(:agent_mayra) { create(:agent, first_name: "Mayra", last_name: "Castello", basic_role_in_organisations: [organisation]) }
-      let!(:agent_stefan) { create(:agent, first_name: "Stefan", last_name: "Hoyt", basic_role_in_organisations: [organisation]) }
+      let!(:agent_mayra) { create(:agent, first_name: "Mayra", basic_role_in_organisations: [organisation]) }
+      let!(:agent_stefan) { create(:agent, first_name: "Stefan", basic_role_in_organisations: [organisation]) }
       let(:agent_context) { instance_double(AgentOrganisationContext, agent: agent_mayra, organisation:) }
       let!(:rdv1) { create(:rdv, agents: [agent_mayra], organisation:, starts_at: Time.zone.parse("2025-04-28 10:30")) }
-      let!(:rdv2) { create(:rdv, agents: [agent_mayra], organisation:, starts_at: Time.zone.parse("2025-04-29 18:00")) }
-      let(:base_attributes) do
-        {
-          starts_at: Time.zone.parse("2025-04-28 10:30"), # le même horaire que rdv1, déclenche une erreur contournable
-          agent_ids: [agent_mayra.id, agent_stefan.id],
-        }
-      end
 
       before { travel_to Time.zone.parse("2025-04-24 10:00") }
 
       context "l’avertissement est contourné" do
-        let(:attributes) { base_attributes.merge(ignore_benign_errors: "1") }
-
-        it { is_expected.to eq("Mayra CASTELLO et Stefan HOYT") }
+        it "met à jour les agents et le rendez-vous" do
+          rdv2 = create(:rdv, agents: [agent_mayra], organisation:, starts_at: Time.zone.parse("2025-04-29 18:00"))
+          form = described_class.new(rdv2, agent_context)
+          form.submit(
+            starts_at: Time.zone.parse("2025-04-28 10:30"), # le même horaire que rdv1, déclenche une erreur contournable
+            agent_ids: [agent_mayra.id, agent_stefan.id],
+            ignore_benign_errors: "1"
+          )
+          expect(form.rdv.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
+          rdv2.reload
+          expect(rdv2.agents).to contain_exactly(agent_mayra, agent_stefan)
+        end
       end
 
       context "l’avertissement n’est pas contourné" do
-        let(:attributes) { base_attributes }
+        it "met à jour les agents et le rendez-vous en mémoire pour réafficher le formulaire, mais pas en base" do
+          rdv2 = create(:rdv, agents: [agent_mayra], organisation:, starts_at: Time.zone.parse("2025-04-29 18:00"))
+          form = described_class.new(rdv2, agent_context)
+          form.submit(
+            starts_at: Time.zone.parse("2025-04-28 10:30"), # le même horaire que rdv1, déclenche une erreur contournable
+            agent_ids: [agent_mayra.id, agent_stefan.id]
+          )
+          expect(form.rdv.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
+          expect(Agent.where(id: form.selected_agent_ids)).to contain_exactly(agent_mayra, agent_stefan)
+          rdv2.reload
+          expect(rdv2.starts_at).to eq Time.zone.parse("2025-04-29 18:00")
+          expect(rdv2.agents).to contain_exactly(agent_mayra)
+        end
+      end
+    end
+  end
 
-        it { is_expected.to eq("Mayra CASTELLO") }
+  context "suppression d’un agent d’un RDV avec une erreur contournable" do
+    let!(:organisation) { create(:organisation) }
+    let!(:agent_mayra) { create(:agent, first_name: "Mayra", basic_role_in_organisations: [organisation]) }
+    let!(:agent_stefan) { create(:agent, first_name: "Stefan", basic_role_in_organisations: [organisation]) }
+    let(:agent_context) { instance_double(AgentOrganisationContext, agent: agent_mayra, organisation:) }
+    let!(:rdv1) { create(:rdv, agents: [agent_mayra], organisation:, starts_at: Time.zone.parse("2025-04-28 10:30")) }
+
+    before { travel_to Time.zone.parse("2025-04-24 10:00") }
+
+    context "l’avertissement est contourné" do
+      it "l’agent est bien supprimé" do
+        rdv2 = create(:rdv, agents: [agent_mayra, agent_stefan], organisation:, starts_at: Time.zone.parse("2025-04-29 18:00"))
+        form = described_class.new(rdv2, agent_context)
+        form.submit(
+          starts_at: Time.zone.parse("2025-04-28 10:30"), # le même horaire que rdv1, déclenche une erreur contournable
+          agent_ids: [agent_mayra.id],
+          ignore_benign_errors: "1"
+        )
+        expect(form.rdv.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
+        expect(Agent.where(id: form.selected_agent_ids)).to contain_exactly(agent_mayra)
+        rdv2.reload
+        expect(rdv2.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
+        expect(rdv2.agents).to contain_exactly(agent_mayra)
+      end
+    end
+
+    context "l’avertissement n’est pas contourné" do
+      it "met à jour les agents et le rendez-vous en mémoire pour réafficher le formulaire, mais pas en base" do
+        rdv2 = create(:rdv, agents: [agent_mayra, agent_stefan], organisation:, starts_at: Time.zone.parse("2025-04-29 18:00"))
+        form = described_class.new(rdv2, agent_context)
+        form.submit(
+          starts_at: Time.zone.parse("2025-04-28 10:30"), # le même horaire que rdv1, déclenche une erreur contournable
+          agent_ids: [agent_mayra.id]
+        )
+        expect(form.rdv.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
+        expect(Agent.where(id: form.selected_agent_ids)).to contain_exactly(agent_mayra)
+        rdv2.reload
+        expect(rdv2.starts_at).to eq Time.zone.parse("2025-04-29 18:00")
+        expect(rdv2.agents).to contain_exactly(agent_mayra, agent_stefan)
       end
     end
   end
