de02dbbd75e8d86e7ae982dde93387ab283cdabb
Victor Mours <victor.mours@gmail.com>
2025-04-28 17:07:22 +0200

    wip

--- app/controllers/super_admins/territory_creation_requests_controller.rb
+++ app/controllers/super_admins/territory_creation_requests_controller.rb
@@ -1,11 +1,30 @@
 module SuperAdmins
   class TerritoryCreationRequestsController < SuperAdmins::ApplicationController
+    def index
+      @territory_creation_requests = policy_scope(TerritoryCreationRequest.where(response: params[:status]), policy_scope_class: SuperAdmin::TerritoryCreationRequestPolicy::Scope)
+      authorize(@territory_creation_requests, policy_class: SuperAdmin::TerritoryCreationRequestPolicy)
+    end
+
     def edit
       super
     end
 
     def update
       ProcessTerritoryCreationRequestForm.new
+      compte_params[:agent][:invited_by] = current_super_admin
+      compte = Compte.new(compte_params, current_domain)
+      authorize_resource(compte)
+
+      if compte.save!
+        redirect_to(
+          super_admins_agent_path(compte.agent),
+          notice: "Le nouveau compte a été créé, et une invitation a été envoyée à #{compte_params.dig(:agent, :email)}"
+        )
+      else
+        render :new, locals: {
+          page: Administrate::Page::Form.new(dashboard, resource),
+        }, status: :unprocessable_entity
+      end
     end
   end
 
@@ -13,5 +32,13 @@ module SuperAdmins
 
   def permitted_params
     params.require(:process_territory_creation_request_form).permit(service_ids: [])
+  end
+
+  def compte_params
+    params.require(:compte).permit(
+      territory: %i[name category],
+      organisation: %i[name],
+      agent: %i[service_ids]
+    )
   end
 end
