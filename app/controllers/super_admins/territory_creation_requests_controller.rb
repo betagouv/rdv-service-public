dd978afb0095f154df4bc5c192e54ee6e78d787c
Victor Mours <victor.mours@gmail.com>
2025-04-28 18:03:21 +0200

    cleanup

--- app/controllers/super_admins/territory_creation_requests_controller.rb
+++ app/controllers/super_admins/territory_creation_requests_controller.rb
@@ -2,7 +2,14 @@ module SuperAdmins
   class TerritoryCreationRequestsController < SuperAdmins::ApplicationController
     def index
       @territory_creation_requests = policy_scope(TerritoryCreationRequest.where(response: params[:status]), policy_scope_class: SuperAdmin::TerritoryCreationRequestPolicy::Scope)
+
+      # le module Administrate::Punditize oblige à faire un appel à #authorize même si on est sur l'index
       authorize(@territory_creation_requests, policy_class: SuperAdmin::TerritoryCreationRequestPolicy)
+    end
+
+    def edit
+      @territory_creation_request = TerritoryCreationRequest.find(params[:id])
+      authorize(@territory_creation_request, policy_class: SuperAdmin::TerritoryCreationRequestPolicy)
     end
 
     def update
@@ -10,26 +17,12 @@ module SuperAdmins
       authorize_resource(@territory_creation_request)
 
       if @territory_creation_request.update(params.require(:territory_creation_request).permit(:response))
-        redirect_to super_admins_territory_creation_requests_path, flash: "Demande traitée"
+        redirect_to super_admins_territory_creation_requests_path, flash: { success: "Demande traitée" }
       else
         # TODO: gérer le cas d'une demande déjà traitée
         # TODO: ajouter le cas d'une demande en attente ?
         render :edit, status: :unprocessable_entity
       end
     end
-  end
-
-  private
-
-  def permitted_params
-    params.require(:process_territory_creation_request_form).permit(service_ids: [])
-  end
-
-  def compte_params
-    params.require(:compte).permit(
-      territory: %i[name category],
-      organisation: %i[name],
-      agent: %i[service_ids]
-    )
   end
 end
