a4378952e91f82b524c876e83abc7ec8355c1a1f
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-04-29 14:46:20 +0200

    force agents weak password implem

--- app/controllers/agents/sessions_controller.rb
+++ app/controllers/agents/sessions_controller.rb
@@ -16,12 +16,21 @@ class Agents::SessionsController < Devise::SessionsController
   end
 
   def create
-    super
-
+    self.resource = warden.authenticate!(auth_options)
     checker = PasswordChecker.new(params[:agent][:password]) # voir aussi app/controllers/users/sessions_controller.rb
-    if checker.too_weak?
-      flash[:notice] = nil
-      flash[:alert] = checker.error_message(current_domain.name)
+    if checker.too_weak? || true
+      reset_password_token = current_agent.send(:set_reset_password_token)
+      sign_out current_agent
+      error = <<~MSG
+        Pour des raisons de sécurité, nous vous demandons de changer votre mot de passe actuel qui présente des vulnérabilités.
+        Merci de le modifier avant d'accéder à votre espace personnel.
+      MSG
+      redirect_to(edit_agent_password_path(reset_password_token:), flash: { error: })
+    else
+      set_flash_message!(:notice, :signed_in)
+      sign_in(resource_name, resource)
+      yield resource if block_given?
+      respond_with resource, location: after_sign_in_path_for(resource)
     end
   end
 
