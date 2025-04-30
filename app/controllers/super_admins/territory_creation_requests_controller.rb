49a73a5e19709cbd86a20e2f18dc22c6096ed948
Victor Mours <victor.mours@gmail.com>
2025-04-30 16:39:56 +0200

    error message for duplicate responses

--- app/controllers/super_admins/territory_creation_requests_controller.rb
+++ app/controllers/super_admins/territory_creation_requests_controller.rb
@@ -19,8 +19,7 @@ module SuperAdmins
       if @territory_creation_request.update(params.require(:territory_creation_request).permit(:response))
         redirect_to super_admins_territory_creation_requests_path, flash: { success: "Demande traitée" }
       else
-        # TODO: gérer le cas d'une demande déjà traitée
-        # TODO: ajouter le cas d'une demande en attente ?
+        flash.now[:error] = @territory_creation_request.errors.full_messages.join(" ")
         render :edit, status: :unprocessable_entity
       end
     end
