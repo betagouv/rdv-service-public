983d33f19bc76cdbdbe20ccd1a6dd85a9173a504
Victor Mours <victor.mours@gmail.com>
2025-04-30 16:03:13 +0200

    remove uneeded route

--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,7 @@ Rails.application.routes.draw do
     resources :territories, except: %i[new create]
     resources :territory_creation_requests, only: %i[index edit update]
     resources :users
-    resources :comptes, only: %i[index new create]
+    resources :comptes, only: %i[new create]
     resources :rdvs, only: %i[show]
     root to: "agents#index"
 
