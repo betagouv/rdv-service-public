e22eb16563ea9b0cd52a0ba73d380b9c09a70604
Victor Mours <victor.mours@beta.gouv.fr>
2025-05-06 16:41:33 +0200

    Update app/models/participation.rb
    Co-authored-by: Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>

--- app/models/participation.rb
+++ app/models/participation.rb
@@ -96,8 +96,7 @@ class Participation < ApplicationRecord
   end
 
   def set_restricted_authentication_token
-    self.restricted_auth_token = SecureRandom.send(:choose, [*"A".."Z", *"0".."9"], 8)
-    self.restricted_auth_token = SecureRandom.send(:choose, [*"A".."Z", *"0".."9"], 8) while Participation.where(restricted_auth_token:).any?
+    self.restricted_auth_token = SecureRandom.send(:choose, [*"A".."Z", *"0".."9"], 8) until restricted_auth_token && Participation.where(restricted_auth_token:).none?
   end
 
   def prescription?
