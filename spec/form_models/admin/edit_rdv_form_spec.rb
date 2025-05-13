40c23f2953bcb48c121ef0da7ea9fc0727ab5dff
Victor Mours <victor.mours@gmail.com>
2025-05-13 14:59:48 +0200

    wip

--- spec/form_models/admin/edit_rdv_form_spec.rb
+++ spec/form_models/admin/edit_rdv_form_spec.rb
@@ -82,7 +82,7 @@ RSpec.describe Admin::EditRdvForm, type: :form do
         it "met à jour les agents et le rendez-vous en mémoire pour réafficher le formulaire, mais pas en base" do
           submit_form
           expect(rdv2.starts_at).to eq Time.zone.parse("2025-04-28 10:30")
-          expect(rdv2.agents.map(&:full_name).sort.to_sentence).to eq("Mayra CASTELLO et Stefan HOYT")
+          expect(rdv2.agents_rdvs.map(&:agent).map(&:full_name).sort.to_sentence).to eq("Mayra CASTELLO et Stefan HOYT")
 
           expect(rdv2.reload.starts_at).to eq Time.zone.parse("2025-04-29 18:00")
           expect(rdv2.reload.agents.map(&:full_name).sort.to_sentence).to eq("Mayra CASTELLO")
