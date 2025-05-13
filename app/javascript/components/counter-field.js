c744e4a88eb820bd3a1507562201f58452945ec0
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-05-13 16:35:42 +0200

    improve JS after victor review

--- app/javascript/components/counter-field.js
+++ app/javascript/components/counter-field.js
@@ -1,38 +1,48 @@
 export default function CounterField() {
-  const inputElt = document.querySelector(`[data-counter-field-target="input"]`)
+  document
+    .querySelectorAll('[data-controller="counter-field"]')
+    .forEach(controllerElt => {
+      const inputElt = controllerElt.querySelector(`[data-counter-field-target="input"]`)
+      const decrementElts = controllerElt.querySelectorAll('[data-action="click->counter-field#decrement"]')
+      const incrementElts = controllerElt.querySelectorAll('[data-action="click->counter-field#increment"]')
 
-  if (!inputElt) return
+      if (!inputElt) return
 
-  const maxValue = inputElt.dataset.max || 1000
-  const minValue = inputElt.dataset.min || 0
+      const maxValue = inputElt.dataset.max || 1000
+      const minValue = inputElt.dataset.min || 0
 
-  const updateDisabled = () => {
-    document.querySelectorAll('[data-action="click->counter-field#decrement"]').forEach((elt) => {
-      elt.disabled = parseInt(inputElt.value) <= minValue
-    })
+      const updateDisabled = () => {
+        decrementElts.forEach((elt) => {
+          elt.disabled = parseInt(inputElt.value) <= minValue
+        })
 
-    document.querySelectorAll('[data-action="click->counter-field#increment"]').forEach((elt) => {
-      elt.disabled = parseInt(inputElt.value) >= maxValue
-    })
-  }
-  updateDisabled()
-
-  document.querySelectorAll('[data-action="click->counter-field#decrement"]').forEach((elt) => {
-    elt.addEventListener('click', (event) => {
-      event.preventDefault()
-      if (parseInt(inputElt.value) > minValue) {
-        inputElt.value = parseInt(inputElt.value) - 1
-      updateDisabled()
+        incrementElts.forEach((elt) => {
+          elt.disabled = parseInt(inputElt.value) >= maxValue
+        })
       }
-    })
-  })
-
-  document.querySelectorAll('[data-action="click->counter-field#increment"]').forEach((elt) => {
-    elt.addEventListener('click', (event) => {
-      event.preventDefault()
-      if (parseInt(inputElt.value) < maxValue)
-        inputElt.value = parseInt(inputElt.value) + 1
       updateDisabled()
-    })
+
+      decrementElts.forEach((elt) => {
+        elt.addEventListener('click', (event) => {
+          event.preventDefault()
+          if (parseInt(inputElt.value) > minValue)
+            inputElt.value = parseInt(inputElt.value) - 1
+          updateDisabled()
+        })
+      })
+
+      incrementElts.forEach((elt) => {
+        elt.addEventListener('click', (event) => {
+          event.preventDefault()
+          if (parseInt(inputElt.value) < maxValue)
+            inputElt.value = parseInt(inputElt.value) + 1
+          updateDisabled()
+        })
+      })
+
+      inputElt.addEventListener("change", (event) => {
+        event.preventDefault()
+        updateDisabled()
+      })
   })
 }
