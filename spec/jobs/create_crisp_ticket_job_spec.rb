44ea28d0dee7aea8f7df2ab87f1855021d23e547
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-05-15 11:51:37 +0200

    Validation de l’email à la création d’un ticket Crisp

---
+++ spec/jobs/create_crisp_ticket_job_spec.rb
@@ -0,0 +1,53 @@
+# rubocop:disable RSpec/AnyInstance, RSpec/StubbedMock
+RSpec.describe CreateCrispTicketJob do
+  describe "#perform" do
+    let(:client) { instance_double(Crisp::Client) }
+    let(:website) { instance_double(Crisp::WebsiteResource) }
+
+    it "crée un ticket" do
+      allow_any_instance_of(described_class).to receive(:client).and_return(client)
+      allow(client).to receive(:website).and_return(website)
+      expect(website).to receive(:create_new_conversation).with(ENV.fetch("CRISP_WEBSITE_ID")).and_return({ "session_id" => "12345" })
+      expect(website).to receive(:send_message_in_conversation).with(
+        ENV.fetch("CRISP_WEBSITE_ID"),
+        "12345",
+        {
+          "type" => "text",
+          "from" => "user",
+          "origin" => ENV.fetch("CRISP_URN"),
+          "content" => "message\n\n---\nMessage envoyé depuis le formulaire de contact\n",
+        }
+      )
+      expect(website).to receive(:update_conversation_metas).with(
+        ENV.fetch("CRISP_WEBSITE_ID"),
+        "12345",
+        {
+          nickname: "nickname",
+          email: "plop@unusager.fr",
+          phone: "061234567",
+          segments: %w[role domain],
+          subject: "subject",
+          device: {
+            locales: ["fr"],
+          },
+        }
+      )
+      described_class.perform_now(email: "plop@unusager.fr", nickname: "nickname", phone: "061234567", subject: "subject", message: "message", role: "role", domain: "domain")
+    end
+
+    context "quand l’email n’a pas un format correct" do
+      it "ne crée pas de ticket" do
+        expect_any_instance_of(described_class).not_to receive(:client)
+        described_class.perform_now(email: "invalid_email", nickname: "nickname", phone: "061234567", subject: "subject", message: "message", role: "role", domain: "domain")
+      end
+    end
+
+    context "quand l’email est de type @example.com" do
+      it "ne crée pas de ticket" do
+        expect_any_instance_of(described_class).not_to receive(:client)
+        described_class.perform_now(email: "toto@example.com", nickname: "nickname", phone: "061234567", subject: "subject", message: "message", role: "role", domain: "domain")
+      end
+    end
+  end
+end
+# rubocop:enable RSpec/AnyInstance, RSpec/StubbedMock
