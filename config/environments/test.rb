dda44360c4095883fbe52a7d433ca8c145318a23
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-05-15 15:14:38 +0200

    Mise en place de throttling par IP sur le formulaire de contact

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -79,8 +79,6 @@ Rails.application.configure do
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
 
-  config.x.rack_attack.limit = 2
-
   config.active_record.encryption.primary_key = "test"
   config.active_record.encryption.deterministic_key = "test"
   config.active_record.encryption.key_derivation_salt = "test"
--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,20 +1,7 @@
 class Rack::Attack
-  throttle("requests by ip", limit: Rails.configuration.x.rack_attack.limit, period: 60) do |request|
-    public_api_controllers = %w[public_link]
-    request.ip if request.path.match("api/v1/(#{public_api_controllers.join('|')})|public_api/public_link")
-  end
-
-  self.throttled_responder = lambda do |request|
-    match_data = request.env["rack.attack.match_data"]
-    now = match_data[:epoch_time]
-
-    headers = {
-      "Content-Type" => "application/json",
-      "RateLimit-Limit" => match_data[:limit].to_s,
-      "RateLimit-Remaining" => "0",
-      "Retry-After" => (match_data[:period] - (now % match_data[:period])).to_s,
-    }
-
-    [429, headers, [{ errors: ["Limite d'appels API atteinte. Merci de patienter.\n"] }.to_json]]
+  throttle("formulaire de demande de support - throttling par IP", limit: Rails.env.test? ? 2 : 10, period: 60) do |request|
+    if request.path.match(%r{aide/demande_support}) && request.post?
+      request.ip
+    end
   end
 end
