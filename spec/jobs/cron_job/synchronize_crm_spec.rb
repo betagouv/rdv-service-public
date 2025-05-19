a4f3e4b490f2694d4fc2a880cfe2796a63ecd8ed
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-05-19 16:04:27 +0200

    On ajoute la date de creation du dernier RDV dans le CRM

--- spec/jobs/cron_job/synchronize_crm_spec.rb
+++ spec/jobs/cron_job/synchronize_crm_spec.rb
@@ -5,8 +5,9 @@ RSpec.describe CronJob::SynchronizeCrm, type: :job do
   let(:territory) { create(:territory) }
   let(:organisation_a) { create(:organisation, territory: territory) }
   let(:organisation_b) { create(:organisation, territory: territory) }
+  let(:organisation_c) { create(:organisation, territory: territory) }
   let!(:rdv_a) { create(:rdv, organisation: organisation_a) }
-  let!(:rdv_b) { create(:rdv, organisation: organisation_b) }
+  let!(:rdv_b) { create(:rdv, organisation: organisation_b, created_at: Date.yesterday) }
 
   before do
     allow(Notion::Client).to receive(:new).and_return(notion_client)
@@ -34,30 +35,40 @@ RSpec.describe CronJob::SynchronizeCrm, type: :job do
     context "quand la variable COMPTE PROD de la page Notion est un territory" do
       let(:compte_prod_url) { "https://demo.rdv-solidarites.fr/territories/#{territory.id}" }
 
-      it "le nombre de RDV est mis à jour dans la page Notion avec la somme des RDV de l'espace" do
+      it "le nombre de RDV et la date du dernier RDV sont mis à jour dans la page Notion avec la somme des RDV de l'espace" do
         described_class.new.perform
 
-        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => 2 })
+        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => 2, "DERNIER RDV" => { start: rdv_a.created_at.strftime("%Y-%m-%d") } })
       end
     end
 
     context "quand la variable COMPTE PROD de la page Notion est une organisation" do
       let(:compte_prod_url) { "https://demo.rdv-solidarites.fr/organisations/#{organisation_a.id}" }
 
-      it "le nombre de RDV est mis à jour dans la page Notion avec la somme des RDV de l’organisation" do
+      it "le nombre de RDV et la date du dernier RDV sont mis à jour dans la page Notion avec la somme des RDV de l’organisation" do
         described_class.new.perform
 
-        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => 1 })
+        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => 1, "DERNIER RDV" => { start: rdv_a.created_at.strftime("%Y-%m-%d") } })
+      end
+
+      context "quand l’organisation n’a pas encore de RDV" do
+        let(:compte_prod_url) { "https://demo.rdv-solidarites.fr/organisations/#{organisation_c.id}" }
+
+        it "le nombre de RDV est mis à jour dans la page Notion avec 0 et la date du dernier RDV est nulle" do
+          described_class.new.perform
+
+          expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => 0, "DERNIER RDV" => nil })
+        end
       end
     end
 
     context "quand la variable COMPTE PROD de la page Notion est une organisation inconnue" do
       let(:compte_prod_url) { "https://demo.rdv-solidarites.fr/organisations/26739" }
 
-      it "retourne un compte nul" do
+      it "retourne un compte nul et une date nulle" do
         described_class.new.perform
 
-        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => nil })
+        expect(notion_client).to have_received(:update_page).with(page_id: "page_id", properties: { "NOMBRE DE RDV" => nil, "DERNIER RDV" => nil })
       end
     end
   end
