3f7977667211845c87df082041b8c24d78d7fccb
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-05-28 09:46:47 +0200

    Fix specs

--- spec/controllers/admin/api/agenda/absences_controller_spec.rb
+++ spec/controllers/admin/api/agenda/absences_controller_spec.rb
@@ -48,6 +48,7 @@ RSpec.describe Admin::Api::Agenda::AbsencesController, type: :controller do
               "title" => absence.title,
               "start" => absence.starts_at.as_json,
               "end" => absence.ends_at.as_json,
+              "resourceIds" => [absence.agent_id],
               "backgroundColor" => "rgba(52, 57, 58, 0.7)",
               "url" => "/admin/organisations/#{organisation.id}/absences/#{absence.id}/edit",
             },
