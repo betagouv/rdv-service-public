b12a12f570da292d5599ee7403427dc99513627a
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-06-02 11:23:26 +0200

    Arrêter de retry les envois de mails lorsqu’il n’y a pas d’adresse de destinataire  (#5362)

--- app/mailers/application_mailer_delivery_job.rb
+++ app/mailers/application_mailer_delivery_job.rb
@@ -1,4 +1,7 @@
 # See https://www.bigbinary.com/blog/rails-5-2-allows-mailers-to-use-custom-active-job-class
+
+class MailArgumentError < StandardError; end
+
 class ApplicationMailerDeliveryJob < ActionMailer::MailDeliveryJob
   include DefaultJobBehaviour
 
@@ -19,6 +22,22 @@ class ApplicationMailerDeliveryJob < ActionMailer::MailDeliveryJob
       Rails.logger.error("Calling retry_job")
       retry_job
     end
+  end
+
+  # to catch a specific ArgumentError by its message, we need to rescue and re-raise.
+  # using discard_on or rescue_from is too broad and does not allow re-raising
+  around_perform do |_job, block|
+    block.call
+  rescue ArgumentError => e
+    if e.message.match(/SMTP To address may not be blank/)
+      raise MailArgumentError, e.message
+    else
+      raise e
+    end
+  end
+
+  discard_on MailArgumentError do |_job, exception|
+    Sentry.capture_exception(exception)
   end
 
   # Don't log first failures to Sentry, to prevent noise
