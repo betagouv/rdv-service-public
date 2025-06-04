4d1d3ff0a8c4c1a30d602ee018278db39c8607e4
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-06-04 14:48:31 +0200

    Refactors RDV Updatable concern et RdvEditForm (#5399)
    * Refactor des specs E2E d’ajout d’un agent à un RDV
    
    pur refacto sans changements fonctionnels.
    L’ajout de prénoms et noms permet aux specs d’être plus lisibles à mon
    avis
    J’ai renommé certains contextes en français
    
    * specs EditRdvForm : ajout et suppression agents
    
    partie refacto :
    - les last_names n’étaient pas utiles à la lecture
    - inlining des submits
    
    - ajout de specs pour les cas de suppressions d’agents
    
    * failing spec Agent::RdvPolicy#update? avec un agent hors scope
    
    Cette nouvelle failing spec est un cas peu probable mais qui permet d’illustrer
    le trou dans la policy actuelle
    
    * Agent::RdvPolicy refuse les MàJ lorsqu’un agent est hors scope
    
    * Ajout spec sur notif agent lors de MàJ de RDV
    
    * failing spec : notif sur ajout agent à RDV
    
    * Corrections des notifications envoyées aux agents ajoutés à un RDV
    
    en utilisant les transactions
    
    * review refactor with victor
    
    * désactiver l’envoi de notifs aux agents
    
    * use open_email helper

--- spec/features/agents/agent_can_update_rdv_spec.rb
+++ spec/features/agents/agent_can_update_rdv_spec.rb
@@ -1,17 +1,16 @@
 RSpec.describe "Agent can update a RDV", js: true do
   let!(:organisation) { create(:organisation) }
   let(:rdv) do
-    create(:rdv, organisation: organisation, motif: motif, agents: [agent], lieu: lieu)
+    create(:rdv, organisation: organisation, motif: motif, agents: [agent_shiraz], lieu: lieu)
   end
-  let!(:service) { create(:service) }
-  let!(:agent) { create(:agent, first_name: "Alain", last_name: "Tiptop", service: service, basic_role_in_organisations: [organisation]) }
-
+  let!(:service) { create(:service, name: "Urbanisme") }
+  let!(:agent_shiraz) { create(:agent, first_name: "Shiraz", last_name: "NADIR", email: "shiraz@angouleme.fr", service:, basic_role_in_organisations: [organisation]) }
   let!(:motif) { create(:motif, service: service, organisation: organisation) }
   let!(:lieu) { create(:lieu, organisation: organisation) }
 
   before do
     stub_netsize_ok
-    login_as(agent, scope: :agent)
+    login_as(agent_shiraz, scope: :agent)
   end
 
   it "update existing RDV with single_use lieu" do
@@ -26,11 +25,15 @@ RSpec.describe "Agent can update a RDV", js: true do
     expect(page).to have_content("Café de la gare")
     expect(page).to have_content("3 Place de la Gare, Strasbourg, 67000")
     expect(page).to have_selector(".badge-info", text: /Ponctuel/)
+    perform_enqueued_jobs
+    open_email "shiraz@angouleme.fr"
+    expect(current_email).not_to be_nil
+    expect(current_email.subject).to match(/RDV .* modifié/)
   end
 
   it "update existing RDV with existing lieu" do
     lieu_ponctuel = create(:lieu, organisation: organisation, availability: :single_use)
-    rdv = create(:rdv, organisation: organisation, motif: motif, agents: [agent], lieu: lieu_ponctuel)
+    rdv = create(:rdv, organisation: organisation, motif: motif, agents: [agent_shiraz], lieu: lieu_ponctuel)
 
     visit edit_admin_organisation_rdv_path(organisation, rdv)
 
@@ -72,23 +75,37 @@ RSpec.describe "Agent can update a RDV", js: true do
     end
   end
 
-  context "adding agents" do
-    let!(:other_agent) { create(:agent, basic_role_in_organisations: [organisation], services: agent.services) }
-    let!(:other_rdv) { create(:rdv, agents: [other_agent], starts_at: rdv.starts_at) }
+  context "ajout d’un agent au RDV" do
+    let!(:agent_jungyoon) { create(:agent, first_name: "Jung Yoon", last_name: "Han", email: "jungyoon@angouleme.fr", service:, basic_role_in_organisations: [organisation]) }
 
-    context "when there is a warning" do
-      it "works" do
+    it "envoie un email à l’agent ajouté", skip: "cf PR #5399" do # rubocop:disable RSpec/Pending
+      visit edit_admin_organisation_rdv_path(organisation, rdv)
+      select("Jung Yoon HAN (Urbanisme)", from: "rdv_agent_ids")
+      click_button "Enregistrer"
+
+      expect(page).to have_content("Jung Yoon HAN (Urbanisme)")
+      expect(page).to have_content("Shiraz NADIR (Urbanisme)")
+      perform_enqueued_jobs
+      open_email "shiraz@angouleme.fr"
+      expect(current_email).not_to be_nil
+      expect(current_email.subject).to match(/RDV .* modifié/)
+      open_email "jungyoon@angouleme.fr"
+      expect(current_email).not_to be_nil
+      expect(current_email.subject).to match(/Nouveau RDV/)
+    end
+
+    context "un RDV existe à la même heure pour l’agent ajouté" do
+      before { create(:rdv, agents: [agent_jungyoon], starts_at: rdv.starts_at) }
+
+      it "affiche un avertissement, une fois contourné l’agent est bien ajouté" do
         visit edit_admin_organisation_rdv_path(organisation, rdv)
-
-        select(other_agent.full_name, from: "rdv_agent_ids")
+        select("Jung Yoon HAN (Urbanisme)", from: "rdv_agent_ids")
         click_button "Enregistrer"
         expect(page).to have_content "Ce rendez-vous en chevauche un autre"
-        expect(rdv.reload.agents).to eq [agent]
-
+        expect(rdv.reload.agents).to contain_exactly(agent_shiraz)
         click_button "Confirmer en ignorant les avertissements"
-
         expect(page).to have_content "Le rendez-vous a été modifié."
-        expect(rdv.reload.agents).to eq [agent, other_agent]
+        expect(rdv.reload.agents).to contain_exactly(agent_shiraz, agent_jungyoon)
       end
     end
   end
