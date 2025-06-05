153712aded4f5455f68abaeda2867a3375f048e2
Victor Mours <victor.mours@gmail.com>
2025-06-05 11:34:55 +0200

    sql go brrrr

--- spec/jobs/cron_job/destroy_old_oauth_objects_spec.rb
+++ spec/jobs/cron_job/destroy_old_oauth_objects_spec.rb
@@ -16,4 +16,27 @@ RSpec.describe CronJob::DestroyOldOauthObjects do
       expect(Doorkeeper::AccessToken.first).to eq token
     end
   end
+
+  context "when there is an old access token, and a more recent one for the same app and agent" do
+    let!(:old_token) do
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: agent.id,
+        application_id: oauth_application.id,
+        created_at: 1.year.ago
+      )
+    end
+
+    let!(:recent_token) do
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: agent.id,
+        application_id: oauth_application.id,
+        created_at: 1.week.ago
+      )
+    end
+
+    it "deletes the old one and keeps the recent one" do
+      described_class.new.perform
+      expect(Doorkeeper::AccessToken.all).to eq [recent_token]
+    end
+  end
 end
