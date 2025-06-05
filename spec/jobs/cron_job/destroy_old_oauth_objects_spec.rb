8ce82210e477368237d36de010d9967f321822b8
Victor Mours <victor.mours@gmail.com>
2025-06-05 11:47:09 +0200

    Add strong spec for join logic

--- spec/jobs/cron_job/destroy_old_oauth_objects_spec.rb
+++ spec/jobs/cron_job/destroy_old_oauth_objects_spec.rb
@@ -39,4 +39,36 @@ RSpec.describe CronJob::DestroyOldOauthObjects do
       expect(Doorkeeper::AccessToken.all).to eq [recent_token]
     end
   end
+
+  context "when there are other tokens, but we still want to keep the old one" do
+    let!(:token) do
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: agent.id,
+        application_id: oauth_application.id,
+        created_at: 1.year.ago
+      )
+    end
+
+    # Ces autres tokens permettent de vérifier que le JOIN est correct
+    let!(:token_for_other_agent) do
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: create(:agent).id,
+        application: oauth_application,
+        created_at: 1.week.ago
+      )
+    end
+
+    let!(:token_for_other_application) do
+      Doorkeeper::AccessToken.create!(
+        resource_owner_id: agent.id,
+        application: create(:oauth_application),
+        created_at: 1.week.ago
+      )
+    end
+
+    it "keeps it indefinitely, so that the client can keep on using the refresh token" do
+      described_class.new.perform
+      expect(Doorkeeper::AccessToken.all).to contain_exactly(token, token_for_other_agent, token_for_other_application)
+    end
+  end
 end
