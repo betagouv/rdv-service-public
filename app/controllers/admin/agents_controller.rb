4df5b0837130fa7d90336f9dcbb19a80645e1965
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-06-05 15:00:12 +0200

    Ne pas limiter le select d'agent à 20 résultats (#5408)
    * Ne pas limiter le select d'agent à 20 résultats
    
    * Faire uniquement le nécessaire en Ajax

--- app/controllers/admin/agents_controller.rb
+++ app/controllers/admin/agents_controller.rb
@@ -2,15 +2,16 @@ class Admin::AgentsController < AgentAuthController
   respond_to :html, :json
 
   def index
-    @agents = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope)
-      .includes(:services, :roles, :organisations)
-      .active
+    @agents = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope).active
 
     @agents = @agents.joins(:organisations).where(organisations: { id: current_organisation.id }) if current_organisation
-    @invited_agents_count = @agents.invitation_not_accepted.where.not(invitation_sent_at: nil).created_by_invite.count
-
     @agents = index_params[:term].present? ? @agents.search_by_text(index_params[:term]) : @agents.ordered_by_last_name
-    @agents = @agents.page(page_number)
+
+    if request.format.html?
+      @invited_agents_count = @agents.invitation_not_accepted.where.not(invitation_sent_at: nil).created_by_invite.count
+      @agents = @agents.includes(:services, :roles, :organisations)
+      @agents = @agents.page(page_number)
+    end
   end
 
   def new
