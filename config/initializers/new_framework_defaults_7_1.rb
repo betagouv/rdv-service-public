8a9670540a54a2f5e0b28527d00ffeb1f9aabb15
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-06-10 10:32:20 +0200

    Rails 7 - `commit_transaction_on_non_local_return = true`

--- config/initializers/new_framework_defaults_7_1.rb
+++ config/initializers/new_framework_defaults_7_1.rb
@@ -218,7 +218,7 @@ Rails.application.config.active_record.before_committed_on_all_records = true
 ###
 # Whether a `transaction` block is committed or rolled back when exited via `return`, `break` or `throw`.
 #++
-# Rails.application.config.active_record.commit_transaction_on_non_local_return = true
+Rails.application.config.active_record.commit_transaction_on_non_local_return = true
 
 ###
 # Controls when to generate a value for <tt>has_secure_token</tt> declarations.
