58faee2346fdfd970b8a3e208f2c1d806c2d72a2
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-06-17 11:12:52 +0200

    Fix rubocop offence

--- config/routes/legacy_planning_routes_redirects.rb
+++ config/routes/legacy_planning_routes_redirects.rb
@@ -1,7 +1,7 @@
 get "/admin/organisations/:organisation_id/agent_agendas/:agent_id", to: (redirect do |path_params, req|
   uri = URI.parse("admin/organisations/#{path_params[:organisation_id]}/planning/agenda")
   original_params = Rack::Utils.parse_nested_query(req.query_string)
-  merged_params = original_params.merge({ 'agent_id[]' => path_params[:agent_id] })
+  merged_params = original_params.merge({ "agent_id[]" => path_params[:agent_id] })
   uri.query = merged_params.to_query
   uri.to_s
 end)
