6582a05d2fc2ada1da4d94ec6fc9c23640f3548a
Victor Mours <victor.mours@gmail.com>
2025-06-18 11:17:07 +0200

    wip

--- spec/support/spec_to_doc.rb
+++ spec/support/spec_to_doc.rb
@@ -26,6 +26,9 @@ class SpecToDoc
   end
 
   def self.upload_to_surge
+    files_to_upload = `ls tmp/capybara/spec_to_doc`
+    return unless files_to_upload["index.html"]
+
     branch_name = `git rev-parse --abbrev-ref HEAD`.strip
     domain_name = "rdv-service-public-#{branch_name}.surge.sh"
     puts "running yarn run surge tmp/capybara/spec_to_doc #{domain_name}"
