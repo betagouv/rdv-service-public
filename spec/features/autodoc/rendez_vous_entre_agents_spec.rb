bec8d1ecaa406e41a833223018c3851f173ecd83
Victor Mours <victor.mours@gmail.com>
2025-07-07 16:45:50 +0200

    make sure ProConnect button is displayed

--- spec/features/autodoc/rendez_vous_entre_agents_spec.rb
+++ spec/features/autodoc/rendez_vous_entre_agents_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe "Prise de rendez-vous entre agents", js: true do
   let(:service) { create(:service, name: "Dinum", short_name: "Dinum") }
 
+  stub_env_with(
+    AGENT_CONNECT_BASE_URL: "https://fca.integ01.dev-agentconnect.fr/api/v2",
+    AGENT_CONNECT_RDVS_CLIENT_SECRET: "un faux secret de test",
+    AGENT_CONNECT_RDVS_CLIENT_ID: "ec41582-1d60-4f11-a63b-d8abaece16aa"
+  )
   before do
     Compte.new(
       {
