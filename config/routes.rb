8572abd1be348dfb463d340df13bfd3173606e50
Victor Mours <victor.mours@beta.gouv.fr>
2025-07-08 09:09:26 +0200

    Utilisation des secteurs d'identifications France Connect v2 (#5492)
    Add france connect sector identifier

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "franceconnect_v2/auth" => "france_connect_v2#auth"
   get "franceconnect_v2/callback" => "france_connect_v2#callback"
   get "franceconnect_v2/post_logout" => "france_connect_v2#post_logout"
+  get "franceconnect_v2/sector_identifier" => "static_pages#france_connect_sector_identifier"
 
   devise_for :super_admins # necessary for helpers like super_admin_signed_in?
   devise_scope :super_admin do
