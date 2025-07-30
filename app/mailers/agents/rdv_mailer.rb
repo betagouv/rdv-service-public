848c87666da0a4c3064d7b30b27e748f209b7a5f
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-07-30 10:01:57 +0200

    Améliorations mineures de syntaxe et d’indentation

--- app/mailers/agents/rdv_mailer.rb
+++ app/mailers/agents/rdv_mailer.rb
@@ -13,32 +13,31 @@ class Agents::RdvMailer < ApplicationMailer
 
   def rdv_created
     self.ics_payload = @rdv.payload(:create, @agent)
-    subject = if @rdv.collectif?
-                if @author.is_a?(Prescripteur)
-                  @user = @author.user
-                end
-                "Nouvelle participation au RDV collectif sur votre agenda #{domain.name} pour #{relative_date(@rdv.starts_at)}"
-              else
-                "Nouveau RDV ajouté sur votre agenda #{domain.name} pour #{relative_date(@rdv.starts_at)}"
-              end
+    subject =
+      if @rdv.collectif?
+        "Nouvelle participation au RDV collectif sur votre agenda #{domain.name} pour #{relative_date(@rdv.starts_at)}"
+      else
+        "Nouveau RDV ajouté sur votre agenda #{domain.name} pour #{relative_date(@rdv.starts_at)}"
+      end
+    @user = @author.user if @rdv.collectif? && @author.is_a?(Prescripteur)
     mail(subject: subject)
   end
 
   def rdv_cancelled(old_starts_at: nil)
     date = relative_date(old_starts_at || @rdv.starts_at)
     self.ics_payload = @rdv.payload(:destroy, @agent)
-    subject = if @rdv.collectif?
-                "Participation au RDV collectif annulée #{date}"
-              else
-                "RDV annulé #{date}"
-              end
+    subject =
+      if @rdv.collectif?
+        "Participation au RDV collectif annulée #{date}"
+      else
+        "RDV annulé #{date}"
+      end
     mail(subject: subject)
   end
 
   def rdv_updated(old_starts_at:, lieu_id:)
     @old_starts_at = old_starts_at
     @address_name = Lieu.find(lieu_id).full_name if lieu_id
-
     self.ics_payload = @rdv.payload(:update, @agent)
     subject = "RDV du #{relative_date(@old_starts_at)} modifié"
     mail(subject: subject)
