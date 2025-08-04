c2c7f955bac2a8db17d2328151321cc1d910d9aa
Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>
2025-08-04 15:26:29 +0200

    Fix LetterOpener CSP (#5540)
    Fix letter opener CSP

--- config/initializers/letter_opener.rb
+++ config/initializers/letter_opener.rb
@@ -7,4 +7,12 @@ Rails.application.config.after_initialize do
       original_adjust_link_targets(contents).gsub(".localhost/", ".localhost:3000/")
     end
   end
+
+  # Nous appliquons des règles strictes au niveau de nos CSP, notamment sur l’exécution du JS.
+  # Depuis la mise en place de ces règles Letter Opener ne fonctionne plus correctement, spécifiquement car il utilise du JS
+  # inliné. Étant donné qu’il s’agit d’un outil utilisé uniquement en développement, on désactive les CSP dans son controller
+  # pour ne pas perturber son fonctionnement.
+  LetterOpenerWeb::ApplicationController.class_eval do
+    content_security_policy false
+  end
 end
