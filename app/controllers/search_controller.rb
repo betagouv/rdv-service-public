875235aed683f18e9530d13b1e932e9ea081941c
Victor Mours <victor.mours@gmail.com>
2025-08-07 18:19:57 +0200

    remove obsolete comment and widden columns

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -32,7 +32,6 @@ class SearchController < ApplicationController
 
   # rubocop:disable Metrics/PerceivedComplexity
   def search_rdv
-    # TODO : public_link_organisation_id has to work if agent is logged in ?
     if current_agent && params[:prescripteur] == Prescripteur::INTERNE && session[:agent_prescripteur_organisation_id]
       redirect_to search_creneau_admin_organisation_prescription_path(session[:agent_prescripteur_organisation_id], agent_search_params)
     else
