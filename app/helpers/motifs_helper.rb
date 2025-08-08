f35167a84f8d4a76dcdf346f8a64c82217bb4667
Victor Mours <victor.mours@gmail.com>
2025-08-08 11:46:17 +0200

    Allow displaying motifs without services in various parts of the ui

--- app/helpers/motifs_helper.rb
+++ app/helpers/motifs_helper.rb
@@ -21,7 +21,7 @@ module MotifsHelper
   end
 
   def motif_name_and_service(motif)
-    "#{motif.name} - #{motif.service_name}"
+    [motif.name, motif.service_name].compact.join(" - ")
   end
 
   def motif_name_with_special_location_type(motif)
