ed3c603959f946eea45285522b345a1eaff2950a
Victor Mours <victor.mours@gmail.com>
2025-08-13 14:14:22 +0200

    Apply proper filter on asj- and match by phone number: but match rate is ever lower at 7%

--- app/controllers/api/justice/lieux_controller.rb
+++ app/controllers/api/justice/lieux_controller.rb
@@ -2,7 +2,7 @@ class Api::Justice::LieuxController < ActionController::Base
   def index
     # TODO: filtrer sur une colonne custom de la table territories plutôt que sur le name
     # le territory_id 3 est un territoire de test sur la prod :-/
-    possible_lieux = Lieu.where(availability: :enabled).joins(organisation: :territory).where("territories.name ilike ?", "CDAD%").where.not(organisations: { territory_id: 3 })
+    possible_lieux = Lieu.where(availability: :enabled).joins(organisation: :territory).where("territories.name ilike ?", "CDAD%").where.not(organisations: { territory_id: 3 }).uniq
 
     lieux_with_match = possible_lieux.select { |lieu| matching_ee_id_for(lieu) }
 
@@ -21,14 +21,39 @@ class Api::Justice::LieuxController < ActionController::Base
   private
 
   def matching_ee_id_for(lieu)
-    matching_line = csv_content.find do |csv_line|
+    matching_ee_id_by_lat_long(lieu) || matching_ee_id_by_phone_and_zipcode(lieu)
+  end
+
+  def matching_ee_id_by_lat_long(lieu)
+    matching_line = cdad_lieux_in_csv.find do |csv_line|
       csv_line["geo"] == "#{lieu.longitude},#{lieu.latitude}"
     end
     matching_line&.fetch("ee_id")
   end
 
+  def matching_ee_id_by_phone_and_zipcode(lieu)
+    return nil unless lieu.phone_number_formatted && lieu.address
+
+    matching_line = cdad_lieux_in_csv.find do |csv_line|
+      next unless csv_line["tel"] && csv_line["adresse"]
+
+      Phonelib.parse(csv_line["tel"]).e164 == Phonelib.parse(lieu.phone_number).e164 && csv_line[/\d{5} /] == lieu.address[/, \d{5}/]
+    end
+    matching_line&.fetch("ee_id")
+  end
+
+  def cdad_lieux_in_csv
+    @cdad_lieux_in_csv ||= csv_content.select do |csv_line|
+      csv_line["type-organisme"].start_with?("asj-")
+    end.map do |line|
+      line.slice("ee_id", "geo", "tel", "adresse")
+    end.tap do |_csv|
+      puts "calling big select"
+    end
+  end
+
   def csv_content
-    @csv_content ||= CSV.parse(raw_csv, headers: :first_row, col_sep: ";", liberal_parsing: true)
+    CSV.parse(raw_csv, headers: :first_row, col_sep: ";", liberal_parsing: true).map(&:to_h)
   end
 
   def raw_csv
