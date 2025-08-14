9060137e2add1c9270ac24d7e6cdee5be46c5eb3
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-08-14 11:05:37 +0200

    fixup! WIP: Sélectionner un autre agent dans la création d’un RDV Plan

--- app/javascript/controllers/rdv_plan_controller.js
+++ app/javascript/controllers/rdv_plan_controller.js
@@ -1,15 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  connect() {
-    console.log("CONNECT RDV PLAN CONTROLLER")
-  }
-
   updateAgent(event) {
-    console.log("update agent")
     const agentId = event.target.value
-    console.log(agentId)
-    console.log(this.data.element.getAttribute("data-rdv-plan-id"))
     fetch(`/agents/rdv_plans/${this.data.element.getAttribute("data-rdv-plan-id")}/update_agent?rdv_plan[rdv_agent_id]=${agentId}`, {
       method: "PATCH",
       headers: {
