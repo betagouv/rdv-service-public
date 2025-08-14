418b0235f4659a4580fbcf5c5f80f78bd76f981f
Victor Mours <victor.mours@gmail.com>
2025-08-14 15:34:45 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -416,13 +416,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_08_11_132916) do
     t.index ["good_job_batch_id"], name: "index_instance_exports_on_good_job_batch_id"
   end
 
-  create_table "justice_lieux_matches", force: :cascade do |t|
-    t.string "ee_id", null: false
-    t.bigint "lieu_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "lieux", force: :cascade do |t|
     t.string "name", null: false
     t.bigint "organisation_id", null: false
