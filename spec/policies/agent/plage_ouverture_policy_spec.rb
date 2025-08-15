872a0e55f066a756a8146a085c0086cf2e45a0bd
Victor Mours <victor.mours@gmail.com>
2025-08-15 14:05:43 +0200

    fix policy spec

--- spec/policies/agent/plage_ouverture_policy_spec.rb
+++ spec/policies/agent/plage_ouverture_policy_spec.rb
@@ -112,6 +112,8 @@ RSpec.describe Agent::PlageOuverturePolicy, type: :policy do
       end
 
       context "when she shares no service with the plage's agent" do
+        let(:plage_ouverture) { create(:plage_ouverture, agent: create(:agent, :with_service)) }
+
         before do
           expect(agent.services.to_set.intersection(plage_ouverture.agent.services.to_set)).to be_empty # rubocop:disable RSpec/ExpectInHook
         end
