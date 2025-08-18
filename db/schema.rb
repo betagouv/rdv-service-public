7b0606622975b85d204fa36d990f227a74d414e7
Victor Mours <victor.mours@gmail.com>
2025-08-18 15:39:27 +0200

    update db schema version number

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_08_11_132916) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_13_133437) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
