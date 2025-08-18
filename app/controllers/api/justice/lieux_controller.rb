3e30c4cdcb9338cb8d2f82392fde043f7ac17403
Victor Mours <victor.mours@gmail.com>
2025-08-18 15:29:24 +0200

    move some logic to model

--- app/controllers/api/justice/lieux_controller.rb
+++ app/controllers/api/justice/lieux_controller.rb
@@ -20,8 +20,8 @@ class Api::Justice::LieuxController < ActionController::Base # rubocop:disable R
       lieux = JusticeLieuxMatch.all.map do |match|
         {
           ee_id: match.ee_id,
-          reservation_en_ligne: match.lieu.plage_ouvertures.joins(:motifs).where(motifs: { bookable_by: :everyone }).where(plage_ouvertures: { expired_cached: false }).any?,
-          url: Rails.application.routes.url_helpers.public_link_to_org_url(organisation_id: match.lieu.organisation_id, org_slug: match.lieu.organisation.slug, host: URI.parse(request.url).host),
+          reservation_en_ligne: match.reservation_en_ligne,
+          url: match.url,
         }
       end
     end
--- app/models/justice_lieux_match.rb
+++ app/models/justice_lieux_match.rb
@@ -1,3 +1,18 @@
 class JusticeLieuxMatch < ApplicationRecord
   belongs_to :lieu
+
+  def reservation_en_ligne
+    lieu.plage_ouvertures.joins(:motifs).where(
+      motifs: { bookable_by: :everyone },
+      plage_ouvertures: { expired_cached: false }
+    ).any?
+  end
+
+  def url(request)
+    Rails.application.routes.url_helpers.public_link_to_org_url(
+      organisation_id: lieu.organisation_id,
+      org_slug: lieu.organisation.slug,
+      host: URI.parse(request.url).host
+    )
+  end
 end
