7a920dc39f8483c9f193a4889c8fb393273c155a
Victor Mours <victor.mours@gmail.com>
2025-08-27 18:10:05 +0200

    remove obsolete specs

--- spec/features/anybody/anybody_can_see_stats_spec.rb
+++ spec/features/anybody/anybody_can_see_stats_spec.rb
@@ -10,23 +10,4 @@ RSpec.describe "Anybody can see stats" do
     # cette page contient l’iframe metabase
     expect(page).to have_css("iframe")
   end
-
-  it "displays the number of agents with public plages or RDV collectif on the territory scoped page" do
-    visit stats_territory_path(territory: organisation.territory)
-    expect(page).to have_content("0 ont des créneaux ouverts au public")
-
-    create(:plage_ouverture, motifs: [create(:motif, organisation:)], organisation:) # reservable online plage
-    create(:rdv, motif:, organisation:) # reservable online RDV collectif
-
-    visit stats_territory_path(territory: organisation.territory)
-    expect(page).to have_content("2 ont des créneaux ouverts au public")
-  end
-
-  it "displays the receipts stats on the Notifications page" do
-    rdv = create(:rdv, motif:, organisation:)
-    create(:receipt, rdv:, organisation:, user: rdv.users.first)
-
-    visit stats_territory_notifications_path(territory: organisation.territory)
-    expect(page).to have_content("Notifications envoyées (1)")
-  end
 end
