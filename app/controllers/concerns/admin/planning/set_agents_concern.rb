0ee802619a4b066ec691a2713a0b2a734ecf5547
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-09-03 15:10:30 +0200

    Nouvelle navigation agenda (avec feature flag)

--- app/controllers/concerns/admin/planning/set_agents_concern.rb
+++ app/controllers/concerns/admin/planning/set_agents_concern.rb
@@ -1,6 +1,12 @@
 module Admin::Planning::SetAgentsConcern
   extend ActiveSupport::Concern
 
+  included do
+    before_action do
+      @beta_planning_layout = current_agent.feature_enabled?("new_planning")
+    end
+  end
+
   def set_agents
     scope = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope)
     agents = Agent.where(id: Array(params[:agent_id]).compact_blank)
