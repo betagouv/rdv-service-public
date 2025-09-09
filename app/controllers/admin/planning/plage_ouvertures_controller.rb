b45a1fb330376337f6da3813ee0ea097e867968a
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-09-09 17:39:19 +0200

    Bugfix : Aucune plage ni indispo affichée si je me sélectionne moi-même

--- app/controllers/admin/planning/plage_ouvertures_controller.rb
+++ app/controllers/admin/planning/plage_ouvertures_controller.rb
@@ -13,7 +13,7 @@ class Admin::Planning::PlageOuverturesController < AgentAuthController
   def index
     all_plage_ouvertures = policy_scope(current_organisation.plage_ouvertures, policy_scope_class: Agent::PlageOuverturePolicy::Scope)
       .includes(:lieu, :organisation, :motifs, :agent)
-      .where(agent_id: filter_params[:agent_id])
+      .where(agent: @agent)
       .order(updated_at: :desc)
     @plage_ouvertures = all_plage_ouvertures
       .where(expired_cached: filter_params[:current_tab] == "expired")
