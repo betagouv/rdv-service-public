eb20e21f2979f3f50ee8c71cfdd59465f2547b44
Victor Mours <victor.mours@gmail.com>
2025-09-12 14:48:58 +0200

    working copy

--- spec/features/autodoc/migration_depuis_rdv_aide_num_spec.rb
+++ spec/features/autodoc/migration_depuis_rdv_aide_num_spec.rb
@@ -15,6 +15,7 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
   let!(:collegue) do
     create(:agent, first_name: "Francis", last_name: "Factice", basic_role_in_organisations: [organisation_rdv_aide_num])
   end
+  let!(:lieu) { create(:lieu, organisation: organisation_rdv_aide_num) }
 
   let!(:users) do
     create_list(:user, 3, organisations: [organisation_rdv_aide_num])
@@ -110,6 +111,8 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
     )
 
     expect(created_organisation.agents.count).to eq 2
+    expect(created_organisation.lieux.last).to have_attributes(name: lieu.name)
+    expect(created_organisation.lieux.last.external_references.last).to have_attributes(external_id: lieu.id.to_s)
 
     login_as(agent_rdv_sp, scope: :agent)
     visit "http://www.rdv-mairie-test.localhost/admin/organisations/#{created_organisation.id}/agents"
