eafc1b9e0fe86ddd6338319fb9b1ff7ecf41bb2b
Victor Mours <victor.mours@gmail.com>
2025-09-15 08:39:40 +0200

    fix scope

--- app/policies/agent/external_reference_policy.rb
+++ app/policies/agent/external_reference_policy.rb
@@ -5,7 +5,7 @@ class Agent::ExternalReferencePolicy < ApplicationPolicy
     alias current_agent pundit_user
 
     def resolve
-      scope.where(territory_id: @pundit_user.organisations.select(:territory_id) + [nil])
+      scope.where(territory_id: @pundit_user.organisations.pluck(:territory_id) + [nil])
         .joins(:oauth_application).merge(
           OauthApplication.joins(:access_tokens).merge(current_agent.access_tokens)
         ).uniq
