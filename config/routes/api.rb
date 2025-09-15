e8683a2b639981e78d3f974a50b0328bc34d89f6
Victor Mours <victor.mours@beta.gouv.fr>
2025-09-15 13:51:02 +0200

    Ajout des motifs à la migration des conums (#5638)
    * Add motifs api
    
    * Add motifs to migration
    
    * pluralize lieux and motifs
    
    * fancy shadows in autodoc
    
    * use constant to make coupling more explicit
    
    * fix

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -17,6 +17,7 @@ namespace :api do
       resources :motifs, only: %i[index]
       resources :rdvs, only: %i[index]
     end
+    resources :motifs, only: %i[create]
     resources :rdvs, only: %i[index]
     resources :participations, only: %i[update]
     resources :rdv_plans, only: %i[create show]
