9105ab10a263e316e3368ec929ec38f5be982e1e
Victor Mours <victor.mours@gmail.com>
2025-09-17 12:39:50 +0200

    improve spec

--- spec/features/autodoc/migration_depuis_rdv_aide_num_spec.rb
+++ spec/features/autodoc/migration_depuis_rdv_aide_num_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
   let!(:recurrent_absence) { create(:absence, :weekly_on_monday, agent: agent_rdv_aide_num) }
   let!(:recurrent_absence_with_end_date) { create(:absence, :weekly_on_monday_until_next_month, agent: agent_rdv_aide_num) }
   let!(:plage_ouverture) do
-    create(:plage_ouverture, :weekly_on_monday_until_next_month, agent: agent_rdv_aide_num, organisation: organisation_rdv_aide_num, lieu: lieu)
+    create(:plage_ouverture, :weekly_on_monday_until_next_month, agent: agent_rdv_aide_num, organisation: organisation_rdv_aide_num, lieu: lieu, motifs: [motif])
   end
 
   let!(:agent_rdv_sp) do
@@ -132,8 +132,13 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
     expect(created_organisation.users.last.notification_email).to be_present
 
     expect(created_organisation.agents.count).to eq 2
-    expect(created_organisation.lieux.last).to have_attributes(name: lieu.name)
-    expect(created_organisation.lieux.last.external_references.last).to have_attributes(external_id: lieu.id.to_s)
+
+    created_lieu = created_organisation.lieux.sole
+    expect(created_lieu).to have_attributes(name: lieu.name)
+    expect(created_lieu.external_references.last).to have_attributes(external_id: lieu.id.to_s)
+
+    created_motif = created_organisation.motifs.sole
+    expect(created_motif).to have_attributes(name: motif.name)
 
     # On crée des absences qui permettent de retrouver les rendez-vous sur l'ancienne instance
     expect(collegue.absences.last.starts_at).to be_within(1.minute).of(future_rdv.starts_at)
@@ -152,6 +157,12 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
 
     new_plage_ouverture = agent_rdv_sp.plage_ouvertures.last
     expect(Montrose::Recurrence.dump(new_plage_ouverture.recurrence)).to eq Montrose::Recurrence.dump(plage_ouverture.recurrence)
+
+    # Les external_ids permettent de configurer les associations correctement
+    expect(new_plage_ouverture).to have_attributes(
+      lieu_id: created_lieu.id,
+      motif_ids: [created_motif.id]
+    )
 
     login_as(agent_rdv_sp, scope: :agent)
     visit "http://www.rdv-mairie-test.localhost/admin/organisations/#{created_organisation.id}/agents"
