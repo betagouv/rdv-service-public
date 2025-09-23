7bccd3802a5a434ce8c456e1fb5a07794e1d80a5
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-09-23 10:29:07 +0200

    WIP

--- spec/requests/admin/territories/sectors_controller_spec.rb
+++ spec/requests/admin/territories/sectors_controller_spec.rb
@@ -37,4 +37,41 @@ RSpec.describe Admin::Territories::SectorsController do
       end
     end
   end
+
+  describe "#destroy" do
+    it "works" do
+      let!(:sector) { create(:sector, territory:) }
+
+      expect do
+        delete admin_territory_sector_path(territory_id: territory.id, id: sector.id)
+      end.to change(Sector, :count).by(-1)
+    end
+
+    context "when passing an arbitrary sector ID" do
+      let!(:external_sector) { create(:sector, territory: create(:territory)) }
+
+      it "does not allow deletion" do
+        expect do
+          delete admin_territory_sector_path(territory_id: territory.id, id: external_sector.id)
+        end.not_to change(Sector, :count)
+      end
+    end
+
+    context "when passing an arbitrary sector ID using a POST request and the _method=delete param" do
+      let!(:legit_sector) { create(:sector, territory:) }
+      let!(:external_sector) { create(:sector, territory: create(:territory)) }
+
+      it "deletes the sector whose ID is in the path" do
+        params = {
+          _method: "delete",
+          sector: {
+            id: external_sector.id,
+          },
+        }
+        post admin_territory_sector_path(territory_id: territory.id, id: legit_sector.id), params: params
+        expect { legit_sector.reload }.to(raise_error(ActiveRecord::RecordNotFound))
+        expect { external_sector.reload }.not_to(raise_error(ActiveRecord::RecordNotFound))
+      end
+    end
+  end
 end
