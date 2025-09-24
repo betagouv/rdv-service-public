3f0fc1795a88bfad98459240d72d82b38c3f79c0
Victor Mours <victor.mours@gmail.com>
2025-09-24 17:56:18 +0200

    turn into autodoc

--- spec/features/autodoc/creation_espace_spec.rb
+++ spec/features/autodoc/creation_espace_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe "Ouverture d'un espace", js: true do
   specify do
+    doc = Autodoc.start_scenario("Ouverture d'un espace", self)
+
+    doc.start_section("Côté agent")
+    doc.add_text("Contexte: Je suis un agent qui n'a jamais utilisé RDV Service Public")
+
     visit "http://www.rdv-mairie-test.localhost/"
+
+    doc.add_screenshot(page, text: "Je clique sur 'Créer un espace'", wait_for: "Créer un espace")
 
     click_on "Créer un espace"
 
@@ -11,7 +18,11 @@ RSpec.describe "Ouverture d'un espace", js: true do
     click_on "Demander à ouvrir un espace"
     fill_in("Nom de votre première organisation", with: "CCAS de Montreuil")
 
-    expect(page).to have_content("Votre demande a bien été enregistrée.")
+    doc.add_screenshot(
+      page,
+      text: "L'équipe déploiement a ensuite reçu la demande de création de compte dans le super admin",
+      wait_for: "Votre demande a bien été enregistrée."
+    )
 
     expect(Agent.last).to have_attributes(email: "francis@factice.org")
   end
