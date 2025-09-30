fcbd8841d7ced7dac9a01c441c852a5d370f98ad
Victor Mours <victor.mours@gmail.com>
2025-09-30 16:04:02 +0200

    update specs

--- spec/features/territory_admins/organisations_spec.rb
+++ spec/features/territory_admins/organisations_spec.rb
@@ -22,16 +22,10 @@ RSpec.describe "Gestion des organisations depuis les paramètres d'espace" do
 
     expect(new_organisation).to have_attributes(territory:, agents: [agent])
 
-    visit admin_territory_organisations_path(territory_id: territory.id)
-    click_on "Fermer une organisation"
+    click_on "Informations de l'organisation"
+    click_on "Fermer MDS de Paris"
 
-    select "MDS de Paris", from: :organisation_id
-
-    click_on "Fermer cette organisation"
-
-    expect(page).to have_content("L'organisation a été fermée.")
-
-    expect(new_organisation.reload.agents).to be_empty
+    expect(page).to have_content "L'organisation a été fermée."
 
     click_on "MDS de Paris"
     click_on "Réouvrir cette organisation"
@@ -48,22 +42,8 @@ RSpec.describe "Gestion des organisations depuis les paramètres d'espace" do
       end
 
       it "ne propose pas de supprimer l'organisation" do
-        visit select_for_close_admin_territory_organisations_path(territory_id: territory.id)
-        expect(page).not_to have_content(organisation.name)
-      end
-    end
-
-    context "quand une race condition fait qu'on essaye quand même de supprimer l'organisation" do
-      let!(:agent_without_rdv) { create(:agent, basic_role_in_organisations: [organisation]) }
-
-      it "garde l'agent courant dans l'organisation, et supprime les agents qui peuvent l'être" do
-        visit select_for_close_admin_territory_organisations_path(territory_id: territory.id)
-        create(:rdv, agents: [other_agent], starts_at: 1.week.from_now, organisation:)
-        click_on "Fermer cette organisation"
-
-        expect(page).to have_content("L'organisation n'a pas pu être fermée parce que des agents on encore des rendez-vous à venir dans cette organisation.")
-
-        expect(organisation.reload.agents).to contain_exactly(agent, other_agent)
+        visit admin_organisation_path(organisation)
+        expect(page).not_to have_content("Fermer #{organisation.name}")
       end
     end
   end
