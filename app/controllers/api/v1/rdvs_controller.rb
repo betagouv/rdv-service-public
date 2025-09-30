62a94452e9f83f428afb64a54616bb9780e9f951
Victor Mours <victor.mours@gmail.com>
2025-09-30 17:30:25 +0200

    Fix for when status is not passed

--- app/controllers/api/v1/rdvs_controller.rb
+++ app/controllers/api/v1/rdvs_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::RdvsController < Api::V1::AgentAuthBaseController
   def index
-    rdvs = policy_scope(Rdv, policy_scope_class: Agent::RdvPolicy::Scope).where(params.permit(:organisation_id, :status))
+    rdvs = policy_scope(Rdv, policy_scope_class: Agent::RdvPolicy::Scope).where(params.permit(:organisation_id))
 
     rdvs = rdvs.starts_after(Time.zone.parse(params[:starts_after])) if params[:starts_after].present?
     rdvs = rdvs.starts_before(Time.zone.parse(params[:starts_before])) if params[:starts_before].present?
@@ -17,6 +17,10 @@ class Api::V1::RdvsController < Api::V1::AgentAuthBaseController
 
     if params[:agent_id].present?
       rdvs = rdvs.where(agents: { id: params[:agent_id] })
+    end
+
+    if params[:status].present?
+      rdvs = rdvs.where(status: params[:status])
     end
 
     render_collection(rdvs)
