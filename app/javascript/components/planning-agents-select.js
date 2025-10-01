a835ca86c4e418ccd4fabb46a182ba6fdb848a5c
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-01 13:16:27 +0200

    Just check if the class is still there

--- app/javascript/components/planning-agents-select.js
+++ app/javascript/components/planning-agents-select.js
@@ -11,7 +11,7 @@ export const planningAgentsSelect = () => {
   multiAgentEnableButton.addEventListener("click", (e) => {
     e.preventDefault();
     agentSelect.multiple = true;
-    agentSelect.classList.add("js-submit-on-change-cancel");
+    agentSelect.classList.remove("js-submit-on-change");
     initInput(agentSelect);
     multiAgentEnableButton.classList.add("hidden");
     submit.classList.remove("hidden");
--- app/javascript/components/submit-on-change.js
+++ app/javascript/components/submit-on-change.js
@@ -1,12 +1,11 @@
 class SubmitOnChange {
   constructor() {
-    document.querySelectorAll('.js-submit-on-change').forEach(input => {
+    document.querySelectorAll(".js-submit-on-change").forEach(input => {
       $(input).on('change', event => {
-        if(event.target.classList.contains("js-submit-on-change-cancel")) {
-          return;
+        // Si la classe a été retirée de l'élément, c'est pour désactiver l'auto-submit.
+        if(event.target.classList.contains("js-submit-on-change")) {
+          event.target.form.submit();
         }
-
-        event.target.form.submit();
       });
     });
   }
