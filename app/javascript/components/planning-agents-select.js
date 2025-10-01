d4c486950d9dac2a66d0325e2727b49660ebaefb
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-01 14:05:55 +0200

    T'as qu'à lire le diff si tu veux savoir

--- app/javascript/components/planning-agents-select.js
+++ app/javascript/components/planning-agents-select.js
@@ -1,11 +1,14 @@
 import { initInput } from "./select2-inputs"
 
 export const planningAgentsSelect = () => {
+  if(!document.querySelector("#planning_agents_select")) {
+    return;
+  }
   const agentSelect = document.querySelector("#agent_id");
   const multiAgentEnableButton = document.querySelector("#multi_agent_enable");
-  const submit = document.querySelector("#submit_agents");
-  if(!multiAgentEnableButton) {
-    return;
+
+  if(agentSelect.multiple) {
+    $(agentSelect).on("change", event => event.target.form.submit());
   }
 
   multiAgentEnableButton.addEventListener("click", (e) => {
@@ -14,6 +17,6 @@ export const planningAgentsSelect = () => {
     agentSelect.classList.remove("js-submit-on-change");
     initInput(agentSelect);
     multiAgentEnableButton.classList.add("hidden");
-    submit.classList.remove("hidden");
+    document.querySelector("#submit_agents").classList.remove("hidden");
   });
 }
