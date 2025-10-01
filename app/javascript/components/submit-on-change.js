a0f0c8334bac7979380878143284c10dc145cf52
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-01 15:03:56 +0200

    Revert unused stuff

--- app/javascript/components/submit-on-change.js
+++ app/javascript/components/submit-on-change.js
@@ -1,11 +1,8 @@
 class SubmitOnChange {
   constructor() {
-    document.querySelectorAll(".js-submit-on-change").forEach(input => {
-      $(input).on('change', event => {
-        // Si la classe a été retirée de l'élément, c'est pour désactiver l'auto-submit.
-        if(event.target.classList.contains("js-submit-on-change")) {
-          event.target.form.submit();
-        }
+    document.querySelectorAll('.js-submit-on-change').forEach(input => {
+      $(input).on('change', input => {
+        input.target.form.submit();
       });
     });
   }
