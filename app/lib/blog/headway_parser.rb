944f3219dbcbdbe3db31e9770f0646dfac97d978
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-05 16:54:15 +0200

    Fix rubocop offence

--- app/lib/blog/headway_parser.rb
+++ app/lib/blog/headway_parser.rb
@@ -9,7 +9,7 @@ class Blog::HeadwayParser
     post_nodes.map do |post_node|
       title_link = post_node.at_css("h2.title a")
       title = title_link&.text&.strip&.squish
-      link = "https://headwayapp.co#{title_link&.[]("href")}"
+      link = "https://headwayapp.co#{title_link&.[]('href')}"
       description = post_node.css('div[itemprop="articleBody"] p').map { |p| p.text.strip }.join(" ").squish
       published_at = Time.zone.parse(post_node.at_css("time")&.[]("datetime"))
       Blog::Post.new(title:, link:, description:, published_at:)
