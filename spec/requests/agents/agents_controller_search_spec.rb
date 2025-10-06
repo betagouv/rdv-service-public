ee3ba592f501e1ac046e8b22b7ed5f753a1b700d
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-06 17:38:43 +0200

    Fix specs

--- spec/requests/agents/agents_controller_search_spec.rb
+++ spec/requests/agents/agents_controller_search_spec.rb
@@ -11,7 +11,17 @@ RSpec.describe Agents::AgentsController, "#search" do
     # The policy is tested separately, but let's make sure it is used.
     expect(Agent::AgentPolicy::Scope).to receive(:new).and_call_original
 
-    get agents_ajax_agents_search_path(organisation_id: organisation), as: :json
-    expect(parsed_response_body[:results]).to eq([{ "id" => agent.id, "text" => agent.reverse_full_name_or_email }])
+    francis = create(:agent, first_name: "Francis", last_name: "Factice", organisations: [organisation])
+    get agents_ajax_agents_search_path(organisation_id: organisation, term: "fra"), as: :json
+    expect(parsed_response_body[:results]).to eq([{ "id" => francis.id, "text" => "FACTICE Francis" }])
+  end
+
+  context "quand un des agents n'a pas encore accepté son invitation" do
+    let!(:unconfirmed_agent) { create(:agent, :not_confirmed, admin_role_in_organisations: [organisation]) }
+
+    it "renvoie son adresse mail" do
+      get agents_ajax_agents_search_path(organisation_id: organisation), as: :json
+      expect(parsed_response_body[:results]).to include({ "id" => unconfirmed_agent.id, "text" => unconfirmed_agent.email })
+    end
   end
 end
