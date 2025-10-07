5dbf2f90bad06a2772f0093dd71ca7266b8f410f
Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>
2025-10-07 15:57:42 +0200

    Utilisation de ProConnect pour la connexion SuperAdmin 2nd try (#5667)

--- app/services/agent_connect_open_id_client/auth.rb
+++ app/services/agent_connect_open_id_client/auth.rb
@@ -1,6 +1,9 @@
-# voir # https://github.com/numerique-gouv/agentconnect-documentation/blob/main/doc_fs/implementation_technique.md
+# voir https://partenaires.proconnect.gouv.fr/docs/fournisseur-service/implementation_technique
 module AgentConnectOpenIdClient
   class Auth
+    SCOPES = "openid email given_name usual_name siret".freeze
+    ACR_FOR_2FA = %w[eidas2 eidas3 https://proconnect.gouv.fr/assurance/consistency-checked-2fa https://proconnect.gouv.fr/assurance/self-asserted-2fa].freeze
+
     def initialize(client_id:, client_secret:, login_hint: nil, force_login: false)
       @login_hint = login_hint
       @force_login = force_login
@@ -12,22 +15,33 @@ module AgentConnectOpenIdClient
 
     attr_reader :state, :nonce
 
-    def redirect_url(callback_url)
-      scopes = "openid email given_name usual_name siret"
-
+    def redirect_url(callback_url, force_2fa: false)
       query_params = {
         response_type: "code",
         client_id: @client_id,
         redirect_uri: callback_url,
-        scope: scopes,
+        scope: SCOPES,
         state: state,
         nonce: nonce,
-        acr_values: "eidas1",
         login_hint: @login_hint,
         prompt: @force_login ? "login" : nil,
+        claims: claims(force_2fa:).to_json,
       }.compact_blank
 
       "#{ENV['AGENT_CONNECT_BASE_URL']}/authorize?#{query_params.to_query}"
+    end
+
+    private
+
+    def claims(force_2fa:)
+      {
+        id_token: {
+          acr: {
+            essential: force_2fa,
+            values: ACR_FOR_2FA,
+          },
+        },
+      }
     end
   end
 end
