71ed90d19b149eed1aff1c2551339ab2f6d3e4b8
Victor Mours <victor.mours@gmail.com>
2025-10-09 16:00:38 +0200

    encore mieux

--- app/controllers/admin/organisations/online_bookings_controller.rb
+++ app/controllers/admin/organisations/online_bookings_controller.rb
@@ -40,6 +40,7 @@ class Admin::Organisations::OnlineBookingsController < AgentAuthController
       motif.update!(bookable_by: :everyone)
     end
 
+    session[:motifs_opened] = true
     # flash[:success] = "Les motifs #{motifs.pluck(:name).to_sentence} sont ouverts pour la réservation en ligne."
     redirect_to admin_organisation_online_booking_path(current_organisation)
   end
