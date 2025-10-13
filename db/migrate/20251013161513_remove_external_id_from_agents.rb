a3112dee29de1b508a9993a52c9b03aba33058d1
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-13 18:29:41 +0200

    Allez, pour faire plaisir à Rubocop

--- db/migrate/20251013161513_remove_external_id_from_agents.rb
+++ db/migrate/20251013161513_remove_external_id_from_agents.rb
@@ -3,7 +3,7 @@ class RemoveExternalIdFromAgents < ActiveRecord::Migration[7.2]
 
   def change
     safety_assured do
-      remove_column :agents, :external_id
+      remove_column :agents, :external_id, :string
     end
   end
 end
