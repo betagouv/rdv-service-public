acb53ef7f4b3d8f6c78af7c25546d2fbcccdf568
Victor Mours <victor.mours@gmail.com>
2025-10-13 10:42:50 +0200

    Improve copy button

--- app/javascript/components/copy_to_clipboard_button.js
+++ app/javascript/components/copy_to_clipboard_button.js
@@ -3,7 +3,7 @@ export default () => {
 
     button.addEventListener('click', e => {
       navigator.clipboard.writeText(button.dataset.clipboardContent)
-      button.innerHTML = "copié"
+      button.innerHTML = '<span class="fr-icon-check-line fr-icon--sm fr-mr-1v" aria-hidden="true"></span> copié'
     })
 
   })
