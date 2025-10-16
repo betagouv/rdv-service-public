4cab3c95eb737d849e2ab3cdc09878723d6e2403
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-16 12:29:32 +0200

    Affine le sélecteur, on ne veut pas que ça s'applique aux modales !

--- app/javascript/components/dsfr-alert-close.js
+++ app/javascript/components/dsfr-alert-close.js
@@ -1,5 +1,5 @@
 export default function DsfrAlertClose() {
-  document.querySelectorAll(".fr-btn--close").forEach(closeButton => {
+  document.querySelectorAll(".fr-alert > .fr-btn--close").forEach(closeButton => {
     closeButton.addEventListener("click", event => {
       const alert = event.target.parentNode;
       alert.parentNode.removeChild(alert);
