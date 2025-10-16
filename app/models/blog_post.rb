69c8456aea6aef27a9451de69b00f58a809d2c44
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-16 11:11:38 +0200

    Ne pas afficher la pastille à tout le monde lors du deploy

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -3,6 +3,11 @@ class BlogPost < ApplicationRecord
 
   def self.new_content_for_agent?(agent)
     return false unless latest_post_at
+
+    # Ce return permet de considérer que les posts Headway postés jusqu'ici ont déjà été lus.
+    # TODO: Supprimer cette ligne lorsque l'on poste sur Headway la prochaine fois.
+    return false if latest_post_at < Time.zone.parse("2025-10-15")
+
     return true if agent.blog_read_at.nil?
 
     latest_post_at > agent.blog_read_at
