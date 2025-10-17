e1fe7e0170d243293ebfdc5681431ef969a82c90
Victor Mours <victor.mours@beta.gouv.fr>
2025-10-17 10:12:07 +0200

    Réservation en ligne : détails des motifs (#5727)
    * Cards for online booking
    
    * refactor to extract controller
    
    * fix
    
    * working toggle
    
    * clean code
    
    * move and update specs
    
    * Add spec and fix banner for rdv collectif
    
    * update autodoc
    
    * update wordings
    
    * add comment

---
+++ app/controllers/admin/organisations/online_booking/motifs_controller.rb
@@ -0,0 +1,54 @@
+class Admin::Organisations::OnlineBooking::MotifsController < AgentAuthController
+  before_action :set_organisation
+  before_action :set_motif
+
+  def show; end
+
+  def edit; end
+
+  def update
+    @motif.assign_attributes(params.require(:motif).permit(:min_public_booking_delay, :max_public_booking_delay, :rdvs_editable_by_user))
+
+    # On fait un deuxième authorize pour s'assurer que les permissions sont encore valides sur la nouvelle version du motif.
+    # C'est pas strictement nécessaire ici vu qu'on ne change pas d'association, mais on le met quand même au cas où des changements de permissions soient ajoutés plus tard.
+    authorize(@motif, policy_class: Agent::MotifPolicy)
+
+    if @motif.save
+      flash[:success] = "Les options de réservation en ligne ont été mises à jour."
+      redirect_to admin_organisation_online_booking_motif_path(current_organisation, motif_id: @motif.id)
+    else
+      render :edit
+    end
+  end
+
+  def open
+    if @motif.update(bookable_by: :everyone)
+      flash[:success] = "Le motif a été ouvert à la réservation en ligne"
+    else
+      flash[:error] = @motif.errors.full_messages
+    end
+
+    redirect_to admin_organisation_online_booking_motif_path(current_organisation, motif_id: @motif.id)
+  end
+
+  def close
+    if @motif.update(bookable_by: :agents)
+      flash[:success] = "Le motif a été fermé à la réservation en ligne"
+    else
+      flash[:error] = @motif.errors.full_messages
+    end
+
+    redirect_to admin_organisation_online_booking_motif_path(current_organisation, motif_id: @motif.id)
+  end
+
+  private
+
+  def set_motif
+    @motif = Motif.find(params[:id])
+    authorize(@motif, policy_class: Agent::MotifPolicy)
+  end
+
+  def pundit_user
+    current_agent
+  end
+end
