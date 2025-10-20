55fda9c68afe27f94596340a806d9a4dbfa8bcdc
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-20 18:17:32 +0200

    Séparer l'endpoint de recherche d'agent Ajax et l'index HTML (#5696)
    * Est-ce que l'orga peut être manquante ?
    
    * Utiliser un endpoint séparé pour le Ajax d'agents et l'index HTML des agents
    
    * Ajoute un redirect qui permet une MEP en douceur
    
    * Fix specs
    
    * Ajoute un test qui montre que le uniq est utile (et qu'on devait override pundit_user)
    
    * Fix specs
    
    * Rend la route plus RESTful

--- app/controllers/admin/agents_controller.rb
+++ app/controllers/admin/agents_controller.rb
@@ -1,21 +1,25 @@
 class Admin::AgentsController < AgentAuthController
-  respond_to :html, :json
+  respond_to :html
 
   before_action :ensure_agent_is_admin, except: :index
 
   def index
+    # TODO: Supprimer ce redirect une semaine après la mise en prod
+    if request.format.json?
+      skip_policy_scope
+      redirect_to search_agents_agents_path(organisation_id: current_organisation, term: params[:term]) and return
+    end
+
     @agents = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope).active
 
-    @agents = @agents.joins(:organisations).where(organisations: { id: current_organisation.id }) if current_organisation
+    @agents = @agents.joins(:organisations).where(organisations: { id: current_organisation.id })
     @agents = index_params[:term].present? ? @agents.search_by_text(index_params[:term]) : @agents.ordered_by_last_name
 
     @display_services = current_territory.services.any? || current_organisation.agents.joins(:agent_services).any?
 
-    if request.format.html?
-      @invited_agents_count = @agents.invitation_not_accepted.where.not(invitation_sent_at: nil).created_by_invite.count
-      @agents = @agents.includes(:services, :roles, :organisations)
-      @agents = @agents.page(page_number)
-    end
+    @invited_agents_count = @agents.invitation_not_accepted.where.not(invitation_sent_at: nil).created_by_invite.count
+    @agents = @agents.includes(:services, :roles, :organisations)
+    @agents = @agents.page(page_number)
   end
 
   def new
---
+++ spec/requests/agents/agents_controller_search_spec.rb
@@ -0,0 +1,39 @@
+RSpec.describe Agents::AgentsController, "#search" do
+  let!(:territory) { create(:territory) }
+  let!(:organisation) { create(:organisation, territory: territory) }
+  let!(:agent) { create(:agent, admin_role_in_organisations: [organisation]) }
+
+  before do
+    sign_in agent
+  end
+
+  it "works" do
+    # The policy is tested separately, but let's make sure it is used.
+    expect(Agent::AgentPolicy::Scope).to receive(:new).and_call_original
+
+    francis = create(:agent, first_name: "Francis", last_name: "Factice", admin_role_in_organisations: [organisation])
+    get search_agents_agents_path(organisation_id: organisation, term: "fra"), as: :json
+    expect(parsed_response_body[:results]).to eq([{ "id" => francis.id, "text" => "FACTICE Francis" }])
+  end
+
+  context "quand un des agents n'a pas encore accepté son invitation" do
+    let!(:unconfirmed_agent) { create(:agent, :not_confirmed, email: "francis@exemple.fr", admin_role_in_organisations: [organisation]) }
+
+    it "renvoie son adresse mail" do
+      get search_agents_agents_path(organisation_id: organisation, term: "fra"), as: :json
+      expect(parsed_response_body[:results]).to include({ "id" => unconfirmed_agent.id, "text" => unconfirmed_agent.email })
+    end
+  end
+
+  context "quand un agent est présent dans plusieurs de mes orgas" do
+    let!(:orga_1) { create(:organisation) }
+    let!(:orga_2) { create(:organisation) }
+    let!(:agent) { create(:agent, admin_role_in_organisations: [orga_1, orga_2]) }
+    let!(:francis) { create(:agent, first_name: "Francis", last_name: "Factice", basic_role_in_organisations: [orga_1, orga_2]) }
+
+    it "ne s'affiche qu'une seule fois dans la liste" do
+      get search_agents_agents_path(organisation_id: [orga_1, orga_2], term: "fra"), as: :json
+      expect(parsed_response_body[:results].sole).to match({ "id" => francis.id, "text" => "FACTICE Francis" })
+    end
+  end
+end
