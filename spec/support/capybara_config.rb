ad4f7f6ad06dd6fab1f9adba805e9e523ec1152e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-21 09:33:38 +0200

    Revert "Est-ce redondant ?"
    This reverts commit 16bbcc3dbf90c8c2b38da81f6cef6073df8938af.

--- spec/support/capybara_config.rb
+++ spec/support/capybara_config.rb
@@ -1,4 +1,8 @@
-WebMock.disable_net_connect!(allow: ["www.rdv-solidarites-test.localhost"])
+WebMock.disable_net_connect!(allow: [
+                               "127.0.0.1",
+                               "localhost",
+                               "www.rdv-solidarites-test.localhost",
+                             ])
 
 def new_capybara_driver(app, **)
   Capybara::Playwright::Driver.new(
