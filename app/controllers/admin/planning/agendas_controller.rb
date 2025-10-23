c517a975a560d454e503c949f7f677cddf78657e
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-23 10:43:56 +0200

    Allez, on va faire comme dans le superadmin

--- app/controllers/admin/planning/agendas_controller.rb
+++ app/controllers/admin/planning/agendas_controller.rb
@@ -23,15 +23,13 @@ class Admin::Planning::AgendasController < AgentAuthController
     redirect_back fallback_location: admin_organisation_planning_agenda_path(current_organisation)
   end
 
-  def enable_new_planning
+  def toggle_new_planning
     authorize(current_agent, :edit?, policy_class: Agent::AgentPolicy)
-    current_agent.enable_feature!(Agent::FeatureFlags::NEW_PLANNING)
-    redirect_back fallback_location: admin_organisation_planning_agenda_path(current_organisation)
-  end
-
-  def disable_new_planning
-    authorize(current_agent, :edit?, policy_class: Agent::AgentPolicy)
-    current_agent.disable_feature!(Agent::FeatureFlags::NEW_PLANNING)
+    if params[:set_to] == "true"
+      current_agent.enable_feature!(Agent::FeatureFlags::NEW_PLANNING)
+    else
+      current_agent.disable_feature!(Agent::FeatureFlags::NEW_PLANNING)
+    end
     redirect_back fallback_location: admin_organisation_planning_agenda_path(current_organisation)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -316,8 +316,7 @@ Rails.application.routes.draw do
         namespace :planning do
           get :agenda, to: "agendas#show"
           put :toggle_displays, to: "agendas#toggle_displays"
-          put :enable_new_planning, to: "agendas#enable_new_planning"
-          put :disable_new_planning, to: "agendas#disable_new_planning"
+          put :toggle_new_planning, to: "agendas#toggle_new_planning"
 
           resources :absences
           resources :plage_ouvertures do
