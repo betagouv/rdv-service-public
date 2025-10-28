8ec3d6522af0684b2eaca038f350e14b6f5a3844
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-10-28 18:09:47 +0100

    Indique quand la création s'est faite par API

--- app/mailers/agents/webhook_mailer.rb
+++ app/mailers/agents/webhook_mailer.rb
@@ -2,10 +2,14 @@ class Agents::WebhookMailer < ApplicationMailer
   def new_webhook_url(webhook_endpoint_id:, notified_agent_id:)
     @webhook_endpoint = WebhookEndpoint.find(webhook_endpoint_id)
     @notified_agent = Agent.find(notified_agent_id)
-    @author = Agent.agent_from_whodunnit(@webhook_endpoint.versions.last.whodunnit)
+    @whodunnit = @webhook_endpoint.versions.last.whodunnit
+    @author = Agent.agent_from_whodunnit(@whodunnit)
+    @via_api = @whodunnit&.include?("(via API)")
 
     to = @notified_agent.email
-    subject = if @notified_agent == @author
+    subject = if @via_api
+                "Un webhook vient d'être ajouté par API"
+              elsif @notified_agent == @author
                 "Vous venez d'ajouter une nouvelle URL de webhook"
               else
                 "Une nouvelle URL de webhook vient d'être ajoutée"
--- spec/mailers/agents/webhook_mailer_spec.rb
+++ spec/mailers/agents/webhook_mailer_spec.rb
@@ -31,5 +31,16 @@ RSpec.describe Agents::WebhookMailer, type: :mailer do
       expect(mail.subject).to eq("Vous venez d'ajouter une nouvelle URL de webhook")
       expect(mail.body.encoded).to include("Vous venez d'introduire une nouvelle URL de webhook :")
     end
+
+    it "specifies when the operation was done via API" do
+      author = create(:agent, first_name: "Amine", last_name: "Despace")
+      notified_agent = create(:agent)
+      PaperTrail.request.whodunnit = author.name_for_paper_trail + " (via API)"
+      mail = described_class.new_webhook_url(webhook_endpoint_id: create(:webhook_endpoint).id, notified_agent_id: notified_agent.id)
+
+      expect(mail.subject).to eq("Un webhook vient d'être ajouté par API")
+      puts mail.body.encoded
+      expect(mail.body.encoded).to include("Une nouvelle URL de webhook vient d'être introduite par Amine DESPACE (via API) :")
+    end
   end
 end
