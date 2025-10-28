4c4ae5fb20cac05268f13f96b20a58b55e2d4e4c
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-10-28 13:39:05 +0100

    À la création de ticket support Zammad, récupérer les infos agent ou usager par matching email ou telephone (#5756)
    * Renseigner les infos customer à la création de tickets Zammad
    
    * Renseigner le Customer Zammad par matching email ou numéro de tel
    
    sole improvements
    
    remove useless create_note
    
    extract
    
    * refacto
    
    * file reorg
    
    * ungenerize matchers
    
    * ungenerize find_user_or_agent_and_augment 1
    
    * ungenerize matchers record
    
    * improve
    
    * improvements
    
    * remove deprecated line
    
    * comments in attributes
    
    * comment augmenter
    
    * remove agent matcher
    
    * rapatrier agentaugmenter
    
    * rapatrier user augmenter
    
    * remove augment_with
    
    * extract to augmenter
    
    * match by email
    
    * multiple_matches_by_phone_number_formatted
    
    * match_by_phone_number_formatted
    
    * multiple phone raw matches
    
    * final blow to user matcher
    
    * zammad customer not attributes
    
    * minor updates
    
    * corrections dans le job

---
+++ spec/services/zammad_customer_spec.rb
@@ -0,0 +1,129 @@
+RSpec.describe ZammadCustomer do
+  describe ZammadCustomer::Augmenter do
+    describe "#run" do
+      context "usager trouvé par email" do
+        let!(:user) { create(:user, email: "soukalina@gmail.com") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(email: "soukalina@gmail.com", phone: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Usager trouvé avec l'email soukalina@gmail.com"
+          expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user.id}"
+        end
+      end
+
+      context "agent trouvé par email" do
+        let!(:agent) { create(:agent, email: "agent@example.com") }
+        let(:args) { {} }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(email: "agent@example.com", phone: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Agent trouvé avec l'email agent@example.com"
+          expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/agents/#{agent.id}"
+        end
+      end
+
+      context "usager trouvé par numéro de téléphone" do
+        let!(:user) { create(:user, phone_number: "0612345678", phone_number_formatted: "+33612345678") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(phone: "0612345678", email: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Usager trouvé avec le numéro de téléphone formatté +33612345678"
+          expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user.id}"
+        end
+      end
+
+      context "aucun usager ni agent trouvé" do
+        specify do
+          zammad_customer = ZammadCustomer.new(email: "unknown@example.com", phone: "013456789")
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Aucun usager ni agent trouvé"
+          expect(zammad_customer.super_admin_url).to be_nil
+        end
+      end
+
+      context "la correspondance par email prévaut sur le téléphone" do
+        let!(:user_by_email) { create(:user, email: "test@example.com") }
+        let!(:user_by_phone) { create(:user, phone_number: "0612345678") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(email: "test@example.com", phone: "0612345678")
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Usager trouvé avec l'email test@example.com"
+          expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user_by_email.id}"
+          expect(zammad_customer.super_admin_url).not_to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user_by_phone.id}"
+        end
+      end
+
+      context "usager trouvé par numéro de téléphone formatté" do
+        let!(:user) { create(:user, phone_number: "06.12-34.56.78", phone_number_formatted: "+33612345678") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(phone: "06 12 34 56 78", email: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Usager trouvé avec le numéro de téléphone formatté +33612345678"
+          expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user.id}"
+        end
+      end
+
+      context "plusieurs usagers trouvés via le numéro de téléphone formatté" do
+        let!(:user1) { create(:user, phone_number: "06.12-34.56.78", phone_number_formatted: "+33612345678") }
+        let!(:user2) { create(:user, phone_number: "06.12-34.56.78", phone_number_formatted: "+33612345678kdkd") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(phone: "06 12 34 56 78", email: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Plusieurs usagers trouvés avec le numéro de téléphone formatté +33612345678"
+          expect(zammad_customer.super_admin_url).to be_nil
+        end
+      end
+
+      context "plusieurs usagers trouvés via le numéro de téléphone non formatté" do
+        let!(:user1) { build(:user, phone_number: "01 23 44").tap { _1.save!(validate: false) } }
+        let!(:user2) { build(:user, phone_number: "01 23 44").tap { _1.save!(validate: false) } }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(phone: "01 23 44", email: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Plusieurs usagers trouvés avec le numéro de téléphone 01 23 44"
+          expect(zammad_customer.super_admin_url).to be_nil
+        end
+      end
+
+      context "usager non trouvé si numéro trop court" do
+        let!(:user) { create(:user, phone_number: "0123456789") }
+
+        specify do
+          zammad_customer = ZammadCustomer.new(phone: "123", email: nil)
+          described_class.new(zammad_customer).run
+          expect(zammad_customer.note).to eq "Aucun usager ni agent trouvé"
+          expect(zammad_customer.super_admin_url).to be_nil
+        end
+      end
+    end
+
+    describe "#augment_with_agent" do
+      let!(:agent) { create(:agent, email: "agent@example.com") }
+
+      specify do
+        zammad_customer = ZammadCustomer.new(phone: "123", email: nil)
+        described_class.new(zammad_customer).augment_with_agent(agent)
+        expect(zammad_customer.note).to be_blank
+        expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/agents/#{agent.id}"
+      end
+    end
+
+    describe "#augment_with_user" do
+      let!(:user) { create(:user, email: "user@example.com") }
+
+      specify do
+        zammad_customer = ZammadCustomer.new(phone: "123", email: nil)
+        described_class.new(zammad_customer).augment_with_user(user)
+        expect(zammad_customer.note).to be_blank
+        expect(zammad_customer.super_admin_url).to eq "http://www.rdv-mairie-test.localhost/super_admins/users/#{user.id}"
+      end
+    end
+  end
+end
