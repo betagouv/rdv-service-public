1d091904644fb6394fd00b5719ec5cad9b8aeba8
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-10-28 13:34:12 +0100

    corrections dans le job

--- app/jobs/create_zammad_ticket_job.rb
+++ app/jobs/create_zammad_ticket_job.rb
@@ -4,20 +4,20 @@ class CreateZammadTicketJob < ApplicationJob
   def perform(sender_role:, email:, phone_number:, first_name:, last_name:, subject:, body:, tags: [], user_id: nil, agent_id: nil)
     raise Error, "Les seuls sender_role valables sont usager et agent" if %w[usager agent].exclude?(sender_role.to_s)
 
-    customer_attributes = ZammadCustomer.new(
+    zammad_customer = ZammadCustomer.new(
       email:, rdvsp_role: sender_role, phone: phone_number,
       firstname: first_name, lastname: last_name
     )
 
     if user_id.present?
-      ZammadCustomer::Augmenter.new(customer_attributes).augment_with_user(User.find(user_id))
+      ZammadCustomer::Augmenter.new(zammad_customer).augment_with_user(User.find(user_id))
     elsif agent_id.present?
-      ZammadCustomer::Augmenter.new(customer_attributes).augment_with_agent(Agent.find(agent_id))
+      ZammadCustomer::Augmenter.new(zammad_customer).augment_with_agent(Agent.find(agent_id))
     else
-      customer_attributes.find_user_or_agent_and_augment
+      ZammadCustomer::Augmenter.new(zammad_customer).run
     end
 
-    zammad_customer = ZammadApiClient.upsert_customer(email:, **customer_attributes.attributes)
-    ZammadApiClient.create_ticket(customer_id: zammad_customer["id"], sender_role:, subject:, body:, tags:)
+    created_zammad_customer = ZammadApiClient.upsert_customer(email:, **zammad_customer.attributes)
+    ZammadApiClient.create_ticket(customer_id: created_zammad_customer["id"], sender_role:, subject:, body:, tags:)
   end
 end
