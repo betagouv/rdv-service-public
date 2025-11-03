3d26f053e38f2ed581a596831cabc77acbf9bdac
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-03 11:48:48 +0100

    Permet de re-créer la review app après son expiration

--- scripts/create_review_app.rb
+++ scripts/create_review_app.rb
@@ -8,15 +8,15 @@ pr_body = `gh pr view --json body --jq '.body'`
 review_app_name = "rdv-service-public-review-app-pr#{pr_number}"
 review_app_url = "https://#{review_app_name}.osc-secnum-fr1.scalingo.io/"
 
+review_app_exists = system("scalingo --app #{review_app_name} stats", out: IO::NULL, err: IO::NULL)
+unless review_app_exists
+  puts "La review app n'existe pas, création en cours..."
+  system("scalingo --region osc-secnum-fr1 --app rdv-service-public-review-app integration-link-manual-review-app #{pr_number}")
+end
+
 if pr_body.include?(review_app_url)
   puts "La description de PR contient déjà un lien vers la review app."
   exit(0)
-end
-
-review_app_exists = system("scalingo --app #{review_app_name} stats", out: IO::NULL, err: IO::NULL)
-unless review_app_exists
-  puts "La review app n'existe pas encore, création en cours..."
-  system("scalingo --region osc-secnum-fr1 --app rdv-service-public-review-app integration-link-manual-review-app #{pr_number}")
 end
 
 link_to_review_app = "[Review app](#{review_app_url})"
