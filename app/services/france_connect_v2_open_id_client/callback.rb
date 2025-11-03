fffa0a91407690b24e8a7328f7ad6ed6b81c4cc6
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-03 16:31:41 +0100

    Keep it simple

--- app/services/france_connect_v2_open_id_client/callback.rb
+++ app/services/france_connect_v2_open_id_client/callback.rb
@@ -1,7 +1,5 @@
 # voir https://github.com/france-connect/Documentation-AgentConnect/blob/main/doc_fs/technique_fca/endpoints.md
 module FranceConnectV2OpenIdClient
-  ES256_ALGORITHM = "ES256".freeze
-
   class Callback
     class OpenIdFlowError < StandardError; end
     class ApiRequestError < StandardError; end
@@ -90,7 +88,7 @@ module FranceConnectV2OpenIdClient
 
       handle_response_error(response)
 
-      JWT.decode(response.body, nil, true, algorithms: ES256_ALGORITHM, jwks: france_connect_v2_config.jwks).first
+      JWT.decode(response.body, nil, true, algorithms: "ES256", jwks: france_connect_v2_config.jwks).first
     end
 
     def handle_response_error(response)
