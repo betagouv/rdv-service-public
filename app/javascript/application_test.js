9792d92a73785d44e388a503e1d401914f2b70c2
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-11-10 14:30:25 +0100

    Add comment

--- app/javascript/application_test.js
+++ app/javascript/application_test.js
@@ -1,1 +1,5 @@
+// Ce fichier est utilisé pour importer le module axe-core pour les tests d'accessibilité qui sont effectués dans :
+// - spec/features/accessibility
+// - spec/features/autodoc (nous intégrons maintenant les tests d'accessibilité dans les captures d'écran autodoc)
+
 import "axe-core/axe.min.js";
