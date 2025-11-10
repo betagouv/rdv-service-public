788539c6ce3d78de863114b16f3d3e55602771c1
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-11-10 14:25:30 +0100

    Remove axe-core SHA in CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -22,16 +22,6 @@ metabase = "rdv-service-public-metabase.osc-secnum-fr1.scalingo.io"
 # Utilisés par swagger pour la documentation de l'api
 swagger_shas = ["'sha256-j4Lx1FqFgvYDBEjW7NQaEY7/HhCi8WVsLWkqC4+wJ3w='", "'sha256-JHKToH7KbGJj6TloPeWnKnbImDel00Whl1rRnBiTYuQ='"]
 
-# Utilisé par Axios pour les tests d'accessibilité
-test_shas = []
-if Rails.env.test?
-  test_shas << if ENV["CI"].present?
-                 "'sha256-4UywW1I9VFu7o60u4zSiU9FmUjIhiIv4N1FflQjVse0='"
-               else
-                 "'sha256-MW9ENekiBxLmssEGlk+IVYZiQXOqQCOggVxAMOB1ePc='"
-               end
-end
-
 # Tant qu'on utilise les Turbolinks, c'est très difficile d'avoir des CSP différentes pour chaque pages,
 # puisque les CSP sont uniquement chargées lors de la première requête qui charle le premier document,
 # et pas lors des appels XHR fait par les turbolinks.
@@ -49,7 +39,7 @@ Rails.application.config.content_security_policy do |policy|
   policy.style_src :self, :unsafe_inline, bootstrap_cdn, unpkg_cdn
   policy.connect_src :self, api_adresse_ign, tiles_etalab, tiles_data_gouv
 
-  policy.script_src :self, unpkg_cdn, *swagger_shas, *test_shas
+  policy.script_src :self, unpkg_cdn, *swagger_shas
 end
 
 # If you are using UJS then enable automatic nonce generation
