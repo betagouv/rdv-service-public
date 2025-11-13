e5cbd36d11fc86e53d00f1d384951dfe48801332
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-13 09:48:50 +0100

    CI 3

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,7 +71,7 @@ RSpec.configure do |config|
     ActiveRecord::Tasks::DatabaseTasks.truncate_all
   end
 
-  # CI 2
+  # CI 3
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
