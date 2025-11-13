dd5ac9ad14c26bf1b1b5bbe1fb9f90a2de870bf4
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-13 11:09:06 +0100

    CI 11

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,8 +71,6 @@ RSpec.configure do |config|
     ActiveRecord::Tasks::DatabaseTasks.truncate_all
   end
 
-  # CI 10
-
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
