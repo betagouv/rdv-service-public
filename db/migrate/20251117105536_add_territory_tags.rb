ad513680ed6071b2e172f24edc91b0b93655e266
Victor Mours <victor.mours@gmail.com>
2025-11-17 14:50:09 +0100

    fix

--- db/migrate/20251117105536_add_territory_tags.rb
+++ db/migrate/20251117105536_add_territory_tags.rb
@@ -8,7 +8,7 @@ class AddTerritoryTags < ActiveRecord::Migration[7.2]
     change_table_comment(:tags, from: nil, to: "Des tags pour catégoriser les espaces en fonctions des partenariats auxquels ils sont liés.")
 
     create_table :territory_tags do |t|
-      t.references :territory, foreign_key: true, index: true
+      t.references :territory, foreign_key: true
       t.references :tag, foreign_key: true, index: true
 
       t.timestamps
