52793ed8ff622752eca3ab3ca58c6d462adb72ba
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-11-17 11:51:20 +0100

    Stats dynamiques sur la page d’accueil (#5825)

---
+++ app/jobs/cron_job/refresh_cached_stats_job.rb
@@ -0,0 +1,79 @@
+class CronJob::RefreshCachedStatsJob < CronJob
+  def perform
+    return unless MetabaseApi.authentication_present?
+
+    queries_by_key.each do |key, query|
+      Rails.logger.debug { "querying Metabase for #{key}…" }
+      count = MetabaseApi.sql_query(query)[0]["c"].gsub(",", "").to_i
+      Rails.logger.info "got #{key} = #{count}. writing to cache…"
+      Rails.cache.write(key, count)
+      Rails.logger.debug "✅ wrote to cache"
+    end
+    Rails.logger.debug "🏁 done"
+  end
+
+  private
+
+  def queries_by_key
+    {
+      "stats.both_instances.2_years.rdvs_count" => <<~SQL.squish,
+        SELECT
+          SUM(c) AS c
+        FROM
+          (
+            (
+              SELECT
+                COUNT("rdvsp".rdvs."id") AS c
+              FROM
+                "rdvsp"."rdvs"
+              WHERE
+                (
+                  "rdvsp"."rdvs"."starts_at" >= CAST((NOW() + INTERVAL '-730 day') AS date)
+                )
+            )
+            UNION ALL
+            (
+              SELECT
+                COUNT("rdvs"."rdvs"."id") AS c
+              FROM
+                "rdvs"."rdvs"
+              WHERE
+                (
+                  "rdvs"."rdvs"."starts_at" >= CAST((NOW() + INTERVAL '-730 day') AS date)
+                )
+            )
+          );
+      SQL
+      "stats.both_instances.2_years.active_organisations_count" => <<~SQL.squish,
+        SELECT
+          SUM(c) AS c
+        FROM
+          (
+            (
+              SELECT
+                COUNT(DISTINCT "rdvsp"."organisations"."id") AS c
+              FROM
+                "rdvsp"."organisations"
+                INNER JOIN "rdvsp"."rdvs" ON "rdvsp"."rdvs"."organisation_id" = "rdvsp"."organisations"."id"
+              WHERE
+                (
+                  "rdvsp"."rdvs"."starts_at" >= CAST((NOW() + INTERVAL '-730 day') AS date)
+                )
+            )
+            UNION ALL
+            (
+              SELECT
+                COUNT(DISTINCT "rdvs"."organisations"."id") AS c
+              FROM
+                "rdvs"."organisations"
+                INNER JOIN "rdvs"."rdvs" ON "rdvs"."rdvs"."organisation_id" = "rdvs"."organisations"."id"
+              WHERE
+                (
+                  "rdvs"."rdvs"."starts_at" >= CAST((NOW() + INTERVAL '-730 day') AS date)
+                )
+            )
+          );
+      SQL
+    }
+  end
+end
