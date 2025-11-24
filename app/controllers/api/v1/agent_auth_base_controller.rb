fa11c24530467eaf6f29c842e0d94963e636e74d
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-24 11:13:51 +0100

    Finalement si on sépare les méthodes ça évite de gérer le rescue

--- app/controllers/api/v1/agent_auth_base_controller.rb
+++ app/controllers/api/v1/agent_auth_base_controller.rb
@@ -4,8 +4,9 @@ class Api::V1::AgentAuthBaseController < Api::V1::BaseController
 
   skip_before_action :verify_authenticity_token
   before_action :authenticate_agent
+  before_action :log_api_call_in_database
   before_action :set_paper_trail_whodunnit
-  around_action :log_api_call_in_database
+  around_action :measure_execution_time
 
   def pundit_user
     AgentOrganisationContext.new(current_agent, current_organisation)
@@ -124,19 +125,13 @@ class Api::V1::AgentAuthBaseController < Api::V1::BaseController
       host: request.host,
     }
 
-    api_call = ApiCall.create!(
+    @_api_call = ApiCall.create!(
       raw_http: raw_http,
       controller_name: controller_name,
       action_name: action_name,
       agent_id: current_agent.id,
       authentication_type: @authentication_type
     )
-
-    start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-    yield
-    end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-
-    api_call.update!(duration_in_ms: (end_time - start_time).in_milliseconds)
   rescue StandardError => e
     Sentry.capture_exception(e, extra: {
                                raw_http: raw_http,
@@ -144,5 +139,14 @@ class Api::V1::AgentAuthBaseController < Api::V1::BaseController
                                action_name: action_name,
                                agent_id: current_agent&.id,
                              })
+  end
+
+  def measure_execution_time
+    start_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    yield
+    end_time = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+
+    duration = end_time - start_time
+    @_api_call.update(duration_in_ms: duration.in_milliseconds)
   end
 end
