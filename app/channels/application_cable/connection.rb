dd84e50d04c5413a5e7d3ec786302e9147f7c908
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-11-26 00:21:30 +0100

    Ajoute un warn a Sentry en cas d'erreur de connexion

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,6 +1,7 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
     identified_by :current_agent
+    rescue_from StandardError, with: :report_error
 
     def connect
       self.current_agent = find_verified_agent
@@ -12,6 +13,10 @@ module ApplicationCable
       session = cookies.encrypted[Rails.application.config.session_options[:key]]
       agent_id_from_session = session["warden.user.agent.key"].first.first
       Agent.find_by(id: agent_id_from_session) || reject_unauthorized_connection
+    end
+
+    def report_error(e)
+      Sentry.capture_exception(e)
     end
   end
 end
