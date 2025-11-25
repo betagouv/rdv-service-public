3fbdef1ad72c8cce6265d1cb250a1484216e4037
Victor Mours <victor.mours@gmail.com>
2025-11-25 10:55:26 +0100

    add job, and script for backfill

--- db/migrate/20251124134240_add_denormalized_rdv_colmuns_to_agents_rdvs.rb
+++ db/migrate/20251124134240_add_denormalized_rdv_colmuns_to_agents_rdvs.rb
@@ -7,12 +7,10 @@ class AddDenormalizedRdvColmunsToAgentsRdvs < ActiveRecord::Migration[8.0]
     add_column :agents_rdvs, :readonly_busy_in_the_future, :boolean, comment: "Colonne indexée et utilisée en lecture"
 
     reversible do |direction|
-      direction.up do
-        AgentsRdv.all.includes(:rdv).find_each do |agents_rdv|
-          agents_rdv.update_columns(
-            readonly_rdv_starts_at: agents_rdv.rdv.starts_at,
-            readonly_rdv_ends_at: agents_rdv.rdv.ends_at,
-            readonly_busy_in_the_future: agents_rdv.rdv.busy_in_the_future?
+      unless Rails.env.production? # En production la migration prend trop longtemps, donc on fera tourner manuellement scripts/initialize_calculator_index_values.rb
+        direction.up do
+          AgentsRdv.update_all(
+            "readonly_rdv_starts_at = rdvs.starts_at, readonly_rdv_ends_at = rdvs.ends_at, readonly_busy_in_the_future = (rdvs.starts_at >= NOW() AND rdvs.status IN ('unknown', 'seen', 'noshow')) FROM rdvs WHERE rdvs.id = agents_rdvs.rdv_id"
           )
         end
       end
