d59a7fa68ad5d20ba77cf4960e1e01e54bc9a195
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-11-25 13:54:12 +0100

    stats homepage : RDV count sur un an + plus de count d’orgas

--- spec/jobs/cron_job/refresh_cached_stats_job_spec.rb
+++ spec/jobs/cron_job/refresh_cached_stats_job_spec.rb
@@ -7,47 +7,47 @@ RSpec.describe CronJob::RefreshCachedStatsJob do
   context "chiffres reçus correctement" do
     specify do
       expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
-      described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
-      expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
+      described_class.new.perform(keys: ["stats.both_instances.1_year.rdvs_count"])
+      expect(Rails.cache.fetch("stats.both_instances.1_year.rdvs_count")).to eq(3_706_950)
     end
   end
 
   context "chiffres formattés avec des virgules" do
     specify do
       expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3,706,950" }])
-      described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
-      expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
+      described_class.new.perform(keys: ["stats.both_instances.1_year.rdvs_count"])
+      expect(Rails.cache.fetch("stats.both_instances.1_year.rdvs_count")).to eq(3_706_950)
     end
   end
 
   context "chiffres similaires dans le cache avant" do
     it "met à jour le cache" do
-      Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
+      Rails.cache.write("stats.both_instances.1_year.rdvs_count", 3_500_000)
       expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
-      described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
-      expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
+      described_class.new.perform(keys: ["stats.both_instances.1_year.rdvs_count"])
+      expect(Rails.cache.fetch("stats.both_instances.1_year.rdvs_count")).to eq(3_706_950)
     end
   end
 
   context "chiffres qui paraissent aberrants" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
-      Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
+      Rails.cache.write("stats.both_instances.1_year.rdvs_count", 3_500_000)
       expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
       expect do
-        described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
+        described_class.new.perform(keys: ["stats.both_instances.1_year.rdvs_count"])
       end.to raise_error(CronJob::RefreshCachedStatsJob::SuspiciousFigureError)
-      expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_500_000)
+      expect(Rails.cache.fetch("stats.both_instances.1_year.rdvs_count")).to eq(3_500_000)
     end
   end
 
   context "chiffres qui paraissent aberrants mais on force le rafraîchissement" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
-      Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
+      Rails.cache.write("stats.both_instances.1_year.rdvs_count", 3_500_000)
       expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
       expect do
-        described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"], force: true)
+        described_class.new.perform(keys: ["stats.both_instances.1_year.rdvs_count"], force: true)
       end.not_to raise_error
-      expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_000)
+      expect(Rails.cache.fetch("stats.both_instances.1_year.rdvs_count")).to eq(3_000)
     end
   end
 end
