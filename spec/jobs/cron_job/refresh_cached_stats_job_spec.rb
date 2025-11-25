a66157bc376fb2ddcf516f8431d3e0774d51475f
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-11-25 14:06:29 +0100

    refacto 2: on limite dans les specs sur une seule clé pour simplifier

--- spec/jobs/cron_job/refresh_cached_stats_job_spec.rb
+++ spec/jobs/cron_job/refresh_cached_stats_job_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable RSpec/StubbedMock
 RSpec.describe CronJob::RefreshCachedStatsJob do
   before do
     allow(MetabaseApi).to receive(:authentication_present?).and_return(true)
@@ -5,55 +6,41 @@ RSpec.describe CronJob::RefreshCachedStatsJob do
 
   context "chiffres reçus correctement" do
     specify do
-      expect(MetabaseApi).to receive(:sql_query).twice.and_return(
-        [{ "c" => "3 706 950" }],
-        [{ "c" => "2,482" }]
-      )
-      described_class.new.perform
+      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
+      described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
-      expect(Rails.cache.fetch("stats.both_instances.2_years.active_organisations_count")).to eq(2_482)
     end
   end
 
   context "chiffres similaires dans le cache avant" do
     it "met à jour le cache" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      Rails.cache.write("stats.both_instances.2_years.active_organisations_count", 2_000)
-      expect(MetabaseApi).to receive(:sql_query).twice.and_return(
-        [{ "c" => "3 706 950" }],
-        [{ "c" => "2,482" }]
-      )
-      described_class.new.perform
+      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
+      described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
-      expect(Rails.cache.fetch("stats.both_instances.2_years.active_organisations_count")).to eq(2_482)
     end
   end
 
   context "chiffres qui paraissent aberrants" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      Rails.cache.write("stats.both_instances.2_years.active_organisations_count", 2_000)
-      expect(MetabaseApi).to receive(:sql_query).twice.and_return(
-        [{ "c" => "3000" }],
-        [{ "c" => "20" }]
-      )
-      expect { described_class.new.perform }.to raise_error(CronJob::RefreshCachedStatsJob::SuspiciousFigureError)
+      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
+      expect do
+        described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"])
+      end.to raise_error(CronJob::RefreshCachedStatsJob::SuspiciousFigureError)
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_500_000)
-      expect(Rails.cache.fetch("stats.both_instances.2_years.active_organisations_count")).to eq(2_000)
     end
   end
 
   context "chiffres qui paraissent aberrants mais on force le rafraîchissement" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      Rails.cache.write("stats.both_instances.2_years.active_organisations_count", 2_000)
-      expect(MetabaseApi).to receive(:sql_query).twice.and_return(
-        [{ "c" => "3000" }],
-        [{ "c" => "20" }]
-      )
-      expect { described_class.new.perform(force: true) }.not_to raise_error
+      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
+      expect do
+        described_class.new.perform(keys: ["stats.both_instances.2_years.rdvs_count"], force: true)
+      end.not_to raise_error
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_000)
-      expect(Rails.cache.fetch("stats.both_instances.2_years.active_organisations_count")).to eq(20)
     end
   end
 end
+# rubocop:enable RSpec/StubbedMock
