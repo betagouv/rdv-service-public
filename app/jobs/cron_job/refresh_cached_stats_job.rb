398d9ffd48fd255ae008335d83e76912601e1fe2
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-11-25 14:04:37 +0100

    refacto 1 : ajout d’un param keys à RefreshCachedStatsJob

--- app/jobs/cron_job/refresh_cached_stats_job.rb
+++ app/jobs/cron_job/refresh_cached_stats_job.rb
@@ -1,27 +1,12 @@
 class CronJob::RefreshCachedStatsJob < CronJob
   class SuspiciousFigureError < StandardError; end
 
-  def perform(force: false)
+  def perform(keys: nil, force: false)
+    @force = force
+
     return unless MetabaseApi.authentication_present?
 
-    queries_by_key.each do |key, query|
-      previous_value = Rails.cache.fetch(key)
-
-      Rails.logger.debug { "querying Metabase for #{key}…" }
-      new_value = MetabaseApi.sql_query(query)[0]["c"]
-        .gsub(/[, ]/, "") # Metabase sometimes splits thousands with spaces or commas 🤷
-        .to_i
-      Rails.logger.info "got #{key} = #{new_value}"
-
-      if previous_value.nil? ||
-         force ||
-         (new_value.to_f / previous_value).between?(0.1, 10) # des valeurs 10x plus hautes ou plus petites sont suspectes
-        Rails.cache.write(key, new_value, expires_at: 30.days.from_now)
-        Rails.logger.debug "✅ wrote to cache"
-      else
-        @suspicious_value = { key:, new_value:, previous_value: }
-      end
-    end
+    (keys || queries_by_key.keys).each { refresh_stats_key(_1) }
 
     # on lève l’exception hors de la boucle pour permettre de remplir les autres valeurs
     raise SuspiciousFigureError, @suspicious_value.to_s if @suspicious_value
@@ -30,6 +15,26 @@ class CronJob::RefreshCachedStatsJob < CronJob
   end
 
   private
+
+  def refresh_stats_key(key)
+    query = queries_by_key[key]
+    previous_value = Rails.cache.fetch(key)
+
+    Rails.logger.debug { "querying Metabase for #{key}…" }
+    new_value = MetabaseApi.sql_query(query)[0]["c"]
+      .gsub(/[, ]/, "") # Metabase sometimes splits thousands with spaces or commas 🤷
+      .to_i
+    Rails.logger.info "got #{key} = #{new_value}"
+
+    if previous_value.nil? ||
+       @force ||
+       (new_value.to_f / previous_value).between?(0.1, 10) # des valeurs 10x plus hautes ou plus petites sont suspectes
+      Rails.cache.write(key, new_value, expires_at: 30.days.from_now)
+      Rails.logger.debug "✅ wrote to cache"
+    else
+      @suspicious_value = { key:, new_value:, previous_value: }
+    end
+  end
 
   def queries_by_key
     {
