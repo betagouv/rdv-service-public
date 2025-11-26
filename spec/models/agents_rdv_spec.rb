553a21c25a67561829a7597979d79dbb1e3fd94f
Victor Mours <victor.mours@gmail.com>
2025-11-26 16:33:22 +0100

    Add more unit specs

--- spec/models/agents_rdv_spec.rb
+++ spec/models/agents_rdv_spec.rb
@@ -1,5 +1,31 @@
 RSpec.describe AgentsRdv do
   describe "données dénormalisées depuis le rendez-vous pour l'index du Calculator" do
+    it "copie les données à la création du rendez-vous" do
+      rdv = build(:rdv)
+      rdv.save!
+      expect(rdv.agents_rdvs.first.reload).to have_attributes(
+        {
+          readonly_rdv_starts_at: rdv.starts_at,
+          readonly_rdv_ends_at: rdv.ends_at,
+          readonly_busy_in_the_future: true,
+        }
+      )
+    end
+
+    it "met à jour les données quand le rendez-vous est modifié" do
+      rdv = create(:rdv, starts_at: 3.days.from_now)
+
+      rdv.update!(starts_at: 3.days.ago)
+
+      expect(rdv.agents_rdvs.first.reload).to have_attributes(
+        {
+          readonly_rdv_starts_at: rdv.starts_at,
+          readonly_rdv_ends_at: rdv.ends_at,
+          readonly_busy_in_the_future: false,
+        }
+      )
+    end
+
     it "copie les données quand on ajoute un agent au rendez-vous" do
       rdv = create(:rdv)
       agent = create(:agent)
