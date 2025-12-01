05fd783cd7e4831342c77357348992554fc9e5e1
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-12-01 16:42:19 +0100

    Ajoute du reporting d'erreur

--- app/channels/agenda_channel.rb
+++ app/channels/agenda_channel.rb
@@ -2,7 +2,10 @@ class AgendaChannel < ApplicationCable::Channel
   # Called when the consumer has successfully
   # become a subscriber to this channel.
   def subscribed
-    agent = Agent.find(params["agent_id"])
+    agent = Agent::AgentPolicy::Scope.new(current_agent, Agent.all).resolve.find(params.fetch("agent_id"))
     stream_for agent.id
+  rescue StandardError => e
+    Sentry.capture_exception(e)
+    reject
   end
 end
--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,4 +1,5 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
+    rescue_from(StandardError) { Sentry.capture_exception(_1) }
   end
 end
