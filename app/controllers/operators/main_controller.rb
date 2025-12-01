d1167d6c4dd76b253555860ec80b7aa05c775a42
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-12-01 14:38:20 +0100

    Remove home

--- app/controllers/operators/main_controller.rb
+++ app/controllers/operators/main_controller.rb
@@ -1,5 +1,7 @@
 class Operators::MainController < OperatorController
   skip_before_action :authenticate_operator_manager!, only: :index
 
-  def index; end
+  def index
+    redirect_to operators_espaces_path if operator_manager_signed_in?
+  end
 end
