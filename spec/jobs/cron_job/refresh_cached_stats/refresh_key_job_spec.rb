55da133af014af66ece405071e869510419f1440
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2025-12-02 15:17:09 +0100

    allow instead of commit

--- spec/jobs/cron_job/refresh_cached_stats/refresh_key_job_spec.rb
+++ spec/jobs/cron_job/refresh_cached_stats/refresh_key_job_spec.rb
@@ -1,4 +1,3 @@
-# rubocop:disable RSpec/StubbedMock
 RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
   before do
     allow(MetabaseApi).to receive(:authentication_present?).and_return(true)
@@ -6,7 +5,7 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
 
   context "chiffres reçus correctement" do
     specify do
-      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
+      allow(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
       described_class.new.perform(key: "stats.both_instances.2_years.rdvs_count")
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
     end
@@ -14,7 +13,7 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
 
   context "chiffres formattés avec des virgules" do
     specify do
-      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3,706,950" }])
+      allow(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3,706,950" }])
       described_class.new.perform(key: "stats.both_instances.2_years.rdvs_count")
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
     end
@@ -23,7 +22,7 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
   context "chiffres similaires dans le cache avant" do
     it "met à jour le cache" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
+      allow(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3 706 950" }])
       described_class.new.perform(key: "stats.both_instances.2_years.rdvs_count")
       expect(Rails.cache.fetch("stats.both_instances.2_years.rdvs_count")).to eq(3_706_950)
     end
@@ -32,7 +31,7 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
   context "chiffres qui paraissent aberrants" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
+      allow(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
       expect do
         described_class.new.perform(key: "stats.both_instances.2_years.rdvs_count")
       end.to raise_error(CronJob::RefreshCachedStats::SuspiciousFigureError)
@@ -43,7 +42,7 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
   context "chiffres qui paraissent aberrants mais on force le rafraîchissement" do
     it "ne remplace pas les valeurs dans le cache, valeurs aberrantes ignorées" do
       Rails.cache.write("stats.both_instances.2_years.rdvs_count", 3_500_000)
-      expect(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
+      allow(MetabaseApi).to receive(:sql_query).and_return([{ "c" => "3000" }])
       expect do
         described_class.new.perform(key: "stats.both_instances.2_years.rdvs_count", force: true)
       end.not_to raise_error
@@ -51,4 +50,3 @@ RSpec.describe CronJob::RefreshCachedStats::RefreshKeyJob do
     end
   end
 end
-# rubocop:enable RSpec/StubbedMock
