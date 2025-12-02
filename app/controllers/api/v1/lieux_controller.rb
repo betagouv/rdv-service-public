ec3af2f7e042bba09664b5bb5edbbbd31c070322
Victor Mours <victor.mours@gmail.com>
2025-12-02 16:18:32 +0100

    fix

--- app/controllers/api/v1/lieux_controller.rb
+++ app/controllers/api/v1/lieux_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::LieuxController < Api::V1::AgentAuthBaseController
   end
 
   def create
-    lieu = Lieu.new(params.permit(:organisation_id, :name, :address, :latitude, :longitude, :phone_numberm, :availability))
+    lieu = Lieu.new(params.permit(:organisation_id, :name, :address, :latitude, :longitude, :phone_number, :availability))
 
     authorize(lieu, policy_class: Agent::LieuPolicy)
 
