1ed1dda5d3ce1c3e2c5958076419d33f1dea5413
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-12-02 18:26:45 +0100

    Move rdv short in redirect controller

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,7 +1,11 @@
 class RedirectController < ApplicationController
-  def short_rdv_without_id
+  def rdv_short_without_id
     @participation = Participation.find_by!(restricted_auth_token: params[:tkn])
 
     redirect_to users_rdv_path(@participation.rdv, invitation_token: params[:tkn])
+  end
+
+  def rdv_short
+    redirect_to users_rdv_path(params[:id], invitation_token: params[:tkn])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -389,13 +389,10 @@ Rails.application.routes.draw do
   get "r", to: redirect("users/rdvs", status: 301), as: "rdvs_short"
 
   # tkn est obligatoire pour s'assurer qu'il est possible de se connecter
-  get "r/:tkn" => "redirect#short_rdv_without_id", as: "rdv_short_no_id"
+  get "r/:tkn" => "redirect#rdv_short_without_id", as: "rdv_short_no_id"
 
   # On préserve la route courte avec id pour la rétrocompatibilité des anciens SMS
-  get "r/:id/:tkn", to: (redirect do |path_params, req|
-    query_params = format_redirect_params(req.params)
-    "users/rdvs/#{path_params[:id]}#{query_params}"
-  end), as: "rdv_short"
+  get "r/:id/:tkn" => "redirect#rdv_short", as: "rdv_short"
 
   get "prdv", to: (redirect do |_path_params, req|
     query_params = format_redirect_params(req.params)
