3a02b3461ceeb0f777703863b1a8f13616499ca6
Victor Mours <victor.mours@beta.gouv.fr>
2025-12-03 11:04:46 +0100

    Ajout des lieux fermés à la migration inter-instance (#5901)
    * Migrate disabled lieux as well
    
    * fix

--- app/models/instance_export.rb
+++ app/models/instance_export.rb
@@ -51,7 +51,7 @@ class InstanceExport < ApplicationRecord
           CopyAgentJob.perform_later(id, agent_id)
         end
 
-        source_organisation.lieux.enabled.pluck(:id).each do |lieu_id|
+        source_organisation.lieux.pluck(:id).each do |lieu_id|
           CopyLieuJob.perform_later(id, lieu_id)
         end
 
@@ -104,7 +104,7 @@ class InstanceExport < ApplicationRecord
     def perform(instance_export_id, lieu_id)
       instance_export = InstanceExport.find(instance_export_id)
       lieu = Lieu.find(lieu_id)
-      attributes = lieu.attributes.slice(*%w[name address latitude longitude phone_number])
+      attributes = lieu.attributes.slice(*%w[name address latitude longitude phone_number availability])
 
       attributes[:external_reference] = { external_id: lieu.id }
       attributes[:organisation_id] = instance_export.destination_organisation_id
