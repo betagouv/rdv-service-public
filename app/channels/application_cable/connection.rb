9938eb278eeaaf63f022a10be416813fb316999a
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-12-04 13:33:49 +0100

    Plus simple

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -9,28 +9,24 @@ module ApplicationCable
 
     private
 
-    # rubocop:disable Metrics/PerceivedComplexity
     def find_verified_agent
       session = cookies.encrypted[Rails.application.config.session_options[:key]]
       reject_unauthorized_connection and return unless session
 
       agent_id_from_session = session["warden.user.agent.key"]&.first&.first
 
-      # Débugging temporaire : on a un cookie de session, mais pas d'agent_id
-      unless agent_id_from_session
-        Redis.with_connection do |redis|
-          redis.lpush("actioncable_connection_debug", session.to_json)
-        end
-      end
-
       if agent_id_from_session
         Agent.find_by(id: agent_id_from_session) || reject_unauthorized_connection
       else
+        # Débugging temporaire : on a un cookie de session, mais pas d'agent_id
+        Redis.with_connection do |redis|
+          redis.lpush("actioncable_connection_debug", session.to_json)
+        end
+
         # On est par exemple dans ce cas lorsqu'un agent laisse son navigateur
         # ouvert toute la nuit et que le cookie a expiré le lendemain.
         reject_unauthorized_connection
       end
     end
-    # rubocop:enable Metrics/PerceivedComplexity
   end
 end
