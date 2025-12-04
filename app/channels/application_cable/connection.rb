857e7188e8d16faba515cb4936afc6fe80d53d73
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2025-12-04 15:24:42 +0100

    Déplace le commentaire au bon endroit

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,6 +11,9 @@ module ApplicationCable
 
     def find_verified_agent
       session = cookies.encrypted[Rails.application.config.session_options[:key]]
+
+      # On est par exemple dans ce cas lorsqu'un agent laisse son navigateur
+      # ouvert toute la nuit et que le cookie de session a expiré le lendemain.
       reject_unauthorized_connection and return unless session
 
       agent_id_from_session = session["warden.user.agent.key"]&.first&.first
@@ -22,9 +25,6 @@ module ApplicationCable
         Redis.with_connection do |redis|
           redis.lpush("actioncable_connection_debug", session.to_json)
         end
-
-        # On est par exemple dans ce cas lorsqu'un agent laisse son navigateur
-        # ouvert toute la nuit et que le cookie a expiré le lendemain.
         reject_unauthorized_connection
       end
     end
