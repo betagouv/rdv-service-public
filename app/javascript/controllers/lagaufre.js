e45b1c882771ed1530f92bd1f9a3043b25cd51ae
Antoine Girard <antoine.girard@beta.gouv.fr>
2025-12-05 13:02:16 +0100

    Première implémentation de la gaufre de la suite numérique

---
+++ app/javascript/controllers/lagaufre.js
@@ -0,0 +1,18 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  connect() {
+    const button = document.getElementById("gaufre_button");
+
+    window._lasuite_widget = window._lasuite_widget || [];
+
+    _lasuite_widget.push(['lagaufre', 'init', {
+      api: 'https://lasuite.numerique.gouv.fr/api/services',
+      background: 'linear-gradient(180deg, #eceffd 0%, #FFFFFF 20%)',
+      buttonElement: button,
+      position: 'fixed',
+      top: 80,
+      right: 10
+    }]);
+  }
+}
