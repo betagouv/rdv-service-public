04dc524781d734e19c76d94ad4d52d6c31a233da
Victor Mours <victor.mours@gmail.com>
2026-01-05 14:16:45 +0100

    Add failing spec for client-side logic

--- spec/lib/rdv_service_public_api_client_spec.rb
+++ spec/lib/rdv_service_public_api_client_spec.rb
@@ -12,15 +12,51 @@ RSpec.describe RdvServicePublicApiClient do
     end
 
     it "ajoute la requête et la réponse HTTP en breadcrumb Sentry et lève une exception" do
-      client = described_class.new("123456")
       expect do
-        client.post("plage_ouvertures", { lieu_external_ids: "asdf" })
+        described_class.new("123456").post("plage_ouvertures", { lieu_external_ids: "asdf" })
       end.to raise_error(Faraday::ResourceNotFound) do |e|
         Sentry.capture_exception(e)
         request_breadcrumb, response_breadcrumb = sentry_events.last.breadcrumbs.compact
         expect(request_breadcrumb.data[:body]).to eq({ lieu_external_ids: "asdf" }.to_json)
         expect(response_breadcrumb.data[:body]).to eq({ error_message: ["Aucun lieu trouvé pour le lieu_external_id 123456"] }.to_json)
       end
+    end
+  end
+
+  context "pour une requête qui échoue à cause d'une erreur métier" do
+    before do
+      stub_request(:post, "http://rdv.localhost/api/v1/motifs")
+        .to_return(
+          status: 422,
+          headers: { "Content-Type": "application/json" },
+          body: ApiSpecHelper.invalid_motif_response.to_json
+        )
+    end
+
+    it "ajoute la requête et la réponse HTTP en breadcrumb Sentry et lève une exception" do
+      expect do
+        described_class.new("123456").post("motifs", {}) # Les paramètres ne sont pas utilisés pas le stub, donc on ne les précise pas
+      end.to raise_error(Faraday::UnprocessableEntityError) do |e|
+        Sentry.capture_exception(e)
+        request_breadcrumb, response_breadcrumb = sentry_events.last.breadcrumbs.compact
+        expect(request_breadcrumb.data[:body]).to be_present
+        expect(response_breadcrumb.data[:body]).to be_present
+      end
+    end
+  end
+
+  context "pour une requête qui échoue parce que la ressource existe déjà" do
+    before do
+      stub_request(:post, "http://rdv.localhost/api/v1/motifs")
+        .to_return(
+          status: 422,
+          headers: { "Content-Type": "application/json" },
+          body: ApiSpecHelper.external_id_error_response.to_json
+        )
+    end
+
+    it "n'échoue pas, parce qu'on veut permettre de faire plusieurs fois des copies de données inter-instances sans doublons et sans lever d'erreur" do
+      described_class.new("123456").post("motifs", {}) # Les paramètres ne sont pas utilisés pas le stub, donc on ne les précise pas
     end
   end
 end
