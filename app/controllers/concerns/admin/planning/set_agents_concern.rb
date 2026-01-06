d5090e7fc8d9a8679f08ff7a400bc8859fb0abbd
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2026-01-06 16:30:12 +0100

    Retrouver facilement les derniers agents sélectionnés dans l'agenda (#5954)
    Retrouver facilement les derniers agents sélectionnés

--- app/controllers/concerns/admin/planning/set_agents_concern.rb
+++ app/controllers/concerns/admin/planning/set_agents_concern.rb
@@ -7,6 +7,8 @@ module Admin::Planning::SetAgentsConcern
     end
   end
 
+  private
+
   def set_agents
     scope = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope)
     agents = Agent.where(id: Array(params[:agent_id]).compact_blank)
@@ -20,7 +22,7 @@ module Admin::Planning::SetAgentsConcern
       @agents = [@agent]
     else
       if current_agent.feature_enabled?(Agent::FeatureFlags::NEW_PLANNING)
-        @agents = scope.where(id: agents)
+        @agents = scope.where(id: agents).load
       else
         # Si l'agent courant n'a pas activé la feature on ne considère qu'il n'y
         # a qu'un seul agent sélectionné, car le code sera en mode mono-agent.
@@ -28,5 +30,24 @@ module Admin::Planning::SetAgentsConcern
         @agents = [@agent]
       end
     end
+
+    if @agents.size > 1
+      store_selected_agents(@agents)
+    else
+      @previously_selected_agents = previously_selected_agents
+    end
+  end
+
+  def store_selected_agents(agents)
+    Redis.with_connection { _1.set(selected_agents_cache_key, agents.map(&:id).join(","), ex: 10.days) }
+  end
+
+  def previously_selected_agents
+    stored_ids = Redis.with_connection { _1.get(selected_agents_cache_key) }&.split(",").presence
+    Agent::AgentPolicy::Scope.apply(current_agent, Agent.all).where(id: stored_ids) if stored_ids
+  end
+
+  def selected_agents_cache_key
+    "previously_selected_agent_ids:#{current_agent.id}:#{current_organisation.id}"
   end
 end
