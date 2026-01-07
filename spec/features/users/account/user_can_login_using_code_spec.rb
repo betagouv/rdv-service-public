0f25c67397f181c0ffaa28f9b9a33c0349db35ea
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-01-07 08:10:41 +0100

    nouvelle verif qu’aucun code n’est créé

--- spec/features/users/account/user_can_login_using_code_spec.rb
+++ spec/features/users/account/user_can_login_using_code_spec.rb
@@ -27,7 +27,7 @@ RSpec.describe "Un usager peut se logger via un code à 6 chiffres" do
     specify do
       visit new_user_session_path
       fill_in "Adresse email", with: "nina@personne.fr"
-      click_on "Recevoir un code de connexion"
+      expect { click_on "Recevoir un code de connexion" }.not_to change(LoginCode, :count)
       expect(page).to have_content("Aucun compte usager n’existe pour cet email")
       expect(page).not_to have_content("code à 6 chiffres")
     end
