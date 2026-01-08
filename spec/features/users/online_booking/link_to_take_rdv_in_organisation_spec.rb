fa723060a9914e462d23eb0e745bd6aa6b9faa7d
Victor Mours <victor.mours@beta.gouv.fr>
2026-01-08 10:44:59 +0100

    Update spec/features/users/online_booking/link_to_take_rdv_in_organisation_spec.rb
    Co-authored-by: François Ferrandis <francois.ferrandis@beta.gouv.fr>

--- spec/features/users/online_booking/link_to_take_rdv_in_organisation_spec.rb
+++ spec/features/users/online_booking/link_to_take_rdv_in_organisation_spec.rb
@@ -120,6 +120,7 @@ RSpec.describe "user can use a link that points to RDV search scoped to an organ
 
     it "still works for links that have been copied before" do
       visit "/org/#{motif.organisation_id}/mds-paris-nord?motif_id=#{motif.id}"
+      expect(page).to have_content "Motif : #{motif.name}"
       expect(page).to have_content "Sélectionnez un lieu de RDV"
     end
   end
