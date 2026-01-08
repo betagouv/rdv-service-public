8517fc98aabcd21f9444022f35f8dd3d09255cd2
Victor Mours <victor.mours@gmail.com>
2026-01-08 15:24:58 +0100

    move specs

--- spec/requests/api/v1/authentication/devise_token_auth_spec.rb
+++ spec/requests/api/v1/authentication/devise_token_auth_spec.rb
@@ -1,4 +1,4 @@
-RSpec.describe "API auth", type: :request do
+RSpec.describe "API authentication" do
   # inspired by https://devise-token-auth.gitbook.io/devise-token-auth/usage/testing
 
   let!(:organisation) { create(:organisation) }
@@ -65,64 +65,6 @@ RSpec.describe "API auth", type: :request do
           "access-token": response.headers["access-token"],
           client: response.headers["client"],
           uid: response.headers["uid"],
-        }
-      )
-      expect(response.status).to eq(200)
-      expect(parsed_response_body["absences"].count).to eq(1)
-    end
-  end
-
-  context "with agent shared secret auth" do
-    let!(:payload) do
-      {
-        id: agent.id,
-        first_name: agent.first_name,
-        last_name: agent.last_name,
-        email: agent.email,
-      }
-    end
-
-    before do
-      allow(ENV).to receive(:fetch).with("SHARED_SECRET_FOR_AGENTS_AUTH").and_return("S3cr3T")
-    end
-
-    it "log sentry and return error when shared secret is invalid" do
-      get(
-        api_v1_absences_path,
-        headers: {
-          uid: agent.email,
-          "X-Agent-Auth-Signature": "BAD_PAYLOAD",
-        }
-      )
-      expect(response).to have_http_status(:unauthorized)
-      expect(parsed_response_body).to eq({ "errors" => ["Vous devez vous connecter ou vous inscrire pour continuer."] })
-      expect(sentry_events.last.message).to eq("API authentication agent was called with an invalid signature !")
-    end
-
-    it "log sentry and return error when shared secret is nil" do
-      get(
-        api_v1_absences_path,
-        headers: {
-          uid: agent.email,
-          "X-Agent-Auth-Signature": nil,
-        }
-      )
-      expect(response).to have_http_status(:unauthorized)
-      expect(parsed_response_body).to eq({ "errors" => ["Vous devez vous connecter ou vous inscrire pour continuer."] })
-      expect(sentry_events.last.message).to eq("API authentication agent was called with an invalid signature !")
-    end
-
-    it "query is correctly processed with the agent authorizations when shared secret is valid" do
-      # Pour une raison inconnue, sentry_events n'est pas vide à la fin de ce test
-      # donc nous avons recours à ce "expect.to receive".
-      expect(Sentry).not_to receive(:capture_message)
-
-      encrypted_payload = OpenSSL::HMAC.hexdigest("SHA256", "S3cr3T", payload.to_json)
-      get(
-        api_v1_absences_path,
-        headers: {
-          uid: agent.email,
-          "X-Agent-Auth-Signature": encrypted_payload,
         }
       )
       expect(response.status).to eq(200)
