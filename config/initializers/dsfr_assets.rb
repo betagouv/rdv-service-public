d66e9a7a852c646bf9576c427f0faf5a72aaf033
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-01-20 12:54:45 +0100

    Review

--- config/initializers/dsfr_assets.rb
+++ config/initializers/dsfr_assets.rb
@@ -1,7 +1,7 @@
 # Crée un lien symbolique vers les pictogrammes de la gem dsfr-assets.
-# Ces fichiers SVG utilisent des fragments (#artwork-decorative, etc.)
-# et ne sont pas compatibles avec le fingerprinting Sprockets.
-# On les sert donc statiquement via public/assets/artwork.
+# Ces fichiers SVG n’étant pas inclus dans les assets précompilés par Rails,
+# on les sert donc statiquement via public/assets/artwork.
+# Il s’agit d’une solution quick-win, car nous allons certainement nous passer de Sprockets à terme.
 
 gem_spec = Gem.loaded_specs["dsfr-assets"]
 return unless gem_spec
@@ -9,18 +9,10 @@ return unless gem_spec
 artwork_source = File.join(gem_spec.full_gem_path, "vendor/assets/stylesheets/artwork")
 artwork_dest = Rails.public_path.join("assets/artwork")
 
-if File.symlink?(artwork_dest)
-  # Vérifie que le symlink pointe vers le bon endroit
-  unless File.readlink(artwork_dest) == artwork_source
-    FileUtils.rm(artwork_dest)
-    FileUtils.ln_s(artwork_source, artwork_dest)
-  end
-elsif File.exist?(artwork_dest)
-  # Si c'est un dossier/fichier réel, on le supprime et recrée le symlink
-  FileUtils.rm_rf(artwork_dest)
-  FileUtils.ln_s(artwork_source, artwork_dest)
+if File.symlink?(artwork_dest) && File.readlink(artwork_dest) == artwork_source
+  # do nothing
 else
-  # Crée le dossier parent si nécessaire
   FileUtils.mkdir_p(File.dirname(artwork_dest))
+  FileUtils.rm_rf(artwork_dest)
   FileUtils.ln_s(artwork_source, artwork_dest)
 end
