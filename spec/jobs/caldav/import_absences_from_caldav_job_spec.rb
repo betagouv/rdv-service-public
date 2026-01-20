d4797c9cd2e591aa2d8e27ce85c103e0b9503e40
François Ferrandis <francois@ferrandis.cool>
2026-01-20 17:48:38 +0100

    Complète la spec d'import

--- spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
+++ spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
@@ -8,8 +8,23 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
         perform_enqueued_jobs
       end.to change(ExternalCalendarEvent, :count).by(2)
 
-      url = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id/fa75d9fe-2063-465e-a323-dd8ae7589746.ics"
-      expect(ExternalCalendarEvent.find_by!(url:)).to have_attributes(agent:, starts_at: Time.zone.parse("2026-01-13 09:45:00"), ends_at: Time.zone.parse("2026-01-13 10:15:00"))
+      # TODO: gérer les récurrences
+      daily = {
+        agent:,
+        starts_at: Time.zone.parse("2026-01-13 09:45:00"),
+        ends_at: Time.zone.parse("2026-01-13 10:15:00"),
+        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id/fa75d9fe-2063-465e-a323-dd8ae7589746.ics",
+      }
+      expect(ExternalCalendarEvent.find_by(url: daily[:url])).to have_attributes(**daily)
+
+      # TODO: gérer les récurrences
+      weekly = {
+        agent:,
+        starts_at: Time.zone.parse("2026-01-13 11:00:00"),
+        ends_at: Time.zone.parse("2026-01-13 12:00:00"),
+        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics",
+      }
+      expect(ExternalCalendarEvent.find_by(url: weekly[:url])).to have_attributes(**weekly)
     end
   end
 end
