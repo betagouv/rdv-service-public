45dfcb991fdf206baaf0b83074e7932839c96127
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-01-21 08:47:31 +0100

    Add comment on idp_id column

--- db/schema.rb
+++ db/schema.rb
@@ -155,7 +155,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_21_064312) do
     t.string "caldav_sync_token"
     t.boolean "pro_connect_2fa_active"
     t.boolean "group_by_agent", default: false, null: false
-    t.string "pro_connect_idp_id"
+    t.string "pro_connect_idp_id", comment: "Fournisseur d'identité ProConnect (identity provider)"
     t.index ["account_deletion_warning_sent_at"], name: "index_agents_on_account_deletion_warning_sent_at"
     t.index ["calendar_uid"], name: "index_agents_on_calendar_uid", unique: true
     t.index ["confirmation_token"], name: "index_agents_on_confirmation_token", unique: true
