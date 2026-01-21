d897181ca16e8b4aa0d50b3c067748665ebf2692
François Ferrandis <francois@ferrandis.cool>
2026-01-21 10:52:17 +0100

    Scrub cassettes automatically

--- spec/factories/agent.rb
+++ spec/factories/agent.rb
@@ -109,9 +109,9 @@ FactoryBot.define do
     end
 
     trait :with_caldav_config do
-      caldav_agenda_url { "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id" }
-      caldav_username { "francis.factice.ext@beta.gouv.fr" }
-      caldav_password { "1234" }
+      caldav_agenda_url { "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234" }
+      caldav_username { "francis.factice@beta.gouv.fr" }
+      caldav_password { "mot_de_passe_factice" }
     end
   end
 end
--- spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
+++ spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
@@ -1,6 +1,11 @@
 RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
   it "works (creates local ExternalCalendarEvent rows for each event)" do
-    agent = create(:agent, :with_caldav_config)
+    agent = create(
+      :agent,
+      caldav_agenda_url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/Y2FsOi8vMC8xMzk2Ng",
+      caldav_username: "francois.ferrandis.ext@beta.gouv.fr",
+      caldav_password: "fake_password"
+    )
 
     VCR.use_cassette("caldav/daily_and_weekly") do
       expect do
@@ -13,7 +18,7 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
         agent:,
         starts_at: Time.zone.parse("2026-01-13 09:45:00"),
         ends_at: Time.zone.parse("2026-01-13 10:15:00"),
-        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id/fa75d9fe-2063-465e-a323-dd8ae7589746.ics",
+        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/Y2FsOi8vMC8xMzk2Ng/fa75d9fe-2063-465e-a323-dd8ae7589746.ics",
       }
       expect(ExternalCalendarEvent.find_by(url: daily[:url])).to have_attributes(**daily)
 
@@ -22,7 +27,7 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
         agent:,
         starts_at: Time.zone.parse("2026-01-13 11:00:00"),
         ends_at: Time.zone.parse("2026-01-13 12:00:00"),
-        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/abcdef_this_is_calendar_id/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics",
+        url: "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/Y2FsOi8vMC8xMzk2Ng/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics",
       }
       expect(ExternalCalendarEvent.find_by(url: weekly[:url])).to have_attributes(**weekly)
     end
