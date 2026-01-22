a3e0ae97638ee04cd849b67369afd799037faac7
François Ferrandis <francois@ferrandis.cool>
2026-01-22 13:33:05 +0100

    Séparer dans Admin::Api::Agenda::ExternalCalendarEventsController

--- app/controllers/admin/api/agenda/absences_controller.rb
+++ app/controllers/admin/api/agenda/absences_controller.rb
@@ -8,29 +8,5 @@ class Admin::Api::Agenda::AbsencesController < Admin::Api::BaseController
     agents = Agent::AgentPolicy::Scope.new(pundit_user, Agent.all).resolve.where(id: params[:agent_id]).load
 
     @absence_occurrences = Absence.where(agent: agents).all_occurrences_for(date_range_params)
-    @external_calendar_occurrences = external_event_occurrences(agents)
-  end
-
-  private
-
-  def external_event_occurrences(agents)
-    external_calendar_events = ExternalCalendarEvent
-      .within_range(time_range_params)
-      .where(agent: agents.select(&:caldav_configured?))
-
-    occurrences_hashes = []
-    external_calendar_events.each do |event|
-      event.all_occurrences_within(time_range_params).each do |occurrence|
-        occurrences_hashes << {
-          title: "Indisponibilité provenant d’un agenda externe",
-          start: occurrence.starts_at.as_json,
-          end: occurrence.ends_at.as_json,
-          backgroundColor: AbsencesHelper::CALENDAR_BACKGROUND_COLOR,
-          resourceIds: [event.agent_id], # https://fullcalendar.io/docs/resources-and-events
-        }
-      end
-    end
-
-    occurrences_hashes
   end
 end
---
+++ app/controllers/admin/api/agenda/external_calendar_events_controller.rb
@@ -0,0 +1,35 @@
+class Admin::Api::Agenda::ExternalCalendarEventsController < Admin::Api::BaseController
+  def index
+    external_calendar_events = ExternalCalendarEvent
+      .within_range(time_range_params)
+      .where(agent: agents.select(&:caldav_configured?))
+
+    external_calendar_occurrences = []
+    external_calendar_events.each do |event|
+      event.all_occurrences_within(time_range_params).each do |occurrence|
+        external_calendar_occurrences << {
+          title: "Indisponibilité provenant d’un agenda externe",
+          start: occurrence.starts_at.as_json,
+          end: occurrence.ends_at.as_json,
+          backgroundColor: AbsencesHelper::CALENDAR_BACKGROUND_COLOR,
+          resourceIds: [event.agent_id], # https://fullcalendar.io/docs/resources-and-events
+        }
+      end
+    end
+
+    render json: external_calendar_occurrences
+  end
+
+  private
+
+  def agents
+    if params[:agent_id].to_i == current_agent.id
+      [current_agent]
+    else
+      # La scope de policy pour ExternalCalendarEvent délèguent à Agent::AgentPolicy::Scope.
+      # Afin d'améliorer les perfs ici, il est préférable de simplement charger les agents via Agent::AgentPolicy::Scope
+      # puis de passer cette liste d'agents en WHERE aux requêtes d'Absence et ExternalCalendarEvent.
+      Agent::AgentPolicy::Scope.new(pundit_user, Agent.all).resolve.where(id: params[:agent_id]).load
+    end
+  end
+end
