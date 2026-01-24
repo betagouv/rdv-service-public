5e554ce25da86f368d3e0e29edf752897faadd88
François Ferrandis <francois@ferrandis.cool>
2026-01-24 19:11:29 +0100

    Refactor import_absences_from_caldav_job.rb for clarity

--- app/jobs/caldav/import_absences_from_caldav_job.rb
+++ app/jobs/caldav/import_absences_from_caldav_job.rb
@@ -18,71 +18,71 @@ module Caldav
     # Pour comprendre l'usage de la gem Calendav, voir la doc très claire :
     # https://github.com/pat/calendav?tab=readme-ov-file#synchronising
     #
-    # rubocop:disable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
     def perform(agent_id)
-      @agent = Agent.find_by_id(agent_id)
-      return unless @agent&.caldav_configured? || @agent&.caldav_disconnect_in_progress?
+      agent = Agent.find(agent_id)
+      Sentry.set_user({ id: agent.id, role: "Agent", email: agent.email })
+      return unless agent.caldav_configured? || agent.caldav_disconnect_in_progress?
 
-      Sentry.set_user({ id: @agent.id, role: "Agent", email: @agent.email })
-
-      if @agent.caldav_sync_token
-        collection = @agent.caldav_client.calendars.sync(@agent.caldav_agenda_url, @agent.caldav_sync_token)
-
-        # Le serveur Caldav de la Suite Numérique signale une suppression à travers un calendar_data vide.
-        updated_events = collection.changes.select(&:calendar_data)
-        deleted_events = collection.changes.reject(&:calendar_data).map(&:url)
-
-        # D'autres serveurs Caldav peuvent utiliser le tableau `deletions` pour signaler une suppression
-        deleted_events += collection.deletions
-
-        update_local_data(updated_events:, deleted_events:, new_sync_token: collection.sync_token)
+      if agent.caldav_sync_token
+        updated_events, deleted_events, new_sync_token = changes_since_last_sync_of(agent:)
       else
-        new_sync_token = @agent.caldav_client.calendars.find(@agent.caldav_agenda_url, sync: true).sync_token
-        updated_events = @agent.caldav_client.events.list(@agent.caldav_agenda_url)
-        deleted_events = []
-        update_local_data(updated_events:, deleted_events:, new_sync_token:)
+        updated_events, deleted_events, new_sync_token = all_events_for(agent:)
       end
 
-      # Import successful
-      set_debounce
-      live_update_calendar if updated_events.any? || deleted_events.any?
+      update_local_events_of(agent:, updated_events:, deleted_events:, new_sync_token:)
+
+      # Import successful: set job debounce and update realtime calendars
+      Redis.with_connection { |redis| redis.set("caldav_sync_absences_job_debounce_#{agent_id}", true, ex: 1.minute) }
+      AgendaChannel.broadcast_to(agent_id, model: "ExternalCalendarEvent")
     end
-    # rubocop:enable Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
     private
 
-    def update_local_data(updated_events:, deleted_events:, new_sync_token:)
+    def changes_since_last_sync_of(agent:)
+      collection = agent.caldav_client.calendars.sync(agent.caldav_agenda_url, agent.caldav_sync_token)
+      new_sync_token = collection.sync_token
+
+      # Le serveur Caldav de la Suite Numérique signale une suppression à travers un calendar_data vide.
+      updated_events = collection.changes.select(&:calendar_data)
+      deleted_events = collection.changes.reject(&:calendar_data).map(&:url)
+
+      # D'autres serveurs Caldav peuvent utiliser le tableau `deletions` pour signaler une suppression
+      deleted_events += collection.deletions
+
+      [updated_events, deleted_events, new_sync_token]
+    end
+
+    def all_events_for(agent:)
+      new_sync_token = agent.caldav_client.calendars.find(agent.caldav_agenda_url, sync: true).sync_token
+      updated_events = agent.caldav_client.events.list(agent.caldav_agenda_url)
+      deleted_events = []
+      [updated_events, deleted_events, new_sync_token]
+    end
+
+    def update_local_events_of(agent:, updated_events:, deleted_events:, new_sync_token:)
       # On exclut le traitement des événements provenant d'un RDV de chez nous
       urls_of_rdvs = AgentsRdv.where(caldav_url: updated_events.map(&:url)).pluck(:caldav_url)
       updated_events = updated_events.reject { _1.url == urls_of_rdvs }
 
       ExternalCalendarEvent.transaction do
-        updated_events.each { |event| upsert_event(event) }
+        hashes_to_upsert = updated_events.map do |event|
+          recurring = event.send(:inner_event).rrule&.first&.valid?
+          raw_ical = recurring ? Ical::Scrubber.new(event.calendar_data).scrubbed : nil
+          {
+            agent_id: agent.id,
+            url: event.url,
+            starts_at: event.dtstart,
+            ends_at: event.dtend,
+            raw_ical:,
+          }
+        end
 
-        ExternalCalendarEvent.where(agent: @agent, url: deleted_events).delete_all if deleted_events.any?
+        ExternalCalendarEvent.upsert_all(hashes_to_upsert, unique_by: :index_external_calendar_events_on_agent_id_and_url) # rubocop:disable Rails/SkipsModelValidations
 
-        @agent.update_columns(caldav_sync_token: new_sync_token) # rubocop:disable Rails/SkipsModelValidations
+        ExternalCalendarEvent.where(agent: agent, url: deleted_events).delete_all if deleted_events.any?
+
+        agent.update_columns(caldav_sync_token: new_sync_token) # rubocop:disable Rails/SkipsModelValidations
       end
-    end
-
-    def set_debounce
-      Redis.with_connection { |redis| redis.set("caldav_sync_absences_job_debounce_#{@agent.id}", true, ex: 1.minute) }
-    end
-
-    def live_update_calendar
-      AgendaChannel.broadcast_to(@agent.id, model: "ExternalCalendarEvent")
-    end
-
-    def upsert_event(event)
-      recurring = event.send(:inner_event).rrule&.first&.valid?
-
-      e = ExternalCalendarEvent.find_or_initialize_by(agent: @agent, url: event.url)
-      e.assign_attributes(
-        starts_at: event.dtstart,
-        ends_at: event.dtend,
-        raw_ical: recurring ? event.calendar_data : nil
-      )
-      e.save!
     end
   end
 end
