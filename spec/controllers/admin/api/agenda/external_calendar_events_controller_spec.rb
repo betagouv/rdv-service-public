6aa74f395cbf0ab35e8401d6ec1077853671c662
François Ferrandis <francois@ferrandis.cool>
2026-01-25 14:50:04 +0100

    Check range

--- spec/controllers/admin/api/agenda/external_calendar_events_controller_spec.rb
+++ spec/controllers/admin/api/agenda/external_calendar_events_controller_spec.rb
@@ -5,27 +5,29 @@ RSpec.describe Admin::Api::Agenda::ExternalCalendarEventsController, type: :cont
     agent = create(:agent, :with_caldav_config)
     sign_in agent
 
-    external_event = ExternalCalendarEvent.create!(
+    event_within_time_range = ExternalCalendarEvent.create!(
       agent:,
       url: "1234",
       starts_at: Time.zone.today.at(Tod::TimeOfDay.parse("10:30")),
       ends_at: Time.zone.today.at(Tod::TimeOfDay.parse("12:00"))
     )
 
-    start_date = Time.zone.today.monday
-    end_date = start_date.end_of_week
-    get :index, params: { agent_id: agent.id, start: start_date, end: end_date, format: :json }
+    # Event out of time range, should not be in response
+    ExternalCalendarEvent.create!(agent:, url: "5678", starts_at: 10.days.from_now, ends_at: 10.days.from_now + 2.hours)
+
+    current_week = Time.zone.now.beginning_of_week.beginning_of_day..Time.zone.now.end_of_week.end_of_day
+    get :index, params: { agent_id: agent.id, start: current_week.begin, end: current_week.end, format: :json }
 
     expected_response = [
       {
         "title" => "Indisponibilité provenant d’un agenda externe",
-        "start" => external_event.starts_at.as_json,
-        "end" => external_event.ends_at.as_json,
-        "resourceIds" => [external_event.agent.id],
+        "start" => event_within_time_range.starts_at.as_json,
+        "end" => event_within_time_range.ends_at.as_json,
+        "resourceIds" => [event_within_time_range.agent.id],
         "backgroundColor" => "rgba(52, 57, 58, 0.7)",
       },
     ]
-    expect(response.parsed_body).to eq(expected_response)
+    expect(response.parsed_body).to match_array(expected_response)
   end
 
   it "returns unauthorized if agent is not logged in" do
