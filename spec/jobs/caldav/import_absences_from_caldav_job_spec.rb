5628ce2775db05d02f6ec81556bc811ed5eafdd5
François Ferrandis <francois@ferrandis.cool>
2026-01-26 10:39:12 +0100

    Refactor cassettes

--- spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
+++ spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
@@ -1,10 +1,17 @@
 RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
   let(:agent) { create(:agent, :with_caldav_config) }
 
+  around do |example|
+    VCR.use_cassette("caldav/token_via_propfind", allow_playback_repeats: true) do
+      example.run
+    end
+  end
+
   context "when agent does not have a token and DB is empty" do
+
     it "works creates local ExternalCalendarEvent row for each event and saves token" do
       described_class.perform_later(agent.id)
-      VCR.use_cassette("caldav/initial_get_token_and_event_list") do
+      VCR.use_cassette("caldav/event_list_weekly_and_daily") do
         expect { perform_enqueued_jobs }.to(
           change(ExternalCalendarEvent, :count).by(2).and(
             change { agent.reload.caldav_sync_token }.from(nil)
@@ -41,16 +48,16 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
     url_of_legit_event = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics"
     create(:agents_rdv, agent:, caldav_url: url_of_local_event)
 
-    VCR.use_cassette("caldav/initial_get_token_and_event_list") do
-      expect { described_class.perform_now(agent.id) }.to change(ExternalCalendarEvent, :count).by(1)
+    VCR.use_cassette("caldav/event_list_weekly_and_daily") do
+      expect { described_class.new.perform(agent.id) }.to change(ExternalCalendarEvent, :count).by(1)
     end
 
     expect(ExternalCalendarEvent.pluck(:url)).to eq([url_of_legit_event])
   end
 
   it "prevents from enqueuing the same job if it ran less than a minute ago" do
-    VCR.use_cassette("caldav/initial_get_token_and_event_list", allow_playback_repeats: true) do
-      described_class.perform_now(agent.id)
+    VCR.use_cassette("caldav/event_list_weekly_and_daily", allow_playback_repeats: true) do
+      described_class.new.perform(agent.id)
       travel_to(10.seconds.from_now) { expect { described_class.perform_later(agent.id) }.not_to have_enqueued_job }
       travel_to(50.seconds.from_now) { expect { described_class.perform_later(agent.id) }.not_to have_enqueued_job }
       travel_to(70.seconds.from_now) { expect { described_class.perform_later(agent.id) }.to have_enqueued_job(described_class).with(agent.id) }
@@ -61,14 +68,14 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
     agent_a = create(:agent, :with_caldav_config)
     agent_b = create(:agent, :with_caldav_config)
 
-    # On exécute un premier job pour un agent donné
+    # On enqueue un premier job pour un agent donné
     described_class.perform_later(agent_a.id)
 
     # On prévoit d'exécuter dans 2 secondes deux jobs: l'un pour le même agent et l'autre pour un agent différent.
     described_class.new(agent_a.id).enqueue(wait_until: 2.seconds.from_now)
     described_class.new(agent_b.id).enqueue(wait_until: 2.seconds.from_now)
 
-    VCR.use_cassette("caldav/initial_get_token_and_event_list", allow_playback_repeats: true) do
+    VCR.use_cassette("caldav/event_list_weekly_and_daily", allow_playback_repeats: true) do
       # On exécute le premier job uniquement, le second reste dans la queue
       perform_enqueued_jobs(at: Time.zone.now)
       expect(enqueued_jobs.size).to eq(2)
