fb3ac41c4d5a4d9969ab065f0a19319723c17995
François Ferrandis <francois@ferrandis.cool>
2026-01-26 10:48:56 +0100

    Add a cassette for "no event", to test basic job behaviour

--- spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
+++ spec/jobs/caldav/import_absences_from_caldav_job_spec.rb
@@ -1,18 +1,16 @@
 RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
   let(:agent) { create(:agent, :with_caldav_config) }
 
-  around do |example|
-    VCR.use_cassette("caldav/token_via_propfind", allow_playback_repeats: true) do
-      example.run
-    end
-  end
-
   context "when agent does not have a token and DB is empty" do
+    around do |example|
+      VCR.use_cassette("caldav/token_via_propfind", allow_playback_repeats: true) do
+        example.run
+      end
+    end
 
     it "works creates local ExternalCalendarEvent row for each event and saves token" do
-      described_class.perform_later(agent.id)
       VCR.use_cassette("caldav/event_list_weekly_and_daily") do
-        expect { perform_enqueued_jobs }.to(
+        expect { described_class.new.perform(agent.id) }.to(
           change(ExternalCalendarEvent, :count).by(2).and(
             change { agent.reload.caldav_sync_token }.from(nil)
           )
@@ -41,52 +39,60 @@ RSpec.describe Caldav::ImportAbsencesFromCaldavJob do
         expect { described_class.new.perform(agent.id) }.not_to change(ExternalCalendarEvent, :count)
       end
     end
+
+    it "does not create events if the external URL corresponds to a local Rdv" do
+      url_of_local_event = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/fa75d9fe-2063-465e-a323-dd8ae7589746.ics"
+      url_of_legit_event = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics"
+      create(:agents_rdv, agent:, caldav_url: url_of_local_event)
+
+      VCR.use_cassette("caldav/event_list_weekly_and_daily") do
+        expect { described_class.new.perform(agent.id) }.to change(ExternalCalendarEvent, :count).by(1)
+      end
+
+      expect(ExternalCalendarEvent.pluck(:url)).to eq([url_of_legit_event])
+    end
   end
 
-  it "does not create events if the external URL corresponds to a local Rdv" do
-    url_of_local_event = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/fa75d9fe-2063-465e-a323-dd8ae7589746.ics"
-    url_of_legit_event = "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/6a54e0b7-93cf-43e4-a854-afd8e3d3f2c4.ics"
-    create(:agents_rdv, agent:, caldav_url: url_of_local_event)
-
-    VCR.use_cassette("caldav/event_list_weekly_and_daily") do
-      expect { described_class.new.perform(agent.id) }.to change(ExternalCalendarEvent, :count).by(1)
+  describe "job debounce" do
+    around do |example|
+      VCR.use_cassette("caldav/token_via_propfind", allow_playback_repeats: true) do
+        VCR.use_cassette("caldav/no_event", allow_playback_repeats: true) do
+          example.run
+        end
+      end
     end
 
-    expect(ExternalCalendarEvent.pluck(:url)).to eq([url_of_legit_event])
-  end
-
-  it "prevents from enqueuing the same job if it ran less than a minute ago" do
-    VCR.use_cassette("caldav/event_list_weekly_and_daily", allow_playback_repeats: true) do
+    it "prevents from enqueuing the same job if it ran less than a minute ago" do
       described_class.new.perform(agent.id)
       travel_to(10.seconds.from_now) { expect { described_class.perform_later(agent.id) }.not_to have_enqueued_job }
       travel_to(50.seconds.from_now) { expect { described_class.perform_later(agent.id) }.not_to have_enqueued_job }
       travel_to(70.seconds.from_now) { expect { described_class.perform_later(agent.id) }.to have_enqueued_job(described_class).with(agent.id) }
     end
-  end
 
-  it "prevents from performing the same job if it ran less than a minute ago" do
-    agent_a = create(:agent, :with_caldav_config)
-    agent_b = create(:agent, :with_caldav_config)
+    it "prevents from performing the same job if it ran less than a minute ago" do
+      agent_a = create(:agent, :with_caldav_config)
+      agent_b = create(:agent, :with_caldav_config)
 
-    # On enqueue un premier job pour un agent donné
-    described_class.perform_later(agent_a.id)
+      # On enqueue un premier job pour un agent donné
+      described_class.perform_later(agent_a.id)
 
-    # On prévoit d'exécuter dans 2 secondes deux jobs: l'un pour le même agent et l'autre pour un agent différent.
-    described_class.new(agent_a.id).enqueue(wait_until: 2.seconds.from_now)
-    described_class.new(agent_b.id).enqueue(wait_until: 2.seconds.from_now)
+      # On prévoit d'exécuter dans 2 secondes deux jobs: l'un pour le même agent et l'autre pour un agent différent.
+      described_class.new(agent_a.id).enqueue(wait_until: 2.seconds.from_now)
+      described_class.new(agent_b.id).enqueue(wait_until: 2.seconds.from_now)
 
-    VCR.use_cassette("caldav/event_list_weekly_and_daily", allow_playback_repeats: true) do
-      # On exécute le premier job uniquement, le second reste dans la queue
-      perform_enqueued_jobs(at: Time.zone.now)
-      expect(enqueued_jobs.size).to eq(2)
+      VCR.use_cassette("caldav/event_list_weekly_and_daily", allow_playback_repeats: true) do
+        # On exécute le premier job uniquement, le second reste dans la queue
+        perform_enqueued_jobs(at: Time.zone.now)
+        expect(enqueued_jobs.size).to eq(2)
 
-      travel_to(3.seconds.from_now) do
-        # Le job pour le même agent ne s'exécute pas
-        expect(Agent).not_to receive(:find).with(agent_a.id)
-        # Le job pour l'autre agent s'exécute sans souci
-        expect(Agent).to receive(:find).once.with(agent_b.id).and_call_original
-        perform_enqueued_jobs
-        expect(enqueued_jobs).to be_empty
+        travel_to(3.seconds.from_now) do
+          # Le job pour le même agent ne s'exécute pas
+          expect(Agent).not_to receive(:find).with(agent_a.id)
+          # Le job pour l'autre agent s'exécute sans souci
+          expect(Agent).to receive(:find).once.with(agent_b.id).and_call_original
+          perform_enqueued_jobs
+          expect(enqueued_jobs).to be_empty
+        end
       end
     end
   end
