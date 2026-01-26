a92ddbe7c59ac8ff0c40fb967789d6e757e1552a
François Ferrandis <francois@ferrandis.cool>
2026-01-26 18:32:50 +0100

    Fix factory

--- spec/factories/external_calendar_event.rb
+++ spec/factories/external_calendar_event.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :external_calendar_event do
+    agent { association(:agent) }
     url { "https://ox8-oidc.ox8-oidc.osprod.dimail1.numerique.gouv.fr/dav/caldav/1234_calendar_id/#{SecureRandom.uuid}.ics" }
     starts_at { Time.zone.today.at(Tod::TimeOfDay.parse("09:00")) }
     ends_at { Time.zone.today.at(Tod::TimeOfDay.parse("11:00")) }
