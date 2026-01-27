275ef0332f23ca255988a6f9cb7a7c3f19d19072
François Ferrandis <francois@ferrandis.cool>
2026-01-27 14:02:24 +0100

    Ajoute l'index dans une migration séparée

--- db/migrate/20260121132127_add_raw_ical_to_external_calendar_events.rb
+++ db/migrate/20260121132127_add_raw_ical_to_external_calendar_events.rb
@@ -1,19 +1,15 @@
 class AddRawIcalToExternalCalendarEvents < ActiveRecord::Migration[8.0]
   def change
-    up_only do
-      ExternalCalendarEvent.delete_all
-
-      Agent.where.not(caldav_agenda_url: nil).each do |agent|
-        agent.update!(caldav_sync_token: nil)
-        Caldav::ImportAbsencesFromCaldavJob.perform_later(agent.id)
-      end
-    end
+    # On vide les données en début de migration pour les re-remplir à la fin (en up comme en down)
+    agents_with_caldav = Agent.where.not(caldav_agenda_url: nil)
+    agents_with_caldav.update_all(caldav_sync_token: nil)
+    ExternalCalendarEvent.delete_all
 
     add_column :external_calendar_events, :raw_ical, :text
 
-    # Au passage, j'avais oublié cet index lors de la création de la table.
-    safety_assured do # La table contient au plus 2000 lignes, ce n'est pas grave de bloquer l'écriture pendant quelques ms.
-      add_index :external_calendar_events, %i[agent_id url], unique: true
+    # On re-remplit les données via les jobs asynchrones
+    agents_with_caldav.each do |agent|
+      Caldav::ImportAbsencesFromCaldavJob.perform_later(agent.id)
     end
   end
 end
