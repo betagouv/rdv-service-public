ecaa8854482d383123fddde14c0f549a060f0218
Victor Mours <victor.mours@gmail.com>
2026-01-27 18:10:57 +0100

    fix

--- app/controllers/api/v1/rdvs_controller.rb
+++ app/controllers/api/v1/rdvs_controller.rb
@@ -10,7 +10,6 @@ class Api::V1::RdvsController < Api::V1::AgentAuthBaseController
     if params[:include].nil?
       rdvs = rdvs.includes(:organisation, :lieu, :agents, :users, participations: [:user], motif: [:motif_category])
     elsif params[:include].is_a?(Array)
-
       rdvs.includes!(:organisation) if "organisation".in?(params[:include])
 
       rdvs.includes!(:lieu) if "lieu".in?(params[:include])
