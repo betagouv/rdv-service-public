c75bba0046b65d1db91a79f5607806c6968d1ded
Victor Mours <victor.mours@gmail.com>
2026-02-02 11:18:29 +0100

    Merge edit and show actions to allow restarting migration

--- app/models/instance_export.rb
+++ app/models/instance_export.rb
@@ -10,7 +10,7 @@ class InstanceExport < ApplicationRecord
     where(source_organisation_id: source_organisation_id, status: "motifs_archived")
   }
 
-  validates :status, in: %w[oauth_connected copying_configuration copying_planning motifs_archived]
+  validates :status, inclusion: { in: %w[oauth_connected copying_configuration copying_planning motifs_archived] }
 
   def new_instance_organisations
     @new_instance_organisations ||= api_client.get("organisations")["organisations"]
--- spec/features/agents/instance_export_spec.rb
+++ spec/features/agents/instance_export_spec.rb
@@ -1,34 +1,36 @@
-RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", js: true do
+RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public" do
   around { |example| perform_enqueued_jobs { example.run } }
 
+  before do
+    login_as agent_rdv_aide_num, scope: :agent
+  end
+
+  let!(:agent_rdv_aide_num) do
+    create(:agent, first_name: "Camille", last_name: "Clavier", admin_role_in_organisations: [organisation_rdv_aide_num])
+  end
+  let(:organisation_rdv_aide_num) { create(:organisation, name: "France Service de Montreuil") }
+
+  let!(:agent_rdv_sp) do
+    create(:agent, first_name: "Camille", last_name: "Clavier", admin_role_in_organisations: rdv_sp_organisations)
+  end
+  let(:api_token) { create(:access_token, resource_owner_id: agent_rdv_sp.id, application: oauth_application) }
+  let(:rdv_sp_organisations) { [] }
+
+  let!(:oauth_application) { create(:oauth_application, name: "RDV Aide Numérique") }
+
   context "quand l'agent a deux organisations sur RDV SP" do
-    let(:organisation_rdv_aide_num) { create(:organisation, name: "France Service de Montreuil") }
-    let!(:agent_rdv_aide_num) do
-      create(:agent, first_name: "Camille", last_name: "Clavier", admin_role_in_organisations: [organisation_rdv_aide_num])
-    end
-
-    let!(:agent_rdv_sp) do
-      create(:agent, first_name: "Camille", last_name: "Clavier",
-                     admin_role_in_organisations: [organisation_rdv_sp1, organisation_rdv_sp2])
-    end
-
+    let(:rdv_sp_organisations) { [organisation_rdv_sp1, organisation_rdv_sp2] }
     let!(:organisation_rdv_sp1) { create(:organisation, name: "France Service de Montreuil") }
     let!(:organisation_rdv_sp2) { create(:organisation, name: "CCAS de Montreuil") }
 
-    let(:api_token) do
-      create(:access_token, resource_owner_id: agent_rdv_sp.id, application: oauth_application)
-    end
     let(:instance_export) do
-      InstanceExport.create!(agent_id: agent_rdv_aide_num.id, api_token: api_token.plaintext_token, refresh_token: api_token.refresh_token)
+      InstanceExport.create!(agent_id: agent_rdv_aide_num.id, api_token: api_token.plaintext_token, refresh_token: api_token.refresh_token, status: "oauth_connected")
     end
-
-    let!(:oauth_application) { create(:oauth_application, name: "RDV Aide Numérique") }
 
     stub_env_with(RDV_SERVICE_PUBLIC_OAUTH_BASE_URL: "http://localhost:#{Capybara.server_port}")
 
-    it "lui permet de choisir vers laquelle il fait la migration" do
-      login_as agent_rdv_aide_num, scope: :agent
-      visit edit_admin_organisation_instance_export_url(organisation_rdv_aide_num, instance_export.id, host: "www.rdv-aide-numerique-test.localhost")
+    it "lui permet de choisir vers laquelle il fait la migration", js: true do
+      visit admin_organisation_instance_export_url(organisation_rdv_aide_num, instance_export.id, host: "www.rdv-aide-numerique-test.localhost")
 
       select "France Service de Montreuil", from: :destination_organisation_id
 
@@ -37,6 +39,29 @@ RSpec.describe "Migration depuis RDV Aide Numérique vers RDV Service Public", j
       expect(page).to have_content "Migration terminée !"
 
       expect(instance_export.reload.destination_organisation_id).to eq organisation_rdv_sp1.id
+    end
+  end
+
+  context "quand l'agent a commencé une migration, mais s'est arrêté après la connexion OAuth" do
+    let!(:instance_export) do
+      InstanceExport.create!(
+        agent_id: agent_rdv_aide_num.id,
+        api_token: api_token.plaintext_token,
+        refresh_token: api_token.refresh_token,
+        status: "oauth_connected",
+        source_organisation_id: organisation_rdv_aide_num.id
+      )
+    end
+
+    stub_env_with(RDV_SERVICE_PUBLIC_OAUTH_BASE_URL: "http://rdv.gouv.localhost:1234")
+
+    before do
+      allow_any_instance_of(RdvServicePublicApiClient).to receive(:get).with("organisations").and_return({ "organisations" => [] })
+    end
+
+    it "lui permet de reprendre la migration depuis l'index" do
+      visit admin_organisation_instance_exports_url(organisation_rdv_aide_num, host: "www.rdv-aide-numerique-test.localhost")
+      click_on "Continuer la migration", match: :first
     end
   end
 end
