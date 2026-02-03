8a3f591ef9d918ee7aacf9992a3545f754bdbf0a
Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>
2026-02-03 16:39:12 +0100

    Correction de l’anonymizer (#6074)

--- spec/anonymizer_spec.rb
+++ spec/anonymizer_spec.rb
@@ -191,7 +191,8 @@ RSpec.describe Anonymizer do
   # this spec will fail when we update RDVSP schema but forget to update anonymizer config
   describe "exhaustivity" do
     it "should be exhaustive" do
-      expect { described_class.validate_exhaustivity! }.not_to raise_error
+      errors = described_class.exhaustivity_errors
+      expect(errors).to eq([]), errors.join("\n")
     end
   end
 end
