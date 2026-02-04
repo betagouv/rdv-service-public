6fb7fa9e5f6ce37426d19f035691b855d9b51ae7
François Ferrandis <francois@ferrandis.cool>
2026-02-04 16:54:50 +0100

    Fix specs

--- app/models/oauth_application.rb
+++ app/models/oauth_application.rb
@@ -2,6 +2,10 @@ class OauthApplication < Doorkeeper::Application
   self.ignored_columns += %w[default_service_id]
 
   def self.agent_is_verified_by_an_application?(agent)
-    OauthApplication.joins(:access_tokens).where(oauth_access_tokens: { resource_owner_id: agent.id }).where(grants_autonomous_signup: true).exists?
+    OauthApplication
+      .joins(:access_tokens)
+      .where(oauth_access_tokens: { resource_owner_id: agent.id })
+      .where(grants_autonomous_signup: true)
+      .any?
   end
 end
