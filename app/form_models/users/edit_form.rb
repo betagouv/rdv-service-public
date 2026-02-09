62a0fa9b1689bce74e3a42362f9fc06d8f51c9c2
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-02-09 12:46:46 +0100

    8b. Extraction de la logique pour l'affichage du champ logement

--- app/form_models/users/edit_form.rb
+++ app/form_models/users/edit_form.rb
@@ -9,4 +9,6 @@ class Users::EditForm
   def show_birth_date_field? = false
 
   def show_ants_pre_demande_number_field? = false
+
+  def show_logement_field? = user_profiles_territories.any?(&:enable_logement_field)
 end
--- app/form_models/users/rdv_booking_form.rb
+++ app/form_models/users/rdv_booking_form.rb
@@ -14,4 +14,6 @@ class Users::RdvBookingForm
   def show_birth_date_field? = !signed_in_with_invitation_token? && rdv.territory&.enable_birth_date_field?
 
   def show_ants_pre_demande_number_field? = rdv.requires_ants_predemande_number?
+
+  def show_logement_field? = rdv.territory.enable_logement_field
 end
