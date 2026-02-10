d10d837defc76b90237ac27159ae1b686d5db1cb
François Ferrandis <francois@ferrandis.cool>
2026-02-10 10:04:19 +0100

    Build et Send dans le même job

--- app/jobs/webhook_build_and_send_job.rb
+++ app/jobs/webhook_build_and_send_job.rb
@@ -1,8 +1,10 @@
 class WebhookBuildAndSendJob < ApplicationJob
   queue_as :latency_30s
 
+  discard_on(ActiveRecord::RecordNotFound) { |_job, error| Sentry.capture_exception(error) }
+
   def perform(record:, action:, webhook_endpoint_id:)
     payload = record.generate_webhook_payload(action)
-    WebhookSendJob.perform_later(payload, webhook_endpoint_id)
+    WebhookSendJob.new.perform(payload, webhook_endpoint_id)
   end
 end
