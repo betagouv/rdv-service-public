a5d9a1103485c768924f350923a78d628de719b8
François Ferrandis <francois@ferrandis.cool>
2026-02-11 15:54:53 +0100

    Premier truc qui fonctionne ?

--- app/javascript/controllers/plage_ouverture_form_controller.js
+++ app/javascript/controllers/plage_ouverture_form_controller.js
@@ -4,9 +4,26 @@ export default class extends Controller {
   static targets = [
     "motifsSection",
     "dateAndTimeSection",
+    "recurrenceCheckbox",
     "labelSection",
     "submitSection",
   ]
+
+  enableRecurrence() {
+    this.recurrenceCheckboxTarget.checked = true;
+    this.recurrenceCheckboxTarget.dispatchEvent(new Event('change'))
+    document.querySelector('label[for="recurrence-source"]').childNodes.filter(child => child.nodeType === Node.TEXT_NODE).forEach((node) => {
+      node.textContent = "Premier jour"
+    });
+  }
+
+  disableRecurrence() {
+    this.recurrenceCheckboxTarget.checked = false;
+    this.recurrenceCheckboxTarget.dispatchEvent(new Event('change'))
+    document.querySelector('label[for="recurrence-source"]').childNodes.filter(child => child.nodeType === Node.TEXT_NODE).forEach((node) => {
+      node.textContent = "Jour"
+    });
+  }
 
   showMotifsSection() {
     this.motifsSectionTarget.hidden = false;
