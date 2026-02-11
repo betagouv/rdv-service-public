7dcea95266b83ab0b88dc0863e9319177f2f9c35
François Ferrandis <francois@ferrandis.cool>
2026-02-11 10:51:12 +0100

    Pourquoi j'ai pas fait ça plus tôt ? Les specs nous le diront.

--- app/controllers/concerns/admin/planning/set_agents_concern.rb
+++ app/controllers/concerns/admin/planning/set_agents_concern.rb
@@ -10,19 +10,20 @@ module Admin::Planning::SetAgentsConcern
   private
 
   def set_agents
-    scope = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope)
-    agents = Agent.active.where(id: Array(params[:agent_id]).compact_blank)
+    agents = policy_scope(Agent, policy_scope_class: Agent::AgentPolicy::Scope)
+      .where(id: Array(params[:agent_id]).compact_blank)
+      .load
 
     case agents.size
     when 0
       @agent = current_agent
       @agents = [current_agent]
     when 1
-      @agent = scope.where(id: agents).first
+      @agent = agents.first
       @agents = [@agent]
     else
       if current_agent.feature_enabled?(Agent::FeatureFlags::NEW_PLANNING)
-        @agents = scope.where(id: agents).load
+        @agents = agents
       else
         # Si l'agent courant n'a pas activé la feature on ne considère qu'il n'y
         # a qu'un seul agent sélectionné, car le code sera en mode mono-agent.
