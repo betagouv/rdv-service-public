589bcef6d55138d940779e74f472273bc3c77c95
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-02-14 11:53:33 +0100

    Déplacement validate+save de UserRdvWizard vers Users::RdvBookingForm
    j'ai essayé de déplacer les validations puis le save mais c'était
    compliqué. C'est donc un commit un peu gros, je n'ai pas réussi à faire
    plus simple.
    
    Je choisis ici de rajouter un param
    `RdvBookingForm.new(user_attributes:)`. Une autre option aurait été de
    rajouter une méthode `RdvBookingForm.submit(user_attributes)`.

--- app/form_models/user_rdv_wizard.rb
+++ app/form_models/user_rdv_wizard.rb
@@ -1,32 +1,9 @@
 class UserRdvWizard
-  include ActiveModel::Model
   include RdvBuilderConcern
-
-  attr_accessor :user
-
-  delegate :errors, to: :user
-
-  validate :phone_number_present_for_motif_by_phone
 
   def initialize(user, attributes)
     @user = user
     build_rdv_from_attributes(attributes)
-
-    @user&.assign_attributes(@attributes.fetch(:user, {}))
-  end
-
-  def save
-    # we make sure the email can be updated only if it is blank
-    @user.skip_reconfirmation! if @user.email_was.blank?
-
-    # dans la vue on appelle form_for(user) plutôt que form_for(user_rdv_wizard),
-    # il faut donc ajouter des validations (et des erreurs) sur l'objet user
-    if rdv.requires_ants_predemande_number?
-      @user.singleton_class.include(User::AntsPreDemandeNumberStatusValidationConcern)
-      @user.ants_meeting_point_id = lieu_id # used in AntsPreDemandeNumberStatusValidation
-    end
-
-    valid? && @user.save
   end
 
   def display_france_connect?
@@ -35,11 +12,5 @@ class UserRdvWizard
 
   def display_pro_connect?
     motif.organisation.online_booking_for_professionnels
-  end
-
-  private
-
-  def phone_number_present_for_motif_by_phone
-    errors.add(:phone_number, :missing_for_phone_motif) if rdv.motif.phone? && user.phone_number.blank?
   end
 end
