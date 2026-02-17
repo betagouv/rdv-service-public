ce81f79a9f42c40792ea89002e586d8add1fc4f1
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-02-17 08:26:31 +0100

    Refactos UserRdvWizard avant fusion des étapes (#6120)
    * Simplification de skip_proches_step?
    
    - Déplacement depuis le UserRdvWizard vers le contrôleur
    
    Je ne comprends pas bien pourquoi ça a été implémenté comme ça l'a
    été avec une méthode de classe dans ac1db67b45
    
    * Remplacement du type check par une variable locale dans le résumé wizard
    
    * déplacement de la gestion du created_user_id vers le controleur
    
    * ne plus appeler le UserRdvWizard#save pour les étapes 2 et 3
    
    Les étapes 2 et 3 du UserRdvWizard ne font rien, on arrête de les
    appeler en anticipation du fait qu'elles vont disparaitre dans un commit
    successif
    
    * Générisation spec UserRdvWizard
    
    C'est un commit simplement pour limiter le diff d'un commit successif
    
    On utilise described_class partout dans le fichier pour que le
    changement à venir soit super simple
    
    * enlever except(:rdv)
    
    C'est moi et Carl qui avons introduit ce .except(:rdv) en 2020 🤯 dans https://github.com/betagouv/rdv-service-public/commit/364fef234279259f0b4384521797e825bd9f1d66#diff-ba6c96a33f5619dc5d00c8b8b356fe0ecb8e91567fc9dcd0acffc872bbb65e7fR45
    
    On peut voir que j'y fais déjà un superbe refacto du wizard et que c'est
    aussi moi qui ait ajouté l'étape de confirmation ! c'est cyclique.
    
    Aujourd'hui to_query ne contient plus de clé RDV, c'est assez facilement
    vérifiable.
    
    * Simplification de UserRdvWizard en une seule classe
    
    Aide à la lecture : Parcourir le fichier user_rdv_wizard.rb de bas en
    haut
    
    * Déplacement validate+save de UserRdvWizard vers Users::RdvBookingForm
    
    j'ai essayé de déplacer les validations puis le save mais c'était
    compliqué. C'est donc un commit un peu gros, je n'ai pas réussi à faire
    plus simple.
    
    Je choisis ici de rajouter un param
    `RdvBookingForm.new(user_attributes:)`. Une autre option aurait été de
    rajouter une méthode `RdvBookingForm.submit(user_attributes)`.
    
    * inlining des méthodes display_fc et display_pc
    
    Ici c'est un peu à contre-temps du reste mais ça permet de « vider »
    UserRdvWizard progressivement
    
    * UserRdvWizard devient Users::RdvBuilder
    
    Le PrescripteurRdvWizard n'hérite plus de UserRdvWizard, il instancie
    maintenant un objet Users::RdvBuilder
    
    À ce stade on a encore à plein d'endroits dans le code
    @rdv_wizard = Users::RdvBuilder.new
    
    * renaming rdv_wizard -> rdv_builder dans les controleurs, specs et services
    
    * minor refacto in Users::RdvBuilder#initialize
    
    * Simplification de la validation ANTS dans RdvBookingForm
    
    Remplacement du hack singleton_class.include par des validations
    conditionnelles directement sur le form object. La validation ANTS
    ne s'exécute que si le motif l'exige et que des attributs user
    sont présents (step 1).
    
    Suppression de AntsPreDemandeNumberStatusValidationConcern devenu
    inutile.
    
    * ne pas instancier RdvBookingForm inutilement + clarification #create
    
    avant ce commit :
    - on instancie le RdvBookingForm inutilement dans les étapes 2 et 3
    - on valide le RdvBookingForm inutilement dans le create de l'étape 2
    
    je simplifie aussi l'appel à RdvBookingForm#save maintenant que c'est un
    vrai form object, plus besoin de tester .valid? et benign_errors
    indépendemment, c'est fait dans le save.
    
    * Correction message d'erreur numéro téléphone invalide
    
    Ce problème n'a rien à voir avec cette PR, il préexiste. Lorsqu'un
    numéro de téléphone est invalide, le message affiché ne contient pas le
    nom du champ, on voit donc seulement « est invalide ». C'est du au fait
    qu'on a mis en place un format sans le nom du champ volontairement pour
    éviter les messages comme « Numéro de téléphone est invalide ».
    
    J'en profite pour le corriger ici.

---
+++ spec/form_models/users/rdv_builder_spec.rb
@@ -0,0 +1,68 @@
+RSpec.describe Users::RdvBuilder do
+  let!(:organisation) { create(:organisation) }
+  let!(:user) { create(:user) }
+  let!(:user_for_rdv) { create(:user) }
+  let!(:motif) { create(:motif, organisation: organisation, default_duration_in_min: 30) }
+  let!(:lieu) { create(:lieu, organisation: organisation) }
+  let!(:creneau) { build(:creneau, :respects_booking_delays, motif: motif, starts_at: Time.zone.parse("2020-10-20 09h30")) }
+  let!(:plage_ouverture) { create(:plage_ouverture, motifs: [motif], lieu: lieu, organisation: organisation) }
+
+  let(:mock_geo_search) { instance_double(Users::GeoSearch) }
+  let(:attributes) do
+    {
+      starts_at: creneau.starts_at,
+      motif_id: motif.id,
+      lieu_id: lieu.id,
+      user_ids: [user_for_rdv.id],
+      departement: "62",
+      city_code: "62100",
+    }
+  end
+
+  it "construit un RDV et un créneau" do
+    returned_creneau = Creneau.new
+
+    allow(Users::GeoSearch).to receive(:new)
+      .with(departement: "62", city_code: "62100")
+      .and_return(mock_geo_search)
+    allow(CreneauxSearch::ForUser).to receive(:creneau_for).with(
+      user: user,
+      motif: motif,
+      lieu: lieu,
+      starts_at: Time.zone.parse("2020-10-20 09h30"),
+      geo_search: mock_geo_search,
+      duration_in_min: 30
+    ).and_return(returned_creneau)
+    rdv_wizard = described_class.new(user, attributes)
+    expect(rdv_wizard.rdv.user_ids).to eq [user_for_rdv.id]
+    expect(rdv_wizard.creneau).to eq returned_creneau
+  end
+
+  it "avec un duration_in_min différent de celui du motif" do
+    returned_creneau = Creneau.new
+    allow(Users::GeoSearch).to receive(:new)
+      .with(departement: "62", city_code: "62100")
+      .and_return(mock_geo_search)
+    allow(CreneauxSearch::ForUser).to receive(:creneau_for).with(
+      user: user,
+      motif: motif,
+      lieu: lieu,
+      starts_at: Time.zone.parse("2020-10-20 09h30"),
+      geo_search: mock_geo_search,
+      duration_in_min: 60
+    ).and_return(returned_creneau)
+    rdv_wizard = described_class.new(user, attributes.merge(duration: 60))
+    expect(rdv_wizard.rdv.user_ids).to eq [user_for_rdv.id]
+    expect(rdv_wizard.creneau).to eq returned_creneau
+  end
+
+  context "Rdv collectif - bookable by agents and prescripteurs" do
+    let(:motif) { create(:motif, :at_public_office, organisation: organisation, bookable_by: :agents_and_prescripteurs, collectif: true) }
+    let!(:rdv) { create(:rdv, motif: motif, organisation: organisation) }
+    let(:attributes) { { rdv_collectif_id: rdv.id } }
+
+    it "trouve le RDV existant" do
+      expect(described_class.new(user_for_rdv, attributes).rdv).to eq(rdv)
+    end
+  end
+end
