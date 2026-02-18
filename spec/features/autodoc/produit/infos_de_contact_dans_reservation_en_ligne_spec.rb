94fd24c23abb9be6a3769210003c4fa4b426f87a
Victor Mours <victor.mours@gmail.com>
2026-02-18 12:17:17 +0100

    Add contact info

--- spec/features/autodoc/produit/infos_de_contact_dans_reservation_en_ligne_spec.rb
+++ spec/features/autodoc/produit/infos_de_contact_dans_reservation_en_ligne_spec.rb
@@ -11,11 +11,12 @@ RSpec.describe "Informatiosn de contact d'une structure lors de la réservation 
     visit public_link_to_motif_path(public_link_id: motif.public_link_id, motif_slug: motif.slug)
 
     expect(page).to have_content(organisation.humanized_phone_number)
+    expect(page).to have_content("Contact de")
 
     doc.add_screenshot(
       page,
-      text: "On affiche les infos de contact",
-      wait_for: "Vous pouvez contacter"
+      text: "On affiche les infos de contact de l'organisations. On n'affiche cette information que\
+    si on est sur le lien direct d'une organisation ou d'un motif, et pas dans le cas d'une recherche par sectorisation (puisque plusieurs organisations peuvent correspondre à la recherche)."
     )
   end
 end
