256530096183a3c34c43f60345986fce935cbc41
Victor Mours <victor.mours@gmail.com>
2026-02-19 16:17:24 +0100

    working for prescripteur

--- app/form_models/prescripteur_rdv_wizard.rb
+++ app/form_models/prescripteur_rdv_wizard.rb
@@ -41,6 +41,9 @@ class PrescripteurRdvWizard
       agents: [creneau.agent],
       participations: [participation]
     )
+
+    rdv.minutes_after_rdv = PlageOuverture.joins(:motifs).where(agent: rdv.agents.map(&:id),
+                                                                motifs: rdv.motif).overlapping_range(rdv.starts_at..rdv.ends_at).pluck(:minutes_between_rdvs).max || 0
     rdv.save!
 
     Notifiers::RdvCreated.perform_with(rdv, @prescripteur)
