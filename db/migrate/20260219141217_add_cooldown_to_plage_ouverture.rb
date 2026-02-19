4c9afc195f8ba8f21cccdbc95874025a99a2bf68
Victor Mours <victor.mours@gmail.com>
2026-02-19 15:56:12 +0100

    working version for online booking but not booking by agent

--- db/migrate/20260219141217_add_cooldown_to_plage_ouverture.rb
+++ db/migrate/20260219141217_add_cooldown_to_plage_ouverture.rb
@@ -1,5 +1,6 @@
 class AddCooldownToPlageOuverture < ActiveRecord::Migration[8.0]
   def change
     add_column :plage_ouvertures, :minutes_between_rdvs, :integer, null: false, default: 0
+    add_column :rdvs, :minutes_after_rdv, :integer, null: false, default: 0
   end
 end
