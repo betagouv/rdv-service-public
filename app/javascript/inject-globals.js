bafbef69c7fad501b6930af341475deaf63698f9
François Ferrandis <francois@ferrandis.cool>
2026-02-19 19:08:33 +0100

    Passage à esbuild (select2 pété)

---
+++ app/javascript/inject-globals.js
@@ -0,0 +1,3 @@
+export { default as $ } from 'jquery';
+export { default as jQuery } from 'jquery';
+export { default as Popper } from 'popper.js';
---
+++ esbuild.config.mjs
@@ -0,0 +1,45 @@
+import * as esbuild from 'esbuild';
+import { sassPlugin } from 'esbuild-sass-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const isWatch = process.argv.includes('--watch');
+const isDev = process.env.NODE_ENV === 'development';
+
+const config = {
+  entryPoints: {
+    administrate: './app/javascript/administrate',
+    application: './app/javascript/application',
+    application_test: './app/javascript/application_test',
+    application_agent: './app/javascript/application_agent',
+    lieux_map: './app/javascript/lieux_map',
+    application_agent_config: './app/javascript/application_agent_config',
+    charts: './app/javascript/charts',
+    rdv_plan: './app/javascript/rdv_plan',
+    mail: './app/javascript/mail',
+    instance_name: './app/javascript/instance_name',
+  },
+  bundle: true,
+  outdir: 'app/assets/builds',
+  sourcemap: true,
+  minify: !isDev,
+  define: { global: 'globalThis' },
+  inject: ['./app/javascript/inject-globals.js'],
+  plugins: [
+    sassPlugin({
+      importMapper: (p) => p.replace(/^~/, ''),
+      loadPaths: [path.resolve(__dirname, 'node_modules')],
+    }),
+  ],
+  external: ['*.svg'],
+  resolveExtensions: ['.js', '.scss', '.sass', '.css'],
+  logLevel: 'info',
+};
+
+if (isWatch) {
+  const ctx = await esbuild.context(config);
+  await ctx.watch();
+} else {
+  await esbuild.build(config);
+}
