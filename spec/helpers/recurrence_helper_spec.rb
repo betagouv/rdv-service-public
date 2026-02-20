e1d2cabe094e3c56f24fae146b711456d21abe95
Victor Mours <victor.mours@beta.gouv.fr>
2026-02-20 14:42:28 +0100

    Clarifications de l'index des plages d'ouvertures (#6169)
    * Order plages ouverture based on starting day
    
    * Remove updated_at from index, it's too noisy and not relevant anymore
    
    * Use dsfr padding for new planning
    
    * Nicer interval wording
    
    * Rename créneau to plage

--- spec/helpers/recurrence_helper_spec.rb
+++ spec/helpers/recurrence_helper_spec.rb
@@ -7,30 +7,35 @@ RSpec.describe RecurrenceHelper do
 
   describe "#display_recurrence" do
     it "with a weekly recurrence" do
-      plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week))
-      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, le mardi", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
+      plage_ouverture = build(:plage_ouverture, :weekly_on_monday)
+      expect(display_recurrence(plage_ouverture)).to eq(["Tous les lundis", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
 
     it "with a weekly recurrence on wednesday" do
-      plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week, on: ["wednesday"]))
-      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, les mercredis", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
+      plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week, on: ["wednesday"], interval: 1))
+      expect(display_recurrence(plage_ouverture)).to eq(["Tous les mercredis", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
+    end
+
+    it "with a recurrence every other week" do
+      plage_ouverture = build(:plage_ouverture, :every_two_weeks)
+      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les 2 semaines, les lundis", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
 
     it "with a monthly recurrence" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:month, day: { 3 => [2] }))
-      expect(display_recurrence(plage_ouverture)).to eq(["Tous les  mois, le 2ème mercredi", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
+      expect(display_recurrence(plage_ouverture)).to eq(["Tous les mois, le 2ème mercredi", "de 08:00 à 12:00", "à partir du mardi 28 décembre 2021"])
     end
 
     it "with a secondary time interval" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week), secondary_start_time: "14:00", secondary_end_time: "17:45")
-      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les  semaines, le mardi", "de 08:00 à 12:00 et de 14:00 à 17:45", "à partir du mardi 28 décembre 2021"])
+      expect(display_recurrence(plage_ouverture)).to eq(["Toutes les semaines, le mardi", "de 08:00 à 12:00 et de 14:00 à 17:45", "à partir du mardi 28 décembre 2021"])
     end
   end
 
   describe "#occurrence_text" do
     it "returns occurrence text" do
       plage_ouverture = build(:plage_ouverture, recurrence: Montrose.every(:week))
-      expect(occurrence_text(plage_ouverture)).to eq("Toutes les  semaines, le mardi de 08:00 à 12:00 à partir du mardi 28 décembre 2021")
+      expect(occurrence_text(plage_ouverture)).to eq("Toutes les semaines, le mardi de 08:00 à 12:00 à partir du mardi 28 décembre 2021")
     end
 
     it "returns" do
