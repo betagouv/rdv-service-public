a36ab1f87e7116f1a39786ac17def2b4afb0af58
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-02-23 12:03:35 +0100

    var d'env

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   include ExplicitPunditConcern
   include DomainDetection
 
-  protect_from_forgery with: :exception
+  # TODO: supprimer cette var d'env après le 23/03/2026 c'était une sécurité le temps de la release
+  protect_from_forgery with: ENV.fetch("PROTECT_FROM_FORGERY_STRATEGY", "exception").to_sym
 
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :store_user_location!, if: :storable_location?
