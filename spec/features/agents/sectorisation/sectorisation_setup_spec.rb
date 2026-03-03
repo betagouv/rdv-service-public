82b3e2b10659358cf8efe6f7ec21cca727ac783d
François Ferrandis <francois@ferrandis.cool>
2026-03-03 15:18:32 +0100

    CI 3

--- spec/features/agents/sectorisation/sectorisation_setup_spec.rb
+++ spec/features/agents/sectorisation/sectorisation_setup_spec.rb
@@ -15,7 +15,7 @@ RSpec.describe "Agent can setup sectorisation", type: :feature do
     fill_in :sector_name, with: "Secteur Nord"
     fill_in :sector_human_id, with: "nord"
     click_on "Enregistrer"
-    # CI 2
+    # CI 3
     click_on "Ajouter une commune ou une rue"
     fill_in_readonly_input("#zone_city_name", "Valence")
     fill_in_readonly_input("#zone_city_code", "26362")
