f0e38d6ad1fc7f350ac2ce4435ac0f12ab4053fa
Romain Neuville <neuvilleromain@gmail.com>
2026-03-03 16:42:51 +0100

    remove useless module (devises emails)

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
     ]
   )
 
-  devise :timeoutable, :async
+  devise :timeoutable
   def timeout_in = 30.minutes # Used by Devise's :timeoutable
 
   include PgSearch::Model
