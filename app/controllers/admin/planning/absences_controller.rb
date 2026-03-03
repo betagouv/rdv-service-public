f45b3e153e9e362ca475263bddfe580b36080447
François Ferrandis <francois@ferrandis.cool>
2026-03-03 14:20:24 +0100

    Afficher la pastille sur les nouveaux onglets si l'agent n'avais pas activé la beta

--- app/controllers/admin/planning/absences_controller.rb
+++ app/controllers/admin/planning/absences_controller.rb
@@ -8,6 +8,11 @@ class Admin::Planning::AbsencesController < AgentAuthController
   before_action { @planning_layout = true }
 
   def index
+    # TODO: retirer ce code 2 semaines après la mise en prod, il affiche une pastille sur les nouveaux onglets.
+    unless current_agent.feature_enabled?(Agent::FeatureFlags::NEW_PLANNING)
+      current_agent.enable_feature!(Agent::FeatureFlags::NEW_PLANNING)
+    end
+
     @multiple_agents_makes_sense = true
     render :multi_agents_index and return if @agents.size > 1
 
