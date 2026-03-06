15ca62422feb06ccad9ff627a1a2044969dc36d2
François Ferrandis <francois@ferrandis.cool>
2026-03-06 10:50:04 +0100

    Migration d'instance : correction de la détection d'upsert côté appel API

--- app/lib/rdv_service_public_api_client.rb
+++ app/lib/rdv_service_public_api_client.rb
@@ -36,9 +36,9 @@ class RdvServicePublicApiClient
   end
 
   def ressource_already_created_error_response?(response)
-    external_id_errors = response.body.dig("errors", "external_id")
+    external_id_errors = Array(response.body.dig("errors", "external_id")) + Array(response.body.dig("errors", "external_references.external_id"))
 
-    external_id_errors&.find do |error_hash|
+    external_id_errors.find do |error_hash|
       error_hash["error"] == "taken"
     end
   end
