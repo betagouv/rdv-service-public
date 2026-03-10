d79ae1967316bc4e7086734a2a2bd1326c263b13
François Ferrandis <francois@ferrandis.cool>
2026-03-10 17:58:06 +0100

    À discuter

--- app/services/users/login_service.rb
+++ app/services/users/login_service.rb
@@ -56,7 +56,7 @@ class Users::LoginService
   end
 
   def upsert_user
-    user = User.joins(:rdvs).order("rdvs.created_at DESC, users.created_at DESC").where(email: email).first
+    user = User.left_joins(:rdvs).order("rdvs.created_at DESC, users.created_at DESC").where(email: email).first
     if user
       update_user(user) if first_name.present? && last_name.present?
     else
