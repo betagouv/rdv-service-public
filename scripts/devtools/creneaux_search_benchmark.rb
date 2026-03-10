93e2b259d15a9b6ef9d79d4c5557837a17caba19
Victor Mours <victor.mours@gmail.com>
2026-03-10 12:00:19 +0100

    fix

--- scripts/devtools/creneaux_search_benchmark.rb
+++ scripts/devtools/creneaux_search_benchmark.rb
@@ -1,5 +1,7 @@
 require "benchmark/ips"
 
+motif = Motif.find(1327) # Carte d'identité pour la mairie de Sèvres
+
 Benchmark.ips do |x|
   # Configure the number of seconds used during
   # the warmup phase (default 2) and calculation phase (default 5)
@@ -7,6 +9,7 @@ Benchmark.ips do |x|
 
   # Typical mode, runs the block as many times as it can
   x.report("creneaux_search") do
-    CreneauxSearch::Calculator.available_slots(motif: Motif.last, lieu: nil, agents: nil, date_range: (Time.zone.now..3.months.from_now))
+    # On trouve environ 1200 créneaux
+    CreneauxSearch::Calculator.available_slots(motif: motif, lieu: nil, agents: nil, date_range: (Time.zone.now..3.months.from_now))
   end
 end
