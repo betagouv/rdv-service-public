c0cdd6a292ee53365d8d54a83293e9a9c84bee2a
Victor Mours <victor.mours@gmail.com>
2026-03-13 11:20:17 +0100

    specs

--- spec/features/anybody/anybody_can_see_stats_spec.rb
+++ spec/features/anybody/anybody_can_see_stats_spec.rb
@@ -21,6 +21,19 @@ RSpec.describe "Anybody can see stats" do
     end
 
     it "lists the territories that are willing to publish public stats" do
+      visit stats_territories_path
+
+      expect(page).to have_content "Territoire public"
+      expect(page).not_to have_content "Territoire secret"
+
+      click_on "Territoire public"
+      expect(page).to have_content "Nombre de RDV par statut"
+    end
+
+    it "doesn't allow seeing the stats of a territory that has disabled public stats" do
+      expect do
+        visit stats_territory_path(territory: territory_without_stats.id)
+      end.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 end
