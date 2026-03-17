af64e550673c094d3631ecbfa085b7c5bd589ff5
François Ferrandis <francois@ferrandis.cool>
2026-03-17 10:25:56 +0100

    Revert "Ajout de la colonne + foreign key"
    This reverts commit 23206eca5247b93482ff90b9896a9afd972bac51.

--- db/migrate/20260317084447_add_latest_used_organisation_id_to_agents.rb
+++
@@ -1,6 +0,0 @@
-class AddLatestUsedOrganisationIdToAgents < ActiveRecord::Migration[8.0]
-  def change
-    add_column :agents, :latest_used_organisation_id, :bigint
-    add_foreign_key :agents, :organisations, column: :latest_used_organisation_id, validate: false
-  end
-end
--- db/migrate/20260317085457_validate_add_latest_used_organisation_id_to_agents.rb
+++
@@ -1,5 +0,0 @@
-class ValidateAddLatestUsedOrganisationIdToAgents < ActiveRecord::Migration[8.0]
-  def change
-    validate_foreign_key :agents, :organisations
-  end
-end
