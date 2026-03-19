c4c36ae0f24b317a044b15d7ece1e39fc8d24ec2
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2026-03-19 17:52:28 +0100

    Permettre de configurer la couleur d'une plage (#6261)
    * Permettre de configurer la couleur d'une plage
    
    * Fix specs
    
    * Fix anonymizer.yml
    
    * Fix A11y offence "Ensure table headers have discernible text" (kinda)

--- db/schema.rb
+++ db/schema.rb
@@ -590,6 +590,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_03_19_000001) do
     t.datetime "recurrence_ends_at"
     t.time "secondary_start_time"
     t.time "secondary_end_time"
+    t.string "hex_color", limit: 7, default: "#c6ecff", null: false
     t.index "tsrange((first_day)::timestamp without time zone, recurrence_ends_at, '[]'::text)", name: "index_plage_ouvertures_on_tsrange_first_day_recurrence_ends_at", using: :gist
     t.index ["agent_id"], name: "index_plage_ouvertures_on_agent_id"
     t.index ["expired_cached"], name: "index_plage_ouvertures_on_expired_cached"
