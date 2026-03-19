1b3d3f2211a0737a5df8043a30e975d9e2faf3ca
François Ferrandis <francois@ferrandis.cool>
2026-03-19 18:28:52 +0100

    Revert "Bugfix: colonne email vide dans le CSV des participants quand il utilise notification_email (#6268)"
    This reverts commit 6b0668b30c1c932cf212cf2977b0bd7fc9387c43.

--- app/services/participants_csv.rb
+++ app/services/participants_csv.rb
@@ -9,7 +9,7 @@ class ParticipantsCsv
       @rdv.participations.includes(:user).order(created_at: :asc).each do |participation|
         csv << [
           participation.user.full_name,
-          participation.user.preferred_email,
+          participation.user.email,
           Rdv.human_attribute_value(:status, participation.temporal_status, disable_cast: true),
         ]
       end
