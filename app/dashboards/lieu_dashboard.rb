03b124e854ce85da1b3fbc98cfb233676c118f27
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-03-23 12:17:59 +0100

    Add option in SuperAdmin

--- app/dashboards/lieu_dashboard.rb
+++ app/dashboards/lieu_dashboard.rb
@@ -24,6 +24,7 @@ class LieuDashboard < Administrate::BaseDashboard
     phone_number_formatted: Field::String,
     availability: Field::Select.with_options(searchable: false, collection: ->(field) { field.resource.class.send(field.attribute.to_s.pluralize).keys }),
     address: PlacesField.with_options(searchable: true),
+    address_without_geocoding: Field::Boolean,
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -60,6 +61,7 @@ class LieuDashboard < Administrate::BaseDashboard
     latitude
     longitude
     address
+    address_without_geocoding
     organisation
   ].freeze
 
