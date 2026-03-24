13f34c7c0ccf8c643d86543f5cbe54ba32c0116d
Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>
2026-03-24 09:48:04 +0100

    Update Rails 8.0.4.1 (#6276)

--- spec/support/route_detect_format.rb
+++ spec/support/route_detect_format.rb
@@ -6,7 +6,7 @@
 if Rails.env.test?
   # Il faut copier la méthode `handle_positional_args` depuis `action_dispatch/routing/route_set.rb:292`
   # puis ajouter le `raise` dans le block `args.each_with_index` comme ci-dessous.
-  raise "Il faut recopier ce code quand on change de version de Rails" if Rails.version != "8.0.4"
+  raise "Il faut recopier ce code quand on change de version de Rails" if Rails.version != "8.0.4.1"
 
   class ActionDispatch::Routing::RouteSet::NamedRouteCollection::UrlHelper
     def handle_positional_args(controller_options, inner_options, args, result, path_params)
@@ -37,5 +37,33 @@ if Rails.env.test?
 
       result.merge!(inner_options)
     end
+  end
+
+  def handle_positional_args(controller_options, inner_options, args, result, path_params)
+    if args.size > 0
+      # take format into account
+      if path_params.include?(:format)
+        path_params_size = path_params.size - 1
+      else
+        path_params_size = path_params.size
+      end
+
+      if args.size < path_params_size
+        path_params -= controller_options.keys
+        path_params -= (result[:path_params] || {}).merge(result).keys
+      else
+        path_params = path_params.dup
+      end
+      inner_options.each_key do |key|
+        path_params.delete(key)
+      end
+
+      args.each_with_index do |arg, index|
+        param = path_params[index]
+        result[param] = arg if param
+      end
+    end
+
+    result.merge!(inner_options)
   end
 end
