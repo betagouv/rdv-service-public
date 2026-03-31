ae72372232dc3d8e4058e131b4533ff7dc16d6fd
François Ferrandis <francois@ferrandis.cool>
2026-03-31 15:36:09 +0200

    Se base sur request.path_parameters[:organisation_id]

--- app/controllers/agent_auth_controller.rb
+++ app/controllers/agent_auth_controller.rb
@@ -25,15 +25,20 @@ class AgentAuthController < ApplicationController
   end
 
   def current_organisation
-    return @current_organisation if defined? @current_organisation
+    return @current_organisation if defined?(@current_organisation)
 
-    @current_organisation = Organisation.find(params[:organisation_id]).tap do |organisation|
-      session[:latest_used_organisation_id] = organisation.id if organisation
+    # Le param `:organisation_id` qui nous intéresse est forcément dans le path puisque les
+    # routes de notre interface orga-centric sont imbriquées dans `resources :organisations`.
+    path_org_id = request.path_parameters[:organisation_id].presence&.to_i
+    return unless path_org_id
+
+    @current_organisation = Organisation.find(path_org_id).tap do |found_org|
+      session[:latest_used_organisation_id] = found_org.id if found_org
     end
   end
 
   def current_territory
-    @current_territory ||= current_organisation.territory
+    @current_territory ||= current_organisation&.territory
   end
 
   def from_modal?
@@ -42,6 +47,6 @@ class AgentAuthController < ApplicationController
 
   def authorize_organisation
     # on n’utilise pas le helper authorize directement pour obliger à faire un autre appel à authorize avec la ressource qui sera réellement utilisée par l'action (par exemple le motif ou le rdv)
-    Pundit.authorize(current_agent, current_organisation, :show?, policy_class: Agent::OrganisationPolicy)
+    Pundit.authorize(current_agent, current_organisation, :show?, policy_class: Agent::OrganisationPolicy) if current_organisation
   end
 end
