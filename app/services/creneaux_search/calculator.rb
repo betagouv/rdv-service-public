953fa2ed0b2cc548d1563e47a73deff93d774df8
Victor Mours <victor.mours@gmail.com>
2026-03-31 10:58:47 +0200

    remove old calculator code

--- app/services/creneaux_search/calculator.rb
+++ app/services/creneaux_search/calculator.rb
@@ -1,11 +1,6 @@
 class CreneauxSearch::Calculator
-  def self.available_slots(date_range:, motif:, lieu: nil, agents: [], duration_in_min: nil, old_calculator: nil)
-    old_calculator = ENV["USE_OLD_CRENEAUX_SEARCH_CALCULATOR"] if old_calculator.nil?
-    if old_calculator
-      CreneauxSearch::OldCalculator.available_slots(motif:, lieu:, date_range:, agents:, duration_in_min:)
-    else
-      new(motif:, lieu:, date_range:, agents:, duration_in_min:).available_slots
-    end
+  def self.available_slots(date_range:, motif:, lieu: nil, agents: [], duration_in_min: nil)
+    new(motif:, lieu:, date_range:, agents:, duration_in_min:).available_slots
   end
 
   def initialize(motif:, lieu:, date_range:, agents:, duration_in_min:)
