b2ef71c338b095e6236226f7fbd8bb5205297cbb
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-03-31 12:02:56 +0200

    Remove agent_accueil_enabled org

--- app/controllers/admin/organisations_controller.rb
+++ app/controllers/admin/organisations_controller.rb
@@ -66,7 +66,7 @@ class Admin::OrganisationsController < AgentAuthController
   end
 
   def organisation_params
-    params.require(:organisation).permit(:name, :horaires, :phone_number, :website, :email, :agent_accueil_enabled)
+    params.require(:organisation).permit(:name, :horaires, :phone_number, :website, :email)
   end
 
   def new_organisation_params
