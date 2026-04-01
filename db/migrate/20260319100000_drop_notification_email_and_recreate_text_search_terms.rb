f9f6e38630ba2d913734c7205f28d3747f0a2bcb
Romain Neuville <neuvilleromain@gmail.com>
2026-04-01 16:14:14 +0200

    finally keep notification_email

--- db/migrate/20260319100000_drop_notification_email_and_recreate_text_search_terms.rb
+++ db/migrate/20260319100000_drop_notification_email_and_recreate_text_search_terms.rb
@@ -1,9 +1,6 @@
 class DropNotificationEmailAndRecreateTextSearchTerms < ActiveRecord::Migration[8.0]
   def up
     safety_assured do
-      remove_index :users, name: "index_users_on_notification_email"
-      remove_column :users, :notification_email, :string
-
       col_definition = <<~COLUMN
         setweight(to_tsvector('simple', translate(lower(coalesce("users"."last_name", '')), 'àâäéèêëïîôöùûüÿç', 'aaaeeeeiioouuuyc')), 'A') ||
         setweight(to_tsvector('simple', translate(lower(coalesce("users"."first_name", '')), 'àâäéèêëïîôöùûüÿç', 'aaaeeeeiioouuuyc')), 'B') ||
