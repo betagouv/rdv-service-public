e4d60d08b6aabbc7f57917cf4000c35669719423
Romain Neuville <neuvilleromain@gmail.com>
2026-04-02 17:23:59 +0200

    remove migrations

--- db/migrate/20260319100001_add_index_on_text_search_terms.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexOnTextSearchTerms < ActiveRecord::Migration[8.0]
-  disable_ddl_transaction!
-
-  def change
-    add_index :users, :text_search_terms, using: :gin, algorithm: :concurrently
-  end
-end
