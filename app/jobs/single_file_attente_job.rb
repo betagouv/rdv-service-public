af3ac0e3af37034efe655d5922315c0b311b1537
Victor Mours <victor.mours@beta.gouv.fr>
2026-04-13 14:32:58 +0200

    Update app/jobs/single_file_attente_job.rb
    Co-authored-by: Antoine Girard <12141434+AntoineGirard@users.noreply.github.com>

--- app/jobs/single_file_attente_job.rb
+++ app/jobs/single_file_attente_job.rb
@@ -2,7 +2,8 @@ class SingleFileAttenteJob < ApplicationJob
   queue_as :latency_5mn
 
   def perform(file_attente_id)
-    fa = FileAttente.find(file_attente_id)
+    fa = FileAttente.find_by(id: file_attente_id)
+    return unless fa
 
     fa.send_notification_if_valid
   end
