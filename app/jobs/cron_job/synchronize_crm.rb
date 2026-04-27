1f34325122247b040d40f8e09c928a858c9bd128
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2026-04-27 15:11:50 +0200

    Éviter `Notion::Api::Errors::TooManyRequests` (#6342)
    * Éviter `Notion::Api::Errors::TooManyRequests`
    
    * 400ms is a sweet spot
    
    * Mornings are busier for workers, let's do that in the evening

--- app/jobs/cron_job/synchronize_crm.rb
+++ app/jobs/cron_job/synchronize_crm.rb
@@ -19,8 +19,9 @@ class CronJob::SynchronizeCrm < CronJob
     }
 
     client.database_query(database_id: NOTION_DATABASE_ID, filter:) do |page|
-      page.results.each do |notion_page|
-        SynchronizeCrmPageJob.perform_later(
+      page.results.each.with_index do |notion_page, index|
+        # Notion rate-limiting is 3 API calls per second, so we'll execute jobs every 400ms
+        SynchronizeCrmPageJob.set(wait: (index * 0.4).seconds).perform_later(
           notion_page_id: notion_page.id,
           account_url: notion_page.properties["COMPTE PROD"].url,
           notion_page_url: notion_page.url,
