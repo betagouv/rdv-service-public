3acb41809b8640c59f17fc203fb0d242f9bc6815
François Ferrandis <francois@ferrandis.cool>
2026-04-27 10:23:00 +0200

    Réparer le preview du mailer de file d'attente

--- spec/mailers/previews/users/file_attente_mailer_preview.rb
+++ spec/mailers/previews/users/file_attente_mailer_preview.rb
@@ -1,8 +1,10 @@
 class Users::FileAttenteMailerPreview < ActionMailer::Preview
   def new_creneau_available
-    rdv = Rdv.last
-    user = User.last
-    token = rdv.participations.find_by(user: user)&.restricted_auth_token
+    participation = Participation.last
+    rdv = participation.rdv
+    user = participation.user
+    participation.set_restricted_authentication_token_if_missing_and_save
+    token = participation.restricted_auth_token
     Users::FileAttenteMailer.with(rdv: rdv, user: user, token: token).new_creneau_available
   end
 end
