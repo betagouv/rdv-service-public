42d27b307bd9c1d16e3fce0e435280732070721e
François Ferrandis <francois@ferrandis.cool>
2026-04-28 09:42:36 +0200

    Simpler implementation

--- app/lib/time_formatter.rb
+++ app/lib/time_formatter.rb
@@ -1,11 +1,9 @@
 module TimeFormatter
   def self.french_time(datetime)
-    minutes = datetime.min.zero? ? nil : datetime.min
-    minute_zero_padding = datetime.min.in?(1..9) ? "0" : ""
-    "#{datetime.hour}h#{minute_zero_padding}#{minutes}"
+    datetime.strftime("%kh%M").strip.gsub("00", "")
   end
 
   def self.french_time_range(start_time, end_time)
-    [french_time(start_time), french_time(end_time)].join("-")
+    [french_time(start_time), french_time(end_time)].join(" à ")
   end
 end
