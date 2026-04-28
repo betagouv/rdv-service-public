c357d8b646bf1a9a39a52f9ad42f9f0ca0e8f825
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2026-04-28 17:35:40 +0200

    Bugfix : deux éléments du menu actifs en même temps (#6330)
    * Renomme set_agents_concern.rb en planning_concern.rb et y ajoute @planning_layout = true
    
    * Refactor du mécanisme qui détermine quelle entrée de menu est active
    
    * C'est plus logique de dire qu'on est dans un contexte de planning
    
    * Consistency
    
    * Just use else
    
    * Utiliser un helper finalement ?

--- app/controllers/admin/planning/absences_controller.rb
+++ app/controllers/admin/planning/absences_controller.rb
@@ -1,11 +1,10 @@
 class Admin::Planning::AbsencesController < AgentAuthController
-  include Admin::Planning::SetAgentsConcern
+  include Admin::Planning::PlanningConcern
   respond_to :html, :json
 
   before_action :set_absence, only: %i[edit update destroy]
   before_action :build_absence, only: [:create]
   before_action :set_agents
-  before_action { @planning_layout = true }
 
   def index
     @multiple_agents_makes_sense = true
--- app/controllers/admin/planning/plage_ouvertures_controller.rb
+++ app/controllers/admin/planning/plage_ouvertures_controller.rb
@@ -1,11 +1,10 @@
 class Admin::Planning::PlageOuverturesController < AgentAuthController
-  include Admin::Planning::SetAgentsConcern
+  include Admin::Planning::PlanningConcern
   respond_to :html, :json
 
   before_action :set_plage_ouverture, only: %i[show edit update destroy]
   before_action :build_plage_ouverture, only: [:create]
   before_action :set_agents
-  before_action { @planning_layout = true }
 
   def show
     authorize(@plage_ouverture, policy_class: Agent::PlageOuverturePolicy)
