aa640819c11ce5a15745e4092b68c2d98044fa14
François Ferrandis <francois@ferrandis.cool>
2026-05-04 16:46:52 +0200

    Fixup #6342 (Éviter `Notion::Api::Errors::TooManyRequests`)

--- app/jobs/cron_job/synchronize_crm.rb
+++ app/jobs/cron_job/synchronize_crm.rb
@@ -18,20 +18,33 @@ class CronJob::SynchronizeCrm < CronJob
       ],
     }
 
+    jobs = []
     client.database_query(database_id: NOTION_DATABASE_ID, filter:) do |page|
-      page.results.each.with_index do |notion_page, index|
-        # Notion rate-limiting is 3 API calls per second, so we'll execute jobs every 400ms
-        SynchronizeCrmPageJob.set(wait: (index * 0.4).seconds).perform_later(
-          notion_page_id: notion_page.id,
-          account_url: notion_page.properties["COMPTE PROD"].url,
-          notion_page_url: notion_page.url,
-          notion_page_title: notion_page.properties["Project name"]["title"][0]["plain_text"]
-        )
+      page.results.each do |notion_page|
+        jobs << sync_job_for(notion_page)
       end
     end
+
+    # Notion rate-limiting is 3 API calls per second, so we'll execute jobs every 400ms
+    delay = 0.seconds
+    jobs.each do |job|
+      job.set(wait: delay)
+      delay += 0.4.seconds
+    end
+
+    ActiveJob.perform_all_later(jobs)
   end
 
   private
+
+  def sync_job_for(notion_page)
+    SynchronizeCrmPageJob.new(
+      notion_page_id: notion_page.id,
+      account_url: notion_page.properties["COMPTE PROD"].url,
+      notion_page_url: notion_page.url,
+      notion_page_title: notion_page.properties["Project name"]["title"][0]["plain_text"]
+    )
+  end
 
   def instance_hostname
     ENV["HOST"]
