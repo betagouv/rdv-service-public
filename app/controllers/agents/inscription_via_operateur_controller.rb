35831cd547b41fec1284f88f1aca7bb3f4f705fd
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-05-06 16:44:22 +0200

    minor syntax tweak

--- app/controllers/agents/inscription_via_operateur_controller.rb
+++ app/controllers/agents/inscription_via_operateur_controller.rb
@@ -4,14 +4,14 @@ class Agents::InscriptionViaOperateurController < AgentAuthController
   def show
     skip_authorization
 
-    data = session.delete(:inscription_via_operateur)
-    if data&.fetch("operator_name", nil).present? && data&.fetch("signup_url", nil).present?
+    operator_name, signup_url = session.delete(:inscription_via_operateur)&.values_at("operator_name", "signup_url")
+    if operator_name.present? && signup_url.present?
       Sentry.capture_message(
         "ProConnectOnboardingRouter: agent invité à s'inscrire via son opérateur",
         level: "info",
-        extra: { agent_id: current_agent.id, operator_name: data["operator_name"] }
+        extra: { agent_id: current_agent.id, operator_name: }
       )
-      render locals: { operator_name: data["operator_name"], signup_url: data["signup_url"] }
+      render locals: { operator_name:, signup_url: }
     else
       flash[:error] = "Une erreur est survenue lors de la récupération des informations de votre opérateur. Nous vous invitons à contacter leur support."
       redirect_to root_path
