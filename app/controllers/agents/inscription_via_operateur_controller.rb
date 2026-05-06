0bd12d6cd277486b1bbe625f4a697fd060eab7ce
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-05-06 16:36:23 +0200

    Passage des infos opérateur depuis l’API ANCT en session plutot qu'en params

--- app/controllers/agents/inscription_via_operateur_controller.rb
+++ app/controllers/agents/inscription_via_operateur_controller.rb
@@ -4,13 +4,14 @@ class Agents::InscriptionViaOperateurController < AgentAuthController
   def show
     skip_authorization
 
-    if params[:operator_name].present? && params[:signup_url].present?
+    data = session.delete(:inscription_via_operateur)
+    if data&.fetch("operator_name", nil).present? && data&.fetch("signup_url", nil).present?
       Sentry.capture_message(
         "ProConnectOnboardingRouter: agent invité à s'inscrire via son opérateur",
         level: "info",
-        extra: { agent_id: current_agent.id, operator_name: params[:operator_name] }
+        extra: { agent_id: current_agent.id, operator_name: data["operator_name"] }
       )
-      render locals: { operator_name: params[:operator_name], signup_url: params[:signup_url] }
+      render locals: { operator_name: data["operator_name"], signup_url: data["signup_url"] }
     else
       flash[:error] = "Une erreur est survenue lors de la récupération des informations de votre opérateur. Nous vous invitons à contacter leur support."
       redirect_to root_path
