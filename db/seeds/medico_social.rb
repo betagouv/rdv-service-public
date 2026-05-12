6422597fefb22ef752743dd0d9bbf58ee6c0f5fa
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-05-12 14:04:04 +0200

    backport seeds with problem from other branch

--- db/seeds/medico_social.rb
+++ db/seeds/medico_social.rb
@@ -795,6 +795,21 @@ Rdv.create!(
   )
 end
 
+# 11 RDVs binôme Martine+Marco il y a 1 mois : ils apparaissent en tête de liste (tri starts_at ASC)
+# s'il y a des bugs de dedoublonnage ils seront visibles dans la pagination en dev
+11.times do |i|
+  Rdv.create!(
+    starts_at: 1.month.ago.beginning_of_day + 8.hours + (i * 30.minutes),
+    duration_in_min: 30,
+    motif_id: motif_org_paris_nord_pmi_suivi.id,
+    lieu: lieu_org_paris_nord_bolivar,
+    organisation_id: org_paris_nord.id,
+    agent_ids: [agent_org_paris_nord_pmi_martine.id, agent_org_paris_nord_pmi_marco.id],
+    user_ids: [user_org_paris_nord_patricia.id],
+    created_by: agent_org_paris_nord_pmi_martine
+  )
+end
+
 # Insert a lot of rdvs in the past 2 years
 # rubocop:disable Rails/SkipsModelValidations
 rdv_attributes = 1000.times.flat_map do |i|
