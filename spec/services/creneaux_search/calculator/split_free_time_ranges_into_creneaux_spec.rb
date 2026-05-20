7fc97e255333f5b436ccfc31a1ad7823671a833d
François Ferrandis <francois@ferrandis.cool>
2026-05-20 15:37:09 +0200

    Add spec to split_free_time_ranges_into_creneaux_spec.rb

--- spec/services/creneaux_search/calculator/split_free_time_ranges_into_creneaux_spec.rb
+++ spec/services/creneaux_search/calculator/split_free_time_ranges_into_creneaux_spec.rb
@@ -28,13 +28,26 @@ RSpec.describe CreneauxSearch::Calculator::SplitFreeTimeRangesIntoCreneaux do
     end
   end
 
-  context "when passed an overriden duration_in_min" do
+  context "when passed an overridden duration_in_min" do
     let(:free_time_ranges) { [Time.zone.parse("20211027 9:00")..Time.zone.parse("20211027 10:15")] }
     let(:duration_in_min) { 20 }
 
     it "returns slots that fit" do
       expect(creneaux.map(&:starts_at).map { _1.strftime("%H:%M") }).to eq(["09:00", "09:20", "09:40"])
       expect(creneaux.map(&:duration_in_min)).to eq([20, 20, 20])
+    end
+  end
+
+  context "quand la plage définit un intervalle entre chaque RDV" do
+    let(:free_time_ranges) { [Time.zone.parse("2021-10-27 09:00")..Time.zone.parse("2021-10-27 12:00")] }
+
+    before do
+      plage_ouverture.update!(minutes_after_rdvs: 10)
+    end
+
+    it "returns slots that fit" do
+      expect(creneaux.map(&:starts_at).map { _1.strftime("%H:%M") }).to eq(["09:00", "09:40", "10:20", "11:00"])
+      expect(creneaux.map(&:duration_in_min)).to eq([30, 30, 30, 30])
     end
   end
 end
