dac2d3268c7ba33da598cec0d539dadbf0a00ed9
François Ferrandis <francois@ferrandis.cool>
2026-05-28 16:09:43 +0200

    Ajoute dans la spec une vérification sur Rdv#minutes_after_rdv

--- spec/features/agents/rdvs/agent_can_set_an_interval_between_rdvs_spec.rb
+++ spec/features/agents/rdvs/agent_can_set_an_interval_between_rdvs_spec.rb
@@ -12,24 +12,31 @@ RSpec.describe "un agent peut définir un intervalle entre chaque RDV" do
     end
   end
 
-  describe "respect de l'intervalle dans le calcul de créneaux côté agent" do
+  describe "prise de RDV par un agent (classique)" do
     let!(:agent) { create(:agent, basic_role_in_organisations: [motif.organisation]) }
     let!(:motif) { create(:motif, :by_phone, default_duration_in_min: 30) }
     let!(:plage) do
       create(:plage_ouverture, organisation: motif.organisation, agent:, motifs: [motif],
                                minutes_after_rdvs: 10, start_time: Tod::TimeOfDay.new(9), end_time: Tod::TimeOfDay.new(12))
     end
+    let!(:user) { create(:user, organisations: [motif.organisation]) }
 
-    it "fonctionne" do
+    it "respecte l'intervalle dans le calcul de créneaux et enregistre l'intervalle dans le RDV créé" do
       login_as(agent, scope: :agent)
-      visit admin_organisation_creneaux_search_path(organisation_id: motif.organisation)
+      visit admin_organisation_creneaux_search_path(organisation_id: motif.organisation, user_ids: [user.id])
       select(motif.name, from: "motif_id")
       click_button("Afficher les créneaux")
       expect(page.all("a.creneau .strong").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
+
+      click_on("10:20")
+      click_on("Continuer")
+      click_on("Continuer")
+      click_on("Confirmer le RDV")
+      expect(Rdv.last).to have_attributes(minutes_after_rdv: 10)
     end
   end
 
-  describe "respect de l'intervalle dans le calcul de créneaux côté usager" do
+  describe "prise de RDV par un usager" do
     let!(:agent) { create(:agent, basic_role_in_organisations: [motif.organisation]) }
     let!(:motif) { create(:motif, :by_phone, bookable_by: :everyone, default_duration_in_min: 30) }
     let!(:plage) do
@@ -37,14 +44,47 @@ RSpec.describe "un agent peut définir un intervalle entre chaque RDV" do
                                minutes_after_rdvs: 10, start_time: Tod::TimeOfDay.new(9), end_time: Tod::TimeOfDay.new(12))
     end
 
-    it "fonctionne" do
+    it "respecte l'intervalle dans le calcul de créneaux et enregistre l'intervalle dans le RDV créé" do
       visit "/org/#{motif.organisation.public_link_id}"
       click_on motif.name
       expect(page.all("a[autofocus]").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
+
+      click_on("10:20")
+      fill_in("Prénom", with: "Patricia")
+      fill_in("Nom", with: "Duroy")
+      fill_in("Adresse email", with: "patricia_duroy@demo.rdv-solidarites.fr")
+      click_on("Recevoir un code de connexion")
+      fill_in("login_code_code", with: LoginCode.last.code)
+      click_on("Valider")
+      fill_in("Téléphone", with: "0611223344")
+      click_on("Continuer")
+      click_on("Continuer")
+      click_on("Confirmer mon RDV")
+      expect(Rdv.last).to have_attributes(minutes_after_rdv: 10)
+    end
+
+    context "via un prescripteur" do
+      it "respecte l'intervalle dans le calcul de créneaux et enregistre l'intervalle dans le RDV créé" do
+        visit "/org/#{motif.organisation.public_link_id}"
+        click_on motif.name
+        expect(page.all("a[autofocus]").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
+
+        click_on("10:20")
+        click_on("Je suis un prescripteur qui oriente un bénéficiaire")
+        fill_in("Votre prénom", with: "Agent")
+        fill_in("Votre nom", with: "Prescripteur")
+        fill_in("Votre email professionnel", with: "prenom.prescripteur@domaine.fr")
+        click_on("Continuer")
+        fill_in("Prénom", with: "Patricia")
+        fill_in("Nom d’usage", with: "Duroy")
+        fill_in("Téléphone mobile", with: "0611223344")
+        click_on("Confirmer le rendez-vous")
+        expect(Rdv.last).to have_attributes(minutes_after_rdv: 10)
+      end
     end
   end
 
-  describe "respect de l'intervalle dans le calcul de créneaux côté agent prescripteur" do
+  describe "prise de RDV par agent prescripteur" do
     let!(:territory) { create(:territory) }
     let!(:orga_du_rdv) { create(:organisation, territory:) }
     let!(:orga_du_prescripteur) { create(:organisation, territory:) }
@@ -55,14 +95,24 @@ RSpec.describe "un agent peut définir un intervalle entre chaque RDV" do
       create(:plage_ouverture, organisation: orga_du_rdv, agent: agent_du_rdv, motifs: [motif],
                                minutes_after_rdvs: 10, start_time: Tod::TimeOfDay.new(9), end_time: Tod::TimeOfDay.new(12))
     end
+    let!(:user) { create(:user, organisations: [motif.organisation]) }
 
-    it "fonctionne" do
+    before do
+      stub_request(:get, "https://data.geopf.fr/geocodage/search/?q=#{user.address.gsub(' ', '%20')}")
+        .to_return(body: file_fixture("geocode_result.json").read)
+    end
+
+    it "respecte l'intervalle dans le calcul de créneaux et enregistre l'intervalle dans le RDV créé" do
       login_as(agent_prescripteur, scope: :agent)
-      visit admin_organisation_creneaux_search_path(organisation_id: orga_du_prescripteur)
+      visit admin_organisation_creneaux_search_path(organisation_id: orga_du_prescripteur, user_ids: [user.id])
       click_link "Élargir la recherche"
       click_on motif.name
       click_on orga_du_rdv.name
       expect(page.all("a[autofocus]").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
+
+      click_on("10:20")
+      click_on("Confirmer le rdv")
+      expect(Rdv.last).to have_attributes(minutes_after_rdv: 10)
     end
   end
 end
