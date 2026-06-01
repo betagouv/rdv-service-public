0187be3139449fbc7e110375f09195024a2d5157
François Ferrandis <francois@ferrandis.cool>
2026-06-01 18:04:16 +0200

    Ajoute un bouton pour déclencher la création des motifs

--- app/controllers/super_admins/organisations_controller.rb
+++ app/controllers/super_admins/organisations_controller.rb
@@ -1,5 +1,12 @@
 module SuperAdmins
   class OrganisationsController < SuperAdmins::ApplicationController
+    def create_france_connect_motifs
+      Compte.upsert_france_service_motifs!(requested_resource)
+      mass_edit_url = admin_territory_motifs_url(territory_id: requested_resource.territory_id)
+      flash[:success] = "Les motifs France Service on bien été créés. Ils sont éditables en masse ici : #{mass_edit_url}"
+      redirect_to super_admins_organisation_path(id: requested_resource.id)
+    end
+
     def update
       super
 
