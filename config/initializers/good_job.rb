d4de16553366749588f7c22244b210be2d431f24
François Ferrandis <francois.ferrandis@beta.gouv.fr>
2026-06-04 09:56:28 +0200

    Corrige l'usage de cleanup_preserved_jobs_before_seconds_ago (GoodJob) (#6408)
    * Corrige l'usage de cleanup_preserved_jobs_before_seconds_ago (GoodJob)
    
    * Rajoute un commentaire pour aider à la découvrabilité de cette option

--- config/initializers/good_job.rb
+++ config/initializers/good_job.rb
@@ -1,8 +1,9 @@
 Rails.application.configure do
   config.active_job.default_priority = 0
 
+  # La durée de conservation (2 semaines par défaut) est configurable via GOOD_JOB_CLEANUP_PRESERVED_JOBS_BEFORE_SECONDS_AGO
   config.good_job.preserve_job_records = true
-  config.cleanup_preserved_jobs_before_seconds_ago = ENV.fetch("GOOD_JOB_CLEANUP_PRESERVED_JOBS_BEFORE_SECONDS_AGO", 1.week.in_seconds)
+
   config.good_job.on_thread_error = ->(exception) { Sentry.capture_exception(exception) } # this is never called !
   config.good_job.execution_mode = :external
   config.good_job.queues = "latency_30s:1; latency_5m,latency_30s:2; *:2"
