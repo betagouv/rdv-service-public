4373aa6405f5f7b8be6fdf3b7ab81be5a31bc4dd
François Ferrandis <francois@ferrandis.cool>
2026-06-04 11:37:17 +0200

    20260219141217_add_cooldown_to_plage_ouverture.rb -> 20260219141217_add_cooldown_to_plage_ouvertures_and_rdvs.rb

--- db/migrate/20260219141217_add_cooldown_to_plage_ouvertures_and_rdvs.rb
+++ db/migrate/20260219141217_add_cooldown_to_plage_ouvertures_and_rdvs.rb
@@ -1,4 +1,4 @@
-class AddCooldownToPlageOuverture < ActiveRecord::Migration[8.0]
+class AddCooldownToPlageOuverturesAndRdvs < ActiveRecord::Migration[8.0]
   def change
     add_column :plage_ouvertures, :minutes_after_rdvs, :integer, default: 0, null: false
     add_column :rdvs, :minutes_after_rdv, :integer, default: 0, null: false
