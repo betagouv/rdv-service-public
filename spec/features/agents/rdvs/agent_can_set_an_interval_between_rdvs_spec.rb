b27e71305652ea1f98ebfac0791481103693677b
François Ferrandis <francois@ferrandis.cool>
2026-06-09 16:50:50 +0200

    Renomme en "battement après"

--- spec/features/agents/rdvs/agent_can_set_an_interval_between_rdvs_spec.rb
+++ spec/features/agents/rdvs/agent_can_set_an_interval_between_rdvs_spec.rb
@@ -1,14 +1,14 @@
-RSpec.describe "un agent peut définir un temps de pause entre chaque RDV" do
+RSpec.describe "un agent peut définir un temps de battement entre chaque RDV" do
   before { travel_to(Time.zone.parse("2026-05-29 08:00")) }
 
-  describe "configuration de du temps de pause" do
+  describe "configuration de du temps de battement" do
     let!(:agent) { create(:agent, basic_role_in_organisations: [motif.organisation]) }
     let!(:motif) { create(:motif, :by_phone, default_duration_in_min: 30) }
 
     it "fonctionne" do
       login_as(agent, scope: :agent)
       visit new_admin_organisation_planning_plage_ouverture_path(organisation_id: motif.organisation)
-      select("10 min.", from: "Temps de pause après chaque rendez-vous")
+      select("10 min.", from: "Temps de battement après chaque rendez-vous")
       click_on "Créer la plage d'ouverture"
       expect(PlageOuverture.last.minutes_after_rdvs).to eq(10)
     end
@@ -23,7 +23,7 @@ RSpec.describe "un agent peut définir un temps de pause entre chaque RDV" do
     end
     let!(:user) { create(:user, organisations: [motif.organisation]) }
 
-    it "respecte le temps de pause dans le calcul de créneaux et enregistre le temps de pause dans le RDV créé" do
+    it "respecte le temps de battement dans le calcul de créneaux et enregistre le temps de battement dans le RDV créé" do
       login_as(agent, scope: :agent)
       visit admin_organisation_creneaux_search_path(organisation_id: motif.organisation, user_ids: [user.id])
       select(motif.name, from: "motif_id")
@@ -46,7 +46,7 @@ RSpec.describe "un agent peut définir un temps de pause entre chaque RDV" do
                                minutes_after_rdvs: 10, start_time: Tod::TimeOfDay.new(9), end_time: Tod::TimeOfDay.new(12))
     end
 
-    it "respecte le temps de pause dans le calcul de créneaux et enregistre le temps de pause dans le RDV créé" do
+    it "respecte le temps de battement dans le calcul de créneaux et enregistre le temps de battement dans le RDV créé" do
       visit "/org/#{motif.organisation.public_link_id}"
       click_on motif.name
       expect(page.all("a[autofocus]").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
@@ -66,7 +66,7 @@ RSpec.describe "un agent peut définir un temps de pause entre chaque RDV" do
     end
 
     context "via un prescripteur" do
-      it "respecte le temps de pause dans le calcul de créneaux et enregistre le temps de pause dans le RDV créé" do
+      it "respecte le temps de battement dans le calcul de créneaux et enregistre le temps de battement dans le RDV créé" do
         visit "/org/#{motif.organisation.public_link_id}"
         click_on motif.name
         expect(page.all("a[autofocus]").map(&:text)).to eq(["09:00", "09:40", "10:20", "11:00"])
@@ -104,7 +104,7 @@ RSpec.describe "un agent peut définir un temps de pause entre chaque RDV" do
         .to_return(body: file_fixture("geocode_result.json").read)
     end
 
-    it "respecte le temps de pause dans le calcul de créneaux et enregistre le temps de pause dans le RDV créé" do
+    it "respecte le temps de battement dans le calcul de créneaux et enregistre le temps de battement dans le RDV créé" do
       login_as(agent_prescripteur, scope: :agent)
       visit admin_organisation_creneaux_search_path(organisation_id: orga_du_prescripteur, user_ids: [user.id])
       click_link "Élargir la recherche"
