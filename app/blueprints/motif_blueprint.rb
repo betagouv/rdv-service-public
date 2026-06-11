4ea02348e4b4975c830ec67666eebefe851cf3aa
Romain Neuville <neuvilleromain@protonmail.com>
2026-06-11 10:53:15 +0200

    Ajoute min/max_public_booking_delay au blueprint des motifs

--- app/blueprints/motif_blueprint.rb
+++ app/blueprints/motif_blueprint.rb
@@ -1,6 +1,8 @@
 class MotifBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :name, :location_type, :deleted_at, :bookable_publicly, :service_id, :organisation_id, :collectif, :follow_up, :instruction_for_rdv, :bookable_by, :default_duration_in_min
+  fields :name, :location_type, :deleted_at, :bookable_publicly, :service_id, :organisation_id, :collectif,
+         :follow_up, :instruction_for_rdv, :bookable_by, :default_duration_in_min,
+         :min_public_booking_delay, :max_public_booking_delay
   association :motif_category, blueprint: MotifCategoryBlueprint
 end
