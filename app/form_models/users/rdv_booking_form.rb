16808d725e26988f64a06378ef01f4039470cc20
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-06-12 10:16:46 +0200

    RdvBookingForm#save créé maintenant le RDV (ou la participation)
    on déplace la logique depuis users/rdvs#create
    et on copie celle depuis users/participations#create

--- app/form_models/users/rdv_booking_form.rb
+++ app/form_models/users/rdv_booking_form.rb
@@ -1,11 +1,11 @@
 class Users::RdvBookingForm
   include Users::UserFormConcern
 
-  attr_reader :rdv_builder
+  attr_reader :rdv_builder, :invitation_token, :rdv
 
-  delegate :to_query, :motif, :service, :rdv, to: :rdv_builder
-  delegate :add_benign_error, :ignore_benign_errors, to: :user
-  delegate :requires_ants_predemande_number?, to: :rdv
+  delegate :to_query, :motif, :service, to: :rdv_builder
+  delegate :add_benign_error, :ignore_benign_errors, :relatives_attributes=, to: :user
+  delegate :collectif?, :requires_ants_predemande_number?, to: :rdv
 
   validates :ants_pre_demande_number, presence: true, if: :requires_ants_predemande_number?
   validates_with AntsPreDemandeNumberStatusValidation, if: :requires_ants_predemande_number?
@@ -15,12 +15,21 @@ class Users::RdvBookingForm
   def initialize(user:, rdv_builder:, domain:, user_attributes: {})
     @user = user
     @rdv_builder = rdv_builder
+    @rdv = rdv_builder.rdv
     @domain = domain
     @user.assign_attributes(user_attributes)
   end
 
   def save
-    valid? && @user.save
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      @user.save!
+      rdv.collectif? ? create_participation : create_individual_rdv
+    end
+    true
+  rescue ActiveRecord::RecordInvalid
+    false
   end
 
   def show_birth_date_field? = !signed_in_with_invitation_token? && rdv.territory&.enable_birth_date_field?
@@ -41,9 +50,29 @@ class Users::RdvBookingForm
 
   def ants_meeting_point_id = rdv_builder.lieu_id
 
+  def new_participation
+    # user_id: plutôt que user: pour éviter que inverse_of ajoute @new_participation à @user.participations
+    # ce qui déclencherait un autosave prématuré lors de @user.save! et ferait échouer create_and_notify!
+    @new_participation ||= Participation.new(rdv:, user_id: users_for_rdv.first&.id, created_by: @user)
+  end
+
   private
 
   def validate_phone_number_present_for_motif_by_phone
     errors.add(:phone_number, :missing_for_phone_motif) if rdv.motif.phone? && user.phone_number.blank?
+  end
+
+  def create_individual_rdv
+    @rdv = rdv_builder.creneau.build_rdv # TODO: ce comportement est extrêmement surprenant, à refacto avec le RdvBuilder
+    @rdv.assign_attributes(users: users_for_rdv, created_by: @user)
+    @rdv.save!
+    notifier = Notifiers::RdvCreated.new(@rdv, @user)
+    notifier.perform
+    @invitation_token = notifier.participations_tokens_by_user_id[@user.id]
+  end
+
+  def create_participation
+    new_participation.create_and_notify!(@user)
+    @invitation_token = new_participation.restricted_auth_token
   end
 end
