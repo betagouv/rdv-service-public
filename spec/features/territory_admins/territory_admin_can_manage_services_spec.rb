76e78dd0b406f8a965710896c6c48d2d7db9c489
François Ferrandis <francois@ferrandis.cool>
2026-06-17 10:38:35 +0200

    Ajoute un formulaire pour créer un nouveau service dans un espace

--- spec/features/territory_admins/territory_admin_can_manage_services_spec.rb
+++ spec/features/territory_admins/territory_admin_can_manage_services_spec.rb
@@ -1,15 +1,16 @@
 RSpec.describe "territory admin can manage services", type: :feature do
   let!(:territory) { create(:territory) }
   let!(:agent) { create(:agent, role_in_territories: [territory]) }
-  let!(:service_a) { create(:service) }
-  let!(:service_b) { create(:service) }
-  let!(:service_c) { create(:service) }
 
   before do
     login_as(agent, scope: :agent)
   end
 
   describe "Listing services" do
+    let!(:service_a) { create(:service) }
+    let!(:service_b) { create(:service) }
+    let!(:service_c) { create(:service) }
+
     it "works" do
       visit edit_admin_territory_services_path(territory)
 
@@ -21,6 +22,10 @@ RSpec.describe "territory admin can manage services", type: :feature do
   end
 
   describe "Activating/Deactivating services" do
+    let!(:service_a) { create(:service) }
+    let!(:service_b) { create(:service) }
+    let!(:service_c) { create(:service) }
+
     it "works" do
       visit edit_admin_territory_services_path(territory)
       check service_a.name
@@ -38,6 +43,38 @@ RSpec.describe "territory admin can manage services", type: :feature do
         territory.reload.services.ids
       }.from([service_a.id, service_b.id]).to([service_a.id, service_c.id])
       expect(page).to have_content("Liste des services disponibles mise à jour")
+    end
+  end
+
+  describe "creating a new service" do
+    it "works" do
+      visit new_admin_territory_services_path(territory_id: territory.id)
+      fill_in "Intitulé du nouveau service", with: "Protection maternelle et infantile"
+      fill_in "Intitulé court du nouveau service", with: "PMI"
+      expect { click_on("Ajouter le nouveau service") }.to change(Service, :count).by(1).and(change { territory.services.reload.size }.by(1))
+      expect(Service.last).to have_attributes(name: "Protection maternelle et infantile", short_name: "PMI")
+      expect(page).to have_content(%(Le service "Protection maternelle et infantile (PMI)" vient d'être créé et activé dans votre espace.))
+    end
+
+    describe "validations" do
+      it "displays error message if the names are too short or too long", type: :request do
+        post admin_territory_services_path(territory_id: territory.id), params: { service: { name: "a" } }
+        expect(response.body).to include("Le nom du service doit contenir au moins 2 caractères.")
+        expect(response.body).to include("Le nom raccourci pour les SMS doit contenir au moins 2 caractères.")
+
+        post admin_territory_services_path(territory_id: territory.id), params: { service: { name: "a" * 70, short_name: "a" * 50 } }
+        expect(response.body).to include("Le nom du service ne doit pas dépasser 60 caractères.")
+        expect(response.body).to include("Le nom raccourci pour les SMS ne doit pas dépasser 40 caractères.")
+      end
+
+      it "displays error message if the name already exists" do
+        create(:service, name: "Protection maternelle et infantile")
+        visit new_admin_territory_services_path(territory_id: territory.id)
+        fill_in "Intitulé du nouveau service", with: "Protection maternelle et infantile"
+        fill_in "Intitulé court du nouveau service", with: "PMI"
+        click_on("Ajouter le nouveau service")
+        expect(page).to have_content("Le nom du service est déjà présent dans la liste de services existants")
+      end
     end
   end
 end
