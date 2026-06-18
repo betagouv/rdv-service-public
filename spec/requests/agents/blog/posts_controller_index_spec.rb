623e55f6ae9bf73c30435433fd232a0a04e7ccf2
Victor Mours <victor.mours@gmail.com>
2026-06-18 18:15:22 +0200

    update blog posts specs

--- spec/requests/agents/blog/posts_controller_index_spec.rb
+++ spec/requests/agents/blog/posts_controller_index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Agents::Blog::PostsController, "#index" do
 
   it "displays all posts" do
     create(:blog_post, title: "Un titre de post", description: "Une description de post", external_url: "https://example.com")
-    get agents_blog_posts_path
+    get agents_blog_posts_path(format: "turbo_stream")
 
     expect(response.body).to include("Un titre de post")
     expect(response.body).to include("Une description de post")
@@ -14,13 +14,13 @@ RSpec.describe Agents::Blog::PostsController, "#index" do
 
   it "updates the agent's blog_read_at" do
     expect do
-      get agents_blog_posts_path
+      get agents_blog_posts_path(format: "turbo_stream")
     end.to change { agent.reload.blog_read_at }
   end
 
   context "when no posts in DB" do
     it "declares that no posts were found" do
-      get agents_blog_posts_path
+      get agents_blog_posts_path(format: "turbo_stream")
       expect(response.body).to include("Aucune nouveauté")
     end
   end
