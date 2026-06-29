da6d55747e7063b269ac7ef0d69ade1b4ddb4268
Victor Mours <victor.mours@gmail.com>
2026-06-29 16:36:06 +0200

    fix script

--- scripts/move_etat_organisations_to_new_domain.rb
+++ scripts/move_etat_organisations_to_new_domain.rb
@@ -5,7 +5,7 @@
 redirection_rules = Object.new.extend(DomainRedirectionAfterLogin)
 
 Organisation.where(verticale: :rdv_mairie).find_each do |organisation|
-  admins = Agent.joins(agent_roles: :organisation).where(agent_roles: { organisation_id: organisation.id, access_level: :admin })
+  admins = Agent.joins(roles: :organisation).where(agent_roles: { organisation_id: organisation.id, access_level: :admin })
 
   all_admins_from_etat = admins.all? do |agent|
     redirection_rules.should_redirect_to_domain_etat?(Domain::RDV_SERVICE_PUBLIC, agent)
