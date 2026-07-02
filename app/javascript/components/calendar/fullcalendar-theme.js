c9a5285cb9f4d92d2697ba50115cf7e291ac04db
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-07-02 11:18:28 +0200

    Fix hover and title

--- app/javascript/components/calendar/fullcalendar-theme.js
+++ app/javascript/components/calendar/fullcalendar-theme.js
@@ -4,11 +4,12 @@
 // Voir : https://v7.fullcalendar.io/custom-themes
 
 export const rdvTheme = {
-  toolbarTitleClass: "rdv-fc-toolbar-title",
+  toolbarTitleClass: "rdv-fc-toolbar-title fr-h6",
   buttonClass: "rdv-fc-button",
   // hasSelection est true pour le groupe des sélecteurs de vue (Mois/Semaine/…),
   // false pour les groupes de navigation (Aujourd'hui/</>)
   buttonGroupClass: (info) => info.hasSelection ? "rdv-fc-view-group" : "rdv-fc-nav-group",
+  blockEventClass: "rdv-fc-block-event",
   backgroundEventClass: "rdv-fc-background-event",
   backgroundEventTitleClass: "rdv-fc-background-event-title",
 }
