87e6e4a33c6871bdf7b4d0bacb7233aafe8488cb
Victor Mours <victor.mours@beta.gouv.fr>
2026-07-10 16:35:20 +0200

    Ajout d'une api de gestion des webhooks pour visioplainte (#6517)
    * wip
    
    * working index
    
    * create endpoint
    
    * update working
    
    * destroy working
    
    * Update swagger doc
    
    * lint fix
    
    * lint fix for real
    
    * Add spec for id injections

---
+++ app/controllers/api/visioplainte/webhook_endpoints_controller.rb
@@ -0,0 +1,38 @@
+# cf docs/interconnexions/visioplainte.md
+
+class Api::Visioplainte::WebhookEndpointsController < Api::Visioplainte::BaseController
+  def index
+    webhook_endpoints = authorized_webhook_endpoint_scope.order(:id)
+    render json: WebhookEndpointBlueprint.render(webhook_endpoints, root: :webhook_endpoints)
+  end
+
+  def create
+    @webhook_endpoint = WebhookEndpoint.new(permitted_params)
+    @webhook_endpoint.organisation_id = Territory.visioplainte.first.organisations.sole.id
+    @webhook_endpoint.save!
+    render json: WebhookEndpointBlueprint.render(@webhook_endpoint), status: :created
+  end
+
+  def update
+    @webhook_endpoint = authorized_webhook_endpoint_scope.find(params[:id])
+    @webhook_endpoint.update!(permitted_params)
+    render json: WebhookEndpointBlueprint.render(@webhook_endpoint), status: :ok
+  end
+
+  def destroy
+    @webhook_endpoint = authorized_webhook_endpoint_scope.find(params[:id])
+    @webhook_endpoint.destroy!
+
+    head :no_content
+  end
+
+  private
+
+  def authorized_webhook_endpoint_scope
+    WebhookEndpoint.joins(organisation: :territory).merge(Territory.visioplainte)
+  end
+
+  def permitted_params
+    params.permit(:target_url, :secret, subscriptions: [])
+  end
+end
