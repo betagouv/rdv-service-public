75ffffec0e2b735354ebca939408b564e433e1e7
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-07-28 08:38:53 +0200

    Corrige le NoMethodError dans to_query_for_search_redirection sans motif

--- app/services/users/rdv_builder.rb
+++ app/services/users/rdv_builder.rb
@@ -68,8 +68,8 @@ class Users::RdvBuilder
 
   def to_query_for_search_redirection
     q = @attributes.slice(:address, :city_code, :street_ban_id, :departement, :organisation_ids, :ants_pre_demandes_count).merge(
-      service: motif.service_id,
-      motif_name_with_location_type: motif.name_with_location_type
+      service: motif&.service_id,
+      motif_name_with_location_type: motif&.name_with_location_type
     )
     q[:address] ||= @attributes[:where]
     q
