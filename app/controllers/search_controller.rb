cec0dca2755f3c3e3dc923d21c12a3e7cc5d73e8
Victor Mours <victor.mours@gmail.com>
2026-08-12 17:09:08 +0200

    fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,8 +5,7 @@ class SearchController < ApplicationController
   include Search::LogParamsConcern
 
   prepend_before_action(only: %i[search_rdv]) do
-    store_restricted_auth_in_session_and_redirect
-    store_rdv_insertion_invitation_in_session
+    store_invitation_in_session_and_redirect(store_rdv_insertion_invitation: true)
   end
 
   def home
