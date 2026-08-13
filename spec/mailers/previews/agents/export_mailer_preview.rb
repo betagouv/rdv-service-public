3bf91bd12833ee257599077cd9edc47e3222a2b4
François Ferrandis <francois@ferrandis.cool>
2026-08-13 11:20:18 +0200

    Factorise Agents::ExportMailer

--- spec/mailers/previews/agents/export_mailer_preview.rb
+++ spec/mailers/previews/agents/export_mailer_preview.rb
@@ -1,9 +1,5 @@
 class Agents::ExportMailerPreview < ActionMailer::Preview
-  def rdv_export
-    Agents::ExportMailer.rdv_export(Export.last)
-  end
-
-  def participations_export
-    Agents::ExportMailer.participations_export(Export.last)
+  def export_ready
+    Agents::ExportMailer.export_ready(Export.last)
   end
 end
