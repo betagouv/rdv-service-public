36d7d38ec9223f10c502478f7c973dc2263d9ba0
Victor Mours <victor.mours@beta.gouv.fr>
2026-08-18 16:35:00 +0200

    Forwarder les emails aux admins ayant créé les intervenant en cas de réponse des usagers (#6613)
    Send email to admins when reply to a notification for rdv with intervenant

--- spec/mailers/previews/agents/reply_transfer_mailer_preview.rb
+++ spec/mailers/previews/agents/reply_transfer_mailer_preview.rb
@@ -27,4 +27,33 @@ class Agents::ReplyTransferMailerPreview < ActionMailer::Preview
       source_mail: source_mail
     )
   end
+
+  def notify_organisation
+    rdv = Rdv.last
+
+    source_mail = Mail.new do
+      subject "Re: RDV confirmé le #{I18n.l(rdv.starts_at, format: :human)}"
+
+      text_part do
+        body "Bonjour,\nVoici une phrase après un saut de ligne."
+      end
+
+      attachments["signature.svg"] = { mime_type: "image/svg+xml", content: "" }
+    end
+
+    body = <<~MARKDOWN
+      Bonjour,
+      Voici une phrase après un saut de ligne.
+
+      Voici une autre phrase après deux sauts de ligne (saut de paragraphe)
+    MARKDOWN
+
+    Agents::ReplyTransferMailer.notify_organisation(
+      rdv: rdv,
+      author: rdv.users.first,
+      organisation: rdv.organisation,
+      reply_body: body,
+      source_mail: source_mail
+    )
+  end
 end
