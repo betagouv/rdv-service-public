c9e593113e79f24baa6aad05d5b387cd667f42fa
Victor Mours <victor.mours@gmail.com>
2026-09-02 10:55:09 +0200

    refactor policy

--- app/policies/agent/rdv_invitation_policy.rb
+++ app/policies/agent/rdv_invitation_policy.rb
@@ -1,11 +1,29 @@
 class Agent::RdvInvitationPolicy < ApplicationPolicy
+  alias current_agent pundit_user
+  alias rdv_invitation record
+
   def create?
-    pundit_user == record.inviting_agent &&
-      (record.lieu_id.blank? || Agent::LieuPolicy.new(pundit_user, record.lieu).show?) &&
-      Agent::UserPolicy.new(AgentOrganisationContext.new(pundit_user, record.organisation), record.user).show? &&
-      Agent::MotifPolicy.new(pundit_user, record.motif).show?
+    same_agent? && can_show_lieu? && can_show_user? && can_show_motif?
   end
 
   alias new? create?
   alias show? create?
+
+  private
+
+  def same_agent?
+    current_agent == rdv_invitation.inviting_agent
+  end
+
+  def can_show_lieu?
+    rdv_invitation.lieu.blank? || Agent::LieuPolicy.new(current_agent, rdv_invitation.lieu).show?
+  end
+
+  def can_show_user?
+    Agent::UserPolicy.new(AgentOrganisationContext.new(current_agent, rdv_invitation.organisation), rdv_invitation.user).show?
+  end
+
+  def can_show_motif?
+    Agent::MotifPolicy.new(current_agent, rdv_invitation.motif).show?
+  end
 end
