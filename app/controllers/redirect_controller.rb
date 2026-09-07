1bf21dea8f6fc36f64da0293ab603159d7679989
Adrien Di Pasquale <adrien.di_pasquale@beta.gouv.fr>
2026-09-07 15:14:16 +0200

    stop passing address in reprendre rdv redirection

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -29,7 +29,6 @@ class RedirectController < ApplicationController
       motif_name_with_location_type: rdv.motif.name_with_location_type,
       public_link_organisation_id: rdv.organisation_id,
       lieu_id: rdv.lieu_id,
-      address: rdv.address,
       invitation_token: token
     )
   end
