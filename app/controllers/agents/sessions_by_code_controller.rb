d1b093ea4a50e915417c219d821267ee75769fd9
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-09-07 11:21:39 +0200

    Remove resend parameter

--- app/controllers/agents/sessions_by_code_controller.rb
+++ app/controllers/agents/sessions_by_code_controller.rb
@@ -10,7 +10,8 @@ class Agents::SessionsByCodeController < ApplicationController
   end
 
   def resend
-    Agents::LoginCodeSender.perform(email: pending_agent.email, domain_id: current_domain.id, resend: true)
+    UnblockBrevoTransactionalContact.new(pending_agent.email).call
+    Agents::LoginCodeSender.perform(email: pending_agent.email, domain_id: current_domain.id)
     redirect_to new_agents_sessions_by_code_path
   end
 
--- app/services/agents/login_code_sender.rb
+++ app/services/agents/login_code_sender.rb
@@ -1,8 +1,7 @@
 class Agents::LoginCodeSender
-  def self.perform(email:, domain_id:, resend: false)
+  def self.perform(email:, domain_id:)
     return if LoginCode.most_recent_usable_for(email:)&.very_recent?
 
-    UnblockBrevoTransactionalContact.new(email).call if resend
     login_code = LoginCode.create!(email:, domain_id:)
     Agents::LoginCodeMailer.with(login_code:).login_code.deliver_later
   end
