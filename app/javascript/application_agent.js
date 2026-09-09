274c900d6ee1dceb6c51811cb92f308ad9c3f370
Victor Mours <victor.mours@gmail.com>
2026-09-09 22:58:56 +0200

    less js

--- app/javascript/application_agent.js
+++ app/javascript/application_agent.js
@@ -32,7 +32,7 @@ import { AgendaMonoAgent } from './components/calendar'
 import { AgendaMultiAgent} from './components/calendar/agenda-multi-agent'
 import { AgendaPlageOuverture} from './components/agenda_plage_ouverture'
 import { ParticipationSelect } from './components/rdv-user-select'
-import { RdvInvitationUserSelect } from './components/rdv-invitation-user-select'
+import RdvInvitationUserSelect from './components/rdv-invitation-user-select'
 import { Tooltips } from './components/tooltips'
 import { PlageOuvertureLieuSelection, PlageOuvertureSecondaryTimes } from './components/plage_ouverture.js'
 import { CheckAll, UnCheckAll } from './components/check-all'
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function() {
   new SubmitOnChange()
 
   new RdvWizardStep2()
-  new RdvInvitationUserSelect()
+  RdvInvitationUserSelect()
 
   new RdvLieu()
 
--- app/javascript/components/rdv-invitation-user-select.js
+++ app/javascript/components/rdv-invitation-user-select.js
@@ -1,32 +1,9 @@
-class RdvInvitationUserSelect {
-
-  constructor() {
-    if (!$('.js-rdv-invitation-user-select').length) { return; }
-
-    this.canonicalUrlStr = document.querySelector("input[name=js-canonical-path]").value;
-
-    this.attachAddUserListener()
-  }
-
-  getCanonicalUrl = () => new URL(this.canonicalUrlStr, window.location)
-
-  getCanonicalUrlSearchParams = () =>
-    new URLSearchParams(this.getCanonicalUrl().search.substr(1))
-
-  attachAddUserListener = () => {
-    console.log("coucou")
-    $('.js-rdv-invitation-user-select').on('select2:select', (e) => {
-      const urlSearchParams = this.getCanonicalUrlSearchParams()
-      urlSearchParams.append("user_id", e.params.data.id)
-      this.redirectToUrlWithSearchParams(urlSearchParams)
-    });
-  }
-
-  redirectToUrlWithSearchParams = (urlSearchParams) => {
-    const canonicalUrl = this.getCanonicalUrl()
-    canonicalUrl.search = urlSearchParams.toString();
-    window.location = canonicalUrl;
-  }
+export default () => {
+  document.querySelectorAll('.js-rdv-invitation-user-select').forEach(elt => {
+    $(elt).on('select2:select', (e) => {
+      let url = new URL(window.location.href);
+      url.searchParams.append("user_id", e.params.data.id);
+      window.location.href = url;
+    })
+  });
 }
-
-export { RdvInvitationUserSelect };
