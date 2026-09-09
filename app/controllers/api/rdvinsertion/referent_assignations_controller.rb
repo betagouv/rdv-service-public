0664874941510e627c41f143a46e8c8e8546fc33
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-09-09 14:12:19 +0200

    Remove join org verticale RDV-I

--- app/controllers/api/rdvinsertion/referent_assignations_controller.rb
+++ app/controllers/api/rdvinsertion/referent_assignations_controller.rb
@@ -25,8 +25,7 @@ class Api::Rdvinsertion::ReferentAssignationsController < Api::Rdvinsertion::Age
   end
 
   def set_user
-    @user = User.joins(:organisations).where(organisations: { verticale: "rdv_insertion" })
-      .distinct.find(referent_assignations_params[:user_id])
+    @user = User.find(referent_assignations_params[:user_id])
     authorize(@user, :show?, policy_class: Agent::UserPolicy)
   rescue ActiveRecord::RecordNotFound
     render_error :not_found, not_found: :user
