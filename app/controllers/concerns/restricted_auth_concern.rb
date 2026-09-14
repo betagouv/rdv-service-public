14318958854ccc8432928fabbb8a31ce1d131391
aminedhobb <amine.dhobb@gmail.com>
2026-09-14 23:35:55 +0200

    extract logic after storing token in separate methodes

--- app/controllers/concerns/restricted_auth_concern.rb
+++ app/controllers/concerns/restricted_auth_concern.rb
@@ -51,23 +51,24 @@ module RestrictedAuthConcern
     return redirect_with_error(t("devise.invitations.current_user_mismatch")) if current_user_mismatch?(user)
 
     if invited_user
-      session[:rdv_insertion_invitation] = current_url_params.except(:invitation_token)
-
-      RestrictedAuthSessionState.authenticate!(session, user_id: invited_user.id)
-
-      redirect_to current_path_without_token
+      start_rdv_insertion_invitation(invited_user)
     else
-      # L'usager a utilisé un lien avec un token envoyé dans une notification
-      # On fait vérifier le début du nom
-      RestrictedAuthSessionState.prepare_for_name_verification!(
-        session,
-        user_id: user.id,
-        rdv_id: participation.rdv_id
-      )
-      session[:return_to_after_verification] = current_path_without_token
-
-      redirect_to new_users_user_name_initials_verification_path
+      start_name_verification(user, participation.rdv_id)
     end
+  end
+
+  # Invitation RDV Insertion : on connecte l'usager et on garde les params d'invitation en session
+  def start_rdv_insertion_invitation(invited_user)
+    session[:rdv_insertion_invitation] = current_url_params.except(:invitation_token)
+    RestrictedAuthSessionState.authenticate!(session, user_id: invited_user.id)
+    redirect_to current_path_without_token
+  end
+
+  # L'usager a utilisé un lien avec un token envoyé dans une notification : on fait vérifier le début du nom
+  def start_name_verification(user, rdv_id)
+    RestrictedAuthSessionState.prepare_for_name_verification!(session, user_id: user.id, rdv_id: rdv_id)
+    session[:return_to_after_verification] = current_path_without_token
+    redirect_to new_users_user_name_initials_verification_path
   end
 
   def current_path_without_token
