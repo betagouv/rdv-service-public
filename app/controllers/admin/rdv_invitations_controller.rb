ced98eff1c630de8bd3d0003c5f592387f145123
Victor Mours <victor.mours@gmail.com>
2026-09-14 16:02:58 +0200

    distinct views for details and confirmation

--- app/controllers/admin/rdv_invitations_controller.rb
+++ app/controllers/admin/rdv_invitations_controller.rb
@@ -11,10 +11,16 @@ class Admin::RdvInvitationsController < AgentAuthController
     if @rdv_invitation.save
       Users::RdvInvitationMailer.with(rdv_invitation: @rdv_invitation).new_invitation.deliver_later
 
-      redirect_to admin_organisation_rdv_invitation_path(current_organisation, @rdv_invitation)
+      flash[:success] = "Invitation envoyée"
+      redirect_to show_confirmation_admin_organisation_rdv_invitation_path(current_organisation, @rdv_invitation)
     else
       render :new
     end
+  end
+
+  def show_confirmation
+    @rdv_invitation = current_organisation.rdv_invitations.find(params[:id])
+    authorize(@rdv_invitation, :show?, policy_class: Agent::RdvInvitationPolicy)
   end
 
   def show
