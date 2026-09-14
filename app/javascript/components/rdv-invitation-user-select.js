339d3ee9a72ae34cb3ee4518808822754927138a
Victor Mours <victor.mours@gmail.com>
2026-09-14 10:53:52 +0200

    Add comment to explain usage of jquery

--- app/javascript/components/rdv-invitation-user-select.js
+++ app/javascript/components/rdv-invitation-user-select.js
@@ -1,5 +1,6 @@
 export default () => {
   document.querySelectorAll('.js-rdv-invitation-user-select').forEach(elt => {
+    // Les events de select2 ne semblent être utilisables que via le système d'events de jQuery, et pas avec un addEventListener normal
     $(elt).on('select2:select', (e) => {
       let url = new URL(window.location.href);
       url.searchParams.append("user_id", e.params.data.id);
