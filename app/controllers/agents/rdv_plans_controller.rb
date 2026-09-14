b294181b674e2eb6e013e27f759adad47325b9af
Victor Mours <victor.mours@gmail.com>
2026-09-14 10:43:10 +0200

    reset lieu when changing motif

--- app/controllers/agents/rdv_plans_controller.rb
+++ app/controllers/agents/rdv_plans_controller.rb
@@ -20,6 +20,7 @@ class Agents::RdvPlansController < AgentAuthController
 
     @rdv_plan.assign_attributes(rdv_plan_params)
     @rdv_plan.duration_in_minutes = @rdv_plan.motif.default_duration_in_min
+    @rdv_plan.lieu_id = nil # On reset le lieu au cas où on passe à un motif sans lieu
 
     authorize(@rdv_plan, :edit?, policy_class: Agent::RdvPlanPolicy)
 
