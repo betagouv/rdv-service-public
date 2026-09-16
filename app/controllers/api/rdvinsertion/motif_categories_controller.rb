e53937c250d2ac2277aaa8ccb2a7832a9b4887d7
aminedhobb <amine.dhobb@gmail.com>
2026-09-16 12:45:41 +0200

    fix(api): Restrict rdvinsertion endpoints permissions

--- app/controllers/api/rdvinsertion/motif_categories_controller.rb
+++ app/controllers/api/rdvinsertion/motif_categories_controller.rb
@@ -1,11 +1,22 @@
 class Api::Rdvinsertion::MotifCategoriesController < Api::Rdvinsertion::AgentAuthBaseController
+  before_action :ensure_agent_is_rdv_insertion_super_admin, only: [:create]
+
   def create
-    # les agents ne peuvent pas lancer cette action, seuls les superadmins rdv-insertion peuvent l'utiliser
     motif_category = MotifCategory.create!(motif_categories_params)
     render_record motif_category
   end
 
   private
+
+  def ensure_agent_is_rdv_insertion_super_admin
+    return if rdv_insertion_super_admin_emails&.include?(current_agent.email)
+
+    render_error :forbidden, error: :forbidden
+  end
+
+  def rdv_insertion_super_admin_emails
+    ENV["RDV_INSERTION_SUPER_ADMIN_EMAILS"]&.split(",")&.map(&:strip)
+  end
 
   def motif_categories_params
     params.permit(:name, :short_name)
