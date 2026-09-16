7c764faa7ef94945cb5d5bc15e43fc9083eb29bf
Victor Mours <victor.mours@gmail.com>
2026-09-16 16:53:27 +0200

    Add specs

--- spec/models/rdv_plan_spec.rb
+++ spec/models/rdv_plan_spec.rb
@@ -1,4 +1,37 @@
 RSpec.describe RdvPlan do
+  describe "#create_rdv_or_send_invitation" do
+    context "for an invitation" do
+      let(:user) { create(:user, email: nil) }
+
+      let(:organisation) { create(:organisation) }
+      let(:motif) { create(:motif, organisation:) }
+      let(:lieu) { create(:lieu, organisation:) }
+
+      let(:rdv_plan) { create(:rdv_plan, by_invitation: true, user:, motif:, lieu:) }
+
+      it "creates an invitation" do
+        rdv_plan.create_rdv_or_send_invitation(user_attributes: { email: "francis@factice.org" })
+        expect(rdv_plan.reload.rdv_invitation).to have_attributes(
+          motif: rdv_plan.motif,
+          lieu: rdv_plan.lieu,
+          user: rdv_plan.user,
+          persisted?: true
+        )
+      end
+
+      it "sends an email" do
+        rdv_plan.create_rdv_or_send_invitation(user_attributes: { email: "francis@factice.org" })
+        perform_enqueued_jobs
+        expect(ActionMailer::Base.deliveries.map(&:to).flatten).to include("francis@factice.org")
+      end
+
+      it "adds the user to the organisation to make sure they are visible" do
+        rdv_plan.create_rdv_or_send_invitation(user_attributes: { email: "francis@factice.org" })
+        expect(user.reload.organisations).to eq [organisation]
+      end
+    end
+  end
+
   describe "#return_url" do
     let(:application) do
       create(:oauth_application,
