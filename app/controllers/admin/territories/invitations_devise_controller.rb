ae7b09e1e28a84fdd0ecc4eda6f84fce68802898
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-09-17 16:30:44 +0200

    Création des comptes agents uniquement avec ProConnect

--- app/controllers/admin/territories/invitations_devise_controller.rb
+++ app/controllers/admin/territories/invitations_devise_controller.rb
@@ -4,8 +4,14 @@ class Admin::Territories::InvitationsDeviseController < Devise::InvitationsContr
 
   # Bloque l'accès aux méthodes du controller parent pour éviter de permettre d'envoyer des invitations n'importe comment
   before_action :block_controller_action, except: %i[edit update] # rubocop:disable Rails/LexicallyScopedActionFilter
+  # Quand ProConnect est disponible, la création de compte par mot de passe est désactivée y compris via un POST direct
+  before_action :block_password_signup_when_pro_connect_available, only: :update # rubocop:disable Rails/LexicallyScopedActionFilter
 
   def block_controller_action
     raise Pundit::NotAuthorizedError, "not authorized"
+  end
+
+  def block_password_signup_when_pro_connect_available
+    raise Pundit::NotAuthorizedError, "not authorized" if helpers.display_pro_connect_button?
   end
 end
