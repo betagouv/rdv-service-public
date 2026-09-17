405feace4f2aee8b4250a0dda3965ec0409dde07
Antoine Girard <antoine.girard@beta.gouv.fr>
2026-09-17 14:42:13 +0200

    Simplify controller

--- app/controllers/agents/pro_connect_linking_controller.rb
+++ app/controllers/agents/pro_connect_linking_controller.rb
@@ -2,16 +2,14 @@ class Agents::ProConnectLinkingController < AgentAuthController
   layout "application_agent_config"
 
   def show
-    @agent = current_agent
-    authorize(@agent, policy_class: Agent::AgentPolicy)
+    authorize(current_agent, policy_class: Agent::AgentPolicy)
   end
 
   def create
-    @agent = current_agent
-    authorize(@agent, policy_class: Agent::AgentPolicy)
+    authorize(current_agent, policy_class: Agent::AgentPolicy)
 
-    email = @agent.email
-    sign_out(@agent)
+    email = current_agent.email
+    sign_out(current_agent)
     redirect_to pro_connect_auth_path(login_hint: email, user_type: "agent")
   end
 
