1cd4a58d53e16724e452711e87fe16f11859f981
Victor Mours <victor.mours@gmail.com>
2026-09-24 09:41:39 +0200

    extract method to model

--- app/models/user_ami_profile.rb
+++ app/models/user_ami_profile.rb
@@ -3,6 +3,16 @@ class UserAmiProfile < ApplicationRecord
 
   encrypts :fc_hash
 
+  def self.show_checkbox_in_user_form?(user, unconfirmed_rdv)
+    return false unless Ami.enabled?
+
+    unconfirmed_rdv&.organisation&.ami_enabled?
+    profile_in_ami_organisation = unconfirmed_rdv.blank? && user.organisations.where(ami_enabled: true).any?
+    hash = UserAmiProfile.find_by(user: user)
+
+    hash && (rdv_for_organisation_with_ami || profile_in_ami_organisation)
+  end
+
   def self.update_notify_by_ami(user, boolean, synchronous: false)
     return if boolean.nil? # Si le champs n'apparait pas dans le formulaire, on ne veut pas changer de valeur
     return unless Ami.enabled?
