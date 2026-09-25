2e2d4a9f65a3c7d83cb8c92b7ee189fa3c90ffee
Victor Mours <victor.mours@gmail.com>
2026-09-25 20:11:09 +0200

    Add toggle for new user btn

--- app/javascript/components/rdv-invitation-user-select.js
+++ app/javascript/components/rdv-invitation-user-select.js
@@ -3,6 +3,29 @@ export default () => {
     const select = $(elt.querySelector(".js-rdv-invitation-user-picker__select"))
     const form = elt.querySelector(".js-rdv-invitation-user-picker__form")
 
+    const newUserBtn = elt.querySelector(".js-rdv-invitation-user-picker__new-user-btn")
+
+    select.select2({
+      dropdownCssClass: 'rdv-select-2--inline',
+      minimumInputLength: 1,
+      language:  {
+        inputTooShort: () => "Commencez à taper pour chercher",
+        noResults: () => "Aucun usager trouvé."
+      },
+      ajax: {
+        url: select.data('url'),
+        dataType: "json",
+        delay: 250,
+        processResults: function(data, params) {
+          if (data.results.length == 0) {
+            newUserBtn.classList.remove("fr-btn--tertiary-no-outline")
+          } else {
+            newUserBtn.classList.add("fr-btn--tertiary-no-outline")
+          }
+          return data
+        }
+      }
+    })
 
     // Un hack pour s'assurer que le dropdown s'ouvre toujours vers le bas
     select.on("select2:open", function(e) {
