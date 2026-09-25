d8c4387ae645d85ed654020d04af346e4ccf3e1e
Victor Mours <victor.mours@gmail.com>
2026-09-25 16:04:48 +0200

    really nice select ux

--- app/javascript/components/rdv-invitation-user-select.js
+++ app/javascript/components/rdv-invitation-user-select.js
@@ -3,6 +3,8 @@ export default () => {
     const select = elt.querySelector(".js-rdv-invitation-user-picker__select")
     const form = elt.querySelector(".js-rdv-invitation-user-picker__form")
 
+    $(select).select2('open')
+
     // Les events de select2 ne semblent être utilisables que via le système d'events de jQuery, et pas avec un addEventListener normal
     $(select).on('select2:select', (e) => {
       form.querySelector("input#rdv_plan_user_id").value = e.params.data.id
